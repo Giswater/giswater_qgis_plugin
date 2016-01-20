f1ce4dae58366e605e19a91acb18091f6e82d233
David Erill <daviderill79@gmail.com>
2016-01-20 16:54:38 +0100

    close #4

---
+++ i18n/Giswater_en.ts
@@ -0,0 +1,191 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_EN">
+<context>
+    <name>Giswater</name>
+    <message>
+        <source>menu_name</source>
+        <translation>Giswater</translation>
+    </message>   
+    <message>
+        <source>New network</source>
+        <translation>Add network</translation>
+    </message>           
+    <message>
+        <source>Copy network as</source>
+        <translation>Copy network as</translation>
+    </message>           
+    <message>
+        <source>Network configuration</source>
+        <translation>Network configuration</translation>
+    </message>           
+    <message>
+        <source>Snapping tolerance</source>
+        <translation>Snapping tolerance</translation>
+    </message>           
+    <message>
+        <source>Node tolerance</source>
+        <translation>Node tolerance</translation>
+    </message>           
+    <message>
+        <source>Network management</source>
+        <translation>Network management</translation>
+    </message>           
+    <message>
+        <source>Table wizard</source>
+        <translation>Table wizard</translation>
+    </message>           
+    <message>
+        <source>Undo wizard</source>
+        <translation>Undo wizard</translation>
+    </message>  
+    <message>
+        <source>Analysis</source>
+        <translation>Analysis</translation>
+    </message>        
+    <message>
+        <source>Result selector</source>
+        <translation>Result selector</translation>
+    </message>           
+    <message>
+        <source>Flow trace node</source>
+        <translation>Flow trace node</translation>
+    </message>           
+    <message>
+        <source>Flow trace arc</source>
+        <translation>Flow trace arc</translation>
+    </message>  
+    <message>
+        <source>Go2Epa</source>
+        <translation>Go2Epa</translation>
+    </message>                 
+    <message>
+        <source>Giswater interface</source>
+        <translation>Giswater interface</translation>
+    </message>           
+    <message>
+        <source>Run simulation</source>
+        <translation>Run simulation</translation>
+    </message>       
+    <message>
+        <source>toolbar_edit_name</source>
+        <translation>Giswater - Edit</translation>
+    </message>        
+    <message>
+        <source>toolbar_epanet_name</source>
+        <translation>Giswater - EPANET</translation>
+    </message>    
+    <message>
+        <source>toolbar_swmm_name</source>
+        <translation>Giswater - SWMM</translation>
+    </message>        
+    <message>
+        <source>01_text</source>
+        <translation>Insert junction</translation>
+    </message>      
+    <message>
+        <source>02_text</source>
+        <translation>Insert outfall</translation>
+    </message>   
+    <message>
+        <source>03_text</source>
+        <translation>Insert divider</translation>
+    </message>      
+    <message>
+        <source>04_text</source>
+        <translation>Insert storage</translation>
+    </message>      
+    <message>
+        <source>05_text</source>
+        <translation>Insert conduit</translation>
+    </message>      
+    <message>
+        <source>06_text</source>
+        <translation>Insert pump</translation>
+    </message>      
+    <message>
+        <source>07_text</source>
+        <translation>Insert orifice</translation>
+    </message>      
+    <message>
+        <source>08_text</source>
+        <translation>Insert weir</translation>
+    </message>      
+    <message>
+        <source>09_text</source>
+        <translation>Insert outlet</translation>
+    </message>      
+    <message>
+        <source>10_text</source>
+        <translation>Insert junction</translation>
+    </message>      
+    <message>
+        <source>11_text</source>
+        <translation>Insert reservoir</translation>
+    </message>      
+    <message>
+        <source>12_text</source>
+        <translation>Insert tank</translation>
+    </message>      
+    <message>
+        <source>13_text</source>
+        <translation>Insert pipe</translation>
+    </message>      
+    <message>
+        <source>14_text</source>
+        <translation>Insert pump</translation>
+    </message> 
+    <message>
+        <source>15_text</source>
+        <translation>Insert xxx</translation>
+    </message>   
+    <message>
+        <source>16_text</source>
+        <translation>Move node</translation>
+    </message>      
+    <message>
+        <source>17_text</source>
+        <translation>Delete node</translation>
+    </message>      
+    <message>
+        <source>18_text</source>
+        <translation>Capture raster elevation</translation>
+    </message>      
+    <message>
+        <source>19_text</source>
+        <translation>Edit arc</translation>
+    </message>      
+    <message>
+        <source>20_text</source>
+        <translation>Table wizard</translation>
+    </message>      
+    <message>
+        <source>21_text</source>
+        <translation>Undo wizard</translation>
+    </message>      
+    <message>
+        <source>22_text</source>
+        <translation>Result selector</translation>
+    </message>      
+    <message>
+        <source>23_text</source>
+        <translation>Go2Epa</translation>
+    </message>      
+    <message>
+        <source>24_text</source>
+        <translation>Go2Epa express</translation>
+    </message>      
+    <message>
+        <source>25_text</source>
+        <translation>Connect tool</translation>
+    </message>      
+    <message>
+        <source>26_text</source>
+        <translation>Flow trace (from arc)</translation>
+    </message>      
+    <message>
+        <source>27_text</source>
+        <translation>Flow trace (from node)</translation>
+    </message>    
+</context>
+</TS>
---
+++ i18n/Giswater_es.ts
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>Giswater</name>
+    <message>
+        <source>menu_name</source>
+        <translation>Giswater</translation>
+    </message>   
+    <message>
+        <source>toolbar_edit_name</source>
+        <translation>Giswater - Edit</translation>
+    </message>        
+    <message>
+        <source>toolbar_epanet_name</source>
+        <translation>Giswater - EPANET</translation>
+    </message>    
+    <message>
+        <source>toolbar_swmm_name</source>
+        <translation>Giswater - SWMM</translation>
+    </message>        
+    <message>
+        <source>01_text</source>
+        <translation>Insert junction</translation>
+    </message>      
+    <message>
+        <source>02_text</source>
+        <translation>Insert outfall</translation>
+    </message>   
+    <message>
+        <source>03_text</source>
+        <translation>Insert divider</translation>
+    </message>      
+    <message>
+        <source>04_text</source>
+        <translation>Insert storage</translation>
+    </message>      
+    <message>
+        <source>05_text</source>
+        <translation>Insert conduit</translation>
+    </message>      
+    <message>
+        <source>06_text</source>
+        <translation>Insert pump</translation>
+    </message>      
+    <message>
+        <source>07_text</source>
+        <translation>Insert orifice</translation>
+    </message>      
+    <message>
+        <source>08_text</source>
+        <translation>Insert weir</translation>
+    </message>      
+    <message>
+        <source>09_text</source>
+        <translation>Insert outlet</translation>
+    </message>      
+    <message>
+        <source>10_text</source>
+        <translation>Insert junction</translation>
+    </message>      
+    <message>
+        <source>11_text</source>
+        <translation>Insert reservoir</translation>
+    </message>      
+    <message>
+        <source>12_text</source>
+        <translation>Insert tank</translation>
+    </message>      
+    <message>
+        <source>13_text</source>
+        <translation>Insert pipe</translation>
+    </message>      
+    <message>
+        <source>14_text</source>
+        <translation>Insert pump</translation>
+    </message> 
+    <message>
+        <source>15_text</source>
+        <translation>Insert xxx</translation>
+    </message>   
+    <message>
+        <source>16_text</source>
+        <translation>Move node</translation>
+    </message>      
+    <message>
+        <source>17_text</source>
+        <translation>Delete node</translation>
+    </message>      
+    <message>
+        <source>18_text</source>
+        <translation>Capture raster elevation</translation>
+    </message>      
+    <message>
+        <source>19_text</source>
+        <translation>Edit arc</translation>
+    </message>      
+    <message>
+        <source>20_text</source>
+        <translation>Table wizard</translation>
+    </message>      
+    <message>
+        <source>21_text</source>
+        <translation>Undo wizard</translation>
+    </message>      
+    <message>
+        <source>22_text</source>
+        <translation>Result selector</translation>
+    </message>      
+    <message>
+        <source>23_text</source>
+        <translation>Go2Epa</translation>
+    </message>      
+    <message>
+        <source>24_text</source>
+        <translation>Go2Epa express</translation>
+    </message>      
+    <message>
+        <source>25_text</source>
+        <translation>Connect tool</translation>
+    </message>      
+    <message>
+        <source>26_text</source>
+        <translation>Flow trace (from arc)</translation>
+    </message>      
+    <message>
+        <source>27_text</source>
+        <translation>Flow trace (from node)</translation>
+    </message>              
+</context>
+</TS>
