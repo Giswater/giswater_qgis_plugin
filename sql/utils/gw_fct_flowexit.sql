5681bc3cc1625d5b29d4a7fece5200966b11be0e
David Erill <bgeo@clabsa.es>
2016-05-23 15:39:35 +0200

    #21 Format code

--- sql/utils/gw_fct_flowexit.sql
+++ sql/utils/gw_fct_flowexit.sql
@@ -1,120 +1,71 @@
--- Function: SCHEMA_NAME.gw_fct_flowexit(character varying)
-
--- DROP FUNCTION SCHEMA_NAME.gw_fct_flowexit(character varying);
-
-CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_flowexit(node_id_arg character varying)
-  RETURNS json AS
-$BODY$DECLARE
-
---	JSON variables
-	Node_json json;
-	Arc_json json;
-	sql text;
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_flowexit(node_id_arg character varying) RETURNS int2 AS $BODY$
+DECLARE
  
-
 BEGIN
 
-
---	Create the temporal table for computing nodes
+	--Create the temporal table for computing nodes
 	DROP TABLE IF EXISTS "SCHEMA_NAME".temp_flow_trace_node CASCADE;
-	CREATE TABLE "SCHEMA_NAME".temp_flow_trace_node
-	(		
+	CREATE TABLE "SCHEMA_NAME".temp_flow_trace_node	(		
 		node_id character varying(16) NOT NULL,
-
---		Force indexed column (for performance)
+		-- Force indexed column (for performance)
 		CONSTRAINT temp_flow_trace_node_pkey PRIMARY KEY (node_id)
 	);
 
-
---	Create the temporal table for computing pipes
+	-- Create the temporal table for computing pipes
 	DROP TABLE IF EXISTS "SCHEMA_NAME".temp_flow_trace_arc CASCADE;
-	CREATE TABLE "SCHEMA_NAME".temp_flow_trace_arc
-	(		
+	CREATE TABLE "SCHEMA_NAME".temp_flow_trace_arc (		
 		arc_id character varying(16) NOT NULL,
-
---		Force indexed column (for performance)
+		-- Force indexed column (for performance)
 		CONSTRAINT temp_flow_trace_arc_pkey PRIMARY KEY (arc_id)
 	);
 
-
-
---	Compute the tributary area using DFS
+	-- Compute the tributary area using DFS
 	PERFORM "SCHEMA_NAME".gw_fct_flowexit_recursive(node_id_arg);
 
---	Create JSON
-	SELECT array_to_json(array_agg(node_id)) INTO Node_json FROM "SCHEMA_NAME".temp_flow_trace_node;
-	SELECT array_to_json(array_agg(arc_id)) INTO Arc_json FROM "SCHEMA_NAME".temp_flow_trace_arc;
-
---	Delete auxiliar tables
-	DROP TABLE IF EXISTS "SCHEMA_NAME".temp_flow_trace_node CASCADE;
-	DROP TABLE IF EXISTS "SCHEMA_NAME".temp_flow_trace_arc CASCADE;
-
-	sql := '{"Node": ' || Node_json || ', "Arc": ' || Arc_json || '}';
-	RETURN sql::json;
-
-
+	RETURN 0;
 		
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION SCHEMA_NAME.gw_fct_flowexit(character varying)
-  OWNER TO geoserver;
 
 
 
 
-
-
-
-
-
-
-
--- Function: SCHEMA_NAME.gw_fct_flowexit_recursive(character varying)
-
--- DROP FUNCTION SCHEMA_NAME.gw_fct_flowexit_recursive(character varying);
-
-CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_flowexit_recursive(node_id_arg character varying)
-  RETURNS void AS
-$BODY$DECLARE
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_flowexit_recursive(node_id_arg character varying) RETURNS void AS $BODY$
+DECLARE
 	exists_id character varying;
 	rec_table record;
 
-
 BEGIN
 
---	Check if the node is already computed
+	-- Check if the node is already computed
 	SELECT node_id INTO exists_id FROM "SCHEMA_NAME".temp_flow_trace_node WHERE node_id = node_id_arg;
 
---	Compute proceed
+	-- Compute proceed
 	IF NOT FOUND THEN
 	
---		Update value
+		-- Update value
 		INSERT INTO "SCHEMA_NAME".temp_flow_trace_node VALUES(node_id_arg);
 		
---		Loop for all the upstream nodes
+		-- Loop for all the upstream nodes
 		FOR rec_table IN SELECT arc_id, node_2 FROM "SCHEMA_NAME".arc WHERE node_1 = node_id_arg
 		LOOP
 
---			Insert into tables
+			-- Insert into tables
 			INSERT INTO "SCHEMA_NAME".temp_flow_trace_arc VALUES(rec_table.arc_id);
 
---			Call recursive function weighting with the pipe capacity
+			-- Call recursive function weighting with the pipe capacity
 			PERFORM "SCHEMA_NAME".gw_fct_flowexit_recursive(rec_table.node_2);
 
-
 		END LOOP;
-
 	
 	END IF;
 
 	RETURN;
-
 		
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION SCHEMA_NAME.gw_fct_flowexit_recursive(character varying)
-  OWNER TO geoserver;
+
