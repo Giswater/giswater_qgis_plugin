c4fd076c3479a0c6c4ec42d8bcb8c97861905f9b
David Erill <daviderill79@gmail.com>
2016-06-01 18:57:04 +0200

    Bug fix in v0.5

---
+++ search/ui/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+curpath = os.path.dirname(os.path.realpath(__file__))
+
+# Adding so that our UI files can find resources_rc.py which is up one level.
+sys.path.append(os.path.join(curpath, '..'))
