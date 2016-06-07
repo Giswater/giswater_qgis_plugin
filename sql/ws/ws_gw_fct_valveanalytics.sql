04d07210d8a1051964d47524f6461d359babea1f
David Erill <daviderill79@gmail.com>
2016-06-07 16:10:38 +0200

    Closes #57

--- sql/ws/ws_gw_fct_valveanalytics.sql
+++ sql/ws/ws_gw_fct_valveanalytics.sql
@@ -1,5 +1,4 @@
-﻿
-/*
+﻿/*
 This file is part of Giswater 2.0
 The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This version of Giswater is provided by Giswater Association
@@ -43,15 +42,14 @@ BEGIN
     first_row = TRUE;
     FOR rec_table IN SELECT node_id, the_geom FROM node WHERE node_id NOT IN (SELECT node_id FROM anl_mincut_node)
     LOOP
-
+    
         -- Delete old
         IF first_row THEN
-	    DELETE FROM anl_mincut_node;
-	    first_row = FALSE;
-	END IF;
-
+            DELETE FROM anl_mincut_node;
+            first_row = FALSE;
+        END IF;
         INSERT INTO anl_mincut_node VALUES(rec_table.node_id, rec_table.the_geom);
-
+        
     END LOOP; 
 
     -- Switch selection of arc
@@ -61,25 +59,23 @@ BEGIN
 
         -- Delete old
         IF first_row THEN
-	    DELETE FROM anl_mincut_arc;
-	    first_row = FALSE;
-	END IF;
+            DELETE FROM anl_mincut_arc;
+            first_row = FALSE;
+        END IF;
 
         INSERT INTO anl_mincut_arc VALUES(rec_table.arc_id, rec_table.the_geom);
 
     END LOOP; 
-    
     
     -- Contruct concave hull for included lines
     polygon_aux := ST_Multi(ST_ConcaveHull(ST_Collect(ARRAY(SELECT the_geom FROM anl_mincut_arc)), 0.80));
 
     -- Concave hull for not included lines
     polygon_aux2 := ST_Multi(ST_Buffer(ST_Collect(ARRAY(SELECT the_geom FROM arc WHERE arc_id NOT IN (SELECT a.arc_id FROM anl_mincut_arc AS a) AND ST_Intersects(the_geom, polygon_aux))), 10, 'join=mitre mitre_limit=1.0'));
-
-
+    
     -- Substract
     IF polygon_aux2 IS NOT NULL THEN
-	polygon_aux := ST_Multi(ST_Difference(polygon_aux, polygon_aux2));
+        polygon_aux := ST_Multi(ST_Difference(polygon_aux, polygon_aux2));
     ELSE
         polygon_aux := polygon_aux;
     END IF;
@@ -94,8 +90,6 @@ END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-
-
 
   
 
