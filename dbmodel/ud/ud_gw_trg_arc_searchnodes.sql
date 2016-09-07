aba168e118e64c28db4a9f10d50368167ec63e79
Xavier Torret <xtorret@bgeo.es>
2016-09-07 03:20:17 +0200

    Bug fix on arcsearchnodes trigger. Unification with arc_geometry_update

--- dbmodel/ud/ud_gw_trg_arc_searchnodes.sql
+++ dbmodel/ud/ud_gw_trg_arc_searchnodes.sql
@@ -6,7 +6,6 @@ This version of Giswater is provided by Giswater Association
 
 
 
-
 CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_arc_searchnodes() 
 RETURNS trigger  LANGUAGE plpgsql    AS
 $$
@@ -35,6 +34,10 @@ BEGIN
 
 	SELECT * INTO optionsRecord FROM inp_options LIMIT 1;
 
+    -- UPDATE dma/sector
+    NEW.sector_id:= (SELECT sector_id FROM sector WHERE ST_DWithin(ST_centroid(NEW.the_geom), sector.the_geom,0.001) LIMIT 1);          
+    NEW.dma_id := (SELECT dma_id FROM dma WHERE ST_DWithin(NEW.the_geom, dma.the_geom,0.001) LIMIT 1);   
+
     -- Control of start/end node
     IF (nodeRecord1.node_id IS NOT NULL) AND (nodeRecord2.node_id IS NOT NULL) THEN	
 	
@@ -43,49 +46,89 @@ BEGIN
 			RETURN audit_function (180,750);
 		ELSE
 
---  	Update coordinates
-		NEW.the_geom := ST_SetPoint(NEW.the_geom, 0, nodeRecord1.the_geom);
-		NEW.the_geom := ST_SetPoint(NEW.the_geom, ST_NumPoints(NEW.the_geom) - 1, nodeRecord2.the_geom);
+--  		Update coordinates
+			NEW.the_geom := ST_SetPoint(NEW.the_geom, 0, nodeRecord1.the_geom);
+			NEW.the_geom := ST_SetPoint(NEW.the_geom, ST_NumPoints(NEW.the_geom) - 1, nodeRecord2.the_geom);
 		
-		IF (optionsRecord.link_offsets = 'DEPTH') THEN
-			z1 := (nodeRecord1.top_elev - NEW.y1);
-			z2 := (nodeRecord2.top_elev - NEW.y2);
-			ELSE
-				z1 := NEW.y1;
-				z2 := NEW.y2;	
-		END IF;
+			IF (optionsRecord.link_offsets = 'DEPTH') THEN
+				z1 := (nodeRecord1.top_elev - NEW.y1);
+				z2 := (nodeRecord2.top_elev - NEW.y2);
+				ELSE
+					z1 := NEW.y1;
+					z2 := NEW.y2;	
+			END IF;
 
-		IF ((z1 > z2) AND NEW.inverted_slope is false) OR ((z1 < z2) AND NEW.inverted_slope is true) THEN
-
-			NEW.node_1 := nodeRecord1.node_id; 
-			NEW.node_2 := nodeRecord2.node_id;
-
-		ELSE 
+			IF ((z1 > z2) AND NEW.inverted_slope is false) OR ((z1 < z2) AND NEW.inverted_slope is true) THEN
+				NEW.node_1 := nodeRecord1.node_id; 
+				NEW.node_2 := nodeRecord2.node_id;
+			ELSE 
 
 --			Update conduit direction
-			NEW.the_geom := ST_reverse(NEW.the_geom);
-			z_aux := NEW.y1;
-			NEW.y1 := NEW.y2;
-			NEW.y2 := z_aux;
+				NEW.the_geom := ST_reverse(NEW.the_geom);
+				z_aux := NEW.y1;
+				NEW.y1 := NEW.y2;
+				NEW.y2 := z_aux;
+				NEW.node_1 := nodeRecord2.node_id;
+				NEW.node_2 := nodeRecord1.node_id;
+				RETURN NEW;
+			END IF;
 
---  Update topology info
-			NEW.node_1 := nodeRecord2.node_id;
-			NEW.node_2 := nodeRecord1.node_id;
+-- Select arcs with start-end on the updated node
+            FOR vnoderec IN SELECT * FROM vnode WHERE ST_DWithin(OLD.the_geom, the_geom, 0.01)
+            LOOP
+
+                -- Update vnode geometry
+                newPoint := ST_LineInterpolatePoint(NEW.the_geom, ST_LineLocatePoint(OLD.the_geom, vnoderec.the_geom));
+                UPDATE vnode SET the_geom = newPoint WHERE vnode_id = vnoderec.vnode_id;
+
+                -- Update link
+                connecPoint := (SELECT the_geom FROM connec WHERE connec_id IN (SELECT a.connec_id FROM link AS a WHERE a.vnode_id = vnoderec.vnode_id));
+                UPDATE link SET the_geom = ST_MakeLine(connecPoint, newPoint) WHERE vnode_id = vnoderec.vnode_id;
+
+            END LOOP; 
+			RETURN NEW;
 		END IF;
+
+
+
+ -- Check auto insert end nodes
+    ELSIF (nodeRecord1.node_id IS NOT NULL) AND (SELECT nodeinsert_arcendpoint FROM config) THEN
+
+        INSERT INTO node (node_id, sector_id, epa_type, nodecat_id, dma_id, the_geom) 
+            VALUES (
+                (SELECT nextval('node_id_seq')),
+                (SELECT sector_id FROM sector WHERE (ST_endpoint(NEW.the_geom) @ sector.the_geom) LIMIT 1), 
+                'JUNCTION', 
+                (SELECT nodeinsert_catalog_vdefault FROM config), 
+                (SELECT dma_id FROM dma WHERE (ST_endpoint(NEW.the_geom) @ dma.the_geom) LIMIT 1), 
+                ST_endpoint(NEW.the_geom)
+            );
+
+        INSERT INTO inp_junction (node_id) VALUES ((SELECT currval('node_id_seq')));
+        INSERT INTO man_junction (node_id) VALUES ((SELECT currval('node_id_seq')));
+
+        -- Update coordinates
+        NEW.the_geom:= ST_SetPoint(NEW.the_geom, 0, nodeRecord1.the_geom);
+        NEW.node_1:= nodeRecord1.node_id; 
+        NEW.node_2:= (SELECT currval('node_id_seq'));
+                
         RETURN NEW;
-	END IF;
-	ELSE
-	RETURN audit_function (182,750);
-    RETURN NULL;
+
+    -- Error, no existing nodes
+		
+	ELSIF ((nodeRecord1.node_id IS NULL) OR (nodeRecord2.node_id IS NULL)) AND (rec.arc_searchnodes_control IS TRUE) THEN
+        RETURN audit_function (182,330);
+        RETURN NULL;  
+    ELSE
+		RETURN audit_function (182,750);
+		RETURN NULL;
     END IF;
 END; 
 $$;
 
 
-
-
 DROP TRIGGER IF EXISTS gw_trg_arc_searchnodes ON "SCHEMA_NAME"."arc";
-CREATE TRIGGER gw_trg_arc_searchnodes BEFORE INSERT OR UPDATE ON "SCHEMA_NAME"."arc" 
+CREATE TRIGGER gw_trg_arc_searchnodes BEFORE INSERT OR UPDATE OF the_geom ON "SCHEMA_NAME"."arc" 
 FOR EACH ROW EXECUTE PROCEDURE "SCHEMA_NAME"."gw_trg_arc_searchnodes"();
 
 
--- dbmodel/ws/ws_gw_trg_arc_searchnodes.sql
+++ dbmodel/ws/ws_gw_trg_arc_searchnodes.sql
@@ -5,52 +5,71 @@ This version of Giswater is provided by Giswater Association
 */
 
 
-CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_arc_searchnodes() RETURNS trigger LANGUAGE plpgsql AS $$
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_trg_arc_searchnodes()
+  RETURNS trigger AS
+$BODY$
 DECLARE 
     nodeRecord1 record; 
     nodeRecord2 record; 
-    rec record;    
+    rec record;  
+    vnoderec record;
+    newPoint public.geometry;    
+    connecPoint public.geometry;
 
 BEGIN 
 
     EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
-
-    SET LC_MESSAGES TO 'es_ES.UTF-8';
     
     -- Get data from config table
-    SELECT * INTO rec FROM config; 
+    SELECT * INTO rec FROM config;    
 
     SELECT * INTO nodeRecord1 FROM node WHERE ST_DWithin(ST_startpoint(NEW.the_geom), node.the_geom, rec.arc_searchnodes)
-    ORDER BY ST_Distance(node.the_geom, ST_startpoint(NEW.the_geom)) LIMIT 1;
+	ORDER BY ST_Distance(node.the_geom, ST_startpoint(NEW.the_geom)) LIMIT 1;
 
     SELECT * INTO nodeRecord2 FROM node WHERE ST_DWithin(ST_endpoint(NEW.the_geom), node.the_geom, rec.arc_searchnodes)
     ORDER BY ST_Distance(node.the_geom, ST_endpoint(NEW.the_geom)) LIMIT 1;
 
-    -- Control of start/end node
-    IF (nodeRecord1.node_id IS NOT NULL) AND (nodeRecord2.node_id IS NOT NULL)  THEN
+    -- UPDATE dma/sector
+    NEW.sector_id:= (SELECT sector_id FROM sector WHERE ST_DWithin(ST_centroid(NEW.the_geom), sector.the_geom,0.001) LIMIT 1);          
+    NEW.dma_id := (SELECT dma_id FROM dma WHERE ST_DWithin(NEW.the_geom, dma.the_geom,0.001) LIMIT 1);         
 
+    -- Control of length line
+    IF (nodeRecord1.node_id IS NOT NULL) AND (nodeRecord2.node_id IS NOT NULL) THEN
+    
         -- Control of same node initial and final
         IF (nodeRecord1.node_id = nodeRecord2.node_id) AND (rec.samenode_init_end_control IS TRUE) THEN
-            RETURN audit_function (180,330);
-
+            RETURN audit_function (180,120);
         ELSE
-        
+		
             -- Update coordinates
             NEW.the_geom:= ST_SetPoint(NEW.the_geom, 0, nodeRecord1.the_geom);
             NEW.the_geom:= ST_SetPoint(NEW.the_geom, ST_NumPoints(NEW.the_geom) - 1, nodeRecord2.the_geom);
             NEW.node_1:= nodeRecord1.node_id; 
             NEW.node_2:= nodeRecord2.node_id;
-            
-            RETURN NEW;
-            
-        END IF;
 
+            -- Select arcs with start-end on the updated node
+            FOR vnoderec IN SELECT * FROM vnode WHERE ST_DWithin(OLD.the_geom, the_geom, 0.01)
+            LOOP
+
+                -- Update vnode geometry
+                newPoint := ST_LineInterpolatePoint(NEW.the_geom, ST_LineLocatePoint(OLD.the_geom, vnoderec.the_geom));
+                UPDATE vnode SET the_geom = newPoint WHERE vnode_id = vnoderec.vnode_id;
+
+                -- Update link
+                connecPoint := (SELECT the_geom FROM connec WHERE connec_id IN (SELECT a.connec_id FROM link AS a WHERE a.vnode_id = vnoderec.vnode_id));
+                UPDATE link SET the_geom = ST_MakeLine(connecPoint, newPoint) WHERE vnode_id = vnoderec.vnode_id;
+
+            END LOOP; 
+            RETURN NEW;
+ 
+        END IF;
+		
     -- Check auto insert end nodes
     ELSIF (nodeRecord1.node_id IS NOT NULL) AND (SELECT nodeinsert_arcendpoint FROM config) THEN
 
         INSERT INTO node (node_id, sector_id, epa_type, nodecat_id, dma_id, the_geom) 
             VALUES (
-                (SELECT nextval('test_ws_0817.node_id_seq')),
+                (SELECT nextval('node_id_seq')),
                 (SELECT sector_id FROM sector WHERE (ST_endpoint(NEW.the_geom) @ sector.the_geom) LIMIT 1), 
                 'JUNCTION', 
                 (SELECT nodeinsert_catalog_vdefault FROM config), 
@@ -58,33 +77,34 @@ BEGIN
                 ST_endpoint(NEW.the_geom)
             );
 
-        INSERT INTO inp_junction (node_id) VALUES ((SELECT currval('test_ws_0817.node_id_seq')));
-        INSERT INTO man_junction (node_id) VALUES ((SELECT currval('test_ws_0817.node_id_seq')));
+        INSERT INTO inp_junction (node_id) VALUES ((SELECT currval('node_id_seq')));
+        INSERT INTO man_junction (node_id) VALUES ((SELECT currval('node_id_seq')));
 
         -- Update coordinates
         NEW.the_geom:= ST_SetPoint(NEW.the_geom, 0, nodeRecord1.the_geom);
         NEW.node_1:= nodeRecord1.node_id; 
-        NEW.node_2:= (SELECT currval('test_ws_0817.node_id_seq'));
+        NEW.node_2:= (SELECT currval('node_id_seq'));
                 
         RETURN NEW;
 
     -- Error, no existing nodes
-       
-    ELSIF ((nodeRecord1.node_id IS NULL) OR (nodeRecord2.node_id IS NULL)) AND (rec.arc_searchnodes_control IS TRUE) THEN
-        PERFORM audit_function (182,330);
+		
+	ELSIF ((nodeRecord1.node_id IS NULL) OR (nodeRecord2.node_id IS NULL)) AND (rec.arc_searchnodes_control IS TRUE) THEN
+        RETURN audit_function (182,330);
         RETURN NULL;
-
+        
     ELSE
-        INSERT INTO anl_arc_no_startend_node (arc_id, the_geom) VALUES (NEW.arc_id, NEW.the_geom);
-        RETURN NEW;
-
+        RETURN audit_function (182,120);
+        RETURN NULL;
     END IF;
 
-END;
-$$;
+END; 
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
 
 
 DROP TRIGGER IF EXISTS gw_trg_arc_searchnodes ON "SCHEMA_NAME"."arc";
-CREATE TRIGGER gw_trg_arc_searchnodes BEFORE INSERT OR UPDATE ON "SCHEMA_NAME"."arc" 
-FOR EACH ROW EXECUTE PROCEDURE "SCHEMA_NAME"."gw_trg_arc_searchnodes"();
+CREATE TRIGGER gw_trg_arc_searchnodes BEFORE INSERT OR UPDATE OF the_geom ON SCHEMA_NAME.arc
+FOR EACH ROW  EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_arc_searchnodes();
 
