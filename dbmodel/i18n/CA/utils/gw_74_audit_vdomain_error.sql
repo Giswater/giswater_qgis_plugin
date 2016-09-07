c8efe355b054d8ce7093f63c50917e9622b37714
Xavier Torret <xtorret@bgeo.es>
2016-09-07 14:04:09 +0200

    Update code error on trigger function error message catalog

--- dbmodel/i18n/CA/utils/gw_74_audit_vdomain_error.sql
+++ dbmodel/i18n/CA/utils/gw_74_audit_vdomain_error.sql
@@ -21,30 +21,30 @@ INSERT INTO audit_cat_error VALUES ('2', 'Trigger UPDATE','Updated', '3', 'f', n
 INSERT INTO audit_cat_error VALUES ('3', 'Trigger DELETE','Deleted', '3', 'f', null);
 
 -- Trigger messages (audit_cat_error.id between 101 and 499) 
-INSERT INTO audit_cat_error VALUES ('100', 'Test trigger', 'Trigger test', '1', 't', 'ws_trg');
-INSERT INTO audit_cat_error VALUES ('105', 'There are no nodes types defined in the model', 'Define at least one', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('110', 'There are no nodes catalog defined in the model', 'Define at least one', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('115', 'There are no sectors defined in the model', 'Define at least one', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('120', 'Feature is out of sector','Please take a look on your map and use the approach of the sectors!', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('125', 'There are no dma defined in the model', 'Define at least one', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('130', 'Feature is out of dma','Please take a look on your map and use the approach of the dma', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('135', 'Change node catalog is forbidden','The new node catalog is not included on the same type (node_type.type) of the old node catalog', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('140', 'There are no arc types defined in the model','Define at least one','1', 't', null);
-INSERT INTO audit_cat_error VALUES ('145', 'There are no arc catalog defined in the model','Define at least one','1', 't', null);
-INSERT INTO audit_cat_error VALUES ('150', 'There are no connec catalog defined in the model','Define at least one', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('152', 'There are no grate catalog defined in the model','Define at least one', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('155', 'Insert a new arc in this table is not allowed', 'To insert new arc, please use the GIS FEATURES layer arc', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('157', 'Delete arcs from this table is not allowed', 'To delete arcs, please use the GIS FEATURES layer arc', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('160', 'Insert a new node in this table is not allowed', 'To insert new node, please use the GIS FEATURES layer node', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('163', 'Delete nodes from this table is not allowed', 'To delete nodes, please use the GIS FEATURES layer node', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('165', 'Insert a new valve in this table is not allowed', 'To insert new valve, please use the GIS FEATURES layer node', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('170', 'There are columns in this table not allowed to edit', 'Try to update open, accesibility, broken, mincut_anl or hydraulic_anl', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('175', 'Delete valves from this table is not allowed', 'To delete valves, please use the GIS FEATURES layer node', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('180', 'One or more arcs has the same Node as Node1 and Node2', 'Please, check your project or modify the configuration propierties', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('182', 'One or more arcs was not inserted because it has not start/end node', 'Please, check your project or modify the configuration propierties', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('185', 'Exists one o more connecs closer than minimum configured,', 'Please, check your project or modify the configuration propierties', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('190', 'Exists one o more nodes closer than minimum configured,', 'Please, check your project or modify the configuration propierties', '1', 't', null);
-INSERT INTO audit_cat_error VALUES ('200', 'Elev is not an updatable column', 'Please use top_elev or ymax to modify this value', '1', 't', null);
+INSERT INTO audit_cat_error VALUES ('100', 'Test trigger', 'Trigger test', '0', 't', 'ws_trg');
+INSERT INTO audit_cat_error VALUES ('105', 'There are no nodes types defined in the model', 'Define at least one', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('110', 'There are no nodes catalog defined in the model', 'Define at least one', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('115', 'There are no sectors defined in the model', 'Define at least one', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('120', 'Feature is out of sector','Please take a look on your map and use the approach of the sectors!', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('125', 'There are no dma defined in the model', 'Define at least one', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('130', 'Feature is out of dma','Please take a look on your map and use the approach of the dma', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('135', 'Change node catalog is forbidden','The new node catalog is not included on the same type (node_type.type) of the old node catalog', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('140', 'There are no arc types defined in the model','Define at least one','2', 't', null);
+INSERT INTO audit_cat_error VALUES ('145', 'There are no arc catalog defined in the model','Define at least one','2', 't', null);
+INSERT INTO audit_cat_error VALUES ('150', 'There are no connec catalog defined in the model','Define at least one', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('152', 'There are no grate catalog defined in the model','Define at least one', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('155', 'Insert a new arc in this table is not allowed', 'To insert new arc, please use the GIS FEATURES layer arc', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('157', 'Delete arcs from this table is not allowed', 'To delete arcs, please use the GIS FEATURES layer arc', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('160', 'Insert a new node in this table is not allowed', 'To insert new node, please use the GIS FEATURES layer node', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('163', 'Delete nodes from this table is not allowed', 'To delete nodes, please use the GIS FEATURES layer node', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('165', 'Insert a new valve in this table is not allowed', 'To insert new valve, please use the GIS FEATURES layer node', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('170', 'There are columns in this table not allowed to edit', 'Try to update open, accesibility, broken, mincut_anl or hydraulic_anl', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('175', 'Delete valves from this table is not allowed', 'To delete valves, please use the GIS FEATURES layer node', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('180', 'One or more arcs has the same Node as Node1 and Node2', 'Please, check your project or modify the configuration propierties', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('182', 'One or more arcs was not inserted because it has not start/end node', 'Please, check your project or modify the configuration propierties', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('185', 'Exists one o more connecs closer than minimum configured,', 'Please, check your project or modify the configuration propierties', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('190', 'Exists one o more nodes closer than minimum configured,', 'Please, check your project or modify the configuration propierties', '2', 't', null);
+INSERT INTO audit_cat_error VALUES ('200', 'Elev is not an updatable column', 'Please use top_elev or ymax to modify this value', '2', 't', null);
 
 
 -- Function messages (audit_cat_error.id between 501 and 998)
