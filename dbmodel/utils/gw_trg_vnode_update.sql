63289b707c2c94a75f8027930b50b93d0abe6364
Xavier Torret <xtorret@bgeo.es>
2016-09-07 03:38:31 +0200

    Bug fix on trigger update_vnode

--- dbmodel/utils/gw_trg_vnode_update.sql
+++ dbmodel/utils/gw_trg_vnode_update.sql
@@ -1,6 +1,9 @@
 
-CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_vnode_update() RETURNS trigger AS $BODY$
+CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_vnode_update() RETURNS trigger AS
+$BODY$
+
 DECLARE
+
     connecPoint geometry;
     arcPoint geometry;
     linkrec record;
@@ -16,25 +19,27 @@ BEGIN
         -- Update link
         connecPoint := (SELECT the_geom FROM connec WHERE connec_id = linkrec.connec_id);
         UPDATE link 
-        SET the_geom = ST_MakeLine(connecPoint, NEW.the_geom) 
-        WHERE link_id = linkrec.link_id;
-
-        -- Update vnode
-        --	SELECT * INTO arcrec FROM arc WHERE ST_DWithin((NEW.the_geom), arc.the_geom, rec.arc_searchnodes) ORDER BY ST_Distance(arc.the_geom, (NEW.the_geom)) LIMIT 1;
-        --	UPDATE vnode
-        --	SET arc_id = arcrec WHERE vnode_id = NEW.vnode_id, 
+	    SET the_geom = ST_MakeLine(connecPoint, NEW.the_geom) WHERE link_id = linkrec.link_id;
 
     END LOOP;
 
-    RETURN OLD;
-    
+    -- Update vnode
+    SELECT * INTO arcrec FROM arc WHERE ST_DWithin((NEW.the_geom), arc.the_geom, 0.01) ORDER BY ST_Distance(arc.the_geom, (NEW.the_geom)) LIMIT 1;
+     
+    IF arcrec.arc_id IS NOT NULL THEN
+        NEW.arc_id = arcrec.arc_id;
+    END IF;
+
+    RETURN NEW;
+
 END;
+
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
 
   
 DROP TRIGGER IF EXISTS gw_trg_vnode_update ON "SCHEMA_NAME"."vnode";
-CREATE TRIGGER gw_trg_vnode_update AFTER UPDATE OF the_geom ON "SCHEMA_NAME"."vnode"
+CREATE TRIGGER gw_trg_vnode_update BEFORE UPDATE OF the_geom ON "SCHEMA_NAME"."vnode"
 FOR EACH ROW EXECUTE PROCEDURE "SCHEMA_NAME"."gw_trg_vnode_update"();
 
