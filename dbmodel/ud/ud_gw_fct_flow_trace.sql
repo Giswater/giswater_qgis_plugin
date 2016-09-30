c69c4f340a2f4153acd6450e07f497fdd9029dfe
David Erill <daviderill79@gmail.com>
2016-09-30 12:05:58 +0200

    Bug fix UTF-8 without BOM

--- dbmodel/ud/ud_gw_fct_flow_trace.sql
+++ dbmodel/ud/ud_gw_fct_flow_trace.sql
@@ -1,11 +1,11 @@
-﻿/*
+/*
 This file is part of Giswater 2.0
 The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This version of Giswater is provided by Giswater Association
 */
 
+
 CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_flow_trace(node_id_arg character varying)  RETURNS smallint AS $BODY$
-DECLARE
  
 BEGIN
 
--- dbmodel/ud/ud_gw_fct_flow_trace_recursive.sql
+++ dbmodel/ud/ud_gw_fct_flow_trace_recursive.sql
@@ -1,11 +1,11 @@
-﻿/*
+/*
 This file is part of Giswater 2.0
 The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This version of Giswater is provided by Giswater Association
 */
 
 
-CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_fct_flow_trace_recursive(node_id_arg character varying)  RETURNS void AS $BODY$
+CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_fct_flow_trace_recursive(node_id_arg character varying) RETURNS void AS $BODY$
 DECLARE
     exists_id character varying;
     rec_table record;
