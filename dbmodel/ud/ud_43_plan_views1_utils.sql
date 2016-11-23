8c9453b8f46a36df53a5a084d173dadb2d2820f9
Xavier Torret <xtorret@bgeo.es>
2016-11-23 15:30:19 +0100

    Improve the strategy to show prices incorporating v_price_arc & v_price_node

--- dbmodel/ud/ud_43_plan_views1_utils.sql
+++ dbmodel/ud/ud_43_plan_views1_utils.sql
@@ -4,13 +4,11 @@ The program is free software: you can redistribute it and/or modify it under the
 This version of Giswater is provided by Giswater Association
 */
 
-
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
+
 -- COMMON SQL (WS & UD)
 
 DROP VIEW IF EXISTS "v_price_compost" CASCADE; 
-
-
 CREATE VIEW "v_price_compost" AS 
 SELECT
   price_compost.id,
@@ -24,10 +22,9 @@ LEFT JOIN price_simple ON (((price_simple.id)::text = (price_compost_value.simpl
 GROUP BY price_compost.id, price_compost.unit, price_compost.descript;
 
 
-DROP VIEW IF EXISTS "v_price_x_catsoil1" CASCADE;
+DROP VIEW IF EXISTS "v_price_x_catsoil1"  CASCADE;
 CREATE VIEW "v_price_x_catsoil1" AS 
 SELECT
-
   cat_soil.id,
   cat_soil.y_param,
   cat_soil.b,
@@ -44,7 +41,6 @@ SELECT
   v_price_compost.price AS m3fill_cost
 FROM (cat_soil
 JOIN v_price_compost ON (((cat_soil."m3fill_cost")::text = (v_price_compost.id)::text)));
-
 
 
 DROP VIEW IF EXISTS "v_price_x_catsoil3" CASCADE;
@@ -66,7 +62,6 @@ JOIN v_price_compost ON (((cat_soil."m2trenchl_cost")::text = (v_price_compost.i
 WHERE (((cat_soil.m2trenchl_cost)::text = (v_price_compost.id)::text)  OR  (cat_soil.m2trenchl_cost)::text = null);
 
 
-
 DROP VIEW IF EXISTS "v_price_x_catsoil" CASCADE;
 CREATE VIEW "v_price_x_catsoil" AS
 SELECT
@@ -85,7 +80,130 @@ LEFT JOIN v_price_x_catsoil4 ON (((v_price_x_catsoil4.id)::text = (v_price_x_cat
 );
 
 
+DROP VIEW IF EXISTS "v_price_x_catsoil" CASCADE;
+CREATE VIEW v_price_x_arc AS
+SELECT
+ arc_id,
+ cat_arc.id as catalog_id, 
+ v_price_compost.id as price_id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN cat_arc ON cat_arc.id::text = arc.arccat_id
+   JOIN v_price_compost ON cat_arc.cost::text = v_price_compost.id::text
+UNION
+ SELECT
+ arc_id,
+ cat_arc.id as catalog_id, 
+ v_price_compost.id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN cat_arc ON cat_arc.id::text = arc.arccat_id
+   JOIN v_price_compost ON cat_arc.m2bottom_cost::text = v_price_compost.id::text
+UNION
+ SELECT
+ arc_id,
+ cat_arc.id as catalog_id, 
+ v_price_compost.id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN cat_arc ON cat_arc.id::text = arc.arccat_id
+   JOIN v_price_compost ON cat_arc.m3protec_cost::text = v_price_compost.id::text
+UNION
+  SELECT
+ arc_id,
+ cat_soil.id, 
+ v_price_compost.id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN cat_soil ON cat_soil.id::text = arc.soilcat_id
+   JOIN v_price_compost ON cat_soil.m3exc_cost::text = v_price_compost.id::text
+UNION
+  SELECT
+ arc_id,
+ cat_soil.id, 
+ v_price_compost.id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN cat_soil ON cat_soil.id::text = arc.soilcat_id
+   JOIN v_price_compost ON cat_soil.m3fill_cost::text = v_price_compost.id::text
+UNION
+  SELECT
+ arc_id,
+ cat_soil.id, 
+ v_price_compost.id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN cat_soil ON cat_soil.id::text = arc.soilcat_id
+   JOIN v_price_compost ON cat_soil.m3excess_cost::text = v_price_compost.id::text
+UNION
+  SELECT
+ arc_id,
+ cat_soil.id, 
+ v_price_compost.id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN cat_soil ON cat_soil.id::text = arc.soilcat_id
+   JOIN v_price_compost ON cat_soil.m2trenchl_cost::text = v_price_compost.id::text
+UNION
+ SELECT
+ arc.arc_id,
+ cat_pavement.id as catalog_id, 
+ v_price_compost.id as price_id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN plan_arc_x_pavement ON plan_arc_x_pavement.arc_id=arc.arc_id
+   JOIN cat_pavement ON cat_pavement.id::text = plan_arc_x_pavement.pavcat_id
+   JOIN v_price_compost ON cat_pavement.m2_cost::text = v_price_compost.id::text
+
+   order by arc_id,catalog_id
 
 
+
+DROP VIEW IF EXISTS "v_price_x_node" CASCADE;
+CREATE OR REPLACE VIEW v_price_x_node AS 
+ SELECT 
+ node.node_id, 
+    node.nodecat_id,
+    v_price_compost.unit,
+    v_price_compost.descript,
+    v_price_compost.price,
+        CASE
+            WHEN v_price_x_catnode.cost_unit::text = 'u'::text THEN NULL::numeric
+            ELSE 
+            CASE
+                WHEN (node.ymax * 1::numeric) = 0::numeric THEN v_price_x_catnode.estimated_y
+                ELSE node.ymax / 2::numeric
+            END
+        END::numeric(12,2) AS calculated_depth, 
+
+        CASE
+            WHEN v_price_x_catnode.cost_unit::text = 'u'::text THEN v_price_x_catnode.cost
+            ELSE 
+            CASE
+                WHEN (node.ymax * 1::numeric) = 0::numeric THEN v_price_x_catnode.estimated_y
+                ELSE (node.ymax / 2::numeric)::numeric(12,2)
+            END * v_price_x_catnode.cost
+        END::numeric(12,2) AS budget
+
+   FROM v_node node
+   LEFT JOIN v_price_x_catnode ON node.nodecat_id::text = v_price_x_catnode.id::text
+   JOIN cat_node on cat_node.id=node.nodecat_id
+   JOIN v_price_compost ON v_price_compost.id=cat_node.cost;
 
 
--- dbmodel/ws/ws_43_plan_views1_utils.sql
+++ dbmodel/ws/ws_43_plan_views1_utils.sql
@@ -80,3 +80,130 @@ LEFT JOIN v_price_x_catsoil4 ON (((v_price_x_catsoil4.id)::text = (v_price_x_cat
 );
 
 
+DROP VIEW IF EXISTS "v_price_x_catsoil" CASCADE;
+CREATE VIEW v_price_x_arc AS
+SELECT
+ arc_id,
+ cat_arc.id as catalog_id, 
+ v_price_compost.id as price_id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN cat_arc ON cat_arc.id::text = arc.arccat_id
+   JOIN v_price_compost ON cat_arc.cost::text = v_price_compost.id::text
+UNION
+ SELECT
+ arc_id,
+ cat_arc.id as catalog_id, 
+ v_price_compost.id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN cat_arc ON cat_arc.id::text = arc.arccat_id
+   JOIN v_price_compost ON cat_arc.m2bottom_cost::text = v_price_compost.id::text
+UNION
+ SELECT
+ arc_id,
+ cat_arc.id as catalog_id, 
+ v_price_compost.id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN cat_arc ON cat_arc.id::text = arc.arccat_id
+   JOIN v_price_compost ON cat_arc.m3protec_cost::text = v_price_compost.id::text
+UNION
+  SELECT
+ arc_id,
+ cat_soil.id, 
+ v_price_compost.id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN cat_soil ON cat_soil.id::text = arc.soilcat_id
+   JOIN v_price_compost ON cat_soil.m3exc_cost::text = v_price_compost.id::text
+UNION
+  SELECT
+ arc_id,
+ cat_soil.id, 
+ v_price_compost.id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN cat_soil ON cat_soil.id::text = arc.soilcat_id
+   JOIN v_price_compost ON cat_soil.m3fill_cost::text = v_price_compost.id::text
+UNION
+  SELECT
+ arc_id,
+ cat_soil.id, 
+ v_price_compost.id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN cat_soil ON cat_soil.id::text = arc.soilcat_id
+   JOIN v_price_compost ON cat_soil.m3excess_cost::text = v_price_compost.id::text
+UNION
+  SELECT
+ arc_id,
+ cat_soil.id, 
+ v_price_compost.id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN cat_soil ON cat_soil.id::text = arc.soilcat_id
+   JOIN v_price_compost ON cat_soil.m2trenchl_cost::text = v_price_compost.id::text
+UNION
+ SELECT
+ arc.arc_id,
+ cat_pavement.id as catalog_id, 
+ v_price_compost.id as price_id,
+ v_price_compost.unit,
+ v_price_compost.descript,
+ v_price_compost.price AS cost
+   FROM arc
+   JOIN plan_arc_x_pavement ON plan_arc_x_pavement.arc_id=arc.arc_id
+   JOIN cat_pavement ON cat_pavement.id::text = plan_arc_x_pavement.pavcat_id
+   JOIN v_price_compost ON cat_pavement.m2_cost::text = v_price_compost.id::text
+
+   order by arc_id,catalog_id
+
+
+
+DROP VIEW IF EXISTS "v_price_x_node" CASCADE;
+CREATE OR REPLACE VIEW v_price_x_node AS 
+ SELECT 
+ node.node_id, 
+    node.nodecat_id,
+    v_price_compost.unit,
+    v_price_compost.descript,
+    v_price_compost.price,
+        CASE
+            WHEN v_price_x_catnode.cost_unit::text = 'u'::text THEN NULL::numeric
+            ELSE 
+            CASE
+                WHEN (node.ymax * 1::numeric) = 0::numeric THEN v_price_x_catnode.estimated_y
+                ELSE node.ymax / 2::numeric
+            END
+        END::numeric(12,2) AS calculated_depth, 
+
+        CASE
+            WHEN v_price_x_catnode.cost_unit::text = 'u'::text THEN v_price_x_catnode.cost
+            ELSE 
+            CASE
+                WHEN (node.ymax * 1::numeric) = 0::numeric THEN v_price_x_catnode.estimated_y
+                ELSE (node.ymax / 2::numeric)::numeric(12,2)
+            END * v_price_x_catnode.cost
+        END::numeric(12,2) AS budget
+
+   FROM v_node node
+   LEFT JOIN v_price_x_catnode ON node.nodecat_id::text = v_price_x_catnode.id::text
+   JOIN cat_node on cat_node.id=node.nodecat_id
+   JOIN v_price_compost ON v_price_compost.id=cat_node.cost;
+
+
