e9c837005fd1c8a4d7aeeb51da52924979b4b401
Xavier Torret <xtorret@bgeo.es>
2016-12-05 21:10:36 +0100

    SQL function to insert/update/delete connec's & hydrometer's from CRM DB

---
+++ dbmodel/extra/custom_tools/br_sp_sc_dae/dae_gw_fct_comercial2gis_hydrometer_data.sql
@@ -0,0 +1,101 @@
+/*
+This file is part of Giswater 2.0
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_comercial2gis_hydrometer_data()
+  RETURNS void AS
+$BODY$
+DECLARE
+rec_hydrometer record;
+text_category_type varchar;
+id_last varchar;
+int_count integer;
+rec_connec record;
+text_sector varchar;
+text_dma varchar;
+text_hydrometer_category varchar;
+counter integer;
+
+BEGIN
+
+    SET search_path = "SCHEMA_NAME", public;
+
+
+-- INSERT NEW HYDROMETER (AND CONNEC IF NOT EXISTS)
+
+FOR rec_hydrometer IN SELECT 
+connec_id,
+category_type,
+connec.the_geom as c_the_geom,
+ext_rtc_hydrometer.code,
+ext_hydrometer_category.observ,
+hydrometer_id,
+hydrometer_category,
+v_lote.the_geom as v_the_geom
+FROM ext_rtc_hydrometer 
+JOIN connec ON connec.connec_id = ext_rtc_hydrometer.code 
+JOIN v_lote ON v_lote.iptu=ext_rtc_hydrometer.code
+JOIN ext_hydrometer_category ON ext_hydrometer_category.id=ext_rtc_hydrometer.hydrometer_category::text
+WHERE hydrometer_id::integer NOT IN (SELECT hydrometer_id::integer FROM rtc_hydrometer)
+LOOP
+    IF rec_hydrometer.connec_id IS NULL THEN
+
+        text_sector:= (SELECT sector_id FROM sector WHERE ST_DWithin(rec_hydrometer.v_the_geom.the_geom, sector.the_geom,0.001) LIMIT 1);
+        text_dma:= (SELECT dma_id FROM dma WHERE ST_DWithin(rec_hydrometer.v_the_geom.the_geom, sector.the_geom,0.001) LIMIT 1);
+        	
+        INSERT INTO connec (connec_id, sector_id, dma_id, category_type, to_review, the_geom) 
+        VALUES (rec_hydrometer.code, text_sector, text_dma, rec_hydrometer.observ, 'A VERIFICAR', st_centroid (rec_hydrometer.v_the_geom));
+    END IF;
+
+    INSERT INTO rtc_hydrometer (hydrometer_id) VALUES (rec_hydrometer.hydrometer_id);
+    INSERT INTO rtc_hydrometer_x_connec (hydrometer_id, connec_id) VALUES (rec_hydrometer.hydrometer_id, rec_hydrometer.connec_id);
+
+    IF rec_hydrometer.observ <> rec_hydrometer.category_type THEN
+        UPDATE connec SET category_type='MISTA' WHERE connec_id=rec_hydrometer.connec_id;
+    END IF;
+
+END LOOP;
+
+
+
+
+-- DELETE OLD HYDROMETER AND UPDATE CONNEC OR DELETE CONNEC IF THERE IS NOT MORE HYDROMETERS ASSOCIATED WITH
+
+FOR rec_hydrometer IN SELECT * FROM rtc_hydrometer 
+WHERE hydrometer_id::integer NOT IN (SELECT hydrometer_id::integer FROM ext_rtc_hydrometer)
+LOOP
+
+    DELETE FROM rtc_hydrometer WHERE hydrometer_id=rec_hydrometer.hydrometer_id;
+    DELETE FROM rtc_hydrometer_x_connec WHERE hydrometer_id=rec_hydrometer.hydrometer_id RETURNING connec_id INTO id_last;
+
+    IF id_last NOT IN (SELECT connec_id FROM rtc_hydrometer_x_connec) THEN
+        DELETE FROM connec WHERE connec_id=id_last;
+    ELSE
+        counter=0;
+        FOR rec_connec IN SELECT * FROM rtc_hydrometer_x_connec WHERE connec_id=id_last
+        LOOP
+            SELECT observ INTO text_hydrometer_category FROM ext_rtc_hydrometer 
+            JOIN ext_hydrometer_category ON ext_hydrometer_category.id=ext_rtc_hydrometer.hydrometer_category;
+            counter=counter+1;
+            IF counter=1 THEN
+                rec_connec.category_type=text_hydrometer_category;
+            ELSE
+		    IF rec_connec.category_type <> text_hydrometer_category THEN
+                      rec_connec.category_type='MISTA';
+                    END IF;
+            END IF;
+        END LOOP;
+        UPDATE connec SET category_type= rec_connec.category_type;
+	
+    END IF;
+END LOOP;
+
+    RETURN;
+            
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
