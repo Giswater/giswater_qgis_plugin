2d9ac743f0b40c0215d98253db6c1471859239d4
Xavier Torret <xtorret@bgeo.es>
2017-01-11 19:55:48 +0100

    UD topological consisteny function and utils

---
+++ dbmodel/ud/ud_61_anl_topological_consistency.sql
@@ -0,0 +1,23 @@
+/*
+This file is part of Giswater 2.0
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+SET search_path = "SCHEMA_NAME", public, pg_catalog;
+
+
+
+
+DROP TABLE IF EXISTS anl_topological_consistency CASCADE;
+CREATE TABLE anl_topological_consistency
+(
+  node_id character varying(16) NOT NULL,
+  node_type character varying(30),
+  num_arcs integer,  
+  the_geom geometry(Point,SRID_VALUE),
+  CONSTRAINT anl_topological_consistency_pkey PRIMARY KEY (node_id)
+)
+WITH (
+  OIDS=FALSE
+);
