8fe4fa6fd24689afe29b22d078f4c42f81ae314f
bbrzepka <bbrzepka@bgeo.es>
2017-01-12 09:20:12 +0100

    WS - fk name in add fk

--- dbmodel/ws/ws_90_ext_sys.sql
+++ dbmodel/ws/ws_90_ext_sys.sql
@@ -11,30 +11,30 @@ SET search_path = "SCHEMA_NAME", public, pg_catalog;
 ------
 
 ALTER TABLE "ext_streetaxis" DROP CONSTRAINT IF EXISTS "ext_streetaxis_type_fkey";
-ALTER TABLE "ext_streetaxis" ADD FOREIGN KEY ("type") REFERENCES "ext_type_street" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ext_streetaxis" ADD CONSTRAINT "ext_streetaxis_type_fkey" FOREIGN KEY ("type") REFERENCES "ext_type_street" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 ALTER TABLE "ext_urban_propierties" DROP CONSTRAINT IF EXISTS "ext_urban_propierties_streetaxis_fkey";
-ALTER TABLE "ext_urban_propierties" ADD FOREIGN KEY ("streetaxis") REFERENCES "ext_streetaxis" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ext_urban_propierties" ADD CONSTRAINT "ext_urban_propierties_streetaxis_fkey" FOREIGN KEY ("streetaxis") REFERENCES "ext_streetaxis" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 
 ALTER TABLE "ext_rtc_scada" DROP CONSTRAINT IF EXISTS "ext_rtc_scada_cat_scada_id_fkey";
-ALTER TABLE "ext_rtc_scada" ADD FOREIGN KEY ("cat_scada_id") REFERENCES "ext_cat_scada" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ext_rtc_scada" ADD CONSTRAINT "ext_rtc_scada_cat_scada_id_fkey" FOREIGN KEY ("cat_scada_id") REFERENCES "ext_cat_scada" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 ALTER TABLE "ext_rtc_scada_x_value" DROP CONSTRAINT IF EXISTS "ext_rtc_scada_x_value_scada_id_fkey";
-ALTER TABLE "ext_rtc_scada_x_value" ADD FOREIGN KEY ("scada_id") REFERENCES "ext_rtc_scada" ("scada_id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "ext_rtc_scada_x_value" ADD CONSTRAINT "ext_rtc_scada_x_value_scada_id_fkey" FOREIGN KEY ("scada_id") REFERENCES "ext_rtc_scada" ("scada_id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 
 ALTER TABLE "ext_rtc_scada_x_data" DROP CONSTRAINT IF EXISTS "ext_rtc_scada_x_data_scada_id_fkey";
-ALTER TABLE "ext_rtc_scada_x_data" ADD FOREIGN KEY ("scada_id") REFERENCES "ext_rtc_scada" ("scada_id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "ext_rtc_scada_x_data" ADD CONSTRAINT "ext_rtc_scada_x_data_scada_id_fkey" FOREIGN KEY ("scada_id") REFERENCES "ext_rtc_scada" ("scada_id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE "ext_rtc_scada_x_data" DROP CONSTRAINT IF EXISTS "ext_rtc_scada_x_data_cat_period_id_fkey";
-ALTER TABLE "ext_rtc_scada_x_data" ADD FOREIGN KEY ("cat_period_id") REFERENCES "ext_cat_period" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "ext_rtc_scada_x_data" ADD CONSTRAINT "ext_rtc_scada_x_data_cat_period_id_fkey" FOREIGN KEY ("cat_period_id") REFERENCES "ext_cat_period" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE "ext_rtc_hydrometer" DROP CONSTRAINT IF EXISTS "ext_rtc_hydrometer_cat_hydrometer_id_fkey";
-ALTER TABLE "ext_rtc_hydrometer" ADD FOREIGN KEY ("cat_hydrometer_id") REFERENCES "ext_cat_hydrometer" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ext_rtc_hydrometer" ADD CONSTRAINT "ext_rtc_hydrometer_cat_hydrometer_id_fkey" FOREIGN KEY ("cat_hydrometer_id") REFERENCES "ext_cat_hydrometer" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 ALTER TABLE "ext_rtc_hydrometer_x_data" DROP CONSTRAINT IF EXISTS "ext_rtc_hydrometer_x_data_cat_period_id_fkey";
-ALTER TABLE "ext_rtc_hydrometer_x_data" ADD FOREIGN KEY ("cat_period_id") REFERENCES "ext_cat_period" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "ext_rtc_hydrometer_x_data" ADD CONSTRAINT "ext_rtc_hydrometer_x_data_cat_period_id_fkey" FOREIGN KEY ("cat_period_id") REFERENCES "ext_cat_period" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 ------
 -- FK 10
@@ -42,9 +42,10 @@ ALTER TABLE "ext_rtc_hydrometer_x_data" ADD FOREIGN KEY ("cat_period_id") REFERE
 
 
 ALTER TABLE "connec" DROP CONSTRAINT IF EXISTS "connec_streetaxis_id_fkey";
-ALTER TABLE "connec" ADD FOREIGN KEY ("streetaxis_id") REFERENCES "ext_streetaxis" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "connec" ADD CONSTRAINT "connec_streetaxis_id_fkey" FOREIGN KEY ("streetaxis_id") REFERENCES "ext_streetaxis" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 ------
 -- FK 60
 ------
-ALTER TABLE "anl_mincut_result_hydrometer" ADD FOREIGN KEY ("hydrometer_id") REFERENCES "ext_rtc_hydrometer" ("hydrometer_id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "anl_mincut_result_hydrometer" DROP CONSTRAINT IF EXISTS "anl_mincut_result_hydrometer_hydrometer_id_fkey";
+ALTER TABLE "anl_mincut_result_hydrometer" ADD CONSTRAINT "anl_mincut_result_hydrometer_hydrometer_id_fkey" FOREIGN KEY ("hydrometer_id") REFERENCES "ext_rtc_hydrometer" ("hydrometer_id") ON DELETE CASCADE ON UPDATE CASCADE;
