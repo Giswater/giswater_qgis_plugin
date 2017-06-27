82ad7c8821de4c91f847a7bbb9d5bdfe5cf52fb1
Xavier Torret <xtorret@bgeo.es>
2017-06-27 10:21:58 +0200

    Arc update state topologic coherence trigger

--- dbmodel/utils/trg/gw_trg_arc_state_update.sql
+++ dbmodel/utils/trg/gw_trg_arc_state_update.sql
@@ -1,43 +1,43 @@
 /*
-This file is part of Giswater
+This file is part of Giswater 2.0
 The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This version of Giswater is provided by Giswater Association
 */
 
 
-CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_arc_state_update()  RETURNS trigger AS $BODY$
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_trg_arc_state_update()  RETURNS trigger AS $BODY$
 
-DECLARE 
-    querystring Varchar; 
-    arcrec Record; 
-    nodeRecord Record; 
+DECLARE
+    querystring Varchar;
+    node1_rec record;
+    node2_rec record;
+    nodeRecord Record;
     check_aux boolean;
-    
-BEGIN 
+
+BEGIN
 
     EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
 
-    SELECT node_topocoh INTO check_aux FROM value_state where NEW.state=id;
+    SELECT arc_topocoh INTO check_aux FROM value_state where NEW.state=id;
 
--- TODO
-/*
-		IF THEN
+    SELECT * INTO node1_rec FROM node WHERE node.node_id=NEW.node_1;
+    SELECT * INTO node2_rec FROM node WHERE node.node_id=NEW.node_2;
+   
+        IF (((node1_rec.state=NEW.state) AND (check_aux IS true)) OR ((node2_rec.state=NEW.state) AND (check_aux IS true))  OR check_aux IS false) THEN
         ELSE
-		RETURN audit_function(215,200); 
+        RAISE EXCEPTION 'Error message';
         END IF;
 
-    END LOOP; 
+    RETURN NEW;
 
-    RETURN NEW;*/
-    
-END; 
+END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
 
 
-DROP TRIGGER IF EXISTS gw_trg_arc_state_update ON "SCHEMA_NAME"."arc";
-CREATE TRIGGER gw_trg_arc_state_update BEFORE INSERT ON "SCHEMA_NAME"."arc" 
-FOR EACH ROW EXECUTE PROCEDURE "SCHEMA_NAME"."gw_trg_arc_state_update"();
+  
+DROP TRIGGER IF EXISTS gw_trg_arc_state_update ON SCHEMA_NAME."arc";
+CREATE TRIGGER gw_trg_arc_state_update AFTER UPDATE OF state ON SCHEMA_NAME."arc" FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME."gw_trg_arc_state_update"();
 
 
