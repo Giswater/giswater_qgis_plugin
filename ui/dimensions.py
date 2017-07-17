b6fdec837a8e846ce057a7735a9bf0d49b971d6a
Xavier Torret <xtorret@bgeo.es>
2017-07-17 20:11:58 +0200

    WiP dimension  tool

--- ui/dimensions.py
+++ ui/dimensions.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from PyQt4 import QtGui, uic
 import os
+from qgis.core import QgsMessageLog
+from PyQt4.QtGui import QSizePolicy
 
-
+'''
 FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), 'dimensions.ui'))
+
+
 
 
 class DimensionsForm(QtGui.QDialog, FORM_CLASS):
@@ -21,3 +25,11 @@ class DimensionsForm(QtGui.QDialog, FORM_CLASS):
         
     def initGui(self):
         pass
+		
+'''	
+		
+def form_open(dialog, layer, feature):
+    QgsMessageLog.logMessage(str(dialog))
+    dialog.parent().setFixedWidth(200)
+    dialog.parent().setFixedHeight(200)
+    dialog.parent().setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
