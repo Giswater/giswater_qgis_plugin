91215290033152454e190e4ef0f01b63468c4533
Xavier Torret <xtorret@bgeo.es>
2017-08-08 00:55:04 +0200

    Update om strategy with BCASA issues

--- dbmodel/utils/ddl/gw_21_om.sql
+++ dbmodel/utils/ddl/gw_21_om.sql
@@ -6,39 +6,29 @@ This version of Giswater is provided by Giswater Association
 
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
-
 -- ----------------------------
 -- System tables
 -- ----------------------------
 
-
 CREATE TABLE om_visit_cat(
 id serial NOT NULL,
+name character varying (30),
 type character varying (18),
-short_des character varying (30),
-descript text,
 startdate date DEFAULT now(),
 enddate date,
+descript text,
 CONSTRAINT om_visit_cat_pkey PRIMARY KEY (id)
 );
-
- 
-CREATE TABLE "om_visit_parameter_type" (
-"id" varchar(30)   NOT NULL,
-"observ" varchar(255)   NOT NULL,
-"context" varchar (30),
-"code" varchar (30),
-"criticity" int2,
-CONSTRAINT om_visit_parameter_type_pkey PRIMARY KEY (id)
-) ;
 
 
 CREATE TABLE "om_visit_parameter" (
 "id" varchar(50)   NOT NULL,
+"code" varchar (30),
 "parameter_type" varchar(30) ,
-"feature" varchar(30) ,
+"featurecat_id" varchar(30) ,
 "data_type" varchar(16) ,
-"descript" varchar(100) ,
+"criticity" int2,
+"descript" varchar(100),
 CONSTRAINT om_visit_parameter_pkey PRIMARY KEY (id)
 );
 
@@ -46,38 +36,33 @@ CONSTRAINT om_visit_parameter_pkey PRIMARY KEY (id)
 CREATE TABLE "om_visit" (
 "id" serial8 NOT NULL,
 "visitcat_id" integer,
-"dma_id" integer NOT NULL,
 "startdate" timestamp(6) WITHOUT TIME ZONE DEFAULT now() ,
 "enddate" timestamp(6) WITHOUT TIME ZONE,
 "user_name" varchar(50) DEFAULT user,
 "webclient_id" character varying(50),
 "the_geom" public.geometry (POINT, SRID_VALUE),
-"expl_id" integer NOT NULL,
 CONSTRAINT om_visit_pkey PRIMARY KEY (id)
 );
 
 
-
 CREATE TABLE "om_visit_event" (
 "id" serial8 NOT NULL,
+"ext_code" varchar(16),
 "visit_id" int8 NOT NULL,
-"tstamp" timestamp(6) WITHOUT TIME ZONE DEFAULT now(),
+"position_id" varchar(50),
+"position_value" float,
 "parameter_id" varchar(50)  ,
 "value" text,
 "value1" integer,
 "value2" integer,
-"text" text,
-"position_id" varchar(50),
-"position_value" float,
-"xcoord" float,
-"ycoord" float,
-"compass" float,
 "geom1" float,
 "geom2" float,
 "geom3" float,
+"xcoord" float,
+"ycoord" float,
+"compass" float,
+"tstamp" timestamp(6) WITHOUT TIME ZONE DEFAULT now(),
 "status" float,
-"status_event_id" float,
-"ext_code" varchar(16),
 CONSTRAINT om_visit_event_pkey PRIMARY KEY (id)
 );
 
@@ -111,8 +96,7 @@ CONSTRAINT om_visit_value_position_pkey PRIMARY KEY (id)
 );
 
 
-
-CREATE TABLE "om_visit_value_context"(
+CREATE TABLE "om_visit_value_parameter_type"(
 id character varying(16),
 obs text,
 CONSTRAINT om_visit_value_context_pkey PRIMARY KEY (id)
@@ -124,11 +108,6 @@ id int2,
 obs text,
 CONSTRAINT om_visit_value_criticity_pkey PRIMARY KEY (id)
 );
-
-
-
-
-
 
 
 CREATE TABLE "om_visit_x_node" (
@@ -151,7 +130,5 @@ CREATE TABLE "om_visit_x_connec" (
 "connec_id" varchar (16),
 CONSTRAINT om_visit_x_connec_pkey PRIMARY KEY (id)
 );
-
-
 
 CREATE INDEX visit_index ON om_visit USING GIST (the_geom);
