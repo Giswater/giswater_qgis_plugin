714acd3be49a36b39e7897c7f19e350f3a80c64a
David Erill <daviderill79@gmail.com>
2017-08-28 17:38:06 +0200

    #151 Add class to manage plugin toolbars

---
+++ models/plugin_toolbar.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+class PluginToolbar():
+    ''' Keep data related with every toolbar of the plugin '''
+    
+    def __init__(self, toolbar_id, name, enabled, toolbar=None, list_actions=[]):
+        """ 
+        :param toolbar: QToolBar
+        :param list_actions: list
+        """
+        self.toolbar_id = toolbar_id
+        self.name = name
+        self.enabled = enabled
+        self.toolbar = toolbar
+        self.list_actions = list_actions
+              
+        
