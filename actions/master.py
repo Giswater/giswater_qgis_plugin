3de33be6df88a0b327778e83b5b6ea8527430d19
nestor <nestor@bgeo.es>
2017-08-30 08:47:43 +0200

    #183 wip btn 49

--- actions/master.py
+++ actions/master.py
@@ -1,13 +1,15 @@
-'''
+"""
 This file is part of Giswater 2.0
 The program is free software: you can redistribute it and/or modify it under the terms of the GNU 
 General Public License as published by the Free Software Foundation, either version 3 of the License, 
 or (at your option) any later version.
-'''
+"""
 
 # -*- coding: utf-8 -*-
 import os
 import sys
+
+from PyQt4.QtGui import QComboBox, QLineEdit
 
 plugin_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(plugin_path)
@@ -32,18 +34,31 @@ class Master(ParentAction):
 
         # Create dialog 
         self.dlg = EstimateResultNew()
-
+        utils_giswater.setDialog(self.dlg)
         # Set signals
         self.dlg.btn_calculate.clicked.connect(self.master_estimate_result_new_calculate)
         self.dlg.btn_close.clicked.connect(self.close_dialog)
-
+        self.result_id = self.dlg.findChild(QComboBox, "result_id")
+        self.text_prices_coeficient = self.dlg.findChild(QLineEdit, "text_prices_coeficient")
         # Manage i18n of the form and open it
         self.controller.translate_form(self.dlg, 'estimate_result_new')
+
+        sql = "SELECT result_id FROM "+self.schema_name+".rpt_cat_result ORDER BY result_id"
+        rows = self.controller.get_rows(sql)
+        utils_giswater.fillComboBox("result_id", rows, False)
         self.dlg.exec_()
 
 
     def master_estimate_result_new_calculate(self):
-        # TODO
+        """ Call function ws30.gw_fct_plan_estimate_result """
+
+        # TODO revisar tablas
+        sql = "SELECT ws30.gw_fct_plan_estimate_result('" + self.result_id.currentText() + "', '"+self.text_prices_coeficient.text() + "')"
+        status = self.controller.execute_sql(sql)
+        if status:
+            message = "Values has been updated"
+            self.controller.show_info(message, context_name='ui_message')
+            self.iface.mapCanvas().refreshAllLayers()
         pass
 
 
@@ -52,17 +67,24 @@ class Master(ParentAction):
 
         # Create dialog 
         self.dlg = EstimateResultSelector()
-
+        utils_giswater.setDialog(self.dlg)
         # Set signals
         self.dlg.btn_accept.clicked.connect(self.master_estimate_result_selector_accept)
         self.dlg.btn_cancel.clicked.connect(self.close_dialog)
+        self.rpt_selector_result_id = self.dlg.findChild(QComboBox, "rpt_selector_result_id")
 
         # Manage i18n of the form and open it
         self.controller.translate_form(self.dlg, 'estimate_result_selector')
+        # TODO revisar tablas
+        sql = "SELECT result_id FROM "+self.schema_name+".plan_selector_result WHERE cur_user = current_user ORDER BY result_id "
+        rows = self.controller.get_rows(sql)
+        utils_giswater.fillComboBox(self.rpt_selector_result_id, rows, False)
+
         self.dlg.exec_()
 
 
     def master_estimate_result_selector_accept(self):
         # TODO
+
         pass
-    
+
