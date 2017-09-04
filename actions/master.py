07cbb3978ded66e1655feecc2b9bf98d241a6f43
David Erill <daviderill79@gmail.com>
2017-09-04 12:03:50 +0200

    Close #183

--- actions/master.py
+++ actions/master.py
@@ -9,7 +9,7 @@ or (at your option) any later version.
 import os
 import sys
 
-from PyQt4.QtGui import QComboBox, QLineEdit, QDoubleValidator
+from PyQt4.QtGui import QDoubleValidator
 
 
 plugin_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -36,32 +36,42 @@ class Master(ParentAction):
         # Create dialog 
         self.dlg = EstimateResultNew()
         utils_giswater.setDialog(self.dlg)
+        
         # Set signals
         self.dlg.btn_calculate.clicked.connect(self.master_estimate_result_new_calculate)
         self.dlg.btn_close.clicked.connect(self.close_dialog)
-        self.result_id = self.dlg.findChild(QComboBox, "result_id")
-        self.text_prices_coeficient = self.dlg.findChild(QLineEdit, "text_prices_coeficient")
-        self.text_prices_coeficient.setValidator(QDoubleValidator())
-        # Manage i18n of the form and open it
-        self.controller.translate_form(self.dlg, 'estimate_result_new')
+        self.dlg.text_prices_coeficient.setValidator(QDoubleValidator())
 
+        # Fill combo box
         sql = "SELECT result_id FROM "+self.schema_name+".rpt_cat_result ORDER BY result_id"
         rows = self.controller.get_rows(sql)
         utils_giswater.fillComboBox("result_id", rows, False)
+        
+        # Manage i18n of the form and open it
+        self.controller.translate_form(self.dlg, 'estimate_result_new')
         self.dlg.exec_()
 
 
     def master_estimate_result_new_calculate(self):
-        """ Call function ws30.gw_fct_plan_estimate_result """
+        """ Execute function 'gw_fct_plan_estimate_result' """
 
-        # TODO revisar tablas
-        sql = "SELECT ws30.gw_fct_plan_estimate_result('" + utils_giswater.getWidgetText(self.result_id) + "', '"+utils_giswater.getWidgetText(self.text_prices_coeficient) + "')"
+        # Get values from form
+        result_id = utils_giswater.getWidgetText("result_id")
+        coefficient = utils_giswater.getWidgetText("text_prices_coeficient")
+        if coefficient == 'null':
+            message = "Please, introduce a coefficient value"
+            self.controller.show_warning(message, context_name='ui_message')  
+            return          
+        
+        # Execute function 'gw_fct_plan_estimate_result'
+        sql = "SELECT "+self.schema_name+".gw_fct_plan_estimate_result('" + result_id + "', " + coefficient + ")"
         status = self.controller.execute_sql(sql)
         if status:
             message = "Values has been updated"
             self.controller.show_info(message, context_name='ui_message')
-            self.iface.mapCanvas().refreshAllLayers()
-        pass
+        
+        # Refresh canvas
+        self.iface.mapCanvas().refreshAllLayers()
 
 
     def master_estimate_result_selector(self):
@@ -70,23 +80,51 @@ class Master(ParentAction):
         # Create dialog 
         self.dlg = EstimateResultSelector()
         utils_giswater.setDialog(self.dlg)
+        
         # Set signals
         self.dlg.btn_accept.clicked.connect(self.master_estimate_result_selector_accept)
         self.dlg.btn_cancel.clicked.connect(self.close_dialog)
-        self.rpt_selector_result_id = self.dlg.findChild(QComboBox, "rpt_selector_result_id")
 
+        # Fill combo box
+        sql = "SELECT result_id FROM "+self.schema_name+".plan_result_cat "
+        sql += " WHERE cur_user = current_user ORDER BY result_id"
+        rows = self.controller.get_rows(sql)
+        if not rows:
+            return
+        
+        utils_giswater.fillComboBox("rpt_selector_result_id", rows, False)
+        
+        # Get selected value from table 'plan_selector_result'
+        sql = "SELECT result_id FROM "+self.schema_name+".plan_selector_result"
+        sql += " WHERE cur_user = current_user"   
+        row = self.controller.get_row(sql)
+        if row:
+            utils_giswater.setSelectedItem("rpt_selector_result_id", str(row[0]))
+        elif row is None and self.controller.last_error:           
+            return
+            
         # Manage i18n of the form and open it
         self.controller.translate_form(self.dlg, 'estimate_result_selector')
-        # TODO revisar tablas
-        sql = "SELECT result_id FROM "+self.schema_name+".plan_selector_result WHERE cur_user = current_user ORDER BY result_id "
-        rows = self.controller.get_rows(sql)
-        utils_giswater.fillComboBox(self.rpt_selector_result_id, rows, False)
-
         self.dlg.exec_()
 
 
     def master_estimate_result_selector_accept(self):
-        # TODO
+        """ Update value of table 'plan_selector_result' """
+    
+        # Get selected value and upsert the table
+        result_id = utils_giswater.getWidgetText("rpt_selector_result_id")
+        sql = " INSERT INTO "+self.schema_name+".plan_selector_result (result_id, cur_user) VALUES ("
+        sql += "'" + result_id + "', current_user)"
+        sql += " ON CONFLICT (cur_user) DO UPDATE"
+        sql += " SET result_id = '" + result_id + "'"
+        sql += " WHERE plan_selector_result.cur_user = current_user"
+        self.controller.log_info(sql)
+        status = self.controller.execute_sql(sql)
+        if status:
+            message = "Values has been updated"
+            self.controller.show_info(message, context_name='ui_message')        
+        
+        # Refresh canvas
         self.iface.mapCanvas().refreshAllLayers()
-        pass
+        
 
