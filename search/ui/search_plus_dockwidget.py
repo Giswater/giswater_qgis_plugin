5cc582f52d2f052e72636c6686c7d8bc787fa446
David Erill <daviderill79@gmail.com>
2017-09-05 09:21:41 +0200

    #155 Change folder and filenames Move files to 'ui' folder

--- search/ui/search_plus_dockwidget.py
+++ search/ui/search_plus_dockwidget.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from PyQt4 import QtGui
-from ui.search_plus_dialog_base_ui import Ui_searchPlusDockWidget
+from search.ui.search_plus_dialog import Ui_searchPlusDockWidget
 
-# cannot apply dynaic loading because cannot promote widget 
+# cannot apply dynamic loading because cannot promote widget 
 # belongin to othe modules
 # import os
 # from PyQt4 import uic
-# FORM_CLASS, _ = uic.loadUiType(os.path.join(
-#     os.path.dirname(__file__), 'ui', 'search_plus_dialog_base.ui'))
+# FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), 'ui', 'search_plus_dialog.ui'))
 # class SearchPlusDockWidget(QtGui.QDockWidget, FORM_CLASS):
 
 class SearchPlusDockWidget(QtGui.QDockWidget, Ui_searchPlusDockWidget):
