73850b833c3fb8b47b133ec5f7c704a344fed8b1
Xavier Torret <xtorret@bgeo.es>
2017-09-22 20:17:10 +0200

    Function to convert from UD-GW1 data on arcs (z1,z2) to (y1,y2)

---
+++ dbmodel/extra/admin_tools/gw_fct_giswater_1to2 .sql
@@ -0,0 +1,37 @@
+﻿
+SET search_path = "SCHEMA_NAME", public, pg_catalog;
+
+
+CREATE OR REPLACE FUNCTION gw_fct_giswater_1to2()
+RETURNS void 
+AS $BODY$
+
+DECLARE
+ rec_table 	record;
+ id_last   	bigint;
+ ym1_var 	float;
+ ym2_var 	float;
+ 
+BEGIN
+
+    -- Search path
+    SET search_path = "SCHEMA_NAME", public;
+
+    --Initiation of the process
+	FOR rec_table IN SELECT * FROM arc
+    LOOP
+		--y1
+		SELECT ymax INTO ym1_var FROM node WHERE rec_table.node_1=node_id;
+		UPDATE arc SET y1 = ym1_var-y1 WHERE arc_id=rec_table.arc_id;
+	
+		--y2
+		SELECT ymax INTO ym2_var FROM node WHERE rec_table.node_2=node_id;
+		UPDATE arc SET y2 = ym2_var-y2 WHERE arc_id=rec_table.arc_id;
+		
+    END LOOP;
+
+RETURN;
+        
+END;$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
