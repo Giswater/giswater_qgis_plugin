3106a4ffe4181881d4cf716da390b614ec0fd2cf
Xavier Torret <xtorret@bgeo.es>
2017-09-25 18:06:18 +0200

    Bug fix on arc_searchnodes WS function

--- dbmodel/ws/fct/ws_gw_fct_repair_arc_searchnodes.sql
+++ dbmodel/ws/fct/ws_gw_fct_repair_arc_searchnodes.sql
@@ -1,4 +1,4 @@
-/*
+﻿/*
 This file is part of Giswater 3
 The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This version of Giswater is provided by Giswater Association
@@ -6,7 +6,6 @@ This version of Giswater is provided by Giswater Association
 
 SET search_path="SCHEMA_NAME",public;
 
-/*
 DROP FUNCTION IF EXISTS gw_fct_topo_arc_searchnodes();
 CREATE OR REPLACE FUNCTION gw_fct_topo_arc_searchnodes() RETURNS void AS
 $BODY$
@@ -33,8 +32,8 @@ BEGIN
 
 	SELECT * INTO nodeRecord2 FROM node WHERE ST_DWithin(ST_endpoint(arcrec.the_geom), node.the_geom, rec.arc_searchnodes)
 	ORDER BY ST_Distance(node.the_geom, ST_endpoint(arcrec.the_geom)) LIMIT 1;
-
-    SELECT * INTO optionsRecord FROM inp_options LIMIT 1;
+	
+	SELECT * INTO optionsRecord FROM inp_options LIMIT 1;
 
 
 	--insert into a1 values (arcrec.arc_id, nodeRecord1.node_id, nodeRecord2.node_id);
@@ -51,15 +50,13 @@ BEGIN
             arcrec.the_geom := ST_SetPoint(arcrec.the_geom, 0, nodeRecord1.the_geom);
             arcrec.the_geom := ST_SetPoint(arcrec.the_geom, ST_NumPoints(arcrec.the_geom) - 1, nodeRecord2.the_geom);
  
-            UPDATE arc 
-			SET the_geom=arcrec.the_geom, y1=arcrec.y1, y2=arcrec.y2, 
-            node_1=arcrec.nodeRecord1.node_id, node_2=nodeRecord2.node_id where arc_id=arcrec.arc_id;    
-
-            END IF;  
+            UPDATE arc SET node_1=nodeRecord1.node_id, node_2=nodeRecord2.node_id, the_geom=arcrec.the_geom  where arc_id=arcrec.arc_id;    
+ 
             
 	    END IF;
 
     END IF;
+
 
     END LOOP;
 
@@ -69,5 +66,3 @@ END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-  
-    */
