f0e0878432ab6c084d2ed65b144d838d30b81ca6
Xavier Torret <xtorret@bgeo.es>
2017-09-26 21:15:28 +0200

    Duplicate topologic trigger on node (part on before and part on after)

--- dbmodel/ud/trg/ud_gw_trg_node_update.sql
+++ dbmodel/ud/trg/ud_gw_trg_node_update.sql
@@ -5,7 +5,7 @@ This version of Giswater is provided by Giswater Association
 */
 
 
-CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_topocontrol_node() RETURNS trigger LANGUAGE plpgsql AS $$
+CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_node_update() RETURNS trigger LANGUAGE plpgsql AS $$
 DECLARE 
     numNodes numeric;
     rec record;
@@ -30,14 +30,12 @@ BEGIN
 	SELECT * INTO optionsRecord FROM inp_options LIMIT 1;
 
 
- -- Looking for state control
-    PERFORM gw_fct_state_control('node', NEW.node_id, NEW.state, TG_OP);
-  
-
-    -- Lookig for state=0
+	-- For state=0
     IF NEW.state=0 THEN
-	RAISE WARNING 'Topology is not enabled with state=0. The feature will be inserted but disconected of the network!';
+		RAISE WARNING 'Topology is not enabled with state=0. The feature will be inserted but disconected of the network!';
+		RETURN NEW;
 
+-- For state=1,2
     ELSE 
         IF TG_OP = 'INSERT' THEN
 		
@@ -73,42 +71,76 @@ BEGIN
 					RAISE EXCEPTION 'Node with state(2) over node with state(2) is not allowed. Please, review your project data!';
 				END IF;
 			END IF;
-				
+			
+			
 		-- Updating expl / dma
 			IF (NEW.the_geom IS DISTINCT FROM OLD.the_geom)THEN   
 				NEW.expl_id:= (SELECT expl_id FROM exploitation WHERE ST_DWithin(NEW.the_geom, exploitation.the_geom,0.001) LIMIT 1);          
 				NEW.dma_id := (SELECT dma_id FROM dma WHERE ST_DWithin(NEW.the_geom, dma.the_geom,0.001) LIMIT 1);         
 			END IF;
 			
-		-- Select arcs with start-end on the updated node
-			querystring := 'SELECT * FROM arc WHERE arc.node_1 = ' || quote_literal(NEW.node_id) || ' OR arc.node_2 = ' || quote_literal(NEW.node_id); 
+	   
+		-- Select arcs with start-end on the updated node to modify coordinates
+			querystring := 'SELECT * FROM "arc" WHERE arc.node_1 = ' || quote_literal(NEW.node_id) || ' OR arc.node_2 = ' || quote_literal(NEW.node_id); 
+	
 			FOR arcrec IN EXECUTE querystring
 			LOOP
-
-			-- Initial and final node of the arc
+		
+				-- Initial and final node of the arc
 				SELECT * INTO nodeRecord1 FROM node WHERE node.node_id = arcrec.node_1;
 				SELECT * INTO nodeRecord2 FROM node WHERE node.node_id = arcrec.node_2;
 
-			-- Control de lineas de longitud 0
+				-- Control de lineas de longitud 0
 				IF (nodeRecord1.node_id IS NOT NULL) AND (nodeRecord2.node_id IS NOT NULL) THEN
-
-				-- Update arc node coordinates, node_id and direction
+		
+					-- Update arc node coordinates, node_id and direction
 					IF (nodeRecord1.node_id = NEW.node_id) THEN
+					
+						-- Coordinates
 						EXECUTE 'UPDATE arc SET the_geom = ST_SetPoint($1, 0, $2) WHERE arc_id = ' || quote_literal(arcrec."arc_id") USING arcrec.the_geom, NEW.the_geom; 
-							ELSE
-							EXECUTE 'UPDATE arc SET the_geom = ST_SetPoint($1, ST_NumPoints($1) - 1, $2) WHERE arc_id = ' || quote_literal(arcrec."arc_id") USING arcrec.the_geom, NEW.the_geom; 
+						
+						-- Search the upstream node
+						IF (optionsRecord.link_offsets = 'DEPTH') THEN
+							z1 = (NEW.top_elev - arcrec.y1);
+							z2 = (nodeRecord2.top_elev - arcrec.y2);
+							
+							-- Update direction if necessary
+							IF ((z2 > z1) AND arcrec.inverted_slope is false) OR ((z2 < z1) AND arcrec.inverted_slope is true) THEN
+								EXECUTE 'UPDATE arc SET node_1 = ' || quote_literal(nodeRecord2.node_id) || ', node_2 = ' || quote_literal(NEW.node_id) || ' WHERE arc_id = ' || quote_literal(arcrec."arc_id"); 
+								EXECUTE 'UPDATE arc SET y1 = ' || arcrec.y2 || ', y2 = ' || arcrec.y1 || ' WHERE arc_id = ' || quote_literal(arcrec."arc_id"); 
+								EXECUTE 'UPDATE arc SET the_geom = ST_reverse($1) WHERE arc_id = ' || quote_literal(arcrec."arc_id") USING arcrec.the_geom;
+							END IF;
+						END IF;
+					ELSE
+						-- Coordinates
+						EXECUTE 'UPDATE arc SET the_geom = ST_SetPoint($1, ST_NumPoints($1) - 1, $2) WHERE arc_id = ' || quote_literal(arcrec."arc_id") USING arcrec.the_geom, NEW.the_geom; 
+		
+						-- Search the upstream node
+						IF (optionsRecord.link_offsets = 'DEPTH') THEN
+							z1 = (nodeRecord1.top_elev - arcrec.y1);
+							z2 = (NEW.top_elev - arcrec.y2);
+		
+							-- Update direction if necessary
+							IF (z2 > z1) THEN
+								EXECUTE 'UPDATE arc SET node_1 = ' || quote_literal(NEW.node_id) || ', node_2 = ' || quote_literal(nodeRecord1.node_id) || ' WHERE arc_id = ' || quote_literal(arcrec."arc_id"); 
+								EXECUTE 'UPDATE arc SET y1 = ' || arcrec.y2 || ', y2 = ' || arcrec.y1 || ' WHERE arc_id = ' || quote_literal(arcrec."arc_id"); 
+								EXECUTE 'UPDATE arc SET the_geom = ST_reverse($1) WHERE arc_id = ' || quote_literal(arcrec."arc_id") USING arcrec.the_geom;
+							END IF;
+						END IF;
 					END IF;
 				END IF;
 			END LOOP; 
+		ELSE 
 		END IF;
-	END IF;
-	
+    END IF;
+
 RETURN NEW;
     
 END; 
 $$;
 
-DROP TRIGGER IF EXISTS gw_trg_topocontrol_node ON "SCHEMA_NAME"."node";
-CREATE TRIGGER gw_trg_topocontrol_node AFTER INSERT OR UPDATE OF the_geom, "state" ON "SCHEMA_NAME"."node" 
-FOR EACH ROW EXECUTE PROCEDURE "SCHEMA_NAME"."gw_trg_topocontrol_node"();
+
+DROP TRIGGER IF EXISTS gw_trg_node_update ON "SCHEMA_NAME"."node";
+CREATE TRIGGER gw_trg_node_update AFTER INSERT OR UPDATE OF the_geom, "state" ON "SCHEMA_NAME"."node" 
+FOR EACH ROW EXECUTE PROCEDURE "SCHEMA_NAME"."gw_trg_node_update"();
 
