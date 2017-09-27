07b0f1c78667bddbb74deb304023c3c7fe35679b
nestor <nestor@bgeo.es>
2017-09-27 12:23:41 +0200

    refactor code

--- actions/parent.py
+++ actions/parent.py
@@ -265,9 +265,9 @@ class ParentAction():
         tbl_all_rows = dialog.findChild(QTableView, "all_rows")
         tbl_all_rows.setSelectionBehavior(QAbstractItemView.SelectRows)
 
-        query_left = "SELECT * FROM " + self.controller.schema_name + "." + tableleft + " WHERE name NOT IN "
-        query_left += "(SELECT name FROM " + self.controller.schema_name + "." + tableleft
-        query_left += " RIGHT JOIN " + self.controller.schema_name + "." + tableright + " ON " + tableleft + "." + field_id_left + " = " + tableright + "." + field_id_right
+        query_left = "SELECT * FROM " + self.schema_name + "." + tableleft + " WHERE name NOT IN "
+        query_left += "(SELECT name FROM " + self.schema_name + "." + tableleft
+        query_left += " RIGHT JOIN " + self.schema_name + "." + tableright + " ON " + tableleft + "." + field_id_left + " = " + tableright + "." + field_id_right
         query_left += " WHERE cur_user = current_user)"
         self.fill_table_by_query(tbl_all_rows, query_left)
         self.hide_colums(tbl_all_rows, [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
@@ -276,8 +276,8 @@ class ParentAction():
         # fill QTableView selected_rows
         tbl_selected_rows = dialog.findChild(QTableView, "selected_rows")
         tbl_selected_rows.setSelectionBehavior(QAbstractItemView.SelectRows)
-        query_right = "SELECT name, cur_user, " + tableleft + "." + field_id_left + ", " + tableright + "." + field_id_right + " FROM " + self.controller.schema_name + "." + tableleft
-        query_right += " JOIN " + self.controller.schema_name + "." + tableright + " ON " + tableleft + "." + field_id_left + " = " + tableright + "." + field_id_right
+        query_right = "SELECT name, cur_user, " + tableleft + "." + field_id_left + ", " + tableright + "." + field_id_right + " FROM " + self.schema_name + "." + tableleft
+        query_right += " JOIN " + self.schema_name + "." + tableright + " ON " + tableleft + "." + field_id_left + " = " + tableright + "." + field_id_right
         query_right += " WHERE cur_user = current_user"
         self.fill_table_by_query(tbl_selected_rows, query_right)
         self.hide_colums(tbl_selected_rows, [1, 2, 3])
@@ -286,7 +286,7 @@ class ParentAction():
         dialog.btn_select.pressed.connect(partial(self.multi_rows_selector, tbl_all_rows, tbl_selected_rows, field_id_left, tableright, "id", query_left, query_right, field_id_right))
 
         # Button unselect
-        query_delete = "DELETE FROM " + self.controller.schema_name + "." + tableright
+        query_delete = "DELETE FROM " + self.schema_name + "." + tableright
         query_delete += " WHERE current_user = cur_user AND " + tableright + "." + field_id_right + "="
         dialog.btn_unselect.pressed.connect(partial(self.unselector, tbl_all_rows, tbl_selected_rows, query_delete, query_left, query_right, field_id_right))
         # QLineEdit
@@ -450,9 +450,9 @@ class ParentAction():
     def query_like_widget_text(self, text_line, qtable, tableleft, tableright, field_id):
         """ Fill the QTableView by filtering through the QLineEdit"""
         query = text_line.text()
-        sql = "SELECT * FROM " + self.controller.schema_name + "." + tableleft + " WHERE name NOT IN "
-        sql += "(SELECT name FROM " + self.controller.schema_name + "." + tableleft
-        sql += " RIGHT JOIN " + self.controller.schema_name + "." + tableright + " ON " + tableleft + "." + field_id + " = " + tableright + "." + field_id
+        sql = "SELECT * FROM " + self.schema_name + "." + tableleft + " WHERE name NOT IN "
+        sql += "(SELECT name FROM " + self.schema_name + "." + tableleft
+        sql += " RIGHT JOIN " + self.schema_name + "." + tableright + " ON " + tableleft + "." + field_id + " = " + tableright + "." + field_id
         sql += " WHERE cur_user = current_user) AND name LIKE '%" + query + "%'"
         self.fill_table_by_query(qtable, sql)
         
