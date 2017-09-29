94d02161bcb587a8bc0e6a111195c632327126df
bbrzepka <bbrzepka@bgeo.es>
2017-09-29 11:57:24 +0200

    WS&UD - Changes on rotation update,

--- dbmodel/ud/trg/ud_gw_trg_edit_node.sql
+++ dbmodel/ud/trg/ud_gw_trg_edit_node.sql
@@ -161,22 +161,6 @@ BEGIN
 
     ELSIF TG_OP = 'UPDATE' THEN
 
-	
-			-- State
-		IF (NEW.state != OLD.state) THEN
-			UPDATE node SET state=NEW.state WHERE node_id = OLD.node_id;
-		END IF;
-			
-		-- The geom
-		IF (NEW.the_geom IS DISTINCT FROM OLD.the_geom)  THEN
-			UPDATE node SET the_geom=NEW.the_geom WHERE node_id = OLD.node_id;
-		END IF;
-	
-		--Label rotation
-		IF (NEW.tank_rotation != OLD.tank_rotation) THEN
-			UPDATE node SET rotation=NEW.tank_rotation WHERE node_id = OLD.node_id;
-		END IF;
-	
 
 		IF (NEW.elev <> OLD.elev) THEN
                 RETURN audit_function(200,810);  
@@ -225,13 +209,29 @@ BEGIN
 				EXECUTE v_sql;
 			END IF;
 		END IF;
+	
+		
+			-- State
+		IF (NEW.state != OLD.state) THEN
+			UPDATE node SET state=NEW.state WHERE node_id = OLD.node_id;
+		END IF;
+			
+		-- The geom
+		IF (NEW.the_geom IS DISTINCT FROM OLD.the_geom)  THEN
+			UPDATE node SET the_geom=NEW.the_geom WHERE node_id = OLD.node_id;
+		END IF;
+	
+		--Label rotation
+		IF (NEW.rotation != OLD.rotation) THEN
+			UPDATE node SET rotation=NEW.rotation WHERE node_id = OLD.node_id;
+		END IF;
 
 		UPDATE node 
 			SET node_id=NEW.node_id, code=NEW.code, top_elev=NEW.top_elev,custom_top_elev=NEW.custom_top_elev, ymax=NEW.ymax, custom_ymax=NEW.custom_ymax, elev=NEW.elev, custom_elev=NEW.custom_elev,
 			node_type=NEW.node_type, nodecat_id=NEW.nodecat_id, epa_type=NEW.epa_type, sector_id=NEW.sector_id, state_type=NEW.state_type, annotation=NEW.annotation, "observ"=NEW.observ, "comment"=NEW.comment, 
 			dma_id=NEW.dma_id, soilcat_id=NEW.soilcat_id, function_type=NEW.function_type, category_type=NEW.category_type, fluid_type=NEW.fluid_type, location_type=NEW.location_type, workcat_id=NEW.workcat_id, 
 			workcat_id_end=NEW.workcat_id_end, buildercat_id=NEW.buildercat_id, builtdate=NEW.builtdate, enddate=NEW.enddate, ownercat_id=NEW.ownercat_id, address_01=NEW.address_01,address_02=NEW.address_02, 
-			address_03=NEW.address_03, descript=NEW.descript, rotation=NEW.rotation, link=NEW.link, verified=NEW.verified, undelete=NEW.undelete, label_x=NEW.label_x, label_y=NEW.label_y, 
+			address_03=NEW.address_03, descript=NEW.descript, link=NEW.link, verified=NEW.verified, undelete=NEW.undelete, label_x=NEW.label_x, label_y=NEW.label_y, 
 			label_rotation=NEW.label_rotation, publish=NEW.publish, inventory=NEW.inventory, uncertain=NEW.uncertain, xyz_date=NEW.xyz_date, unconnected=NEW.unconnected, expl_id=NEW.expl_id,
 			num_value=NEW.num_value
 			WHERE node_id = OLD.node_id;
