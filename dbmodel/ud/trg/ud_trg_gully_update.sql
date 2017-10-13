a3f3fb0758a8fba0206c4686d27bdf3e1f40ef41
Xavier Torret <xtorret@bgeo.es>
2017-10-13 22:52:27 +0200

    Add connec and gully triggers to control the update of geometry to update also links relateds (if exists)

--- dbmodel/ud/trg/ud_trg_gully_update.sql
+++ dbmodel/ud/trg/ud_trg_gully_update.sql
@@ -5,13 +5,12 @@ This version of Giswater is provided by Giswater Association
 */
 
 
-CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_trg_connec_update() RETURNS trigger LANGUAGE plpgsql AS $$
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_trg_gully_update() RETURNS trigger LANGUAGE plpgsql AS $$
 DECLARE 
-
-linkrec Record; 
-querystring text;
-connecRecord1 record; 
-connecRecord2 record;
+    querystring Varchar; 
+    linkrec Record; 
+    gullyRecord1 Record;
+    gullyRecord2 Record;
 
 BEGIN 
 
@@ -19,17 +18,17 @@ BEGIN
 
 --  Select links with start-end on the updated node
 	querystring := 'SELECT * FROM link WHERE (link.feature_id = ' || quote_literal(NEW.connec_id) || ' 
-	AND feature_type=''CONNEC'') OR (link.exit_id = ' || quote_literal(NEW.connec_id)|| ' AND feature_type=''CONNEC'');'; 
+	AND feature_type=''GULLY'') OR (link.exit_id = ' || quote_literal(NEW.gully_id)|| ' AND feature_type=''GULLY'');'; 
 	FOR linkrec IN EXECUTE querystring
 	LOOP
-		-- Initial and final connec of the LINK
-		SELECT * INTO connecRecord1 FROM v_edit_connec WHERE v_edit_connec.connec_id = linkrec.feature_id;
-		SELECT * INTO connecRecord2 FROM v_edit_connec WHERE v_edit_connec.connec_id = linkrec.exit_id;
+		-- Initial and final gully of the LINK
+		SELECT * INTO gullyRecord1 FROM v_edit_gully WHERE v_edit_gully.gully_id = linkrec.feature_id;
+		SELECT * INTO gullyRecord2 FROM v_edit_gully WHERE v_edit_gully.gully_id = linkrec.exit_id;
 
 		-- Update link
-		IF (connecRecord1.connec_id = NEW.connec_id) THEN
+		IF (gullyRecord1.gully_id = NEW.gully_id) THEN
 			EXECUTE 'UPDATE link SET the_geom = ST_SetPoint($1, 0, $2) WHERE link_id = ' || quote_literal(linkrec."link_id") USING linkrec.the_geom, NEW.the_geom; 
-		ELSIF (connecRecord2.connec_id = NEW.connec_id) THEN
+		ELSIF (gullyRecord2.gully_id = NEW.gully_id) THEN
 			EXECUTE 'UPDATE link SET the_geom = ST_SetPoint($1, ST_NumPoints($1) - 1, $2) WHERE link_id = ' || quote_literal(linkrec."link_id") USING linkrec.the_geom, NEW.the_geom; 
 		END IF;
 	END LOOP;
@@ -41,6 +40,6 @@ END;
 $$;
 
 
-DROP TRIGGER IF EXISTS gw_trg_connec_update ON "SCHEMA_NAME"."connec";
-CREATE TRIGGER gw_trg_connec_update AFTER UPDATE OF the_geom ON "SCHEMA_NAME"."connec" 
-FOR EACH ROW EXECUTE PROCEDURE "SCHEMA_NAME"."gw_trg_connec_update"();
+DROP TRIGGER IF EXISTS gw_trg_gully_update ON "SCHEMA_NAME"."gully";
+CREATE TRIGGER gw_trg_gully_update AFTER UPDATE OF the_geom ON "SCHEMA_NAME"."gully" 
+FOR EACH ROW EXECUTE PROCEDURE "SCHEMA_NAME"."gw_trg_gully_update"();
