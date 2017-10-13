4bff6e45885106df4250741aa27d6cc5b84aa48d
Xavier Torret <xtorret@bgeo.es>
2017-10-13 22:58:10 +0200

    Improve functio connect_to_network

--- dbmodel/utils/fct/gw_fct_connect_to_network.sql
+++ dbmodel/utils/fct/gw_fct_connect_to_network.sql
@@ -7,22 +7,28 @@ This version of Giswater is provided by Giswater Association
 DROP FUNCTION SCHEMA_NAME.gw_fct_connect_to_network(character varying[], character varying);
 
 
-CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_connect_to_network(connec_array character varying[], feature_type character varying) RETURNS void AS $BODY$
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_connect_to_network(connec_array character varying[], feature_type_aux character varying) RETURNS void AS $BODY$
 
 DECLARE
-    connec_id_aux  varchar;
+    rec record;
+    connect_id_aux  varchar;
     arc_geom       public.geometry;
     candidate_line integer;
     connect_geom   public.geometry;
     link_geom           public.geometry;
     vnode_geom          public.geometry;
-    vnode_id       integer;
-    vnode_id_aux   varchar;
-    arc_id_aux     varchar;
+    vnode_id_aux   integer;
+    link_id_aux   integer;
+    arc_id_aux	varchar;
+    arcrec record;
     userDefined    boolean;
-    sector_aux     varchar;
+    sector_aux     integer;
 	expl_id_int integer;
 	link_id integer;
+	state_aux integer;
+	dma_aux integer;
+	expl_aux integer;
+	
 
 	
 BEGIN
@@ -30,67 +36,90 @@ BEGIN
     -- Search path
     SET search_path = "SCHEMA_NAME", public;
 
+    SELECT * INTO rec FROM config;
+
     -- Main loop
-    FOREACH connec_id_aux IN ARRAY connec_array
+    FOREACH connect_id_aux IN ARRAY connec_array
     LOOP
 
         -- Control user defined
-        SELECT b.userdefined_pos, b.vnode_id INTO userDefined, vnode_id_aux FROM vnode AS b WHERE b.vnode_id IN (SELECT a.vnode_id FROM link AS a WHERE feature_id = connec_id_aux) LIMIT 1;
+	SELECT userdefined_geom INTO userDefined FROM link WHERE feature_id = connect_id_aux AND feature_type=feature_type_aux;
 
-        IF NOT FOUND OR (FOUND AND NOT userDefined) THEN
-			
-		--	 Get connec or gully geometry
-			IF feature_type ='CONNEC' THEN          
-				SELECT the_geom INTO connect_geom FROM connec WHERE connec_id = connec_id_aux;
-			END IF;
-			IF feature_type ='GULLY' THEN 
-				SELECT the_geom INTO connect_geom FROM gully WHERE gully_id = connec_id_aux;
-			END IF;
-			RAISE NOTICE 'connect_geom %', connect_geom;
-			
-			-- Improved version for curved lines (not perfect!)
-            WITH index_query AS
-            (
-                SELECT ST_Distance(the_geom, connect_geom) as d, arc_id FROM v_edit_arc ORDER BY the_geom <-> connect_geom LIMIT 10
-            )
-            SELECT arc_id INTO arc_id_aux FROM index_query ORDER BY d limit 1;
-            
-            -- Get v_edit_arc geometry
-            SELECT the_geom INTO arc_geom FROM v_edit_arc WHERE arc_id = arc_id_aux;
+	RAISE NOTICE 'userdefined % ', userDefined;
 
-            -- Compute link
-            IF arc_geom IS NOT NULL THEN
+        IF userDefined IS TRUE THEN
 
-                -- Link line
-                link_geom := ST_ShortestLine(connect_geom, arc_geom);
+		RAISE NOTICE 'As the value of link on userdefined_geom is true, it is not enabled to modify the link related to the connec % ', connect_id_aux;
+	
+	ELSE	
+		
+         	-- Get connec or gully geometry
+		IF feature_type_aux ='CONNEC' THEN          
+			SELECT the_geom INTO connect_geom FROM connec WHERE connec_id = connect_id_aux;
+		ELSIF feature_type_aux ='GULLY' THEN 
+			SELECT the_geom INTO connect_geom FROM gully WHERE gully_id = connect_id_aux;
+		END IF;
+					
+		-- Improved version for curved lines (not perfect!)
+
+		RAISE NOTICE 'Perico % ', feature_type_aux;
+
+		WITH index_query AS
+		(
+			SELECT ST_Distance(the_geom, connect_geom) as d, arc_id FROM v_edit_arc ORDER BY the_geom <-> connect_geom LIMIT 10
+		)
+		SELECT arc_id INTO arc_id_aux FROM index_query ORDER BY d limit 1;
+		
+		-- Get v_edit_arc geometry
+		SELECT * INTO arcrec FROM v_edit_arc WHERE arc_id = arc_id_aux;
+
+
+		-- Compute link
+            IF arcrec.the_geom IS NOT NULL THEN
+
+                 -- Link line
+                link_geom := ST_ShortestLine(connect_geom, arcrec.the_geom);
 
                 -- Line end point
                 vnode_geom := ST_EndPoint(link_geom);
 
-                -- Delete old vnode
-                DELETE FROM vnode AS a WHERE a.vnode_id::text = vnode_id_aux;
-
-                -- Detect vnode sector
-                --SELECT sector_id INTO sector_aux FROM sector WHERE (the_geom @ sector.the_geom) LIMIT 1;
-            
-			-- New id
-				--PERFORM setval('urn_id_seq', gw_fct_urn(),true);
-				link_id:= (SELECT nextval('urn_id_seq'));
-				vnode_id:= (SELECT nextval('urn_id_seq'));
-
-			--Exploitation ID
-				--expl_id_int := (SELECT expl_id FROM exploitation WHERE ST_DWithin(connect_geom, exploitation.the_geom,0.001) LIMIT 1);
-				--RAISE NOTICE 'expl_id_int %', expl_id_int;
-				
-                -- Insert new vnode
-                INSERT INTO vnode (vnode_id, arc_id, vnode_type, userdefined_pos, the_geom) VALUES (vnode_id, arc_id_aux, feature_type, FALSE, vnode_geom);
-			RAISE NOTICE 'feature_type %', feature_type;	
                 -- Delete old link
-                DELETE FROM link WHERE feature_id = connec_id_aux;
-					
+                SELECT exit_id INTO vnode_id_aux FROM link WHERE feature_id = connect_id_aux AND feature_type=feature_type_aux;
+                DELETE FROM vnode WHERE vnode_id=vnode_id_aux ;
+                DELETE FROM link WHERE feature_id = connect_id_aux AND feature_type=feature_type_aux;
+
+
+             --Checking if there is vnode exiting
+                SELECT vnode_id INTO vnode_id_aux FROM vnode WHERE ST_DWithin(vnode_geom, vnode.the_geom, rec.node_proximity) LIMIT 1;
+
+                IF vnode_id_aux IS NULL THEN
+
+                        --Get values state, sector, dma, expl_id from arc
+			state_aux:= arcrec.state;
+			sector_aux:= arcrec.sector_id;
+			dma_aux:= arcrec.dma_id;
+			expl_aux:= arcrec.expl_id;
+			vnode_id_aux := (SELECT nextval('urn_id_seq'));
+
+			-- Insert new vnode
+			INSERT INTO vnode (vnode_id, vnode_type, state, sector_id, dma_id, expl_id, the_geom) 
+			VALUES (vnode_id_aux, 'AUTO',state_aux, sector_aux, dma_aux, expl_aux, vnode_geom);
+		END IF;
+  
+
                 -- Insert new link
-                INSERT INTO link (link_id, the_geom, feature_id, vnode_id,feature_type) VALUES (link_id,link_geom, connec_id_aux, vnode_id,feature_type);
-			RAISE NOTICE 'feature_type %', feature_type;	
+		link_id_aux := (SELECT nextval('urn_id_seq'));
+                
+                INSERT INTO link (link_id, the_geom, feature_id, feature_type, exit_id, exit_type, userdefined_geom) 
+                VALUES (link_id_aux, link_geom, connect_id_aux, feature_type_aux, vnode_id_aux, 'VNODE', FALSE);
+
+		-- Update connec or gully arc_id
+		IF feature_type_aux ='CONNEC' THEN          
+			UPDATE connec SET arc_id=arc_id_aux WHERE connec_id = connect_id_aux;
+		ELSIF feature_type_aux ='GULLY' THEN 
+			UPDATE connec SET arc_id=arc_id_aux WHERE connec_id = connect_id_aux;
+		END IF;
+				
             END IF;
             
         END IF;
