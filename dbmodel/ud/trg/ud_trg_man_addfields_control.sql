2b7e07bb78f678bd2a862fa9c152a8ce22f528d6
Xavier Torret <xtorret@bgeo.es>
2017-10-13 22:45:06 +0200

    Triggere to manage as 'fk' the values of feature_id on man_addfields_value table related to node_id, arc_id, connec_id and gully_id

---
+++ dbmodel/ud/trg/ud_trg_man_addfields_control.sql
@@ -0,0 +1,16 @@
+﻿
+SET search_path='SCHEMA_NAME';
+
+
+CREATE TRIGGER gw_trg_man_addfields_value_connec_gully AFTER INSERT OR UPDATE OF gully_id OR DELETE ON SCHEMA_NAME.control
+FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_man_addfields_value_control('GULLY');
+
+
+
+
+
+
+
+
+
+  
