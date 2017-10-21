c1742ae600a84a60affa856d896bcb1eeebf6979
Xavier Torret <xtorret@bgeo.es>
2017-10-21 19:06:22 +0200

    Improve form views of visit-event

--- dbmodel/ud/view/ud_23_om_views.sql
+++ dbmodel/ud/view/ud_23_om_views.sql
@@ -12,21 +12,27 @@ CREATE OR REPLACE VIEW v_ui_om_visit_x_gully AS
 SELECT
 om_visit_event.id as event_id,
 om_visit.id as visit_id,
-om_visit.startdate,
-om_visit.enddate,
+om_visit.ext_code as code,
+om_visit.visitcat_id as visitcat_id,
+om_visit.startdate visit_start,
+om_visit.enddate visit_end,
+om_visit.user_name,
+om_visit.is_done,
+om_visit_event.tstamp,
 om_visit_x_gully.gully_id,
 om_visit_event.parameter_id,
 om_visit_parameter.parameter_type,
 om_visit_event.value,
-om_visit_event.position_id,
 om_visit_event.xcoord,
 om_visit_event.ycoord,
 om_visit_event.compass,
-om_visit_event.tstamp,
-om_visit.user_name
+om_visit_event.ext_code as event_ext_code,
+CASE WHEN a.event_id is null then false ELSE true END AS gallery,
+CASE WHEN a.visit_id is null then false ELSE true END AS document
 FROM om_visit_event
 JOIN om_visit ON om_visit.id = om_visit_event.visit_id
 JOIN om_visit_x_gully ON om_visit_x_gully.visit_id=om_visit.id
-JOIN om_visit_parameter ON om_visit_parameter.id=om_visit_event.parameter_id
-JOIN gully ON gully.gully_id = om_visit_x_gully.gully_id
+JOIN om_visit_parameter ON om_visit_parameter.id=om_visit_event.gully
+LEFT JOIN (SELECT DISTINCT event_id from ws30.om_visit_event_photo ) a on event_id=om_visit_event.id
+LEFT JOIN (SELECT DISTINCT visit_id from ws30.doc_x_visit ) a on visit_id=om_visit.id
 ORDER BY gully_id;
--- dbmodel/utils/view/gw_23_om_views.sql
+++ dbmodel/utils/view/gw_23_om_views.sql
@@ -12,23 +12,30 @@ CREATE OR REPLACE VIEW v_ui_om_visit_x_node AS
 SELECT
 om_visit_event.id as event_id,
 om_visit.id as visit_id,
-om_visit.startdate,
-om_visit.enddate,
+om_visit.ext_code as code,
+om_visit.visitcat_id as visitcat_id,
+om_visit.startdate visit_start,
+om_visit.enddate visit_end,
+om_visit.user_name,
+om_visit.is_done,
+om_visit_event.tstamp,
 om_visit_x_node.node_id,
 om_visit_event.parameter_id,
 om_visit_parameter.parameter_type,
 om_visit_event.value,
-om_visit_event.position_id,
 om_visit_event.xcoord,
 om_visit_event.ycoord,
 om_visit_event.compass,
-om_visit_event.tstamp,
-om_visit.user_name
+om_visit_event.ext_code as event_ext_code,
+CASE WHEN a.event_id is null then false ELSE true END AS gallery,
+CASE WHEN a.visit_id is null then false ELSE true END AS document
 FROM om_visit_event
 JOIN om_visit ON om_visit.id = om_visit_event.visit_id
 JOIN om_visit_x_node ON om_visit_x_node.visit_id=om_visit.id
 JOIN om_visit_parameter ON om_visit_parameter.id=om_visit_event.parameter_id
 JOIN node ON node.node_id = om_visit_x_node.node_id
+LEFT JOIN (SELECT DISTINCT event_id from ws30.om_visit_event_photo ) a on event_id=om_visit_event.id
+LEFT JOIN (SELECT DISTINCT visit_id from ws30.doc_x_visit ) a on visit_id=om_visit.id
 ORDER BY node_id;
 
 
@@ -37,23 +44,30 @@ CREATE OR REPLACE VIEW v_ui_om_visit_x_arc AS
 SELECT
 om_visit_event.id as event_id,
 om_visit.id as visit_id,
-om_visit.startdate,
-om_visit.enddate,
+om_visit.ext_code as code,
+om_visit.visitcat_id as visitcat_id,
+om_visit.startdate visit_start,
+om_visit.enddate visit_end,
+om_visit.user_name,
+om_visit.is_done,
+om_visit_event.tstamp,
 om_visit_x_arc.arc_id,
 om_visit_event.parameter_id,
 om_visit_parameter.parameter_type,
 om_visit_event.value,
-om_visit_event.position_id,
 om_visit_event.xcoord,
 om_visit_event.ycoord,
 om_visit_event.compass,
-om_visit_event.tstamp,
-om_visit.user_name
+om_visit_event.ext_code as event_ext_code,
+CASE WHEN a.event_id is null then false ELSE true END AS gallery,
+CASE WHEN a.visit_id is null then false ELSE true END AS document
 FROM om_visit_event
 JOIN om_visit ON om_visit.id = om_visit_event.visit_id
 JOIN om_visit_x_arc ON om_visit_x_arc.visit_id=om_visit.id
 JOIN om_visit_parameter ON om_visit_parameter.id=om_visit_event.parameter_id
 JOIN arc ON arc.arc_id = om_visit_x_arc.arc_id
+LEFT JOIN (SELECT DISTINCT event_id from ws30.om_visit_event_photo ) a on event_id=om_visit_event.id
+LEFT JOIN (SELECT DISTINCT visit_id from ws30.doc_x_visit ) a on visit_id=om_visit.id
 ORDER BY arc_id;
 
 
@@ -62,24 +76,32 @@ CREATE OR REPLACE VIEW v_ui_om_visit_x_connec AS
 SELECT
 om_visit_event.id as event_id,
 om_visit.id as visit_id,
-om_visit.startdate,
-om_visit.enddate,
+om_visit.ext_code as code,
+om_visit.visitcat_id as visitcat_id,
+om_visit.startdate visit_start,
+om_visit.enddate visit_end,
+om_visit.user_name,
+om_visit.is_done,
+om_visit_event.tstamp,
 om_visit_x_connec.connec_id,
 om_visit_event.parameter_id,
 om_visit_parameter.parameter_type,
 om_visit_event.value,
-om_visit_event.position_id,
 om_visit_event.xcoord,
 om_visit_event.ycoord,
 om_visit_event.compass,
-om_visit_event.tstamp,
-om_visit.user_name
+om_visit_event.ext_code as event_ext_code,
+CASE WHEN a.event_id is null then false ELSE true END AS gallery,
+CASE WHEN a.visit_id is null then false ELSE true END AS document
 FROM om_visit_event
 JOIN om_visit ON om_visit.id = om_visit_event.visit_id
 JOIN om_visit_x_connec ON om_visit_x_connec.visit_id=om_visit.id
 JOIN om_visit_parameter ON om_visit_parameter.id=om_visit_event.parameter_id
 JOIN connec ON connec.connec_id = om_visit_x_connec.connec_id
+LEFT JOIN (SELECT DISTINCT event_id from ws30.om_visit_event_photo ) a on event_id=om_visit_event.id
+LEFT JOIN (SELECT DISTINCT visit_id from ws30.doc_x_visit ) a on visit_id=om_visit.id
 ORDER BY connec_id;
+
 
 
    
