a9d997af52275237c0f2f19270974c147e32664a
Xavier Torret <xtorret@bgeo.es>
2017-11-03 09:21:16 +0100

    Reordering files

---
+++ dbmodel/extra/admin_tools/permisions/permisions.sql
@@ -0,0 +1,23 @@
+﻿--rol_admin
+GRANT ALL ON DATABASE "gis" TO "rol_admin" ;
+GRANT ALL ON SCHEMA "ws" TO "rol_admin";
+GRANT ALL ON ALL TABLES IN SCHEMA "ud" TO "rol_admin";
+GRANT ALL ON ALL SEQUENCES IN SCHEMA "ud" TO "rol_admin";
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA "ud" TO "rol_admin";
+
+
+--rol_edit_plus
+GRANT ALL ON DATABASE "gis" TO "rol_edit_plus" ;
+GRANT ALL ON SCHEMA "ws" TO "rol_edit_plus";
+GRANT ALL ON ALL TABLES IN SCHEMA "ws" TO "rol_edit_plus";
+GRANT ALL ON ALL SEQUENCES IN SCHEMA "ws" TO "rol_edit_plus";
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA "ws" TO "rol_edit_plus";
+
+
+
+--rol_basic
+GRANT ALL ON DATABASE "gis" TO "rol_basic" ;
+GRANT ALL ON SCHEMA "ud" TO "rol_basic";
+GRANT SELECT ON ALL TABLES IN SCHEMA "ud" TO "rol_basic";
+GRANT SELECT ON ALL FUNCTIONS IN SCHEMA "ud" TO "rol_basic";
+
