49e0ff451c5b4c60a32d5ec6fa8159f4216a8b5e
Xavier Torret <xtorret@bgeo.es>
2017-11-21 15:52:49 +0100

    Update audit function to v.3

--- dbmodel/utils/fct/gw_fct_audit_function.sql
+++ dbmodel/utils/fct/gw_fct_audit_function.sql
@@ -5,23 +5,6 @@ This version of Giswater is provided by Giswater Association
 */
 
 
-CREATE OR REPLACE FUNCTION SCHEMA_NAME.audit_function(p_audit_cat_error_id int4) RETURNS "pg_catalog"."int2" AS $BODY$
-BEGIN
-    SET search_path = "SCHEMA_NAME", public;
-    RETURN audit_function($1, null, null);
-END;
-$BODY$
-LANGUAGE 'plpgsql' VOLATILE COST 100;
-  
-  
-CREATE OR REPLACE FUNCTION SCHEMA_NAME.audit_function(p_audit_cat_error_id int4, p_debug_info text) RETURNS "pg_catalog"."int2" AS $BODY$
-BEGIN
-    SET search_path = "SCHEMA_NAME", public;
-    RETURN audit_function($1, null, $2);
-END;
-$BODY$
-LANGUAGE 'plpgsql' VOLATILE COST 100;
-
 
 CREATE OR REPLACE FUNCTION SCHEMA_NAME.audit_function(p_audit_cat_function_id integer, p_audit_cat_error_id integer) RETURNS "pg_catalog"."int2" AS $BODY$
 BEGIN
@@ -33,55 +16,38 @@ LANGUAGE plpgsql VOLATILE COST 100;
   
 
 
+CREATE OR REPLACE FUNCTION audit_function(    p_audit_cat_error_id integer,    p_audit_cat_function_id integer,    p_debug_text text)
+RETURNS smallint AS
 
-CREATE OR REPLACE FUNCTION SCHEMA_NAME.audit_function(p_audit_cat_error_id integer, p_audit_cat_function_id integer, p_debug_info text) RETURNS smallint AS $BODY$
+$BODY$
 DECLARE
-    control_rec boolean;
     function_rec record;
-    audit_cat_error_rec record;
-    message_rec text;
+    cat_error_rec record;
 
 BEGIN
     
-    SET search_path = "SCHEMA_NAME", public; 
-    
-    SELECT audit_function_control INTO control_rec FROM config;
-    SELECT * INTO audit_cat_error_rec FROM audit_cat_error WHERE audit_cat_error.id=p_audit_cat_error_id;  
+    SET search_path = "ud30", public; 
+    SELECT * INTO cat_error_rec FROM audit_cat_error WHERE audit_cat_error.id=p_audit_cat_error_id;  
 
     BEGIN
     
-        IF control_rec IS TRUE AND audit_cat_error_rec.log_level < 3 THEN
-            INSERT INTO audit_function_actions (audit_cat_error_id, audit_cat_function_id, debug_info, query, user_name, addr) 
-            VALUES (p_audit_cat_error_id, p_audit_cat_function_id, p_debug_info, current_query(), session_user, inet_client_addr());
-        END IF;
-
+        
         -- log_level of type 'INFO' or 'SUCCESS'
-        IF audit_cat_error_rec.log_level = 0 OR audit_cat_error_rec.log_level = 3 THEN 
+        IF cat_error_rec.log_level = 0 OR cat_error_rec.log_level = 3 THEN 
             RETURN p_audit_cat_error_id;
 
         -- log_level of type 'WARNING' (mostly applied to functions)
-        ELSIF audit_cat_error_rec.log_level = 1 THEN
-            SELECT 'WNG['||id||'] - '||error_message||'. '||CASE WHEN hint_message IS NULL THEN '' ELSE hint_message END INTO message_rec 
-            FROM audit_cat_error WHERE audit_cat_error.id=p_audit_cat_error_id;   
-            SELECT * INTO function_rec 
-            FROM audit_cat_function WHERE audit_cat_function.id=p_audit_cat_function_id; 
-            RAISE WARNING 'Function [%]', function_rec.name USING HINT = message_rec;
+        ELSIF cat_error_rec.log_level = 1 THEN
+            RAISE WARNING 'Function: [%] - %, (%)', function_rec.name, cat_error_rec.error_message, p_debug_text USING HINT = cat_error_rec.hint_message ;
             RETURN p_audit_cat_error_id;
         
         -- log_level of type 'ERROR' (mostly applied to trigger functions) 
-        ELSIF audit_cat_error_rec.log_level = 2 THEN
-            SELECT 'ERR['||id||'] - '||error_message||'. '||CASE WHEN hint_message IS NULL THEN '' ELSE hint_message END INTO message_rec 
-            FROM audit_cat_error WHERE audit_cat_error.id=p_audit_cat_error_id;   
+        ELSIF cat_error_rec.log_level = 2 THEN
             SELECT * INTO function_rec 
             FROM audit_cat_function WHERE audit_cat_function.id=p_audit_cat_function_id; 
-            RAISE EXCEPTION 'Function [%]', function_rec.name USING HINT = message_rec;
+            RAISE EXCEPTION 'Function: [%] - %, (%)', function_rec.name, cat_error_rec.error_message, p_debug_text USING HINT = cat_error_rec.hint_message ;
         
         END IF;
-
-    EXCEPTION WHEN foreign_key_violation THEN
-        INSERT INTO audit_function_actions (audit_cat_error_id, audit_cat_function_id, debug_info, query, user_name, addr) 
-        VALUES (999, p_audit_cat_function_id, p_debug_info, current_query(), session_user, inet_client_addr());
-        RETURN 999;
         
     END;
     
@@ -89,6 +55,6 @@ END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-  
+
   
 
