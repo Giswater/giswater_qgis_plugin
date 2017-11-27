dcb67f5c574ed9e8eae86e280c1f57dd9c727172
Néstor Ibáñez <nestor@bgeo.es>
2017-11-27 17:06:39 +0100

    #164 WIP

--- map_tools/cad_add_circle.py
+++ map_tools/cad_add_circle.py
@@ -7,6 +7,8 @@ or (at your option) any later version.
 
 # -*- coding: utf-8 -*-
 from map_tools.parent import ParentMapTool
+from ..ui.create_circle import Create_circle             # @UnresolvedImport
+import utils_giswater
 
 
 class CadAddCircle(ParentMapTool):
@@ -17,6 +19,21 @@ class CadAddCircle(ParentMapTool):
 
         # Call ParentMapTool constructor
         super(CadAddCircle, self).__init__(iface, settings, action, index_action)
+
+    def init_create_circle_form(self):
+        # Create the dialog and signals
+        self.dlg_create_circle = Create_circle()
+        utils_giswater.setDialog(self.dlg_create_circle)
+
+        self.dlg_create_circle.btn_accept.pressed.connect(self.get_radius)
+        self.dlg_create_circle.btn_cancel.pressed.connect(self.dlg_create_circle.close)
+
+        self.dlg_create_circle.exec_()
+
+    def get_radius(self):
+
+        radius = self.dlg_create_circle.radius.text()
+
 
 
     """ QgsMapTools inherited event functions """
@@ -30,7 +47,7 @@ class CadAddCircle(ParentMapTool):
 
 
     def activate(self):
-        pass
+        self.init_create_circle_form()
 
 
     def deactivate(self):
