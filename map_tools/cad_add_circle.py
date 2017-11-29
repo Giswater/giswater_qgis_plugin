c4c0c2ba13be2c29c1836975703fab42965ee1de
Néstor Ibáñez <nestor@bgeo.es>
2017-11-29 11:38:14 +0100

    #164 WIP

--- map_tools/cad_add_circle.py
+++ map_tools/cad_add_circle.py
@@ -6,12 +6,12 @@ or (at your option) any later version.
 '''
 
 # -*- coding: utf-8 -*-
-from PyQt4.QtCore import QPoint
-from PyQt4.QtCore import Qt
+from PyQt4.QtCore import QPoint, Qt
+
 
 from map_tools.parent import ParentMapTool
 
-from qgis.core import QgsMapLayerRegistry, QgsExpression, QgsFeatureRequest, QgsPoint
+from qgis.core import QgsMapLayerRegistry, QgsExpression, QgsFeatureRequest, QgsPoint, QgsFeature, QgsGeometry, QgsVectorLayer
 
 
 from ..ui.create_circle import Create_circle             # @UnresolvedImport
@@ -29,21 +29,24 @@ class CadAddCircle(ParentMapTool):
         # Call ParentMapTool constructor
         super(CadAddCircle, self).__init__(iface, settings, action, index_action)
 
+
     def init_create_circle_form(self):
+        """   """
         # Create the dialog and signals
         self.dlg_create_circle = Create_circle()
         utils_giswater.setDialog(self.dlg_create_circle)
 
         self.dlg_create_circle.btn_accept.pressed.connect(self.get_radius)
         self.dlg_create_circle.btn_cancel.pressed.connect(self.cancel)
-        self.active_layer=self.iface.mapCanvas().currentLayer()
 
-        self.virtual_layer = QgsMapLayerRegistry.instance().mapLayersByName('Point_aux')[0]
+        self.active_layer = self.iface.mapCanvas().currentLayer()
+        self.virtual_layer = QgsMapLayerRegistry.instance().mapLayersByName('Polygon_aux')[0]
 
         self.dlg_create_circle.exec_()
 
 
     def get_radius(self):
+        """   """
         radius = self.dlg_create_circle.radius.text()
         self.controller.log_info(str("RADIUS: " + radius))
         self.controller.log_info(str("ACTIVE: " + self.active_layer.name()))
@@ -52,11 +55,16 @@ class CadAddCircle(ParentMapTool):
         self.dlg_create_circle.close()
 
 
-
     def cancel(self):
-        if self.active_layer.isEditing():
-            self.controller.log_info(str("CANCEL"))
+        """   """
+
+        if self.active_layer.isEditable():
             self.active_layer.commitChanges()
+        elif self.virtual_layer.isEditable():
+            self.virtual_layer.commitChanges()
+        self.dlg_create_circle.close()
+
+
     """ QgsMapTools inherited event functions """
 
     def canvasMoveEvent(self, event):
@@ -67,10 +75,6 @@ class CadAddCircle(ParentMapTool):
         # Get the click
         x = event.pos().x()
         y = event.pos().y()
-        self.controller.log_info(str("TEST 1"))
-        self.controller.log_info(str("X: "+ str(x)))
-        self.controller.log_info(str("Y: "+ str(y)))
-        self.controller.log_info(str("TEST 2"))
         #Plugin reloader bug, MapTool should be deactivated
         try:
             event_point = QPoint(x, y)
@@ -95,10 +99,68 @@ class CadAddCircle(ParentMapTool):
 
 
     def canvasReleaseEvent(self, event):
-        self.controller.log_info(str("TEST 3"))
-        pass
+        """   """
+        #self.vertex_marker.hide()
+
+        # Get the click
+        x = event.pos().x()
+        y = event.pos().y()
+        event_point = QPoint(x, y)
+        # #event_point = QgsPoint(x, y)
+        # snapped_feat = None
+        #
+        # # Snapping
+        # (retval, result) = self.snapper.snapToBackgroundLayers(event_point)  # @UnusedVariable
+        # self.controller.log_info(str("X: " + str(x)))
+        # self.controller.log_info(str("Y: " + str(y)))
+        # # That's the snapped features
+        # if result:
+        #     for snapped_feat in result:
+        #         # Check if feature belongs to 'node' group
+        #         exist = self.snapper_manager.check_node_group(snapped_feat.layer)
+        #         if exist:
+        #             # Get the point
+        #             point = QgsPoint(result[0].snappedVertex)  # @UnusedVariable
+        #             snapped_feat = next(
+        #                 result[0].layer.getFeatures(QgsFeatureRequest().setFilterFid(result[0].snappedAtGeometry)))
+        #             break
+        #
+        # if snapped_feat is not None:
+        #     # Get selected features and layer type: 'node'
+        #     feature = snapped_feat
+
+        # vpoly = QgsVectorLayer("Polygon", "pointbuffer", "memory")
+        # feature = QgsFeature()
+        # feature.setGeometry(QgsGeometry.fromPoint(QgsPoint(x, y)).buffer(25, 25))
+        # provider = vpoly.dataProvider()
+        # vpoly.startEditing()
+        # provider.addFeatures([feature])
+        # vpoly.commitChanges()
 
 
+        feature = QgsFeature()
+        feature.setGeometry(QgsGeometry.fromPoint(QgsPoint(x,y)).buffer(250,250))
+        self.controller.log_info(str(self.virtual_layer.name()))
+        provider = self.virtual_layer.dataProvider()
+        self.virtual_layer.startEditing()
+        provider.addFeatures([feature])
+        self.virtual_layer.commitChanges()
+
+
+
+        self.controller.log_info(str("TEST 4"))
+
+        # vpoly = QgsVectorLayer("Polygon", "pointbuffer", "memory")
+        # feature = QgsFeature()
+        # feature.setGeometry(QgsGeometry.fromPoint(QgsPoint(y, x)).buffer(250, 25))
+        # provider = vpoly.dataProvider()
+        # vpoly.startEditing()
+        # provider.addFeatures([feature])
+        # vpoly.commitChanges()
+        # self.controller.log_info(str("TEST 5"))
+
+        self.iface.mapCanvas().refresh()
+        self.canvas.refreshAllLayers()
 
 
     def activate(self):
