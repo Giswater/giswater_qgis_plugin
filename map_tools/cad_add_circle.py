53032fed1ed8d356b1c9b29e083c7536a5b31d2c
Néstor Ibáñez <nestor@bgeo.es>
2017-11-29 16:43:39 +0100

    #164 wip (circle working)

--- map_tools/cad_add_circle.py
+++ map_tools/cad_add_circle.py
@@ -7,11 +7,11 @@ or (at your option) any later version.
 
 # -*- coding: utf-8 -*-
 from PyQt4.QtCore import QPoint, Qt
-
+from PyQt4.QtGui import QDoubleValidator
 
 from map_tools.parent import ParentMapTool
 
-from qgis.core import QgsMapLayerRegistry, QgsExpression, QgsFeatureRequest, QgsPoint, QgsFeature, QgsGeometry, QgsVectorLayer
+from qgis.core import QgsMapLayerRegistry, QgsExpression, QgsFeatureRequest, QgsPoint, QgsFeature, QgsGeometry, QgsVectorLayer, QgsMapToPixel
 
 
 from ..ui.create_circle import Create_circle             # @UnresolvedImport
@@ -36,8 +36,13 @@ class CadAddCircle(ParentMapTool):
         self.dlg_create_circle = Create_circle()
         utils_giswater.setDialog(self.dlg_create_circle)
 
+        validator=QDoubleValidator(0.00, 9999.00, 3)
+        validator.setNotation(QDoubleValidator().StandardNotation)
+
+        self.dlg_create_circle.radius.setValidator(validator)
         self.dlg_create_circle.btn_accept.pressed.connect(self.get_radius)
         self.dlg_create_circle.btn_cancel.pressed.connect(self.cancel)
+        self.dlg_create_circle.radius.setFocus()
 
         self.active_layer = self.iface.mapCanvas().currentLayer()
         self.virtual_layer = QgsMapLayerRegistry.instance().mapLayersByName('Polygon_aux')[0]
@@ -47,8 +52,8 @@ class CadAddCircle(ParentMapTool):
 
     def get_radius(self):
         """   """
-        radius = self.dlg_create_circle.radius.text()
-        self.controller.log_info(str("RADIUS: " + radius))
+        self.radius = self.dlg_create_circle.radius.text()
+        self.controller.log_info(str("RADIUS: " + self.radius))
         self.controller.log_info(str("ACTIVE: " + self.active_layer.name()))
         self.controller.log_info(str("VIRTUAL: " + self.virtual_layer.name()))
         self.virtual_layer.startEditing()
@@ -58,10 +63,10 @@ class CadAddCircle(ParentMapTool):
     def cancel(self):
         """   """
 
-        if self.active_layer.isEditable():
-            self.active_layer.commitChanges()
-        elif self.virtual_layer.isEditable():
+        if self.virtual_layer.isEditable():
             self.virtual_layer.commitChanges()
+        ParentMapTool.deactivate(self)
+        self.deactivate(self)
         self.dlg_create_circle.close()
 
 
@@ -100,75 +105,35 @@ class CadAddCircle(ParentMapTool):
 
     def canvasReleaseEvent(self, event):
         """   """
-        #self.vertex_marker.hide()
+        if event.button() == Qt.LeftButton:
+            # Get the click
+            x = event.pos().x()
+            y = event.pos().y()
+            point = QgsMapToPixel.toMapCoordinates(self.canvas.getCoordinateTransform(), x, y)
 
-        # Get the click
-        x = event.pos().x()
-        y = event.pos().y()
-        event_point = QPoint(x, y)
-        # #event_point = QgsPoint(x, y)
-        # snapped_feat = None
-        #
-        # # Snapping
-        # (retval, result) = self.snapper.snapToBackgroundLayers(event_point)  # @UnusedVariable
-        # self.controller.log_info(str("X: " + str(x)))
-        # self.controller.log_info(str("Y: " + str(y)))
-        # # That's the snapped features
-        # if result:
-        #     for snapped_feat in result:
-        #         # Check if feature belongs to 'node' group
-        #         exist = self.snapper_manager.check_node_group(snapped_feat.layer)
-        #         if exist:
-        #             # Get the point
-        #             point = QgsPoint(result[0].snappedVertex)  # @UnusedVariable
-        #             snapped_feat = next(
-        #                 result[0].layer.getFeatures(QgsFeatureRequest().setFilterFid(result[0].snappedAtGeometry)))
-        #             break
-        #
-        # if snapped_feat is not None:
-        #     # Get selected features and layer type: 'node'
-        #     feature = snapped_feat
+            self.controller.log_info(str("X: " + str(x)))
+            self.controller.log_info(str("Y: " + str(y)))
 
-        # vpoly = QgsVectorLayer("Polygon", "pointbuffer", "memory")
-        # feature = QgsFeature()
-        # feature.setGeometry(QgsGeometry.fromPoint(QgsPoint(x, y)).buffer(25, 25))
-        # provider = vpoly.dataProvider()
-        # vpoly.startEditing()
-        # provider.addFeatures([feature])
-        # vpoly.commitChanges()
+            self.init_create_circle_form()
 
+            feature = QgsFeature()
+            feature.setGeometry(QgsGeometry.fromPoint(point).buffer(float(self.radius),25))
+            self.controller.log_info(str(self.virtual_layer.name()))
+            provider = self.virtual_layer.dataProvider()
+            self.virtual_layer.startEditing()
+            provider.addFeatures([feature])
 
-        feature = QgsFeature()
-        feature.setGeometry(QgsGeometry.fromPoint(QgsPoint(x,y)).buffer(250,250))
-        self.controller.log_info(str(self.virtual_layer.name()))
-        provider = self.virtual_layer.dataProvider()
-        self.virtual_layer.startEditing()
-        provider.addFeatures([feature])
+        elif event.button() == Qt.RightButton:
+            ParentMapTool.deactivate(self)
+            self.deactivate(self)
+
         self.virtual_layer.commitChanges()
-
-
-
-        self.controller.log_info(str("TEST 4"))
-
-        # vpoly = QgsVectorLayer("Polygon", "pointbuffer", "memory")
-        # feature = QgsFeature()
-        # feature.setGeometry(QgsGeometry.fromPoint(QgsPoint(y, x)).buffer(250, 25))
-        # provider = vpoly.dataProvider()
-        # vpoly.startEditing()
-        # provider.addFeatures([feature])
-        # vpoly.commitChanges()
-        # self.controller.log_info(str("TEST 5"))
-
-        self.iface.mapCanvas().refresh()
-        self.canvas.refreshAllLayers()
 
 
     def activate(self):
 
         # Check button
         self.action().setChecked(True)
-
-        self.init_create_circle_form()
 
         # Store user snapping configuration
         self.snapper_manager.store_snapping_options()
