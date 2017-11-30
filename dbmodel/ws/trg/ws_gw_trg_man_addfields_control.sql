ab1e239e5ad0dc4f587c7830fedf97fde161e867
Xavier Torret <xtorret@bgeo.es>
2017-11-30 20:40:42 +0100

    Reorganizce man_addfields_control trigger

--- dbmodel/ws/trg/ws_gw_trg_man_addfields_control.sql
+++ dbmodel/ws/trg/ws_gw_trg_man_addfields_control.sql
@@ -1,14 +1,15 @@
-﻿/*
+﻿
+/*
 This file is part of Giswater 3
 The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This version of Giswater is provided by Giswater Association
 */
 
---FUNCTION CODE: 1250
+--FUNCTION CODE: 1128
 
 SET search_path= "SCHEMA_NAME", public, pg_catalog;
 
-DROP FUNCTION IF EXISTS gw_trg_man_addfields_value_control() CASCADE;
+
 CREATE OR REPLACE FUNCTION gw_trg_man_addfields_value_control()
   RETURNS trigger AS
 $BODY$
@@ -18,23 +19,20 @@ feature_new_aux varchar;
 feature_old_aux varchar;
     
 BEGIN
-    
+
     EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
 
     feature_type_aux:= TG_ARGV[0];
   
-	IF feature_type_aux='ARC' THEN
+	IF feature_type_aux='NODE' THEN
 			feature_new_aux:= NEW.node_id;
-			feature_old_aux:= OLD.node_id;	
+			feature_old_aux:= OLD.node_id;
 	ELSIF feature_type_aux='NODE' THEN
 			feature_new_aux:= NEW.arc_id;
 			feature_old_aux:= OLD.arc_id;
 	ELSIF feature_type_aux='CONNEC' THEN
 			feature_new_aux:= NEW.connec_id;
 			feature_old_aux:= OLD.connec_id;
-	ELSIF feature_type_aux='GULLY' THEN
-			feature_new_aux:= NEW.gully_id;
-			feature_old_aux:= OLD.gully_id;
 	END IF;
 
     IF TG_OP ='UPDATE' THEN
@@ -43,13 +41,15 @@ BEGIN
 		DELETE FROM man_addfields_value WHERE feature_id=feature_old_aux;	
     END IF;
 
+	
 RETURN NEW;
-
+    
 END; 
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
 
+ 
 DROP TRIGGER IF EXISTS gw_trg_man_addfields_value_node_control ON node;
 CREATE TRIGGER gw_trg_man_addfields_value_node_control AFTER UPDATE OF node_id OR DELETE ON SCHEMA_NAME.node
 FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_man_addfields_value_control('NODE');
@@ -61,10 +61,12 @@ FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_man_addfields_value_control('A
 DROP TRIGGER IF EXISTS gw_trg_man_addfields_value_connec_control ON connec;
 CREATE TRIGGER gw_trg_man_addfields_value_connec_control AFTER UPDATE OF connec_id OR DELETE ON SCHEMA_NAME.connec
 FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_man_addfields_value_control('CONNEC');
-  
-DROP TRIGGER IF EXISTS gw_trg_man_addfields_value_gully_control ON gully;
-CREATE TRIGGER gw_trg_man_addfields_value_gully_control AFTER UPDATE OF gully_id OR DELETE ON SCHEMA_NAME.gully
-FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_man_addfields_value_control('GULLY');
+
+
+
+
+
+
 
 
 
