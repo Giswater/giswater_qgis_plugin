9cfc26ebebe5022be1fa9f858d54cf30b746b8f6
Xavier Torret <xtorret@bgeo.es>
2017-12-27 19:56:30 +0100

    Bug fix on sql file

--- dbmodel/extra/admin_tools/audit/gw_fct_audit_structure.sql
+++ dbmodel/extra/admin_tools/audit/gw_fct_audit_structure.sql
@@ -1,4 +1,4 @@
-/*
+﻿/*
 This file is part of Giswater 3
 The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This version of Giswater is provided by Giswater Association
@@ -42,7 +42,7 @@ BEGIN
 	where table_schema='SCHEMA_NAME' and udt_name <> 'inet';
 
 	CREATE OR REPLACE VIEW "v_audit_schema_table" AS 
-	select
+	select distinct on (table_name)
 	table_catalog,
 	table_schema,
 	table_name
