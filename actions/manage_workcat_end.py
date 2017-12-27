c47498e67f34738ba72b9271087372877cc81dea
Néstor Ibáñez <nestor@bgeo.es>
2017-12-27 14:23:28 +0100

    #311 WIP

--- actions/manage_workcat_end.py
+++ actions/manage_workcat_end.py
@@ -7,12 +7,15 @@ or (at your option) any later version.
 
 # -*- coding: utf-8 -*-
 
-from PyQt4.QtCore import Qt
 
+
+from PyQt4.QtCore import Qt
+from PyQt4.Qt import QDate, QDateTime
+from qgis.gui import QgsDateTimeEdit
 import os
 import sys
 from functools import partial
-
+from datetime import datetime
 plugin_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(plugin_path)
 import utils_giswater
@@ -67,18 +70,40 @@ class ManageWorkcatEnd(ParentManage):
         table_object = "cat_work"
         self.set_completer_object(table_object)
 
-        modelo = self.dlg.tbl_cat_work_x_arc.model()
         tablename = 'v_edit_arc'
         geom_type = 'arc'
         self.dlg.btn_accept.pressed.connect(partial(self.manage_wk_end_accept,self.dlg.tbl_cat_work_x_arc, tablename, geom_type))
         self.dlg.btn_cancel.pressed.connect(partial(self.manage_close, table_object, cur_active_layer))
-
-        self.dlg.tab_feature.currentChanged.connect(partial(self.tab_feature_changed, table_object))
-        #self.dlg.feature_id.textChanged.connect(partial(self.exist_object, table_object))
+        self.dlg.rejected.connect(partial(self.manage_close, table_object, cur_active_layer))
 
         self.dlg.btn_insert.pressed.connect(partial(self.insert_geom_has_group, table_object))
         self.dlg.btn_delete.pressed.connect(partial(self.delete_records, table_object, True))
         self.dlg.btn_snapping.pressed.connect(partial(self.snapping_init, table_object))
+
+        self.dlg.workcat_id_end.currentIndexChanged.connect(partial(self.fill_workids))
+        self.dlg.tab_feature.currentChanged.connect(partial(self.tab_feature_changed, table_object))
+
+        #Set values
+        sql=("SELECT value FROM " + self.controller.schema_name + ".config_param_user "
+            " WHERE parameter ='enddate_vdefault' and cur_user = current_user")
+        row = self.controller.get_row(sql)
+
+        if row:
+            date_value = datetime.strptime(row[0], '%Y-%m-%d')
+        else:
+            date_value = QDateTime.currentDateTime()
+        utils_giswater.setCalendarDate("enddate", date_value)
+        if row:
+            enddate = QDate.fromString(row[0], 'yyyy-MM-dd')
+            self.dlg.enddate.setDate(enddate)
+        else:
+            enddate = QDateTime.currentDateTime()
+            utils_giswater.setCalendarDate(self.dlg.enddate,enddate)
+
+        sql = ("SELECT id FROM " + self.controller.schema_name + ".cat_work")
+        rows = self.controller.get_rows(sql)
+        utils_giswater.fillComboBox(self.dlg.workcat_id_end, rows)
+        utils_giswater.set_autocompleter(self.dlg.workcat_id_end)
 
 
         # Adding auto-completion to a QLineEdit for default feature
@@ -95,25 +120,24 @@ class ManageWorkcatEnd(ParentManage):
         self.dlg.setWindowFlags(Qt.WindowStaysOnTopHint)
         self.dlg.open()
 
+    def fill_workids(self):
+        workcat_id = utils_giswater.getWidgetText(self.dlg.workcat_id_end)
+        sql = ("SELECT workid_key1, workid_key2 FROM " + self.controller.schema_name + ".cat_work "
+               " WHERE id='"+workcat_id+"'")
+        row = self.controller.get_row(sql)
+        utils_giswater.setText(self.dlg.doc_id, row[0])
+        utils_giswater.setText(self.dlg.doc_id_2, row[1])
 
     def manage_wk_end_accept(self, widget, tablename, geom_type):
         selected_list = widget.model()
-        for x in range(0, selected_list.rowCount()):
-            self.controller.log_info(str(x))
-            self.controller.log_info(str("TEST"))
-
-            index = selected_list.index(x,0)
-            self.controller.log_info(str(selected_list.data(index)))
-            sql = (" UPDATE " + self.schema_name + "." + tablename + " SET state='0' WHERE "+geom_type+"_id ='"+str(selected_list.data(index))+"'")
-            self.controller.log_info(str(sql))
-            #status = self.controller.execute_sql(sql)
-
-
-
-
-
-
-
-
-        pass
+        if selected_list is not None:
+            for x in range(0, selected_list.rowCount()):
+                index = selected_list.index(x,0)
+                self.controller.log_info(str(selected_list.data(index)))
+                sql = (" UPDATE " + self.schema_name + "." + tablename + " SET state='0' WHERE "+geom_type+"_id ='"+str(selected_list.data(index))+"'")
+                self.controller.log_info(str(sql))
+                #status = self.controller.execute_sql(sql)
+        else:
+            message = "Select some feature"
+            self.controller.show_info(message)
 
