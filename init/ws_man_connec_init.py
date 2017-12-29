e59a8a991dfd8bacce43b4538e6a86c7485562cf
Albert Bofill <abofill@bgeo.es>
2017-12-29 14:25:30 +0100

    Bug fix on init files

--- init/ws_man_connec_init.py
+++ init/ws_man_connec_init.py
@@ -70,10 +70,10 @@ class ManConnecDialog(ParentDialog):
         self.tbl_hydrometer.clicked.connect(self.check_url)
 
         # Manage custom fields   
-        connectype_id = self.dialog.findChild(QLineEdit, "connectype_id")
-        self.feature_cat_id = connectype_id.text()        
-        tab_custom_fields = 1
-        self.manage_custom_fields(self.feature_cat_id, tab_custom_fields)
+        # connectype_id = self.dialog.findChild(QLineEdit, "connectype_id")
+        # self.feature_cat_id = connectype_id.text()        
+        # tab_custom_fields = 1
+        # self.manage_custom_fields(self.feature_cat_id, tab_custom_fields)
         
         # Check if exist URL from field 'link' in main tab
         self.check_link()
