be9b9ff7fca01e5d6475bacf760c9766782ab2a8
Albert Bofill <abofill@bgeo.es>
2017-12-29 14:07:12 +0100

    Bug fix on sql file

--- dbmodel/ws/trg/ws_gw_trg_edit_arc.sql
+++ dbmodel/ws/trg/ws_gw_trg_edit_arc.sql
@@ -128,13 +128,12 @@ BEGIN
     
         -- FEATURE INSERT
     		INSERT INTO arc (arc_id, code, node_1,node_2, arccat_id, epa_type, sector_id, "state", state_type, annotation, observ,"comment",custom_length,dma_id, presszonecat_id, soilcat_id, function_type, category_type, fluid_type, location_type,
-					workcat_id, workcat_id_end, buildercat_id, builtdate, enddate, ownercat_id, muni_id,streetaxis_id,postcode, streetaxis2_id, postnumber, postnumber2,
-					postcomplement, postcomplement2, descript,verified,the_geom,undelete,label_x,label_y,label_rotation, 
-					publish, inventory, expl_id,num_value)
+					workcat_id, workcat_id_end, buildercat_id, builtdate, enddate, ownercat_id, muni_id, postcode, streetaxis_id, postnumber, postcomplement, streetaxis2_id, postnumber2, postcomplement2,
+					descript,verified,the_geom,undelete,label_x,label_y,label_rotation, publish, inventory, expl_id,num_value)
 					VALUES (NEW.arc_id,NEW.code, null, null, NEW.arccat_id, NEW.epa_type, NEW.sector_id, NEW.state, NEW.state_type, NEW.annotation, NEW.observ, NEW."comment", NEW.custom_length,NEW.dma_id, NEW.presszonecat_id, NEW.soilcat_id, NEW.function_type, 
 					NEW.category_type, NEW.fluid_type, NEW.location_type, NEW.workcat_id, NEW.workcat_id_end, NEW.buildercat_id, NEW.builtdate, NEW.enddate, NEW.ownercat_id, 
-					NEW.muni_id, NEW.streetaxis_id, NEW.postcode, NEW.postcomplement, NEW.postcomplement2,
-					NEW.streetaxis2_id, NEW.postnumber, NEW.postnumber2, NEW.descript, NEW.verified, NEW.the_geom,NEW.undelete, NEW.label_x,NEW.label_y,NEW.label_rotation, 
+					NEW.muni_id, NEW.postcode, NEW.streetaxis_id, NEW.postnumber, NEW.postcomplement, NEW.streetaxis2_id, NEW.postnumber2, NEW.postcomplement2,
+					NEW.descript, NEW.verified, NEW.the_geom,NEW.undelete, NEW.label_x,NEW.label_y,NEW.label_rotation, 
 					NEW.publish, NEW.inventory, NEW.expl_id, NEW.num_value);
 
         -- EPA INSERT
