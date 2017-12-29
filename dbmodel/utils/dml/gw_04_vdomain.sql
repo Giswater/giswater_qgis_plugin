aa03dcc571089ae46ee0ea0b61cdc28865a36fab
Xavier Torret <xtorret@bgeo.es>
2017-12-29 20:24:58 +0100

    Adding vdomain file of custom fields

---
+++ dbmodel/utils/dml/gw_04_vdomain.sql
@@ -0,0 +1,22 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = "SCHEMA_NAME", public, pg_catalog;
+
+INSERT INTO man_addfields_cat_datatype VALUES ('integer', NULL);
+INSERT INTO man_addfields_cat_datatype VALUES ('text', NULL);
+INSERT INTO man_addfields_cat_datatype VALUES ('date', NULL);
+INSERT INTO man_addfields_cat_datatype VALUES ('boolean', NULL);
+INSERT INTO man_addfields_cat_datatype VALUES ('numeric', NULL);
+
+
+INSERT INTO man_addfields_cat_widgettype VALUES ('QLineEdit', NULL);
+INSERT INTO man_addfields_cat_widgettype VALUES ('QComboBox', NULL);
+INSERT INTO man_addfields_cat_widgettype VALUES ('QCheckBox', NULL);
+INSERT INTO man_addfields_cat_widgettype VALUES ('QDateEdit', NULL);
+INSERT INTO man_addfields_cat_widgettype VALUES ('QDateTimeEdit', NULL);
+INSERT INTO man_addfields_cat_widgettype VALUES ('QTextEdit', NULL);
