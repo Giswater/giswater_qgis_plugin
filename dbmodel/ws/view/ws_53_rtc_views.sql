ad11d08ecdfa674ccab3f526dfb06c9df903a1f1
David Erill <daviderill79@gmail.com>
2018-01-03 18:20:24 +0100

    Bug fix in hydrometer views Field type of 'hydrometer_id' is text

--- dbmodel/ws/view/ws_53_rtc_views.sql
+++ dbmodel/ws/view/ws_53_rtc_views.sql
@@ -54,12 +54,10 @@ ext_cat_hydrometer.multi_jet_flow,
 ext_cat_hydrometer.dnom,
 concat((select value from config_param_system where parameter='hydrometer_link_absolute_path'),rtc_hydrometer.link) as hydrometer_link
    FROM rtc_hydrometer
-     LEFT JOIN ext_rtc_hydrometer ON ext_rtc_hydrometer.hydrometer_id::integer = rtc_hydrometer.hydrometer_id::integer
-     LEFT JOIN ext_cat_hydrometer ON ext_cat_hydrometer.id::text = ext_rtc_hydrometer.cat_hydrometer_id
-     JOIN rtc_hydrometer_x_connec ON rtc_hydrometer_x_connec.hydrometer_id::integer = rtc_hydrometer.hydrometer_id::integer
-     JOIN connec ON rtc_hydrometer_x_connec.connec_id::text = connec.connec_id::text;
-
-
+     LEFT JOIN ext_rtc_hydrometer ON ext_rtc_hydrometer.hydrometer_id = rtc_hydrometer.hydrometer_id
+     LEFT JOIN ext_cat_hydrometer ON ext_cat_hydrometer.id = ext_rtc_hydrometer.cat_hydrometer_id
+     JOIN rtc_hydrometer_x_connec ON rtc_hydrometer_x_connec.hydrometer_id = rtc_hydrometer.hydrometer_id
+     JOIN connec ON rtc_hydrometer_x_connec.connec_id = connec.connec_id;
 
 
 
@@ -76,9 +74,9 @@ CREATE OR REPLACE VIEW v_rtc_hydrometer_period AS
             ELSE ext_rtc_hydrometer_x_data.sum * 1000::double precision / ext_cat_period.period_seconds::double precision
         END AS lps_avg
    FROM ext_rtc_hydrometer
-     JOIN ext_rtc_hydrometer_x_data ON ext_rtc_hydrometer_x_data.hydrometer_id::integer= ext_rtc_hydrometer.hydrometer_id::integer
+     JOIN ext_rtc_hydrometer_x_data ON ext_rtc_hydrometer_x_data.hydrometer_id = ext_rtc_hydrometer.hydrometer_id
      JOIN ext_cat_period ON ext_rtc_hydrometer_x_data.cat_period_id = ext_cat_period.id
-     JOIN rtc_hydrometer_x_connec ON rtc_hydrometer_x_connec.hydrometer_id::integer= ext_rtc_hydrometer.hydrometer_id::integer
+     JOIN rtc_hydrometer_x_connec ON rtc_hydrometer_x_connec.hydrometer_id = ext_rtc_hydrometer.hydrometer_id
      JOIN connec ON connec.connec_id = rtc_hydrometer_x_connec.connec_id
      JOIN inp_options ON inp_options.rtc_period_id = ext_cat_period.id;
 
@@ -91,7 +89,7 @@ CREATE OR REPLACE VIEW v_rtc_dma_hydrometer_period AS
     sum(v_rtc_hydrometer_period.m3_total_period) AS m3_total_period, 
     ext_cat_period.period_seconds
    FROM v_rtc_hydrometer_period
-   JOIN ext_rtc_hydrometer_x_data ON ext_rtc_hydrometer_x_data.hydrometer_id::integer = v_rtc_hydrometer_period.hydrometer_id::integer
+   JOIN ext_rtc_hydrometer_x_data ON ext_rtc_hydrometer_x_data.hydrometer_id = v_rtc_hydrometer_period.hydrometer_id
    JOIN inp_options ON inp_options.rtc_period_id = ext_rtc_hydrometer_x_data.cat_period_id
    JOIN ext_cat_period ON inp_options.rtc_period_id = ext_cat_period.id
   GROUP BY v_rtc_hydrometer_period.dma_id, ext_cat_period.id, ext_cat_period.period_seconds;
@@ -141,7 +139,7 @@ CREATE OR REPLACE VIEW v_rtc_hydrometer_x_node_period AS
     v_rtc_dma_parameter_period.cmax,
     v_rtc_hydrometer_period.lps_avg * 0.5::double precision * v_rtc_dma_parameter_period.cmax AS lps_max
    FROM v_rtc_hydrometer_x_arc
-     LEFT JOIN v_rtc_hydrometer_period ON v_rtc_hydrometer_period.hydrometer_id::integer = v_rtc_hydrometer_x_arc.hydrometer_id::integer
+     LEFT JOIN v_rtc_hydrometer_period ON v_rtc_hydrometer_period.hydrometer_id = v_rtc_hydrometer_x_arc.hydrometer_id
      LEFT JOIN v_rtc_dma_parameter_period ON v_rtc_hydrometer_period.period_id = v_rtc_dma_parameter_period.period_id
 UNION
  SELECT 
@@ -158,7 +156,7 @@ UNION
     v_rtc_dma_parameter_period.cmax,
     v_rtc_hydrometer_period.lps_avg * 0.5::double precision * v_rtc_dma_parameter_period.cmax AS lps_max
    FROM v_rtc_hydrometer_x_arc
-     LEFT JOIN v_rtc_hydrometer_period ON v_rtc_hydrometer_period.hydrometer_id::integer = v_rtc_hydrometer_x_arc.hydrometer_id::integer
+     LEFT JOIN v_rtc_hydrometer_period ON v_rtc_hydrometer_period.hydrometer_id = v_rtc_hydrometer_x_arc.hydrometer_id
      LEFT JOIN v_rtc_dma_parameter_period ON v_rtc_hydrometer_period.period_id = v_rtc_dma_parameter_period.period_id;
 
 
