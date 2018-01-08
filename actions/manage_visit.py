b95d3b9a1d4bd84261feb3e9e9c3bfea8fed7243
Luigi Pirelli <luipir@gmail.com>
2018-01-08 12:33:33 +0100

    moved combo size adjust to the .ui

--- actions/manage_visit.py
+++ actions/manage_visit.py
@@ -128,9 +128,6 @@ class ManageVisit(ParentManage):
     
     def fill_combos(self):
         """ Fill combo boxes of the form """
-        
-        # adjust combo policy size to be adapted to the content
-        self.visitcat_id.setSizeAdjustPolicy(QComboBox.AdjustToContents)
 
         # Fill ComboBox visitcat_id
         sql = ("SELECT id, name"
