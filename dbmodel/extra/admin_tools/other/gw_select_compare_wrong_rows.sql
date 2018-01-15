888e47ed647af038c3d3f3e6a50ce6f1ccaaf4f5
Albert Bofill <abofill@bgeo.es>
2018-01-15 16:51:46 +0100

    New query to compare audit_cat_table with other schema

---
+++ dbmodel/extra/admin_tools/other/gw_select_compare_wrong_rows.sql
@@ -0,0 +1,6 @@
+﻿SELECT row_number() OVER (ORDER BY id) AS rid,
+    id,
+    v_audit_schema_foreign_table.table_name
+   FROM ws_data.audit_cat_table
+     LEFT JOIN ws_data.v_audit_schema_foreign_table ON v_audit_schema_foreign_table.table_name::text = id
+  WHERE v_audit_schema_foreign_table.table_name IS NULL
