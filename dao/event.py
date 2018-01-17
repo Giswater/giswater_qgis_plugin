8d6e1e771a0b57d5c2e1ca27e69f44d7228751da
Luigi Pirelli <luipir@gmail.com>
2018-01-17 17:19:29 +0100

    a almost definitive Event and Table class implementation

--- dao/event.py
+++ dao/event.py
@@ -14,183 +14,34 @@ __copyright__ = '(C) 2018, Luigi Pirelli'
 
 __revision__ = '$Format:%H$'
 
-class Event(object):
-    """Base class for composed events."""
+from dao.table import (
+    Table,
+    GenericDescriptor
+)
+
+class Event(Table):
+    """Class table for Events."""
+
+    id = GenericDescriptor(None)
+    ext_code = GenericDescriptor(None)
+    visit_id = GenericDescriptor(None)
+    position_id = GenericDescriptor(None)
+    position_value = GenericDescriptor(None)
+    parameter_id = GenericDescriptor(None)
+    value = GenericDescriptor(None)
+    value1 = GenericDescriptor(None)
+    value2 = GenericDescriptor(None)
+    geom1 = GenericDescriptor(None)
+    geom2 = GenericDescriptor(None)
+    geom3 = GenericDescriptor(None)
+    xcoord = GenericDescriptor(None)
+    ycoord = GenericDescriptor(None)
+    compass = GenericDescriptor(None)
+    tstamp = GenericDescriptor(None)
+    text = GenericDescriptor(None)
+    index_val = GenericDescriptor(None)
+    is_last = GenericDescriptor(None)
 
     def __init__(self, controller):
         """constructor."""
-        super(Event, self)-__init__()
-
-        # save DB engine top allow queries
-        _controller = controller
-
-        _id = None
-        _ext_code = None
-        _position_id = None
-        _position_value = None
-        _parameter_id = None
-        _value = None
-        _value1 = None
-        _value2 = None
-        _geom1 = None
-        _geom2 = None
-        _geom3 = None
-        _xcoord = None
-        _ycoord = None
-        _compass = None
-        _tstamp = None
-        _text = None
-        _index_val = None
-        _is_last = None
-
-    def get(self, id):
-        """retrieve a record with a specified primary key id."""
-        pass
-
-    def save(self):
-        """Save current event in the DB as new record."""
-        pass
-
-    def update(self):
-        """Dump current event state in db if record exist."""
-        pass
-
-    @property
-    def id(self):
-        return self._id
-    @id.setter
-    def id(self, v):
-        self._id = v
-
-    @property
-    def ext_code(self):
-        return self._ext_code
-    @ext_code.setter
-    def ext_code(self, v):
-        self._ext_code = v
-
-    @property
-    def visit_id(self):
-        return self._visit_id
-    @visit_id.setter
-    def visit_id(self, v):
-        self._visit_id = v
-
-    @property
-    def position_id(self):
-        return self._position_id
-    @position_id.setter
-    def position_id(self, v):
-        self._position_id = v
-
-    @property
-    def position_value(self):
-        return self._position_value
-    @position_value.setter
-    def position_value(self, v):
-        self._position_value = v
-
-    @property
-    def parameter_id(self):
-        return self._parameter_id
-    @parameter_id.setter
-    def parameter_id(self, v):
-        self._parameter_id = v
-
-    @property
-    def value(self):
-        return self._value
-    @value.setter
-    def value(self, v):
-        self._value = v
-
-    @property
-    def value1(self):
-        return self._value1
-    @value1.setter
-    def value1(self, v):
-        self._value1 = v
-
-    @property
-    def value2(self):
-        return self._value2
-    @value2.setter
-    def value2(self, v):
-        self._value2 = v
-
-    @property
-    def geom1(self):
-        return self._geom1
-    @geom1.setter
-    def geom1(self, v):
-        self._geom1 = v
-
-    @property
-    def geom2(self):
-        return self._geom2
-    @geom2.setter
-    def geom2(self, v):
-        self._geom2 = v
-
-    @property
-    def geom3(self):
-        return self._geom3
-    @geom3.setter
-    def geom3(self, v):
-        self._geom3 = v
-
-    @property
-    def xcoord(self):
-        return self._xcoord
-    @xcoord.setter
-    def xcoord(self, v):
-        self._xcoord = v
-
-    @property
-    def ycoord(self):
-        return self._ycoord
-    @ycoord.setter
-    def ycoord(self, v):
-        self._ycoord = v
-
-    @property
-    def compass(self):
-        return self._compass
-    @compass.setter
-    def compass(self, v):
-        self._compass = v
-
-    @property
-    def tstamp(self):
-        return self._tstamp
-    @tstamp.setter
-    def tstamp(self, v):
-        self._tstamp = v
-
-    @property
-    def tstamp(self):
-        return self._tstamp
-    @tstamp.setter
-    def tstamp(self, v):
-        self._tstamp = v
-
-    @property
-    def text(self):
-        return self._text
-    @text.setter
-    def text(self, v):
-        self._text = v
-
-    @property
-    def index_val(self):
-        return self._index_val
-    @index_val.setter
-    def index_val(self, v):
-        self._index_val = v
-
-    @property
-    def is_last(self):
-        return self._is_last
-    @is_last.setter
-    def is_last(self, v):
-        self._is_last = v
+        super(Event, self).__init__(controller, 'om_visit_event', 'id')
---
+++ dao/table.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+This file is part of Giswater 2.0
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+"""
+
+__author__ = 'Luigi Pirelli'
+__date__ = 'January 2018'
+__copyright__ = '(C) 2018, Luigi Pirelli'
+
+# This will get replaced with a git SHA1 when you do a git archive
+
+__revision__ = '$Format:%H$'
+
+from weakref import WeakKeyDictionary
+
+class GenericDescriptor(object):
+    """A descriptor that set getter and setter.
+    class example from: http://nbviewer.jupyter.org/urls/gist.github.com/ChrisBeaumont/5758381/raw/descriptor_writeup.ipynb"""
+    def __init__(self, default):
+        self.default = default
+        self.data = WeakKeyDictionary()
+        
+    def __get__(self, instance, owner):
+        # we get here when someone calls x.d, and d is a NonNegative instance
+        # instance = x
+        # owner = type(x)
+        return self.data.get(instance, self.default)
+    
+    def __set__(self, instance, value):
+        # we get here when someone calls x.d = val
+        # instance = x
+        # value = val
+        self.data[instance] = value
+
+
+class Table(object):
+    """Base class representing a table.
+    Assume it have to be used as a pure virtual."""
+    def __init__(self, controller, tableName, pk):
+        self.__controller = controller
+        self.__tableName = tableName
+        self.__pk = pk
+
+
+    def fetch(self):
+        """retrieve a record with a specified primary key id."""
+        if not getattr(self, self.__pk):
+            message = "No " + self.__pk + " primary key value set"
+            self.__controller.show_info(message)
+            return
+
+        fields = vars(self.__class__).keys()
+        # remove all _<classname>__<name> or __<names>__ vars, e.g. private vars
+        fields = [x for x in fields if "__" not in x]
+        
+        sql = "SELECT {0} FROM {1}.{2} WHERE {3} = {4}".format(
+            ", ".join(fields),
+            self.__controller.schema_name,
+            self.__tableName,
+            self.__pk,
+            getattr(self, self.__pk))
+        row = self.__controller.get_row(sql)
+        if not row:
+            message = "No records of " + type(self).__name__ + " found with sql: " + sql
+            self.__controller.show_info(message)
+            return
+        
+        # set values of the current Event get from row values
+        for field, value in zip(fields, row):
+            setattr(self, field, value)
+
+
+    def upsert(self):
+        """Save current event state in the DB as new record.
+        Eventually add the record if it is not available"""
+        fields = vars(self).keys()
+        # remove all _<classname>__<name> or __<names>__ vars, e.g. private vars
+        fields = [x for x in fields if "__" not in x]
+        values = [ getattr(self, field) for field in fields]
+
+        status = self.__controller.execute_upsert(self.__tableName, self.__pk, fields, values)
+        if status:
+            message = "Values has been added/updated"
+            self.__controller.show_info(message)
+        
+        # get new added id in case of an insert
+        if not getattr(self, self.__pk):
+            # get latest updated sequence ASSUMED a sequence is available!
+            # usign lastval can generate problems in case of parallel inserts
+            # sql = ("SELECT lastval()")
+            sql = ("SELECT currval(pg_get_serial_sequence(" + "'" + self.__tableName + "', '" + self.__pk + "'))")
+            row = self.__controller.get_row(sql)
+            setattr(self, self.__pk, row[0])
+
