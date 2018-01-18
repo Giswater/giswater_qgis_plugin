8582d3d1fa68605b4dc32c264a9e9f7de6ddffb4
Luigi Pirelli <luipir@gmail.com>
2018-01-18 20:21:03 +0100

    ORM for tables modified in tab Relations

---
+++ dao/om_visit_x_connec.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+This file is part of Giswater 2.0
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+"""
+
+__author__ = 'Luigi Pirelli'
+__date__ = 'January 2018'
+__copyright__ = '(C) 2018, Luigi Pirelli'
+
+# This will get replaced with a git SHA1 when you do a git archive
+
+__revision__ = '$Format:%H$'
+
+from dao.table import (
+    Table,
+    GenericDescriptor
+)
+
+class om_visit_x_connec(Table):
+    """Class table for Visit."""
+
+    id = GenericDescriptor(-1)
+    visit_id = GenericDescriptor(None)
+    connec_id = GenericDescriptor(None)
+    is_last = GenericDescriptor(None)
+
+    def __init__(self, controller):
+        """constructor."""
+        Table.__init__(self, controller, 'om_visit_x_connec', 'id')
