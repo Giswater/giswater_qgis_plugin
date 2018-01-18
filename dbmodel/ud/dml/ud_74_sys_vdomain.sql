10f3fc9e39e0c0cb2adb7c5b68d7038bde24a7ad
Albert Bofill <abofill@bgeo.es>
2018-01-18 11:23:25 +0100

    UD- Bug fix on roles

--- dbmodel/ud/dml/ud_74_sys_vdomain.sql
+++ dbmodel/ud/dml/ud_74_sys_vdomain.sql
@@ -45,12 +45,12 @@ INSERT INTO sys_feature_cat VALUES ('WWTP', 'NODE', 10, 'v_edit_man_wwtp', 'P');
 -- -----------------------------
 -- Records of sys_role
 -- -----------------------------
-INSERT INTO sys_role VALUES ('basic', 'basic', NULL);
-INSERT INTO sys_role VALUES ('edit', 'edit', NULL);
-INSERT INTO sys_role VALUES ('om', 'om', NULL);
-INSERT INTO sys_role VALUES ('epa', 'epa', NULL);
-INSERT INTO sys_role VALUES ('master', 'master', NULL);
-INSERT INTO sys_role VALUES ('admin', 'admin', NULL);
+INSERT INTO sys_role VALUES ('role_basic', 'basic', NULL);
+INSERT INTO sys_role VALUES ('role_edit', 'edit', NULL);
+INSERT INTO sys_role VALUES ('role_om', 'om', NULL);
+INSERT INTO sys_role VALUES ('role_epa', 'epa', NULL);
+INSERT INTO sys_role VALUES ('role_master', 'master', NULL);
+INSERT INTO sys_role VALUES ('role_admin', 'admin', NULL);
 
 
 -- ----------------------------
