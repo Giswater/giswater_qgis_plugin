3afee5f6a0cb2999e2d9c00d6aab5639d2f21a18
Albert Bofill <abofill@bgeo.es>
2018-01-24 10:08:12 +0100

    Bug fix on sql file

--- dbmodel/utils/fct/gw_fct_audit_log_feature.sql
+++ dbmodel/utils/fct/gw_fct_audit_log_feature.sql
@@ -6,7 +6,7 @@ This version of Giswater is provided by Giswater Association
 
 --FUNCTION CODE: xxxx
 
-CREATE OR REPLACE FUNCTION "ws_data_albert".gw_fct_audit_log_feature(enabled_aux text)
+CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_fct_audit_log_feature(enabled_aux text)
   RETURNS void AS
 $BODY$ 
 DECLARE
@@ -14,7 +14,7 @@ project_type_aux varchar;
 
 BEGIN
 
-	SET search_path = "ws_data_albert", public;
+	SET search_path = "SCHEMA_NAME", public;
 	
 	SELECT wsoftware INTO project_type_aux FROM version LIMIT 1;
 	
@@ -26,38 +26,38 @@ BEGIN
 
 			CREATE TRIGGER gw_trg_audit_log_feature_arc
 			  AFTER UPDATE OR DELETE
-			  ON "ws_data_albert".arc
+			  ON "SCHEMA_NAME".arc
 			  FOR EACH ROW
-			  EXECUTE PROCEDURE "ws_data_albert".gw_trg_audit_log_feature();
+			  EXECUTE PROCEDURE "SCHEMA_NAME".gw_trg_audit_log_feature();
 		  
 
 			CREATE TRIGGER gw_trg_audit_log_feature_node
 			  AFTER UPDATE OR DELETE
-			  ON "ws_data_albert".node
+			  ON "SCHEMA_NAME".node
 			  FOR EACH ROW
-			  EXECUTE PROCEDURE "ws_data_albert".gw_trg_audit_log_feature();
+			  EXECUTE PROCEDURE "SCHEMA_NAME".gw_trg_audit_log_feature();
 			  
 			  
 			CREATE TRIGGER gw_trg_audit_log_feature_connec
 			  AFTER UPDATE OR DELETE
-			  ON "ws_data_albert".connec
+			  ON "SCHEMA_NAME".connec
 			  FOR EACH ROW
-			  EXECUTE PROCEDURE "ws_data_albert".gw_trg_audit_log_feature();
+			  EXECUTE PROCEDURE "SCHEMA_NAME".gw_trg_audit_log_feature();
 			  
 			  
 			CREATE TRIGGER gw_trg_audit_log_feature_element
 			  AFTER UPDATE OR DELETE
-			  ON "ws_data_albert".element
+			  ON "SCHEMA_NAME".element
 			  FOR EACH ROW
-			  EXECUTE PROCEDURE "ws_data_albert".gw_trg_audit_log_feature();
+			  EXECUTE PROCEDURE "SCHEMA_NAME".gw_trg_audit_log_feature();
 				  
 			IF project_type_aux='UD' THEN
 				  
 				CREATE TRIGGER gw_trg_audit_log_feature_gully
 				AFTER UPDATE OR DELETE
-				ON "ws_data_albert".gully 
+				ON "SCHEMA_NAME".gully 
 				FOR EACH ROW
-				EXECUTE PROCEDURE "ws_data_albert".gw_trg_audit_log_feature();
+				EXECUTE PROCEDURE "SCHEMA_NAME".gw_trg_audit_log_feature();
 			END IF;	  
 		
 		END IF;
@@ -68,14 +68,14 @@ BEGIN
 
 		IF enabled_aux = 'ENABLE' THEN
 
-			ALTER TABLE "ws_data_albert".arc ENABLE TRIGGER gw_trg_audit_log_feature_arc;
-			ALTER TABLE "ws_data_albert".node ENABLE TRIGGER gw_trg_audit_log_feature_node;
-			ALTER TABLE "ws_data_albert".connec ENABLE TRIGGER gw_trg_audit_log_feature_connec;
-			ALTER TABLE "ws_data_albert".element ENABLE TRIGGER gw_trg_audit_log_feature_element;
+			ALTER TABLE "SCHEMA_NAME".arc ENABLE TRIGGER gw_trg_audit_log_feature_arc;
+			ALTER TABLE "SCHEMA_NAME".node ENABLE TRIGGER gw_trg_audit_log_feature_node;
+			ALTER TABLE "SCHEMA_NAME".connec ENABLE TRIGGER gw_trg_audit_log_feature_connec;
+			ALTER TABLE "SCHEMA_NAME".element ENABLE TRIGGER gw_trg_audit_log_feature_element;
 
 			IF project_type_aux='UD' THEN
 
-				ALTER TABLE "ws_data_albert".gully ENABLE TRIGGER gw_trg_audit_log_feature_gully;
+				ALTER TABLE "SCHEMA_NAME".gully ENABLE TRIGGER gw_trg_audit_log_feature_gully;
 			END IF;
 
 		END IF;
@@ -86,14 +86,14 @@ BEGIN
 	
 		IF enabled_aux = 'DISABLE' THEN
 			
-			ALTER TABLE "ws_data_albert".arc DISABLE TRIGGER gw_trg_audit_log_feature_arc;
-			ALTER TABLE "ws_data_albert".node DISABLE TRIGGER gw_trg_audit_log_feature_node;
-			ALTER TABLE "ws_data_albert".connec DISABLE TRIGGER gw_trg_audit_log_feature_connec;
-			ALTER TABLE "ws_data_albert".element DISABLE TRIGGER gw_trg_audit_log_feature_element;
+			ALTER TABLE "SCHEMA_NAME".arc DISABLE TRIGGER gw_trg_audit_log_feature_arc;
+			ALTER TABLE "SCHEMA_NAME".node DISABLE TRIGGER gw_trg_audit_log_feature_node;
+			ALTER TABLE "SCHEMA_NAME".connec DISABLE TRIGGER gw_trg_audit_log_feature_connec;
+			ALTER TABLE "SCHEMA_NAME".element DISABLE TRIGGER gw_trg_audit_log_feature_element;
 			
 			IF project_type_aux='UD' THEN
 		
-			ALTER TABLE "ws_data_albert".gully DISABLE TRIGGER gw_trg_audit_log_feature_gully;
+			ALTER TABLE "SCHEMA_NAME".gully DISABLE TRIGGER gw_trg_audit_log_feature_gully;
 
 			END IF;
 			
