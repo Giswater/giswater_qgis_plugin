63002a885c52d4adfc350caf52e9d704b8b8a766
Luigi Pirelli <luipir@gmail.com>
2018-01-24 13:42:41 +0100

    GUI reorganization to be flexible and using standatd Dialog buttons

--- ui/visit_management.py
+++ ui/visit_management.py
@@ -6,7 +6,7 @@ form_name = 'visit_management.ui'
 FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), form_name))
 
 
-class VisitManagement(QtGui.QDialog, FORM_CLASS):
+class VisitManagement(FORM_CLASS):
 
     def __init__(self):
         """ Constructor """
