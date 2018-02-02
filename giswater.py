ef9145d179c4aadab86750323de456f139407886
Edgar Fuste <efuste@bgeo.es>
2018-02-02 09:14:55 +0100

    #328 enhance reed project (user msg put in method)

--- giswater.py
+++ giswater.py
@@ -679,42 +679,7 @@ class Giswater(QObject):
                     self.layer_version = cur_layer
 
         #Edgar
-        sql = ("DELETE FROM" + self.schema_name + ".audit_check_project" 
-               " WHERE user_name = current_user AND fprocesscat_id = 1")
-        self.controller.execute_sql(sql)
-        for layer in layers:
-            layer_source = self.controller.get_layer_source(layer)
-            schema_name = layer_source['schema']
-            if schema_name is not None:
-                schema_name = schema_name.replace('"', '')
-                table_name = layer_source['table']
-                db_name = layer_source['db']
-                host_name = layer_source['host']
-
-                sql = ("INSERT INTO " + self.schema_name + ".audit_check_project (table_schema, table_id, table_dbname, table_host, fprocesscat_id) "
-                       " VALUES ('" + schema_name + "', '" + table_name + "','" + db_name + "','" + host_name + "',1)")
-                self.controller.execute_sql(sql)
-        sql = ("SELECT " + self.schema_name + ".gw_fct_audit_check_project(1)")
-        row = self.controller.get_row(sql, commit=True)
-        self.controller.log_info(str(row))
-
-        if str(row) == "[-1]":
-            message = "This is not a GisWater Project."
-            self.controller.ask_question(message, "Alert!!")
-
-        elif row > 0:
-            message = "You are missing " + str(row) + " layers of your rol. Do you want show them?"
-            answer = self.controller.ask_question(message, "Alert!!")
-            if answer:
-                sql = ("SELECT table_id FROM " + self.schema_name + ".audit_check_project where enabled=false ")
-                rows = self.controller.get_rows(sql)
-                self.controller.log_info(str(rows))
-                message = ""
-                for row in rows:
-                    message = str(message+row[0]+"\n")
-                self.controller.ask_question(message, "Info :")
-
-
+        self.populate_audit_check_project(layers)
 
         #Edgar
         # Check if table 'version' and man_junction exists
@@ -932,4 +897,39 @@ class Giswater(QObject):
                " VALUES(" + expl_id + ", current_user);")
         self.controller.execute_sql(sql)        
         
-        
+    def populate_audit_check_project(self, layers):
+        sql = ("DELETE FROM" + self.schema_name + ".audit_check_project"
+               " WHERE user_name = current_user AND fprocesscat_id = 1")
+        self.controller.execute_sql(sql)
+        for layer in layers:
+            layer_source = self.controller.get_layer_source(layer)
+            schema_name = layer_source['schema']
+            if schema_name is not None:
+                schema_name = schema_name.replace('"', '')
+                table_name = layer_source['table']
+                db_name = layer_source['db']
+                host_name = layer_source['host']
+
+                sql = (
+                        "INSERT INTO " + self.schema_name + ".audit_check_project (table_schema, table_id, table_dbname, table_host, fprocesscat_id) "
+                                                            " VALUES ('" + schema_name + "', '" + table_name + "','" + db_name + "','" + host_name + "',1)")
+                self.controller.execute_sql(sql)
+        sql = ("SELECT " + self.schema_name + ".gw_fct_audit_check_project(1)")
+        row = self.controller.get_row(sql, commit=True)
+        self.controller.log_info(str(row))
+
+        if str(row) == "[-1]":
+            message = "This is not a GisWater Project."
+            self.controller.ask_question(message, "Alert!!")
+
+        elif row > 0:
+            message = "You are missing " + str(row) + " layers of your rol. Do you want show them?          "
+            answer = self.controller.ask_question(message, "Alert!!")
+            if answer:
+                sql = ("SELECT table_id FROM " + self.schema_name + ".audit_check_project where enabled=false ")
+                rows = self.controller.get_rows(sql)
+                self.controller.log_info(str(rows))
+                message = ""
+                for row in rows:
+                    message = str(message + row[0] + "\n")
+                self.controller.ask_question(message, "Info :")
