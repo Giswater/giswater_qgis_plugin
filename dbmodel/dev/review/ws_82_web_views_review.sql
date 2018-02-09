ac2d38937d6617fcac8bff440fae3aaefff3298d
Barbara Rzepka <bbrzepka@bgeo.es>
2018-02-09 13:39:03 +0100

    WS - new review trg WIP

--- dbmodel/dev/review/ws_82_web_views_review.sql
+++ dbmodel/dev/review/ws_82_web_views_review.sql
@@ -6,60 +6,129 @@ This version of Giswater is provided by Giswater Association
 
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
-/*
+
 	
  --REVIEW VIEWS
  
-DROP VIEW IF EXISTS v_edit_review_node CASCADE;
-CREATE VIEW v_edit_review_node AS 
- SELECT audit_review_node.node_id,
-	node.nodecat_id,
-    node.elevation,
-    node."depth",
-	node."state",
-	audit_review_node.nodecat_id AS field_nodecat_id,
-    audit_review_node.elevation AS field_elevation,
-    audit_review_node."depth" AS field_depth,
-    audit_review_node.annotation,
-    audit_review_node.observ,
-	audit_review_node.moved_geom,
-    audit_review_node.office_checked,
-	audit_review_node.the_geom	
-   FROM node
-     RIGHT JOIN audit_review_node ON node.node_id = audit_review_node.node_id
-  WHERE audit_review_node.field_checked IS TRUE AND audit_review_node.office_checked IS NOT TRUE;
+DROP VIEW IF EXISTS v_edit_review_arc;
+CREATE VIEW  v_edit_review_arc AS SELECT
+review_arc.arc_id,
+review_arc.matcat_id, 
+review_arc.pnom, 
+review_arc.dnom, 
+review_arc.annotation, 
+review_arc.observ, 
+review_arc.expl_id, 
+review_arc.the_geom, 
+review_arc.field_checked
+FROM review_arc, selector_expl
+WHERE selector_expl.cur_user="current_user"() AND review_arc.expl_id=selector_expl.expl_id;
   
+   
+DROP VIEW IF EXISTS v_edit_review_audit_arc;
+CREATE VIEW v_edit_review_audit_arc AS SELECT
+review_audit_arc.id,
+arc_id,
+old_matcat_id, 
+new_matcat_id,
+old_pnom, 
+new_pnom, 
+old_dnom, 
+new_dnom, 
+old_arccat_id, 
+new_arccat_id,  
+annotation,
+observ,
+review_audit_arc.expl_id,
+the_geom,
+review_status_id,
+field_date,
+field_user,
+is_validated
+FROM review_audit_arc,selector_expl
+WHERE selector_expl.cur_user="current_user"() AND review_audit_arc.expl_id=selector_expl.expl_id
+AND review_status_id!=0;
+ 
+ 
+DROP VIEW IF EXISTS v_edit_review_node;
+CREATE VIEW  v_edit_review_node AS SELECT
+node_id, 
+elevation, 
+depth, 
+from_plot, 
+nodetype_id, 
+nodecat_id, 
+annotation, 
+observ,
+review_node.expl_id,
+the_geom, 
+field_checked
+FROM review_node, selector_expl
+WHERE selector_expl.cur_user="current_user"() AND review_node.expl_id=selector_expl.expl_id;
 
-CREATE OR REPLACE VIEW v_edit_review_arc AS 
- SELECT audit_review_arc.arc_id,
-    arc.arccat_id,
-	arc."state",
-    audit_review_arc.arccat_id AS field_arccat_id,
-    audit_review_arc.annotation,
-    audit_review_arc.observ,
-    audit_review_arc.moved_geom,
-    audit_review_arc.office_checked,
-    audit_review_arc.the_geom
-   FROM arc
-     RIGHT JOIN audit_review_arc ON arc.arc_id = audit_review_arc.arc_id
-  WHERE audit_review_arc.field_checked IS TRUE AND audit_review_arc.office_checked IS NOT TRUE;
-  
-  
-  CREATE OR REPLACE VIEW v_edit_review_connec AS 
-	SELECT audit_review_connec.connec_id,
-	 connec.elevation,
-	 connec."depth",
-	 connec.connecat_id,
-	 connec."state",
-	 audit_review_connec.elevation as field_elevation,
-	 audit_review_connec."depth" AS field_depth,
-	 audit_review_connec.connecat_id AS field_connecat_id,
-	 audit_review_connec.annotation,
-	 audit_review_connec.observ,
-	 audit_review_connec.moved_geom,
-	 audit_review_connec.office_checked,
-	 audit_review_connec.the_geom
-	 FROM connec
-		RIGHT JOIN audit_review_connec ON connec.connec_id = audit_review_connec.connec_id
-  WHERE audit_review_connec.field_checked IS TRUE AND audit_review_connec.office_checked IS NOT TRUE;
-*/
+
+
+DROP VIEW IF EXISTS v_edit_review_audit_node;
+CREATE VIEW  v_edit_review_audit_node AS SELECT
+review_audit_node.id, 
+node_id, 
+old_elevation, 
+new_elevation, 
+old_depth, 
+new_depth, 
+old_from_plot, 
+new_from_plot, 
+old_nodetype_id, 
+new_nodetype_id, 
+old_nodecat_id, 
+new_nodecat_id,
+annotation,
+observ,
+review_audit_node.expl_id,
+the_geom,
+review_status_id,
+field_date,
+field_user,
+is_validated
+FROM review_audit_node, selector_expl
+WHERE selector_expl.cur_user="current_user"() AND review_audit_node.expl_id=selector_expl.expl_id
+AND review_status_id!=0;
+
+
+
+DROP VIEW IF EXISTS v_edit_review_connec;
+CREATE VIEW  v_edit_review_connec AS SELECT
+connec_id, 
+matcat_id, 
+pnom, 
+dnom, 
+annotation, 
+observ,
+review_connec.expl_id, 
+the_geom, 
+field_checked
+FROM review_connec,selector_expl
+WHERE selector_expl.cur_user="current_user"() AND review_connec.expl_id=selector_expl.expl_id;
+
+
+DROP VIEW IF EXISTS v_edit_review_audit_connec;
+CREATE VIEW v_edit_review_audit_connec AS SELECT
+review_audit_connec.id,
+connec_id, 
+old_matcat_id, 
+new_matcat_id, 
+old_pnom, 
+new_pnom, 
+old_dnom, 
+new_dnom,
+annotation, 
+observ, 
+review_audit_connec.expl_id, 
+the_geom, 
+review_status_id, 
+field_date, 
+field_user, 
+is_validated
+FROM review_audit_connec,selector_expl
+WHERE selector_expl.cur_user="current_user"() AND review_audit_connec.expl_id=selector_expl.expl_id
+AND review_status_id!=0;
--- dbmodel/ws/ddl/ws_81_review.sql
+++ dbmodel/ws/ddl/ws_81_review.sql
@@ -10,21 +10,42 @@ SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
 CREATE TABLE review_arc (  
 arc_id character varying(16) NOT NULL,
-the_geom geometry(LINESTRING,SRID_VALUE),
 matcat_id varchar(30)  ,
 pnom varchar(16)  ,
 dnom varchar(16)  ,
 annotation character varying(254),
 observ character varying(254),
-verified text,
+expl_id integer,
+the_geom geometry(LINESTRING,SRID_VALUE),
 field_checked boolean,
 CONSTRAINT review_arc_pkey PRIMARY KEY (arc_id)
+);
+ 
+  
+CREATE TABLE review_audit_arc(  
+id serial PRIMARY KEY,
+arc_id character varying(16) NOT NULL,
+old_matcat_id varchar(30)  ,
+new_matcat_id varchar(30)  ,
+old_pnom varchar(16)  ,
+new_pnom varchar(16)  ,
+old_dnom varchar(16)  ,
+new_dnom varchar(16)  ,
+old_arccat_id varchar (30),
+new_arccat_id varchar (30), 
+annotation text,
+observ text,
+expl_id integer,
+the_geom geometry(LINESTRING,SRID_VALUE),
+review_status_id smallint,
+field_date timestamp (6) without time zone,
+field_user text,
+is_validated boolean
 );
 
 
 CREATE TABLE review_node ( 
 node_id character varying(16) NOT NULL,
-the_geom geometry(POINT,SRID_VALUE),
 elevation numeric(12,3),
 depth numeric(12,3),
 from_plot numeric(12,3),
@@ -32,91 +53,65 @@ nodetype_id character varying(30),
 nodecat_id character varying(30),
 annotation character varying(254),
 observ character varying(254),
-verified text,
+expl_id integer,
+the_geom geometry(POINT,SRID_VALUE),
 field_checked boolean,
 CONSTRAINT review_node_pkey PRIMARY KEY (node_id)
 );
-
-
+  
+  
+CREATE TABLE review_audit_node(  
+id serial PRIMARY KEY,
+node_id character varying(16) NOT NULL,
+old_elevation numeric(12,3),
+new_elevation numeric(12,3),
+old_depth numeric(12,3),
+new_depth numeric(12,3),
+old_from_plot numeric(12,3),
+new_from_plot numeric(12,3),
+old_nodetype_id character varying(30),
+new_nodetype_id character varying(30),
+old_nodecat_id character varying(30),
+new_nodecat_id character varying(30),
+annotation character varying(254),
+observ character varying(254),
+expl_id integer,
+the_geom geometry(POINT,SRID_VALUE),
+review_status_id smallint,
+field_date timestamp (6) without time zone,
+field_user text,
+is_validated boolean
+);
+  
+  
 CREATE TABLE review_connec( 
 connec_id character varying(16) NOT NULL,
-the_geom geometry(POINT,SRID_VALUE),
 matcat_id varchar(30)  ,
 pnom varchar(16)  ,
 dnom varchar(16)  ,
 annotation character varying(254),
 observ character varying(254),
-verified text,
+expl_id integer,
+the_geom geometry(POINT,SRID_VALUE),
 field_checked boolean,
 CONSTRAINT review_connec_pkey PRIMARY KEY (connec_id)
 );
-  
-  
-  
-CREATE TABLE audit_review_arc(  
-arc_id character varying(16) NOT NULL,
-the_geom geometry(LINESTRING,SRID_VALUE),
-matcat_id varchar(30)  ,
-pnom varchar(16)  ,
-dnom varchar(16)  ,
-annotation character varying(254),
-observ character varying(254),
-field_checked boolean,
-field_op character varying(25),
-field_user varchar (50),  
-field_date timestamp (6) without time zone,
-field_verified character varying(16),
-field_updated_geom boolean,
-office_arccat_id character varying(30),
-office_updated_geom boolean,
-office_checked boolean,
-CONSTRAINT audit_review_arc_pkey PRIMARY KEY (arc_id)
-);
-  
-  
-CREATE TABLE audit_review_node(  
-node_id character varying(16) NOT NULL,
-the_geom geometry(POINT,SRID_VALUE),
-elevation numeric(12,3),
-depth numeric(12,3),
-from_plot numeric(12,3),
-nodetype_id character varying(30),
-nodecat_id character varying(30),
-annotation character varying(254),
-observ character varying(254),
-field_checked boolean,
-field_op character varying(25),
-field_user varchar (50),  
-field_date timestamp (6) without time zone,
-field_verified character varying(16),
-field_updated_geom boolean,
-office_elevation numeric(12,3),
-office_depth numeric(12,3),
-office_from_plot numeric(12,3),
-office_nodetype_id character varying(30),
-office_nodecat_id character varying(30),
-office_updated_geom boolean,
-office_checked boolean,
-CONSTRAINT audit_review_node_pkey PRIMARY KEY (node_id)
-);
-  
-  
-CREATE TABLE audit_review_connec (  
+
+CREATE TABLE review_audit_connec (  
+id serial PRIMARY KEY,
 connec_id character varying(16) NOT NULL,
-the_geom geometry(POINT,SRID_VALUE),
-matcat_id varchar(30)  ,
-pnom varchar(16)  ,
-dnom varchar(16)  ,
+old_matcat_id varchar(30)  ,
+new_matcat_id varchar(30)  ,
+old_pnom varchar(16)  ,
+new_pnom varchar(16)  ,
+old_dnom varchar(16)  ,
+new_dnom varchar(16)  ,
 annotation character varying(254),
 observ character varying(254),
-field_checked boolean,
-field_op character varying(25),
-field_user varchar (50),  
+expl_id integer,
+the_geom geometry(POINT,SRID_VALUE),
+review_status_id smallint,
 field_date timestamp (6) without time zone,
-field_verified character varying(16),
-field_updated_geom boolean,
-office_arccat_id character varying(30),
-office_updated_geom boolean,
-office_checked boolean,
-CONSTRAINT audit_review_connec_pkey PRIMARY KEY (connec_id)
+field_user text,
+is_validated boolean
 );
