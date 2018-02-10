af7ec27d7747e8ed6a1128d60204a47f1cf6b62e
Xavier Torret <xtorret@bgeo.es>
2018-02-10 21:21:00 +0100

    Bug fix on rpt2pg function

--- dbmodel/ws/fct/ws_gw_fct_rpt2pg.sql
+++ dbmodel/ws/fct/ws_gw_fct_rpt2pg.sql
@@ -22,14 +22,6 @@ BEGIN
 	RAISE NOTICE 'Starting rpt2pg process.';
 	
 	
-	-- Reset sequences of rpt_* tables
-	FOR rec_var IN SELECT id FROM audit_cat_table WHERE context='Hydraulic result data' AND sys_sequence IS NOT NULL
-	LOOP
-		EXECUTE 'SELECT max(id) INTO setvalue_int FROM '||rec_var.id||';';
-		EXECUTE 'SELECT setval(SCHEMA_NAME.'||rec_var.sys_sequence||', '||setvalue_int||', true);';
-	END LOOP;
-
-	
 	-- Reverse geometries where flow is negative and updating flow values with absolute value
 	UPDATE rpt_inp_arc SET the_geom=st_reverse(the_geom) FROM rpt_arc WHERE rpt_arc.arc_id=rpt_inp_arc.arc_id AND flow<0 AND rpt_inp_arc.result_id=result_id_var;
 	UPDATE rpt_arc SET flow=(-1)*flow WHERE flow<0 and result_id=result_id_var;
