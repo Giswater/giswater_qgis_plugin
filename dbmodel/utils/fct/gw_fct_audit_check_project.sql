874141a018a57b4b3297a92c7bc6ad68b76880d1
Xavier Torret <xtorret@bgeo.es>
2018-02-13 23:35:22 +0100

    Bug fix on audit_check_project table

--- dbmodel/utils/fct/gw_fct_audit_check_project.sql
+++ dbmodel/utils/fct/gw_fct_audit_check_project.sql
@@ -74,7 +74,7 @@ BEGIN
 	EXECUTE 'SELECT setval(''SCHEMA_NAME.urn_id_seq'','||max_aux||', true)';
 
 	-- rest of sequences	
-	FOR table_record IN SELECT * FROM audit_cat_table WHERE sys_sequence IS NOT NULL
+	FOR table_record IN SELECT * FROM audit_cat_table WHERE sys_sequence IS NOT NULL AND sys_sequence_field IS NOT NULL
 	LOOP 
 		query_string:= 'SELECT max('||table_record.sys_sequence_field||') FROM '||table_record.id||';' ;
 		EXECUTE query_string INTO max_aux;	
