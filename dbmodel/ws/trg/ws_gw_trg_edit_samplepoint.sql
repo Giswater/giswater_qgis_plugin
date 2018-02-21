2be5eee970afc2ee7d2b1ca398e97952c611467d
Barbara Rzepka <bbrzepka@bgeo.es>
2018-02-21 14:26:53 +0100

    Bug fix on samplepoint trg WS&UD, ud - varc view

--- dbmodel/ws/trg/ws_gw_trg_edit_samplepoint.sql
+++ dbmodel/ws/trg/ws_gw_trg_edit_samplepoint.sql
@@ -62,11 +62,11 @@ BEGIN
 -- FEATURE INSERT      
 		
 				INSERT INTO samplepoint (sample_id, code, lab_code, feature_id, featurecat_id, dma_id, presszonecat_id, "state", builtdate, enddate, 
-				workcat_id, workcat_id_end, rotation, muni_id, streetaxis_id, postnumber, streetaxis2_id, place_name, cabinet, 
+				workcat_id, workcat_id_end, rotation, muni_id, postcode, streetaxis_id, postnumber, postcomplement, streetaxis2_id, postnumber2, postcomplement2, place_name, cabinet, 
 				observations, the_geom, expl_id, verified)
 				VALUES (NEW.sample_id, NEW.code, NEW.lab_code, NEW.feature_id, NEW.featurecat_id,  NEW.dma_id, NEW.presszonecat_id, NEW."state", NEW.builtdate, 
-				NEW.enddate, NEW.workcat_id, NEW.workcat_id_end, NEW.rotation, NEW.muni_id, NEW.streetaxis_id, NEW.postnumber, NEW.streetaxis2_id,
-				NEW.place_name, NEW.cabinet, NEW.observations, NEW.the_geom, expl_id_int, NEW.verified);
+				NEW.enddate, NEW.workcat_id, NEW.workcat_id_end, NEW.rotation, NEW.muni_id, NEW.postcode, NEW.streetaxis_id, NEW.postnumber, NEW.postcomplement, NEW.streetaxis2_id,
+				NEW.postnumber2, NEW.postcomplement2,	NEW.place_name, NEW.cabinet, NEW.observations, NEW.the_geom, expl_id_int, NEW.verified);
 	
 		RETURN NEW;
 						
@@ -79,9 +79,9 @@ BEGIN
 			UPDATE samplepoint 
 			SET sample_id=NEW.sample_id,  code=NEW.code,lab_code=NEW.lab_code,  feature_id=NEW.feature_id, featurecat_id=NEW.featurecat_id, dma_id=NEW.dma_id, 
 			presszonecat_id=NEW.presszonecat_id,"state"=NEW."state", rotation=NEW.rotation, builtdate=NEW.builtdate, enddate=NEW.enddate,
-			workcat_id=NEW.workcat_id, workcat_id_end=NEW.workcat_id_end, muni_id=NEW.muni_id, streetaxis_id=NEW.streetaxis_id, 
-			postnumber=NEW.postnumber, streetaxis2_id=NEW.streetaxis2_id, place_name=NEW.place_name, cabinet=NEW.cabinet, observations=NEW.observations, 
-			the_geom=NEW.the_geom, expl_id=NEW.expl_id, verified=NEW.verified
+			workcat_id=NEW.workcat_id, workcat_id_end=NEW.workcat_id_end, muni_id=NEW.muni_id, postcode=NEW.postcode, streetaxis_id=NEW.streetaxis_id, 
+			postnumber=NEW.postnumber, postcomplement=NEW.postcomplement, streetaxis2_id=NEW.streetaxis2_id, postnumber2=NEW.postnumber2, postcomplement2=NEW.postcomplement2,
+			place_name=NEW.place_name, cabinet=NEW.cabinet, observations=NEW.observations, the_geom=NEW.the_geom, expl_id=NEW.expl_id, verified=NEW.verified
 			WHERE sample_id=NEW.sample_id;
 
         RETURN NEW;
