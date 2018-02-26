d11f0f8df22d615620480028b8de4e2a8030a7c9
David Erill <daviderill79@gmail.com>
2018-02-26 13:04:17 +0100

    Merge 'issue-351' into 'dev-3.0' (#387)
    * #351 Add toolbar 'info'
    
    * #374 Add .bat file to compile .ts files
    
    * #351 Update UI form
    
    * #351 Add function to get Postgis version
    
    * Close #351

--- actions/utils.py
+++ actions/utils.py
@@ -190,15 +190,6 @@ class Utils(ParentAction):
         self.refresh_map_canvas()
 
 
-    def utils_giswater_jar(self):
-        """ Button 36: Open giswater.jar with selected .gsw file """
-
-        if 'nt' in sys.builtin_module_names:
-            self.execute_giswater("ed_giswater_jar")
-        else:
-            self.controller.show_info("Function not supported in this Operating System")
-
-
     def utils_config(self):
         """ Button 99: Config utils """
 
@@ -542,12 +533,6 @@ class Utils(ParentAction):
         message = "Values has been updated"
         self.controller.show_info(message)
         self.close_dialog(self.dlg)
-
-
-    def utils_info(self):
-        """ Button 100: Utils info """
-
-        self.controller.log_info("utils_info")
 
 
     def utils_import_csv(self):
