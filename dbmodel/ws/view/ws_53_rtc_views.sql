42a0ffeac12730f58f4838b45976e2bb41192b7b
Xavier Torret <xtorret@bgeo.es>
2018-03-07 17:49:28 +0100

    Improve hydrometer search using expl_id on v_rtc_hydrometer table

--- dbmodel/ws/view/ws_53_rtc_views.sql
+++ dbmodel/ws/view/ws_53_rtc_views.sql
@@ -45,31 +45,33 @@ JOIN ext_rtc_scada_x_value ON ext_rtc_scada_x_value.scada_id = rtc_scada_node.sc
 	 
 
 DROP VIEW IF EXISTS v_rtc_hydrometer CASCADE;
-CREATE OR REPLACE VIEW v_rtc_hydrometer AS
-SELECT
-rtc_hydrometer.hydrometer_id,
-rtc_hydrometer_x_connec.connec_id,
-connec.code AS urban_propierties_code,
-ext_rtc_hydrometer.code,
-ext_rtc_hydrometer.hydrometer_category,
-ext_rtc_hydrometer.house_number,
-ext_rtc_hydrometer.id_number,
-ext_rtc_hydrometer.cat_hydrometer_id,
-ext_rtc_hydrometer.hydrometer_number,
-ext_rtc_hydrometer.identif,
-ext_cat_hydrometer.madeby,
-ext_cat_hydrometer.class,
-ext_cat_hydrometer.ulmc,
-ext_cat_hydrometer.voltman_flow,
-ext_cat_hydrometer.multi_jet_flow,
-ext_cat_hydrometer.dnom,
-concat((select value from config_param_system where parameter='hydrometer_link_absolute_path'),rtc_hydrometer.link) as hydrometer_link
+CREATE OR REPLACE VIEW v_rtc_hydrometer AS 
+SELECT 
+    rtc_hydrometer.hydrometer_id,
+    rtc_hydrometer_x_connec.connec_id,
+    connec.customer_code as connec_customer_code,
+    expl_id,
+    ext_rtc_hydrometer.code as hydrometer_customer_code,
+    hydrometer_category,
+    house_number,
+    id_number,
+    cat_hydrometer_id,
+    hydrometer_number,
+    identif,
+    madeby,
+    class,
+    ulmc,
+    voltman_flow,
+    multi_jet_flow,
+    dnom,
+    concat(( SELECT config_param_system.value
+           FROM config_param_system
+          WHERE config_param_system.parameter::text = 'hydrometer_link_absolute_path'::text), rtc_hydrometer.link) AS hydrometer_link
    FROM rtc_hydrometer
-     LEFT JOIN ext_rtc_hydrometer ON ext_rtc_hydrometer.hydrometer_id = rtc_hydrometer.hydrometer_id
-     LEFT JOIN ext_cat_hydrometer ON ext_cat_hydrometer.id = ext_rtc_hydrometer.cat_hydrometer_id
-     JOIN rtc_hydrometer_x_connec ON rtc_hydrometer_x_connec.hydrometer_id = rtc_hydrometer.hydrometer_id
-     JOIN connec ON rtc_hydrometer_x_connec.connec_id = connec.connec_id;
-
+     LEFT JOIN ext_rtc_hydrometer ON ext_rtc_hydrometer.hydrometer_id::text = rtc_hydrometer.hydrometer_id::text
+     LEFT JOIN ext_cat_hydrometer ON ext_cat_hydrometer.id::text = ext_rtc_hydrometer.cat_hydrometer_id
+     JOIN rtc_hydrometer_x_connec ON rtc_hydrometer_x_connec.hydrometer_id::text = rtc_hydrometer.hydrometer_id::text
+     JOIN connec ON rtc_hydrometer_x_connec.connec_id::text = connec.connec_id::text;
 
 
 CREATE OR REPLACE VIEW v_rtc_hydrometer_period AS 
