b7bd1963b536873bd8970e4664d51559f1ec1087
Xavier Torret <xtorret@bgeo.es>
2018-03-18 13:46:42 +0100

    Bug fix on v_ui_connec view

--- dbmodel/utils/view/gw_03_krn_views.sql
+++ dbmodel/utils/view/gw_03_krn_views.sql
@@ -40,7 +40,7 @@ dma.name dma_name,
 exploitation.name expl_name
 FROM v_edit_connec
 JOIN value_state ON v_edit_connec.state=value_state.id
-JOIN v_edit_connec ON v_node.state_type=value_state_type.id
+JOIN value_state_type ON v_edit_connec.state_type=value_state_type.id
 JOIN sector ON v_edit_connec.sector_id=sector.sector_id
 JOIN dma ON v_edit_connec.dma_id=dma.dma_id
 JOIN exploitation ON v_edit_connec.expl_id=exploitation.expl_id;
