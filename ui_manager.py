473b0f7942a3e8af7b55cb2ff43482f32d07055e
David Erill <daviderill79@gmail.com>
2018-03-20 16:50:07 +0100

    #457 Add module 'ui_manager'

---
+++ ui_manager.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from PyQt4 import QtGui, uic
+import os
+
+# Folder that contains UI files
+# In this file we will add all classes currently located in that folder
+ui_path = os.path.dirname(__file__) + os.sep + 'ui'
+        
+                        
