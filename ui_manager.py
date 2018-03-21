c85c81445d07cf675a7f901b403b0008adb71b6b
David <daviderill79@gmail.com>
2018-03-21 10:48:53 +0100

    #457 Add form classes to ui_manager

--- ui_manager.py
+++ ui_manager.py
@@ -5,5 +5,25 @@ import os
 # Folder that contains UI files
 # In this file we will add all classes currently located in that folder
 ui_path = os.path.dirname(__file__) + os.sep + 'ui'
-        
-                        
+
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(ui_path, 'add_doc.ui'))
+class AddDoc(QtGui.QDialog, FORM_CLASS):
+    def __init__(self):
+        QtGui.QDialog.__init__(self)
+        self.setupUi(self)
+
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(ui_path, 'add_element.ui'))
+class AddElement(QtGui.QDialog, FORM_CLASS):
+    def __init__(self):
+        QtGui.QDialog.__init__(self)
+        self.setupUi(self)
+
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(ui_path, 'add_visit.ui'))
+class AddVisit(QtGui.QDialog, FORM_CLASS):
+    def __init__(self):
+        QtGui.QDialog.__init__(self)
+        self.setupUi(self)
+
