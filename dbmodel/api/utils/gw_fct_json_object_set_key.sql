fcbc4d60ba60f3514be1b0ef1ccb36c0eac3c338
Xavier Torret <xtorret@bgeo.es>
2018-03-22 20:44:21 +0100

    Bug fix on automatic export functions

--- dbmodel/api/utils/gw_fct_json_object_set_key.sql
+++ dbmodel/api/utils/gw_fct_json_object_set_key.sql
@@ -1,10 +1,13 @@
-﻿CREATE OR REPLACE FUNCTION "SCHEMA_NAME"."gw_fct_json_object_set_key"(json json, key_to_set text, value_to_set anyelement) RETURNS pg_catalog.json AS $BODY$
-SELECT concat('{', string_agg(to_json("key") || ':' || "value", ','), '}')::json
-  FROM (SELECT *
+﻿
+CREATE OR REPLACE FUNCTION "SCHEMA_NAME"."gw_fct_json_object_set_key"(json json, key_to_set text, value_to_set anyelement) RETURNS pg_catalog.json AS $BODY$
+BEGIN
+	SELECT concat('{', string_agg(to_json("key") || ':' || "value", ','), '}')::json
+	FROM (SELECT *
           FROM json_each("json")
          WHERE "key" <> "key_to_set"
          UNION ALL
-        SELECT "key_to_set", to_json("value_to_set")) AS "fields"
+        SELECT "key_to_set", to_json("value_to_set")) AS "fields";
+END;
 $BODY$
 LANGUAGE 'plpgsql' VOLATILE COST 100;
 
