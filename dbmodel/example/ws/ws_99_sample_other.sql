af653134c23a6218049828e6d1c4a2d89428c60a
Albert Bofill <abofill@bgeo.es>
2018-05-14 17:05:22 +0200

    Bug fix on example values

--- dbmodel/example/ws/ws_99_sample_other.sql
+++ dbmodel/example/ws/ws_99_sample_other.sql
@@ -55,7 +55,7 @@ update rtc_hydrometer set link='https://www.giswater.org';
 refresh MATERIALIZED VIEW v_ui_workcat_polygon_aux;
 
 
-update ext_rtc_hydrometer SET state=1
+update ext_rtc_hydrometer SET state=1;
 
-INSERT INTO selector_hydrometer (state_id, cur_user) VALUES (1, 'postgres')
+INSERT INTO selector_hydrometer (state_id, cur_user) VALUES (1, 'postgres');
 
