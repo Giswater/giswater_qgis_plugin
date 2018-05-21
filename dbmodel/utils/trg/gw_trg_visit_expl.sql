38ae9fab95bd9a69f8296e33d296528f95ae3c23
Albert Bofill <abofill@bgeo.es>
2018-05-21 15:18:54 +0200

    Update trigger visit expl

--- dbmodel/utils/trg/gw_trg_visit_expl.sql
+++ dbmodel/utils/trg/gw_trg_visit_expl.sql
@@ -32,9 +32,5 @@ $BODY$
   COST 100;
 
 
-
-CREATE TRIGGER gw_trg_visit_expl
-  BEFORE INSERT OR UPDATE OF the_geom
-  ON SCHEMA_NAME.om_visit
-  FOR EACH ROW
-  EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_visit_expl();
+DROP TRIGGER IF EXISTS gw_trg_visit_expl ON SCHEMA_NAME.om_visit;
+CREATE TRIGGER gw_trg_visit_expl BEFORE INSERT OR UPDATE OF the_geom ON SCHEMA_NAME.om_visit FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_visit_expl();
