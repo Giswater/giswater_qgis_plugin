9e2da7be7faae2d8bca7b48076eee33d31a354a5
DESKTOP-5PSLC89\Nestor <nestor@bgeo.es>
2018-06-02 18:52:13 +0200

    wip

--- actions/basic.py
+++ actions/basic.py
@@ -43,9 +43,9 @@ class Basic(ParentAction):
         self.dlg_expoitation.btn_ok.clicked.connect(partial(self.close_dialog, self.dlg_expoitation))
         self.dlg_expoitation.rejected.connect(partial(self.close_dialog, self.dlg_expoitation))
         self.dlg_expoitation.setWindowTitle("Explotation selector")
-        utils_giswater.setWidgetText(self.dlg_expoitation.lbl_filter, self.controller.tr('Filter by: Exploitation name', context_name='labels'))
-        utils_giswater.setWidgetText(self.dlg_expoitation.lbl_unselected, self.controller.tr('Unselected exploitations', context_name='labels'))
-        utils_giswater.setWidgetText(self.dlg_expoitation.lbl_selected, self.controller.tr('Selected exploitations', context_name='labels'))
+        utils_giswater.setWidgetText(self.dlg_expoitation, self.dlg_expoitation.lbl_filter, self.controller.tr('Filter by: Exploitation name', context_name='labels'))
+        utils_giswater.setWidgetText(self.dlg_expoitation, self.dlg_expoitation.lbl_unselected, self.controller.tr('Unselected exploitations', context_name='labels'))
+        utils_giswater.setWidgetText(self.dlg_expoitation, self.dlg_expoitation.lbl_selected, self.controller.tr('Selected exploitations', context_name='labels'))
 
         tableleft = "exploitation"
         tableright = "selector_expl"
@@ -68,8 +68,8 @@ class Basic(ParentAction):
         self.dlg_state.rejected.connect(partial(self.close_dialog, self.dlg_state))
         self.dlg_state.txt_name.setVisible(False)
         self.dlg_state.setWindowTitle("State selector")
-        utils_giswater.setWidgetText(self.dlg_state.lbl_unselected, self.controller.tr('Unselected states', context_name='labels'))
-        utils_giswater.setWidgetText(self.dlg_state.lbl_selected, self.controller.tr('Selected states', context_name='labels'))
+        utils_giswater.setWidgetText(self.dlg_state, self.dlg_state.lbl_unselected, self.controller.tr('Unselected states', context_name='labels'))
+        utils_giswater.setWidgetText(self.dlg_state, self.dlg_state.lbl_selected, self.controller.tr('Selected states', context_name='labels'))
         tableleft = "value_state"
         tableright = "selector_state"
         field_id_left = "id"
@@ -90,8 +90,8 @@ class Basic(ParentAction):
         self.dlg_hydro_state.rejected.connect(partial(self.close_dialog, self.dlg_hydro_state))
         self.dlg_hydro_state.txt_name.setVisible(False)
         self.dlg_hydro_state.setWindowTitle("Hydrometer selector")
-        utils_giswater.setWidgetText(self.dlg_hydro_state.lbl_unselected, self.controller.tr('Unselected hydrometers', context_name='labels'))
-        utils_giswater.setWidgetText(self.dlg_hydro_state.lbl_selected, self.controller.tr('Selected hydrometers', context_name='labels'))
+        utils_giswater.setWidgetText(self.dlg_hydro_state, self.dlg_hydro_state.lbl_unselected, self.controller.tr('Unselected hydrometers', context_name='labels'))
+        utils_giswater.setWidgetText(self.dlg_hydro_state, self.dlg_hydro_state.lbl_selected, self.controller.tr('Selected hydrometers', context_name='labels'))
         tableleft = "ext_rtc_hydrometer_state"
         tableright = "selector_hydrometer"
         field_id_left = "id"
