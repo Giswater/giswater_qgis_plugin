e32776e692d7f58f82afe91484a2bce2ab2c24a0
Sergi Muñoz <smunoz@bgeo.es>
2018-06-18 20:04:25 +0200

    Bug fix on sql file

--- dbmodel/ud/trg/ud_gw_trg_edit_man_node.sql
+++ dbmodel/ud/trg/ud_gw_trg_edit_man_node.sql
@@ -9,6 +9,26 @@ This version of Giswater is provided by Giswater Association
 
 -- DROP FUNCTION "SCHEMA_NAME".gw_trg_edit_man_node();
 
+CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_edit_man_node()
+  RETURNS trigger AS
+$BODY$
+DECLARE 
+    inp_table varchar;
+    man_table varchar;
+    new_man_table varchar;
+    old_man_table varchar;
+    v_sql varchar;
+    v_sql2 varchar;
+    old_nodetype varchar;
+    new_nodetype varchar;
+    man_table_2 varchar;
+	rec Record;
+    node_id_seq int8;
+	code_autofill_bool boolean;
+	count_aux integer;
+	promixity_buffer_aux double precision;
+
+
 BEGIN
 
     EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
