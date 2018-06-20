9f60a199e6d70f5df56eb46868ff8846e7a0dc52
Albert Bofill <abofill@bgeo.es>
2018-06-20 17:08:23 +0200

    Update audit_cat_error

--- dbmodel/i18n/EN/utils/gw_74_audit_vdomain_error.sql
+++ dbmodel/i18n/EN/utils/gw_74_audit_vdomain_error.sql
@@ -45,7 +45,7 @@ INSERT INTO audit_cat_error VALUES (2002, 'Node not found', 'Please check table 
 INSERT INTO audit_cat_error VALUES (2004, 'It is impossible to delete the node','Pipes have different types', 2, true, 'ws');
 INSERT INTO audit_cat_error VALUES (2006, 'It is impossible to delete the node', 'Node doesn''t have 2 arcs', 2, true, 'ws');
 INSERT INTO audit_cat_error VALUES (2008, 'Arc not found', 'Please check table arc', 2, true, 'ws_fct');
-INSERT INTO audit_cat_error VALUES (1080, 'You are not allowed to manage the features with state=2. Review your profile parameters', 'Only users with masterplan or admin role can manage planified state', 2, true, NULL);
+INSERT INTO audit_cat_error VALUES (1080, 'You need to have at least one psector created to add planified elements', 'Create a new psector', 2, true, NULL);
 INSERT INTO audit_cat_error VALUES (1088, 'Connec catalog is different than connec type', 'Use a connec type defined in connec catalogs', 2, true, NULL);
 INSERT INTO audit_cat_error VALUES (1090, 'You must choose a node catalog value for this feature', 'Nodecat_id is required. Fill the table cat_node or use a default value', 2, true, NULL);
 INSERT INTO audit_cat_error VALUES (1094, 'Your catalog is different than node type', 'You must use a node type defined in node catalogs', 2, true, NULL);
