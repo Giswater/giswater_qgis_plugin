c6fa00aa70fee7132699fe55bb40cdb60e31411d
Barbara Rzepka <bbrzepka@bgeo.es>
2018-06-26 13:31:58 +0200

    ws&ud - update cat&es vdomain

--- dbmodel/i18n/CA/ws/ws_04_cuore_vdomain.sql
+++ dbmodel/i18n/CA/ws/ws_04_cuore_vdomain.sql
@@ -15,6 +15,14 @@ INSERT INTO value_state VALUES (0, 'OBSOLET');
 INSERT INTO value_state VALUES (1, 'EN_SERVEI');
 INSERT INTO value_state VALUES (2, 'PLANIFICAT');
 
+-- ----------------------------
+-- Records of value_state_type
+-- ----------------------------
+INSERT INTO value_state_type VALUES (1, 0, 'OBSOLET', false, false);
+INSERT INTO value_state_type VALUES (2, 1, 'EN_SERVEI', true, true);
+INSERT INTO value_state_type VALUES (3, 2, 'PLANIFICAT', true, true);
+INSERT INTO value_state_type VALUES (4, 2, 'RECONSTRUIR', true, false);
+INSERT INTO value_state_type VALUES (5, 1, 'PROVISIONAL', false, true);
 
 -- ----------------------------
 -- Records of value_verified
