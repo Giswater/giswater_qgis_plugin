76e83ceec98bcd7c116225eef0c54484d0058303
Xavier Torret <xtorret@bgeo.es>
2018-06-28 17:06:31 +0200

    Update reh tables

--- dbmodel/updates/ud/31100.sql
+++ dbmodel/updates/ud/31100.sql
@@ -19,21 +19,54 @@ ALTER TABLE cat_grate ADD COLUMN label varchar(255);
 -------------
 --28/06/2018
 -------------
-DROP TABLE om_reh_parameter_x_works;
-CREATE TABLE om_reh_parameter_x_works
-(
+DROP TABLE IF EXISTS om_reh_cat_location;
+CREATE TABLE om_reh_cat_location(
   id serial NOT NULL,
-  parameter_id character varying(50),
-  init_condition integer,
-  end_condition integer,
-  arcclass_id integer,
-  loc_condition_id character varying(30),
-  catwork_id character varying(30),
-  CONSTRAINT om_reh_parameter_x_works_pkey PRIMARY KEY (id)
-)
-WITH (
-  OIDS=FALSE
+  from_value integer,
+  to_value integer,
+  location_id text,
+  percent double precision,
+  CONSTRAINT om_reh_cat_location_pkey PRIMARY KEY (id)
 );
 
 
+DROP TABLE om_reh_result_arc cascade;
+CREATE TABLE om_reh_result_arc(
+  id serial NOT NULL,
+  result_id integer NOT NULL,
+  arc_id character varying(16) NOT NULL,
+  node_1 character varying(16),
+  node_2 character varying(16),
+  arc_type character varying(18) NOT NULL,
+  arccat_id character varying(30) NOT NULL,
+  sector_id integer NOT NULL,
+  state smallint NOT NULL,
+  expl_id integer,
+  parameter_id character varying(30),
+  work_id character varying(30),
+  pcompost_id character varying(16),
+  geom1 double precision,
+  geom2 double precision,
+  geom3 double precision,
+  position_value double precision,
+  value1 double precision,
+  value2 double precision,
+  measurement double precision,
+  pcompost_price double precision,
+  total_cost double precision,
+  the_geom geometry(LineString,25831),
+  CONSTRAINT om_reh_result_arc_pkey PRIMARY KEY (id),
+  CONSTRAINT om_reh_result_arc_result_id_fkey FOREIGN KEY (result_id)
+      REFERENCES vpat.om_result_cat (result_id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+);
 
+DROP TABLE om_reh_parameter_x_works;
+CREATE TABLE om_reh_parameter_x_works(
+  id serial NOT NULL,
+  parameter_id character varying(50),
+  arcclass_id integer,
+  location_id character varying(30),
+  work_id character varying(30),
+  CONSTRAINT om_reh_parameter_x_works_pkey PRIMARY KEY (id)
+);
