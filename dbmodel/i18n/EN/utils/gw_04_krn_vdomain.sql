7cc493b673e455779c1e38ac3ad7ecea3c8117e3
Albert Bofill <abofill@bgeo.es>
2018-07-06 10:15:38 +0200

    Fill sys_csv2pg_cat in english

--- dbmodel/i18n/EN/utils/gw_04_krn_vdomain.sql
+++ dbmodel/i18n/EN/utils/gw_04_krn_vdomain.sql
@@ -7,6 +7,19 @@ This version of Giswater is provided by Giswater Association
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
 
-INSERT INTO sys_csv2pg_cat VALUES (2, 'Import node visit file', 'Import node visit file', 'The csv file must contains next columns on same position: [node_id], [unit]', 'role_om');
-INSERT INTO sys_csv2pg_cat VALUES (3, 'Import arc visit file', 'Import arc visit file', 'The csv file must contains next columns on same position: [arc_id], [unit]. The column [...] must be numeric with two decimals', 'role_om');
-INSERT INTO sys_csv2pg_cat VALUES (1, 'Import db prices', 'Import db prices', 'The csv file must contains next columns on same position: [id], [unit], [descript], [text], [price]. The column [price] must be numeric with two decimals', 'role_master');
+INSERT INTO sys_csv2pg_cat VALUES (1, 'Import db prices', 'Import db prices', 
+'The csv file must contains next columns on same position: id, unit, descript, text, price. 
+- The column price must be numeric with two decimals. 
+- You can choose a catalog name for these prices setting an import label. 
+- Be careful, csv file needs a header line', 'role_master');
+INSERT INTO sys_csv2pg_cat VALUES (2, 'Import om visit table', 'Import om visit table', 'The csv file must contains next columns on same position: node_id, unit', 'role_om');
+INSERT INTO sys_csv2pg_cat VALUES (3, 'Import elements', 'Import elements', 
+'The csv file must containts next columns on same position: feature_id, elementcat_id, observ, comment, num_elements. 
+- You have to fill Import label with the element type you need to import (node, arc, connec, gully).
+- Observ and Comment fields are opcional.
+- Be careful, csv file needs a header line', 'role_admin');
+INSERT INTO sys_csv2pg_cat VALUES (4, 'Import addfields', 'Import addfields', 'The csv file must containts next columns on same position: 
+feature_id (can be arc, node or connec), parameter_id (choose from man_addfields_parameter), value_param. 
+- Import label is mandatory, but you can put whatever you what in this option. 
+- Be careful, csv file needs a header line', 'role_admin');
+
