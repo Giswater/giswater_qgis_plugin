351a8e40a0364d8d5e38e88ea7296583eac76891
Xavier Torret <xtorret@bgeo.es>
2018-07-14 19:13:20 +0200

    New function of daily updates

---
+++ dbmodel/extra/admin_tools/maintenance/gw_fct_util_daily_update.sql
@@ -0,0 +1,33 @@
+﻿/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+--FUNCTION CODE: XXXX
+
+DROP FUNCTION IF EXISTS SCHEMA_NAME.gw_fct_util_daily_update();
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_util_daily_update ();
+
+RETURNS void AS
+$BODY$
+
+DECLARE 
+
+BEGIN 
+
+	
+	-- Daily updates
+	SELECT crm.gw_fct_crm2gis();
+	SELECT ud.gw_fct_refresh_mat_view();
+	SELECT ws.gw_fct_refresh_mat_view();
+		
+
+		
+RETURN ;
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
