8a8b05d8e45341ece61d4c3874989c33acf74ed0
Barbara Rzepka <bbrzepka@bgeo.es>
2018-10-02 14:48:36 +0200

    Update anl_node_orphan, create reverse_link fct

--- dbmodel/utils/fct/gw_fct_anl_node_orphan.sql
+++ dbmodel/utils/fct/gw_fct_anl_node_orphan.sql
@@ -9,8 +9,10 @@ This version of Giswater is provided by Giswater Association
 
 CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_fct_anl_node_orphan() RETURNS void AS $$
 DECLARE
-    rec_node record;
 
+    rec_node record;
+    v_closest_arc_id varchar;
+    v_closest_arc_distance numeric;
 BEGIN
 
     -- Search path
@@ -18,16 +20,19 @@ BEGIN
 
     -- Reset values
     DELETE FROM anl_node WHERE cur_user="current_user"() AND fprocesscat_id=7;
-		
+        
     -- Computing process
     FOR rec_node IN SELECT DISTINCT * FROM node AS a WHERE (SELECT COUNT(*) FROM arc WHERE node_1 = a.node_id OR node_2 = a.node_id) = 0
     LOOP
-        INSERT INTO anl_node (node_id, state, expl_id, fprocesscat_id, the_geom, nodecat_id) 
-		VALUES (rec_node.node_id, rec_node.state, rec_node.expl_id, 7, rec_node.the_geom, rec_node.nodecat_id);
+        --find the closest arc and the distance between arc and node
+        SELECT ST_Distance(arc.the_geom, rec_node.the_geom) as d, arc.arc_id INTO v_closest_arc_distance, v_closest_arc_id FROM arc ORDER BY arc.the_geom <-> rec_node.the_geom  LIMIT 1;
+    
+        INSERT INTO anl_node (node_id, state, expl_id, fprocesscat_id, the_geom, nodecat_id,arc_id,arc_distance) 
+        VALUES (rec_node.node_id, rec_node.state, rec_node.expl_id, 7, rec_node.the_geom, rec_node.nodecat_id,v_closest_arc_id,v_closest_arc_distance);
     END LOOP;
 
     DELETE FROM selector_audit WHERE fprocesscat_id=7 AND cur_user=current_user;    
-	INSERT INTO selector_audit (fprocesscat_id,cur_user) VALUES (7, current_user);
+    INSERT INTO selector_audit (fprocesscat_id,cur_user) VALUES (7, current_user);
 
     RETURN;
         
