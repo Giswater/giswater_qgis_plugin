5288d8a2d4f1402fe8ec7e134968d18bb19de37b
Xavier Torret <xtorret@bgeo.es>
2018-10-05 21:06:22 +0200

    Delete extra folder

--- dbmodel/extra/admin_tools/old/other/gw_select_compare_wrong_rows.sql
+++
@@ -1,6 +0,0 @@
-﻿SELECT row_number() OVER (ORDER BY id) AS rid,
-    id,
-    v_audit_schema_foreign_table.table_name
-   FROM SCHEMA_NAME.audit_cat_table
-     LEFT JOIN SCHEMA_NAME.v_audit_schema_foreign_table ON v_audit_schema_foreign_table.table_name::text = id
-  WHERE v_audit_schema_foreign_table.table_name IS NULL
--- dbmodel/extra/custom_tools/es_bcn_sbd_cc/dml/deletes import_csv.sql
+++
@@ -1,4 +0,0 @@
-﻿delete from sanejament.ext_om_visit_lot;
-delete from sanejament.om_visit where the_geom is null;
-delete from sanejament.om_visit_review_node;
-delete from sanejament.om_visit_review_arc;
