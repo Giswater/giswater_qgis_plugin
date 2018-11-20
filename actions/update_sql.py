3e6c169ed75a4229f380422fc47cba0929e21852
Edgar Fuste <efuste@bgeo.es>
2018-11-20 13:03:02 +0100

    wip read project

--- actions/update_sql.py
+++ actions/update_sql.py
@@ -14,7 +14,7 @@ from sqlite3 import OperationalError
 import utils_giswater
 from giswater.actions.parent import ParentAction
 from giswater.ui_manager import InfoShowInfo
-from PyQt4.QtGui import QCheckBox, QRadioButton, QAction, QWidget, QComboBox
+from PyQt4.QtGui import QCheckBox, QRadioButton, QAction, QWidget, QComboBox, QLineEdit,QPushButton
 from PyQt4.QtCore import QSettings
 import psycopg2
 
@@ -38,7 +38,10 @@ class UpdateSQL(ParentAction):
         self.load_settings(self.dlg_info_show_info)
         self.dlg_info_show_info.btn_close.clicked.connect(partial(self.close_dialog, self.dlg_info_show_info))
 
-        # Get widgets from toolbox
+        # Get widgets from form
+
+        self.chk_schema_ddl_dml = self.dlg_info_show_info.findChild(QRadioButton, 'rdb_ud')
+        self.chk_schema_view = self.dlg_info_show_info.findChild(QRadioButton, 'rdb_ws')
 
         # Checkbox SCHEMA & API
         self.chk_schema_ddl_dml = self.dlg_info_show_info.findChild(QCheckBox, 'chk_schema_ddl_dml')
@@ -54,9 +57,14 @@ class UpdateSQL(ParentAction):
         self.chk_api_funcion = self.dlg_info_show_info.findChild(QCheckBox, 'chk_api_funcion')
         self.chk_api_trigger = self.dlg_info_show_info.findChild(QCheckBox, 'chk_api_trigger')
 
-        # Actions to Menu
-        action_create_sample = self.dlg_info_show_info.menuSample.findChildren(QAction, 'action_create_sample')
-        # action_create_sample_dev = self.dlg_info_show_info.findChild(QAction, 'action_create_sample_dev')
+        self.cmb_locale = 'EN'
+        self.filter_srid_value = self.controller.plugin_settings_value('srid')
+        print(self.filter_srid_value)
+        #TODO: Populate combo project type with all projects types (Dinamic)
+        cmb_project_type = self.dlg_info_show_info.findChild(QComboBox, 'cmb_project_type')
+        cmb_project_type.addItem('ws')
+        cmb_project_type.addItem('ud')
+        cmb_project_type.addItem('tree manage')
 
         # # RadioButton FK & Not null
         # self.rdb_enable_fk = self.dlg_info_show_info.findChild(QRadioButton, 'rdb_enable_fk')
@@ -86,20 +94,13 @@ class UpdateSQL(ParentAction):
         # Declare sql directory
         self.sql_dir = os.path.normpath(os.path.normpath(os.path.dirname(os.path.abspath(__file__)) + os.sep + os.pardir)) + '\sql'
 
-        # Populate combo with all locales
-        self.cmb_locale = self.dlg_info_show_info.findChild(QComboBox, 'locale')
-        locales = os.listdir(self.sql_dir + '\i18n/')
-        for locale in locales:
-            self.cmb_locale.addItem(locale)
-
         # Declare all directorys
         self.folderSoftware = self.sql_dir + '/' + self.project_type + '/'
-        self.folderLocale = self.sql_dir + '\i18n/' + utils_giswater.getWidgetText(self.dlg_info_show_info, self.cmb_locale) + '/'
+        self.folderLocale = self.sql_dir + '\i18n/' + self.cmb_locale + '/'
         self.folderUtils = self.sql_dir + '\utils/'
         self.folderUpdates = self.sql_dir + '\updates/'
         self.folderExemple = self.sql_dir, '\example/'
         self.folderPath = ''
-
 
         # Set Listeners
         self.dlg_info_show_info.btn_schema_create.clicked.connect(partial(self.open_create_project))
@@ -109,9 +110,9 @@ class UpdateSQL(ParentAction):
         self.dlg_info_show_info.btn_api_custom_load_file.clicked.connect(partial(self.load_custom_sql_files))
         self.dlg_info_show_info.btn_schema_file_to_db.clicked.connect(partial(self.schema_file_to_db))
         self.dlg_info_show_info.btn_api_file_to_db_2.clicked.connect(partial(self.api_file_to_db))
-        # action_create_sample[1].triggered.connect(self.open_create_project)
 
-        self.cmb_locale.currentIndexChanged.connect(partial(self.update_locale))
+        cmb_project_type.currentIndexChanged.connect(partial(self.populate_data_shcema_name, cmb_project_type))
+
 
         # self.chk_schema_update.stateChanged.connect(partial(self.check_primary_to_foreing, self.chk_schema_update, self.chk_schema_reload_update_funcion, self.chk_schema_reload_update_trigger))
         # self.chk_api_update.stateChanged.connect(partial(self.check_primary_to_foreing, self.chk_api_update, self.chk_api_reload_update_funcion, self.chk_api_reload_update_trigger))
@@ -120,14 +121,13 @@ class UpdateSQL(ParentAction):
         # self.chk_api_reload_update_funcion.stateChanged.connect(partial(self.check_foreing_to_primary, self.chk_api_reload_update_funcion,self.chk_api_update))
         # self.chk_api_reload_update_trigger.stateChanged.connect(partial(self.check_foreing_to_primary, self.chk_api_reload_update_trigger,self.chk_api_update))
 
-        if self.check_relaod_views() is True:
-            self.chk_schema_view.setEnabled(False)
-            self.chk_api_view.setEnabled(False)
-        if self.check_version_schema() is False:
-            self.chk_schema_ddl_dml.setEnabled(False)
-            self.chk_api_ddl_dml.setEnabled(False)
-
-        self.controller.log_info(str(self.schema_name))
+        # TODO:: Descomentar esto
+        # if self.check_relaod_views() is True:
+        #     self.chk_schema_view.setEnabled(False)
+        #     self.chk_api_view.setEnabled(False)
+        # if self.check_version_schema() is False:
+        #     self.chk_schema_ddl_dml.setEnabled(False)
+        #     self.chk_api_ddl_dml.setEnabled(False)
 
         # Open dialog
         self.dlg_info_show_info.show()
@@ -311,7 +311,6 @@ class UpdateSQL(ParentAction):
                             if self.process_folder(self.folderLocale, 'EN') is False:
                                 return False
                             else:
-                                print("HOLA EDGAR")
                                 status = self.executeFiles(os.listdir(
                                     self.folderLocale + utils_giswater.getWidgetText(self.dlg_info_show_info,
                                                                                      self.cmb_locale)),
@@ -495,26 +494,48 @@ class UpdateSQL(ParentAction):
 
     # BUTTONS CALLING FUNCTIONS
 
-    def create_project_data_schema(self, chk_import_data, chk_dissable_ct):
-        if chk_dissable_ct.isChecked():
-            if chk_import_data.isChecked():
-                print(str("IMPORT EPA NO CT"))
-                self.import_epa_file()
-            else:
-                print(str("NO CT NO EPA"))
-                self.load_base_no_ct()
-                self.load_update_ddl_dml()
-                # self.execute_last_process()
-        else:
-            if chk_import_data.isChecked():
-                print(str("IMPORT EPA CT"))
-                self.import_epa_file()
-            else:
-                print(str("CT NO EPA"))
-                self.load_base()
-                self.load_update_ddl_dml()
-                self.load_views()
-                # self.execute_last_process()
+    def create_project_data_schema(self):
+        if self.rdb_import_data.isChecked():
+            print("rdb_import_data")
+            self.load_base_no_ct()
+            self.execute_import_data()
+            self.load_fk()
+            self.load_rules()
+            self.load_trg()
+            self.load_dev_data()
+            # self.execute_last_process()
+            return
+        elif self.rdb_no_ct.isChecked():
+            print(str("rdb_no_ct"))
+            self.load_base_no_ct()
+            self.load_update_ddl_dml()
+            self.load_views()
+            # self.execute_last_process()
+            return
+        elif self.rdb_sample.isChecked():
+            print(str("rdb_sample"))
+            self.load_base()
+            self.update_ddl_dml()
+            self.load_views()
+            self.load_sample_data()
+            # self.execute_last_process()
+            return
+        elif self.rdb_sample_dev.isChecked():
+            print(str("rdb_sample_dev"))
+            self.load_base()
+            self.load_update_ddl_dml()
+            self.load_views()
+            self.load_sample_data()
+            self.load_dev_data()
+            # self.execute_last_process()
+            return
+        elif self.rdb_data.isChecked():
+            print(str("rdb_data"))
+            self.load_base()
+            self.load_update_ddl_dml()
+            self.load_views()
+            # self.execute_last_process()
+            return
 
     def create_sample(self):
         self.load_base()
@@ -588,8 +609,27 @@ class UpdateSQL(ParentAction):
     # OTHER FUNCTIONS
 
     def update_locale(self):
-        print(utils_giswater.getWidgetText(self.dlg_info_show_info, self.cmb_locale))
+        print(utils_giswater.getWidgetText(self.dlg_readsql_create_project, self.cmb_locale))
         self.folderLocale = self.sql_dir + '\i18n/' + utils_giswater.getWidgetText(self.dlg_info_show_info, self.cmb_locale) + '/'
+
+    def enable_datafile(self):
+        if self.rdb_import_data.isChecked() is True:
+            self.data_file.setEnabled(True)
+            self.btn_push_file.setEnabled(True)
+        else:
+            self.data_file.setEnabled(False)
+            self.btn_push_file.setEnabled(False)
+
+    def populate_data_shcema_name(self, widget):
+        # Get filter
+        filter = str(utils_giswater.getWidgetText(self.dlg_info_show_info, widget))
+        # Populate Project data schema Name
+        sql = ("SELECT schema_name, schema_name FROM information_schema.schemata WHERE schema_name LIKE '%"+filter+"%'")
+        rows = self.controller.get_rows(sql)
+        utils_giswater.set_item_data(self.dlg_info_show_info.project_schema_name, rows, 1)
+
+    def filter_srid_changed(self):
+        self.filter_srid_value = utils_giswater.getWidgetText(self.dlg_readsql_create_project, self.filter_srid)
 
     def process_folder(self, folderPath, filePattern):
         status = True
@@ -671,24 +711,74 @@ class UpdateSQL(ParentAction):
         self.dlg_readsql_create_project = ReadsqlCreateProject()
         self.load_settings(self.dlg_readsql_create_project)
 
-        chk_import_data = self.dlg_readsql_create_project.findChild(QCheckBox, 'chk_import_data')
-        chk_disable_ct = self.dlg_readsql_create_project.findChild(QCheckBox, 'chk_disable_ct')
+        permisions = False
+
+        # Find Widgets in form
+        self.project_name = self.dlg_readsql_create_project.findChild(QLineEdit, 'project_name')
+        self.project_title = self.dlg_readsql_create_project.findChild(QLineEdit, 'project_title')
+        self.project_author = self.dlg_readsql_create_project.findChild(QLineEdit, 'author')
+        self.project_date = self.dlg_readsql_create_project.findChild(QLineEdit, 'date')
+
+        self.rdb_no_ct = self.dlg_readsql_create_project.findChild(QRadioButton, 'rdb_no_ct')
+        self.rdb_sample = self.dlg_readsql_create_project.findChild(QRadioButton, 'rdb_sample')
+        self.rdb_sample_dev = self.dlg_readsql_create_project.findChild(QRadioButton, 'rdb_sample_dev')
+        self.rdb_data = self.dlg_readsql_create_project.findChild(QRadioButton, 'rdb_data')
+
+        self.data_file = self.dlg_readsql_create_project.findChild(QLineEdit, 'data_file')
+        self.btn_push_file  = self.dlg_readsql_create_project.findChild(QPushButton, 'btn_push_file')
+
+
+        if permisions is False:
+            self.rdb_no_ct.setEnabled(False)
+            self.rdb_sample_dev.setEnabled(False)
+
+        self.filter_srid = self.dlg_readsql_create_project.findChild(QLineEdit, 'filter')
+
+        self.rdb_import_data = self.dlg_readsql_create_project.findChild(QRadioButton, 'rdb_import_data')
+
+        # TODO::
+        cmb_project_type = self.dlg_readsql_create_project.findChild(QComboBox, 'cmb_project_type')
+        # Populate combo with all locales
+        self.cmb_locale = self.dlg_readsql_create_project.findChild(QComboBox, 'cmb_locale')
+        locales = os.listdir(self.sql_dir + '\i18n/')
+        for locale in locales:
+            self.cmb_locale.addItem(locale)
+
+        # enable_disable data file widgets
+        self.enable_datafile()
+
+        # TODO: Filter line edit and populate table
+        self.filter_srid_value = '25831'
 
         # Set listeners
-        self.dlg_readsql_create_project.btn_accept.clicked.connect(partial(self.create_project_data_schema, chk_import_data, chk_disable_ct))
+        self.dlg_readsql_create_project.btn_accept.clicked.connect(partial(self.executeFiles))
+        self.cmb_locale.currentIndexChanged.connect(partial(self.update_locale))
+        self.rdb_import_data.toggled.connect(partial(self.enable_datafile))
+        self.filter_srid.textChanged.connect(partial(self.filter_srid_changed))
 
         # Open dialog
         self.dlg_readsql_create_project.show()
 
     def executeFiles(self, filelist, filedir):
+        schema_name = self.schema_name.replace('"','')
+        filter_srid_value = self.filter_srid_value.replace('"','')
         for file in filelist:
-            f = open(filedir + '/' + file, 'r')
-            if f:
-                f_to_read = str(f.read().replace("SCHEMA_NAME", "ws_sample").decode(str('utf-8-sig')))
-                status = self.controller.execute_sql(str(f_to_read))
-                if status is False:
-                    print(str(file))
+            try:
+                f = open(filedir + '/' + file, 'r')
+                if f:
+                    f_to_read = str(f.read().replace("SCHEMA_NAME", schema_name).replace("SRID", filter_srid_value).decode(str('utf-8-sig')))
+                    # f_to_read = str(f.read().replace("SRID", filter_srid_value))
+                    # f_to_read = f_to_read.decode(str('utf-8-sig'))
+                    status = self.controller.execute_sql(str(f_to_read))
+                    if status is False:
+                        print(str(file))
+                        return False
+                else:
                     return False
-            else:
-                return False
+            except:
+                print "Command skipped. Unexpected error"
+                # self.dao.rollback()
+                # self.dao.close()
+                # print lengthy error description!!
+                # sys.exit(2)
         return True
