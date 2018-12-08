88fa97e782faa49abf75059e0d8378537c8d287e
Xavier Torret <xtorret@bgeo.es>
2018-12-08 13:27:03 +0100

    Bug fix on v_visit

--- dbmodel/updates/31/31105/ws/view.sql
+++ dbmodel/updates/31/31105/ws/view.sql
@@ -15,6 +15,64 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 	- has not trigger
 */
 
+DROP VIEW v_om_visit;
+CREATE OR REPLACE VIEW v_om_visit AS 
+ SELECT om_visit.id AS visit_id,
+    om_visit.ext_code AS code,
+    om_visit.visitcat_id,
+    om_visit_cat.name,
+    om_visit.startdate AS visit_start,
+    om_visit.enddate AS visit_end,
+    om_visit.user_name,
+    om_visit.is_done,
+    om_visit_x_node.node_id AS feature_id,
+    'NODE'::text as feature_type,
+    om_visit.the_geom
+   FROM selector_state,
+    om_visit
+     JOIN om_visit_x_node ON om_visit_x_node.visit_id = om_visit.id
+     JOIN node ON node.node_id::text = om_visit_x_node.node_id::text
+     JOIN om_visit_cat ON om_visit.visitcat_id = om_visit_cat.id
+  WHERE selector_state.state_id = node.state AND selector_state.cur_user = "current_user"()::text
+UNION
+ SELECT om_visit.id AS visit_id,
+    om_visit.ext_code AS code,
+    om_visit.visitcat_id,
+    om_visit_cat.name,
+    om_visit.startdate AS visit_start,
+    om_visit.enddate AS visit_end,
+    om_visit.user_name,
+    om_visit.is_done,
+    om_visit_x_arc.arc_id AS feature_id,
+    'ARC'::text as feature_type,
+    om_visit.the_geom
+   FROM selector_state,
+    om_visit
+     JOIN om_visit_x_arc ON om_visit_x_arc.visit_id = om_visit.id
+     JOIN arc ON arc.arc_id::text = om_visit_x_arc.arc_id::text
+     JOIN om_visit_cat ON om_visit.visitcat_id = om_visit_cat.id
+  WHERE selector_state.state_id = arc.state AND selector_state.cur_user = "current_user"()::text
+UNION
+ SELECT om_visit.id AS visit_id,
+    om_visit.ext_code AS code,
+    om_visit.visitcat_id,
+    om_visit_cat.name,
+    om_visit.startdate AS visit_start,
+    om_visit.enddate AS visit_end,
+    om_visit.user_name,
+    om_visit.is_done,
+    om_visit_x_connec.connec_id AS feature_id,
+    'CONNEC'::text as feature_type,
+    om_visit.the_geom
+   FROM selector_state,
+    om_visit
+     JOIN om_visit_x_connec ON om_visit_x_connec.visit_id = om_visit.id
+     JOIN connec ON connec.connec_id::text = om_visit_x_connec.connec_id::text
+     JOIN om_visit_cat ON om_visit.visitcat_id = om_visit_cat.id
+  WHERE selector_state.state_id = connec.state AND selector_state.cur_user = "current_user"()::text;
+
+
+
 
 CREATE OR REPLACE VIEW SCHEMA_NAME.v_arc_dattrib AS 
  SELECT arc_id,
