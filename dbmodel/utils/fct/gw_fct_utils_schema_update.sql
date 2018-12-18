9dc150afaa47656f1fe04bbb4cb7a795aae53579
Xavier Torret <xtorret@bgeo.es>
2018-12-18 19:40:29 +0100

    WiP-#590

--- dbmodel/utils/fct/gw_fct_utils_schema_update.sql
+++ dbmodel/utils/fct/gw_fct_utils_schema_update.sql
@@ -6,77 +6,78 @@ This version of Giswater is provided by Giswater Association
 
 --FUNCTION CODE: 2546
 
-CREATE OR REPLACE FUNCTION ws_sample.gw_fct_utils_schema_update(p_newversion text, p_projectype text, p_lang text, p_epsg integer) RETURNS json AS
+CREATE OR REPLACE FUNCTION ws_sample.gw_fct_utils_schema_update(p_data json) RETURNS json AS
 $BODY$
+
+/*
+SELECT ws_sample.gw_api_getlist($${
+"client":{"lang":"ES"},
+"data":{"isNewProject":"TRUE", "gwVersion":"3.1.105", "projectType"="WS", "epsg":25831}}$$)
+*/
 
 DECLARE 
 	v_dbnname varchar;
-	v_schema_array name[];
-	v_schemaname varchar;
-	v_tablerecord record;
 	v_projecttype text;
-	v_query_text text;
-	v_function_name text;
-	v_apiservice boolean;	
 	v_priority integer;
 	v_message text;
 	v_version record;
+	v_gwversion text;
+	v_language text;
+	v_epsg integer;
+	v_isnew boolean;
 
 BEGIN 
-
 	-- search path
 	SET search_path = "ws_sample", public;
-	
-		
-	-- check inconsistency for ws project:
-	IF v_projecttype = 'WS' THEN
-	
-		-- look for inp_pattern_value bug (+18 values are not possible)
-		IF (SELECT factor_19, factor_20, factor_21, factor_22, factor_23, factor_24 FROM inp_pattern_value LIMIT 1) IS NOT NULL THEN
-			INSERT INTO audit_log_project (fprocesscat_id, table_id, log_message) 
-					VALUES (33, inp_pattern_value, '"version":"'||p_newversion||'", "message":"There are some values on columns form 19 to 24. It must be deleted because it causes a bug on EPANET"');
-			v_priority=1;
-		
-		END IF;
-			
-	ELSIF v_projecttype = 'UD' THEN
-	
-	END IF;
-	
+
+	-- get input parameters
+	v_gwversion := (p_data ->> 'data')::json->> 'gwVersion';
+	v_language := (p_data ->> 'client')::json->> 'lang';
+	v_projecttype := (p_data ->> 'data')::json->> 'projectType';
+	v_epsg := (p_data ->> 'data')::json->> 'epsg';
+	v_isnew := (p_data ->> 'data')::json->> 'isNewProject';
+
 	-- update permissions	
 	PERFORM gw_fct_utils_role_permissions();
+
 	
-	-- version table (for new project)
-	IF p_lang IS NOT NULL THEN 
-		-- inserting version table
-		INSERT INTO version (giswater, wsoftware, language, epsg) VALUES (p_newversion, p_projectype, v_version.language, v_version.epsg);
-		
-	-- for updates
-	ELSE 
-		-- Looking for project type
-		SELECT * INTO v_version FROM version LIMIT 1;
+	-- last proccess
+	IF v_isnew IS TRUE THEN
 
 		-- inserting version table
-		INSERT INTO version (giswater, wsoftware, language, epsg) VALUES (p_newversion, v_version.wsoftware, p_lang, p_epsg);
+		INSERT INTO version (giswater, wsoftware, language, epsg) VALUES (v_gwversion, v_projectype, v_language, v_epsg);		
+	ELSE
+		-- check project consistency
+		IF v_projecttype = 'WS' THEN
+	
+			-- look for inp_pattern_value bug (+18 values are not possible)
+			IF (SELECT factor_19, factor_20, factor_21, factor_22, factor_23, factor_24 FROM inp_pattern_value LIMIT 1) IS NOT NULL THEN
+				INSERT INTO audit_log_project (fprocesscat_id, table_id, log_message) 
+				VALUES (33, inp_pattern_value, '"version":"'||v_gwversion||'", "message":"There are some values on columns form 19 to 24. It must be deleted because it causes a bug on EPANET"');
+				v_priority=1;
+			END IF;
+			
+		ELSIF v_projecttype = 'UD' THEN
+
+		END IF;
+
+		-- inserting version table
+		SELECT * INTO v_version FROM version LIMIT 1;	
+		INSERT INTO version (giswater, wsoftware, language, epsg) VALUES (v_gwversion, v_version.wsoftware, v_version.language, v_version.epsg);
 	END IF;
 	
 	-- get return message
 	IF v_priority=0 THEN
 		v_message="Project updated sucessfully";
-	
 	ELSIF v_priority=1 THEN
 		v_message=concat($$Project updated but there some warnings. Please check on audit_log_project table to see it 
-					SELECT * FROM audit_log_project WHERE fprocesscat_id=33 and (message::json->>'version')=$$, p_newversion);
-	
+				SELECT * FROM audit_log_project WHERE fprocesscat_id=33 and (message::json->>'version')=$$, v_gwversion);
 	ELSIF v_priority=2 THEN
-	
+		v_message="Project updated failed";
 	END IF;
 	
 	-- Return
-    RETURN ('{"message":{"priority":"'||v_priority||'", "text":"'||v_message||'"}}');
-	
-			
-
+	RETURN ('{"message":{"priority":"'||v_priority||'", "text":"'||v_message||'"}}');	
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
