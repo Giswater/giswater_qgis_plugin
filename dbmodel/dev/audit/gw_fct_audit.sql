965bacb3f349ee7825e6d04a6ab4adc47a966524
Xavier Torret <xtorret@bgeo.es>
2019-02-01 23:26:21 +0100

    Implement customizable audit strategy

---
+++ dbmodel/dev/audit/gw_fct_audit.sql
@@ -0,0 +1,41 @@
+﻿/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+--FUNCTION CODE: XXXX
+
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_audit_schema(p_action text)
+  RETURNS integer AS
+$BODY$
+
+/*
+SELECT SCHEMA_NAME.gw_fct_audit_schema('enable')
+SELECT SCHEMA_NAME.gw_fct_audit_schema('disable')
+*/
+
+DECLARE 
+      v_schema_name text;
+
+BEGIN 
+	-- search path
+	SET search_path = "SCHEMA_NAME", public;
+	v_schema_name = 'SCHEMA_NAME';
+
+
+	FOR table_record IN SELECT * FROM audit_cat_table WHERE isaudit  IS TRUE
+	LOOP 
+		EXECUTE 'DROP TRIGGER IF EXISTS gw_trg_audit'||table_record.id||' ON '||table_record.id;
+		IF p_action='enable' THEN
+			EXECUTE 'CREATE TRIGGER gw_trg_audit'||table_record.id||' AFTER INSERT OR UPDATE OR DELETE ON '||v_schema_name||'.'
+			||table_record.id||'FOR EACH ROW EXECUTE PROCEDURE '||v_schema_name||'.gw_trg_audit()';
+		END IF;
+	END LOOP;		
+
+return 0;
+END;
+
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
---
+++ dbmodel/dev/audit/gw_trg_audit_manager.sql
@@ -0,0 +1,28 @@
+﻿/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+--FUNCTION CODE: XXXX
+
+
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_trg_audit_manager() RETURNS TRIGGER AS $body$
+DECLARE
+    
+BEGIN
+
+--	Set search path to local schema
+	SET search_path = SCHEMA_NAME, public;
+	
+	FOR table_record IN SELECT * FROM audit_cat_table WHERE isaudit  IS TRUE
+	LOOP 
+		DELETE FROM audit.log WHERE interval (now()-tstamp) > table_record.keepauditdays
+	END LOOP;	
+
+
+END;
+$body$
+LANGUAGE plpgsql;
+
