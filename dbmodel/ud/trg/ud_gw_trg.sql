a46bf8a0ddd7a5487008020ff8a0fb46e4baca4b
Xavier Torret <xtorret@bgeo.es>
2019-02-21 10:43:20 +0100

    Bug fix on rename functionality (trigger gully)

--- dbmodel/ud/trg/ud_gw_trg.sql
+++ dbmodel/ud/trg/ud_gw_trg.sql
@@ -238,6 +238,6 @@ DROP TRIGGER IF EXISTS gw_trg_update_link_arc_id ON "SCHEMA_NAME".gully;
 CREATE TRIGGER gw_trg_update_link_arc_id AFTER UPDATE OF arc_id ON "SCHEMA_NAME".gully
 FOR EACH ROW EXECUTE PROCEDURE "SCHEMA_NAME".gw_trg_update_link_arc_id(gully);
 
-DROP TRIGGER IF EXISTS gw_trg_man_addfields_value_gully_control ON gully;
+DROP TRIGGER IF EXISTS gw_trg_man_addfields_value_gully_control ON "SCHEMA_NAME".gully;
 CREATE TRIGGER gw_trg_man_addfields_value_gully_control AFTER UPDATE OF gully_id OR DELETE ON SCHEMA_NAME.gully
 FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_man_addfields_value_control('GULLY');
