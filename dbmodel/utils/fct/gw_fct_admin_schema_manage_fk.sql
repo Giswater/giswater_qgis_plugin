b17b0ee4cd5616c5b1e1d7a4619828564d342364
Barbara Rzepka <bbrzepka@bgeo.es>
2019-03-12 13:02:47 +0100

    gw_fct_admin_schema_manage_fk - drop/add not null

--- dbmodel/utils/fct/gw_fct_admin_schema_manage_fk.sql
+++ dbmodel/utils/fct/gw_fct_admin_schema_manage_fk.sql
@@ -1,11 +1,6 @@
-﻿/*
-This file is part of Giswater 3
-The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-This version of Giswater is provided by Giswater Association
-*/
+﻿-- Function: SCHEMA_NAME.gw_fct_admin_schema_manage_fk(json)
 
---FUNCTION CODE: 2648
-
+-- DROP FUNCTION SCHEMA_NAME.gw_fct_admin_schema_manage_fk(json);
 
 CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_admin_schema_manage_fk(p_data json)
   RETURNS void AS
@@ -37,8 +32,9 @@ BEGIN
 	IF v_action = 'DROP' THEN
 
 		-- Insert fk on temp_table
-		DELETE FROM SCHEMA_NAME.temp_table WHERE fprocesscat_id=36;
-		INSERT INTO SCHEMA_NAME.temp_table (fprocesscat_id, text_column)
+		DELETE FROM temp_table WHERE fprocesscat_id=36;
+		
+		INSERT INTO temp_table (fprocesscat_id, text_column)
 		SELECT distinct on (conname)
 		36,
 		concat(
@@ -56,15 +52,44 @@ BEGIN
 		AND constraint_type = 'FOREIGN KEY'
 		AND nspname =v_schemaname;
 
+		DELETE FROM temp_table WHERE fprocesscat_id=37;
+
+		INSERT INTO temp_table (fprocesscat_id, text_column)
+		SELECT distinct
+				37,
+				concat(
+				'{"tablename":"',
+				table_name,
+				'","attributename":"',
+				column_name,
+				'","definition":"',
+			--	pg_get_attributedef(c.oid),
+				'"}')
+		FROM information_schema.columns
+		WHERE table_schema = v_schemaname
+		AND is_nullable='NO' and concat(column_name,'_',table_name) not in (select concat(kcu.column_name,'_',tc.table_name)  FROM 
+ 		 	  information_schema.table_constraints AS tc 
+  			  JOIN information_schema.key_column_usage AS kcu
+  			  ON tc.constraint_name = kcu.constraint_name AND tc.table_schema = kcu.table_schema
+			  WHERE tc.constraint_type = 'PRIMARY KEY' OR tc.constraint_type ='UNIQUE' );
+
 		-- Drop fk
-		FOR v_tablerecord IN SELECT * FROM SCHEMA_NAME.temp_table WHERE fprocesscat_id=36
+		FOR v_tablerecord IN SELECT * FROM temp_table WHERE fprocesscat_id=36
 		LOOP
 			v_query_text:= 'ALTER TABLE '||((v_tablerecord.text_column)::json->>'tablename')||
 				       ' DROP CONSTRAINT IF EXISTS '||((v_tablerecord.text_column)::json->>'constraintname')||';';
 			raise notice 'asgsdg %',v_query_text;
 			EXECUTE v_query_text;
 		END LOOP;
-	
+
+		FOR v_tablerecord IN SELECT * FROM temp_table WHERE fprocesscat_id=37
+		LOOP
+			v_query_text:= 'ALTER TABLE '||((v_tablerecord.text_column)::json->>'tablename')||
+				       ' ALTER COLUMN '||((v_tablerecord.text_column)::json->>'attributename')||' DROP NOT NULL;';
+			raise notice 'NOTNULL %',v_query_text;
+			EXECUTE v_query_text;
+		END LOOP;
+
 	ELSIF v_action = 'ADD' THEN
 		
 		FOR v_tablerecord IN SELECT * FROM temp_table WHERE fprocesscat_id=36
@@ -72,6 +97,15 @@ BEGIN
 			v_query_text:=  'ALTER TABLE '||((v_tablerecord.text_column::json)->>'tablename')|| 
 							' ADD CONSTRAINT '||((v_tablerecord.text_column::json)->>'constraintname')||
 							' '||((v_tablerecord.text_column::json)->>'definition');
+			EXECUTE v_query_text;
+		END LOOP;
+
+		
+		FOR v_tablerecord IN SELECT * FROM temp_table WHERE fprocesscat_id=37
+		LOOP
+			v_query_text:=  'ALTER TABLE '||((v_tablerecord.text_column::json)->>'tablename')|| 
+							' ALTER COLUMN '||((v_tablerecord.text_column::json)->>'attributename')||' SET NOT NULL;' ;
+		raise notice 'SET NOTNULL %',v_query_text;
 			EXECUTE v_query_text;
 		END LOOP;
 		
@@ -83,4 +117,3 @@ END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-
