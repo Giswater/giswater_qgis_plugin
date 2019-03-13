0c1d4d221cce16c6cb7f445e2b1545380c9674c4
Néstor Ibáñez <nestor@bgeo.es>
2019-03-13 12:19:42 +0100

    bug fix when value is null

--- actions/api_parent.py
+++ actions/api_parent.py
@@ -1101,7 +1101,7 @@ class ApiParent(ParentAction):
                         widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
                     elif field['widgettype'] == 'check':
                         widget = QCheckBox()
-                        if field['value'].lower() == "true":
+                        if field['value'] is not None and field['value'].lower() == "true":
                             widget.setChecked(True)
                         else:
                             widget.setChecked(False)
