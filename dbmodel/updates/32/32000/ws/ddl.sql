0bf0d4fc052a5b232d304e098e2ba14aa41542e8
Xavier Torret <xtorret@bgeo.es>
2019-03-17 19:32:29 +0100

    Separate delta files (3.1.109 - 3.2.000) - Part 9

--- dbmodel/updates/32/32000/ws/ddl.sql
+++ dbmodel/updates/32/32000/ws/ddl.sql
@@ -8,20 +8,6 @@ This version of Giswater is provided by Giswater Association
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 
--- 2019/01/30
-ALTER TABLE rpt_inp_arc ADD COLUMN flw_code text;
-
-
-ALTER TABLE inp_pipe ALTER COLUMN minorloss SET DEFAULT 0;
-
-
-CREATE TABLE rpt_selector_hourly_compare
-( id serial NOT NULL,
-  "time" character varying(100) NOT NULL,
-  cur_user text NOT NULL,
-  CONSTRAINT rpt_selector_result_hourly_compare_pkey PRIMARY KEY (id)
-);
-
 
 
 CREATE TABLE inp_valve_importinp
