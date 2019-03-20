6b8e76a80e7940d299b6db9878184d1b5d130397
Néstor Ibáñez <nestor@bgeo.es>
2019-03-20 17:08:26 +0100

    improve depth control

--- init/ud_man_node_init.py
+++ init/ud_man_node_init.py
@@ -209,27 +209,37 @@ class ManNodeDialog(ParentDialog):
         widget_ymax.setStyleSheet("border: 1px solid gray")
         node_id = utils_giswater.getWidgetText(self.dialog, 'node_id')
         ymax = utils_giswater.getWidgetText(self.dialog, widget_ymax)
+        if ymax is None:
+            return
         bad_alert = False
         sql = ("SELECT * from " + self.schema_name + ".v_ui_node_x_connection_upstream "
                " WHERE node_id = '" + str(node_id) + "'")
-        rows = self.controller.get_rows(sql, log_sql=True)
+        rows = self.controller.get_rows(sql, log_sql=False)
+        arcs_list = ""
         if len(rows) > 0:
+            arcs_list += "Upstream: "
             for row in rows:
-                if float(ymax) < float(row['downstream_depth']):
-                    widget_ymax.setStyleSheet("border: 1px solid red")
-                    bad_alert = True
+                if row['upstream_depth'] is not None:
+                    if float(ymax) < float(row['upstream_depth']):
+                        widget_ymax.setStyleSheet("border: 1px solid red")
+                        arcs_list += str((row['feature_id'] + ", "))
+                        bad_alert = True
         sql = ("SELECT * from " + self.schema_name + ".v_ui_node_x_connection_downstream "
                " WHERE node_id = '" + str(node_id) + "'")
-        rows = self.controller.get_rows(sql, log_sql=True)
+        rows = self.controller.get_rows(sql, log_sql=False)
         if len(rows) > 0:
+            arcs_list += "Downstream: "
             for row in rows:
-                if float(ymax) < float(row['upstream_depth']):
-                    widget_ymax.setStyleSheet("border: 1px solid red")
-                    bad_alert = True
-        self.controller.log_info(str(show_message))
-        self.controller.log_info(str(bad_alert))
+                if row['downstream_depth'] is not None:
+                    if float(ymax) < float(row['downstream_depth']):
+                        widget_ymax.setStyleSheet("border: 1px solid red")
+                        arcs_list += str((row['feature_id'] + ", "))
+                        bad_alert = True
+        if len(arcs_list)>2:
+            arcs_list = arcs_list[:-2]
+
         if show_message and bad_alert:
-            msg = "The depth of Y max is less than the arc"
+            msg = "The depth of this node is less than the arc/'s {}".format(arcs_list)
             # self.controller.show_info_box(text=msg, title="Info")
             msg_box = QMessageBox()
             msg_box.setIcon(3)
