b0a145c146d27504a432d74b8932f7f3a0bd022c
Xavier Torret <xtorret@bgeo.es>
2019-03-29 20:57:03 +0100

    add new user variable inp_options_use_dma_patterns

---
+++ dbmodel/updates/32/32003/ws/dml.sql
@@ -0,0 +1,13 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+
+-- 2919/03/29
+INSERT INTO audit_cat_param_user VALUES ('inp_options_use_dma_pattern', 'epaoptions', 'Use dma standard pattern for non CRM simulations', 'role_epa', true, 'Use dma default pattern',  
+NULL, NULL, TRUE, 15, 3, 'ws', NULL, NULL, NULL, NULL, NULL, 'boolean', 'check', TRUE, NULL, 'TRUE', 'grl_inpother_15', NULL, TRUE, NULL, NULL, NULL, null);
