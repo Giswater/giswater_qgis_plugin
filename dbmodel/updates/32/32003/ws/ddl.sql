15ca20846801487404dc9889e922675d46cde465
Xavier Torret <xtorret@bgeo.es>
2019-04-04 18:41:49 +0200

    Move from 3.2.004 to 3.2.003 alter inp_pattern_value table

--- dbmodel/updates/32/32003/ws/ddl.sql
+++ dbmodel/updates/32/32003/ws/ddl.sql
@@ -11,3 +11,29 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 -- 2019/03/29
 ALTER TABLE rpt_inp_node ADD column pattern_id varchar(16);
+
+
+
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_2 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_3 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_4 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_5 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_6 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_7 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_8 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_9 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_10 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_11 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_12 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_13 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_14 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_15 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_16 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_17 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN factor_18 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN _factor_19 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN _factor_20 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN _factor_21 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN _factor_22 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN _factor_23 DROP DEFAULT;
+ALTER TABLE inp_pattern_value ALTER COLUMN _factor_24 DROP DEFAULT;
--- dbmodel/updates/32/32004/ws/ddl.sql
+++ dbmodel/updates/32/32004/ws/ddl.sql
@@ -40,29 +40,3 @@ ALTER TABLE inp_energy_gl ALTER COLUMN id SET DEFAULT nextval('SCHEMA_NAME.inp_e
 ALTER TABLE inp_energy_el ALTER COLUMN id SET DEFAULT nextval('SCHEMA_NAME.inp_energy_el_id_seq'::regclass);
 ALTER TABLE inp_reactions_gl ALTER COLUMN id SET DEFAULT nextval('SCHEMA_NAME.inp_reactions_gl_id_seq'::regclass);
 ALTER TABLE inp_reactions_el ALTER COLUMN id SET DEFAULT nextval('SCHEMA_NAME.inp_reactions_el_id_seq'::regclass);
-
-
-
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_2 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_3 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_4 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_5 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_6 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_7 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_8 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_9 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_10 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_11 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_12 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_13 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_14 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_15 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_16 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_17 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN factor_18 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN _factor_19 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN _factor_20 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN _factor_21 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN _factor_22 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN _factor_23 DROP DEFAULT;
-ALTER TABLE inp_pattern_value ALTER COLUMN _factor_24 DROP DEFAULT;
