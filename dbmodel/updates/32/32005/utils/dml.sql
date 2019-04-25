95089a926ba79e949e0d27acddf542a0e8ea1cb0
Albert Bofill <abofill@bgeo.es>
2019-04-25 16:28:22 +0200

    Update sys_role_id on audit_cat_table

--- dbmodel/updates/32/32005/utils/dml.sql
+++ dbmodel/updates/32/32005/utils/dml.sql
@@ -35,6 +35,8 @@ UPDATE audit_cat_table SET sys_role_id='role_om' WHERE id='v_ui_om_visitman_x_ar
 UPDATE audit_cat_table SET sys_role_id='role_om' WHERE id='v_ui_om_visitman_x_node';
 UPDATE audit_cat_table SET sys_role_id='role_om' WHERE id='v_ui_om_visitman_x_connec';
 
+UPDATE audit_cat_table SET sys_role_id='role_om' WHERE id='v_ui_om_visit';
+
 
 INSERT INTO audit_cat_error VALUES (2015, 'There is no state-1 feature as endpoint of link. It is impossible to create it', 'Try to connect the link to one arc / node / connec / gully or vnode with state=1', 2, true, NULL)
 ON CONFLICT (id) DO NOTHING;
