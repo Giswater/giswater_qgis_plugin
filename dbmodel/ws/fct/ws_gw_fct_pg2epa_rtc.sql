3cd640281603750ba63d1359b84dc287c392aa44
Xavier Torret <xtorret@bgeo.es>
2019-05-03 09:26:43 +0200

    Bug fix on sql pg2epa_rtc function

--- dbmodel/ws/fct/ws_gw_fct_pg2epa_rtc.sql
+++ dbmodel/ws/fct/ws_gw_fct_pg2epa_rtc.sql
@@ -54,7 +54,7 @@ BEGIN
 	END IF;
 
 	-- update patterns
-	UPDATE rpt_inp_node SET pattern=pattern_id FROM v_rtc_hydrometer_x_node_period a WHERE result_id=result_id_var AND rpt_inp_node.node_id=a.node_id;
+	UPDATE rpt_inp_node SET pattern_id=a.pattern_id FROM v_rtc_hydrometer_x_node_period a WHERE result_id=result_id_var AND rpt_inp_node.node_id=a.node_id;
 
 	
 RETURN 1;
