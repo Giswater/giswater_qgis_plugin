d8493c00d4e9822eed19d61b9f60450b204a2bd5
Xavier Torret <xtorret@bgeo.es>
2019-05-10 01:26:28 +0200

    Enhance role permissions function

--- dbmodel/utils/fct/gw_fct_admin_role_permissions.sql
+++ dbmodel/utils/fct/gw_fct_admin_role_permissions.sql
@@ -11,6 +11,7 @@ CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_admin_role_permissions() RETURNS v
 $BODY$
 
 DECLARE 
+	v_roleexists text;
 	v_dbnname varchar;
 	v_schema_array name[];
 	v_schemaname varchar;
@@ -35,8 +36,57 @@ BEGIN
 	v_schema_array := current_schemas(FALSE);
 	v_schemaname :=v_schema_array[1];
 	
-	-- get role permissions
-    v_rolepermissions = (SELECT value::boolean FROM config_param_system WHERE parameter='sys_role_permissions');
+
+	-- Create (if not exists) roles
+	SELECT rolname into v_roleexists FROM pg_roles WHERE rolname = 'role_basic';
+	IF v_roleexists is null THEN
+		CREATE ROLE "role_basic" NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+	END IF;
+   
+	SELECT rolname into v_roleexists FROM pg_roles WHERE rolname = 'role_om';
+	IF v_roleexists is null THEN
+		CREATE ROLE "role_om" NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+	END IF;
+
+	SELECT rolname into v_roleexists FROM pg_roles WHERE rolname = 'role_edit';
+	IF v_roleexists is null THEN
+		CREATE ROLE "role_edit" NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+	END IF;
+
+	SELECT rolname into v_roleexists FROM pg_roles WHERE rolname = 'role_epa';
+	IF v_roleexists is null THEN
+		CREATE ROLE "role_epa" NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+	END IF;
+
+	SELECT rolname into v_roleexists FROM pg_roles WHERE rolname = 'role_master';
+	IF v_roleexists is null THEN
+		CREATE ROLE "role_master" NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+	END IF;
+
+	SELECT rolname into v_roleexists FROM pg_roles WHERE rolname = 'role_admin';
+	IF v_roleexists is null THEN
+		CREATE ROLE "role_admin" NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+	END IF;
+	
+	SELECT rolname into v_roleexists FROM pg_roles WHERE rolname = 'role_crm';
+	IF v_roleexists is null THEN
+		CREATE ROLE "role_crm" NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+	END IF;
+
+	-- Grant permissions
+	GRANT role_basic TO role_om;
+	GRANT role_om TO role_edit;
+	GRANT role_edit TO role_epa;
+	GRANT role_epa TO role_master;
+	GRANT role_master TO role_admin;
+	
+
+	-- Grant role admin to postgres user
+	GRANT role_admin TO postgres;
+	
+
+    --v_rolepermissions = (SELECT value::boolean FROM config_param_system WHERE parameter='sys_role_permissions'); -- deprecated variable. Allvays roles are needed
+	v_rolepermissions = true; 
 	v_apiservice = (SELECT value::boolean FROM config_param_system WHERE parameter='sys_api_service');
 		
 	-- role permissions for schema
