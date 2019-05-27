50460371cb35312b788a6b0da258355950070ca5
Edgar Fuste <efuste@bgeo.es>
2019-05-27 10:38:54 +0200

    Bug fix: Remove non exist columns from query

--- dbmodel/api/fct/gw_api_getchilds.sql
+++ dbmodel/api/fct/gw_api_getchilds.sql
@@ -110,7 +110,7 @@ BEGIN
 
 		--  Combo rows child CATALOG
 		EXECUTE 'SELECT array_agg(row_to_json(a)) FROM (SELECT column_id, widgettype, column_id as widgetname,
-		dv_querytext, isparent, dv_parent_id, row_number()over(ORDER BY layout_id, layout_order) AS orderby , dv_querytext_filterc, isautoupdate, placeholder, dv_orderby_id, feature_dv_parent_value, editability, description AS tooltip
+		dv_querytext, isparent, dv_parent_id, row_number()over(ORDER BY layout_id, layout_order) AS orderby , dv_querytext_filterc, isautoupdate, placeholder, dv_orderby_id,  editability, tooltip
 		FROM config_api_form_fields WHERE formname = '||quote_literal(v_parameter)||' AND dv_parent_id='||quote_literal(v_comboparent)||' ORDER BY orderby) a WHERE widgettype = ''combo'''
 		INTO v_combo_rows_child;
 		v_combo_rows_child := COALESCE(v_combo_rows_child, '{}');
@@ -119,7 +119,7 @@ BEGIN
 	ELSE
 		--  Combo rows child
 		EXECUTE 'SELECT array_agg(row_to_json(a)) FROM (SELECT id, column_id, widgettype, datatype, concat(''data_'',column_id) as widgetname,
-		dv_querytext, isparent, dv_parent_id, row_number()over(ORDER BY layout_id, layout_order) AS orderby , dv_querytext_filterc, isautoupdate, placeholder, dv_orderby_id, feature_dv_parent_value, editability, description AS tooltip
+		dv_querytext, isparent, dv_parent_id, row_number()over(ORDER BY layout_id, layout_order) AS orderby , dv_querytext_filterc, isautoupdate, placeholder, dv_orderby_id,  editability, tooltip
 		FROM config_api_form_fields WHERE formname = $1 AND dv_parent_id='||quote_literal(v_comboparent)||' ORDER BY orderby) a WHERE widgettype = ''combo'''
 		INTO v_combo_rows_child
 		USING v_tablename;
