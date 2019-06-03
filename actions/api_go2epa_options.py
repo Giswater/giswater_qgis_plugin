7fbd39f31f9a5b8818d46331bbbe55d1e66b5bdd
Edgar Fusté <efuste@bgeo.es>
2019-06-03 13:03:37 +0200

    Enhancement: Add combo child functionality on epa option form

--- actions/api_go2epa_options.py
+++ actions/api_go2epa_options.py
@@ -9,11 +9,14 @@ from qgis.PyQt.QtWidgets import QGroupBox, QSpacerItem, QSizePolicy
 from qgis.PyQt.QtWidgets import QGridLayout, QWidget
 
 import json
+import utils_giswater
+import operator
 from collections import OrderedDict
 from functools import partial
 
 from giswater.actions.api_parent import ApiParent
 from giswater.ui_manager import ApiEpaOptions
+from PyQt4.QtGui import QComboBox
 
 
 class Go2EpaOptions(ApiParent):
@@ -60,6 +63,10 @@ class Go2EpaOptions(ApiParent):
                     spacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
                     grl.addItem(spacer)
 
+        # Event on change from combo parent
+        self.get_event_combo_parent('fields', complet_result[0]['body']['form']['formTabs'])
+
+
         self.dlg_options.btn_accept.clicked.connect(partial(self.update_values, self.epa_options_list))
         self.dlg_options.btn_cancel.clicked.connect(partial(self.close_dialog, self.dlg_options))
         self.dlg_options.show()
@@ -77,3 +84,26 @@ class Go2EpaOptions(ApiParent):
         self.controller.show_info(message)
         # Close dialog
         self.close_dialog(self.dlg_options)
+
+    def get_event_combo_parent(self, fields, row):
+
+        if fields == 'fields':
+            for field in row[0]["fields"]:
+                if field['isparent']:
+                    widget = self.dlg_options.findChild(QComboBox, field['widgetname'])
+                    widget.currentIndexChanged.connect(partial(self.fill_child, widget))
+
+    def fill_child(self, widget):
+
+        combo_parent = widget.objectName()
+        combo_id = utils_giswater.get_item_data(self.dlg_options, widget)
+        sql = ("SELECT " + self.schema_name + ".gw_api_get_combochilds('epaoptions" + "' ,'' ,'' ,'" + str(combo_parent) + "', '" + str(combo_id) + "','')")
+        row = self.controller.get_row(sql, log_sql=True)
+        for combo_child in row[0]['fields']:
+            if combo_child is not None:
+                self.populate_child(combo_child)
+
+    def populate_child(self, combo_child):
+        child = self.dlg_options.findChild(QComboBox, str(combo_child['widgetname']))
+        if child:
+            self.populate_combo(child, combo_child)
