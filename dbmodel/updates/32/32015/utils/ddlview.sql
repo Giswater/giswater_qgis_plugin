67cc097ebec3b5c618ddc0073189b8afb99a100e
Xavier Torret - BGEO <xtorret@bgeo.es>
2019-06-27 07:17:51 +0200

    Update sql files

--- dbmodel/updates/32/32015/utils/ddlview.sql
+++ dbmodel/updates/32/32015/utils/ddlview.sql
@@ -8,47 +8,23 @@ This version of Giswater is provided by Giswater Association
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 
-CREATE OR REPLACE VIEW v_state_connec AS 
-(
-         SELECT connec.connec_id
-           FROM selector_state,
-            selector_expl,
-            connec
-          WHERE connec.state = selector_state.state_id AND connec.expl_id = selector_expl.expl_id AND selector_state.cur_user = "current_user"()::text AND selector_expl.cur_user = "current_user"()::text
-        EXCEPT
-         SELECT plan_psector_x_connec.connec_id
-           FROM selector_psector,
-            selector_expl,
-            plan_psector_x_connec
-             JOIN plan_psector ON plan_psector.psector_id = plan_psector_x_connec.psector_id
-          WHERE plan_psector_x_connec.psector_id = selector_psector.psector_id AND selector_psector.cur_user = "current_user"()::text AND plan_psector_x_connec.state = 0 AND plan_psector.expl_id = selector_expl.expl_id AND selector_expl.cur_user = "current_user"()::text
-) UNION
- SELECT plan_psector_x_connec.connec_id
-   FROM selector_psector,
-    selector_expl,
-    plan_psector_x_connec
-     JOIN plan_psector ON plan_psector.psector_id = plan_psector_x_connec.psector_id
-  WHERE plan_psector_x_connec.psector_id = selector_psector.psector_id AND selector_psector.cur_user = "current_user"()::text AND plan_psector_x_connec.state = 1 AND plan_psector.expl_id = selector_expl.expl_id AND selector_expl.cur_user = "current_user"()::text;
-
-  
-
-
 CREATE OR REPLACE VIEW v_edit_link AS 
- SELECT link.link_id,
+ SELECT 
+    link.link_id,
     link.feature_type,
     link.feature_id,
-    sector.macrosector_id,
-    dma.macrodma_id,
     link.exit_type,
     link.exit_id,
         CASE
             WHEN link.feature_type::text = 'CONNEC'::text THEN v_edit_connec.sector_id
             ELSE vnode.sector_id
         END AS sector_id,
+	    sector.macrosector_id,
         CASE
             WHEN link.feature_type::text = 'CONNEC'::text THEN v_edit_connec.dma_id
             ELSE vnode.dma_id
         END AS dma_id,
+	    dma.macrodma_id,
         CASE
             WHEN link.feature_type::text = 'CONNEC'::text THEN v_edit_connec.expl_id
             ELSE vnode.expl_id
