c997a7f936514230ea495d865d21770a3b6f827f
Albert Bofill <abofill@bgeo.es>
2019-07-03 12:16:54 +0200

    Bug fix on v_plan_node definition

--- dbmodel/updates/32/32015/ud/ddlview.sql
+++ dbmodel/updates/32/32015/ud/ddlview.sql
@@ -586,3 +586,45 @@ SELECT om_visit_event.id AS event_id,
     LEFT JOIN ( SELECT DISTINCT doc_x_visit.visit_id
           FROM doc_x_visit) b ON b.visit_id = om_visit.id
  ORDER BY om_visit_x_gully.gully_id;
+
+ 
+CREATE OR REPLACE VIEW v_plan_node AS 
+SELECT
+v_edit_node.node_id,
+nodecat_id,
+sys_type AS node_type,
+top_elev,
+elev,
+epa_type,
+sector_id,
+state,
+annotation,
+the_geom,
+v_price_x_catnode.cost_unit,
+v_price_compost.descript,
+v_price_compost.price as cost,
+CASE WHEN v_price_x_catnode.cost_unit::text = 'u' THEN 1
+     WHEN v_price_x_catnode.cost_unit::text = 'm3' THEN (CASE 	WHEN sys_type='STORAGE' THEN man_storage.max_volume::numeric 
+																WHEN sys_type='CHAMBER' THEN man_chamber.max_volume::numeric
+																ELSE NULL END)
+     WHEN v_price_x_catnode.cost_unit::text = 'm' THEN
+          CASE WHEN v_edit_node.ymax = 0 THEN v_price_x_catnode.estimated_y
+			   WHEN v_edit_node.ymax IS NULL THEN v_price_x_catnode.estimated_y
+               ELSE v_edit_node.ymax END
+    END::numeric(12,2) AS measurement,
+CASE WHEN v_price_x_catnode.cost_unit::text = 'u' THEN v_price_x_catnode.cost
+     WHEN v_price_x_catnode.cost_unit::text = 'm3' THEN (CASE 	WHEN sys_type='STORAGE' THEN man_storage.max_volume*v_price_x_catnode.cost 
+																WHEN sys_type='CHAMBER' THEN man_chamber.max_volume*v_price_x_catnode.cost 
+																ELSE NULL END)
+     WHEN v_price_x_catnode.cost_unit::text = 'm' THEN
+          CASE WHEN v_edit_node.ymax = 0 THEN v_price_x_catnode.estimated_y*v_price_x_catnode.cost
+               WHEN v_edit_node.ymax IS NULL THEN v_price_x_catnode.estimated_y*v_price_x_catnode.cost
+               ELSE v_edit_node.ymax*v_price_x_catnode.cost END 
+END::numeric(12,2) AS budget,
+expl_id
+FROM v_edit_node
+LEFT JOIN v_price_x_catnode ON v_edit_node.nodecat_id::text = v_price_x_catnode.id::text
+LEFT JOIN man_chamber ON man_chamber.node_id=v_edit_node.node_id
+LEFT JOIN man_storage ON man_storage.node_id=v_edit_node.node_id
+JOIN cat_node ON cat_node.id::text = v_edit_node.nodecat_id::text
+LEFT JOIN v_price_compost ON v_price_compost.id::text = cat_node.cost::text;
--- dbmodel/updates/32/32015/ws/ddlview.sql
+++ dbmodel/updates/32/32015/ws/ddlview.sql
@@ -193,3 +193,36 @@ CREATE OR REPLACE VIEW v_edit_vnode AS
    join v_edit_connec ON v_edit_link.feature_id=connec_id
    join arc USING (arc_id)
    left join plan_psector_x_connec USING (arc_id, connec_id);
+   
+CREATE OR REPLACE VIEW v_plan_node AS 
+SELECT
+v_edit_node.node_id,
+nodecat_id,
+sys_type AS node_type,
+elevation AS top_elev,
+elevation-depth as elev,
+epa_type,
+sector_id,
+state,
+annotation,
+the_geom,
+v_price_x_catnode.cost_unit,
+v_price_compost.descript,
+v_price_compost.price as cost,
+CASE WHEN v_price_x_catnode.cost_unit::text = 'u' THEN (CASE WHEN sys_type='PUMP' THEN (CASE WHEN pump_number IS NOT NULL THEN pump_number ELSE 1 END) ELSE 1 END)
+     WHEN v_price_x_catnode.cost_unit::text = 'm3' THEN (CASE WHEN sys_type='TANK' THEN vmax ELSE NULL END)
+     WHEN v_price_x_catnode.cost_unit::text = 'm' THEN (CASE WHEN v_edit_node.depth = 0 THEN v_price_x_catnode.estimated_depth 
+															 WHEN v_edit_node.depth IS NULL THEN v_price_x_catnode.estimated_depth ELSE v_edit_node.depth END)
+END::numeric(12,2) AS measurement,
+CASE WHEN v_price_x_catnode.cost_unit::text = 'u' THEN (CASE WHEN sys_type='PUMP' THEN (CASE WHEN pump_number IS NOT NULL THEN pump_number ELSE 1 END) ELSE 1 END)*v_price_x_catnode.cost
+     WHEN v_price_x_catnode.cost_unit::text = 'm3' THEN (CASE WHEN sys_type='TANK' THEN vmax ELSE NULL END)*v_price_x_catnode.cost
+     WHEN v_price_x_catnode.cost_unit::text = 'm' THEN (CASE WHEN v_edit_node.depth = 0 THEN v_price_x_catnode.estimated_depth
+															 WHEN v_edit_node.depth IS NULL THEN v_price_x_catnode.estimated_depth ELSE v_edit_node.depth END)*v_price_x_catnode.cost
+END::numeric(12,2) AS budget,
+expl_id
+FROM v_edit_node
+LEFT JOIN v_price_x_catnode ON v_edit_node.nodecat_id::text = v_price_x_catnode.id::text
+LEFT JOIN man_tank ON man_tank.node_id=v_edit_node.node_id
+LEFT JOIN man_pump ON man_pump.node_id=v_edit_node.node_id
+JOIN cat_node ON cat_node.id::text = v_edit_node.nodecat_id::text
+LEFT JOIN v_price_compost ON v_price_compost.id::text = cat_node.cost::text;
