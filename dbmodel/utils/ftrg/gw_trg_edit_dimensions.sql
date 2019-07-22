587c1592eedfe467675cff1ec39ace0d05a4e757
Barbara Rzepka <bbrzepka@bgeo.es>
2019-07-22 08:01:17 +0200

    Dimensioning - capture expl by location if null

--- dbmodel/utils/ftrg/gw_trg_edit_dimensions.sql
+++ dbmodel/utils/ftrg/gw_trg_edit_dimensions.sql
@@ -26,6 +26,9 @@ BEGIN
 		
 		IF NEW.expl_id IS NULL THEN
 			NEW.expl_id := (SELECT "value" FROM config_param_user WHERE "parameter"='exploitation_vdefault' AND "cur_user"="current_user"());
+			IF (NEW.expl_id IS NULL) THEN
+				NEW.expl_id := (SELECT expl_id FROM exploitation WHERE ST_DWithin(NEW.the_geom, exploitation.the_geom,0.001) LIMIT 1);
+			END IF;
 		END IF;
 
 		-- State
