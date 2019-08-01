c353f8e63346b49db1af9b2445bfeeed32265537
Albert Bofill <abofill@bgeo.es>
2019-08-01 17:57:18 +0200

    Bug fix on trg_edit_man_node (WS)

--- dbmodel/ws/ftrg/ws_gw_trg_edit_man_node.sql
+++ dbmodel/ws/ftrg/ws_gw_trg_edit_man_node.sql
@@ -490,7 +490,7 @@ BEGIN
 		function_type=NEW.function_type, category_type=NEW.category_type, fluid_type=NEW.fluid_type, location_type=NEW.location_type, workcat_id=NEW.workcat_id, workcat_id_end=NEW.workcat_id_end,  
 		buildercat_id=NEW.buildercat_id,builtdate=NEW.builtdate, enddate=NEW.enddate, ownercat_id=NEW.ownercat_id, muni_id=NEW.muni_id, streetaxis_id=NEW.streetaxis_id, postcomplement=NEW.postcomplement, postcomplement2=NEW.postcomplement2, 
 		streetaxis2_id=NEW.streetaxis2_id,postcode=NEW.postcode,postnumber=NEW.postnumber,postnumber2=NEW.postnumber2, descript=NEW.descript, verified=NEW.verified, undelete=NEW.undelete, label_x=NEW.label_x, 
-		label_y=NEW.label_y, label_rotation=NEW.label_rotation, publish=NEW.publish, inventory=NEW.inventory, expl_id=NEW.expl_id, num_value=NEW.num_value, link=NEW.link
+		label_y=NEW.label_y, label_rotation=NEW.label_rotation, publish=NEW.publish, inventory=NEW.inventory, rotation=NEW.rotation, expl_id=NEW.expl_id, num_value=NEW.num_value, link=NEW.link
 		WHERE node_id = OLD.node_id;
 		
 		IF man_table ='man_junction' THEN
