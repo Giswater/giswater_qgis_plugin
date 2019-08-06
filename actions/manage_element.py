58e8179d9129b3f303336b33bca2ef4e442df1a4
Néstor Ibáñez <nestor@bgeo.es>
2019-08-06 10:38:38 +0200

    bug fix

--- actions/manage_element.py
+++ actions/manage_element.py
@@ -244,7 +244,7 @@ class ManageElement(ParentManage):
         if row is None:
             # If object not exist perform an INSERT
             if element_id == '':
-                sql = ("INSERT INTO v_edit_element (element_id,  num_elements, state"
+                sql = ("INSERT INTO v_edit_element (elementcat_id,  num_elements, state"
                        ", expl_id, rotation, comment, observ, link, undelete, builtdate"
                        ", ownercat_id, location_type, buildercat_id, workcat_id, workcat_id_end, verified, the_geom, code)")
                 sql_values = (" VALUES ('" + str(elementcat_id) + "', '" + str(num_elements) + "', '" + str(state) + "', '"
@@ -299,7 +299,7 @@ class ManageElement(ParentManage):
             else:
                 sql_values += ", null"
             if element_id == '':
-                sql += sql_values + ") RETURNING elementcat_id;"
+                sql += sql_values + ") RETURNING element_id;"
                 new_elem_id = self.controller.execute_returning(sql, search_audit=False, log_sql=True)
                 sql_values = ""
                 sql = ""
