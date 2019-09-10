7182829cf377f0b1aa088d9d4516bde033b885f2
Barbara Rzepka <bbrzepka@bgeo.es>
2019-09-10 12:05:11 +0200

    ud updates - recreate vi trg

--- dbmodel/updates/32/32016/ud/trg.sql
+++ dbmodel/updates/32/32016/ud/trg.sql
@@ -21,3 +21,32 @@ DROP TRIGGER gw_trg_topocontrol_node ON node;
 CREATE TRIGGER gw_trg_topocontrol_node AFTER INSERT OR UPDATE 
 OF the_geom, state, top_elev, ymax, elev, custom_top_elev, custom_ymax, custom_elev, sys_elev
 ON node  FOR EACH ROW  EXECUTE PROCEDURE gw_trg_topocontrol_node();
+
+
+DROP TRIGGER IF EXISTS gw_trg_vi_outfalls ON SCHEMA_NAME.vi_outfalls;
+CREATE TRIGGER gw_trg_vi_outfalls INSTEAD OF INSERT OR UPDATE OR DELETE ON SCHEMA_NAME.vi_outfalls
+FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_vi('vi_outfalls');
+
+DROP TRIGGER IF EXISTS  gw_trg_vi_pollutants ON SCHEMA_NAME.vi_pollutants;
+CREATE TRIGGER gw_trg_vi_pollutants INSTEAD OF INSERT OR UPDATE OR DELETE ON SCHEMA_NAME.vi_pollutants
+FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_vi('vi_pollutants');
+
+DROP TRIGGER IF EXISTS gw_trg_vi_landuses ON SCHEMA_NAME.vi_landuses;
+CREATE TRIGGER gw_trg_vi_landuses INSTEAD OF INSERT OR UPDATE OR DELETE ON SCHEMA_NAME.vi_landuses
+FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_vi('vi_landuses');
+
+DROP TRIGGER IF EXISTS gw_trg_vi_inflows ON SCHEMA_NAME.vi_treatment;
+CREATE TRIGGER gw_trg_vi_treatment INSTEAD OF INSERT OR UPDATE OR DELETE ON SCHEMA_NAME.vi_treatment
+FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_vi('vi_treatment');
+
+DROP TRIGGER IF EXISTS gw_trg_vi_inflows ON SCHEMA_NAME.vi_transects;
+CREATE TRIGGER gw_trg_vi_transects INSTEAD OF INSERT OR UPDATE OR DELETE ON SCHEMA_NAME.vi_transects
+FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_vi('vi_transects');
+
+DROP TRIGGER IF EXISTS gw_trg_vi_inflows ON SCHEMA_NAME.vi_temperature;
+CREATE TRIGGER gw_trg_vi_temperature INSTEAD OF INSERT OR UPDATE OR DELETE ON SCHEMA_NAME.vi_temperature
+FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_vi('vi_temperature');
+
+DROP TRIGGER IF EXISTS gw_trg_vi_inflows ON SCHEMA_NAME.vi_evaporation;
+CREATE TRIGGER gw_trg_vi_evaporation INSTEAD OF INSERT OR UPDATE OR DELETE ON SCHEMA_NAME.vi_evaporation
+FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_vi('vi_evaporation');
