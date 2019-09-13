ceada18e704f50feb191a0fe0723fa16cdd44dd7
Xavier Torret <xtorret@bgeo.es>
2019-09-13 14:44:04 +0200

    Bug fix arcdivide function

--- dbmodel/utils/fct/gw_fct_arc_divide.sql
+++ dbmodel/utils/fct/gw_fct_arc_divide.sql
@@ -142,18 +142,18 @@ BEGIN
 			IF  v_querytext IS NULL THEN 
 				v_querytext='';
 			END IF;
-			v_querytext1 =  'INSERT INTO '||v_mantable||' SELECT rec_aux1.arc_id '||v_querytext||' FROM '||v_mantable||' WHERE arc_id= '||arc_id_aux;
-			v_querytext2 =  'INSERT INTO '||v_mantable||' SELECT rec_aux2.arc_id '||v_querytext||' FROM '||v_mantable||' WHERE arc_id= '||arc_id_aux;
+			v_querytext1 =  'INSERT INTO '||v_mantable||' SELECT ';
+			v_querytext2 =  v_querytext||' FROM '||v_mantable||' WHERE arc_id= '||arc_id_aux||'::text';
 
 			-- In function of states and user's variables proceed.....
 			IF (state_aux=1 AND state_node_arg=1) THEN 
 			
 				-- Insert new records into arc table
 				INSERT INTO arc SELECT rec_aux1.*;
-				EXECUTE v_querytext1;
+				EXECUTE v_querytext1||rec_aux1.arc_id::text||v_querytext2;
 				
 				INSERT INTO arc SELECT rec_aux2.*;
-				EXECUTE v_querytext2;
+				EXECUTE v_querytext1||rec_aux2.arc_id::text||v_querytext2;
 
 				-- update node_1 and node_2 because it's not possible to pass using parameters
 				UPDATE arc SET node_1=rec_aux1.node_1,node_2=rec_aux1.node_2 where arc_id=rec_aux1.arc_id;
@@ -314,10 +314,10 @@ BEGIN
 				-- Insert new records into arc table
 				UPDATE config_param_system SET value = replace (value, 'true', 'false') WHERE parameter='arc_searchnodes';
 				INSERT INTO arc SELECT rec_aux1.*;
-				EXECUTE v_querytext1;
+				EXECUTE v_querytext1||rec_aux1.arc_id::text||v_querytext2;
 				
 				INSERT INTO arc SELECT rec_aux2.*;
-				EXECUTE v_querytext2;
+				EXECUTE v_querytext1||rec_aux2.arc_id::text||v_querytext2;
 				
 				UPDATE config_param_system SET value = replace (value, 'false', 'true') WHERE parameter='arc_searchnodes';
 
@@ -348,10 +348,10 @@ BEGIN
 				-- Insert new records into arc table
 				UPDATE config_param_system SET value = replace (value, 'true', 'false') WHERE parameter='arc_searchnodes';
 				INSERT INTO arc SELECT rec_aux1.*;
-				EXECUTE v_querytext1;
+				EXECUTE v_querytext1||rec_aux1.arc_id::text||v_querytext2;
 
 				INSERT INTO arc SELECT rec_aux2.*;
-				EXECUTE v_querytext2;
+				EXECUTE v_querytext1||rec_aux2.arc_id::text||v_querytext2;
 
 				UPDATE config_param_system SET value = replace (value, 'false', 'true') WHERE parameter='arc_searchnodes';
 
