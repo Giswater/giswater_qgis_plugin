48838454041fed2dba92de86af1e2ddf173409d4
Xavier Torret <xtorret@bgeo.es>
2019-10-01 08:39:49 +0200

    Set null for vdefault of report link/nodes/subcatch

--- dbmodel/updates/33/33001/ud/dml.sql
+++ dbmodel/updates/33/33001/ud/dml.sql
@@ -11,3 +11,6 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 INSERT INTO audit_cat_param_user VALUES ('arctype_vdefault', 'config', 'Default value of arc type', 'role_edit', NULL, NULL, 'Arc Type:', 'SELECT id AS id, id AS idval FROM arc_type WHERE id IS NOT NULL', NULL, true, 10, 1, 'ud', false, NULL, NULL, NULL, false, 'string', 'combo', false, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, false);
 UPDATE audit_cat_param_user set layout_order=2 where id = 'arccat_vdefault';
+
+
+update audit_cat_param_user SET vdefault=NULL WHERE id IN ('inp_report_links','inp_report_nodes','inp_report_subcatchments');
