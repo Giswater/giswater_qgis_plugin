fb7d8f4c5691b408f1bf14cebec3893dbbde4449
Edgar Fuste <efuste@bgeo.es>
2019-10-01 09:10:11 +0200

    Enhancement: Rename label for widget on config

---
+++ dbmodel/api/updates/33/33001/utils/dml.sql
@@ -0,0 +1,12 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+
+-- 01/10/2019
+UPDATE audit_cat_param_user SET label = 'Keep opened edition on update feature:' WHERE id = 'cf_keep_opened_edition';
