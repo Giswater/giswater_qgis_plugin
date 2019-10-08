47a9a7511351aaf6ff6103b2c8d523296ed9dc9e
Xavier Torret <xtorret@bgeo.es>
2019-10-08 16:59:08 +0200

    Bug fix on sql file

--- dbmodel/updates/33/33001/ws/ddl.sql
+++ dbmodel/updates/33/33001/ws/ddl.sql
@@ -7,15 +7,14 @@ This version of Giswater is provided by Giswater Association
 
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
-
-DROP VIEW v_rpt_node;
-DROP VIEW v_rpt_node_all;
-DROP VIEW v_rpt_comp_node;
+DROP VIEW IF EXISTS v_rpt_node;
+DROP VIEW IF EXISTS v_rpt_node_all;
+DROP VIEW IF EXISTS v_rpt_comp_node;
 ALTER TABLE rpt_inp_node ALTER COLUMN node_type TYPE character varying(30);
 
 
-DROP VIEW v_rpt_comp_arc ;
-DROP VIEW v_rpt_arc_all ;
-DROP VIEW v_rpt_arc ;
-DROP VIEW vi_pipes;
+DROP VIEW IF EXISTS v_rpt_comp_arc ;
+DROP VIEW IF EXISTS v_rpt_arc_all ;
+DROP VIEW IF EXISTS v_rpt_arc ;
+DROP VIEW IF EXISTS vi_pipes;
 ALTER TABLE rpt_inp_arc ALTER COLUMN arc_type TYPE character varying(30);
