6b3c80e779d10dec8b84118b5059fdd6e047904d
Xavier Torret <xtorret@bgeo.es>
2019-10-09 12:15:40 +0200

    Bug fixed again due this bug was fixed on 3.3.002 delta files after release

--- dbmodel/api/updates/33/33004/ws/dml.sql
+++ dbmodel/api/updates/33/33004/ws/dml.sql
@@ -7,7 +7,7 @@ This version of Giswater is provided by Giswater Association
 
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
--- 09/10/2019  (due this bug was fixed on 3.3.002 delta files after release, it's mantadoty to be fixed again
+-- 09/10/2019  (due this bug was fixed on 3.3.002 delta files after release, it's mantadoty to be fixed again)
 UPDATE  config_api_form_fields SET widgettype='typeahead', 
 dv_querytext = 'SELECT cat_node.id, cat_node.id as idval FROM cat_node JOIN cat_feature ON cat_feature.id=cat_node.nodetype_id WHERE system_id = ''VALVE''', 
 typeahead = '{"fieldToSearch": "cat_node.id", "threshold": 3, "noresultsMsg": "No results", "loadingMsg": "Searching"}' 
