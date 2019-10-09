e37ee01158fe0360b83c25303497366e58cb78f0
Barbara Rzepka <bbrzepka@bgeo.es>
2019-10-09 11:35:07 +0200

    bug fix on sql

--- dbmodel/api/updates/33/33002/utils/dml.sql
+++ dbmodel/api/updates/33/33002/utils/dml.sql
@@ -10,8 +10,8 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 -- 08/10/2019
 update config_api_form_fields set ismandatory = false where column_id='private_connecat_id';
 
-UPDATE config_api_form_fields SET widgettype='typeahead', dv_querytext = 'SELECT pol_id AS id, id AS idval FROM polygon WHERE id is not null ', 
-typeahead='{"fieldToSearch": "id", "threshold": 3, "noresultsMsg": "No results", "loadingMsg": "Searching"}' WHERE column_id = 'pol_id';
+UPDATE config_api_form_fields SET widgettype='typeahead', dv_querytext = 'SELECT pol_id AS id, pol_id AS idval FROM polygon WHERE pol_id is not null ', 
+typeahead='{"fieldToSearch": "pol_id", "threshold": 3, "noresultsMsg": "No results", "loadingMsg": "Searching"}' WHERE column_id = 'pol_id';
 
 
 UPDATE  config_api_form_fields SET widgettype='typeahead', dv_querytext = 'SELECT id, id as idval FROM cat_grate WHERE id IS NOT NULL', isreload = false,
