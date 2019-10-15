487514ffda6d40af4254a6c65955655435b67345
Néstor Ibáñez <nestor@bgeo.es>
2019-10-15 12:19:47 +0200

    update sql

--- dbmodel/updates/33/33006/utils/dml.sql
+++ dbmodel/updates/33/33006/utils/dml.sql
@@ -8,7 +8,7 @@ This version of Giswater is provided by Giswater Association
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 -- 11/10/2019
-UPDATE config_param_user SET parameter = 'qgis_toolbar_hide_actions', value = '{"actions_index":[199,74,75,76]}' WHERE parameter = 'actions_to_hide';
+UPDATE config_param_user SET parameter = 'qgis_toolbar_hidebuttons', value = '{"action_index":[199,74,75,76]}' WHERE parameter = 'actions_to_hide';
 
 UPDATE audit_cat_param_user SET dv_querytext = 'SELECT UNNEST(ARRAY (select (text_column::json->>''list_layers_name'')::text[] from temp_table where fprocesscat_id = 63 and user_name = current_user)) as id, UNNEST(ARRAY (select (text_column::json->>''list_layers_name'')::text[] 
 FROM temp_table WHERE fprocesscat_id = 63 and user_name = current_user)) as idval ' WHERE id = 'cad_tools_base_layer_vdefault';
