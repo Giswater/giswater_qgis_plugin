5edbd190702343525fb18f6761f7833050d83f48
Néstor Ibáñez <nestor@bgeo.es>
2019-10-21 14:17:35 +0200

    add UNION inorder to get layers

--- dao/controller.py
+++ dao/controller.py
@@ -1112,9 +1112,9 @@ class DaoController(object):
         sql = ("SELECT tablename FROM "
                "(SELECT tablename, 1 as c FROM sys_feature_cat"
                " WHERE type = '" + geom_type.upper() + "'"
-               " UNION SELECT parentlayer, 0 FROM sys_feature_type"
-               " WHERE id='" + geom_type.upper() + "') as t "
-               " ORDER BY c")
+               " UNION SELECT parentlayer, 0 FROM sys_feature_type WHERE id='" + geom_type.upper() + "'"
+               " UNION SELECT child_layer, 2 as c FROM cat_feature WHERE feature_type = '" + geom_type.upper() + "') as t "
+               " ORDER BY c, tablename")
         rows = self.get_rows(sql, log_sql=True)
         if rows:
             for row in rows:
