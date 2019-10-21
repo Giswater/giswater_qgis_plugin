92db35af1738d393c7e4133de8bb72f930e43203
Xavier Torret <xtorret@bgeo.es>
2019-10-21 17:04:45 +0200

    Commit on back chekpoints (3.2.014) only to prevent conflicts on corporate enviroment with null retro-forward incidency

--- dbmodel/updates/32/32014/i18n/EN/utils.sql
+++ dbmodel/updates/32/32014/i18n/EN/utils.sql
@@ -8,12 +8,12 @@ This version of Giswater is provided by Giswater Association
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 
-INSERT INTO plan_typevalue VALUES ('psector_status', 0, 'EXECUTED', 'Psector done');
-INSERT INTO plan_typevalue VALUES ('psector_status', 1, 'ONGOING', 'Psector on course');
-INSERT INTO plan_typevalue VALUES ('psector_status', 2, 'PLANNED', 'Psector planned');
-INSERT INTO plan_typevalue VALUES ('psector_status', 3, 'CANCELED', 'Psector canceled');
+INSERT INTO plan_typevalue VALUES ('psector_status', 0, 'EXECUTED', 'Psector done') ON CONFLICT (typevalue, id) DO NOTHING;
+INSERT INTO plan_typevalue VALUES ('psector_status', 1, 'ONGOING', 'Psector on course') ON CONFLICT (typevalue, id) DO NOTHING;
+INSERT INTO plan_typevalue VALUES ('psector_status', 2, 'PLANNED', 'Psector planned') ON CONFLICT (typevalue, id) DO NOTHING;
+INSERT INTO plan_typevalue VALUES ('psector_status', 3, 'CANCELED', 'Psector canceled') ON CONFLICT (typevalue, id) DO NOTHING;
 
-INSERT INTO om_typevalue VALUES ('visit_cat_status', 1, 'Started');
-INSERT INTO om_typevalue VALUES ('visit_cat_status', 2, 'Stand-by');
-INSERT INTO om_typevalue VALUES ('visit_cat_status', 3, 'Canceled');
-INSERT INTO om_typevalue VALUES ('visit_cat_status', 4, 'Finished');
+INSERT INTO om_typevalue VALUES ('visit_cat_status', 1, 'Started') ON CONFLICT (typevalue, id) DO NOTHING;
+INSERT INTO om_typevalue VALUES ('visit_cat_status', 2, 'Stand-by') ON CONFLICT (typevalue, id) DO NOTHING;
+INSERT INTO om_typevalue VALUES ('visit_cat_status', 3, 'Canceled') ON CONFLICT (typevalue, id) DO NOTHING;
+INSERT INTO om_typevalue VALUES ('visit_cat_status', 4, 'Finished') ON CONFLICT (typevalue, id) DO NOTHING;
