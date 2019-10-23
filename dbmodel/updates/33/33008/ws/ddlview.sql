2bd1f7d1bf424e73c358b3059b1554ed9fe6d7ae
Barbara Rzepka <bbrzepka@bgeo.es>
2019-10-23 16:08:44 +0200

    separate v_edit_vnode for ws / ud

--- dbmodel/updates/33/33008/ws/ddlview.sql
+++ dbmodel/updates/33/33008/ws/ddlview.sql
@@ -44,3 +44,55 @@ CREATE OR REPLACE VIEW v_edit_samplepoint AS
      JOIN v_state_samplepoint ON samplepoint.sample_id::text = v_state_samplepoint.sample_id::text
      LEFT JOIN dma ON dma.dma_id = samplepoint.dma_id
   WHERE samplepoint.expl_id = selector_expl.expl_id AND selector_expl.cur_user = "current_user"()::text;
+
+
+
+DROP VIEW v_edit_vnode;
+CREATE OR REPLACE VIEW v_edit_vnode AS 
+ SELECT a.vnode_id,
+    a.vnode_type,
+    a.feature_type,
+    a.elev,
+    a.sector_id,
+    a.dma_id,
+    a.state,
+    a.annotation,
+    a.the_geom,
+    a.expl_id,
+    a.rotation,
+    a.ispsectorgeom,
+    a.psector_rowid
+   FROM ( SELECT DISTINCT ON (vnode.vnode_id) vnode.vnode_id,
+            vnode.vnode_type,
+            link.feature_type,
+            vnode.elev,
+            vnode.sector_id,
+            vnode.dma_id,
+                CASE
+                    WHEN plan_psector_x_connec.vnode_geom IS NULL THEN link.state
+                    ELSE plan_psector_x_connec.state
+                END AS state,
+            vnode.annotation,
+                CASE
+                    WHEN plan_psector_x_connec.vnode_geom IS NULL THEN vnode.the_geom
+                    ELSE plan_psector_x_connec.vnode_geom
+                END AS the_geom,
+            vnode.expl_id,
+            vnode.rotation,
+                CASE
+                    WHEN plan_psector_x_connec.link_geom IS NULL THEN false
+                    ELSE true
+                END AS ispsectorgeom,
+                CASE
+                    WHEN plan_psector_x_connec.link_geom IS NULL THEN NULL::integer
+                    ELSE plan_psector_x_connec.id
+                END AS psector_rowid
+           FROM link
+             JOIN vnode ON link.exit_id::text = vnode.vnode_id::text AND link.exit_type::text = 'VNODE'::text
+             LEFT JOIN v_state_connec ON link.feature_id::text = v_state_connec.connec_id::text
+             LEFT JOIN arc USING (arc_id)
+             JOIN sector ON sector.sector_id::text = arc.sector_id::text
+             JOIN dma ON dma.dma_id::text = arc.dma_id::text
+             LEFT JOIN plan_psector_x_connec USING (arc_id, connec_id)
+          WHERE link.feature_id::text = v_state_connec.connec_id::text) a
+  WHERE a.state < 2;
