044b33b5a70813d42b69f091b40f77803224e6ed
Barbara Rzepka <bbrzepka@bgeo.es>
2019-10-23 12:34:00 +0200

    raster_dem tables and configuration

--- dbmodel/updates/33/33008/utils/dml.sql
+++ dbmodel/updates/33/33008/utils/dml.sql
@@ -17,3 +17,34 @@ ON CONFLICT (parameter) DO NOTHING;
 UPDATE  config_param_system SET label='State type for planned elements', project_type='utils', isdeprecated=FALSE WHERE parameter='plan_statetype_planned';
 
 UPDATE value_state_type SET is_operative=false WHERE state=0;
+
+
+UPDATE audit_cat_function SET input_params='{"featureType":[]}' WHERE input_params='{"featureType":""}';
+UPDATE audit_cat_function SET input_params='{"featureType":["node"]}' WHERE input_params='{"featureType":"node"}';
+UPDATE audit_cat_function SET input_params='{"featureType":["connec"]}' WHERE input_params='{"featureType":"connec"}';
+UPDATE audit_cat_function SET input_params='{"featureType":["arc"]}' WHERE input_params='{"featureType":"arc"}';
+
+
+INSERT INTO sys_fprocess_cat(68,'Update elevation from DEM', 'edit','Update elevation from DEM','utils');
+
+INSERT INTO audit_cat_function(id, function_name, project_type, function_type, input_params,return_type, descript, sys_role_id, isdeprecated, 
+istoolbox, alias, isparametric)
+VALUES (2760, 'gw_fct_update_elevation_from_dem', 'utils', 'function','{"featureType":["node","connec","vnode"]}',
+'[{"widgetname":"exploitation", "label":"Exploitation:", "widgettype":"text", "datatype":"integer","layoutname":"grl_option_parameters","layout_order":1,"value":null},
+{"widgetname":"updateValues", "label":"Update values:", "widgettype":"combo","datatype":"text","layoutname":"grl_option_parameters","layout_order":4,
+"comboIds":["allValues","nullValues"],"comboNames":["allValues", "nullValues"],"comboNames":["Update all values","Update only null values"], "selectedId":"allValues"}]',
+'Function that updates the values of elevation using ones captured from raster DEM', 'role_edit', false, true,'Capture elevation from DEM',true);
+
+INSERT INTO config_param_system( parameter, value, data_type, context, descript, label, isenabled, project_type, isdeprecated)
+VALUES ('sys_raster_dem', 'FALSE','boolean','edit','Use raster DEM for elevation values', 'Raster DEM', false, 'utils', false);
+
+INSERT INTO audit_cat_param_user(id, formname, description, sys_role_id, label, isenabled, layout_id, layout_order, 
+project_type, isparent, isautoupdate, datatype, widgettype,ismandatory, isdeprecated)
+VALUES ('edit_upsert_elevation_from_dem','config','If true, the the elevation will be automatically inserted from the DEM raster',
+'role_edit', 'Elevation from DEM:', true, 5,7,'utils',false,false,'boolean','check',false,false);
+
+INSERT INTO edit_typevalue (typevalue, id, idval) VALUES ('raster_type', 'DEM','DEM');
+INSERT INTO edit_typevalue (typevalue, id, idval) VALUES ('raster_type', 'Slope','Slope');
+
+INSERT INTO typevalue_fk(typevalue_table, typevalue_name, target_table, target_field)
+VALUES ('edit_typevalue', 'raster_type', 'cat_raster', 'raster_type');
