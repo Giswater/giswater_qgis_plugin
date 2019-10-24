04b81e1970a2555a82a5d7d7c6a6015d95aad6ad
Edgar Fuste <efuste@bgeo.es>
2019-10-24 17:03:46 +0200

    Enhancement: new tab on mincut manager

--- dbmodel/corporate/web/ws/fct/gw_fct_getmincuts.sql
+++ dbmodel/corporate/web/ws/fct/gw_fct_getmincuts.sql
@@ -30,6 +30,7 @@ DECLARE
     v_tab1 boolean=true;
     v_tab2 boolean=false;
     v_tab3 boolean=false;
+    v_tab4 boolean=false;
     v_muni_id integer;
     v_class_id integer;
     v_workorder text;
@@ -57,22 +58,30 @@ BEGIN
             v_tab1 := false;
             v_tab2 := true;
             v_tab3 := false;
+            v_tab4 := false;
 
         ELSIF v_tab='executed' THEN 
             v_tab1 := false;
             v_tab2 := false;
             v_tab3 := true;
-
-	ELSIF v_tab='planified' THEN 
+			v_tab4 := false;
+		ELSIF v_tab='planified' THEN 
             v_tab1 := true;
             v_tab2 := false;
-	    v_tab3 := false;           
+			v_tab3 := false; 
+			v_tab4 := false;
+		ELSIF v_tab='virtual' THEN 
+            v_tab1 := false;
+            v_tab2 := false;
+			v_tab3 := false;  
+			v_tab4 := true;          
         END IF;
     
     ELSIF (SELECT id FROM anl_mincut_result_cat WHERE exec_user=current_user AND mincut_state=1 LIMIT 1) IS NOT NULL THEN
         v_tab1 := false;
         v_tab2 := true;
         v_tab3 := false;
+        v_tab4 := false;
     END IF;
 
 --  Get municipallity & class combo values
@@ -102,7 +111,7 @@ raise notice 'planified ';
 	a.id as "Mincut Id", work_order AS "Expedient", ext_municipality.name AS "Municipi", ext_streetaxis.name AS "Via", ST_X(anl_the_geom) 
 	AS sys_x, ST_Y(anl_the_geom) AS sys_y, lower(anl_feature_type) AS sys_id_name
 	FROM anl_mincut_result_cat a LEFT JOIN ext_municipality USING (muni_id) LEFT JOIN ext_streetaxis ON (streetaxis_id = ext_streetaxis.id)
-        WHERE mincut_state=0 AND ext_municipality.muni_id = ' || v_muni_id|| ' AND mincut_class = ' || v_class_id ||' ORDER BY 1 DESC';
+        WHERE mincut_state=0 AND ext_municipality.muni_id = ' || v_muni_id|| ' AND mincut_class = ' || v_class_id ||' AND virtual is False ORDER BY 1 DESC';
 
 --  mix
     IF v_workorder IS NOT NULL THEN 
@@ -166,7 +175,7 @@ raise notice 'on going ';
             ext_municipality.name AS "Municipi", ext_streetaxis.name AS "Via", ST_X(anl_the_geom) AS sys_x, ST_Y(anl_the_geom) AS sys_y, lower(anl_feature_type) AS sys_id_name, exec_user AS "Operari"
             FROM anl_mincut_result_cat a LEFT JOIN ext_municipality USING (muni_id) LEFT JOIN ext_streetaxis ON (streetaxis_id = ext_streetaxis.id)
             LEFT JOIN (VALUES (current_user, 1)) AS x(value, order_number) ON a.exec_user = x.value
-            WHERE mincut_state=1 AND ext_municipality.muni_id = ' || v_muni_id|| ' AND mincut_class = ' || v_class_id||' ORDER BY x.order_number, 2 desc';
+            WHERE mincut_state=1 AND ext_municipality.muni_id = ' || v_muni_id|| ' AND mincut_class = ' || v_class_id||' AND virtual is False ORDER BY x.order_number, 2 desc';
      
 --  mix
     IF v_workorder IS NOT NULL THEN 
@@ -226,7 +235,7 @@ raise notice 'executed ';
 	    exec_start::date as "Data execució", a.id as "Mincut Id", work_order AS "Expedient", 
             ext_municipality.name AS "Municipi", ext_streetaxis.name AS "Via", ST_X(anl_the_geom) AS sys_x, ST_Y(anl_the_geom) AS sys_y, lower(anl_feature_type) AS sys_id_name
             FROM anl_mincut_result_cat a LEFT JOIN ext_municipality USING (muni_id) LEFT JOIN ext_streetaxis ON (streetaxis_id = ext_streetaxis.id)
-            WHERE mincut_state=2 AND ext_municipality.muni_id = ' || v_muni_id|| ' AND mincut_class = ' || v_class_id ||' ORDER BY 1 DESC';
+            WHERE mincut_state=2 AND ext_municipality.muni_id = ' || v_muni_id|| ' AND mincut_class = ' || v_class_id ||' AND virtual is False ORDER BY 1 DESC';
 
 
 --  mix
@@ -270,8 +279,64 @@ raise notice 'executed ';
     tabAux := gw_fct_json_object_set_key(tabAux, 'fields', array_to_json(json_array[0:4]));
     formTabs := formTabs || ',' || tabAux::text;
 
-    
-    
+
+-- VIRTUALS TAB
+-------------------------------
+
+raise notice 'Virtuals TAB ';
+
+
+--  Get query for mincut
+    query_result := 'SELECT a.id AS sys_id, 
+       (CASE WHEN work_order IS NULL THEN concat (a.id::text,''-'',ext_streetaxis.name) ELSE concat (a.id::text,''-'',a.work_order,''-'',ext_streetaxis.name) END) as sys_search, 
+        forecast_start::date as "Data planificada", 
+	a.id as "Mincut Id", work_order AS "Expedient", ext_municipality.name AS "Municipi", ext_streetaxis.name AS "Via", ST_X(anl_the_geom) 
+	AS sys_x, ST_Y(anl_the_geom) AS sys_y, lower(anl_feature_type) AS sys_id_name
+	FROM anl_mincut_result_cat a LEFT JOIN ext_municipality USING (muni_id) LEFT JOIN ext_streetaxis ON (streetaxis_id = ext_streetaxis.id)
+        WHERE mincut_state=0 AND ext_municipality.muni_id = ' || v_muni_id|| ' AND mincut_class = ' || v_class_id ||' AND virtual is True ORDER BY 1 DESC';
+
+--  mix
+    IF v_workorder IS NOT NULL THEN 
+        query_result := 'SELECT * FROM ('||(query_result)||')a WHERE sys_search= '||quote_literal(v_workorder);
+    END IF;   
+
+raise notice 'query_result %', query_result;
+
+
+-- execute
+    EXECUTE 'SELECT array_to_json(array_agg(row_to_json(a))) FROM (' || (query_result) || ') a'
+        INTO query_result_mincuts;
+
+raise notice 'query_result_mincuts %', query_result_mincuts;
+
+-- Control NULL's
+   query_result_mincuts := COALESCE(query_result_mincuts, '{}');
+
+     
+    -- Form controls
+    -- create municipality combo
+    SELECT * INTO rec_fields FROM config_web_fields WHERE table_id='F45' AND name='mincut_muni';
+    json_array[0] := gw_fct_createcombojson(rec_fields.label, rec_fields.name, 'combo', 'string', '', FALSE, 'ext_municipality','muni_id','name',v_muni_id::text);
+
+    -- create class combo
+    SELECT * INTO rec_fields FROM config_web_fields WHERE table_id='F45' AND name='mincut_class';
+    json_array[1] := gw_fct_createcombojson(rec_fields.label, rec_fields.name, 'combo', 'string', '', FALSE, 'anl_mincut_cat_class','id','name',v_class_id::text);
+
+    -- Create mix search field
+    SELECT * INTO rec_fields FROM config_web_fields WHERE table_id='F45' AND name='mincut_workorder';
+    json_array[2] := json_build_object('label',rec_fields.label, 'name', 'mincut_workorder', 'type','typeahead','dataType','string','placeholder','','disabled',false,'noresultsMsg','No results','loadingMsg','Searching...');
+
+    -- Create widget fivider
+    json_array[3] := gw_fct_createwidgetjson(NULL, 'divider', 'formDivider', 'string', NULL, TRUE, NULL);
+
+   -- Create array of results
+   json_array[4] := json_build_object('label', 'Llistat talls planificats', 'active', 1, 'name', 'talls', 'type', 'list', 'dataType', 'string','disabled', TRUE, 'value', query_result_mincuts);
+
+   -- State tab
+   tabAux := json_build_object('tabName','virtual','tabLabel','Virtuals','tabText','','active',v_tab4);
+   tabAux := gw_fct_json_object_set_key(tabAux, 'fields', array_to_json(json_array[0:4]));
+   formTabs := formTabs || ',' || tabAux::text;
+
 --Finish the construction of formtabs
 -------------------------------------
 
