8098fce04107e6e23b12cdd155fce326876c9abb
David Erill <daviderill79@gmail.com>
2019-10-29 17:24:04 +0100

    Execute external synchronization python script

--- actions/crm_trace.py
+++ actions/crm_trace.py
@@ -7,6 +7,8 @@ or (at your option) any later version.
 # -*- coding: utf-8 -*-
 from qgis.PyQt.QtWidgets import QRadioButton, QPushButton
 
+import os
+import subprocess
 from .. import utils_giswater
 from ..ui_manager import DlgTrace
 from functools import partial
@@ -35,7 +37,7 @@ class CrmTrace(ApiParent):
         self.dlg_trace.rejected.connect(partial(self.save_settings, self.dlg_trace))
 
         # Fill combo 'exploitation'
-        sql = "SELECT expl_id, name FROM exploitation ORDER BY name"
+        sql = "SELECT name FROM exploitation ORDER BY name"
         rows = self.controller.get_rows(sql, log_sql=True)
         utils_giswater.fillComboBox(self.dlg_trace, 'cbo_expl', rows)
 
@@ -47,6 +49,40 @@ class CrmTrace(ApiParent):
         """ Main process """
 
         # Get selected 'exploitation'
-        expl = utils_giswater.getWidgetText(self.dlg_trace, 'cbo_expl')
-        self.controller.log_info(str(expl))
+        expl_name = utils_giswater.getWidgetText(self.dlg_trace, 'cbo_expl')
+        self.controller.log_info(str(expl_name))
+
+        # Execute synchronization script
+        self.execute_script(expl_name)
+
+
+    def execute_script(self, expl_name='ZA'):
+        """ Execute synchronization script """
+
+        self.controller.log_info("execute_script")
+
+        if expl_name is None:
+            self.controller.show_warning("Any exploitation selected")
+            return
+
+        # Get python synchronization script path
+        try:
+            script_path = self.controller.cfgp_user['guk_script_path'].value
+        except KeyError as e:
+            self.controller.log_warning(str(e))
+            return
+
+        # Check if script path exists
+        if not os.path.exists(script_path):
+            self.controller.log_warning("File not found", parameter=script_path)
+            return
+
+        # Execute script
+        args = ['python', script_path, expl_name]
+        self.controller.log_info(str(args))
+        try:
+            status = subprocess.call(args)
+            self.controller.log_info(status)
+        except Exception as e:
+            self.controller.log_warning(str(e))
 
