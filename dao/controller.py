02388dc30d13df9136e7bb0f91ea489176b444d0
Néstor Ibáñez <nestor@bgeo.es>
2019-11-06 10:29:51 +0100

    refactor functions

--- dao/controller.py
+++ dao/controller.py
@@ -1374,15 +1374,15 @@ class DaoController(object):
             self.cfgp_user[row['parameter']] = CfgUser(row['parameter'], row['value'])
 
 
-    def get_value_from_cfgp_user(self, key):
-        value = None
+    def get_obj_from_cfgp_user(self, key):
+        obj = None
         try:
-            value = self.cfgp_user[key].value
+            obj = self.cfgp_user[key]
         except KeyError as e:
             print(f"{type(e).__name__} --> {e}")
             pass
         finally:
-            return value
+            return obj
 
     def get_config_param_system(self):
         """ Populate dictionary with table config_param_system """
@@ -1394,15 +1394,15 @@ class DaoController(object):
         for row in rows:
             self.cfgp_system[row['parameter']] = CfgSystem(row['parameter'], row['value'], row['data_type'], row['context'], row['descript'], row['label'])
 
-    def get_value_from_cfgp_system(self, key):
-        value = None
+    def get_obj_from_cfgp_system(self, key):
+        obj = None
         try:
-            value = self.cfgp_system[key].value
+            obj = self.cfgp_system[key]
         except KeyError as e:
             print(f"{type(e).__name__} --> {e}")
             pass
         finally:
-            return value
+            return obj
 
     def indexing_spatial_layer(self, layer_name):
         """ Force reload dataProvider of layer """
