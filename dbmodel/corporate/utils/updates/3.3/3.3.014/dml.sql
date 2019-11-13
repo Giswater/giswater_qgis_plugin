dd7211db25d7f049d12fd26c93dab16bd51a3164
Barbara Rzepka <bbrzepka@bgeo.es>
2019-11-13 14:26:46 +0100

    create raster tables in schema utils

---
+++ dbmodel/corporate/utils/updates/3.3/3.3.014/dml.sql
@@ -0,0 +1,14 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = "utils", public, pg_catalog;
+
+
+UPDATE audit_cat_table SET context='view from external schema'  WHERE id IN ('ext_cat_raster','ext_raster_dem');
+
+
+
