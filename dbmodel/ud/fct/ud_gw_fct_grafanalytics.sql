3ba1e1733c4dc50528a4c698fa2984ce97a7a081
Xavier Torret <xtorret@bgeo.es>
2019-11-15 19:32:27 +0100

    Revert "Reverse line"
    This reverts commit a3ccec7fe51e4d5e6caf14956b2e227dd4ce61a8.

--- dbmodel/ud/fct/ud_gw_fct_grafanalytics.sql
+++ dbmodel/ud/fct/ud_gw_fct_grafanalytics.sql
@@ -14,7 +14,7 @@ $BODY$
 
 /*
 --EXAMPLE
-SELECT gw_fct_grafanalytics('{"data":{"parameters":{"node":"5100"}}}');
+SELECT SCHEMA_NAME.gw_fct_grafanalytics('{"data":{"parameters":{"node":"5100"}}}');
 
 vacuum analyze
 
