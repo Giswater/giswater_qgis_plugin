5e5dd4cd2b3fcb97146a8a931cc7d2251cd610c9
Barbara <bbrzepka@bgeo.es>
2019-12-05 15:05:38 +0100

    Modify cat_manager trg - allow assign same expl to many managers

--- dbmodel/utils/ftrg/gw_trg_cat_manager.sql
+++ dbmodel/utils/ftrg/gw_trg_cat_manager.sql
@@ -7,40 +7,120 @@ This version of Giswater is provided by Giswater Association
 --FUNCTION CODE: 2754
 
 
-CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_cat_manager() 
-RETURNS trigger LANGUAGE plpgsql AS $$
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_trg_cat_manager()
+  RETURNS trigger AS
+$BODY$
 DECLARE 
  
- 
+ rec_expl integer;
+ rec_user text;
+ v_new_id integer;
 BEGIN 
 
 	EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
 
 	-- Delete orphan nodes
-	IF TG_OP = 'INSERT' OR (TG_OP = 'UPDATE' AND (NEW.expl_id::text != OLD.expl_id::text OR NEW.username::text != OLD.username::text )) THEN
-
+	IF (TG_OP = 'INSERT' AND  NEW.username !='{}') THEN
 
 		--Upsert values on exploitation_x_user according exploitation_x_user info
-		DELETE FROM exploitation_x_user WHERE manager_id=NEW.id;
 		INSERT INTO exploitation_x_user (expl_id, username, manager_id)
 		SELECT expl, usern, NEW.id  FROM (SELECT unnest(expl_id) expl FROM cat_manager WHERE id=NEW.id) p CROSS JOIN 
-		(SELECT unnest(username) usern FROM cat_manager WHERE id=NEW.id) q;
+		(SELECT unnest(username) usern FROM cat_manager WHERE id=NEW.id) q ON CONFLICT (expl_id,username) DO NOTHING;
+			
+		RETURN NEW;
+		
+	ELSIF TG_OP = 'UPDATE'THEN
+		
+		--if username was empty insert exploitation_x_user or combinations for this manager 
+		IF OLD.username ='{}' THEN
+
+			INSERT INTO exploitation_x_user (expl_id, username, manager_id)
+			SELECT expl, usern, NEW.id  FROM (SELECT unnest(expl_id) expl FROM cat_manager WHERE id=NEW.id) p CROSS JOIN 
+			(SELECT unnest(username) usern FROM cat_manager WHERE id=NEW.id) q ON CONFLICT (expl_id,username) DO NOTHING;
+		
+		--if the user was removed from the cat_manager, replace assignation	if exists the same relation user - expl for another manager
+		ELSIF array_length(NEW.username,1) < array_length(OLD.username,1) or NEW.username ='{}'  THEN 
+		
+			FOREACH rec_user IN ARRAY OLD.username LOOP
+				
+				FOREACH rec_expl IN ARRAY OLD.expl_id LOOP
+				
+					IF (SELECT count(id) FROM cat_manager WHERE rec_user = ANY(username) AND rec_expl::integer = ANY(expl_id))>0 THEN
+					
+						SELECT id INTO v_new_id FROM cat_manager WHERE rec_user = ANY(username) AND rec_expl::integer = ANY(expl_id);
+						
+						--if there is a different manager with the same expl assigned change the manager_id and maintain relation
+						--if not, remove it	
+						IF (SELECT count(id) FROM exploitation_x_user WHERE username=rec_user and expl_id=rec_expl) > 0 THEN
+
+							UPDATE exploitation_x_user set manager_id = v_new_id WHERE username=rec_user and expl_id=rec_expl;
+						END IF;
+					ELSE
+						DELETE FROM exploitation_x_user WHERE rec_user=username and expl_id=rec_expl and manager_id=NEW.id;
+					END IF;
+				END LOOP;
+			END LOOP;
+		--if user was added insetr new relation into exploitation_x_user
+		ELSIF array_length(NEW.username,1) > array_length(OLD.username,1) then 
+			INSERT INTO exploitation_x_user (expl_id, username, manager_id)
+			SELECT expl, usern, NEW.id  FROM (SELECT unnest(expl_id) expl FROM cat_manager WHERE id=NEW.id) p CROSS JOIN 
+			(SELECT unnest(username) usern FROM cat_manager WHERE id=NEW.id) q ON CONFLICT (expl_id,username) DO NOTHING;
+		END IF;
+		
+		--if the expl was removed from the cat_manager, replace assignation if exists the same relation user - expl for another manager
+		IF array_length(NEW.expl_id,1) < array_length(OLD.expl_id,1) THEN
+	
+			FOREACH rec_expl IN ARRAY OLD.expl_id LOOP
+	
+				FOREACH rec_user IN ARRAY OLD.username LOOP
+				
+					IF (SELECT count(id) FROM cat_manager WHERE rec_user = ANY(username) AND rec_expl::integer = ANY(expl_id))>0 THEN
+					
+						SELECT id INTO v_new_id FROM cat_manager WHERE rec_user = ANY(username) AND rec_expl::integer = ANY(expl_id);
+							
+						IF (SELECT count(id) FROM exploitation_x_user WHERE username=rec_user and expl_id=rec_expl) > 0 THEN
+							UPDATE exploitation_x_user set manager_id = v_new_id WHERE username=rec_user and expl_id=rec_expl;
+						END IF;
+					ELSE
+						DELETE FROM exploitation_x_user WHERE rec_user=username and expl_id=rec_expl and manager_id=NEW.id;
+					END IF;
+				END LOOP;
+			END LOOP;
+
+		
+		--if exploitation was added insert new posible relations into exploitation_x_user
+		ELSIF array_length(NEW.expl_id,1) > array_length(OLD.expl_id,1) THEN 
+			INSERT INTO exploitation_x_user (expl_id, username, manager_id)
+			SELECT expl, usern, NEW.id  FROM (SELECT unnest(expl_id) expl FROM cat_manager WHERE id=NEW.id) p CROSS JOIN 
+			(SELECT unnest(username) usern FROM cat_manager WHERE id=NEW.id) q ON CONFLICT (expl_id,username) DO NOTHING;
+		END IF;
 
 		RETURN NEW;
-	
-	ELSIF TG_OP = 'DELETE' THEN
-
-		--Upsert values on exploitation_x_user according exploitation_x_user info
-		DELETE FROM exploitation_x_user WHERE manager_id=OLD.id;
-		INSERT INTO exploitation_x_user (expl_id, username, manager_id)
-		SELECT expl, usern, OLD.id  FROM (SELECT unnest(expl_id) expl FROM cat_manager WHERE id=OLD.id) p CROSS JOIN 
-		(SELECT reverse(substring(reverse(substring((unnest(username)) from 2)),2)) usern FROM cat_manager WHERE id=OLD.id) q;
 		
+	ELSIF TG_OP = 'DELETE' THEN
+		--if the manager was removed from the cat_manager, replace assignation if exists the same relation user - expl for another manager
+		FOREACH rec_expl IN ARRAY OLD.expl_id LOOP
+		
+			FOREACH rec_user IN ARRAY OLD.username LOOP
+				IF (SELECT count(id) FROM cat_manager WHERE rec_user = ANY(username) AND rec_expl::integer = ANY(expl_id))>0 THEN
+					
+					SELECT id INTO v_new_id FROM cat_manager WHERE rec_user = ANY(username) AND rec_expl::integer = ANY(expl_id);
+							
+					INSERT INTO exploitation_x_user  (expl_id, username, manager_id)
+					VALUES (rec_expl, rec_user, v_new_id) ON CONFLICT (expl_id, username) DO NOTHING;
+					
+					END IF;
+				END LOOP;
+			END LOOP;
+			
 		RETURN OLD;
 
 	END IF;
 
-	RETURN NEW;
 
 END;
-$$;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION gw_trg_cat_manager()
+  OWNER TO postgres;
