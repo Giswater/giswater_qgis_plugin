0903be1529f173daf40bc80f74efc2883b43b4b5
Barbara <bbrzepka@bgeo.es>
2019-12-11 12:25:56 +0100

    bug fix on audit_cat_function return type for gw_fct_grafanalytics_mapzones_advanced

--- dbmodel/updates/33/33019/utils/dml.sql
+++ dbmodel/updates/33/33019/utils/dml.sql
@@ -14,6 +14,16 @@ false, false, null, false) ON CONFLICT (id) DO NOTHING;
 
 UPDATE audit_cat_function SET istoolbox=false, isparametric=false, alias=null WHERE id=2774;
 
+--11/12/2019
 INSERT INTO sys_fprocess_cat(id, fprocess_name, context, fprocess_i18n, project_type)
 VALUES (106, 'Manage dxf file', 'Edit','Manage dxf file','utils')
 ON CONFLICT (id) DO NOTHING;
+
+UPDATE audit_cat_function SET return_type = '[{"widgetname":"grafClass", "label":"Graf class:", "widgettype":"combo","datatype":"text","layoutname":"grl_option_parameters","layout_order":1,"comboIds":["PRESSZONE","DQA","DMA","SECTOR"],
+"comboNames":["Pressure Zonification (PRESSZONE)", "District Quality Areas (DQA) ", "District Metering Areas (DMA)", "Inlet Sectorization (SECTOR-HIGH / SECTOR-LOW)"], "selectedId":"DMA"}, 
+{"widgetname":"exploitation", "label":"Exploitation id''s:","widgettype":"text","datatype":"json","layoutname":"grl_option_parameters","layout_order":2, "placeholder":"[1,2]", "value":""},
+{"widgetname":"updateFeature", "label":"Update feature attributes:","widgettype":"check","datatype":"boolean","layoutname":"grl_option_parameters","layout_order":7, "value":"FALSE"},
+{"widgetname":"updateMapZone", "label":"Update geometry (if true choose only one parameter belove)","widgettype":"check","datatype":"boolean","layoutname":"grl_option_parameters","layout_order":8, "value":"FALSE"},
+{"widgetname":"buffer", "label":"1: Buffer for arc disolve approach:","widgettype":"text","datatype":"float","layoutname":"grl_option_parameters","layout_order":9, "ismandatory":false, "placeholder":"10", "value":""},
+{"widgetname":"concaveHullParam", "label":"2: Hull parameter for concave polygon approach:","widgettype":"text","datatype":"float","layoutname":"grl_option_parameters","layout_order":9, "ismandatory":false, "placeholder":"0.9", "value":""}]' 
+WHERE function_name ='gw_fct_grafanalytics_mapzones_advanced';
