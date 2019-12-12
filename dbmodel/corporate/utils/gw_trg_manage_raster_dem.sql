cf39e84189410e1cf25cc00b020bde5d01f66f4a
Xavier Torret <xtorret@bgeo.es>
2019-12-12 20:16:47 +0100

    Create trigger to manage raster catalog in case of corporate / utils

---
+++ dbmodel/corporate/utils/gw_trg_manage_raster_dem.sql
@@ -0,0 +1,44 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+--FUNCTION CODE: xxxx
+
+CREATE OR REPLACE FUNCTION "utils".gw_trg_manage_raster_dem()  RETURNS trigger AS
+$BODY$
+
+DECLARE 
+
+
+BEGIN
+
+    EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
+	
+    IF TG_OP = 'INSERT' THEN
+	
+		INSERT INTO cat_raster (id, raster_type, tstamp, insert_user) VALUES (NEW.rastercat_id, 'DEM', now(), current_user)
+		ON CONFLICT (id) DO NOTHING;
+		
+		
+		RETURN NEW;
+				
+    ELSIF TG_OP = 'DELETE' THEN  
+
+		-- only if it's last row for that raster
+		IF (SELECT count(*) FROM raster_dem WHERE rastercat_id=OLD.rastercat_id) = 0 THEN
+	 		DELETE FROM cat_raster WHERE id = OLD.rastercat_id;		
+	 	END IF;
+	 	
+		RETURN NULL;
+     
+    END IF;
+
+END;
+	
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+
