d27f21854291a9a84c7cdb31990bb5ace35ca5dc
Albert Bofill <abofill@bgeo.es>
2019-12-13 11:59:10 +0100

    #720 Add *_type to catalog tables for UD and functionality in config_api_form_fields

--- dbmodel/api/updates/33/33019/ud/dml.sql
+++ dbmodel/api/updates/33/33019/ud/dml.sql
@@ -11,3 +11,20 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 UPDATE config_api_form_fields SET widgettype='typeahead',
 typeahead='{"fieldToSearch": "id", "threshold": 3, "noresultsMsg": "No results", "loadingMsg": "Searching"}'
 WHERE column_id in ('nodecat_id', 'arccat_id', 'connecat_id');
+
+
+UPDATE config_api_form_fields SET isparent=TRUE WHERE column_id='node_type' AND formtype='feature';
+UPDATE config_api_form_fields SET iseditable=FALSE WHERE column_id='node_type' AND formtype='feature';
+UPDATE config_api_form_fields SET dv_parent_id='node_type', dv_querytext_filterc=' AND cat_node.node_type IS NULL OR cat_node.node_type=' WHERE column_id='nodecat_id' AND formtype='feature';
+
+UPDATE config_api_form_fields SET isparent=TRUE WHERE column_id='arc_type' AND formtype='feature';
+UPDATE config_api_form_fields SET iseditable=FALSE WHERE column_id='arc_type' AND formtype='feature';
+UPDATE config_api_form_fields SET dv_parent_id='arc_type', dv_querytext_filterc=' AND cat_arc.arc_type IS NULL OR cat_arc.arc_type=' WHERE column_id='arccat_id' AND formtype='feature';
+
+UPDATE config_api_form_fields SET isparent=TRUE WHERE column_id='connec_type' AND formtype='feature';
+UPDATE config_api_form_fields SET iseditable=FALSE WHERE column_id='connec_type' AND formtype='feature';
+UPDATE config_api_form_fields SET dv_parent_id='connec_type', dv_querytext_filterc=' AND cat_connec.connec_type IS NULL OR cat_connec.connec_type=' WHERE column_id='connecat_id' AND formtype='feature';
+
+UPDATE config_api_form_fields SET isparent=TRUE WHERE column_id='gully_type' AND formtype='feature';
+UPDATE config_api_form_fields SET iseditable=FALSE WHERE column_id='gully_type' AND formtype='feature';
+UPDATE config_api_form_fields SET dv_parent_id='gully_type', dv_querytext_filterc=' AND cat_grate.gully_type IS NULL OR cat_grate.gully_type=' WHERE column_id='gratecat_id' AND formtype='feature';
