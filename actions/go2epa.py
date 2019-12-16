73c588d4de2bcc69f62c7d80f4da1f6a951be95f
Néstor Ibáñez <nestor@bgeo.es>
2019-12-16 14:12:50 +0100

    reset text for info_log

--- actions/go2epa.py
+++ actions/go2epa.py
@@ -678,7 +678,7 @@ class Go2Epa(ApiParent):
             # Export to inp file
             if export_inp is True:
                 if complet_result[0]['status'] == "Accepted":
-                    self.add_temp_layer(self.dlg_go2epa, complet_result[0]['body']['data'], 'INP results', True, False, 1, False)
+                    self.add_temp_layer(self.dlg_go2epa, complet_result[0]['body']['data'], 'INP results', True, True, 1, False)
                 message = complet_result[0]['message']['text']
 
                 # Get values from temp_csv2pg and insert into INP file
@@ -758,7 +758,7 @@ class Go2Epa(ApiParent):
                             if rpt_result[0]['status'] == "Accepted":
                                 if 'body' in rpt_result[0]:
                                     if 'data' in rpt_result[0]['body']:
-                                        self.add_temp_layer(self.dlg_go2epa, rpt_result[0]['body']['data'], 'RPT results', True, False, 1, False)
+                                        self.add_temp_layer(self.dlg_go2epa, rpt_result[0]['body']['data'], 'RPT results', True, True, 1, False)
                         message = rpt_result[0]['message']['text']
 
                     # final message
