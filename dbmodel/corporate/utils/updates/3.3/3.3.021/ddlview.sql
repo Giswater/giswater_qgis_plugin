38dd7f9fd83dcbec125d6e01e5ee678dd7a10c3f
LAPTOP-599OP0T2\user <xtorret@bgeo.es>
2019-12-23 22:58:30 +0100

    Enhance ext_raster view

--- dbmodel/corporate/utils/updates/3.3/3.3.021/ddlview.sql
+++ dbmodel/corporate/utils/updates/3.3/3.3.021/ddlview.sql
@@ -8,11 +8,11 @@ This version of Giswater is provided by Giswater Association
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
 -- 2019/12/21
-CREATE OR REPLACE VIEW ws_test.ext_raster_dem AS 
- SELECT raster_dem.id,
-   raster_dem.rast,
-   raster_dem.rastercat_id,
-   raster_dem.envelope
-   FROM utils.raster_dem, ws_test.exploitation a, ws_test.selector_expl b
-   WHERE st_dwithin (envelope, the_geom, 0)
-   AND a.expl_id=b.expl_id AND cur_user=current_user;
+SELECT raster_dem.id,
+    raster_dem.rast,
+    a.expl_id,
+    raster_dem.rastercat_id,
+    raster_dem.envelope
+   FROM utils.raster_dem,
+    ws_test.exploitation a
+  WHERE st_dwithin(raster_dem.envelope, a.the_geom, 0::double precision);
