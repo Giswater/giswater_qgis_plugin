77bb27c55bd9bbf6ecf1cdaced816c5bb080db44
LAPTOP-599OP0T2\user <xtorret@bgeo.es>
2019-12-23 23:01:23 +0100

    Comment on tables for deprecated fields when it is not possible to delete it (because is not end field on system table)

--- dbmodel/updates/33/33021/ws/ddl.sql
+++ dbmodel/updates/33/33021/ws/ddl.sql
@@ -7,8 +7,9 @@ This version of Giswater is provided by Giswater Association
 
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
--- 2019/12/21
-SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"ext_raster_dem", "column":"envelope", "dataType":"geometry (polygon, SRID_VALUE)","isUtils":"True"}}$$);
-
 -- 2019/12/23
-COMMENT ON TABLE sys_fprocess_cat IS 'FIELD fprocess_i18n IS DEPRECATED';
+COMMENT ON TABLE sector IS 'FIELD nodeparent IS DEPRECATED';
+COMMENT ON TABLE dma IS 'FIELD nodeparent IS DEPRECATED';
+COMMENT ON TABLE dqa IS 'FIELD nodeparent IS DEPRECATED';
+COMMENT ON TABLE cat_presszone IS 'FIELD nodeparent IS DEPRECATED';
+COMMENT ON TABLE anl_mincut_x_exploitation IS 'FIELD toarc IS DEPRECATED';
