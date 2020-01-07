b72c3031724c79fa1ddd8be60eb7d623b433318d
Barbara <bbrzepka@bgeo.es>
2020-01-07 12:21:43 +0100

    refactor variables fct_cad_add_relative_point

--- dbmodel/utils/fct/gw_fct_cad_add_relative_point.sql
+++ dbmodel/utils/fct/gw_fct_cad_add_relative_point.sql
@@ -22,20 +22,19 @@ CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_cad_add_relative_point(
   $BODY$
 
 DECLARE
-geom_aux geometry;
-percent_aux float;
-point_aux geometry;
-point1_aux geometry;
-point2_aux geometry;
-point_result geometry;
-angle_aux float;
-xcoord float;
-ycoord float;
+v_geom geometry;
+v_percent float;
+v_point geometry;
+v_point1 geometry;
+v_point2 geometry;
+v_point_result geometry;
+v_angle float;
+v_xcoord float;
+v_ycoord float;
 rec record;
-coords_arr float[];
-angle0_aux float;
-x0coord float;
-y0coord float;
+v_angle0 float;
+v_x0coord float;
+v_y0coord float;
 
 BEGIN
 
@@ -45,60 +44,60 @@ BEGIN
     -- Initialize variables	
     SELECT * into rec FROM version;
 
-    geom_aux= st_makeline(geom1_aux, geom2_aux);
+    v_geom= st_makeline(geom1_aux, geom2_aux);
 	
 	--Check init node to place the support point
 	IF start_point=2 THEN
-		x_var=x_var+st_length(geom_aux);
+		x_var=x_var+st_length(v_geom);
 	END IF;
 	
 	--Check the position of the support point
     IF x_var <= 0 THEN
     	-- azimut calculation
-		SELECT ST_LineInterpolatePoint(geom_aux, 0.000) into point1_aux;
-		SELECT ST_LineInterpolatePoint(geom_aux, 0.001) into point2_aux;
-		angle0_aux:=(st_azimuth(point1_aux,point2_aux));
-		angle_aux:=(st_azimuth(point1_aux,point2_aux)-3.14159/2);
+		SELECT ST_LineInterpolatePoint(v_geom, 0.000) into v_point1;
+		SELECT ST_LineInterpolatePoint(v_geom, 0.001) into v_point2;
+		v_angle0:=(st_azimuth(v_point1,v_point2));
+		v_angle:=(st_azimuth(v_point1,v_point2)-3.14159/2);
 
 		-- point coordinates calculation
-		x0coord = ST_x(point1_aux)+(sin(angle0_aux))*x_var::float;
-		y0coord = ST_y(point1_aux)+(cos(angle0_aux))*x_var::float;
-		xcoord = x0coord+(sin(angle_aux))*y_var::float;
-		ycoord = y0coord+(cos(angle_aux))*y_var::float;
+		v_x0coord = ST_x(v_point1)+(sin(v_angle0))*x_var::float;
+		v_y0coord = ST_y(v_point1)+(cos(v_angle0))*x_var::float;
+		v_xcoord = v_x0coord+(sin(v_angle))*y_var::float;
+		v_ycoord = v_y0coord+(cos(v_angle))*y_var::float;
 
-    ELSIF (x_var >0 AND x_var < st_length(geom_aux)) THEN
+    ELSIF (x_var >0 AND x_var < st_length(v_geom)) THEN
 	
 		-- percent calculation
-		percent_aux= x_var/st_length(geom_aux);
+		v_percent= x_var/st_length(v_geom);
 
 		-- azimut calculation
-		SELECT ST_LineInterpolatePoint(geom_aux, (percent_aux-0.001)) into point1_aux;
-		SELECT ST_LineInterpolatePoint(geom_aux, (percent_aux+0.001)) into point2_aux;	
-		angle_aux:=(st_azimuth(point1_aux,point2_aux)-3.14159/2);
+		SELECT ST_LineInterpolatePoint(v_geom, (v_percent-0.001)) into v_point1;
+		SELECT ST_LineInterpolatePoint(v_geom, (v_percent+0.001)) into v_point2;	
+		v_angle:=(st_azimuth(v_point1,v_point2)-3.14159/2);
 
 		-- point coordinates calculation
-		SELECT ST_LineInterpolatePoint(geom_aux, percent_aux) into point_aux;
-		xcoord = ST_x(point_aux)+(sin(angle_aux))*y_var::float;
-		ycoord = ST_y(point_aux)+(cos(angle_aux))*y_var::float;
+		SELECT ST_LineInterpolatePoint(v_geom, v_percent) into v_point;
+		v_xcoord = ST_x(v_point)+(sin(v_angle))*y_var::float;
+		v_ycoord = ST_y(v_point)+(cos(v_angle))*y_var::float;
 
-    ELSIF x_var >= st_length(geom_aux) THEN
+    ELSIF x_var >= st_length(v_geom) THEN
         
         -- azimut calculation
-		SELECT ST_LineInterpolatePoint(geom_aux, 0) into point1_aux;
-		SELECT ST_LineInterpolatePoint(geom_aux, 1) into point2_aux;
-		angle0_aux:=(st_azimuth(point1_aux,point2_aux));
-		angle_aux:=(st_azimuth(point1_aux,point2_aux)-3.14159/2);
+		SELECT ST_LineInterpolatePoint(v_geom, 0) into v_point1;
+		SELECT ST_LineInterpolatePoint(v_geom, 1) into v_point2;
+		v_angle0:=(st_azimuth(v_point1,v_point2));
+		v_angle:=(st_azimuth(v_point1,v_point2)-3.14159/2);
 
 		-- point coordinates calculation
-		x0coord = ST_x(point1_aux)+(sin(angle0_aux))*x_var::float;
-		y0coord = ST_y(point1_aux)+(cos(angle0_aux))*x_var::float;
-		xcoord = x0coord+(sin(angle_aux))*y_var::float;
-		ycoord = y0coord+(cos(angle_aux))*y_var::float;
+		v_x0coord = ST_x(v_point1)+(sin(v_angle0))*x_var::float;
+		v_y0coord = ST_y(v_point1)+(cos(v_angle0))*x_var::float;
+		v_xcoord = v_x0coord+(sin(v_angle))*y_var::float;
+		v_ycoord = v_y0coord+(cos(v_angle))*y_var::float;
 
     
     END IF;
 
-    point_result = ST_SetSRID(ST_MakePoint(xcoord, ycoord),rec.epsg);
+    v_point_result = ST_SetSRID(ST_MakePoint(v_xcoord, v_ycoord),rec.epsg);
 
     -- delete previous registers if user selection is enabled
     IF del_previous_bool IS TRUE THEN 
@@ -106,10 +105,10 @@ BEGIN
     END IF;
     
     -- Insert into temporal table the values
-    INSERT INTO temp_table (fprocesscat_id, geom_point)  VALUES (27, point_result);
+    INSERT INTO temp_table (fprocesscat_id, geom_point)  VALUES (27, v_point_result);
 
 
-RETURN point_result;
+RETURN v_point_result;
         
 END;$BODY$
   LANGUAGE plpgsql VOLATILE
