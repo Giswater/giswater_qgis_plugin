a4793e5b614577949e1d103eab42187060936442
Barbara <bbrzepka@bgeo.es>
2020-01-08 15:09:34 +0100

    fix trg creation

--- dbmodel/updates/33/33022/utils/trg.sql
+++ dbmodel/updates/33/33022/utils/trg.sql
@@ -7,17 +7,17 @@ This version of Giswater is provided by Giswater Association
 
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
-DROP TRIGGER IF EXISTS SCHEMA_NAME.gw_trg_edit_address ON v_ext_address;
-CREATE TRIGGER SCHEMA_NAME.gw_trg_edit_address INSTEAD OF INSERT OR UPDATE OR DELETE
+DROP TRIGGER IF EXISTS gw_trg_edit_address ON v_ext_address;
+CREATE TRIGGER gw_trg_edit_address INSTEAD OF INSERT OR UPDATE OR DELETE
 ON v_ext_address FOR EACH ROW
 EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_edit_address();
 
-DROP TRIGGER IF EXISTS SCHEMA_NAME.gw_trg_edit_plot ON v_ext_address;
+DROP TRIGGER IF EXISTS gw_trg_edit_plot ON v_ext_plot;
 CREATE TRIGGER gw_trg_edit_plot INSTEAD OF INSERT OR UPDATE OR DELETE
 ON SCHEMA_NAME.v_ext_plot
 FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_edit_plot();
 
-DROP TRIGGER IF EXISTS SCHEMA_NAME.gw_trg_edit_streetaxis ON v_ext_address;
+DROP TRIGGER IF EXISTS gw_trg_edit_streetaxis ON v_ext_streetaxis;
 CREATE TRIGGER gw_trg_edit_streetaxis INSTEAD OF INSERT OR UPDATE OR DELETE
 ON SCHEMA_NAME.v_ext_streetaxis
 FOR EACH ROW EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_edit_streetaxis();
