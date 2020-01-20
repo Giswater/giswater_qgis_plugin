617919b18e28e5e9c253baeaae839b16c1f7043a
Néstor Ibáñez <nestor@bgeo.es>
2020-01-20 13:37:42 +0100

    bug fix(control is new)

--- actions/mincut.py
+++ actions/mincut.py
@@ -204,13 +204,12 @@ class MincutParent(ParentAction, MultipleSelection):
         sql = ("SELECT setval('" + self.schema_name + ".anl_mincut_result_cat_seq', (SELECT max(id::integer) "
                "FROM " + self.schema_name + ".anl_mincut_result_cat) , true)")
         row = self.controller.get_row(sql, log_sql=True)
+        if not row or row[0] is None:
+            result_mincut_id = '1'
+        elif row[0]:
+            result_mincut_id = str(int(row[0])+1)
 
-        if row:
-            if row[0] is not None:
-                if self.is_new:
-                    result_mincut_id = str(int(row[0])+1)
-
-        self.result_mincut_id.setText(str(result_mincut_id))
+        utils_giswater.setWidgetText(self.dlg_mincut, self.dlg_mincut.result_mincut_id, str(result_mincut_id))
 
 
     def mg_mincut(self):
@@ -369,7 +368,7 @@ class MincutParent(ParentAction, MultipleSelection):
 
     def accept_save_data(self):
         """ Slot function button 'Accept' """
-
+        self.save_settings(self.dlg_mincut)
         mincut_result_state = self.current_state
 
         # Manage 'address'
@@ -606,6 +605,7 @@ class MincutParent(ParentAction, MultipleSelection):
             sql = ("INSERT INTO " + self.schema_name + ".anl_mincut_result_cat (id, mincut_class) "
                    " VALUES ('" + str(result_mincut_id_text) + "', 2);")
             self.controller.execute_sql(sql)
+            self.is_new = False
 
         # Disable Auto, Custom, Hydrometer
         self.action_mincut.setDisabled(True)
@@ -767,6 +767,7 @@ class MincutParent(ParentAction, MultipleSelection):
             sql = ("INSERT INTO " + self.schema_name + ".anl_mincut_result_cat (id, mincut_class)"
                    " VALUES ('" + str(result_mincut_id_text) + "', 3);")
             self.controller.execute_sql(sql)
+            self.is_new = False
 
         # On inserting work order
         self.action_mincut.setDisabled(True)
