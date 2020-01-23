4d80c4639c0aa4a271c83a951b4825437d0ca98b
Barbara <bbrzepka@bgeo.es>
2020-01-23 14:49:04 +0100

    reinsert parameters to audit_cat_param_user, update layout_order

--- dbmodel/updates/33/33026/utils/dml.sql
+++ dbmodel/updates/33/33026/utils/dml.sql
@@ -8,8 +8,35 @@ This version of Giswater is provided by Giswater Association
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 
--- 2021/01/10
+-- 2020/01/20
 insert into audit_cat_function (id,function_name, project_type, function_type,descript, sys_role_id, isdeprecated, istoolbox, 
        isparametric)
 VALUES (2794,'gw_fct_audit_check_project','utils','function','Functions that controls the qgis project',
 'role_basic', false,false,false);
+
+--2020/01/23
+INSERT INTO audit_cat_param_user (id, formname, description, sys_role_id, qgis_message, idval, 
+       label, dv_querytext, dv_parent_id, isenabled, layout_id, layout_order, 
+       project_type, isparent, dv_querytext_filterc, feature_field_id, 
+       feature_dv_parent_value, isautoupdate, datatype, widgettype, 
+       ismandatory, widgetcontrols, vdefault, layoutname, reg_exp, iseditable, 
+       dv_orderby_id, dv_isnullvalue, stylesheet, placeholder, isdeprecated)
+VALUES ('audit_project_epa_result', 'config', 'Id of EPA results to analyze when audit check project function', 'role_epa', 
+NULL, NULL, 'EPA result to check database on load project', NULL, NULL, true, 8, 14, 'utils', false, NULL, NULL, NULL, 
+false, 'string', 'text', true, NULL, 'gw_check_project', NULL, NULL, NULL, NULL, NULL, NULL, NULL, false)
+ON conflict (id) DO NOTHING;
+
+INSERT INTO audit_cat_param_user (id, formname, description, sys_role_id, qgis_message, idval, 
+       label, dv_querytext, dv_parent_id, isenabled, layout_id, layout_order, 
+       project_type, isparent, dv_querytext_filterc, feature_field_id, 
+       feature_dv_parent_value, isautoupdate, datatype, widgettype, 
+       ismandatory, widgetcontrols, vdefault, layoutname, reg_exp, iseditable, 
+       dv_orderby_id, dv_isnullvalue, stylesheet, placeholder, isdeprecated)
+VALUES ('audit_project_plan_result', 'config', 'Id of PLAN results to analyze when audit check project function', 
+'role_master', NULL, NULL, 'PLAN result to check database on load project', NULL, NULL, true, 8, 15, 'utils', false, NULL, 
+NULL, NULL, false, 'string', 'text', true, NULL, 'gw_check_project', NULL, NULL, NULL, NULL, NULL, NULL, NULL, false)
+ON conflict (id) DO NOTHING;
+
+
+UPDATE audit_cat_param_user set  layout_order=15  where id='audit_project_epa_result';
+UPDATE audit_cat_param_user set layout_order=16  where id='audit_project_plan_result';
