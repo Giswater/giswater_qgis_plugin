0742370416fd41a5ad9c4b2a5bb6e298b71685b3
David Erill <derill@bgeo.es>
2020-01-23 10:15:46 +0100

    show message&warning: add parameter title

--- dao/controller.py
+++ dao/controller.py
@@ -310,7 +310,7 @@ class DaoController(object):
         return self.postgis_version           
         
     
-    def show_message(self, text, message_level=1, duration=5, context_name=None, parameter=None):
+    def show_message(self, text, message_level=1, duration=5, context_name=None, parameter=None, title=""):
         """ Show message to the user with selected message level
         message_level: {INFO = 0(blue), WARNING = 1(yellow), CRITICAL = 2(red), SUCCESS = 3(green)} """
         
@@ -319,29 +319,29 @@ class DaoController(object):
             msg = self.tr(text, context_name)
             if parameter:
                 msg += ": " + str(parameter)             
-        self.iface.messageBar().pushMessage("", msg, message_level, duration)
+        self.iface.messageBar().pushMessage(title, msg, message_level, duration)
             
 
-    def show_info(self, text, duration=5, context_name=None, parameter=None, logger_file=True):
+    def show_info(self, text, duration=5, context_name=None, parameter=None, logger_file=True, title=""):
         """ Show information message to the user """
 
-        self.show_message(text, 0, duration, context_name, parameter)
+        self.show_message(text, 0, duration, context_name, parameter, title)
         if self.logger and logger_file:
             self.logger.info(text)            
 
 
-    def show_warning(self, text, duration=5, context_name=None, parameter=None, logger_file=True):
+    def show_warning(self, text, duration=5, context_name=None, parameter=None, logger_file=True, title=""):
         """ Show warning message to the user """
 
-        self.show_message(text, 1, duration, context_name, parameter)
+        self.show_message(text, 1, duration, context_name, parameter, title)
         if self.logger and logger_file:
             self.logger.warning(text)
 
 
-    def show_critical(self, text, duration=5, context_name=None, parameter=None, logger_file=True):
+    def show_critical(self, text, duration=5, context_name=None, parameter=None, logger_file=True, title=""):
         """ Show warning message to the user """
 
-        self.show_message(text, 2, duration, context_name, parameter)
+        self.show_message(text, 2, duration, context_name, parameter, title)
         if self.logger and logger_file:
             self.logger.critical(text)
         
