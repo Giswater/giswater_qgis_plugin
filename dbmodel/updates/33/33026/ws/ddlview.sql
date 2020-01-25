b2ceac1175b27967434544911897964b17dd0956
Xavier Torret <xtorret@bgeo.es>
2020-01-25 17:13:07 +0100

    Refactor  v_ui_anl_mincut_result_cat removing expl_id and adding > 1 to show only real mincuts

--- dbmodel/updates/33/33026/ws/ddlview.sql
+++ dbmodel/updates/33/33026/ws/ddlview.sql
@@ -65,3 +65,47 @@ CREATE OR REPLACE VIEW v_edit_link AS
              LEFT JOIN plan_psector_x_connec USING (arc_id, connec_id)
           WHERE link.feature_id::text = v_state_connec.connec_id::text) a
   WHERE a.state < 2;
+  
+  
+DROP VIEW v_ui_anl_mincut_result_cat;
+
+CREATE OR REPLACE VIEW v_ui_anl_mincut_result_cat AS 
+ SELECT anl_mincut_result_cat.id,
+    anl_mincut_result_cat.id AS name,
+    anl_mincut_result_cat.work_order,
+    anl_mincut_cat_state.name AS state,
+    anl_mincut_cat_class.name AS class,
+    anl_mincut_result_cat.mincut_type,
+    anl_mincut_result_cat.received_date,
+    exploitation.name AS exploitation,
+    ext_municipality.name AS municipality,
+    anl_mincut_result_cat.postcode,
+    ext_streetaxis.name AS streetaxis,
+    anl_mincut_result_cat.postnumber,
+    anl_mincut_result_cat.anl_cause,
+    anl_mincut_result_cat.anl_tstamp,
+    anl_mincut_result_cat.anl_user,
+    anl_mincut_result_cat.anl_descript,
+    anl_mincut_result_cat.anl_feature_id,
+    anl_mincut_result_cat.anl_feature_type,
+    anl_mincut_result_cat.forecast_start,
+    anl_mincut_result_cat.forecast_end,
+    cat_users.name AS assigned_to,
+    anl_mincut_result_cat.exec_start,
+    anl_mincut_result_cat.exec_end,
+    anl_mincut_result_cat.exec_user,
+    anl_mincut_result_cat.exec_descript,
+    anl_mincut_result_cat.exec_from_plot,
+    anl_mincut_result_cat.exec_depth,
+    anl_mincut_result_cat.exec_appropiate,
+    anl_mincut_result_cat.notified,
+    output
+   FROM anl_mincut_result_cat
+     LEFT JOIN anl_mincut_cat_class ON anl_mincut_cat_class.id = anl_mincut_result_cat.mincut_class
+     LEFT JOIN anl_mincut_cat_state ON anl_mincut_cat_state.id = anl_mincut_result_cat.mincut_state
+     LEFT JOIN exploitation ON exploitation.expl_id = anl_mincut_result_cat.expl_id
+     LEFT JOIN macroexploitation ON macroexploitation.macroexpl_id = anl_mincut_result_cat.macroexpl_id
+     LEFT JOIN ext_municipality ON ext_municipality.muni_id = anl_mincut_result_cat.muni_id
+     LEFT JOIN ext_streetaxis ON ext_streetaxis.id::text = anl_mincut_result_cat.streetaxis_id::text
+     LEFT JOIN cat_users ON cat_users.id::text = anl_mincut_result_cat.assigned_to::text
+     WHERE anl_mincut_result_cat.id >0;
