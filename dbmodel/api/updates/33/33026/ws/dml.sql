61fdf9f05aee049157c644f5426ba82f527bfd92
Edgar Fuste <efuste@bgeo.es>
2020-01-29 09:12:02 +0100

    Enhancement: move update api_selector_mincut query into api folder

---
+++ dbmodel/api/updates/33/33026/ws/dml.sql
@@ -0,0 +1,14 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+
+-- 2020/01/28
+UPDATE config_param_system SET parameter = 'api_selector_mincut', value =
+'{"table":"anl_mincut_result_cat", "selector":"anl_mincut_result_selector, "label":"id, '' ('', CASE WHEN work_order IS NULL THEN ''N/I'' ELSE work_order END, '') on '', forecast_start::date, '' at '', forecast_start::time, ''H-'', forecast_end::time,''H''"}'
+WHERE parameter = 'api_selector_label';
--- dbmodel/updates/33/33026/ws/dml.sql
+++ dbmodel/updates/33/33026/ws/dml.sql
@@ -26,8 +26,3 @@ This analysis may take a lot of time. Be patient!!!'
 WHERE id=2712;
 
 UPDATE config_param_system SET value = 'TRUE' WHERE parameter = 'om_mincut_valvestat_using_valveunaccess';
-
-
-UPDATE config_param_system SET parameter = 'api_selector_mincut', value =
-'{"table":"anl_mincut_result_cat", "selector":"anl_mincut_result_selector, "label":"id, '' ('', CASE WHEN work_order IS NULL THEN ''N/I'' ELSE work_order END, '') on '', forecast_start::date, '' at '', forecast_start::time, ''H-'', forecast_end::time,''H''"}'
-WHERE parameter = 'api_selector_label';
