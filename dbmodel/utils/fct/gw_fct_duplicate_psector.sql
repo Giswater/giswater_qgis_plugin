4d08394b0cbc40db0ee56c85fb9e455546da72ff
Barbara <bbrzepka@bgeo.es>
2020-02-10 07:58:51 +0100

    improve log on duplicate psector

--- dbmodel/utils/fct/gw_fct_duplicate_psector.sql
+++ dbmodel/utils/fct/gw_fct_duplicate_psector.sql
@@ -21,7 +21,7 @@ SELECT SCHEMA_NAME.gw_fct_duplicate_psector($${
 
 $BODY$
 DECLARE
-api_version json;
+v_api_version json;
 v_arc_id TEXT;
 v_project_type text;
 v_version text;
@@ -43,6 +43,8 @@ v_sql text;
 v_connect2network text;
 v_psector_vdefault text;
 v_schemaname text;
+v_feature_list text;
+v_error_context text;
 
 BEGIN
 
@@ -53,7 +55,7 @@ BEGIN
 	SELECT wsoftware, giswater  INTO v_project_type, v_version FROM version order by 1 desc limit 1;
 
 	EXECUTE 'SELECT row_to_json(row) FROM (SELECT value FROM config_param_system WHERE parameter=''ApiVersion'') row'
-	INTO api_version;
+	INTO v_api_version;
 
 	-- manage log (fprocesscat = 51)
 	DELETE FROM audit_check_data WHERE fprocesscat_id=53 AND user_name=current_user;
@@ -67,7 +69,7 @@ BEGIN
 	UPDATE  config_param_system SET value = gw_fct_json_object_set_key(v_connec_proximity::json, 'activated'::text, 'false'::text) WHERE parameter='connec_proximity';
 	UPDATE  config_param_system SET value = gw_fct_json_object_set_key(v_gully_proximity::json, 'activated'::text, 'false'::text) WHERE parameter='gully_proximity';
 	
-	INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) VALUES (53, v_result_id, concat('Deactivate topology control -> Done' ));
+	INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) VALUES (53, v_result_id, concat('Deactivate topology control for connecs and gullies.' ));
 	
 	--capture current value and deactivate automatic link creation
 	SELECT value INTO v_connect2network FROM config_param_user WHERE parameter='edit_connect_force_automatic_connect2network' and cur_user=current_user;
@@ -89,6 +91,9 @@ BEGIN
 
  	INSERT INTO plan_psector_selector(psector_id,cur_user) VALUES (v_new_psector_id, current_user);
 	
+	INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) 
+	VALUES (53, v_result_id, concat('Copy psector ',v_old_psector_id,' as ',v_new_psector_name,'.' ));
+
 	SELECT value INTO v_psector_vdefault FROM config_param_user where parameter = 'psector_vdefault' and cur_user=current_user;
 	
 	IF v_psector_vdefault IS NULL THEN
@@ -98,30 +103,56 @@ BEGIN
 	END IF;
 	
 	INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) 
-	VALUES (53, v_result_id, concat('Copy psector ',v_old_psector_id,' as ',v_new_psector_name,' -> Done' ));
+	VALUES (53, v_result_id, concat('Set ',v_new_psector_name,' as current psector.' ));
 
-	INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) 
-	VALUES (53, v_result_id, concat('Set ',v_new_psector_name,' as current psector -> Done' ));
+	--copy arcs with state 0 inside plan_psector tables
+	SELECT string_agg(arc_id,',') INTO v_feature_list FROM plan_psector_x_arc  WHERE psector_id=v_old_psector_id and state=0;
 
-	--copy features with state 0 inside plan_psector tables
-	INSERT INTO plan_psector_x_arc(arc_id, psector_id, state, doable, descript) 
-	SELECT arc_id, v_new_psector_id, state, doable, descript FROM plan_psector_x_arc WHERE psector_id=v_old_psector_id and state=0;
+	IF v_feature_list IS NOT NULL THEN
+		INSERT INTO plan_psector_x_arc(arc_id, psector_id, state, doable, descript) 
+		SELECT arc_id, v_new_psector_id, state, doable, descript FROM plan_psector_x_arc WHERE psector_id=v_old_psector_id and state=0;
 
-	INSERT INTO plan_psector_x_node(node_id, psector_id, state, doable, descript) 
-	SELECT node_id, v_new_psector_id, state, doable, descript FROM plan_psector_x_node WHERE psector_id=v_old_psector_id and state=0;
-
-	INSERT INTO plan_psector_x_connec(connec_id, psector_id, state, doable, descript,link_geom, vnode_geom) 
-	SELECT connec_id, v_new_psector_id, state, doable, descript,link_geom, vnode_geom FROM plan_psector_x_connec WHERE psector_id=v_old_psector_id and state=0;
-	
-	INSERT INTO plan_psector_x_other(price_id, measurement, psector_id, descript)
-	SELECT  price_id, measurement, psector_id, descript FROM plan_psector_x_other WHERE psector_id=v_old_psector_id;
-
-	IF v_project_type='UD' THEN
-		INSERT INTO plan_psector_x_gully(gully_id, psector_id, state, doable, descript,link_geom, vnode_geom) 
-		SELECT gully_id, v_new_psector_id, state, doable, descript,link_geom, vnode_geom FROM plan_psector_x_gully WHERE psector_id=v_old_psector_id and state=0;
+		INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) VALUES (53, v_result_id, concat('Copied arcs with state 0: ', v_feature_list ));
 	END IF;
 
-	INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) VALUES (53, v_result_id, concat('Copy features with state 0 -> Done' ));
+	--copy nodes with state 0 inside plan_psector tables
+	SELECT string_agg(node_id,',') INTO v_feature_list FROM plan_psector_x_node  WHERE psector_id=v_old_psector_id and state=0;
+
+	IF v_feature_list IS NOT NULL THEN
+		INSERT INTO plan_psector_x_node(node_id, psector_id, state, doable, descript) 
+		SELECT node_id, v_new_psector_id, state, doable, descript FROM plan_psector_x_node WHERE psector_id=v_old_psector_id and state=0;
+
+		INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) VALUES (53, v_result_id, concat('Copied nodes with state 0: ', v_feature_list ));
+	END IF;
+
+	--copy connecs with state 0 inside plan_psector tables
+	SELECT string_agg(connec_id,',') INTO v_feature_list FROM plan_psector_x_connec WHERE psector_id=v_old_psector_id and state=0;
+
+	IF v_feature_list IS NOT NULL THEN
+		INSERT INTO plan_psector_x_connec(connec_id, psector_id, state, doable, descript,link_geom, vnode_geom) 
+		SELECT connec_id, v_new_psector_id, state, doable, descript,link_geom, vnode_geom FROM plan_psector_x_connec WHERE psector_id=v_old_psector_id and state=0;
+
+		INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) VALUES (53, v_result_id, concat('Copied connecs with state 0: ', v_feature_list ));
+	END IF;
+
+	--copy gullies with state 0 inside plan_psector tables
+	IF v_project_type='UD' THEN
+		SELECT string_agg(gully_id,',') INTO v_feature_list FROM plan_psector_x_gully WHERE psector_id=v_old_psector_id and state=0;
+		IF v_feature_list IS NOT NULL THEN
+			INSERT INTO plan_psector_x_gully(gully_id, psector_id, state, doable, descript,link_geom, vnode_geom) 
+			SELECT gully_id, v_new_psector_id, state, doable, descript,link_geom, vnode_geom FROM plan_psector_x_gully WHERE psector_id=v_old_psector_id and state=0;
+	
+			INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) VALUES (53, v_result_id, concat('Copied gullies with state 0: ', v_feature_list ));
+		END IF;
+	END IF;
+	--copy featuthers inside plan_psector tables
+	SELECT string_agg(price_id,',') INTO v_feature_list FROM plan_psector_x_other WHERE psector_id=v_old_psector_id;	
+	IF v_feature_list IS NOT NULL THEN
+		INSERT INTO plan_psector_x_other(price_id, measurement, psector_id, descript)
+		SELECT  price_id, measurement, psector_id, descript FROM plan_psector_x_other WHERE psector_id=v_old_psector_id;
+		
+		INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) VALUES (53, v_result_id, concat('Copied other prices: ', v_feature_list ));
+	END IF;
 
 	--insert copy of the planified feature in the corresponding v_edit_* view and insert it into plan_psector_x_* table
 	FOR rec_type IN (SELECT * FROM sys_feature_type WHERE net_category=1 ORDER BY CASE 
@@ -157,11 +188,17 @@ BEGIN
 			EXECUTE 'INSERT INTO v_edit_'||lower(rec_type.id)||' ('||v_insert_fields||',state) SELECT '||v_insert_fields||',2 FROM '||lower(rec_type.id)||'
 			WHERE '||lower(rec_type.id)||'_id='''||v_field_id||''';';
 			
-				
 		END LOOP;
-	END LOOP;
-	INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) VALUES (53, v_result_id, concat('Copy features with state 1 -> Done' ));
 
+		EXECUTE 'SELECT string_agg('||lower(rec_type.id)||'_id,'','') FROM plan_psector_x_'||lower(rec_type.id)||' WHERE psector_id='||v_old_psector_id||' and state=1'
+		into v_feature_list;
+		
+		IF v_feature_list IS NOT NULL THEN
+			INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) 
+			VALUES (53, v_result_id, concat('New ',lower(rec_type.id),' inserted with state 1: ', v_feature_list));
+		END IF;
+
+	END LOOP;
 	
 	--activate the functions an set back the values of parameters
 	IF v_connect2network is not null then
@@ -171,7 +208,7 @@ BEGIN
 	UPDATE config_param_system SET value = v_gully_proximity WHERE parameter='gully_proximity';     
 	UPDATE config_param_system SET value ='FALSE' WHERE parameter='edit_topocontrol_dsbl_error';
 
-	INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) VALUES (53, v_result_id, concat('Activate topology control -> Done' ));
+	INSERT INTO audit_check_data (fprocesscat_id, result_id, error_message) VALUES (53, v_result_id, concat('Activate topology control.'));
 	
 	SELECT array_to_json(array_agg(row_to_json(row))) INTO v_result 
 	FROM (SELECT id, error_message AS message FROM audit_check_data WHERE user_name="current_user"() AND fprocesscat_id=53) row; 
@@ -180,10 +217,15 @@ BEGIN
 	v_result := COALESCE(v_result, '{}'); 
 	v_result_info = concat ('{"geometryType":"", "values":',v_result, '}');
 	v_result_info := COALESCE(v_result_info, '{}'); 
-	api_version := COALESCE(api_version, '[]');
+	v_api_version := COALESCE(v_api_version, '[]');
 
-RETURN ('{"status":"Accepted", "apiVersion":'||api_version||
+RETURN ('{"status":"Accepted", "apiVersion":'||v_api_version||
             ',"message":{"priority":1, "text":""},"body":{"data": {"info":'||v_result_info||'}}}')::json;
+
+	EXCEPTION WHEN OTHERS THEN
+	 GET STACKED DIAGNOSTICS v_error_context = PG_EXCEPTION_CONTEXT;
+	 RETURN ('{"status":"Failed","NOSQLERR":' || to_json(SQLERRM) || ',"SQLSTATE":' || to_json(SQLSTATE) ||',"SQLCONTEXT":' || to_json(v_error_context) || '}')::json;
+
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
