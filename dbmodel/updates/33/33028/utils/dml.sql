e66c8fd49686285b2b7aaf59c990e55d60c2fdaf
Edgar Fuste <efuste@bgeo.es>
2020-02-13 12:38:55 +0100

    Enhancement: update parameters on audit_cat_param_user for api config form

--- dbmodel/updates/33/33028/utils/dml.sql
+++ dbmodel/updates/33/33028/utils/dml.sql
@@ -21,3 +21,15 @@ VALUES (112, 'Arc divide', 'edit', 'utils') ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO sys_fprocess_cat(id, fprocess_name, context, project_type)
 VALUES (113, 'Node interpolate', 'edit', 'ud') ON CONFLICT (id) DO NOTHING;
+
+-- 13/02/2020
+
+UPDATE audit_cat_param_user SET isparent = False WHERE id = 'state_vdefault';
+UPDATE audit_cat_param_user SET label = 'State type (On service):', dv_querytext = 'SELECT id as id, name as idval FROM value_state_type WHERE id IS NOT NULL AND state = 1 ', dv_parent_id = NULL, dv_querytext_filterc = NULL WHERE id = 'statetype_vdefault';
+UPDATE audit_cat_param_user SET label = 'State type (Obsolete):' WHERE id = 'statetype_end_vdefault';
+UPDATE audit_cat_param_user SET sys_role_id = 'role_edit', label = 'State type (Planified):', layout_id = 3, layout_order = 4 WHERE id = 'statetype_plan_vdefault';
+UPDATE audit_cat_param_user SET layout_order = 5 WHERE id = 'workcat_vdefault';
+UPDATE audit_cat_param_user SET layout_order = 6 WHERE id = 'builtdate_vdefault';
+UPDATE audit_cat_param_user SET layout_order = 7 WHERE id = 'enddate_vdefault';
+UPDATE audit_cat_param_user SET layout_order = 8 WHERE id = 'workcat_id_end_vdefault';
+UPDATE audit_cat_param_user SET label = 'Enddate' WHERE label = 'End date:';
