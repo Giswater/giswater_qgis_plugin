75e9ce9af158cfd64831ea27570e379149f2e9b6
Xavier Torret <xtorret@bgeo.es>
2020-02-17 21:07:39 +0100

    Bug fix on update link

--- dbmodel/utils/ftrg/gw_trg_update_link_arc_id.sql
+++ dbmodel/utils/ftrg/gw_trg_update_link_arc_id.sql
@@ -6,31 +6,38 @@ This version of Giswater is provided by Giswater Association
 
 -- FUNCTION CODE: 2688
 
-
-
-CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_trg_update_link_arc_id() RETURNS trigger AS $BODY$
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_trg_update_link_arc_id()
+  RETURNS trigger AS
+$BODY$
 DECLARE 
+v_featuretype text;
+v_link record;
+v_projecttype text;
+v_geom public.geometry;
 
-	v_exittype text;
-	v_link record;
-	v_projecttype text;
 BEGIN
 
     EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
-    v_exittype:= TG_ARGV[0];
+    v_featuretype:= TG_ARGV[0];
 
 		-- get project type
 	SELECT wsoftware INTO v_projecttype FROM version LIMIT 1;
 
-	IF v_exittype='connec' THEN
-	FOR v_link IN SELECT * FROM link WHERE (exit_type='CONNEC' AND exit_id=OLD.connec_id)
+	IF v_featuretype='connec' THEN
+
+		-- feature as startpoint
+		v_geom =  (SELECT the_geom FROM connec WHERE connec_id = OLD.connec_id);
+		UPDATE link SET the_geom = ST_SetPoint(the_geom, 0, v_geom) WHERE feature_id = OLD.connec_id AND feature_type = 'CONNEC';
+
+		-- feature as endpoint
+		FOR v_link IN SELECT * FROM link WHERE (exit_type='CONNEC' AND exit_id=OLD.connec_id)
 		LOOP
 
 			IF v_link.feature_type='CONNEC' THEN
 
 				-- update connec, mandatory to use v_edit_connec because it's identified and managed when arc_id comes from plan psector tables
 				UPDATE v_edit_connec SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, dma_id= NEW.dma_id, sector_id=NEW.sector_id WHERE connec_id=v_link.feature_id;
-								
+							
 				IF v_projecttype = 'WS' THEN
 
 					-- update presszone
@@ -45,10 +52,22 @@ BEGIN
 				UPDATE v_edit_gully SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, dma_id= NEW.dma_id, sector_id=NEW.sector_id WHERE gully_id=v_link.feature_id;
 				
 			END IF;
+
+			-- update link geometry in case of exit geometry has changed
+			v_geom =  (SELECT the_geom FROM connec WHERE connec_id = OLD.connec_id);
+			UPDATE link SET the_geom = ST_SetPoint(the_geom, ST_NumPoints(the_geom) - 1, v_geom) WHERE exit_id = OLD.connec_id;
+			
 		END LOOP;
+		
 	
-	ELSIF v_exittype='gully' THEN
-	FOR v_link IN SELECT * FROM link WHERE (exit_type='GULLY' AND exit_id=OLD.gully_id)
+	ELSIF v_featuretype='gully' THEN
+
+		-- feature as startpoint
+		v_geom =  (SELECT the_geom FROM gully WHERE gully_id = OLD.gully_id);
+		UPDATE link SET the_geom = ST_SetPoint(the_geom, 0, v_geom) WHERE feature_id = OLD.gully_id AND feature_type = 'GULLY';
+
+		-- feature as endpoint
+		FOR v_link IN SELECT * FROM link WHERE (exit_type='GULLY' AND exit_id=OLD.gully_id)
 		LOOP
 			IF v_link.feature_type='CONNEC' THEN
 			
@@ -58,15 +77,21 @@ BEGIN
 		
 				UPDATE v_edit_gully SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, dma_id= NEW.dma_id, sector_id=NEW.sector_id WHERE gully_id=v_link.feature_id;
 			END IF;
+
+			-- update link geometry in case of exit geometry has changed
+			v_geom =  (SELECT the_geom FROM gully WHERE gully_id = OLD.gully_id);
+			UPDATE link SET the_geom = ST_SetPoint(the_geom, ST_NumPoints(the_geom) - 1, v_geom) WHERE exit_id = OLD.gully_id;
+
 		END LOOP;
 		
 	END IF;
 	
-	RETURN NEW;
-   
+	RETURN NEW;   
 END;
 $BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
+LANGUAGE plpgsql VOLATILE
+COST 100;
+
+
 
 
