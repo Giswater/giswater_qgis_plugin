660480acb2e497674eedd15fb39e9c3d977a075b
Albert Bofill <abofill@bgeo.es>
2020-02-28 15:08:43 +0100

    Bug fix on trg om visit

--- dbmodel/utils/ftrg/gw_trg_om_visit.sql
+++ dbmodel/utils/ftrg/gw_trg_om_visit.sql
@@ -66,17 +66,17 @@ BEGIN
 			IF v_featuretype ='arc' THEN	
 				IF (SELECT arc_id FROM om_visit_lot_x_arc where arc_id=NEW.arc_id AND lot_id=v_lot) IS NULL THEN
 					v_code = (SELECT code FROM arc WHERE arc_id=NEW.arc_id);
-					INSERT INTO om_visit_lot_x_arc SELECT (v_lot, NEW.arc_id, v_code, 1);
+					INSERT INTO om_visit_lot_x_arc VALUES (v_lot, NEW.arc_id, v_code, 1);
 				END IF;				
 			ELSIF v_featuretype ='node' THEN	
 				IF (SELECT node_id FROM om_visit_lot_x_node where node_id=NEW.node_id AND lot_id=v_lot) IS NULL THEN
 					v_code = (SELECT code FROM node WHERE node_id=NEW.node_id);
-					INSERT INTO om_visit_lot_x_node SELECT (v_lot, NEW.node_id, v_code, 1);
+					INSERT INTO om_visit_lot_x_node VALUES (v_lot, NEW.node_id, v_code, 1);
 				END IF;	
 			ELSIF v_featuretype ='connec' THEN	
 				IF (SELECT connec_id FROM om_visit_lot_x_connec where connec_id=NEW.connec_id AND lot_id=v_lot) IS NULL THEN
 					v_code = (SELECT code FROM connec WHERE connec_id=NEW.connec_id);
-					INSERT INTO om_visit_lot_x_connec SELECT (v_lot, NEW.connec_id, v_code, 1);
+					INSERT INTO om_visit_lot_x_connec VALUES (v_lot, NEW.connec_id, v_code, 1);
 				END IF;	
 			ELSIF v_featuretype ='gully' THEN	
 				IF (SELECT gully_id FROM om_visit_lot_x_gully where gully_id=NEW.gully_id AND lot_id=v_lot) IS NULL THEN
