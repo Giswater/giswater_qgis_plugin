691990ba54e036d9681b360fc3cd147a74083444
Barbara <bbrzepka@bgeo.es>
2020-03-13 16:52:26 +0100

    UD - v_ui_node_connection_downstream - change unknown field to text

--- dbmodel/updates/33/33036/ud/ddlview.sql
+++ dbmodel/updates/33/33036/ud/ddlview.sql
@@ -61,3 +61,32 @@ CREATE OR REPLACE VIEW v_anl_flow_node AS
     anl_node
   WHERE anl_node.expl_id = selector_expl.expl_id AND selector_expl.cur_user = "current_user"()::text AND 
   anl_node.cur_user::name = "current_user"() AND (fprocesscat_id = 120 OR fprocesscat_id = 121);
+
+
+--2020/03/13
+DROP VIEW IF EXISTS v_ui_node_x_connection_downstream;
+CREATE OR REPLACE VIEW v_ui_node_x_connection_downstream AS 
+ SELECT row_number() OVER (ORDER BY v_edit_arc.node_1)+1000000 AS rid,
+    v_edit_arc.node_1 AS node_id,
+    v_edit_arc.arc_id AS feature_id,
+    v_edit_arc.code AS feature_code,
+    v_edit_arc.arc_type AS featurecat_id,
+    v_edit_arc.arccat_id,
+    v_edit_arc.y2 AS depth,
+    st_length2d(v_edit_arc.the_geom)::numeric(12,2) AS length,
+    node.node_id AS downstream_id,
+    node.code AS downstream_code,
+    node.node_type AS downstream_type,
+    v_edit_arc.y1 AS downstream_depth,
+    sys_type,
+    st_x(st_lineinterpolatepoint(v_edit_arc.the_geom, 0.5::double precision)) AS x,
+    st_y(st_lineinterpolatepoint(v_edit_arc.the_geom, 0.5::double precision)) AS y,
+    cat_arc.descript,
+    value_state.name AS state,
+    'v_edit_arc'::text as sys_table_id
+   FROM v_edit_arc
+     JOIN node ON v_edit_arc.node_2::text = node.node_id::text
+     JOIN arc_type ON arc_type.id::text = v_edit_arc.arc_type::text
+     LEFT JOIN cat_arc ON v_edit_arc.arccat_id::text = cat_arc.id::text
+     JOIN value_state ON v_edit_arc.state = value_state.id;
+
