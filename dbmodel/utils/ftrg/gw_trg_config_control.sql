3b810cd74ef4f69dbbc22aad3ed3d5ad7ed18f42
Xavier Torret <xtorret@bgeo.es>
2020-03-13 10:11:37 +0100

    Bug fix on config_control trigger

--- dbmodel/utils/ftrg/gw_trg_config_control.sql
+++ dbmodel/utils/ftrg/gw_trg_config_control.sql
@@ -35,32 +35,30 @@ BEGIN
 	ELSIF v_configtable = 'config_api_form_fields' THEN 
 	
 		IF TG_OP = 'INSERT' THEN
-		--check dv_querytext is correct
+			--check dv_querytext is correct
 			IF (NEW.widgettype = 'combo' OR NEW.widgettype = 'typeahead') AND NEW.dv_querytext IS NOT NULL THEN
-
 				v_querytext = NEW.dv_querytext;
 				EXECUTE v_querytext;
 			END IF;
 		
-		--check isautoupdate is FALSE when typeahead
-		IF NEW.widgettype = 'typeahead' AND NEW.isautoupdate = TRUE THEN
-		    SELECT gw_fct_getmessage($${"client":{"device":3, "infoType":100, "lang":"ES"},"feature":{},"data":{"error":"3096", "function":"2816","debug":null}}$$);	
-		END IF;
+			--check isautoupdate is FALSE when typeahead
+			IF NEW.widgettype = 'typeahead' AND NEW.isautoupdate = TRUE THEN
+				SELECT gw_fct_getmessage($${"client":{"device":3, "infoType":100, "lang":"ES"},"feature":{},"data":{"error":"3096", "function":"2816","debug":null}}$$);	
+			END IF;
 			
 			RETURN NEW;
 
 		ELSIF TG_OP = 'UPDATE' THEN
-		--check dv_querytext is correct
+			--check dv_querytext is correct
 			IF (NEW.widgettype = 'combo' OR NEW.widgettype = 'typeahead') AND (NEW.dv_querytext != OLD.dv_querytext) AND NEW.dv_querytext IS NOT NULL THEN
-
 				v_querytext = NEW.dv_querytext;
 				EXECUTE v_querytext;
 			END IF;
 		
-		--check isautoupdate is FALSE when typeahead
-		IF NEW.widgettype = 'typeahead' AND NEW.isautoupdate = TRUE THEN
-		    SELECT gw_fct_getmessage($${"client":{"device":3, "infoType":100, "lang":"ES"},"feature":{},"data":{"error":"3096", "function":"2816","debug":null}}$$);	
-		END IF;
+			--check isautoupdate is FALSE when typeahead
+			IF NEW.widgettype = 'typeahead' AND NEW.isautoupdate = TRUE THEN
+				SELECT gw_fct_getmessage($${"client":{"device":3, "infoType":100, "lang":"ES"},"feature":{},"data":{"error":"3096", "function":"2816","debug":null}}$$);	
+			END IF;
 			
 			RETURN NEW;
 
@@ -69,10 +67,8 @@ BEGIN
 
 		END IF;
 
-	END IF
+	END IF;
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION SCHEMA_NAME.gw_trg_config_control()
-  OWNER TO postgres;
