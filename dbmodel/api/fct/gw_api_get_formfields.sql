e0088b4b70a8ba5753f642988f28915eb001edc4
Xavier Torret <xtorret@bgeo.es>
2020-03-22 21:21:16 +0100

    Bug fix on config_api_form fields for getinfoplan

--- dbmodel/api/fct/gw_api_get_formfields.sql
+++ dbmodel/api/fct/gw_api_get_formfields.sql
@@ -115,20 +115,21 @@ BEGIN
 	ELSE
 		EXECUTE 'SELECT array_agg(row_to_json(b)) FROM (
 			SELECT (row_number()over(ORDER BY 1)) AS layout_order, (row_number()over(ORDER BY 1)) AS orderby, * FROM 
-				(SELECT ''individual'' as widtget_context, concat(unit, ''. '', descript) AS label, identif AS column_id, ''label'' AS widgettype, 
+				(SELECT concat(unit, ''. '', descript) AS label, identif AS column_id, ''label'' AS widgettype, 
 				concat ('||quote_literal(p_tabname)||',''_'',identif) AS widgetname, ''string'' AS datatype, 
-				NULL AS tooltip, NULL AS placeholder, FALSE AS iseditable, orderby as layout_order, ''1'' AS layoutname,  NULL AS dv_parent_id, 
+				NULL AS tooltip, NULL AS placeholder, FALSE AS iseditable, orderby as layout_order, ''lyt_plan_1'' AS layoutname,  NULL AS dv_parent_id, 
 				NULL AS isparent, NULL as ismandatory, NULL AS button_function, NULL AS dv_querytext, 
 				NULL AS dv_querytext_filterc, NULL AS linkedaction, NULL AS isautoupdate, concat (measurement,'' '',unit,'' x '', cost , 
 				'' €/'',unit,'' = '', total_cost::numeric(12,2), '' €'') as value, null as stylesheet,
 				null as widgetcontrols, null as hidden
 				FROM ' ||p_tablename|| ' WHERE ' ||p_idname|| ' = $2
 			UNION
-				SELECT ''resumen'' as widtget_context, label, column_id, widgettype, 
+				SELECT label, column_id, widgettype, 
 				concat ('||quote_literal(p_tabname)||',''_'',column_id) AS widgetname, datatype, 
-				tooltip, placeholder, iseditable, layout_order, layoutname,  NULL AS dv_parent_id, NULL AS isparent, ismandatory,  NULL AS widgetfunction, NULL AS dv_querytext, 
+				tooltip, placeholder, iseditable, layout_order+100 as layout_order, ''lyt_plan_1'' as layoutname,  NULL AS dv_parent_id, NULL AS isparent, ismandatory,  
+				NULL AS widgetfunction, NULL AS dv_querytext, 
 				NULL AS dv_querytext_filterc, NULL AS linkedaction, NULL AS isautoupdate, null as value, null as stylesheet, widgetcontrols::text, hidden
-				FROM config_api_form_fields WHERE formname  = ''infoplan'' ORDER BY 1, layout_order) a
+				FROM config_api_form_fields WHERE formname  = ''infoplan'' ORDER BY layoutname, layout_order) a
 			ORDER BY 1) b'
 			INTO fields_array
 			USING p_formname, p_id ;
