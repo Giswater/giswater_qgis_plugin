232567a8d12b4ad5e2e8f4a2f571730b16e66c85
Xavier Torret <xtorret@bgeo.es>
2020-04-07 18:47:30 +0200

    Bug fix on new variable for config_param_system

--- dbmodel/api/updates/33/33042/utils/dml.sql
+++ dbmodel/api/updates/33/33042/utils/dml.sql
@@ -12,5 +12,6 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 INSERT INTO config_api_form_tabs (id, formname, tabname, label,  tooltip, sys_role) 
 VALUES (710, 'exploitation', 'tabExploitation', 'Exploitation', 'Exploitation Selector', 'role_basic');
 
-INSERT INTO api_ws_sample.config_param_system (id, parameter, value, context,  descript, label, project_type, datatype, isdeprecated) 
-VALUES (1020, 'api_selector_exploitation', '{"table":"exploitation", "selector":"selector_expl", "table_id":"expl_id",  "selector_id":"expl_id",  "label":"expl_id, '' - '', name, '' '', CASE WHEN descript IS NULL THEN '' ELSE concat('' - '', descript) END, ''"}', 'system', 'Select which label to display for selectors', 'Selector labels:', 'utils', 'json', FALSE);
+SELECT setval('config_param_system_id_seq', (SELECT max(id) FROM config_param_system), true);
+INSERT INTO config_param_system (parameter, value, context,  descript, label, project_type, datatype, isdeprecated) 
+VALUES ('api_selector_exploitation', '{"table":"exploitation", "selector":"selector_expl", "table_id":"expl_id",  "selector_id":"expl_id",  "label":"expl_id, '' - '', name, '' '', CASE WHEN descript IS NULL THEN '' ELSE concat('' - '', descript) END, ''"}', 'system', 'Select which label to display for selectors', 'Selector labels:', 'utils', 'json', FALSE);
