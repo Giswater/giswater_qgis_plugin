c47786c1e448c735f920adeb139b8a9d6977eb31
Barbara <bbrzepka@bgeo.es>
2020-04-09 14:06:19 +0200

    adjust copy schema to using schema 'utils' and source schema name 'ws'

--- dbmodel/utils/fct/gw_fct_admin_manage_schema.sql
+++ dbmodel/utils/fct/gw_fct_admin_manage_schema.sql
@@ -53,37 +53,45 @@ BEGIN
 
 		IF v_project_type = 'WS' or v_project_type = 'UD' THEN
 
-			FOR v_view IN EXECUTE 'SELECT table_name, replace(view_definition, '||quote_literal(v_source)||','||quote_literal(v_target)||') as definition 
+			FOR v_view IN EXECUTE 'SELECT table_name, replace(view_definition, concat('||quote_literal(v_source)||',''.''),
+			concat('||quote_literal(v_target)||',''.'')) as definition 
 			FROM information_schema.VIEWS WHERE  table_schema = '||quote_literal(v_target)||' AND (table_name ILIKE ''v_anl_%'' OR 
 			table_name ILIKE ''ext_%''  OR table_name ILIKE ''vu_%'' OR table_name ILIKE ''vi_%''  OR table_name ILIKE ''v_visit_%''
 			 OR table_name ILIKE ''v_value_%'' OR table_name ILIKE ''v_state_%'')'
 			LOOP
+		
 				EXECUTE 'CREATE OR REPLACE VIEW ' ||v_view.table_name || ' AS ' || v_view.definition;
 
 				v_view_list:= array_append(v_view_list, quote_literal(v_view.table_name::text));
 			END LOOP;
 
-			FOR v_view IN EXECUTE 'SELECT table_name, replace(view_definition, '||quote_literal(v_source)||','||quote_literal(v_target)||') as definition 
+			FOR v_view IN EXECUTE 'SELECT table_name, replace(view_definition, concat('||quote_literal(v_source)||',''.''),
+			concat('||quote_literal(v_target)||',''.'')) as definition 
 			FROM information_schema.VIEWS WHERE  table_schema = '||quote_literal(v_target)||' AND (table_name IN (''v_arc'', ''v_node'',''v_connec'',''v_gully'',
 			''v_edit_arc'', ''v_edit_node'',''v_edit_connec'',''v_edit_gully''))'
 			LOOP
+			
 				EXECUTE 'CREATE OR REPLACE VIEW ' ||v_view.table_name || ' AS ' || v_view.definition;
 
 				v_view_list:= array_append(v_view_list, quote_literal(v_view.table_name::text));
 			END LOOP;
 
-			FOR v_view IN EXECUTE 'SELECT table_name, replace(view_definition, '||quote_literal(v_source)||','||quote_literal(v_target)||') as definition 
+			FOR v_view IN EXECUTE 'SELECT table_name, replace(view_definition, concat('||quote_literal(v_source)||',''.''),
+			concat('||quote_literal(v_target)||',''.'')) as definition 
 			FROM information_schema.VIEWS WHERE  table_schema = '||quote_literal(v_target)||' AND (table_name ILIKE ''ve_%'' OR 
-			table_name ILIKE ''v_edit%'' OR table_name ILIKE ''ext_%'')'
+			table_name ILIKE ''v_edit%'' )'--OR table_name ILIKE ''ext_%'')'
 			LOOP
+		
 				EXECUTE 'CREATE OR REPLACE VIEW ' ||v_view.table_name || ' AS ' || v_view.definition;
 			
 				v_view_list:= array_append(v_view_list, quote_literal(v_view.table_name::text));
 			END LOOP;
 
-			FOR v_view IN EXECUTE 'SELECT table_name, replace(view_definition, '||quote_literal(v_source)||','||quote_literal(v_target)||') as definition 
+			FOR v_view IN EXECUTE 'SELECT table_name, replace(view_definition, concat('||quote_literal(v_source)||',''.''),
+			concat('||quote_literal(v_target)||',''.'')) as definition 
 			FROM information_schema.VIEWS WHERE  table_schema = '||quote_literal(v_target)||' AND table_name NOT IN ('||array_to_string(v_view_list,',')||')' 
 			LOOP
+
 				EXECUTE 'CREATE OR REPLACE VIEW ' ||v_view.table_name || ' AS ' || v_view.definition;
 			END LOOP;
 		END IF;
--- dbmodel/utils/fct/gw_fct_clone_schema.sql
+++ dbmodel/utils/fct/gw_fct_clone_schema.sql
@@ -11,7 +11,6 @@ DROP FUNCTION IF EXISTS "SCHEMA_NAME".clone_schema(text, text);
 CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_fct_clone_schema(p_data json)
 RETURNS json AS
 $BODY$
-
 /*
 SELECT SCHEMA_NAME.gw_fct_clone_schema($${
 "client":{"device":9, "infoType":100, "lang":"ES"}, 
@@ -20,220 +19,229 @@ SELECT SCHEMA_NAME.gw_fct_clone_schema($${
 */
 
 DECLARE
-    rec_view record;
-    rec_fk record;
-    rec_table text;
-    v_tablename text;
-    v_default text;
-    v_column text;
-    v_source_schema text; 
-    v_dest_schema text;
-    v_version text;
-    v_error_context text;
-    rec_fct record;
-    v_fct_definition text;
-    v_query_text text;
-    v_id_field text;
-    rec_seq  record;
-    rec_trg record;
-    v_trg_fields text;  
-    v_replace_query text;
-    v_result json;
-    v_result_info json;
-    v_result_point json;
-    v_result_polygon json;
-    v_result_line json;
-    v_count integer;
-    
+rec_view record;
+rec_fk record;
+rec_table text;
+rec_fct record;
+rec_seq  record;
+rec_trg record;
+v_tablename text;
+v_default text;
+v_column text;
+v_source_schema text; 
+v_dest_schema text;
+v_version text;
+v_error_context text;
+v_fct_definition text;
+v_query_text text;
+v_id_field text;
+v_trg_fields text;  
+v_replace_query text;
+v_result json;
+v_result_info json;
+v_result_point json;
+v_result_polygon json;
+v_result_line json;
+v_count integer;
+v_software text;
 BEGIN
 
-    -- Search path
-    SET search_path = "SCHEMA_NAME", public;
+	-- Search path
+	SET search_path = "SCHEMA_NAME", public;
 
-    SELECT giswater INTO v_version FROM version order by id desc limit 1;
+	SELECT giswater INTO v_version FROM version order by id desc limit 1;
 
-    -- get input data   
-    v_source_schema := ((p_data ->>'data')::json->>'parameters')::json->>'source_schema'::text;
-    v_dest_schema := ((p_data ->>'data')::json->>'parameters')::json->>'dest_schema'::text;
+	-- get input data   
+	v_source_schema := ((p_data ->>'data')::json->>'parameters')::json->>'source_schema'::text;
+	v_dest_schema := ((p_data ->>'data')::json->>'parameters')::json->>'dest_schema'::text;
 
-    -- Create destination schema
-    EXECUTE 'CREATE SCHEMA ' || v_dest_schema ;
-     
-    -- Sequences
-    FOR rec_table IN
-        SELECT sequence_name FROM information_schema.SEQUENCES WHERE sequence_schema = v_source_schema
-    LOOP
-        EXECUTE 'CREATE SEQUENCE ' || v_dest_schema || '.' || rec_table;
-    END LOOP;
-     
-    -- Tables
-    FOR rec_table IN
-        SELECT table_name FROM information_schema.TABLES WHERE table_schema = v_source_schema AND table_type = 'BASE TABLE' ORDER BY table_name
-    LOOP
+	-- Create destination schema
+	EXECUTE 'CREATE SCHEMA ' || v_dest_schema ;
+	 
+	-- Sequences
+	FOR rec_table IN
+		SELECT sequence_name FROM information_schema.SEQUENCES WHERE sequence_schema = v_source_schema
+	LOOP
+		EXECUTE 'CREATE SEQUENCE ' || v_dest_schema || '.' || rec_table;
+	END LOOP;
+	 
+	-- Tables
+	FOR rec_table IN
+		SELECT table_name FROM information_schema.TABLES WHERE table_schema = v_source_schema AND table_type = 'BASE TABLE' ORDER BY table_name
+	LOOP
 
-        -- Create table in destination schema
-        v_tablename := v_dest_schema || '.' || rec_table;
-        EXECUTE 'CREATE TABLE ' || v_tablename || ' (LIKE ' || v_source_schema || '.' || rec_table || ' INCLUDING CONSTRAINTS INCLUDING INDEXES INCLUDING DEFAULTS)';
-        
-        -- Set contraints
-        FOR v_column, v_default IN
-            SELECT column_name, REPLACE(column_default, v_source_schema, v_dest_schema) 
-            FROM information_schema.COLUMNS 
-            WHERE table_schema = v_dest_schema AND table_name = rec_table AND column_default LIKE 'nextval(%' || v_source_schema || '%::regclass)'
-        LOOP
-            EXECUTE 'ALTER TABLE ' || v_tablename || ' ALTER COLUMN ' || v_column || ' SET DEFAULT ' || v_default;
-        END LOOP;
-        
-        -- Copy table contents to destination schema
-        EXECUTE 'INSERT INTO ' || v_tablename || ' SELECT * FROM ' || v_source_schema || '.' || rec_table;  
-        
-    END LOOP;
-    
+		-- Create table in destination schema
+		v_tablename := v_dest_schema || '.' || rec_table;
+		EXECUTE 'CREATE TABLE ' || v_tablename || ' (LIKE ' || v_source_schema || '.' || rec_table || ' INCLUDING CONSTRAINTS INCLUDING INDEXES INCLUDING DEFAULTS)';
+		
+		-- Set contraints
+		FOR v_column, v_default IN
+			SELECT column_name, REPLACE(column_default, v_source_schema, v_dest_schema) 
+			FROM information_schema.COLUMNS 
+			WHERE table_schema = v_dest_schema AND table_name = rec_table AND column_default LIKE 'nextval(%' || v_source_schema || '%::regclass)'
+		LOOP
+			EXECUTE 'ALTER TABLE ' || v_tablename || ' ALTER COLUMN ' || v_column || ' SET DEFAULT ' || v_default;
+		END LOOP;
+		
+		-- Copy table contents to destination schema
+		EXECUTE 'INSERT INTO ' || v_tablename || ' SELECT * FROM ' || v_source_schema || '.' || rec_table;  
+		
+	END LOOP;
+	
 
 -- fk,check
 
-    FOR rec_fk IN
-        EXECUTE 'SELECT distinct on (conname) conrelid::regclass AS tablename, conname AS constraintname, pg_get_constraintdef(c.oid),
-        replace(pg_get_constraintdef(c.oid),''REFERENCES'',concat(''REFERENCES '','''||v_dest_schema||''',''.'')) AS definition
-        FROM   pg_constraint c JOIN   pg_namespace n ON n.oid = c.connamespace
-        join   information_schema.table_constraints tc ON conname=constraint_name WHERE contype IN (''f'',''c'',''u'')
-        AND nspname = '''||v_source_schema||''' AND  conname not ilike ''%category_type%'';'
-    LOOP
-            raise notice 'rec_fk,%',rec_fk.constraintname;
-        v_query_text:=  'ALTER TABLE '||v_dest_schema || '.' || rec_fk.tablename||' DROP CONSTRAINT IF EXISTS '|| rec_fk.constraintname||' CASCADE;';
-        EXECUTE v_query_text;
-        v_query_text:=  'ALTER TABLE '||v_dest_schema || '.' || rec_fk.tablename||' ADD CONSTRAINT '|| rec_fk.constraintname|| ' '||rec_fk.definition||';';
-        EXECUTE v_query_text;
+	FOR rec_fk IN
+		EXECUTE 'SELECT distinct on (conname) conrelid::regclass AS tablename, conname AS constraintname, pg_get_constraintdef(c.oid),
+		replace(pg_get_constraintdef(c.oid),''REFERENCES'',concat(''REFERENCES '','''||v_dest_schema||''',''.'')) AS definition
+		FROM   pg_constraint c JOIN   pg_namespace n ON n.oid = c.connamespace
+		join   information_schema.table_constraints tc ON conname=constraint_name WHERE contype IN (''f'',''c'',''u'')
+		AND nspname = '''||v_source_schema||''' AND  conname not ilike ''%category_type%'';'
+	LOOP
+			raise notice 'rec_fk,%',rec_fk.constraintname;
+		v_query_text:=  'ALTER TABLE '||v_dest_schema || '.' || rec_fk.tablename||' DROP CONSTRAINT IF EXISTS '|| rec_fk.constraintname||' CASCADE;';
+		EXECUTE v_query_text;
+		v_query_text:=  'ALTER TABLE '||v_dest_schema || '.' || rec_fk.tablename||' ADD CONSTRAINT '|| rec_fk.constraintname|| ' '||rec_fk.definition||';';
+		EXECUTE v_query_text;
 
-    END LOOP;
+	END LOOP;
 
-    --reset sequences
-    FOR rec_seq IN
-        (SELECT seq_ns.nspname as sequence_schema, seq.relname as sequence_name, tab.relname as related_table
-        FROM pg_class seq JOIN pg_namespace seq_ns on seq.relnamespace = seq_ns.oid
-        JOIN pg_depend d ON d.objid = seq.oid AND d.deptype = 'a' 
-        JOIN pg_class tab ON d.objid = seq.oid AND d.refobjid = tab.oid
-        WHERE seq.relkind = 'S' AND seq_ns.nspname = v_source_schema)
-    LOOP
+	--reset sequences
+	FOR rec_seq IN
+		(SELECT seq_ns.nspname as sequence_schema, seq.relname as sequence_name, tab.relname as related_table
+		FROM pg_class seq JOIN pg_namespace seq_ns on seq.relnamespace = seq_ns.oid
+		JOIN pg_depend d ON d.objid = seq.oid AND d.deptype = 'a' 
+		JOIN pg_class tab ON d.objid = seq.oid AND d.refobjid = tab.oid
+		WHERE seq.relkind = 'S' AND seq_ns.nspname = v_source_schema)
+	LOOP
 
-        EXECUTE 'SELECT distinct on (conname) replace(replace(pg_get_constraintdef(c.oid), ''PRIMARY KEY ('',''''),'')'','''') 
-        FROM   pg_constraint c JOIN   pg_namespace n ON n.oid = c.connamespace
-        join   information_schema.table_constraints tc ON conname=constraint_name WHERE contype =''p'' 
-        AND nspname = '''||v_source_schema||''' AND table_name = '''||rec_seq.related_table||''';'
-        INTO v_id_field;
-         
-        IF v_id_field is not null THEN
-            EXECUTE 'SELECT count('||v_id_field||') FROM '||rec_seq.related_table||';'
-            INTO v_count;
-            
-            IF v_count > 0 THEN
-                EXECUTE 'SELECT setval('''||v_dest_schema||'.'||rec_seq.sequence_name||''', 
-                (SELECT max('||v_id_field||')+1 FROM '||rec_seq.related_table||'), true);';
-            END IF;
-        END IF;
+		EXECUTE 'SELECT distinct on (conname) replace(replace(pg_get_constraintdef(c.oid), ''PRIMARY KEY ('',''''),'')'','''') 
+		FROM   pg_constraint c JOIN   pg_namespace n ON n.oid = c.connamespace
+		join   information_schema.table_constraints tc ON conname=constraint_name WHERE contype =''p'' 
+		AND nspname = '''||v_source_schema||''' AND table_name = '''||rec_seq.related_table||''';'
+		INTO v_id_field;
+		 
+		IF v_id_field is not null THEN
+			EXECUTE 'SELECT count('||v_id_field||') FROM '||rec_seq.related_table||';'
+			INTO v_count;
+			
+			IF v_count > 0 THEN
+				EXECUTE 'SELECT setval('''||v_dest_schema||'.'||rec_seq.sequence_name||''', 
+				(SELECT max('||v_id_field||')+1 FROM '||rec_seq.related_table||'), true);';
+			END IF;
+		END IF;
 
-    END LOOP;
+	END LOOP;
 
-    EXECUTE 'SELECT setval('''||v_dest_schema||'.urn_id_seq'', gw_fct_setvalurn(),true);';
+	EXECUTE 'SELECT setval('''||v_dest_schema||'.urn_id_seq'', gw_fct_setvalurn(),true);';
 
-    -- Views
-    FOR rec_view IN
-        EXECUTE 'SELECT table_name, view_definition as definition 
-        FROM information_schema.VIEWS WHERE table_schema = '''||v_source_schema||''''
-    LOOP
+	-- VieSCHEMA_NAME
+	FOR rec_view IN
+		EXECUTE 'SELECT table_name, view_definition as definition 
+		FROM information_schema.VIESCHEMA_NAME WHERE table_schema = '''||v_source_schema||''''
+	LOOP
 
-        EXECUTE 'CREATE VIEW ' || v_dest_schema || '.' || rec_view.table_name || ' AS  
-        '||rec_view.definition||';';
-    END LOOP;
+		EXECUTE 'CREATE VIEW ' || v_dest_schema || '.' || rec_view.table_name || ' AS  
+		'||rec_view.definition||';';
+	END LOOP;
 
-    FOR rec_view IN 
-        EXECUTE 'SELECT schemaname, matviewname, replace(definition, '||quote_literal(v_source_schema)||','||quote_literal(v_dest_schema)||') AS definition 
-        from pg_matviews WHERE schemaname='''||v_source_schema||''''
-    LOOP
-        EXECUTE 'CREATE MATERIALIZED VIEW ' || v_dest_schema || '.' || rec_view.matviewname || ' AS  
-        '||rec_view.definition||';';
-    END LOOP;
+	FOR rec_view IN 
+		EXECUTE 'SELECT schemaname, matviewname, replace(definition, 
+		concat('||quote_literal(v_source_schema)||',''.''),concat('||quote_literal(v_dest_schema)||',''.'')) AS definition 
+		from pg_matvieSCHEMA_NAME WHERE schemaname='''||v_source_schema||''''
+	LOOP
+		EXECUTE 'CREATE MATERIALIZED VIEW ' || v_dest_schema || '.' || rec_view.matviewname || ' AS  
+		'||rec_view.definition||';';
+	END LOOP;
 
-    EXECUTE 'SELECT gw_fct_admin_manage_schema($${"client":{"lang":"ES"}, 
-    "data":{"action":"RENAMESCHEMA","source":"'||v_source_schema||'", "target":"'||v_dest_schema||'"}}$$);';
+	EXECUTE 'SELECT gw_fct_admin_manage_schema($${"client":{"lang":"ES"}, 
+	"data":{"action":"RENAMESCHEMA","source":"'||v_source_schema||'", "target":"'||v_dest_schema||'"}}$$);';
 
-    --Functions
-    FOR rec_fct IN 
-        EXECUTE 'SELECT routine_name,concat(routine_name,''('',string_agg(parameters.data_type,'', ''),'')'') FROM information_schema.routines
-       LEFT JOIN information_schema.parameters ON routines.specific_name=parameters.specific_name
-        WHERE routines.specific_schema='''||v_source_schema||''' and routine_name!=''audit_function'' and routine_name!=''gw_fct_repair_arc''
-        group by routine_name'
-    LOOP
+	--Functions
+	raise notice 'FCT';
+	EXECUTE 'SELECT replace(''SCHEMA_NAMEoftware'', '''||v_source_schema||''','''||v_dest_schema||''')'
+	into v_software;
 
-        EXECUTE 'select * from pg_get_functiondef('''||v_source_schema||'.'|| rec_fct.routine_name||'''::regproc)'
-        INTO v_fct_definition;
-        v_fct_definition = REPLACE (v_fct_definition,v_source_schema, v_dest_schema);
-        EXECUTE v_fct_definition;
+	FOR rec_fct IN 
+	
+		EXECUTE 'SELECT routine_name,concat(routine_name,''('',string_agg(parameters.data_type,'', ''),'')'') FROM information_schema.routines
+	   LEFT JOIN information_schema.parameters ON routines.specific_name=parameters.specific_name
+		WHERE routines.specific_schema='''||v_source_schema||''' and routine_name!=''audit_function'' and routine_name!=''gw_fct_repair_arc''
+		group by routine_name'
+	LOOP
+	raise notice 'rec_fct.routine_name,%',rec_fct.routine_name;
+		EXECUTE 'select * from pg_get_functiondef('''||v_source_schema||'.'|| rec_fct.routine_name||'''::regproc)'
+		INTO v_fct_definition;
+		v_fct_definition = REPLACE (v_fct_definition,v_source_schema, v_dest_schema);
+		
+		IF v_fct_definition ~* v_software THEN
+		v_fct_definition = REPLACE (v_fct_definition,v_software, 'SCHEMA_NAMEoftware');
+		END IF; 
+		EXECUTE v_fct_definition;
 
  
-    END LOOP;
+	END LOOP;
 
-    --trg
-    FOR rec_trg IN 
-        select event_object_schema as table_schema, event_object_table as table_name, trigger_schema, trigger_name,
-        string_agg(event_manipulation, ',') as event, action_timing as activation, action_condition as condition, action_statement as definition
-        from information_schema.triggers where event_object_schema = v_source_schema group by 1,2,3,4,6,7,8 order by table_schema, table_name
-    LOOP
+	--trg
+	FOR rec_trg IN 
+		select event_object_schema as table_schema, event_object_table as table_name, trigger_schema, trigger_name,
+		string_agg(event_manipulation, ',') as event, action_timing as activation, action_condition as condition, action_statement as definition
+		from information_schema.triggers where event_object_schema = v_source_schema group by 1,2,3,4,6,7,8 order by table_schema, table_name
+	LOOP
 
-        SELECT string_agg(event_object_column, ',') INTO v_trg_fields FROM information_schema.triggered_update_columns 
-        WHERE event_object_schema = v_source_schema and event_object_table=rec_trg.table_name AND trigger_name = rec_trg.trigger_name;
-
-
-        EXECUTE 'select replace('''||rec_trg.event||''', '','', '' OR '')'
-        INTO v_replace_query;
-        
-        IF v_trg_fields IS NULL THEN 
-
-            rec_trg.definition = replace(replace(rec_trg.definition,'EXECUTE PROCEDURE',''),v_source_schema,v_dest_schema);
-
-            EXECUTE 'CREATE TRIGGER '||rec_trg.trigger_name||' '||rec_trg.activation||' '||v_replace_query||'
-            ON '||v_dest_schema||'.'||rec_trg.table_name||' FOR EACH ROW EXECUTE PROCEDURE '|| rec_trg.definition||';';
-          
-        ELSE   
-
-            EXECUTE 'select replace('''||v_replace_query||''', ''UPDATE'', '' UPDATE  OF '||v_trg_fields||''')'
-            INTO v_replace_query; 
-       
-
-            EXECUTE 'CREATE TRIGGER '||rec_trg.trigger_name||' '||rec_trg.activation||' '||v_replace_query||' ON 
-            '||v_dest_schema||'.'||rec_trg.table_name||' FOR EACH ROW '|| rec_trg.definition||';';
-
-        END IF;
-
-    END LOOP;
+		SELECT string_agg(event_object_column, ',') INTO v_trg_fields FROM information_schema.triggered_update_columns 
+		WHERE event_object_schema = v_source_schema and event_object_table=rec_trg.table_name AND trigger_name = rec_trg.trigger_name;
 
 
-    -- admin role permissions
-    PERFORM gw_fct_admin_role_permissions();
+		EXECUTE 'select replace('''||rec_trg.event||''', '','', '' OR '')'
+		INTO v_replace_query;
+		
+		IF v_trg_fields IS NULL THEN 
 
-    v_result_info := COALESCE(v_result, '{}'); 
-    v_result_info = concat ('{"geometryType":"", "values":',v_result_info, '}');
+			rec_trg.definition = replace(replace(rec_trg.definition,'EXECUTE PROCEDURE',''),v_source_schema,v_dest_schema);
 
-    --geometry
+			EXECUTE 'CREATE TRIGGER '||rec_trg.trigger_name||' '||rec_trg.activation||' '||v_replace_query||'
+			ON '||v_dest_schema||'.'||rec_trg.table_name||' FOR EACH ROW EXECUTE PROCEDURE '|| rec_trg.definition||';';
+		  
+		ELSE   
 
-    v_result_line = '{"geometryType":"", "features":[]}';
-    v_result_polygon = '{"geometryType":"", "features":[]}';
-    v_result_point = '{"geometryType":"", "features":[]}';
+			EXECUTE 'select replace('''||v_replace_query||''', ''UPDATE'', '' UPDATE  OF '||v_trg_fields||''')'
+			INTO v_replace_query; 
+	   
+
+			EXECUTE 'CREATE TRIGGER '||rec_trg.trigger_name||' '||rec_trg.activation||' '||v_replace_query||' ON 
+			'||v_dest_schema||'.'||rec_trg.table_name||' FOR EACH ROW '|| rec_trg.definition||';';
+
+		END IF;
+
+	END LOOP;
+
+
+	-- admin role permissions
+	PERFORM gw_fct_admin_role_permissions();
+
+	v_result_info := COALESCE(v_result, '{}'); 
+	v_result_info = concat ('{"geometryType":"", "values":',v_result_info, '}');
+
+	--geometry
+
+	v_result_line = '{"geometryType":"", "features":[]}';
+	v_result_polygon = '{"geometryType":"", "features":[]}';
+	v_result_point = '{"geometryType":"", "features":[]}';
 
 --  Return
-    RETURN ('{"status":"Accepted", "message":{"priority":1, "text":"Copy schema done successfully"}, "version":"'||v_version||'"'||
-             ',"body":{"form":{}'||
-             ',"data":{ "info":'||v_result_info||','||
-                '"setVisibleLayers":[]'||','||
-                '"point":'||v_result_point||','||
-                '"line":'||v_result_line||','||
-                '"polygon":'||v_result_polygon||'}'||
-               '}'||
-        '}')::json;
-
-    EXCEPTION WHEN OTHERS THEN
-        GET STACKED DIAGNOSTICS v_error_context = PG_EXCEPTION_CONTEXT;
-        RETURN ('{"status":"Failed","NOSQLERR":' || to_json(SQLERRM) || ',"SQLSTATE":' || to_json(SQLSTATE) ||',"SQLCONTEXT":' || to_json(v_error_context) || '}')::json;
+	RETURN ('{"status":"Accepted", "message":{"priority":1, "text":"Copy schema done successfully"}, "version":"'||v_version||'"'||
+			 ',"body":{"form":{}'||
+			 ',"data":{ "info":'||v_result_info||','||
+				'"setVisibleLayers":[]'||','||
+				'"point":'||v_result_point||','||
+				'"line":'||v_result_line||','||
+				'"polygon":'||v_result_polygon||'}'||
+			   '}'||
+		'}')::json;
+	EXCEPTION WHEN OTHERS THEN
+		GET STACKED DIAGNOSTICS v_error_context = PG_EXCEPTION_CONTEXT;
+		RETURN ('{"status":"Failed","NOSQLERR":' || to_json(SQLERRM) || ',"SQLSTATE":' || to_json(SQLSTATE) ||',"SQLCONTEXT":' || to_json(v_error_context) || '}')::json;
 
  
 END;
