8acc42efc8681aed789d6ac2a0e6fc606883fe12
Xavier Torret <xtorret@bgeo.es>
2020-04-09 14:04:00 +0200

    Enhance checkvalve table

--- dbmodel/updates/33/33035/ws/ddl.sql
+++ dbmodel/updates/33/33035/ws/ddl.sql
@@ -10,8 +10,7 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 --2020/04/04
 
 CREATE TABLE anl_mincut_checkvalve(
-  id serial NOT NULL,
   node_id character varying(16) NOT NULL,
   to_arc character varying(16) NOT NULL,
-  CONSTRAINT anl_mincut_checkvalve_pkey PRIMARY KEY (id)
+  CONSTRAINT anl_mincut_checkvalve_pkey PRIMARY KEY (node_id)
 );
