a7f58384cc1dd9bbf8ba5b48950d1dedb9ec8486
Albert Bofill <abofill@bgeo.es>
2020-04-21 09:52:31 +0200

    Updates on utils raster views

--- dbmodel/corporate/utils/updates/3.3/3.3.043/ddlview.sql
+++ dbmodel/corporate/utils/updates/3.3/3.3.043/ddlview.sql
@@ -8,40 +8,32 @@ This version of Giswater is provided by Giswater Association
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
 
+DROP VIEW IF EXISTS v_ext_raster_dem;
 DROP VIEW IF EXISTS ext_raster_dem;
+
 CREATE OR REPLACE VIEW ext_raster_dem AS 
-SELECT raster_dem.id,
-	code,
-	alias,
-  raster_type, 
-  cat_raster.descript,
-  source,
-  provider,
-  year,
+ SELECT raster_dem.id,
     raster_dem.rast,
-    a.expl_id,
     raster_dem.rastercat_id,
     raster_dem.envelope
-   FROM 
-    exploitation a, utils.raster_dem
-    JOIN utils.cat_raster ON cat_raster.id=rastercat_id;
-    
+   FROM utils.raster_dem;
 
-DROP VIEW IF EXISTS v_ext_raster_dem;
+
 CREATE OR REPLACE VIEW v_ext_raster_dem AS 
-SELECT r.id,
-r.code,
-r.alias,
-r.raster_type, 
-r.descript,
-r.source,
-r.provider,
-r.year,
+SELECT DISTINCT ON (r.id) r.id,
+c.code,
+c.alias,
+c.raster_type, 
+c.descript,
+c.source,
+c.provider,
+c.year,
 r.rast,
 r.rastercat_id,
 r.envelope
 FROM 
 v_edit_exploitation a, ext_raster_dem r
+JOIN utils.cat_raster c ON c.id=rastercat_id
 WHERE st_dwithin(r.envelope, a.the_geom, 0::double precision);
 
 
