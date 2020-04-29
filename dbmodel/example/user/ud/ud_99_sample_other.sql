e4ef519b58d028f5f80f152fc537cf2a3c1e5f27
Albert Bofill <abofill@bgeo.es>
2020-04-29 11:00:02 +0200

    Minor syntax error on example data

--- dbmodel/example/user/ud/ud_99_sample_other.sql
+++ dbmodel/example/user/ud/ud_99_sample_other.sql
@@ -405,7 +405,7 @@ UPDATE config_api_form_fields set layoutname = 'lyt_data_1' WHERE column_id = 'w
 UPDATE config_api_form_fields set layoutname = 'lyt_data_1' WHERE column_id = 'width' AND formname ='ve_node_weir';
 
 UPDATE config_param_system SET value='TRUE' WHERE parameter='sys_raster_dem';
-INSERT INTO config_param_user (parameter, value, cur_user) VALUES ('edit_upser_elevation_from_dem', 'true', current_user);
+INSERT INTO config_param_user (parameter, value, cur_user) VALUES ('edit_upser_elevation_from_dem', 'true', current_user)
 ON CONFLICT (parameter, cur_user) DO NOTHING;
 
 INSERT INTO config_param_user (parameter, value, cur_user) VALUES ('qgis_form_docker', 'true', current_user)
