81d9971331b68f53427281f141c56a9493e47dcf
David Erill <daviderill79@gmail.com>
2020-05-01 13:43:11 +0200

    #759 Add task 'Import RPT to database'

--- actions/go2epa.py
+++ actions/go2epa.py
@@ -24,7 +24,7 @@ from functools import partial
 from .. import utils_giswater
 from .api_go2epa_options import Go2EpaOptions
 from .api_parent import ApiParent
-from .gw_task import GwTask
+from .task_import_rpt import TaskImportRpt
 from .update_sql import UpdateSQL
 from ..ui_manager import EpaResultCompareSelector, EpaResultManager, FileManager, HydrologySelector, Multirow_selector
 
@@ -465,124 +465,6 @@ class Go2Epa(ApiParent):
         del file1
 
 
-    def insert_rpt_into_db(self, folder_path=None):
-
-        self.controller.log_info(f"Import RPT to database - Start")
-
-        _file = open(folder_path, "r+")
-        full_file = _file.readlines()
-        progress = 0
-        self.dlg_go2epa.progressBar.setFormat("The RPT file is begin imported...")
-        self.dlg_go2epa.progressBar.setAlignment(Qt.AlignCenter)
-        self.dlg_go2epa.progressBar.setValue(progress)
-
-        # Create dict with sources
-        sql = "SELECT tablename, target FROM sys_csv2pg_config WHERE pg2csvcat_id = '11';"
-        rows = self.controller.get_rows(sql)
-        sources = {}
-        for row in rows:
-            aux = row[1].replace('{','').replace('}', '')
-            item = aux.split(',')
-            for i in item:
-                sources[i.strip()]=row[0].strip()
-
-        # While we don't find a match with the source, source and csv40 must be null
-        source = "null"
-        csv40 = "null"
-        sql = ""
-        row_count = sum(1 for rows in full_file)  # @UnusedVariable
-
-        self.controller.log_info(f"Import RPT to database - row_count: {row_count}")
-
-        self.task_rpt_to_db = GwTask('Import RPT to database')
-        QgsApplication.taskManager().addTask(self.task_rpt_to_db)
-
-        for line_number, row in enumerate(full_file):
-
-            self.task_rpt_to_db.setProgress((line_number * 100) / row_count)
-            progress += 1
-            if '**' in row or '--' in row:
-                continue
-
-            row = row.rstrip()
-            dirty_list = row.split(' ')
-
-            # Clean unused items
-            for x in range(len(dirty_list) - 1, -1, -1):
-                if dirty_list[x] == '':
-                    dirty_list.pop(x)
-
-            sp_n = []
-            if len(dirty_list) > 0:
-                for x in range(0, len(dirty_list)):
-                    if bool(re.search('[0-9][-]\d{1,2}[.]]*', str(dirty_list[x]))):
-                        last_index = 0
-                        for i, c in enumerate(dirty_list[x]):
-                            if "-" == c:
-                                aux = dirty_list[x][last_index:i]
-                                last_index = i
-                                sp_n.append(aux)
-                        aux = dirty_list[x][last_index:i]
-                        sp_n.append(aux)
-
-                    elif bool(re.search('(\d\..*\.\d)', str(dirty_list[x]))):
-                        if 'Version' not in dirty_list and 'VERSION' not in dirty_list:
-                            self.controller.log_info(f"Error near line {line_number+1} -> {dirty_list}")
-                            message = ("The rpt file has a heavy inconsistency. As a result it's not posible to import it. " 
-                                  "Columns are overlaped one againts other, this is a not valid simulation. " 
-                                  "Please ckeck and fix it before continue")
-                            self.controller.show_message(message, 1)
-                            _file.close()
-                            del _file
-                            return False
-                    else:
-                        sp_n.append(dirty_list[x])
-
-            # Find strings into dict and set source column
-            for k, v in sources.items():
-                try:
-                    if k in (f'{sp_n[0]} {sp_n[1]}', f'{sp_n[0]}'):
-                        source = "'" + v + "'"
-                        _time = re.compile('^([012]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$')
-                        if _time.search(sp_n[3]):
-                            csv40 = "'" + sp_n[3] + "'"
-                except IndexError:
-                    pass
-                except Exception as e:
-                    self.controller.log_info(type(e).__name__)
-
-            if len(sp_n) > 0:
-                sql += f"INSERT INTO temp_csv2pg (csv2pgcat_id, source, csv40, "
-                values = f"VALUES(11, {source}, {csv40}, "
-                for x in range(0, len(sp_n)):
-                    if "''" not in sp_n[x]:
-                        sql += f"csv{x + 1}, "
-                        value = "'" + sp_n[x].strip().replace("\n", "") + "', "
-                        values += value.replace("''", "null")
-                    else:
-                        sql += f"csv{x + 1}, "
-                        values = "VALUES(null, "
-                sql = sql[:-2] + ") "
-                values = values[:-2] + ");\n"
-                sql += values
-
-            # Execute SQL every in batches of 20000 records
-            if progress % 20000 == 0:
-                self.controller.log_info(f"insert_rpt_into_db - progress: {progress}")
-                self.controller.execute_sql(sql)
-                sql = ""
-
-        if sql != "":
-            self.controller.execute_sql(sql)
-
-        _file.close()
-        del _file
-
-        self.controller.log_info(f"Import RPT to database - End")
-
-        return True
-
-
     def show_widgets(self, visible=False):
 
         self.dlg_go2epa.progressBar.setVisible(visible)
@@ -632,10 +514,6 @@ class Go2Epa(ApiParent):
         elif self.project_type in 'ud':
             opener = self.plugin_dir + "/epa/ud_swmm50022.exe"
 
-        self.show_widgets(True)
-
-        counter = 0
-
         extras = '"iterative":"off"'
         if is_iterative:
             extras = '"iterative":"start"'
@@ -645,8 +523,10 @@ class Go2Epa(ApiParent):
         body = self.create_body(extras=extras)
 
         # Start process
+        counter = 0
         _continue = True
         while _continue:
+
             if self.imports_canceled:
                 break
 
@@ -657,15 +537,11 @@ class Go2Epa(ApiParent):
                 self.controller.show_info_box(message)
                 return
 
-            steps = f"{counter}/{complet_result['steps']}"
-            utils_giswater.setWidgetText(self.dlg_go2epa, self.dlg_go2epa.lbl_counter, steps)
             counter += 1
             self.controller.log_info(f"{counter}:{complet_result['steps']}:{complet_result['continue']}")
-            _continue = False
             common_msg = ""
             message = None
-            if str(complet_result['continue']).lower() == 'true':
-                _continue = True
+            _continue = (str(complet_result['continue']).lower() == 'true')
 
             # Export to inp file
             if export_inp is True:
@@ -709,63 +585,33 @@ class Go2Epa(ApiParent):
             if import_result is True:
                 if os.path.exists(self.file_rpt):
 
-                    # delete previous values of user on temp table
-                    sql = ("DELETE FROM temp_csv2pg "
-                           "WHERE user_name = current_user AND csv2pgcat_id = 11")
-                    self.controller.execute_sql(sql)
+                    # Set background task 'Import RPT to database'
+                    description = f"Import RPT to database: result_name = '{self.result_name}'"
+                    self.task_rpt_to_db = TaskImportRpt(description, self.controller, self.dlg_go2epa, self.file_rpt,
+                        self.result_name)
+                    QgsApplication.taskManager().addTask(self.task_rpt_to_db)
+                    QgsApplication.taskManager().triggerTask(self.task_rpt_to_db)
 
-                    # Importing file to temporal table
-                    status = self.insert_rpt_into_db(self.file_rpt)
-                    if not status:
-                        self.show_widgets(False)
-                        self.check_result_id()
-                        return
-
-                    # Call function gw_fct_rpt2pg_main
-                    function_name = 'gw_fct_rpt2pg_main'
-                    extras = '"iterative":"disabled"'
-                    if is_iterative and _continue:
-                        extras = '"iterative":"enabled"'
-                    extras += f', "resultId":"{self.result_name}"'
-                    extras += f', "currentStep":"{counter}"'
-                    extras += f', "continue":"{_continue}"'
-                    body = self.create_body(extras=extras)
-                    sql = f"SELECT {function_name}({body})::text"
-                    row = self.controller.get_row(sql)
-                    if not row or row[0] is None:
-                        self.controller.show_warning("NOT ROW FOR: " + sql)
-                        message = "Import failed"
-                        self.controller.show_info_box(message)
-                        return
-                    else:
-                        rpt_result = [json.loads(row[0], object_pairs_hook=OrderedDict)]
-                        if 'status' in rpt_result[0]:
-                            if rpt_result[0]['status'] == "Accepted":
-                                if 'body' in rpt_result[0]:
-                                    if 'data' in rpt_result[0]['body']:
-                                        self.add_layer.add_temp_layer(self.dlg_go2epa, rpt_result[0]['body']['data'], 'RPT results', True, True, 1, False)
-                        message = rpt_result[0]['message']['text']
-
-                    # final message
-                    common_msg += "Import RPT finished."
                 else:
                     msg = "Can't export rpt, File not found"
                     self.controller.show_warning(msg, parameter=self.file_rpt)
 
+            """
             # Create new body for next iteration
             extras = '"iterative":"ongoing"'
             extras += f', "resultId":"{self.result_name}"'
             extras += f', "useNetworkGeom":"{net_geom}"'
             extras += f', "dumpSubcatch":"{export_subcatch}"'
             body = self.create_body(extras=extras)
-
+            """
+        """
         if common_msg != "" and self.imports_canceled is False:
             self.controller.show_info(common_msg)
         if message is not None and self.imports_canceled is False:
             self.controller.show_info_box(message)
-
-        self.show_widgets(False)
-        self.check_result_id()
+        """
+        #self.show_widgets(False)
+        #self.check_result_id()
 
 
     def set_completer_result(self, widget, viewname, field_name):
