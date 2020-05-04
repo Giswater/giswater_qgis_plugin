0ac2044df6abfcb9642ca933b05d863467270598
Barbara <bbrzepka@bgeo.es>
2020-05-04 13:55:34 +0200

    minor bug fix on cat_feature trg

--- dbmodel/utils/ftrg/gw_trg_cat_feature.sql
+++ dbmodel/utils/ftrg/gw_trg_cat_feature.sql
@@ -101,7 +101,7 @@ BEGIN
 			DELETE FROM audit_cat_param_user WHERE id = concat(lower(OLD.id),'_vdefault');        
 		END IF;
 
-		INSERT INTO audit_cat_param_user(id, formname, descript, sys_role_id, label, isenabled, layoutname, layout_order, 
+		INSERT INTO audit_cat_param_user(id, formname, description, sys_role_id, label, isenabled, layoutname, layout_order, 
 		dv_querytext, feature_field_id, project_type, isparent, isautoupdate, datatype, widgettype, ismandatory, isdeprecated, iseditable)
 		VALUES (concat(v_id,'_vdefault'),'config',concat ('Value default catalog for ',v_id,' cat_feature'), 'role_edit', concat ('Default catalog for ', v_id), true, v_layout ,v_layout_order,
 		v_querytext, v_feature_field_id, lower(v_projecttype),false,false,'text', 'combo', true, false, true)
