fbe4a0268f1d5e5a37aedb79cc143b69a49ddf95
Xavier Torret <xtorret@bgeo.es>
2020-05-17 02:02:16 +0200

    New variable to manage guide map

--- dbmodel/updates/33/33037/utils/dml.sql
+++ dbmodel/updates/33/33037/utils/dml.sql
@@ -17,3 +17,12 @@ UPDATE ws.audit_cat_table SET sys_role_id = 'role_master' WHERE id = 'config_for
 INSERT INTO config_param_system (parameter, value, data_type, context, descript, label, isenabled, layout_id, layout_order, project_type, datatype, widgettype, ismandatory, isdeprecated) 
 VALUES ('mapzones_dynamic_symbology', 'false', 'boolean', 'system', 'Mapzones dynamic symbology', null, FALSE, null, null, 'ws', 'boolean', null, null, false) 
 ON CONFLICT (parameter) DO NOTHING;
+
+--2020/05/17
+INSERT INTO audit_cat_param_user
+VALUES ('qgis_init_guide_map','config','If true, qgis starts with all extension for exploitations', 'role_basic', NULL, NULL, 
+'QGIS init guide map', NULL, NULL, true, 8, 20, 'utils', false, NULL, NULL, NULL, 
+false, 'boolean', 'check', true, NULL, false, NULL, NULL, NULL, NULL, NULL, NULL, NULL, false)
+ON conflict (id) DO NOTHING;
+
+
