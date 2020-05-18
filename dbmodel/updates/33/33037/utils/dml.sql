f658c97764ae5846d2ccd518dd9926b399382a4a
Barbara <bbrzepka@bgeo.es>
2020-05-18 14:46:27 +0200

    bug fix on schema name on updates

--- dbmodel/updates/33/33037/utils/dml.sql
+++ dbmodel/updates/33/33037/utils/dml.sql
@@ -11,7 +11,7 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 UPDATE audit_cat_table SET sys_role_id = 'role_basic'  WHERE id = 'audit_check_data';
 
 --2020/05/09
-UPDATE ws.audit_cat_table SET sys_role_id = 'role_master' WHERE id = 'config_form_fields';
+UPDATE audit_cat_table SET sys_role_id = 'role_master' WHERE id = 'config_form_fields';
 
 --2020/05/14
 INSERT INTO config_param_system (parameter, value, data_type, context, descript, label, isenabled, layout_id, layout_order, project_type, datatype, widgettype, ismandatory, isdeprecated) 
