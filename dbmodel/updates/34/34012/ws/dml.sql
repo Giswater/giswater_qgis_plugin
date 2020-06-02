cd1d020efe6d21d43211e67813e01af5b2ff6d22
Edgar Fuste <efuste@bgeo.es>
2020-06-02 11:23:20 +0200

    Enhancement: insert config for api selectors on config_param_system

--- dbmodel/updates/34/34012/ws/dml.sql
+++ dbmodel/updates/34/34012/ws/dml.sql
@@ -13,3 +13,15 @@ WHERE id = 'presszone_vdefault';
 
  UPDATE config_form_fields SET  dv_querytext = 'SELECT presszone.presszone_id AS id, presszone.name AS idval FROM presszone WHERE presszone_id IS NOT NULL' 
  WHERE columnname = 'presszone_id';
+ 
+ -- 2020/06/02
+ --SELECT setval('config_param_system_id_seq', (SELECT max(id) FROM config_param_system), true);
+INSERT INTO config_param_system (parameter, value, context,  descript, label, project_type, datatype, isdeprecated) 
+VALUES ('basic_selector_hydrometer', '{"table":"ext_rtc_hydrometer_state", "selector":"selector_hydrometer", "table_id":"id",  "selector_id":"state_id",  "label":"id, '' - '', name", 
+ "manageAll":true, "query_filter":""}', 'system', 'Select which label to display for selectors', 'Selector labels:', 'ws', 'json', FALSE);
+ 
+  INSERT INTO config_typevalue (typevalue, id, idval) 
+VALUES ('tabname_typevalue', 'tabHydrometer', 'tabHydrometer');
+
+ INSERT INTO config_form_tabs (formname,tabname, label, tooltip, sys_role, device) 
+VALUES ('hydrometer', 'tabHydrometer', 'Hydrometer', 'Hydrometer Selector', 'role_basic', 4);
