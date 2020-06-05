a46e5ed85de4df5effdb2b6b35427982e1ec3920
Xavier Torret <xtorret@bgeo.es>
2020-06-05 09:02:31 +0200

    Enhance profile

--- dbmodel/updates/34/34013/utils/dml.sql
+++ dbmodel/updates/34/34013/utils/dml.sql
@@ -21,3 +21,11 @@ INSERT INTO config_typevalue VALUES ('formtemplate_typevalue','visit_class','vis
 INSERT INTO config_info_layer VALUES ('v_edit_om_visit',FALSE,NULL,TRUE,'v_edit_om_visit','visit','Visit',10);
 
 INSERT INTO config_info_layer_x_type  VALUES ('v_edit_om_visit', 1, 'v_edit_om_visit');
+
+
+INSERT INTO om_typevalue VALUES ('profile_papersize','0','CUSTOM',null);
+INSERT INTO om_typevalue VALUES ('profile_papersize','1','DIN A5 - 210x148',null,'{"xdim":210, "ydim":148}');
+INSERT INTO om_typevalue VALUES ('profile_papersize','2','DIN A4 - 297x210',null,'{"xdim":297, "ydim":210}');
+INSERT INTO om_typevalue VALUES ('profile_papersize','3','DIN A3 - 420x297',null,'{"xdim":420, "ydim":297}');
+INSERT INTO om_typevalue VALUES ('profile_papersize','4','DIN A2 - 594x420',null,'{"xdim":594, "ydim":420}');
+INSERT INTO om_typevalue VALUES ('profile_papersize','5','DIN A1 - 840x594',null,'{"xdim":840, "ydim":594}');
