a474a406087964d11a8a3ec76f05552273adcbd7
Albert Bofill <abofill@bgeo.es>
2020-06-05 11:19:07 +0200

    Drop non-standard views in audit_cat_table

--- dbmodel/updates/33/33038/utils/dml.sql
+++ dbmodel/updates/33/33038/utils/dml.sql
@@ -12,3 +12,5 @@ UPDATE arc SET feature_type='ARC' WHERE feature_type IS NULL;
 UPDATE connec SET feature_type='CONNEC' WHERE feature_type IS NULL;
 UPDATE element SET feature_type='ELEMENT' WHERE feature_type IS NULL;
 UPDATE node SET feature_type='NODE' WHERE feature_type IS NULL;
+
+DELETE FROM audit_cat_table WHERE id IN ('v_plan_psector_arc_affect', 'v_plan_psector_arc_current', 'v_plan_psector_arc_planif');
