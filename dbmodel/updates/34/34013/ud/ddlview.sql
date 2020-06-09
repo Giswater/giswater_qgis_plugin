01168c0c4ec766f01cc23d61d8338e45602fcf82
Xavier Torret <xtorret@bgeo.es>
2020-06-09 12:28:13 +0200

    Bug fix on view ui for rpt

--- dbmodel/updates/34/34013/ud/ddlview.sql
+++ dbmodel/updates/34/34013/ud/ddlview.sql
@@ -10,7 +10,7 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 
 CREATE OR REPLACE VIEW v_ui_rpt_cat_result AS 
- SELECT rpt_cat_result.id,
+ SELECT
     rpt_cat_result.result_id,
     rpt_cat_result.flow_units,
     rpt_cat_result.rain_runof,
@@ -41,3 +41,66 @@ CREATE TRIGGER gw_trg_ui_rpt_cat_result
   FOR EACH ROW
   EXECUTE PROCEDURE gw_trg_ui_rpt_cat_result();
 
+
+
+CREATE OR REPLACE VIEW v_anl_flow_arc AS 
+ SELECT anl_arc.id,
+    anl_arc.arc_id,
+    anl_arc.arccat_id AS arc_type,
+        CASE
+            WHEN anl_arc.fid = 220 THEN 'Flow trace'::text
+            WHEN anl_arc.fid = 221 THEN 'Flow exit'::text
+            ELSE NULL::text
+        END AS context,
+    anl_arc.expl_id,
+    anl_arc.the_geom
+   FROM selector_expl,
+    anl_arc
+  WHERE anl_arc.expl_id = selector_expl.expl_id AND selector_expl.cur_user = "current_user"()::text AND anl_arc.cur_user::name = "current_user"() AND (anl_arc.fid = 220 OR anl_arc.fid = 221);
+
+
+
+CREATE OR REPLACE VIEW v_anl_flow_connec AS 
+ SELECT connec.connec_id,
+        CASE
+            WHEN anl_arc.fid = 220 THEN 'Flow trace'::text
+            WHEN anl_arc.fid = 221 THEN 'Flow exit'::text
+            ELSE NULL::text
+        END AS context,
+    anl_arc.expl_id,
+    connec.the_geom
+   FROM anl_arc
+     JOIN connec ON anl_arc.arc_id::text = connec.arc_id::text
+     JOIN selector_expl ON anl_arc.expl_id = selector_expl.expl_id AND selector_expl.cur_user = "current_user"()::text AND anl_arc.cur_user::name = "current_user"() AND (anl_arc.fid = 220 OR anl_arc.fid = 221);
+
+
+
+CREATE OR REPLACE VIEW v_anl_flow_gully AS 
+ SELECT v_gully.gully_id,
+        CASE
+            WHEN anl_arc.fid = 220 THEN 'Flow trace'::text
+            WHEN anl_arc.fid = 221 THEN 'Flow exit'::text
+            ELSE NULL::text
+        END AS context,
+    anl_arc.expl_id,
+    v_gully.the_geom
+   FROM anl_arc
+     JOIN v_gully ON anl_arc.arc_id::text = v_gully.arc_id::text
+     JOIN selector_expl ON anl_arc.expl_id = selector_expl.expl_id AND selector_expl.cur_user = "current_user"()::text AND anl_arc.cur_user::name = "current_user"() AND (anl_arc.fid = 220 OR anl_arc.fid = 221);
+
+
+
+CREATE OR REPLACE VIEW v_anl_flow_node AS 
+ SELECT anl_node.id,
+    anl_node.node_id,
+    anl_node.nodecat_id AS node_type,
+        CASE
+            WHEN anl_node.fid = 220 THEN 'Flow trace'::text
+            WHEN anl_node.fid = 221 THEN 'Flow exit'::text
+            ELSE NULL::text
+        END AS context,
+    anl_node.expl_id,
+    anl_node.the_geom
+   FROM selector_expl,
+    anl_node
+  WHERE anl_node.expl_id = selector_expl.expl_id AND selector_expl.cur_user = "current_user"()::text AND anl_node.cur_user::name = "current_user"() AND (anl_node.fid = 220 OR anl_node.fid = 221);
