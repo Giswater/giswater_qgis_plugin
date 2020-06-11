9d5c8b402d090d493e8f2e55e02ae4e1e99379d8
Xavier Torret <xtorret@bgeo.es>
2020-06-11 11:20:11 +0200

    Bug fix on multipump exportation

--- dbmodel/ws/fct/ws_gw_fct_pg2epa_pump_additional.sql
+++ dbmodel/ws/fct/ws_gw_fct_pg2epa_pump_additional.sql
@@ -68,8 +68,9 @@ BEGIN
 				angle=(ST_Azimuth(ST_startpoint(arc_rec.the_geom), ST_endpoint(arc_rec.the_geom)))+1.57;
 			else 
 				angle=(ST_Azimuth(ST_startpoint(arc_rec.the_geom), ST_endpoint(arc_rec.the_geom)))-1.57;
-				pump_order = (pump_rec.order_id);
 			end if;
+
+            pump_order = (pump_rec.order_id);
 
 			-- Copiyng values from patter arc
 			record_new_arc.node_1 = arc_rec.node_1;
@@ -81,18 +82,18 @@ BEGIN
 			
 			-- Geometry construction from pattern arc
 			-- intermediate variables
-			n1_geom = ST_LineInterpolatePoint(arc_rec.the_geom, 0.100000);
-			n2_geom = ST_LineInterpolatePoint(arc_rec.the_geom, 0.900000);
-			dist = (ST_Distance(ST_transform(ST_startpoint(arc_rec.the_geom),rec.epsg), ST_LineInterpolatePoint(arc_rec.the_geom, 0.2000000))); 
+			n1_geom = ST_LineInterpolatePoint(arc_rec.the_geom, 0.500000);
+			n2_geom = ST_LineInterpolatePoint(arc_rec.the_geom, 0.500000);
+			dist = (ST_Distance(ST_transform(ST_startpoint(arc_rec.the_geom),rec.epsg), ST_LineInterpolatePoint(arc_rec.the_geom, 0.5000000))); 
 
 			--create point1
-			yp1 = ST_y(n1_geom)-(cos(angle))*dist*0.10000*(pump_order::float);
-			xp1 = ST_x(n1_geom)-(sin(angle))*dist*0.10000*(pump_order::float);
+			xp1 = ST_x(n1_geom)-(sin(angle))*dist*0.50000*(pump_order::float);
+			yp1 = ST_y(n1_geom)-(cos(angle))*dist*0.50000*(pump_order::float);
 			raise notice' % % %', cos(angle), dist, pump_order::float;
 			
 			--create point2
-			yp2 = ST_y(n2_geom)-cos(angle)*dist*0.100000*(pump_order::float);
-			xp2 = ST_x(n2_geom)-sin(angle)*dist*0.100000*(pump_order::float);
+			xp2 = ST_x(n2_geom)-sin(angle)*dist*0.500000*(pump_order::float);
+		    yp2 = ST_y(n2_geom)-cos(angle)*dist*0.500000*(pump_order::float);
 
 			p1_geom = ST_SetSRID(ST_MakePoint(xp1, yp1),rec.epsg);	
 			p2_geom = ST_SetSRID(ST_MakePoint(xp2, yp2),rec.epsg);	
