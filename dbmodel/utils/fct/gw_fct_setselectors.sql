b178fd1981271a8333b935721c05f88f057b339d
Xavier Torret <xtorret@bgeo.es>
2020-06-11 14:34:12 +0200

    Bug on setselectors when user has more than on expl

--- dbmodel/utils/fct/gw_fct_setselectors.sql
+++ dbmodel/utils/fct/gw_fct_setselectors.sql
@@ -55,7 +55,7 @@ BEGIN
 	
 	-- get expl from muni
 	IF v_selectortype = 'explfrommuni' THEN
-		v_expl = (SELECT expl_id FROM exploitation e, ext_municipality m WHERE st_dwithin(st_centroid(e.the_geom), m.the_geom, 0) AND muni_id::text = v_id::text);
+		v_expl = (SELECT expl_id FROM exploitation e, ext_municipality m WHERE st_dwithin(st_centroid(e.the_geom), m.the_geom, 0) AND muni_id::text = v_id::text limit 1);
 		EXECUTE 'DELETE FROM selector_expl WHERE cur_user = current_user';
 		EXECUTE 'INSERT INTO selector_expl (expl_id, cur_user) VALUES('|| v_expl ||', '''|| current_user ||''')';	
 	END IF;
