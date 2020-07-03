efd29ec091a951cc1aafb7c7e33d3cb77409d52b
Xavier Torret <xtorret@bgeo.es>
2020-07-03 19:13:29 +0200

    Enhance performance for crosstab on child views

--- dbmodel/utils/fct/gw_fct_admin_manage_child_views_view.sql
+++ dbmodel/utils/fct/gw_fct_admin_manage_child_views_view.sql
@@ -91,6 +91,7 @@ BEGIN
 			JOIN '||v_schemaname||'.man_'||v_feature_system_id||' ON man_'||v_feature_system_id||'.'||v_feature_type||'_id = ve_'||v_feature_type||'.'||v_feature_type||'_id
 			LEFT JOIN (SELECT ct.feature_id, '||v_ct_param||' FROM crosstab (''SELECT feature_id, parameter_id, value_param
 			FROM '||v_schemaname||'.man_addfields_value LEFT JOIN '||v_schemaname||'.sys_addfields ON sys_addfields.id=parameter_id
+			JOIN v_state_'||v_feature_type||' ON '||v_feature_type||'_id = feature_id
 			WHERE cat_feature_id='''''||v_feature_cat||''''' OR cat_feature_id is null  ORDER BY 1,2''::text, 
 			''VALUES '||v_id_param||'''::text) ct(feature_id character varying,'||v_datatype||' )) a 
 			ON a.feature_id::text=ve_'||v_feature_type||'.'||v_feature_type||'_id 
