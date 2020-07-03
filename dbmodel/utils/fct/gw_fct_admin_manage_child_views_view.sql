7d6be13082e88b6625586d3667604953ee8b1d46
Xavier Torret <xtorret@bgeo.es>
2020-07-03 19:34:05 +0200

    Enhance performance on crosstab for child views adding where value_param is not null

--- dbmodel/utils/fct/gw_fct_admin_manage_child_views_view.sql
+++ dbmodel/utils/fct/gw_fct_admin_manage_child_views_view.sql
@@ -119,9 +119,10 @@ BEGIN
 			FROM '||v_schemaname||'.ve_'||v_feature_type||'
 			JOIN '||v_schemaname||'.man_'||v_feature_system_id||' 
 			ON man_'||v_feature_system_id||'.'||v_feature_type||'_id = ve_'||v_feature_type||'.'||v_feature_type||'_id
-			LEFT JOIN (SELECT ct.feature_id, '||v_ct_param||' FROM crosstab (''SELECT feature_id, parameter_id, value_param
-			FROM '||v_schemaname||'.man_addfields_value LEFT JOIN '||v_schemaname||'.sys_addfields ON sys_addfields.id=parameter_id
-			WHERE cat_feature_id='''''||v_feature_cat||''''' OR cat_feature_id is null  ORDER BY 1,2''::text, 
+			FROM '||v_schemaname||'.man_addfields_value 
+			JOIN '||v_schemaname||'.sys_addfields ON sys_addfields.id=parameter_id
+			JOIN (SELECT ct.feature_id, '||v_ct_param||' FROM crosstab (''SELECT feature_id, parameter_id, value_param
+			WHERE value_param IS NOT NULL AND cat_feature_id='''''||v_feature_cat||''''' OR cat_feature_id is null  ORDER BY 1,2''::text, 
 			''VALUES '||v_id_param||'''::text) ct(feature_id character varying,'||v_datatype||' )) a 
 			ON a.feature_id::text=ve_'||v_feature_type||'.'||v_feature_type||'_id 
 			WHERE '||v_feature_type||'_type ='''||v_feature_cat||''' ;';
