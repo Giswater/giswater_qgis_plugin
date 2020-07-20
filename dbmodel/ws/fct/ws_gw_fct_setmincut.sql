44f06f12802c66be88e156dd648defffe2b81e89
Xavier Torret <xtorret@bgeo.es>
2020-07-20 14:44:25 +0200

    new function setmincut to harmonize return json

---
+++ dbmodel/ws/fct/ws_gw_fct_setmincut.sql
@@ -0,0 +1,52 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+--FUNCTION CODE: XXX
+
+DROP FUNCTION IF EXISTS SCHEMA_NAME.gw_fct_setmincut(json);
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_setmincut(p_data json)
+RETURNS json AS
+$BODY$
+
+/*
+SELECT gw_fct_setmincut('{"data":{"valveUnaccess":{"status":false}, "mincutId":"3", "arcId":"2001"}}');
+
+SELECT gw_fct_setmincut('{"data":{"valveUnaccess":{"status":true, "nodeId":1001}, "mincutId":"3"}}');
+
+
+*/
+
+DECLARE
+
+v_status boolean;
+v_node integer;
+v_mincut integer;
+v_id integer;
+
+BEGIN
+
+	-- Search path
+	SET search_path = "SCHEMA_NAME", public;
+	
+	-- get input parameters
+	v_mincut :=	 ((p_data ->>'feature')::json->>'mincutId')::integer;
+	v_arc :=	 ((p_data ->>'data')::json->>'arcId')::integer;
+	v_valveunaccess := ((p_data ->>'data')::json->>'valveUnaccess')::json;
+	v_status := v_valveunaccess->>'status';
+	v_node := v_valveunaccess->>'nodeId';
+	
+	-- execute process
+	IF v_status
+		RETURN gw_fct_mincut_valveunaccess(v_node, v_mincut, current_user);
+	
+	ELSE	
+		RETURN gw_fct_mincut(v_mincut, 'arc'::text, v_arc);
+	END IF;
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
