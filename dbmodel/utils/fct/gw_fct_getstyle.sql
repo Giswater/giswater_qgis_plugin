729a0254f2b72c82fdb0bf60d00531dcd7191c6b
Néstor Ibáñez <nestor@bgeo.es>
2020-07-20 13:35:22 +0200

    gw_fct_getstyle

--- dbmodel/utils/fct/gw_fct_getstyle.sql
+++ dbmodel/utils/fct/gw_fct_getstyle.sql
@@ -13,76 +13,29 @@ CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_getstyle(p_data json)
 $BODY$
 
 /*
- SELECT SCHEMA_NAME.gw_fct_getstyle($${"client":{"device":4, "infoType":1, "lang":"ES"}, "form":{}, "feature":{}, "data":{"filterFields":{}, "pageInfo":{},  "function_id":"2431"}}$$);
-SELECT SCHEMA_NAME.gw_fct_getstyle($${"client":{"device":4, "infoType":1, "lang":"ES"}, "form":{}, "feature":{}, "data":{"filterFields":{}, "pageInfo":{}, "layers":[ "v_edit_arc",  "v_edit_connec","v_edit_node"], "function_id":"2431"}}$$);
-
-SELECT SCHEMA_NAME.gw_fct_getstyle($${"client":{"device":4, "infoType":1, "lang":"ES"}, "form":{}, "feature":{}, "data":{"filterFields":{}, "pageInfo":{}, "temp_layer":"point", "function_id":"2670"}}$$);
-
+ SELECT SCHEMA_NAME.gw_fct_getstyle($${"client":{"device":4, "infoType":1, "lang":"ES"}, "form":{}, "feature":{}, "data":{"filterFields":{}, "pageInfo":{},  "style_id":"106"}}$$);
 */
  
 DECLARE
-
-v_value json;
-v_layer text;
-v_layers json;
 v_return json;
-v_result json;
-v_style_type text;
+v_style_id text;
+v_style text;
 v_version json;
-v_layers_array text[];
-v_temp_layer text;
---v_style text;
-v_json_array json[];
+
+
+
+
 BEGIN
 
 	-- Search path
 	SET search_path = 'SCHEMA_NAME', public;
+
 	
-	v_layers = ((p_data ->>'data')::json->>'layers')::json;
-	v_temp_layer = ((p_data ->>'data')::json->>'temp_layer')::text;
-	v_style_type =((p_data ->>'data')::json->>'style_type')::text;
-	v_layers_array = (select array_agg(value) as list from  json_array_elements_text(v_layers));
-	-- WHEN COME FROM config_function.layermanager
-	IF v_layers IS NOT NULL THEN
-		FOREACH v_layer IN ARRAY v_layers_array LOOP
-			EXECUTE 'SELECT jsonb_build_object ('''||v_layer||''',feature)
-					FROM	(
-				SELECT jsonb_build_object(				
-				''stylevalue'',stylevalue
-				) AS feature
-				FROM (SELECT stylevalue FROM sys_style 
-				JOIN config_table ON config_table.style::text = sys_style.id::text
-				AND config_table.id = '||quote_literal(v_layer)||') row) a;'
-				INTO v_result;
-					
-			SELECT array_append(v_json_array, v_result) into v_json_array;			
-			
-		END LOOP;		
-		v_return = gw_fct_json_object_set_key((p_data->>'body')::json, 'layers', v_json_array);
+	v_style_id = ((p_data ->>'data')::json->>'style_id')::json;
+	v_style = (SELECT stylevalue FROM sys_style where id::text = v_style_id::text);
+	v_return = gw_fct_json_object_set_key((p_data->>'body')::json, 'style', v_style);
 
-	END IF;
-	RAISE NOTICE 'p_data-->%',p_data;
-	
-	-- WHEN COME FROM config_function.returnmanager
-	/*
-	IF v_temp_layer IS NOT NULL THEN
-
-		EXECUTE '
-			SELECT jsonb_build_object(
-			''styletype'', row.styletype,
-			''stylevalue'', row.stylevalue
-			) 
-			FROM (SELECT styletype, stylevalue from sys_style WHERE idval ='''||v_style_type||''') row ;'
-			INTO v_style;
-
-		IF v_style IS NOT NULL THEN			
-			v_return=gw_fct_json_object_set_key((v_value)::json, 'style', v_style);			
-		END IF;
-
-	END IF;
-	*/
-    
-    	v_version := COALESCE(v_version, '{}');
+	v_version := COALESCE(v_version, '{}');
 	v_return := COALESCE(v_return, '{}');
 	 
 	-- Return
