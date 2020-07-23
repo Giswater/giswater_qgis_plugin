1ae8fc1d097ae492023e2fe369c88f0e468ef884
Barbara <bbrzepka@bgeo.es>
2020-07-23 16:15:13 +0200

    add drop fct if exists on manage_child_views - change of return type

--- dbmodel/utils/fct/gw_fct_admin_manage_child_views.sql
+++ dbmodel/utils/fct/gw_fct_admin_manage_child_views.sql
@@ -5,7 +5,7 @@ This version of Giswater is provided by Giswater Association
 */
 
 --FUNCTION CODE: 2716
-DROP FUNCTION SCHEMA_NAME.gw_fct_admin_manage_child_views(p_data json);
+DROP FUNCTION IF EXISTS SCHEMA_NAME.gw_fct_admin_manage_child_views(p_data json);
 CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_admin_manage_child_views(p_data json)
   RETURNS json AS
 $BODY$
