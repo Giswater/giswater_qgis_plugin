673598b5fb28ac01ef75bcdc934576dbb732d886
Albert Bofill <abofill@bgeo.es>
2020-07-27 17:54:53 +0200

    Bug fix on DROP FUNCTION pg2epa_check_data

--- dbmodel/ws/fct/ws_gw_fct_pg2epa_check_data.sql
+++ dbmodel/ws/fct/ws_gw_fct_pg2epa_check_data.sql
@@ -6,8 +6,7 @@ This version of Giswater is provided by Giswater Association
 
 --FUNCTION CODE:2430
 
-DROP FUNCTION IF EXISTS SCHEMA_NAME.gw_fct_epa_check_data(text);
-DROP FUNCTION IF EXISTS SCHEMA_NAME.gw_fct_epa_check_data(json);
+DROP FUNCTION IF EXISTS SCHEMA_NAME.gw_fct_pg2epa_check_data(character varying);
 CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_pg2epa_check_data(p_data json)
   RETURNS json AS
 $BODY$
