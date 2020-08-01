50cfe5bdb37643df7190403de902c10eb14df3b1
Xavier Torret <xtorret@bgeo.es>
2020-08-01 13:50:58 +0200

    Bug fix from previous commit

--- dbmodel/ud/ftrg/ud_gw_trg_edit_node.sql
+++ dbmodel/ud/ftrg/ud_gw_trg_edit_node.sql
@@ -6,7 +6,8 @@ This version of Giswater is provided by Giswater Association
 
 --FUNCTION CODE: 1220
  
-CREATE OR REPLACE FUNCTION ud_sample.gw_trg_edit_node()
+
+CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_edit_node()
   RETURNS trigger AS
 $BODY$
 DECLARE 
