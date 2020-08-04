ae10610e605cb4dfdcb555a22db295d449e4b1e9
Barbara <bbrzepka@bgeo.es>
2020-08-04 07:45:01 +0200

    minor bug fix on trg edit dimensions

--- dbmodel/utils/ftrg/gw_trg_edit_dimensions.sql
+++ dbmodel/utils/ftrg/gw_trg_edit_dimensions.sql
@@ -54,8 +54,8 @@ BEGIN
 		-- Insert
 		INSERT INTO dimensions (id, distance, depth, the_geom, x_label, y_label, rotation_label, offset_label, direction_arrow, x_symbol, y_symbol, 
 			feature_id, feature_type, state, expl_id, observ, comment)
-		VALUES (NEW.id, NEW.distance, NEW.depth, NEW.the_geom, NEW.x_label, NEW.y_label, NEW.rotation_label, NEW.offset_label, NEW.direction_arrow, NEW.x_symbol, NEW.y_symbol, 
-			NEW.feature_id, NEW.feature_type, NEW.state, NEW.expl_id, NEW.observ, NEW.comment);
+		VALUES (NEW.id, NEW.distance, NEW.depth, NEW.the_geom, NEW.x_label, NEW.y_label, NEW.rotation_label, NEW.offset_label, NEW.direction_arrow, 
+			NEW.x_symbol, NEW.y_symbol, NEW.feature_id, NEW.feature_type, NEW.state, NEW.expl_id, NEW.observ, NEW.comment);
 
 	
 		RETURN NEW;
@@ -64,9 +64,10 @@ BEGIN
 	ELSIF TG_OP = 'UPDATE' THEN
 
 		UPDATE dimensions
-		SET id=NEW.id, distance=NEW.distance, depth=NEW.depth, the_geom=NEW.the_geom, x_label=NEW.x_label, y_label=NEW.y_label, rotation_label=NEW.rotation_label, offset_label=NEW.offset_label, 
-		direction_arrow=NEW.direction_arrow, x_symbol=NEW.x_symbol, y_symbol=NEW.y_symbol, feature_id=NEW.feature_id, feature_type=NEW.feature_type, expl_id=NEW.expl_id, observ=NEW.observ, state=NEW.state
-		comment=NEW.comment
+		SET id=NEW.id, distance=NEW.distance, depth=NEW.depth, the_geom=NEW.the_geom, x_label=NEW.x_label, y_label=NEW.y_label, 
+		rotation_label=NEW.rotation_label, offset_label=NEW.offset_label, direction_arrow=NEW.direction_arrow, x_symbol=NEW.x_symbol, 
+		y_symbol=NEW.y_symbol, feature_id=NEW.feature_id, feature_type=NEW.feature_type, 
+		expl_id=NEW.expl_id, observ=NEW.observ, state=NEW.state, comment=NEW.comment
 		WHERE id=NEW.id;
 
 		RETURN NEW;
