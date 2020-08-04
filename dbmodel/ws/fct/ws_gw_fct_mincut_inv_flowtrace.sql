867aac439efe95366f2d315c14e6ce1fbe657cc7
Barbara <bbrzepka@bgeo.es>
2020-08-04 11:04:32 +0200

    minor bug fix on fct_mincut_inv_flowtrace

--- dbmodel/ws/fct/ws_gw_fct_mincut_inv_flowtrace.sql
+++ dbmodel/ws/fct/ws_gw_fct_mincut_inv_flowtrace.sql
@@ -60,7 +60,7 @@ BEGIN
 		JOIN v_edit_node ON v_edit_node.node_id=config_mincut_inlet.node_id
 		JOIN value_state_type ON state_type=value_state_type.id 
 		JOIN exploitation ON exploitation.expl_id=config_mincut_inlet.expl_id
-		WHERE (is_operative IS TRUE) AND (exploitation.macroexpl_id=v_macroexpl) AND config_mincut_inlet.active IS TRUE AND 
+		WHERE (is_operative IS TRUE) AND (exploitation.macroexpl_id=v_macroexpl) AND config_mincut_inlet.active IS TRUE 
 		AND v_edit_node.the_geom IS NOT NULL AND v_edit_node.node_id NOT IN (select node_id FROM om_mincut_node WHERE result_id=result_id_arg)
 		ORDER BY 1
 		LOOP
