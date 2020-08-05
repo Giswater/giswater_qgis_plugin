b2c4602c4e2a26a575aafac2ecdcb8978cec0cf1
Barbara <bbrzepka@bgeo.es>
2020-08-05 08:30:35 +0200

    separate edit_upsert_elevation_from_dem - config layout order

--- dbmodel/updates/34/34019/utils/dml.sql
+++ dbmodel/updates/34/34019/utils/dml.sql
@@ -26,6 +26,8 @@ VALUES (2994, 'gw_fct_vnode_repair', 'utils', 'function')ON CONFLICT (id) DO NOT
 UPDATE sys_param_user SET id = 'edit_insert_elevation_from_dem', label = 'Insert elevation from DEM:'
 WHERE id = 'edit_upsert_elevation_from_dem';
 
+UPDATE sys_param_user SET layoutorder = layoutorder+1 WHERE layoutorder > 17 AND layoutname = 'lyt_other';
+
 INSERT INTO sys_param_user(id, formname, descript, sys_role,  label, isenabled, layoutorder, project_type, isparent, 
 isautoupdate, datatype, widgettype, ismandatory, layoutname, iseditable,  isdeprecated)
 VALUES ('edit_update_elevation_from_dem', 'config', 'If true, the the elevation will be automatically updated from the DEM raster',
