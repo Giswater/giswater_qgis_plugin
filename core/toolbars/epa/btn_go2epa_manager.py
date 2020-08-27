99b800320ec10ea5a2e3d4a3f161066df230540c
nullptr <eliesbertran@gmail.com>
2020-08-27 14:00:41 +0200

    Moved functionality from go2epa.py to its corresponding button

--- core/toolbars/epa/btn_go2epa_manager.py
+++ core/toolbars/epa/btn_go2epa_manager.py
@@ -7,16 +7,71 @@ or (at your option) any later version.
 # -*- coding: utf-8 -*-
 from ..parent_action import GwParentAction
 
-from ...actions.epa.go2epa import GwGo2Epa
+from qgis.PyQt.QtCore import QRegExp
+from qgis.PyQt.QtWidgets import QAbstractItemView
+from qgis.PyQt.QtGui import QRegExpValidator
+
+from functools import partial
+
+from lib import qt_tools
+from ....ui_manager import EpaManager
+from ...utils.giswater_tools import load_settings, open_dialog, close_dialog
+
+from ....actions.api_parent_functs import fill_table
+from ....actions.parent_functs import set_table_columns, multi_rows_delete
+
 
 class GwGo2EpaManagerButton(GwParentAction):
 	
 	def __init__(self, icon_path, text, toolbar, action_group):
 		super().__init__(icon_path, text, toolbar, action_group)
-		
-		self.go2epa = GwGo2Epa()
 	
 	
 	def clicked_event(self):
-		self.go2epa.go2epa_result_manager()
+		
+		# Create the dialog
+		self.dlg_manager = EpaManager()
+		load_settings(self.dlg_manager)
+		
+		# Manage widgets
+		reg_exp = QRegExp("^[A-Za-z0-9_]{1,16}$")
+		self.dlg_manager.txt_result_id.setValidator(QRegExpValidator(reg_exp))
+		
+		# Fill combo box and table view
+		self.fill_combo_result_id()
+		self.dlg_manager.tbl_rpt_cat_result.setSelectionBehavior(QAbstractItemView.SelectRows)
+		fill_table(self.dlg_manager.tbl_rpt_cat_result, 'v_ui_rpt_cat_result')
+		set_table_columns(self.dlg_manager, self.dlg_manager.tbl_rpt_cat_result, 'v_ui_rpt_cat_result')
+		
+		# Set signals
+		self.dlg_manager.btn_delete.clicked.connect(partial(multi_rows_delete, self.dlg_manager.tbl_rpt_cat_result,
+															'rpt_cat_result', 'result_id'))
+		self.dlg_manager.btn_close.clicked.connect(partial(close_dialog, self.dlg_manager))
+		self.dlg_manager.rejected.connect(partial(close_dialog, self.dlg_manager))
+		self.dlg_manager.txt_result_id.editTextChanged.connect(self.filter_by_result_id)
+		
+		# Open form
+		open_dialog(self.dlg_manager, dlg_name='go2epa_manager')
+	
+	
+	def fill_combo_result_id(self):
+	
+		sql = "SELECT result_id FROM v_ui_rpt_cat_result ORDER BY result_id"
+		rows = self.controller.get_rows(sql)
+		qt_tools.fillComboBox(self.dlg_manager, self.dlg_manager.txt_result_id, rows)
+	
+	
+	def filter_by_result_id(self):
+		
+		table = self.dlg_manager.tbl_rpt_cat_result
+		widget_txt = self.dlg_manager.txt_result_id
+		tablename = 'v_ui_rpt_cat_result'
+		result_id = qt_tools.getWidgetText(self.dlg_manager, widget_txt)
+		if result_id != 'null':
+			expr = f" result_id ILIKE '%{result_id}%'"
+			# Refresh model with selected filter
+			table.model().setFilter(expr)
+			table.model().select()
+		else:
+			fill_table(table, tablename)
 		
