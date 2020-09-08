2f3e58b0832802cc39e1b4601e164089444c5739
Xavier Torret <xtorret@bgeo.es>
2020-09-09 00:54:01 +0200

    Enhance performance for vu_arc views (ud & ws)

--- dbmodel/updates/34/34021/ud/ddlview.sql
+++ dbmodel/updates/34/34021/ud/ddlview.sql
@@ -79,6 +79,139 @@ CREATE OR REPLACE VIEW v_edit_vnode AS
 
 
 
+CREATE OR REPLACE VIEW vu_arc AS 
+WITH vu_node AS (
+         SELECT node.node_id,
+            node.top_elev,
+            node.custom_top_elev,
+                CASE
+                    WHEN node.custom_top_elev IS NOT NULL THEN node.custom_top_elev
+                    ELSE node.top_elev
+                END AS sys_top_elev,
+            node.ymax,
+            node.custom_ymax,
+                CASE
+                    WHEN node.custom_ymax IS NOT NULL THEN node.custom_ymax
+                    ELSE node.ymax
+                END AS sys_ymax,
+            node.elev,
+            node.custom_elev,
+                CASE
+                    WHEN node.elev IS NOT NULL AND node.custom_elev IS NULL THEN node.elev
+                    WHEN node.custom_elev IS NOT NULL THEN node.custom_elev
+                    ELSE NULL::numeric(12,3)
+		END AS sys_elev,
+		CASE
+                    WHEN node.matcat_id IS NULL THEN cat_node.matcat_id
+                    ELSE node.matcat_id
+                END AS matcat_id,
+            node.node_type,
+            macroexpl_id      
+           FROM  node
+           LEFT JOIN cat_node ON nodecat_id = id
+           LEFT JOIN exploitation ON node.expl_id = exploitation.expl_id)
+ SELECT arc.arc_id,
+    arc.code,
+    arc.node_1,
+    a.node_type AS nodetype_1,
+    arc.y1,
+    arc.custom_y1,
+    arc.elev1,
+    arc.custom_elev1,
+    arc.sys_elev1,
+    a.sys_top_elev - arc.sys_elev1 AS sys_y1,
+    a.sys_top_elev - arc.sys_elev1 - cat_arc.geom1 AS r1,
+        CASE
+            WHEN a.sys_elev IS NOT NULL THEN arc.sys_elev1 - a.sys_elev
+            ELSE (arc.sys_elev1 - (a.sys_top_elev - a.sys_ymax))::numeric(12,3)
+        END AS z1,
+    arc.node_2,
+    a.node_type AS nodetype_2,
+    arc.y2,
+    arc.custom_y2,
+    arc.elev2,
+    arc.custom_elev2,
+    arc.sys_elev2,
+    b.sys_top_elev - arc.sys_elev2 AS sys_y2,
+    b.sys_top_elev - arc.sys_elev2 - cat_arc.geom1 AS r2,
+        CASE
+            WHEN b.sys_elev IS NOT NULL THEN arc.sys_elev2 - b.sys_elev
+            ELSE (arc.sys_elev2 - (b.sys_top_elev - b.sys_ymax))::numeric(12,3)
+        END AS z2,
+    arc.sys_slope AS slope,
+    arc.arc_type,
+    cat_feature.system_id AS sys_type,
+    arc.arccat_id,
+        CASE
+            WHEN arc.matcat_id IS NULL THEN cat_arc.matcat_id
+            ELSE arc.matcat_id
+        END AS matcat_id,
+    cat_arc.shape AS cat_shape,
+    cat_arc.geom1 AS cat_geom1,
+    cat_arc.geom2 AS cat_geom2,
+    cat_arc.width,
+    arc.epa_type,
+    arc.expl_id,
+    a.macroexpl_id,
+    arc.sector_id,
+    sector.macrosector_id,
+    arc.state,
+    arc.state_type,
+    arc.annotation,
+    st_length(arc.the_geom)::numeric(12,2) AS gis_length,
+    arc.custom_length,
+    arc.inverted_slope,
+    arc.observ,
+    arc.comment,
+    arc.dma_id,
+    dma.macrodma_id,
+    arc.soilcat_id,
+    arc.function_type,
+    arc.category_type,
+    arc.fluid_type,
+    arc.location_type,
+    arc.workcat_id,
+    arc.workcat_id_end,
+    arc.builtdate,
+    arc.enddate,
+    arc.buildercat_id,
+    arc.ownercat_id,
+    arc.muni_id,
+    arc.postcode,
+    arc.district_id,
+    c.name AS streetname,
+    arc.postnumber,
+    arc.postcomplement,
+    d.name AS streetname2,
+    arc.postnumber2,
+    arc.postcomplement2,
+    arc.descript,
+    concat(cat_feature.link_path, arc.link) AS link,
+    arc.verified,
+    arc.undelete,
+    cat_arc.label,
+    arc.label_x,
+    arc.label_y,
+    arc.label_rotation,
+    arc.publish,
+    arc.inventory,
+    arc.uncertain,
+    arc.num_value,
+    date_trunc('second'::text, arc.tstamp) AS tstamp,
+    arc.insert_user,
+    date_trunc('second'::text, arc.lastupdate) AS lastupdate,
+    arc.lastupdate_user,
+    arc.the_geom
+   FROM arc
+     JOIN cat_arc ON arc.arccat_id::text = cat_arc.id::text
+     LEFT JOIN vu_node a ON a.node_id::text = arc.node_1::text
+     LEFT JOIN vu_node b ON b.node_id::text = arc.node_2::text
+     JOIN sector ON sector.sector_id = arc.sector_id
+     JOIN cat_feature ON arc.arc_type::text = cat_feature.id::text
+     JOIN dma ON arc.dma_id = dma.dma_id
+     LEFT JOIN ext_streetaxis c ON c.id::text = arc.streetaxis_id::text
+     LEFT JOIN ext_streetaxis d ON d.id::text = arc.streetaxis2_id::text;
+
 SELECT gw_fct_admin_manage_fields($${"data":{"action":"DROP","table":"vnode", "column":"vnode_type"}}$$);
 SELECT gw_fct_admin_manage_fields($${"data":{"action":"DROP","table":"vnode", "column":"annotation"}}$$);
 SELECT gw_fct_admin_manage_fields($${"data":{"action":"DROP","table":"vnode", "column":"sector_id"}}$$);
--- dbmodel/updates/34/34021/ws/ddlview.sql
+++ dbmodel/updates/34/34021/ws/ddlview.sql
@@ -50,6 +50,106 @@ CREATE OR REPLACE VIEW v_edit_vnode AS
   WHERE a.state < 2;
 
 
+
+CREATE OR REPLACE VIEW vu_arc AS 
+WITH vu_node AS (SELECT node_id, elevation, depth, nodetype_id, staticpressure FROM node JOIN cat_node ON nodecat_id = id)
+ SELECT arc.arc_id,
+    arc.code,
+    arc.node_1,
+    arc.node_2,
+    a.elevation AS elevation1,
+    a.depth AS depth1,
+    b.elevation AS elevation2,
+    b.depth AS depth2,
+    arc.arccat_id,
+    cat_arc.arctype_id AS arc_type,
+    cat_feature.system_id AS sys_type,
+    cat_arc.matcat_id AS cat_matcat_id,
+    cat_arc.pnom AS cat_pnom,
+    cat_arc.dnom AS cat_dnom,
+    arc.epa_type,
+    arc.expl_id,
+    exploitation.macroexpl_id,
+    arc.sector_id,
+    sector.name AS sector_name,
+    sector.macrosector_id,
+    arc.state,
+    arc.state_type,
+    arc.annotation,
+    arc.observ,
+    arc.comment,
+    st_length2d(arc.the_geom)::numeric(12,2) AS gis_length,
+    arc.custom_length,
+    arc.minsector_id,
+    arc.dma_id,
+    dma.name AS dma_name,
+    dma.macrodma_id,
+    arc.presszone_id,
+    presszone.name AS presszone_name,
+    arc.dqa_id,
+    dqa.name AS dqa_name,
+    dqa.macrodqa_id,
+    arc.soilcat_id,
+    arc.function_type,
+    arc.category_type,
+    arc.fluid_type,
+    arc.location_type,
+    arc.workcat_id,
+    arc.workcat_id_end,
+    arc.buildercat_id,
+    arc.builtdate,
+    arc.enddate,
+    arc.ownercat_id,
+    arc.muni_id,
+    arc.postcode,
+    arc.district_id,
+    c.name AS streetname,
+    arc.postnumber,
+    arc.postcomplement,
+    d.name AS streetname2,
+    arc.postnumber2,
+    arc.postcomplement2,
+    arc.descript,
+    concat(cat_feature.link_path, arc.link) AS link,
+    arc.verified,
+    arc.undelete,
+    cat_arc.label,
+    arc.label_x,
+    arc.label_y,
+    arc.label_rotation,
+    arc.publish,
+    arc.inventory,
+    arc.num_value,
+    cat_arc.arctype_id AS cat_arctype_id,
+    a.nodetype_id AS nodetype_1,
+    a.staticpressure AS staticpress1,
+    b.nodetype_id AS nodetype_2,
+    b.staticpressure AS staticpress2,
+    date_trunc('second'::text, arc.tstamp) AS tstamp,
+    arc.insert_user,
+    date_trunc('second'::text, arc.lastupdate) AS lastupdate,
+    arc.lastupdate_user,
+    arc.the_geom,
+    arc.depth,
+    arc.adate,
+    arc.adescript,
+    sector.stylesheet AS sector_style,
+    dma.stylesheet AS dma_style,
+    presszone.stylesheet AS presszone_style,
+    dqa.stylesheet AS dqa_style
+   FROM arc
+     LEFT JOIN sector ON arc.sector_id = sector.sector_id
+     LEFT JOIN exploitation ON arc.expl_id = exploitation.expl_id
+     LEFT JOIN cat_arc ON arc.arccat_id::text = cat_arc.id::text
+     JOIN cat_feature ON cat_feature.id::text = cat_arc.arctype_id::text
+     LEFT JOIN dma ON arc.dma_id = dma.dma_id
+     LEFT JOIN vu_node a ON a.node_id::text = arc.node_1::text
+     LEFT JOIN vu_node b ON b.node_id::text = arc.node_2::text
+     LEFT JOIN dqa ON arc.dqa_id = dqa.dqa_id
+     LEFT JOIN presszone ON presszone.presszone_id::text = arc.presszone_id::text
+     LEFT JOIN ext_streetaxis c ON c.id::text = arc.streetaxis_id::text
+     LEFT JOIN ext_streetaxis d ON d.id::text = arc.streetaxis2_id::text;
+
   
 SELECT gw_fct_admin_manage_fields($${"data":{"action":"DROP","table":"vnode", "column":"vnode_type"}}$$);
 SELECT gw_fct_admin_manage_fields($${"data":{"action":"DROP","table":"vnode", "column":"annotation"}}$$);
