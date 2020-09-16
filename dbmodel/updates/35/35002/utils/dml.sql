eefabe1d8f15af2f6a403753ac8f99b21470f80d
Néstor Ibáñez <nestor@bgeo.es>
2020-09-16 12:42:33 +0200

    Remove zoom to layer v_edit_arc when execute gw_fct_getdimensioning

--- dbmodel/updates/35/35002/utils/dml.sql
+++ dbmodel/updates/35/35002/utils/dml.sql
@@ -29,3 +29,5 @@ UPDATE config_visit_parameter SET data_type = lower(data_type);
 -- 2020/16/09
 UPDATE sys_param_user SET dv_querytext =$$SELECT UNNEST(ARRAY (select (text_column::json->>'list_tables_name')::text[] from temp_table where fid =163 and cur_user = current_user)) as id, 
 UNNEST(ARRAY (select (text_column::json->>'list_layers_name')::text[] FROM temp_table WHERE fid = 163 and cur_user = current_user)) as idval $$ WHERE id = 'edit_cadtools_baselayer_vdefault';
+
+UPDATE config_function set layer_manager = {"visible": ["v_edit_dimensions"]} WHERE id = 2824;
