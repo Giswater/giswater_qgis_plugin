c3313e058bba0ad352273ae2bc568c70a8ee8e26
Edgar Fuste <efuste@bgeo.es>
2020-09-23 09:34:24 +0200

    Bug fix: manage None layers when open existing visit
    (cherry picked from commit 3c0349fc7a7aaaaac320055afafba7fabfb3b720)

--- actions/parent_manage_funct.py
+++ actions/parent_manage_funct.py
@@ -600,6 +600,9 @@ def lazy_configuration(widget, init_function):
 def select_features_by_ids(geom_type, expr, layers=None):
     """ Select features of layers of group @geom_type applying @expr """
 
+    if layers is None:
+        return
+    
     if not geom_type in layers:
         return
 
