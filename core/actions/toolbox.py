efb89324de11186d02a5417652a30b2fd89fc586
Edgar Fuste <efuste@bgeo.es>
2020-10-07 11:57:46 +0200

    Move functions 'dxf' to toolbox

--- core/actions/toolbox.py
+++ core/actions/toolbox.py
@@ -8,7 +8,7 @@ or (at your option) any later version.
 import os
 import json
 
-from qgis.core import QgsProject
+from qgis.core import QgsProject, QgsVectorLayer
 from qgis.gui import QgsDateTimeEdit
 from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtGui import QColor, QIcon, QStandardItemModel, QStandardItem
@@ -18,6 +18,7 @@ from collections import OrderedDict
 from functools import partial
 
 from ...lib import tools_qt
+from ..utils.tools_giswater import getWidgetText, open_file_path
 from .layer_tools import GwLayerTools
 from ..actions.api_parent import ApiParent
 from ..ui.ui_manager import ToolboxDockerUi, ToolboxUi
@@ -602,3 +603,113 @@ class GwToolBox(ApiParent):
         except KeyError:
             return 0
 
+
+    def gw_function_dxf(self, **kwargs):
+        """ Function called in def add_button(self, dialog, field): -->
+                widget.clicked.connect(partial(getattr(self, function_name), dialog, widget)) """
+
+        path, filter_ = open_file_path(filter_="DXF Files (*.dxf)")
+        if not path:
+            return
+
+        dialog = kwargs['dialog']
+        widget = kwargs['widget']
+        temp_layers_added = kwargs['temp_layers_added']
+        complet_result = self.manage_dxf(dialog, path, False, True)
+
+        for layer in complet_result['temp_layers_added']:
+            temp_layers_added.append(layer)
+        if complet_result is not False:
+            widget.setText(complet_result['path'])
+
+        dialog.btn_run.setEnabled(True)
+        dialog.btn_cancel.setEnabled(True)
+
+
+    def manage_dxf(self, dialog, dxf_path, export_to_db=False, toc=False, del_old_layers=True):
+        """ Select a dxf file and add layers into toc
+        :param dxf_path: path of dxf file
+        :param export_to_db: Export layers to database
+        :param toc: insert layers into TOC
+        :param del_old_layers: look for a layer with the same name as the one to be inserted and delete it
+        :return:
+        """
+
+        srid = self.controller.plugin_settings_value('srid')
+        # Block the signals so that the window does not appear asking for crs / srid and / or alert message
+        self.iface.mainWindow().blockSignals(True)
+        dialog.txt_infolog.clear()
+
+        sql = "DELETE FROM temp_table WHERE fid = 206;\n"
+        self.controller.execute_sql(sql)
+        temp_layers_added = []
+        for type_ in ['LineString', 'Point', 'Polygon']:
+
+            # Get file name without extension
+            dxf_output_filename = os.path.splitext(os.path.basename(dxf_path))[0]
+
+            # Create layer
+            uri = f"{dxf_path}|layername=entities|geometrytype={type_}"
+            dxf_layer = QgsVectorLayer(uri, f"{dxf_output_filename}_{type_}", 'ogr')
+
+            # Set crs to layer
+            crs = dxf_layer.crs()
+            crs.createFromId(srid)
+            dxf_layer.setCrs(crs)
+
+            if not dxf_layer.hasFeatures():
+                continue
+
+            # Get the name of the columns
+            field_names = [field.name() for field in dxf_layer.fields()]
+
+            sql = ""
+            geom_types = {0: 'geom_point', 1: 'geom_line', 2: 'geom_polygon'}
+            for count, feature in enumerate(dxf_layer.getFeatures()):
+                geom_type = feature.geometry().type()
+                sql += (f"INSERT INTO temp_table (fid, text_column, {geom_types[int(geom_type)]})"
+                        f" VALUES (206, '{{")
+                for att in field_names:
+                    if feature[att] in (None, 'NULL', ''):
+                        sql += f'"{att}":null , '
+                    else:
+                        sql += f'"{att}":"{feature[att]}" , '
+                geometry = self.add_layer.manage_geometry(feature.geometry())
+                sql = sql[:-2] + f"}}', (SELECT ST_GeomFromText('{geometry}', {srid})));\n"
+                if count != 0 and count % 500 == 0:
+                    status = self.controller.execute_sql(sql)
+                    if not status:
+                        return False
+                    sql = ""
+
+            if sql != "":
+                status = self.controller.execute_sql(sql)
+                if not status:
+                    return False
+
+            if export_to_db:
+                self.add_layer.export_layer_to_db(dxf_layer, crs)
+
+            if del_old_layers:
+                self.add_layer.delete_layer_from_toc(dxf_layer.name())
+
+            if toc:
+                if dxf_layer.isValid():
+                    self.add_layer.from_dxf_to_toc(dxf_layer, dxf_output_filename)
+                    temp_layers_added.append(dxf_layer)
+
+        # Unlock signals
+        self.iface.mainWindow().blockSignals(False)
+
+        extras = "  "
+        for widget in dialog.grb_parameters.findChildren(QWidget):
+            widget_name = widget.property('columnname')
+            value = getWidgetText(dialog, widget, add_quote=False)
+            extras += f'"{widget_name}":"{value}", '
+        extras = extras[:-2]
+        body = self.create_body(extras)
+        result = self.controller.get_json('gw_fct_check_importdxf', None)
+        if not result:
+            return False
+
+        return {"path": dxf_path, "result": result, "temp_layers_added": temp_layers_added}
