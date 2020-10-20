30e20461de68e67a6841c569f1e7122b32582558
Edgar Fuste <efuste@bgeo.es>
2020-10-20 16:43:30 +0200

    Enhancement: add key tableName before call gw_fct_setfields if we make an info

--- actions/api_cf.py
+++ actions/api_cf.py
@@ -1204,7 +1204,8 @@ class ApiCF(ApiParent, QObject):
         # If we make an info
         else:
             my_json = json.dumps(_json)
-            feature = f'"id":"{self.feature_id}" '
+            feature = f'"id":"{self.feature_id}", '
+            feature += f'"tableName":"{p_table_id}" '
 
         feature += f', "featureType":"{self.feature_type}"'
         extras = f'"fields":{my_json}, "reload":"{fields_reload}"'
