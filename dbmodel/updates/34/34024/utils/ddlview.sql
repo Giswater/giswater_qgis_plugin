7eda969381fb682fc13f2386db155914a0277849
Xavier Torret <xtorret@bgeo.es>
2020-10-27 11:15:30 +0100

    Enhance psector budget only with doable = true

--- dbmodel/updates/34/34024/utils/ddlview.sql
+++ dbmodel/updates/34/34024/utils/ddlview.sql
@@ -50,6 +50,7 @@ CREATE OR REPLACE VIEW v_plan_psector_x_node AS
      JOIN plan_psector ON plan_psector.psector_id = plan_psector_x_node.psector_id
   ORDER BY plan_psector_x_node.psector_id;
 
+
 CREATE OR REPLACE VIEW v_plan_psector_all AS 
 SELECT plan_psector.psector_id,
     plan_psector.name,
@@ -168,3 +169,94 @@ CREATE OR REPLACE VIEW v_edit_plan_psector AS
    FROM selector_expl,plan_psector
    LEFT JOIN v_plan_psector_all p USING (psector_id)
   WHERE plan_psector.expl_id = selector_expl.expl_id AND selector_expl.cur_user = "current_user"()::text;
+  
+
+CREATE OR REPLACE VIEW v_plan_current_psector AS 
+ SELECT plan_psector.psector_id,
+    plan_psector.name,
+    plan_psector.psector_type,
+    plan_psector.descript,
+    plan_psector.priority,
+    a.suma::numeric(14,2) AS total_arc,
+    b.suma::numeric(14,2) AS total_node,
+    c.suma::numeric(14,2) AS total_other,
+    plan_psector.text1,
+    plan_psector.text2,
+    plan_psector.observ,
+    plan_psector.rotation,
+    plan_psector.scale,
+    plan_psector.sector_id,
+    plan_psector.active,
+    (
+        CASE
+            WHEN a.suma IS NULL THEN 0::numeric
+            ELSE a.suma
+        END +
+        CASE
+            WHEN b.suma IS NULL THEN 0::numeric
+            ELSE b.suma
+        END +
+        CASE
+            WHEN c.suma IS NULL THEN 0::numeric
+            ELSE c.suma
+        END)::numeric(14,2) AS pem,
+    plan_psector.gexpenses,
+    ((100::numeric + plan_psector.gexpenses) / 100::numeric)::numeric(14,2) * (
+        CASE
+            WHEN a.suma IS NULL THEN 0::numeric
+            ELSE a.suma
+        END +
+        CASE
+            WHEN b.suma IS NULL THEN 0::numeric
+            ELSE b.suma
+        END +
+        CASE
+            WHEN c.suma IS NULL THEN 0::numeric
+            ELSE c.suma
+        END)::numeric(14,2) AS pec,
+    plan_psector.vat,
+    ((100::numeric + plan_psector.gexpenses) / 100::numeric * ((100::numeric + plan_psector.vat) / 100::numeric))::numeric(14,2) * (
+        CASE
+            WHEN a.suma IS NULL THEN 0::numeric
+            ELSE a.suma
+        END +
+        CASE
+            WHEN b.suma IS NULL THEN 0::numeric
+            ELSE b.suma
+        END +
+        CASE
+            WHEN c.suma IS NULL THEN 0::numeric
+            ELSE c.suma
+        END)::numeric(14,2) AS pec_vat,
+    plan_psector.other,
+    ((100::numeric + plan_psector.gexpenses) / 100::numeric * ((100::numeric + plan_psector.vat) / 100::numeric) * ((100::numeric + plan_psector.other) / 100::numeric))::numeric(14,2) * (
+        CASE
+            WHEN a.suma IS NULL THEN 0::numeric
+            ELSE a.suma
+        END +
+        CASE
+            WHEN b.suma IS NULL THEN 0::numeric
+            ELSE b.suma
+        END +
+        CASE
+            WHEN c.suma IS NULL THEN 0::numeric
+            ELSE c.suma
+        END)::numeric(14,2) AS pca,
+    plan_psector.the_geom
+   FROM plan_psector
+     JOIN selector_plan_psector ON plan_psector.psector_id = selector_plan_psector.psector_id
+     LEFT JOIN ( SELECT sum(v_plan_psector_x_arc.total_budget) AS suma,
+            v_plan_psector_x_arc.psector_id
+           FROM v_plan_psector_x_arc
+           WHERE doable is true
+          GROUP BY v_plan_psector_x_arc.psector_id) a ON a.psector_id = plan_psector.psector_id
+     LEFT JOIN ( SELECT sum(v_plan_psector_x_node.total_budget) AS suma,
+            v_plan_psector_x_node.psector_id
+           FROM v_plan_psector_x_node
+           WHERE doable is true
+          GROUP BY v_plan_psector_x_node.psector_id) b ON b.psector_id = plan_psector.psector_id
+     LEFT JOIN ( SELECT sum(v_plan_psector_x_other.total_budget) AS suma,
+            v_plan_psector_x_other.psector_id
+           FROM v_plan_psector_x_other
+          GROUP BY v_plan_psector_x_other.psector_id) c ON c.psector_id = plan_psector.psector_id
+  WHERE selector_plan_psector.cur_user = "current_user"()::text;
