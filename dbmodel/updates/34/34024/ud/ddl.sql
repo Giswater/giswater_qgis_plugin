b156b88ac1522730a0bff288f600610cdaab00d4
Xavier Torret <xtorret@bgeo.es>
2020-11-02 18:36:46 +0100

    Drop _node_id and _to_arc deprecated fields on ud flowregulators features

--- dbmodel/updates/34/34024/ud/ddl.sql
+++ dbmodel/updates/34/34024/ud/ddl.sql
@@ -11,3 +11,12 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 -- 2020/10/22
 UPDATE inp_subcatchment SET outlet_id = _parent_id WHERE _parent_id IS NOT NULL;
 SELECT gw_fct_admin_manage_fields($${"data":{"action":"DROP","table":"inp_subcatchment", "column":"_parent_id", "isUtils":"False"}}$$);
+
+-- 2020/11/02
+ALTER TABLE inp_outlet DROP COLUMN _node_id;
+ALTER TABLE inp_pump DROP COLUMN _node_id;
+ALTER TABLE inp_weir DROP COLUMN _node_id;
+ALTER TABLE inp_orifice DROP COLUMN _node_id;
+ALTER TABLE inp_pump DROP COLUMN _to_arc;
+ALTER TABLE inp_weir DROP COLUMN _to_arc;
+ALTER TABLE inp_orifice DROP COLUMN _to_arc;
