bfbdc31c0e0af1e217fc960c8c6513ee5f7625ce
Xavier Torret <xtorret@bgeo.es>
2020-11-11 16:56:58 +0100

    Update function role_resetuserprofile to work with toolbox (admin)

--- dbmodel/utils/fct/gw_fct_admin_role_resetuserprofile.sql
+++ dbmodel/utils/fct/gw_fct_admin_role_resetuserprofile.sql
@@ -15,10 +15,7 @@ $BODY$
 SELECT SCHEMA_NAME.gw_fct_admin_role_resetuserprofile($${"user":"postgres", "values":{}}$$)
 
 -- taking values from another user
-SELECT SCHEMA_NAME.gw_fct_admin_role_resetuserprofile($${"user":"user1", "values":{"copyFromUserSameRole":"user2"}}$$)
-
--- Setting specific values (todo)
-SELECT SCHEMA_NAME.gw_fct_admin_role_resetuserprofile($${"user":"user1", "values":{"exploitation":{1,2,3}, "state":{1}}}$$)
+SELECT SCHEMA_NAME.gw_fct_admin_role_resetuserprofile($${"data":{"parameters":{"user":"user1", "values":{"copyFromUserSameRole":"user2"}}}}$$)
 */
 
 DECLARE
@@ -36,14 +33,14 @@ BEGIN
 	SELECT project_type INTO v_projecttype FROM sys_version LIMIT 1;
 
 	--  Get input parameters:
-	v_user := (p_data ->> 'user');
-	v_copyfromuser := (p_data ->> 'values')::json->> 'copyFromUserSameRole';
+	v_user := (((p_data ->> 'data')::json->>'parameters')::json->>'user');
+	v_copyfromuser := (((p_data ->> 'data')::json->>'parameters')::json->>'values')::json->> 'copyFromUserSameRole';
 
 	-- setting using sample values from another use
 	IF v_copyfromuser IS NOT NULL THEN
 	
-	-- todo: improve with controls
-	--IF (v_copyfromuser IS NOT NULL) AND (v_copyfromuser not exists as user) AND (v_copyfromuser not has same role than v_user) THEN
+		-- todo: improve with controls
+		--IF (v_copyfromuser IS NOT NULL) AND (v_copyfromuser not exists as user) AND (v_copyfromuser not has same role than v_user) THEN
 
 		-- config_param_user
 		DELETE FROM config_param_user WHERE cur_user=v_user;
@@ -51,33 +48,60 @@ BEGIN
 		SELECT parameter, value, v_user FROM config_param_user WHERE cur_user=v_copyfromuser;
 
 		-- selectors
-		DELETE FROM selector_expl WHERE cur_user=v_user;
-		DELETE FROM selector_psector WHERE cur_user=v_user;
-		DELETE FROM selector_state WHERE cur_user=v_user;
+		DELETE FROM selector_audit WHERE cur_user=v_user;
 		DELETE FROM selector_date WHERE cur_user=v_user;
+		DELETE FROM selector_expl WHERE cur_user=v_user;
 		DELETE FROM selector_hydrometer WHERE cur_user=v_user;
-		DELETE FROM selector_workcat WHERE cur_user=v_user;
-
-		INSERT INTO selector_expl (expl_id, cur_user) SELECT expl_id, v_user FROM selector_expl WHERE cur_user=v_copyfromuser;
-		INSERT INTO selector_psector (psector_id, cur_user) SELECT psector_id, v_user FROM selector_psector WHERE cur_user=v_copyfromuser;
-		INSERT INTO selector_state (state_id, cur_user) SELECT state_id, v_user FROM selector_state WHERE cur_user=v_copyfromuser;
-		INSERT INTO selector_date (from_date, to_date, context, cur_user) SELECT from_date, to_date, context, v_user FROM selector_date WHERE cur_user=v_copyfromuser;
-		INSERT INTO selector_hydrometer (state_id, cur_user) SELECT state_id, v_user FROM selector_hydrometer WHERE cur_user=v_copyfromuser;
-		INSERT INTO selector_workcat (workcat_id, cur_user) SELECT workcat_id, v_user FROM selector_workcat WHERE cur_user=v_copyfromuser;
-	
-		-- role epa
-		DELETE FROM selector_sector WHERE cur_user=v_user;
 		DELETE FROM selector_inp_result WHERE cur_user=v_user;
-		
-		INSERT INTO selector_sector (sector_id, cur_user) SELECT sector_id, v_user FROM selector_sector WHERE cur_user=v_copyfromuser;
+
+		DELETE FROM selector_plan_psector WHERE cur_user=v_user;
+		DELETE FROM selector_plan_result WHERE cur_user=v_user;
+		DELETE FROM selector_psector WHERE cur_user=v_user;
+
+		DELETE FROM selector_rpt_compare WHERE cur_user=v_user;
+		DELETE FROM selector_rpt_main WHERE cur_user=v_user;
+
+		DELETE FROM selector_sector WHERE cur_user=v_user;
+		DELETE FROM selector_state WHERE cur_user=v_user;
+		DELETE FROM selector_workcat WHERE cur_user=v_user;
+			
+
+		INSERT INTO selector_audit (fid, cur_user) SELECT fid, v_user FROM selector_audit WHERE cur_user=v_copyfromuser;
+		INSERT INTO selector_date (from_date, to_date, context, cur_user) SELECT from_date, to_date, context, v_user FROM selector_date WHERE cur_user=v_copyfromuser;
+		INSERT INTO selector_expl (expl_id, cur_user) SELECT expl_id, v_user FROM selector_expl WHERE cur_user=v_copyfromuser;
+		INSERT INTO selector_hydrometer (state_id, cur_user) SELECT state_id, v_user FROM selector_hydrometer WHERE cur_user=v_copyfromuser;
 		INSERT INTO selector_inp_result (result_id, cur_user) SELECT result_id, v_user FROM selector_inp_result WHERE cur_user=v_copyfromuser;
 		
-		IF v_projecttype ='UD' THEN
+		INSERT INTO selector_plan_psector (psector_id, cur_user) SELECT psector_id, v_user FROM selector_plan_psector WHERE cur_user=v_copyfromuser;
+		INSERT INTO selector_plan_result (result_id, cur_user) SELECT result_id, v_user FROM selector_plan_result WHERE cur_user=v_copyfromuser;
+		INSERT INTO selector_psector (psector_id, cur_user) SELECT psector_id, v_user FROM selector_psector WHERE cur_user=v_copyfromuser;
+
+		INSERT INTO selector_rpt_compare (result_id, cur_user) SELECT result_id, v_user FROM selector_rpt_compare WHERE cur_user=v_copyfromuser;
+		INSERT INTO selector_rpt_main (result_id, cur_user) SELECT result_id, v_user FROM selector_rpt_main WHERE cur_user=v_copyfromuser;
+
+		INSERT INTO selector_sector (sector_id, cur_user) SELECT sector_id, v_user FROM selector_sector WHERE cur_user=v_copyfromuser;
+		INSERT INTO selector_state (state_id, cur_user) SELECT state_id, v_user FROM selector_state WHERE cur_user=v_copyfromuser;
+		INSERT INTO selector_workcat (workcat_id, cur_user) SELECT workcat_id, v_user FROM selector_workcat WHERE cur_user=v_copyfromuser;		
+		
+		
+		IF v_projecttype ='UD' THEN	
 			DELETE FROM selector_inp_hydrology WHERE cur_user=v_user;
+			DELETE FROM selector_rpt_compare_tstep WHERE cur_user=v_user;
+			DELETE FROM selector_rpt_main_tstep WHERE cur_user=v_user;
+
 			INSERT INTO selector_inp_hydrology (hydrology_id, cur_user) SELECT hydrology_id, v_user FROM selector_inp_hydrology WHERE cur_user=v_copyfromuser;
+			INSERT INTO selector_rpt_main_tstep (resultdate, resulttime, cur_user) SELECT resultdate, resulttime, v_user FROM selector_rpt_main_tstep WHERE cur_user=v_copyfromuser;
+			INSERT INTO selector_rpt_compare_tstep (resultdate, resulttime, cur_user) SELECT resultdate, resulttime, v_user FROM selector_rpt_compare_tstep WHERE cur_user=v_copyfromuser;
 		ELSE
 			DELETE FROM selector_inp_demand WHERE cur_user=v_user;
+			DELETE FROM selector_mincut_result WHERE cur_user=v_user;
+			DELETE FROM selector_rpt_compare_tstep WHERE cur_user=v_user;
+			DELETE FROM selector_rpt_main_tstep WHERE cur_user=v_user;
+						
 			INSERT INTO selector_inp_demand (dscenario_id, cur_user) SELECT dscenario_id, v_user FROM selector_inp_demand WHERE cur_user=v_copyfromuser;
+			INSERT INTO selector_mincut_result (result_id, cur_user) SELECT result_id, v_user FROM selector_mincut_result WHERE cur_user=v_copyfromuser;
+			INSERT INTO selector_rpt_main_tstep (timestep, cur_user) SELECT timestep, v_user FROM selector_rpt_main_tstep WHERE cur_user=v_copyfromuser;
+			INSERT INTO selector_rpt_compare_tstep (timestep, cur_user) SELECT timestep, v_user FROM selector_rpt_compare_tstep WHERE cur_user=v_copyfromuser;
 		END IF;
 
 	-- setting values wihtout user sample
@@ -86,7 +110,7 @@ BEGIN
 		-- config_param_user, getting role parameters where ismandatory is true (forced always)
 		DELETE FROM config_param_user WHERE cur_user=v_user;
 		INSERT INTO config_param_user (parameter, value, cur_user)
-		SELECT sys_param_user.id, vdefault, v_user FROM SCHEMA_NAME.config_param_user RIGHT JOIN SCHEMA_NAME.sys_param_user ON sys_param_user.id=parameter 
+		SELECT sys_param_user.id, vdefault, v_user FROM config_param_user RIGHT JOIN sys_param_user ON sys_param_user.id=parameter 
 		WHERE ismandatory IS TRUE AND sys_role IN (SELECT rolname FROM pg_roles WHERE pg_has_role(v_user, oid, 'member'));
 
 		-- selectors
