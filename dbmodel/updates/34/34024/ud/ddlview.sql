46fba2d016af4c60f6cf10a887bdd0c8ac837481
Albert Bofill <abofill@bgeo.es>
2020-11-16 11:45:50 +0100

    Minor syntax error on inp views
    (cherry picked from commit 40d8ecd74e410b406736900fed4bb703265585cc)

--- dbmodel/updates/34/34024/ud/ddlview.sql
+++ dbmodel/updates/34/34024/ud/ddlview.sql
@@ -302,12 +302,12 @@ CREATE OR REPLACE VIEW vi_xsections AS
     0::text AS other4,
     rpt_inp_arc.barrels::integer AS other5,
     NULL::text AS other6
-   FROM inp_selector_result,
+   FROM selector_inp_result,
     rpt_inp_arc
      JOIN inp_conduit ON rpt_inp_arc.arc_id::text = inp_conduit.arc_id::text
      JOIN cat_arc ON rpt_inp_arc.arccat_id::text = cat_arc.id::text
      JOIN cat_arc_shape ON cat_arc_shape.id::text = cat_arc.shape::text
-  WHERE cat_arc_shape.epa::text = 'CUSTOM'::text AND rpt_inp_arc.result_id::text = inp_selector_result.result_id::text AND inp_selector_result.cur_user = "current_user"()::text
+  WHERE cat_arc_shape.epa::text = 'CUSTOM'::text AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text
 UNION
  SELECT rpt_inp_arc.arc_id,
     cat_arc_shape.epa AS shape,
@@ -317,12 +317,12 @@ UNION
     cat_arc.geom4::text AS other4,
     rpt_inp_arc.barrels::integer AS other5,
     inp_conduit.culvert::text AS other6
-   FROM inp_selector_result,
+   FROM selector_inp_result,
     rpt_inp_arc
      JOIN inp_conduit ON rpt_inp_arc.arc_id::text = inp_conduit.arc_id::text
      JOIN cat_arc ON rpt_inp_arc.arccat_id::text = cat_arc.id::text
      JOIN cat_arc_shape ON cat_arc_shape.id::text = cat_arc.shape::text
-  WHERE cat_arc_shape.epa::text <> 'CUSTOM'::text AND cat_arc_shape.epa::text <> 'IRREGULAR'::text AND rpt_inp_arc.result_id::text = inp_selector_result.result_id::text AND inp_selector_result.cur_user = "current_user"()::text
+  WHERE cat_arc_shape.epa::text <> 'CUSTOM'::text AND cat_arc_shape.epa::text <> 'IRREGULAR'::text AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text
 UNION
  SELECT rpt_inp_arc.arc_id,
     cat_arc_shape.epa AS shape,
@@ -332,12 +332,12 @@ UNION
     NULL::text AS other4,
     NULL::integer AS other5,
     NULL::text AS other6
-   FROM inp_selector_result,
+   FROM selector_inp_result,
     rpt_inp_arc
      JOIN inp_conduit ON rpt_inp_arc.arc_id::text = inp_conduit.arc_id::text
      JOIN cat_arc ON rpt_inp_arc.arccat_id::text = cat_arc.id::text
      JOIN cat_arc_shape ON cat_arc_shape.id::text = cat_arc.shape::text
-  WHERE cat_arc_shape.epa::text = 'IRREGULAR'::text AND rpt_inp_arc.result_id::text = inp_selector_result.result_id::text AND inp_selector_result.cur_user = "current_user"()::text
+  WHERE cat_arc_shape.epa::text = 'IRREGULAR'::text AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text
 UNION
  SELECT inp_orifice.arc_id,
     inp_typevalue.idval AS shape,
@@ -347,11 +347,11 @@ UNION
     inp_orifice.geom4::text AS other4,
     NULL::integer AS other5,
     NULL::text AS other6
-   FROM inp_selector_result,
+   FROM selector_inp_result,
     rpt_inp_arc
      JOIN inp_orifice ON inp_orifice.arc_id::text = rpt_inp_arc.arc_id::text
      LEFT JOIN inp_typevalue ON inp_typevalue.id::text = inp_orifice.shape::text
-  WHERE inp_typevalue.typevalue::text = 'inp_value_orifice'::text AND rpt_inp_arc.result_id::text = inp_selector_result.result_id::text AND inp_selector_result.cur_user = "current_user"()::text
+  WHERE inp_typevalue.typevalue::text = 'inp_value_orifice'::text AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text
 UNION
  SELECT rpt_inp_arc.arc_id,
     inp_typevalue.idval AS shape,
@@ -361,11 +361,11 @@ UNION
     inp_flwreg_orifice.geom4::text AS other4,
     NULL::integer AS other5,
     NULL::text AS other6
-   FROM inp_selector_result,
+   FROM selector_inp_result,
     rpt_inp_arc
      JOIN inp_flwreg_orifice ON rpt_inp_arc.flw_code::text = concat(inp_flwreg_orifice.node_id, '_', inp_flwreg_orifice.to_arc, '_ori_', inp_flwreg_orifice.flwreg_id)
      LEFT JOIN inp_typevalue ON inp_typevalue.id::text = inp_flwreg_orifice.shape::text
-  WHERE inp_typevalue.typevalue::text = 'inp_value_orifice'::text AND rpt_inp_arc.result_id::text = inp_selector_result.result_id::text AND inp_selector_result.cur_user = "current_user"()::text
+  WHERE inp_typevalue.typevalue::text = 'inp_value_orifice'::text AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text
 UNION
  SELECT rpt_inp_arc.arc_id,
     inp_typevalue.descript::character varying(30) AS shape,
@@ -375,11 +375,11 @@ UNION
     inp_weir.geom4::text AS other4,
     NULL::integer AS other5,
     NULL::text AS other6
-   FROM inp_selector_result,
+   FROM selector_inp_result,
     rpt_inp_arc
      JOIN inp_weir ON inp_weir.arc_id::text = rpt_inp_arc.arc_id::text
      JOIN inp_typevalue ON inp_weir.weir_type::text = inp_typevalue.idval::text
-  WHERE rpt_inp_arc.result_id::text = inp_selector_result.result_id::text AND inp_selector_result.cur_user = "current_user"()::text AND inp_typevalue.typevalue::text = 'inp_value_weirs'::text
+  WHERE rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text AND inp_typevalue.typevalue::text = 'inp_value_weirs'::text
 UNION
  SELECT rpt_inp_arc.arc_id,
     inp_typevalue.descript::character varying(30) AS shape,
@@ -389,8 +389,8 @@ UNION
     inp_flwreg_weir.geom4::text AS other4,
     NULL::integer AS other5,
     NULL::text AS other6
-   FROM inp_selector_result,
+   FROM selector_inp_result,
     rpt_inp_arc
      JOIN inp_flwreg_weir ON rpt_inp_arc.flw_code::text = concat(inp_flwreg_weir.node_id, '_', inp_flwreg_weir.to_arc, '_weir_', inp_flwreg_weir.flwreg_id)
      JOIN inp_typevalue ON inp_flwreg_weir.weir_type::text = inp_typevalue.idval::text
-  WHERE rpt_inp_arc.result_id::text = inp_selector_result.result_id::text AND inp_selector_result.cur_user = "current_user"()::text AND inp_typevalue.typevalue::text = 'inp_value_weirs'::text;
+  WHERE rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text AND inp_typevalue.typevalue::text = 'inp_value_weirs'::text;
