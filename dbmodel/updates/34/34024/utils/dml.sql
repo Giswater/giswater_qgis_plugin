c5b648cdd58dad0ce76abab25ab2e4cc6aa52c93
Néstor Ibáñez <nestor@bgeo.es>
2020-11-17 09:43:30 +0100

    FIX: ON CONFLICT keys
    (cherry picked from commit 8f51c5bb3aac7ed029255229fa6e17f6916a887c)

--- dbmodel/updates/34/34024/utils/dml.sql
+++ dbmodel/updates/34/34024/utils/dml.sql
@@ -313,7 +313,7 @@ UPDATE config_toolbox SET alias = 'Linear Reference System' WHERE alias = 'LRS';
 -- 2020/11/16
 INSERT INTO sys_function(id, function_name, project_type, function_type, input_params, return_type, descript, sys_role, sample_query)
 VALUES (3010, 'gw_fct_getmincut', 'ws', 'function','json', 'json', 
-'Function that gets mincut information', 'role_om', NULL) ON CONFLICT (id) DO NOTHING;
+'Function that gets mincut information', 'role_om', NULL) ON CONFLICT (function_name, project_type) DO NOTHING;
 
 INSERT INTO sys_fprocess(fid, fprocess_name, project_type)
 
