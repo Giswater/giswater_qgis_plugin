b3277cca8dba2a8562889ce976984b865bcc70e3
Albert Bofill <abofill@bgeo.es>
2020-11-23 15:26:37 +0100

    Add onService execute mode to trg_edit_psector
    (cherry picked from commit 79f8f991961fd67a9df25d38c74f84cc36351a40)

--- dbmodel/utils/ftrg/gw_trg_edit_psector.sql
+++ dbmodel/utils/ftrg/gw_trg_edit_psector.sql
@@ -26,13 +26,21 @@ v_state_canceled_ficticious integer;
 v_plan_statetype_ficticious integer;
 v_plan_statetype_planned integer;
 v_current_state_type integer;
-v_id text;  
+v_id text; 
+v_statetype_obsolete integer; 
+v_statetype_onservice integer;
+v_auto_downgrade_link boolean; 
+v_ischild text;
+v_execute_mode text;
+v_parent_id integer;
+v_temporal_psector_id integer;
 
 BEGIN
 
     EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
 
     om_aux:= TG_ARGV[0];
+    v_execute_mode:= (SELECT value::json ->> 'mode' FROM config_param_system WHERE parameter='plan_psector_execute_action');
 
     -- Control insertions ID
     IF TG_OP = 'INSERT' THEN
@@ -68,11 +76,7 @@ BEGIN
 	END IF;
 		
 	-- Control insertions ID
-	IF (NEW.psector_id IS NULL and om_aux='om') THEN
-		NEW.psector_id:= (SELECT nextval('om_psector_id_seq'));
-	ELSIF (NEW.psector_id IS NULL and om_aux='plan') THEN
-		NEW.psector_id:= (SELECT nextval('plan_psector_id_seq'));
-	END IF;   
+	NEW.psector_id:= (SELECT nextval('plan_psector_id_seq'));
 	
 	IF om_aux='plan' THEN
 
@@ -97,74 +101,207 @@ BEGIN
 		text3=NEW.text3, text4=NEW.text4, text5=NEW.text5, text6=NEW.text6, num_value=NEW.num_value
 		WHERE psector_id=OLD.psector_id;
 
-		--if the status of a psector had changed to 3 or 0 proceed with changes of feature states
-		IF (OLD.status != NEW.status) AND (NEW.status = 0 OR NEW.status = 2 OR NEW.status = 3)  THEN
+		--onService mode transform all features afected by psector to its planified state and makes a copy of psector
+		IF v_execute_mode='onService' THEN
 
-			--get the values of future state_types 
-			SELECT ((value::json)->>'done_planified') INTO v_state_done_planified FROM config_param_system WHERE parameter='plan_psector_statetype';
-			SELECT ((value::json)->>'done_ficticious') INTO v_state_done_ficticious FROM config_param_system WHERE parameter='plan_psector_statetype';
-			SELECT ((value::json)->>'canceled_planified') INTO v_state_canceled_planified FROM config_param_system WHERE parameter='plan_psector_statetype';
-			SELECT ((value::json)->>'canceled_ficticious') INTO v_state_canceled_ficticious FROM config_param_system WHERE parameter='plan_psector_statetype';
-			SELECT value::integer INTO v_plan_statetype_ficticious FROM config_param_system WHERE parameter = 'plan_statetype_ficticius';
-			SELECT value::integer INTO v_plan_statetype_planned FROM config_param_system WHERE parameter = 'plan_statetype_planned';
+			-- update psector status to EXECUTED
+			IF (OLD.status != NEW.status) AND (NEW.status = 0) THEN
 
-			--temporary remove topology control
-			UPDATE config_param_system set value = 'false' WHERE parameter='edit_state_topocontrol';
+				--make a copy of the psector to mantain traceability
+				EXECUTE 'SELECT SCHEMA_NAME.gw_fct_psector_duplicate($${
+				"client":{"device":4, "infoType":1, "lang":"ES"},
+				"form":{},"feature":{"type":"PSECTOR"},
+				"data":{"psector_id":"'||OLD.psector_id||'","new_psector_name":"temporal_psector"}}$$);';
 
-			--loop over network feature types in order to get the data from each plan_psector_x_* table 
-			FOR rec_type IN (SELECT * FROM sys_feature_type WHERE classlevel = 1 OR classlevel = 2 ORDER BY id asc) LOOP
-
-				v_sql = 'SELECT '||rec_type.id||'_id as id FROM plan_psector_x_'||lower(rec_type.id)||' WHERE state = 1 AND psector_id = '||OLD.psector_id||';';
-
-				--loop over each feature in plan_psector_x_* table in order to update state values
-				FOR rec IN EXECUTE v_sql LOOP
+				SELECT value::integer INTO v_statetype_obsolete FROM config_param_user WHERE parameter='edit_statetype_0_vdefault' AND cur_user=current_user;
+				IF v_statetype_obsolete IS NULL THEN
+				        EXECUTE 'SELECT SCHEMA_NAME.gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
+					"data":{"message":"3134", "function":"2446","debug_msg":null}}$$);';
+				END IF;
 				
-					--get the current state_type of a feature
-					EXECUTE 'SELECT state_type FROM v_edit_'||lower(rec_type.id)||' WHERE '||lower(rec_type.id)||'_id = '''||rec.id||''';'
-					INTO v_current_state_type;
+				SELECT value::integer INTO v_statetype_onservice FROM config_param_user WHERE parameter='edit_statetype_1_vdefault' AND cur_user=current_user;
+				IF v_statetype_onservice IS NULL THEN
+				        EXECUTE 'SELECT SCHEMA_NAME.gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
+					"data":{"message":"3136", "function":"2446","debug_msg":null}}$$);';
+				END IF;
+				
+				--temporary remove topology control
+				UPDATE config_param_system set value = 'false' WHERE parameter='edit_state_topocontrol';
 
-					--set planned features to obsolete and update state_type depending on the new status and current state_type
-					IF NEW.status = 0 THEN	
-						IF v_current_state_type = v_plan_statetype_ficticious OR v_current_state_type = v_state_canceled_ficticious THEN
-							EXECUTE 'UPDATE v_edit_'||lower(rec_type.id)||' SET state = 0, state_type = '||v_state_done_ficticious||'
-							WHERE '||lower(rec_type.id)||'_id = '''||rec.id||''';';
+				--use automatic downgrade link variable 
+				SELECT value::boolean INTO v_auto_downgrade_link FROM config_param_user WHERE parameter='edit_connect_downgrade_link' AND cur_user=current_user;
+				IF v_auto_downgrade_link IS NULL THEN
+					INSERT INTO config_param_user VALUES ('edit_connect_downgrade_link', 'true', current_user);
+				END IF;
+
+				--change state/state_type of psector features acording to its current status on the psector_x_* table
+				FOR rec_type IN (SELECT * FROM sys_feature_type WHERE classlevel = 1 OR classlevel = 2 ORDER BY id asc) LOOP
+
+					v_sql = 'SELECT '||rec_type.id||'_id as id, state FROM plan_psector_x_'||lower(rec_type.id)||' WHERE psector_id = '||OLD.psector_id||';';
+
+					FOR rec IN EXECUTE v_sql LOOP
 							
-						ELSE
-							EXECUTE 'UPDATE v_edit_'||lower(rec_type.id)||' SET state = 0, state_type = '||v_state_done_planified||'
-							WHERE '||lower(rec_type.id)||'_id = '''||rec.id||''';';
-						END IF;
+						-- capture if arc is child in order to add their parents' relations
+						SELECT ((addparam::json) ->> 'arcDivide') INTO v_ischild FROM plan_psector_x_arc WHERE arc_id=rec.id AND psector_id=OLD.psector_id;
+
+						IF v_ischild='child' THEN
+							-- get its parent_id
+							SELECT arc_id::integer INTO v_parent_id FROM audit_arc_traceability WHERE (arc_id1=rec.id) or (arc_id2=rec.id);
+
+							--insert related documents to child arc
+							INSERT INTO doc_x_arc(doc_id, arc_id) 
+							SELECT doc_id, rec.id FROM (SELECT doc_id FROM doc_x_arc WHERE arc_id::integer=v_parent_id)a;
 						
-					ELSIF NEW.status = 2 THEN
-						IF v_current_state_type = v_state_done_ficticious OR v_current_state_type = v_state_canceled_ficticious THEN
-							EXECUTE 'UPDATE v_edit_'||lower(rec_type.id)||' SET state = 2, state_type = '||v_plan_statetype_ficticious||'
-							WHERE '||lower(rec_type.id)||'_id = '''||rec.id||''';';
+							--insert related elements to child arc
+							INSERT INTO element_x_arc(element_id, arc_id) 
+							SELECT element_id, rec.id FROM (SELECT element_id FROM element_x_arc WHERE arc_id::integer=v_parent_id)a;
 							
-						ELSE
-							EXECUTE 'UPDATE v_edit_'||lower(rec_type.id)||' SET state = 2, state_type = '||v_plan_statetype_planned||'
-							WHERE '||lower(rec_type.id)||'_id = '''||rec.id||''';';
-							
-						END IF;			
+							--insert related visits to child arc
+							INSERT INTO om_visit_x_arc(visit_id, arc_id) 
+							SELECT visit_id, rec.id FROM (SELECT visit_id FROM om_visit_x_arc WHERE arc_id::integer=v_parent_id)a;
+
+						END IF;
+
+						-- set obsolete features where psector_x_* state is 0
+						EXECUTE 'UPDATE '||lower(rec_type.id)||' n SET state = 0, state_type = '||v_statetype_obsolete||' 
+						FROM plan_psector_x_'||lower(rec_type.id)||' p WHERE n.'||lower(rec_type.id)||'_id = p.'||lower(rec_type.id)||'_id 
+						AND p.state = 0 AND p.psector_id='||OLD.psector_id||' AND n.'||lower(rec_type.id)||'_id = '''||rec.id||''';';
 						
-					ELSIF NEW.status = 3 THEN
-						IF v_current_state_type = v_plan_statetype_ficticious OR v_current_state_type = v_state_done_ficticious THEN
-							EXECUTE 'UPDATE v_edit_'||lower(rec_type.id)||' SET state = 0, state_type = '||v_state_canceled_ficticious||'
-							WHERE '||lower(rec_type.id)||'_id = '''||rec.id||''';';
-							
-						ELSE
-							EXECUTE 'UPDATE v_edit_'||lower(rec_type.id)||' SET state = 0, state_type = '||v_state_canceled_planified||'
-							WHERE '||lower(rec_type.id)||'_id = '''||rec.id||''';';
+						-- delete from psector_x_* where state is 0
+						EXECUTE 'DELETE FROM plan_psector_x_'||lower(rec_type.id)||' 
+						WHERE psector_id = '||OLD.psector_id||' AND '||lower(rec_type.id)||'_id = '''||rec.id||''' AND '''||rec.state||''' = 0;';
+
+						-- set on service feature where psector_x_* state is 1
+						EXECUTE 'UPDATE '||lower(rec_type.id)||' n SET state = 1, state_type = '||v_statetype_onservice||' 
+						FROM plan_psector_x_'||lower(rec_type.id)||' p WHERE n.'||lower(rec_type.id)||'_id = p.'||lower(rec_type.id)||'_id 
+						AND p.state = 1 AND p.psector_id='||OLD.psector_id||' AND n.'||lower(rec_type.id)||'_id = '''||rec.id||''';';
+
+						-- change arc_id for planified connecs (useful when existing connec changes to planified arc)
+						IF lower(rec_type.id) IN ('connec', 'gully') THEN
+							EXECUTE 'UPDATE '||lower(rec_type.id)||' n SET arc_id = p.arc_id
+							FROM plan_psector_x_'||lower(rec_type.id)||' p WHERE n.'||lower(rec_type.id)||'_id = p.'||lower(rec_type.id)||'_id 
+							AND p.state = 1 AND p.psector_id='||OLD.psector_id||' AND n.'||lower(rec_type.id)||'_id = '''||rec.id||''';';
+
+							EXECUTE 'UPDATE link SET the_geom = link_geom, userdefined_geom = p.userdefined_geom
+							FROM plan_psector_x_'||lower(rec_type.id)||' p WHERE link.feature_id = p.'||lower(rec_type.id)||'_id 
+							AND p.state = 1 AND p.psector_id='||OLD.psector_id||' AND link.feature_id = '''||rec.id||''';';
+
+							EXECUTE 'UPDATE vnode SET the_geom = vnode_geom	FROM plan_psector_x_'||lower(rec_type.id)||' p 
+							JOIN link ON link.feature_id = p.'||lower(rec_type.id)||'_id AND p.state = 1 AND p.psector_id='||OLD.psector_id||' 
+							AND link.feature_id = '''||rec.id||''' WHERE link.exit_id::integer=vnode.vnode_id;';
 							
 						END IF;
-					END IF;
-					
+
+						-- delete from psector_x_* where state is 1
+						EXECUTE 'DELETE FROM plan_psector_x_'||lower(rec_type.id)||' 
+						WHERE psector_id = '||OLD.psector_id||' AND '||lower(rec_type.id)||'_id = '''||rec.id||''' AND '''||rec.state||''' = 1;';
+
+						-- set link on service to connecs where psector_x_connec state is 1
+						EXECUTE 'UPDATE link SET state=1 WHERE feature_id IN (SELECT connec_id FROM plan_psector_x_connec 
+						WHERE psector_id = '||OLD.psector_id||' AND state= 1);';
+						
+					END LOOP;
 				END LOOP;
-				--reestablish topology control
-				UPDATE config_param_system set value = 'true' WHERE parameter='edit_state_topocontrol';
-				--show information about performed state update
 
-				EXECUTE 'SELECT gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
+				--reset downgrade link variable
+				IF v_auto_downgrade_link IS NULL THEN
+					DELETE FROM config_param_user WHERE parameter='edit_connect_downgrade_link' AND cur_user=current_user;
+				END IF;
+
+				--fill old psector with the copied values to mantain tracability (only state=1 values)
+				SELECT psector_id INTO v_temporal_psector_id FROM plan_psector WHERE name='temporal_psector';
+				
+				INSERT INTO plan_psector_x_arc(arc_id, psector_id, state, doable, descript, addparam) 
+				SELECT arc_id, OLD.psector_id, state, doable, descript, addparam FROM plan_psector_x_arc 
+				WHERE psector_id=v_temporal_psector_id AND (addparam IS NULL  OR ((addparam::json) ->> 'arcDivide')='parent');
+
+				INSERT INTO plan_psector_x_node(node_id, psector_id, state, doable, descript) 
+				SELECT node_id, OLD.psector_id, state, doable, descript FROM plan_psector_x_node WHERE psector_id=v_temporal_psector_id;
+
+				INSERT INTO plan_psector_x_connec(connec_id, arc_id, psector_id, state, doable, descript, link_geom, vnode_geom, userdefined_geom) 
+				SELECT connec_id, arc_id, OLD.psector_id, state, doable, descript, link_geom, vnode_geom, userdefined_geom FROM plan_psector_x_connec WHERE psector_id=v_temporal_psector_id;
+
+				--delete temporal psector after all changes
+				DELETE FROM plan_psector WHERE psector_id=v_temporal_psector_id;
+
+				PERFORM setval('plan_psector_id_seq', (SELECT max(psector_id) FROM plan_psector));
+				
+			END IF;
+		END IF;
+
+		--obsolete mode set all features afected to obsolete but manage their state_type
+		IF v_execute_mode='obsolete' THEN
+		
+			--if the status of a psector had changed to 3 or 0 proceed with changes of feature states
+			IF (OLD.status != NEW.status) AND (NEW.status = 0 OR NEW.status = 2 OR NEW.status = 3)  THEN
+
+				--get the values of future state_types 
+				SELECT ((value::json)->>'done_planified') INTO v_state_done_planified FROM config_param_system WHERE parameter='plan_psector_statetype';
+				SELECT ((value::json)->>'done_ficticious') INTO v_state_done_ficticious FROM config_param_system WHERE parameter='plan_psector_statetype';
+				SELECT ((value::json)->>'canceled_planified') INTO v_state_canceled_planified FROM config_param_system WHERE parameter='plan_psector_statetype';
+				SELECT ((value::json)->>'canceled_ficticious') INTO v_state_canceled_ficticious FROM config_param_system WHERE parameter='plan_psector_statetype';
+				SELECT value::integer INTO v_plan_statetype_ficticious FROM config_param_system WHERE parameter = 'plan_statetype_ficticius';
+				SELECT value::integer INTO v_plan_statetype_planned FROM config_param_system WHERE parameter = 'plan_statetype_planned';
+
+				--temporary remove topology control
+				UPDATE config_param_system set value = 'false' WHERE parameter='edit_state_topocontrol';
+
+				--loop over network feature types in order to get the data from each plan_psector_x_* table 
+				FOR rec_type IN (SELECT * FROM sys_feature_type WHERE classlevel = 1 OR classlevel = 2 ORDER BY id asc) LOOP
+
+					v_sql = 'SELECT '||rec_type.id||'_id as id FROM plan_psector_x_'||lower(rec_type.id)||' WHERE state = 1 AND psector_id = '||OLD.psector_id||';';
+
+					--loop over each feature in plan_psector_x_* table in order to update state values
+					FOR rec IN EXECUTE v_sql LOOP
+					
+						--get the current state_type of a feature
+						EXECUTE 'SELECT state_type FROM v_edit_'||lower(rec_type.id)||' WHERE '||lower(rec_type.id)||'_id = '''||rec.id||''';'
+						INTO v_current_state_type;
+
+						--set planned features to obsolete and update state_type depending on the new status and current state_type
+						IF NEW.status = 0 THEN	
+							IF v_current_state_type = v_plan_statetype_ficticious OR v_current_state_type = v_state_canceled_ficticious THEN
+								EXECUTE 'UPDATE v_edit_'||lower(rec_type.id)||' SET state = 0, state_type = '||v_state_done_ficticious||'
+								WHERE '||lower(rec_type.id)||'_id = '''||rec.id||''';';
+								
+							ELSE
+								EXECUTE 'UPDATE v_edit_'||lower(rec_type.id)||' SET state = 0, state_type = '||v_state_done_planified||'
+								WHERE '||lower(rec_type.id)||'_id = '''||rec.id||''';';
+							END IF;
+							
+						ELSIF NEW.status = 2 THEN
+							IF v_current_state_type = v_state_done_ficticious OR v_current_state_type = v_state_canceled_ficticious THEN
+								EXECUTE 'UPDATE v_edit_'||lower(rec_type.id)||' SET state = 2, state_type = '||v_plan_statetype_ficticious||'
+								WHERE '||lower(rec_type.id)||'_id = '''||rec.id||''';';
+								
+							ELSE
+								EXECUTE 'UPDATE v_edit_'||lower(rec_type.id)||' SET state = 2, state_type = '||v_plan_statetype_planned||'
+								WHERE '||lower(rec_type.id)||'_id = '''||rec.id||''';';
+								
+							END IF;			
+							
+						ELSIF NEW.status = 3 THEN
+							IF v_current_state_type = v_plan_statetype_ficticious OR v_current_state_type = v_state_done_ficticious THEN
+								EXECUTE 'UPDATE v_edit_'||lower(rec_type.id)||' SET state = 0, state_type = '||v_state_canceled_ficticious||'
+								WHERE '||lower(rec_type.id)||'_id = '''||rec.id||''';';
+								
+							ELSE
+								EXECUTE 'UPDATE v_edit_'||lower(rec_type.id)||' SET state = 0, state_type = '||v_state_canceled_planified||'
+								WHERE '||lower(rec_type.id)||'_id = '''||rec.id||''';';
+								
+							END IF;
+						END IF;
+						
+					END LOOP;
+					--reestablish topology control
+					UPDATE config_param_system set value = 'true' WHERE parameter='edit_state_topocontrol';
+					--show information about performed state update
+
+					EXECUTE 'SELECT gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
 				"data":{"message":"3034", "function":"2446","debug_msg":null}}$$);';
-			END LOOP;
+				END LOOP;
+				
+			END IF;
 		END IF;
 	END IF;
                
