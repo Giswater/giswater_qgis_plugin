b255304c434afb98e9c8213345250d78096ed746
Barbara <bbrzepka@bgeo.es>
2020-11-26 08:17:02 +0100

    settoarc - show message for features that arent delimiters

--- dbmodel/ws/fct/ws_gw_fct_settoarc.sql
+++ dbmodel/ws/fct/ws_gw_fct_settoarc.sql
@@ -42,6 +42,7 @@ v_mapzone_array text[];
 rec text;
 v_check_grafconfig text;
 
+v_level integer;
 v_result_info text;
 v_result text;
 v_version text;
@@ -72,116 +73,131 @@ BEGIN
         SELECT arc_id INTO v_arc_id FROM arc WHERE node_1 =  v_feature_id;
     END IF;
 
-    --Insert to_arc of GO2EPA
-    IF v_epatype = 'PUMP' THEN
-        UPDATE inp_pump SET to_arc = v_arc_id WHERE node_id = v_feature_id;
-        
-        INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (359,1, concat('Set to_arc of pump ', v_feature_id, ' with value ',v_arc_id, '.'));
+    IF v_epatype IN ('PUMP', 'VALVE', 'SHORTPIPE') OR v_systype IN ('TANK', 'RESERVOIR', 'WATERWELL', 'SOURCE') OR v_grafdelim != 'NONE' THEN
+        --Insert to_arc of GO2EPA
+        IF v_epatype = 'PUMP' THEN
+            UPDATE inp_pump SET to_arc = v_arc_id WHERE node_id = v_feature_id;
+            
+            INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (359,1, concat('Set to_arc of pump ', v_feature_id, ' with value ',v_arc_id, '.'));
 
-    ELSIF v_epatype = 'VALVE' THEN
-        UPDATE inp_valve SET to_arc = v_arc_id WHERE node_id = v_feature_id;
+        ELSIF v_epatype = 'VALVE' THEN
+            UPDATE inp_valve SET to_arc = v_arc_id WHERE node_id = v_feature_id;
 
-        INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (359,1, concat('Set to_arc of valve ', v_feature_id, ' with value ',v_arc_id, '.'));
+            INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (359,1, concat('Set to_arc of valve ', v_feature_id, ' with value ',v_arc_id, '.'));
 
-    ELSIF v_epatype = 'SHORTPIPE' THEN
-        UPDATE inp_shortpipe SET to_arc = v_arc_id WHERE node_id = v_feature_id;
+        ELSIF v_epatype = 'SHORTPIPE' THEN
+            UPDATE inp_shortpipe SET to_arc = v_arc_id WHERE node_id = v_feature_id;
 
-        INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (359,1, concat('Set to_arc of shortpipe ', v_feature_id, ' with value ',v_arc_id, '.'));
-    END IF;
+            INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (359,1, concat('Set to_arc of shortpipe ', v_feature_id, ' with value ',v_arc_id, '.'));
+        END IF;
 
-    --Insert to_arc of MINCUT
-    IF v_systype IN ('TANK', 'RESERVOIR', 'WATERWELL', 'SOURCE') THEN
-        UPDATE config_mincut_inlet SET parameters = jsonb_build_object('inletArc', ARRAY[v_arc_id::text]) WHERE node_id = v_feature_id;
+        --Insert to_arc of MINCUT
+        IF v_systype IN ('TANK', 'RESERVOIR', 'WATERWELL', 'SOURCE') THEN
+            
+            EXECUTE 'INSERT INTO config_mincut_inlet(node_id, expl_id,parameters, active)
+            SELECT '||v_feature_id||', expl_id, jsonb_build_object(''inletArc'', ARRAY['||v_arc_id::text||']), TRUE 
+            FROM node WHERE node.node_id = '||quote_literal(v_feature_id)||'
+            ON CONFLICT (node_id, expl_id) DO
+            UPDATE SET parameters = jsonb_build_object(''inletArc'', ARRAY['||v_arc_id::text||']) WHERE config_mincut_inlet.node_id = '||quote_literal(v_feature_id)||';';
 
-        INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (359,1, concat('Set inletArc of config_mincut_inlet for node ', v_feature_id, ' with value ',v_arc_id, '.'));
+            INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (359,1, concat('Set inletArc of config_mincut_inlet for node ', v_feature_id, ' with value ',v_arc_id, '.'));
 
-    END IF;
+        END IF;
 
-    --define list of mapzones to be set 
-    IF  v_systype IN ('TANK', 'RESERVOIR', 'WATERWELL', 'SOURCE') THEN
-        v_mapzone_array = ARRAY['dma','presszone'];
-    ELSE 
-        v_mapzone_array = ARRAY[lower(v_grafdelim)];
-    END IF;
+        --define list of mapzones to be set 
+        IF  v_systype IN ('TANK', 'RESERVOIR', 'WATERWELL', 'SOURCE') THEN
+            v_mapzone_array = ARRAY['dma','presszone'];
+        ELSE 
+            v_mapzone_array = ARRAY[lower(v_grafdelim)];
+        END IF;
 
-    FOREACH rec IN ARRAY(v_mapzone_array) LOOP
+        FOREACH rec IN ARRAY(v_mapzone_array) LOOP
 
-        IF rec = 'minsector' THEN
-            INSERT INTO config_checkvalve (node_id, to_arc, active) 
-            VALUES (v_feature_id, v_arc_id,TRUE) ON CONFLICT (node_id) DO UPDATE SET to_arc = v_arc_id;
+            IF rec = 'minsector' THEN
+                INSERT INTO config_checkvalve (node_id, to_arc, active) 
+                VALUES (v_feature_id, v_arc_id,TRUE) ON CONFLICT (node_id) DO UPDATE SET to_arc = v_arc_id;
 
-            INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (359,1, concat('Set to_arc of config_checkvalve for node ', v_feature_id, ' with value ',v_arc_id, '.'));
+                INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (359,1, concat('Set to_arc of config_checkvalve for node ', v_feature_id, ' with value ',v_arc_id, '.'));
 
-        ELSIF rec = 'dma' OR rec = 'presszone'  THEN
-            IF rec = 'dma' THEN
-                v_mapzone_id=v_dma_id;
+            ELSIF rec = 'dma' OR rec = 'presszone'  THEN
+                IF rec = 'dma' THEN
+                    v_mapzone_id=v_dma_id;
 
-                INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (359,1, concat('Set to_arc of dma ', v_dma_id, ' with value ',v_arc_id, '.'));
+                    INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (359,1, concat('Set to_arc of dma ', v_dma_id, ' with value ',v_arc_id, '.'));
 
-            ELSIF rec = 'presszone' THEN
-                v_mapzone_id=v_presszone_id;
+                ELSIF rec = 'presszone' THEN
+                    v_mapzone_id=v_presszone_id;
 
-                INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (359,1, concat('Set to_arc of presszone ', v_dma_id, ' with value ',v_arc_id, '.'));
-            END IF;
+                    INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (359,1, concat('Set to_arc of presszone ', v_dma_id, ' with value ',v_arc_id, '.'));
+                END IF;
 
-            IF v_mapzone_id IS NULL THEN
-                EXECUTE 'SELECT '||rec||'_id  FROM node WHERE node_id = '||quote_literal(v_feature_id)||''
-                INTO v_mapzone_id;
-            END IF;
+                IF v_mapzone_id IS NULL THEN
+                    EXECUTE 'SELECT '||rec||'_id  FROM node WHERE node_id = '||quote_literal(v_feature_id)||''
+                    INTO v_mapzone_id;
+                END IF;
 
-            EXECUTE 'SELECT grafconfig FROM '||rec||' WHERE '||rec||'_id::text = '||v_mapzone_id||'::text'
-            INTO v_check_grafconfig;
-
-                IF v_check_grafconfig IS NULL THEN 
-            --Define grafconfig in case when its null
-           
-                EXECUTE 'SELECT jsonb_build_object(''use'',ARRAY[a.feature], ''ignore'',''{}''::text[]) FROM (
-                SELECT jsonb_build_object(
-                ''nodeParent'','||v_feature_id||'::text,
-                ''toArc'',   ARRAY['||v_arc_id||'::text] ) AS feature)a'
-                INTO v_config;
-
-                EXECUTE'UPDATE '||rec||' SET grafconfig = '||quote_literal(v_config)||' WHERE '||rec||'_id::text = '||v_mapzone_id||'::text;';
-            ELSE
-                
-                EXECUTE  'SELECT 1 FROM '||rec||' CROSS JOIN json_array_elements((grafconfig->>''use'')::json) elem 
-                WHERE elem->>''nodeParent'' = '||quote_literal(v_feature_id)||' AND '||rec||'_id::text = '||v_mapzone_id||'::text LIMIT 1'
+                EXECUTE 'SELECT grafconfig FROM '||rec||' WHERE '||rec||'_id::text = '||v_mapzone_id||'::text'
                 INTO v_check_grafconfig;
 
-                IF v_check_grafconfig = '1'  THEN 
-                    --Define grafconfig in case when there is definition for mapzone and node is already defined there
-
-                    EXECUTE 'UPDATE '||rec||' set grafconfig = replace(grafconfig::text,a.toarc,'||v_arc_id||'::text)::json FROM (
-                    select json_array_elements_text((elem->>''toArc'')::json) as toarc, elem->>''nodeParent'' as elem
-                    from '||rec||'
-                    cross join json_array_elements((grafconfig->>''use'')::json) elem
-                    where elem->>''nodeParent'' = '||quote_literal(v_feature_id)||' AND '||rec||'_id::text = '||v_mapzone_id||'::text)a 
-                    WHERE '||rec||'_id::text ='||v_mapzone_id||'::text';
-
-                ELSE    
-                    --Define grafconfig in case when there is definition for mapzone and node is not defined there
-                    EXECUTE 'SELECT jsonb_build_object(
+                    IF v_check_grafconfig IS NULL THEN 
+                --Define grafconfig in case when its null
+               
+                    EXECUTE 'SELECT jsonb_build_object(''use'',ARRAY[a.feature], ''ignore'',''{}''::text[]) FROM (
+                    SELECT jsonb_build_object(
                     ''nodeParent'','||v_feature_id||'::text,
-                    ''toArc'',   ARRAY['||v_arc_id||'::text] ) AS feature'
+                    ''toArc'',   ARRAY['||v_arc_id||'::text] ) AS feature)a'
                     INTO v_config;
-            
 
-                    EXECUTE 'SELECT jsonb_build_object(''use'',b.feature, ''ignore'',(grafconfig->>''ignore'')::json) FROM '||rec||',( 
-                    SELECT jsonb_agg(a.use) || '''||v_config||'''::jsonb as feature   FROM (
-                    select '||rec||'_id, json_array_elements((grafconfig->>''use'')::json) as use, grafconfig
-                    from '||rec||' where '||rec||'_id::text ='||v_mapzone_id||'::text)a)b where '||rec||'_id::text ='||v_mapzone_id||'::text'
-                    INTO v_config;
- 
                     EXECUTE'UPDATE '||rec||' SET grafconfig = '||quote_literal(v_config)||' WHERE '||rec||'_id::text = '||v_mapzone_id||'::text;';
+                ELSE
                     
+                    EXECUTE  'SELECT 1 FROM '||rec||' CROSS JOIN json_array_elements((grafconfig->>''use'')::json) elem 
+                    WHERE elem->>''nodeParent'' = '||quote_literal(v_feature_id)||' AND '||rec||'_id::text = '||v_mapzone_id||'::text LIMIT 1'
+                    INTO v_check_grafconfig;
+
+                    IF v_check_grafconfig = '1'  THEN 
+                        --Define grafconfig in case when there is definition for mapzone and node is already defined there
+
+                        EXECUTE 'UPDATE '||rec||' set grafconfig = replace(grafconfig::text,a.toarc,'||v_arc_id||'::text)::json FROM (
+                        select json_array_elements_text((elem->>''toArc'')::json) as toarc, elem->>''nodeParent'' as elem
+                        from '||rec||'
+                        cross join json_array_elements((grafconfig->>''use'')::json) elem
+                        where elem->>''nodeParent'' = '||quote_literal(v_feature_id)||' AND '||rec||'_id::text = '||v_mapzone_id||'::text)a 
+                        WHERE '||rec||'_id::text ='||v_mapzone_id||'::text';
+
+                    ELSE    
+                        --Define grafconfig in case when there is definition for mapzone and node is not defined there
+                        EXECUTE 'SELECT jsonb_build_object(
+                        ''nodeParent'','||v_feature_id||'::text,
+                        ''toArc'',   ARRAY['||v_arc_id||'::text] ) AS feature'
+                        INTO v_config;
+                
+
+                        EXECUTE 'SELECT jsonb_build_object(''use'',b.feature, ''ignore'',(grafconfig->>''ignore'')::json) FROM '||rec||',( 
+                        SELECT jsonb_agg(a.use) || '''||v_config||'''::jsonb as feature   FROM (
+                        select '||rec||'_id, json_array_elements((grafconfig->>''use'')::json) as use, grafconfig
+                        from '||rec||' where '||rec||'_id::text ='||v_mapzone_id||'::text)a)b where '||rec||'_id::text ='||v_mapzone_id||'::text'
+                        INTO v_config;
+     
+                        EXECUTE'UPDATE '||rec||' SET grafconfig = '||quote_literal(v_config)||' WHERE '||rec||'_id::text = '||v_mapzone_id||'::text;';
+                        
+                    END IF;
                 END IF;
-            END IF;
-       END IF;
-    END LOOP;
+           END IF;
+        END LOOP;
+    ELSE
+        v_level = 0;
+        INSERT INTO audit_check_data (fid, criticity, error_message) 
+        VALUES (359,1, concat('There is nothing to configure for  ', v_feature_type,' ', v_feature_id, '.'));
+    END IF;
 
     SELECT  string_agg(error_message,' ') INTO v_result  FROM audit_check_data WHERE cur_user="current_user"() AND 
     fid = 359;
-    
+   
+    IF v_level IS NULL THEN
+        v_level=3;
+    END IF; 
+
     v_result_info = v_result;
     
     --    Control nulls
@@ -191,7 +207,7 @@ BEGIN
     v_result = concat ('{"geometryType":"", "values":',to_json(v_result), '}');
 
     --  Return
-    RETURN ('{"status":"Accepted", "message":{"level":3, "text":"'||v_result_info||'"}, "version":"'||v_version||'"'||
+    RETURN ('{"status":"Accepted", "message":{"level":'||v_level||', "text":"'||v_result_info||'"}, "version":"'||v_version||'"'||
              ',"body":{"form":{}'||
              ',"data":{ "info":'||v_result||'}'||
             '}'||
