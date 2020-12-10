8c7e0a81f85a8da23383b5a57662cf38d1080bce
Edgar Fuste <efuste@bgeo.es>
2020-12-10 16:51:34 +0100

    WIP refactor variable project_vars

--- global_vars.py
+++ global_vars.py
@@ -22,7 +22,7 @@ project_type = None
 
 settings = None
 qgis_settings = None
-project_vars = {'gwInfoType': None,} # qgis_project_vars
+project_vars = {}
 session_vars = {'user_settings':None, 'user_settings_path':None, 'min_log_level':20, 'min_message_level':0, 'last_error': None, 'show_db_exception':True, 'dlg_info':None,'gw_infotools':None,'current_user':None,'dlg_docker':None,'show_docker':None,'docker_type':None,'logged':None,
                 'db':None, 'postgresql_version':None, 'dao':None, 'credentials':None}
 srid = None
