e21ac276609af1ddc4784383eddb876a72eeac47
Barbara <bbrzepka@bgeo.es>
2020-12-14 15:23:10 +0100

    minor bug fix on fct manage_backup

--- dbmodel/utils/fct/gw_fct_admin_manage_backup.sql
+++ dbmodel/utils/fct/gw_fct_admin_manage_backup.sql
@@ -193,7 +193,7 @@ BEGIN
 	    '}')::json;
 
     EXCEPTION WHEN OTHERS THEN
-	GETT STACKED DIAGNOSTICS v_error_context = PG_EXCEPTION_CONTEXT;
+	GET STACKED DIAGNOSTICS v_error_context = PG_EXCEPTION_CONTEXT;
 	RETURN ('{"status":"Failed","NOSQLERR":' || to_json(SQLERRM) || ',"SQLSTATE":' || to_json(SQLSTATE) ||',"SQLCONTEXT":' || to_json(v_error_context) || '}')::json;
 
 END;
