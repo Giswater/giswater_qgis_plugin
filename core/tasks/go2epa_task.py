9e7d06909de0e325495d8d9c993f6ddd53081901
Edgar Fuste <efuste@bgeo.es>
2021-01-05 14:56:33 +0100

    Minor bug fix on previous commit ("ef07cfac9afaa097ee14a04665f56830f7286345") Refactor file path reference

--- core/tasks/go2epa_task.py
+++ core/tasks/go2epa_task.py
@@ -235,7 +235,7 @@ class GwGo2EpaTask(QgsTask):
         if self.project_type in 'ws':
             opener = f"{self.plugin_dir}{os.sep}resources{os.sep}epa{os.sep}epanet{os.sep}ws_epanet20012.exe"
         elif self.project_type in 'ud':
-            opener = f"{self.plugin_dir}resources{os.sep}epa{os.sep}swmm{os.sep}ud_swmm50022.exe"
+            opener = f"{self.plugin_dir}{os.sep}resources{os.sep}epa{os.sep}swmm{os.sep}ud_swmm50022.exe"
 
         if opener is None:
             return False
