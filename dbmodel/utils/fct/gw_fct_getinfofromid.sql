7605c40a72bfb95dabaef125b466c36db4370fed
Barbara <bbrzepka@bgeo.es>
2021-01-07 09:09:17 +0100

    minor bug fix on getinfofromid where id is null
    (cherry picked from commit c9838dfdb2a284b4ad6842546c982b1b029bd600)

--- dbmodel/utils/fct/gw_fct_getinfofromid.sql
+++ dbmodel/utils/fct/gw_fct_getinfofromid.sql
@@ -208,25 +208,26 @@ BEGIN
 		IF (SELECT isarcdivide FROM cat_feature_node JOIN cat_feature USING (id) WHERE child_layer=v_tablename) IS FALSE THEN
 				v_isarcdivide = FALSE;
 		END IF;
-
-	ELSE  
+	ELSE
 		-- tablename is used as table parent.
 		v_table_parent = v_tablename;
-		IF v_table_parent='v_edit_node' THEN
-			EXECUTE 'SELECT nodetype_id FROM '||v_table_parent||' WHERE node_id = '||quote_literal(v_id)||';'
-			INTO v_nodetype;
-			IF (SELECT isarcdivide FROM cat_feature_node WHERE id=v_nodetype) IS FALSE THEN
-				v_isarcdivide = FALSE;
+		IF v_id IS NOT NULL THEN 
+			IF v_table_parent='v_edit_node' THEN
+				EXECUTE 'SELECT nodetype_id FROM '||v_table_parent||' WHERE node_id = '||quote_literal(v_id)||';'
+				INTO v_nodetype;
+				IF (SELECT isarcdivide FROM cat_feature_node WHERE id=v_nodetype) IS FALSE THEN
+					v_isarcdivide = FALSE;
+				END IF;
 			END IF;
-		END IF;
 
-		--check if is delimiter
-		IF upper(v_project_type) = 'WS' AND v_table_parent='v_edit_node' AND v_id IS NOT NULL THEN
-			
-			IF (SELECT upper(graf_delimiter) FROM cat_feature_node WHERE id=v_nodetype) IN ('DMA','PRESSZONE') THEN
-				v_isgrafdelimiter = TRUE;
-			ELSIF (SELECT upper(epa_type) FROM node WHERE node_id = v_id) IN ('PUMP', 'VALVE', 'SHORTPIPE') THEN
-				v_isepatoarc = TRUE;
+			--check if is delimiter
+			IF upper(v_project_type) = 'WS' AND v_table_parent='v_edit_node' AND v_id IS NOT NULL THEN
+				
+				IF (SELECT upper(graf_delimiter) FROM cat_feature_node WHERE id=v_nodetype) IN ('DMA','PRESSZONE') THEN
+					v_isgrafdelimiter = TRUE;
+				ELSIF (SELECT upper(epa_type) FROM node WHERE node_id = v_id) IN ('PUMP', 'VALVE', 'SHORTPIPE') THEN
+					v_isepatoarc = TRUE;
+				END IF;
 			END IF;
 		END IF;
 	END IF;
