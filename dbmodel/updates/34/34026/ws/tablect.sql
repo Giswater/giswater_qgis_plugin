79f335e81d8e64ee079d5f2b9c9b31dd24577a05
Barbara <bbrzepka@bgeo.es>
2021-01-15 10:07:55 +0100

    remove constraints related to unexisting tables
    (cherry picked from commit c9c243174abf9871ee5fd1bd79d1a1d0fc46cfd9)

---
+++ dbmodel/updates/34/34026/ws/tablect.sql
@@ -0,0 +1,16 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+
+-- 2021/01/15
+ALTER TABLE rtc_scada_node DROP CONSTRAINT rtc_scada_node_scada_id_fkey;
+
+ALTER TABLE rtc_scada_x_dma DROP CONSTRAINT rtc_scada_x_dma_scada_id_fkey;
+
+ALTER TABLE rtc_scada_x_sector DROP CONSTRAINT rtc_scada_x_sector_scada_id_fkey;
