4d98bb1495d2047b64085a05dd38d3652454cf21
Barbara <bbrzepka@bgeo.es>
2021-01-15 11:25:09 +0100

    move update to 34

--- dbmodel/updates/34/34026/utils/dml.sql
+++ dbmodel/updates/34/34026/utils/dml.sql
@@ -79,3 +79,4 @@ INSERT INTO inp_typevalue VALUES ('inp_pjoint_type', 'VNODE', 'VNODE') ON CONFLI
 
 INSERT INTO config_param_system VALUES ('admin_config_control_trigger', 'TRUE', 'Enable or disable trigger related to config tables. If true, is enabled', 'Config control trigger:', NULL, NULL, FALSE, NULL, 'utils', NULL, NULL, 'boolean') ON CONFLICT (parameter) DO NOTHING;
 
+UPDATE config_form_fields SET dv_parent_id = 'feature_id', dv_querytext = 'SELECT id, idval FROM (SELECT node_1 AS id, node_1 AS idval FROM arc UNION SELECT DISTINCT node_2 AS id, node_2 AS idval FROM arc)c WHERE id IS NOT NULL' WHERE formname = 'visit_singlevent' AND columnname = 'position_id';
