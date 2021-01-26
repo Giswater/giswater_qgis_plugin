235dbe7b12df6ab9742f3716a4e77aed115309de
Xavier Torret <xtorret@bgeo.es>
2021-01-26 22:46:38 +0100

    Rename python files from *_btn to *_button in order to harmonize className againts fileName

--- core/toolbars/buttons.py
+++ core/toolbars/buttons.py
@@ -7,51 +7,51 @@ or (at your option) any later version.
 # -*- coding: utf-8 -*-
 
 # Basic
-from .basic.info_btn import GwInfoButton
-from .basic.search_btn import GwSearchButton
-from .basic.selector_btn import GwSelectorButton
+from .basic.info_button import GwInfoButton
+from .basic.search_button import GwSearchButton
+from .basic.selector_button import GwSelectorButton
 
 # Om
-from .om.mincut_btn import GwMincutButton
-from .om.mincut_manager_btn import GwMincutManagerButton
+from .om.mincut_button import GwMincutButton
+from .om.mincut_manager_button import GwMincutManagerButton
 
-from .om.flow_trace_btn import GwFlowTraceButton
-from .om.flow_exit_btn import GwFlowExitButton
+from .om.flow_trace_button import GwFlowTraceButton
+from .om.flow_exit_button import GwFlowExitButton
 
-from .om.profile_btn import GwProfileButton
-from .om.visit_btn import GwVisitButton
-from .om.visit_manager_btn import GwVisitManagerButton
-from .om.date_selector_btn import GwDateSelectorButton
+from .om.profile_button import GwProfileButton
+from .om.visit_button import GwVisitButton
+from .om.visit_manager_button import GwVisitManagerButton
+from .om.date_selector_button import GwDateSelectorButton
 
 # Edit
 from .edit.point_add_btn import GwPointAddButton
-from .edit.arc_add_btn import GwArcAddButton
-from .edit.feature_replace_btn import GwFeatureReplaceButton
-from .edit.arc_divide_btn import GwArcDivideButton
-from .edit.arc_fusion_btn import GwArcFusionButton
-from .edit.nodetype_change_btn import GwNodeTypeChangeButton
-from .edit.connect_link_btn import GwConnectLinkButton
-from .edit.feature_end_btn import GwFeatureEndButton
-from .edit.feature_delete_btn import GwFeatureDeleteButton
-from .edit.dimensioning_btn import GwDimensioningButton
-from .edit.document_btn import GwDocumentButton
-from .edit.document_manager_btn import GwDocumentManagerButton
-from .edit.element_btn import GwElementButton
-from .edit.element_manager_btn import GwElementManagerButton
+from .edit.arc_add_button import GwArcAddButton
+from .edit.feature_replace_button import GwFeatureReplaceButton
+from .edit.arc_divide_button import GwArcDivideButton
+from .edit.arc_fusion_button import GwArcFusionButton
+from .edit.nodetype_change_butotn import GwNodeTypeChangeButton
+from .edit.connect_link_button import GwConnectLinkButton
+from .edit.feature_end_button import GwFeatureEndButton
+from .edit.feature_delete_button import GwFeatureDeleteButton
+from .edit.dimensioning_button import GwDimensioningButton
+from .edit.document_button import GwDocumentButton
+from .edit.document_manager_button import GwDocumentManagerButton
+from .edit.element_button import GwElementButton
+from .edit.element_manager_button import GwElementManagerButton
 
 # Cad
-from .cad.aux_circle_add import GwAuxCircleAddButton
-from .cad.aux_point_add import GwAuxPointAddButton
+from .cad.aux_circle_add_button import GwAuxCircleAddButton
+from .cad.aux_point_add_button import GwAuxPointAddButton
 
 # Epa
-from .epa.go2epa_btn import GwGo2EpaButton
-from .epa.go2epa_manager_btn import GwGo2EpaManagerButton
-from .epa.go2epa_selector_btn import GwGo2EpaSelectorButton
+from .epa.go2epa_button import GwGo2EpaButton
+from .epa.go2epa_manager_button import GwGo2EpaManagerButton
+from .epa.go2epa_selector_button import GwGo2EpaSelectorButton
 
 # Plan
-from .plan.psector_btn import GwPsectorButton
-from .plan.psector_manager_btn import GwPsectorManagerButton
-from .plan.price_manager_btn import GwPriceManagerButton
+from .plan.psector_button import GwPsectorButton
+from .plan.psector_manager_button import GwPsectorManagerButton
+from .plan.price_manager_button import GwPriceManagerButton
 
 # Utilities
 from .utilities.toolbox_btn import GwToolBoxButton
@@ -61,4 +61,4 @@ from .utilities.print_btn import GwPrintButton
 from .utilities.project_check_btn import GwProjectCheckButton
 
 # ToC
-from .toc.add_child_layer_btn import GwAddChildLayerButton
+from .toc.add_child_layer_button import GwAddChildLayerButton
