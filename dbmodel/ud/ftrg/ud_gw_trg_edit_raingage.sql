7fc44d6bc6eb1625e57a864be20d35d3d39e8a0c
Xavier Torret <xtorret@bgeo.es>
2021-01-30 16:45:10 +0100

    Add sequence for rg_id on raingage table and clean code on trg_raingage

--- dbmodel/ud/ftrg/ud_gw_trg_edit_raingage.sql
+++ dbmodel/ud/ftrg/ud_gw_trg_edit_raingage.sql
@@ -13,53 +13,45 @@ DECLARE
 
 BEGIN
 
-    EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
+	EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
 	
-    -- Control insertions ID
-    IF TG_OP = 'INSERT' THEN
-     		     
-	-- Exploitation
+	-- Control insertions ID
+	IF TG_OP = 'INSERT' THEN
+   		     
+		-- Exploitation
 		IF (NEW.expl_id IS NULL) THEN
 			NEW.expl_id := (SELECT "value" FROM config_param_user WHERE "parameter"='edit_exploitation_vdefault' AND "cur_user"="current_user"());
 			IF (NEW.expl_id IS NULL) THEN
 				NEW.expl_id := (SELECT expl_id FROM exploitation WHERE active IS TRUE AND ST_DWithin(NEW.the_geom, exploitation.the_geom,0.001) LIMIT 1);
 				IF (NEW.expl_id IS NULL) THEN
 					EXECUTE 'SELECT gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
-      		 		"data":{"message":"2012", "function":"1216","debug_msg":"'||NEW.rg_id||'"}}$$);';
+				"data":{"message":"2012", "function":"1216","debug_msg":"'||NEW.rg_id||'"}}$$);';
 				END IF;		
 			END IF;
 		END IF;	
 
-		
-		        -- FEATURE INSERT
+		-- FEATURE INSERT
 		INSERT INTO raingage (rg_id, form_type, intvl, scf, rgage_type, timser_id, fname, sta, units, the_geom, expl_id) 
 		VALUES (NEW.rg_id, NEW.form_type, NEW.intvl, NEW.scf, NEW.rgage_type, NEW.timser_id, NEW.fname, NEW.sta, NEW.units, NEW.the_geom, NEW.expl_id);
-		
 			
-        RETURN NEW;
+		RETURN NEW;
 
 
-    ELSIF TG_OP = 'UPDATE' THEN
-
-       
-       -- UPDATE values
-		
-			UPDATE raingage 
-			SET rg_id=NEW.rg_id, form_type=NEW.form_type, intvl=NEW.intvl, scf=NEW.scf, rgage_type=NEW.rgage_type, timser_id=NEW.timser_id, fname=NEW.fname, sta=NEW.sta, units=NEW.units, the_geom=NEW.the_geom, expl_id=NEW.expl_id
-			WHERE rg_id = OLD.rg_id;
-
-
-                
-        RETURN NEW;
+	ELSIF TG_OP = 'UPDATE' THEN
+	
+		-- UPDATE values
+		UPDATE raingage 
+		SET rg_id=NEW.rg_id, form_type=NEW.form_type, intvl=NEW.intvl, scf=NEW.scf, rgage_type=NEW.rgage_type, timser_id=NEW.timser_id, fname=NEW.fname, sta=NEW.sta, units=NEW.units, the_geom=NEW.the_geom, expl_id=NEW.expl_id
+		WHERE rg_id = OLD.rg_id;
+               
+		RETURN NEW;
     
-
-    ELSIF TG_OP = 'DELETE' THEN
-        DELETE FROM raingage WHERE rg_id = OLD.rg_id;
-
-        RETURN NULL;
+	ELSIF TG_OP = 'DELETE' THEN
+		DELETE FROM raingage WHERE rg_id = OLD.rg_id;
+		
+		RETURN NULL;
    
-    END IF;
-
+	END IF;
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
--- dbmodel/updates/35/35002/ud/ddl.sql
+++ dbmodel/updates/35/35002/ud/ddl.sql
@@ -20,3 +20,13 @@ ALTER TABLE cat_mat_grate ALTER COLUMN active SET DEFAULT TRUE;
 
 SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"cat_mat_gully", "column":"active", "dataType":"boolean", "isUtils":"False"}}$$);
 ALTER TABLE cat_mat_gully ALTER COLUMN active SET DEFAULT TRUE;
+
+--2020/01/30
+CREATE SEQUENCE raingage_rg_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+ALTER TABLE raingage ALTER COLUMN rg_id SET DEFAULT nextval('ud_sample.raingage_rg_id_seq'::regclass);
