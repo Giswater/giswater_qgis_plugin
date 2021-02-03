3b75ca7684f5f1cbb6e2771b29eb4ae149e20ed3
Edgar Fuste <efuste@bgeo.es>
2021-02-03 15:58:25 +0100

    Enhancement: Harmonize python files grouping by private and public functions

--- core/toolbars/om/flow_exit_button.py
+++ core/toolbars/om/flow_exit_button.py
@@ -40,27 +40,7 @@ class GwFlowExitButton(GwMaptool):
 
     def canvasReleaseEvent(self, event):
         """ With left click the digitizing is finished """
-        self.set_flow_exit(event)
-
-
-    def set_flow_exit(self, event):
-        if event.button() == Qt.LeftButton and self.current_layer:
-
-            # Execute SQL function
-            function_name = "gw_fct_grafanalytics_downstream"
-
-            elem_id = self.snapped_feat.attribute('node_id')
-            feature_id = f'"id":["{elem_id}"]'
-            body = tools_gw.create_body(feature=feature_id)
-            result = tools_gw.execute_procedure(function_name, body)
-            if not result or result['status'] == 'Failed':
-                return
-
-            # Refresh map canvas
-            self.refresh_map_canvas()
-
-            # Set action pan
-            self.set_action_pan()
+        self._set_flow_exit(event)
 
 
     def activate(self):
@@ -107,3 +87,25 @@ class GwFlowExitButton(GwMaptool):
 
     # endregion
 
+    # region private functions
+
+    def _set_flow_exit(self, event):
+        if event.button() == Qt.LeftButton and self.current_layer:
+
+            # Execute SQL function
+            function_name = "gw_fct_grafanalytics_downstream"
+
+            elem_id = self.snapped_feat.attribute('node_id')
+            feature_id = f'"id":["{elem_id}"]'
+            body = tools_gw.create_body(feature=feature_id)
+            result = tools_gw.execute_procedure(function_name, body)
+            if not result or result['status'] == 'Failed':
+                return
+
+            # Refresh map canvas
+            self.refresh_map_canvas()
+
+            # Set action pan
+            self.set_action_pan()
+
+     # endregion
