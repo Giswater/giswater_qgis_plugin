e0afed780c357032219634e6ef8f1293d0e7a723
David Erill <daviderill79@gmail.com>
2021-02-08 13:25:15 +0100

    Mincut: Add parameter 'usePsectors'

--- core/threads/auto_mincut_execute.py
+++ core/threads/auto_mincut_execute.py
@@ -5,7 +5,6 @@ General Public License as published by the Free Software Foundation, either vers
 or (at your option) any later version.
 """
 # -*- coding: utf-8 -*-
-
 from qgis.PyQt.QtCore import pyqtSignal
 from qgis.core import QgsTask
 
@@ -15,11 +14,11 @@ from ...lib import tools_db, tools_qt
 
 
 class GwAutoMincutTask(GwTask):
-    """ This shows how to subclass QgsTask """
 
     task_finished = pyqtSignal(list)
 
     def __init__(self, description, mincut_class, element_id):
+
         super().__init__(description, QgsTask.CanCancel)
         self.mincut_class = mincut_class
         self.element_id = element_id
@@ -28,12 +27,13 @@ class GwAutoMincutTask(GwTask):
 
     def run(self):
         """ Automatic mincut: Execute function 'gw_fct_mincut' """
+
         try:
-            real_mincut_id = tools_qt.get_text(self.mincut_class.dlg_mincut, self.mincut_class.dlg_mincut.result_mincut_id)
+
+            real_mincut_id = tools_qt.get_text(self.mincut_class.dlg_mincut, 'result_mincut_id')
             if self.mincut_class.is_new:
                 self.mincut_class.set_id_val()
                 self.mincut_class.is_new = False
-
                 sql = ("INSERT INTO om_mincut (mincut_state)"
                        " VALUES (0) RETURNING id;")
                 new_mincut_id = tools_db.execute_returning(sql)
@@ -45,19 +45,21 @@ class GwAutoMincutTask(GwTask):
                 else:
                     real_mincut_id = new_mincut_id[0]
 
-            tools_qt.set_widget_text(self.mincut_class.dlg_mincut, self.mincut_class.dlg_mincut.result_mincut_id, real_mincut_id)
-
-            extras = f'"action":"mincutNetwork", '
-            extras += f'"mincutId":"{real_mincut_id}", "arcId":"{self.element_id}"'
+            tools_qt.set_widget_text(self.mincut_class.dlg_mincut, 'result_mincut_id', real_mincut_id)
+            use_planified = tools_qt.is_checked(self.mincut_class.dlg_mincut, 'chk_use_planified')
+            extras = (f'"action":"mincutNetwork", "mincutId":"{real_mincut_id}", "arcId":"{self.element_id}", '
+                      f'"usePsectors":"{use_planified}"')
             body = tools_gw.create_body(extras=extras)
             self.complet_result = tools_gw.execute_procedure('gw_fct_setmincut', body)
             return True
+
         except KeyError as e:
             self.exception = e
             return False
 
 
     def finished(self, result):
+
         if self.exception is not None:
             msg = f"<b>Key: </b>{self.exception}<br>"
             msg += f"<b>key container: </b>'body/data/ <br>"
@@ -70,6 +72,3 @@ class GwAutoMincutTask(GwTask):
         elif 'mincutOverlap' in self.complet_result or self.complet_result['status'] == 'Accepted':
             self.task_finished.emit([True, self.complet_result])
 
-
-    def cancel(self):
-        super().cancel()
