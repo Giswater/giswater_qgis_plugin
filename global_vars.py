0859f40f9981f60218f396836ffb7765073f43e9
Edgar Fuste <efuste@bgeo.es>
2021-02-24 13:44:45 +0100

    Enhancement: structure and bug fix: recover schame_name variable

--- global_vars.py
+++ global_vars.py
@@ -12,73 +12,43 @@ from qgis.PyQt.QtCore import QSettings
 # static variables
 # values are initialized on load project without changes during session
 
-# An instance of interface that provides the hook by which you can manipulate the QGIS application at run time.
-# type; "QgsInterface"
-iface = None
-# An insance of QGIS canvas. Contains "canvas", "mapTool", "xyCoordinates", "Cursor", "Extent"
-canvas = None
-# An instance of plugin directory path
-plugin_dir = None
-# An instance of plugin name
-plugin_name = None
-# An instance of configurable user variables directory path
-user_folder_dir = None
-# An instance of schema name retrieved from qgis project connection with postgresql
-project_type = None
-# An instance of srid parameter retrieved from qgis project layer "v_edit_node"
-srid = None
-# An instance of GwLogger class thats found in "/lib/tools_log.py"
-logger = None
-# An instance of QGIS settings relating to Giswater variables such as toolbars and actions chekeables"
-giswater_settings = None
-# An instance of QGIS settings"
-qgis_settings = None
-# An instance of user credential used to establish the connection with PostgreSql
-current_user = None
-# An instance of QSqlDatabase (QPSQL) used to manage QTableView widgets
-qgis_db_credentials = None
-# An instance of GwPgDao class thats found in "/lib/tools_db.py"
-dao = None
-# An instance of credentials used to establish the connection with PostgreSql
-# Saving {db, schema, table, service, host, port, user, password, sslmode}
-dao_db_credentials = None
-# An instance of PostgreSql version of current connection
-pg_version = None
-# An instance of used shortcut_keys for Giswater menu
-# This keys are configurated on file "init.config" from user config path "/user/AppData/Roaming/Giswater/"
-shortcut_keys = []
+iface = None                            # An instance of interface that provides the hook by which you can manipulate the QGIS application at run time. Type "QgsInterface"
+canvas = None                           # An insance of QGIS canvas. Contains "canvas", "mapTool", "xyCoordinates", "Cursor", "Extent"
+plugin_dir = None                       # An instance of plugin directory path
+plugin_name = None                      # An instance of plugin name
+user_folder_dir = None                  # An instance of configurable user variables directory path
+schema_name = None                      # An instance of schema name retrieved from qgis project connection with PostgreSql
+project_type = None                     # An instance of project type getting from "version" table from PostgreSql
+srid = None                             # An instance of srid parameter retrieved from qgis project layer "v_edit_node"
+logger = None                           # An instance of GwLogger class thats found in "/lib/tools_log.py"
+giswater_settings = None                # An instance of QGIS settings relating to Giswater variables such as toolbars and actions chekeables"
+qgis_settings = None                    # An instance of QGIS settings"
+current_user = None                     # An instance of user credential used to establish the connection with PostgreSql
+qgis_db_credentials = None              # An instance of QSqlDatabase (QPSQL) used to manage QTableView widgets
+dao = None                              # An instance of GwPgDao class thats found in "/lib/tools_db.py"
+dao_db_credentials = None               # An instance of credentials used to establish the connection with PostgreSql. Saving {db, schema, table, service, host, port, user, password, sslmode}
+pg_version = None                       # An instance of PostgreSql version of current connection
+shortcut_keys = []                      # An instance of used shortcut_keys for Giswater menu. This keys are configurated on file "init.config" from user config path "/user/AppData/Roaming/Giswater/"
 
 # A instance of project variables from QgsProject relating to Giswater
 project_vars = {}
-# gwInfoType
-project_vars['info_type'] = None
-# gwAddSchema
-project_vars['add_schema'] = None
-# gwMainSchema
-project_vars['main_schema'] = None
-# gwProjectRole
-project_vars['project_role'] = None
-# gwProjectType
-project_vars['project_type'] = None
+project_vars['info_type'] = None        # gwInfoType
+project_vars['add_schema'] = None       # gwAddSchema
+project_vars['main_schema'] = None      # gwMainSchema
+project_vars['project_role'] = None     # gwProjectRole
+project_vars['project_type'] = None     # gwProjectType
 
 
-# session dynamic variables (may change value during user's session)
+# dynamic variables (session)
+# variables (may change value during user's session)
 session_vars = {}
-# An instance of the last database runtime error
-session_vars['last_error'] = None
-# An instance of the different threads for the execution of the Giswater functionalities (type:list)
-session_vars['threads'] = []
-# An instance of GwDocker from "/core/ui/docker.py" which is used to mount a docker form
-session_vars['dialog_docker'] = None
-# An instance of current status of the info docker form configured by user
-# Can be True or False
-session_vars['info_docker'] = None
-# An instance of current status of the docker form configured by user
-# Can be configured "qgis_info_docker" and "qgis_form_docker"
-session_vars['docker_type'] = None
-# An instance of connection status
-# Can be True or False
-session_vars['logged_status'] = None
+session_vars['last_error'] = None       # An instance of the last database runtime error
+session_vars['threads'] = []            # An instance of the different threads for the execution of the Giswater functionalities (type:list)
+session_vars['dialog_docker'] = None    # An instance of GwDocker from "/core/ui/docker.py" which is used to mount a docker form
+session_vars['info_docker'] = None      # An instance of current status of the info docker form configured by user. Can be True or False
+session_vars['docker_type'] = None      # An instance of current status of the docker form configured by user. Can be configured "qgis_info_docker" and "qgis_form_docker"
+session_vars['logged_status'] = None    # An instance of connection status. Can be True or False
+
 
 
 def init_global(p_iface, p_canvas, p_plugin_dir, p_plugin_name, p_user_folder_dir):
