070e2be1a032a75c354740de02bac3a45abb73fb
Xavier Torret <xtorret@bgeo.es>
2021-02-27 22:53:11 +0100

    Update curve_values notify
    (cherry picked from commit 6d390d1d76b12fc95d5380ced3be486c3a3c34dd)

--- dbmodel/updates/34/34033/ud/dml.sql
+++ dbmodel/updates/34/34033/ud/dml.sql
@@ -10,3 +10,5 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 -- 2021/02/27
 UPDATE config_form_tableview SET status = TRUE WHERE tablename  ='v_ui_rpt_cat_result' AND columnname = 'id';
+
+UPDATE sys_table SET notify_action = '[{"channel":"desktop","name":"refresh_attribute_table", "enabled":"true", "trg_fields":"id","featureType":["inp_flwreg_pump", "v_edit_inp_pump", "inp_flwreg_outlet", "v_edit_inp_outlet", "inp_curve","inp_curve_value", "v_edit_inp_divider","v_edit_inp_storage"]}]' WHERE id = 'inp_curve';
