aeca62b60dcf35d64ae980ab2849edf09c5c8fa1
Albert Bofill <abofill@bgeo.es>
2021-03-04 17:54:50 +0100

    Update v_edit_link

--- dbmodel/updates/34/34033/ud/ddlview.sql
+++ dbmodel/updates/34/34033/ud/ddlview.sql
@@ -33,3 +33,126 @@ CREATE OR REPLACE VIEW v_edit_inp_junction AS
    FROM selector_sector,  v_edit_node n
      JOIN inp_junction USING (node_id)
   WHERE n.sector_id = selector_sector.sector_id AND selector_sector.cur_user = "current_user"()::text;
+  
+  
+--2021/03/04
+CREATE OR REPLACE VIEW v_edit_link AS 
+ SELECT DISTINCT ON (a.link_id) a.link_id,
+    a.feature_type,
+    a.feature_id,
+    a.macrosector_id,
+    a.macrodma_id,
+    a.exit_type,
+    a.exit_id,
+    a.sector_id,
+    a.dma_id,
+    a.expl_id,
+    a.state,
+    a.gis_length,
+    a.userdefined_geom,
+    a.the_geom,
+    a.ispsectorgeom,
+    a.psector_rowid,
+    a.fluid_type,
+    a.vnode_topelev
+   FROM ( SELECT link.link_id,
+            link.feature_type,
+            link.feature_id,
+            sector.macrosector_id,
+            dma.macrodma_id,
+            link.exit_type,
+            link.exit_id,
+            link.vnode_topelev,
+            c.fluid_type,
+            arc.sector_id,
+            arc.dma_id,
+            arc.expl_id,
+            c.state,
+            st_length2d(link.the_geom) AS gis_length,
+            userdefined_geom,	
+            link.the_geom,
+            false as ispsectorgeom,
+            null::integer as psector_rowid
+        FROM link
+        RIGHT JOIN v_edit_connec c ON link.feature_id::text = c.connec_id::text
+        LEFT JOIN arc USING (arc_id)
+        LEFT JOIN sector ON sector.sector_id::text = arc.sector_id::text
+        LEFT JOIN dma ON dma.dma_id::text = arc.dma_id::text
+	UNION
+		SELECT link.link_id,
+		    link.feature_type,
+		    link.feature_id,
+		    sector.macrosector_id,
+		    dma.macrodma_id,
+		    link.exit_type,
+		    link.exit_id,
+		    link.vnode_topelev,
+		    c.fluid_type,
+		    arc.sector_id,
+		    arc.dma_id,
+		    arc.expl_id,
+		    plan_psector_x_connec.state,
+		    st_length2d(plan_psector_x_connec.link_geom) AS gis_length,
+		    plan_psector_x_connec.userdefined_geom,
+		    plan_psector_x_connec.link_geom,
+		    true,
+		    plan_psector_x_connec.id AS psector_rowid
+		FROM plan_psector_x_connec, v_edit_connec c
+		LEFT JOIN link ON link.feature_id::text = c.connec_id::text
+		LEFT JOIN arc USING (arc_id)
+		LEFT JOIN sector ON sector.sector_id::text = arc.sector_id::text
+		LEFT JOIN dma ON dma.dma_id::text = arc.dma_id::text
+		WHERE c.arc_id = plan_psector_x_connec.arc_id AND c.connec_id = plan_psector_x_connec.connec_id
+    UNION
+        SELECT link.link_id,
+            link.feature_type,
+            link.feature_id,
+            sector.macrosector_id,
+            dma.macrodma_id,
+            link.exit_type,
+            link.exit_id,
+            link.vnode_topelev,
+            g.fluid_type,
+            arc.sector_id,
+            arc.dma_id,
+            arc.expl_id,
+            g.state,
+            st_length2d(link.the_geom) AS gis_length,
+            userdefined_geom,	
+            link.the_geom,
+            false as ispsectorgeom,
+            null::integer as psector_rowid
+        FROM link
+        RIGHT JOIN v_edit_gully g ON link.feature_id::text = g.gully_id::text
+        LEFT JOIN arc USING (arc_id)
+        LEFT JOIN sector ON sector.sector_id::text = arc.sector_id::text
+        LEFT JOIN dma ON dma.dma_id::text = arc.dma_id::text
+	UNION
+		SELECT link.link_id,
+		    link.feature_type,
+		    link.feature_id,
+		    sector.macrosector_id,
+		    dma.macrodma_id,
+		    link.exit_type,
+		    link.exit_id,
+		    link.vnode_topelev,
+		    g.fluid_type,
+		    arc.sector_id,
+		    arc.dma_id,
+		    arc.expl_id,
+		    plan_psector_x_gully.state,
+		    st_length2d(plan_psector_x_gully.link_geom) AS gis_length,
+		    plan_psector_x_gully.userdefined_geom,
+		    plan_psector_x_gully.link_geom,
+		    true,
+		    plan_psector_x_gully.id AS psector_rowid
+		FROM plan_psector_x_gully, v_edit_gully g
+		LEFT JOIN link ON link.feature_id::text = g.gully_id::text
+		LEFT JOIN arc USING (arc_id)
+		LEFT JOIN sector ON sector.sector_id::text = arc.sector_id::text
+		LEFT JOIN dma ON dma.dma_id::text = arc.dma_id::text
+		WHERE g.arc_id = plan_psector_x_gully.arc_id AND g.gully_id = plan_psector_x_gully.gully_id) a
+  WHERE (a.state IN ( SELECT selector_state.state_id
+           FROM selector_state
+          WHERE selector_state.cur_user = "current_user"()::text)) AND a.link_id IS NOT NULL;
+
--- dbmodel/updates/34/34033/ws/ddlview.sql
+++ dbmodel/updates/34/34033/ws/ddlview.sql
@@ -10,9 +10,10 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 -- 2021/02/27
 
+DROP TRIGGER IF EXISTS gw_trg_vi_demands ON vi_demands;
 CREATE TRIGGER gw_trg_vi_demands
   INSTEAD OF INSERT OR UPDATE OR DELETE
-  ON SCHEMA_NAME.vi_demands
+  ON vi_demands
   FOR EACH ROW
   EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_vi('vi_demands');
 
@@ -35,3 +36,103 @@ CREATE OR REPLACE VIEW v_edit_inp_junction AS
      JOIN inp_junction USING (node_id)
   WHERE n.sector_id = selector_sector.sector_id AND selector_sector.cur_user = "current_user"()::text;
   
+
+
+CREATE OR REPLACE VIEW v_edit_inp_valve AS 
+SELECT v_node.node_id,
+            v_node.elevation,
+            v_node.depth,
+            v_node.nodecat_id,
+            v_node.sector_id,
+            v_node.macrosector_id,
+            v_node.state,
+            v_node.state_type,
+            v_node.annotation,
+            v_node.expl_id,
+            inp_valve.valv_type,
+            inp_valve.pressure,
+            inp_valve.flow,
+            inp_valve.coef_loss,
+            inp_valve.curve_id,
+            inp_valve.minorloss,
+            inp_valve.to_arc,
+            inp_valve.status,
+            v_node.the_geom,
+            inp_valve.custom_dint
+           FROM selector_sector, v_node
+             JOIN inp_valve USING (node_id)
+               WHERE v_node.sector_id = selector_sector.sector_id AND selector_sector.cur_user = "current_user"()::text;
+               
+
+
+CREATE OR REPLACE VIEW v_edit_link AS 
+ SELECT DISTINCT ON (a.link_id) a.link_id,
+    a.feature_type,
+    a.feature_id,
+    a.exit_type,
+    a.exit_id,
+    a.sector_id,
+    a.macrosector_id,
+    a.dma_id,
+    a.macrodma_id,
+    a.expl_id,
+    a.state,
+    a.gis_length,
+    a.userdefined_geom,
+    a.the_geom,
+    a.ispsectorgeom,
+    a.psector_rowid,
+    a.fluid_type,
+    a.vnode_topelev
+   FROM ( SELECT link.link_id,
+            link.feature_type,
+            link.feature_id,
+            link.exit_type,
+            link.exit_id,
+            arc.sector_id,
+            sector.macrosector_id,
+            arc.dma_id,
+            link.vnode_topelev,
+            dma.macrodma_id,
+            arc.expl_id,
+            v_edit_connec.state,
+            st_length2d(link.the_geom) AS gis_length,
+            userdefined_geom,
+            link.the_geom,
+            false as ispsectorgeom,
+            null::integer as psector_rowid,
+            arc.fluid_type
+           FROM v_edit_connec
+             LEFT JOIN link ON link.feature_id::text = v_edit_connec.connec_id::text
+             LEFT JOIN arc USING (arc_id)
+             LEFT JOIN sector ON sector.sector_id::text = arc.sector_id::text
+             LEFT JOIN dma ON dma.dma_id::text = arc.dma_id::text
+	UNION
+	    SELECT link.link_id,
+            link.feature_type,
+            link.feature_id,
+            link.exit_type,
+            link.exit_id,
+            arc.sector_id,
+            sector.macrosector_id,
+            arc.dma_id,
+            link.vnode_topelev,
+            dma.macrodma_id,
+            arc.expl_id,
+            plan_psector_x_connec.state,
+            st_length2d(plan_psector_x_connec.link_geom) AS gis_length,
+            plan_psector_x_connec.userdefined_geom,
+            plan_psector_x_connec.link_geom,
+            true,
+            plan_psector_x_connec.id AS psector_rowid,
+            arc.fluid_type
+           FROM plan_psector_x_connec, v_edit_connec
+             LEFT JOIN link ON link.feature_id::text = v_edit_connec.connec_id::text
+             LEFT JOIN arc USING (arc_id)
+             LEFT JOIN sector ON sector.sector_id::text = arc.sector_id::text
+             LEFT JOIN dma ON dma.dma_id::text = arc.dma_id::text
+             WHERE v_edit_connec.arc_id = plan_psector_x_connec.arc_id AND v_edit_connec.connec_id = plan_psector_x_connec.connec_id) a
+  WHERE (a.state IN ( SELECT selector_state.state_id
+           FROM selector_state
+          WHERE selector_state.cur_user = "current_user"()::text)) AND a.link_id IS NOT NULL;
+
