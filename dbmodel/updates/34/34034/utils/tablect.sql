01c2de24b59b96a7608f21ee944b2bb133846f5b
Albert Bofill <abofill@bgeo.es>
2021-03-17 17:54:52 +0100

    Drop deprecated constraint

---
+++ dbmodel/updates/34/34034/utils/tablect.sql
@@ -0,0 +1,12 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+
+-- 2021/03/17
+ALTER TABLE plan_psector DROP CONSTRAINT IF EXISTS plan_psector_sector_id_fkey;
