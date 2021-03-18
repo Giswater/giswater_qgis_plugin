5c153f180da927d3bc42d674db643170abd26aeb
Barbara Rzepka <bbrzepka@bgeo.es>
2021-03-18 10:04:37 +0100

    reorganize updates 35003

--- dbmodel/updates/35/35003/ud/ddl.sql
+++ dbmodel/updates/35/35003/ud/ddl.sql
@@ -9,66 +9,13 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 
 --2021/02/27
-UPDATE sys_feature_cat SET man_table = f.man_table FROM cat_feature_gully f WHERE sys_feature_cat.id=f.type;
-
 ALTER TABLE cat_feature_gully DROP COLUMN man_table;
 
 ALTER TABLE inp_arc_type ADD COLUMN epa_table character varying(30);
 ALTER TABLE inp_node_type ADD COLUMN epa_table character varying(30);
 
-UPDATE inp_arc_type SET epa_table = f.epa_table FROM cat_feature_arc f WHERE inp_arc_type.id=f.epa_default;
-UPDATE inp_arc_type SET epa_table = 'inp_outlet' where id = 'OUTLET';
-UPDATE inp_arc_type SET epa_table = 'inp_orifice' where id = 'ORIFICE';
-UPDATE inp_arc_type SET epa_table = 'inp_pump' where id = 'PUMP';
-UPDATE inp_arc_type SET epa_table = 'inp_virtual' where id = 'VIRTUAL';
-UPDATE inp_arc_type SET epa_table = 'inp_weir' where id = 'WEIR';
-
-UPDATE inp_node_type SET epa_table = f.epa_table FROM cat_feature_node f WHERE inp_node_type.id=f.epa_default;
-UPDATE inp_node_type SET epa_table = 'inp_divider' where id = 'DIVIDER';
-
 ALTER TABLE cat_feature_node DROP COLUMN epa_table;
 ALTER TABLE cat_feature_arc DROP COLUMN epa_table;
 
-INSERT INTO sys_feature_epa_type SELECT id, 'NODE', epa_table FROM inp_node_type;
-INSERT INTO sys_feature_epa_type SELECT id, 'ARC', epa_table FROM inp_arc_type;
-
-ALTER TABLE cat_feature_node DROP CONSTRAINT cat_feature_node_epa_default_fkey;
-ALTER TABLE cat_feature_arc DROP CONSTRAINT cat_feature_arc_epa_default_fkey;
-
-ALTER TABLE cat_feature_arc ADD CONSTRAINT cat_feature_arc_inp_check 
-CHECK (epa_default = ANY(ARRAY['CONDUIT', 'WEIR', 'ORIFICE', 'VIRTUAL', 'PUMP', 'OUTLET', 'NOT DEFINED', 'UNDEFINED']));
-ALTER TABLE cat_feature_node ADD CONSTRAINT cat_feature_node_inp_check 
-CHECK (epa_default = ANY(ARRAY['JUNCTION', 'STORAGE', 'DIVIDER', 'OUTFALL', 'NOT DEFINED', 'UNDEFINED']));
-
-ALTER TABLE arc ADD CONSTRAINT arc_epa_type_check 
-CHECK (epa_type = ANY(ARRAY['CONDUIT', 'WEIR', 'ORIFICE', 'VIRTUAL', 'PUMP', 'OUTLET', 'NOT DEFINED', 'UNDEFINED']));
-ALTER TABLE node ADD CONSTRAINT node_epa_type_check 
-CHECK (epa_type = ANY(ARRAY['JUNCTION', 'STORAGE', 'DIVIDER', 'OUTFALL', 'NOT DEFINED', 'UNDEFINED']));
-
-ALTER TABLE node DROP CONSTRAINT node_epa_type_fkey ;
-ALTER TABLE arc DROP CONSTRAINT arc_epa_type_fkey ;
-
 DROP TABLE IF EXISTS inp_node_type;
 DROP TABLE IF EXISTS inp_arc_type;
-
-UPDATE cat_feature_node SET epa_default  ='UNDEFINED' WHERE epa_default  ='NOT DEFINED';
-UPDATE cat_feature_arc SET epa_default  ='UNDEFINED' WHERE epa_default  ='NOT DEFINED';
-UPDATE sys_feature_epa_type SET id  ='UNDEFINED' WHERE id  ='NOT DEFINED';
-UPDATE arc SET epa_type ='UNDEFINED' WHERE epa_type  ='NOT DEFINED';
-UPDATE node SET epa_type ='UNDEFINED' WHERE epa_type  ='NOT DEFINED';
-
-ALTER TABLE cat_feature_arc DROP CONSTRAINT cat_feature_arc_inp_check;
-ALTER TABLE cat_feature_arc ADD CONSTRAINT cat_feature_arc_inp_check 
-CHECK (epa_default = ANY(ARRAY['CONDUIT', 'WEIR', 'ORIFICE', 'VIRTUAL', 'PUMP', 'OUTLET', 'UNDEFINED']));
-ALTER TABLE cat_feature_node DROP CONSTRAINT cat_feature_node_inp_check;
-ALTER TABLE cat_feature_node ADD CONSTRAINT cat_feature_node_inp_check 
-CHECK (epa_default = ANY(ARRAY['JUNCTION', 'STORAGE', 'DIVIDER', 'OUTFALL', 'UNDEFINED']));
-
-ALTER TABLE arc DROP CONSTRAINT arc_epa_type_check;
-ALTER TABLE arc ADD CONSTRAINT arc_epa_type_check 
-CHECK (epa_type = ANY(ARRAY['CONDUIT', 'WEIR', 'ORIFICE', 'VIRTUAL', 'PUMP', 'OUTLET', 'UNDEFINED']));
-ALTER TABLE node DROP CONSTRAINT node_epa_type_check;
-ALTER TABLE node ADD CONSTRAINT node_epa_type_check 
-CHECK (epa_type = ANY(ARRAY['JUNCTION', 'STORAGE', 'DIVIDER', 'OUTFALL', 'UNDEFINED']));
-
-
--- dbmodel/updates/35/35003/ws/ddl.sql
+++ dbmodel/updates/35/35003/ws/ddl.sql
@@ -12,60 +12,13 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 ALTER TABLE inp_arc_type ADD COLUMN epa_table character varying(30);
 ALTER TABLE inp_node_type ADD COLUMN epa_table character varying(30);
 
-UPDATE inp_arc_type SET epa_table = f.epa_table FROM cat_feature_arc f WHERE inp_arc_type.id=f.epa_default;
-UPDATE inp_arc_type SET epa_table = 'inp_pump_importinp' where id = 'PUMP-IMPORTINP';
-UPDATE inp_arc_type SET epa_table = 'inp_valve_importinp' where id = 'VALVE-IMPORTINP';
-UPDATE inp_arc_type SET epa_table = 'inp_virtualvalve' where id = 'VIRTUALVALVE';
-
-UPDATE inp_node_type SET epa_table = f.epa_table FROM cat_feature_node f WHERE inp_node_type.id=f.epa_default;
-UPDATE inp_node_type SET epa_table = 'inp_inlet' where id = 'INLET';
-
 ALTER TABLE cat_feature_node DROP COLUMN epa_table;
 ALTER TABLE cat_feature_arc DROP COLUMN epa_table;
-
-INSERT INTO sys_feature_epa_type SELECT id, 'NODE', epa_table FROM inp_node_type;
-INSERT INTO sys_feature_epa_type SELECT id, 'ARC', epa_table FROM inp_arc_type;
-
-ALTER TABLE cat_feature_node DROP CONSTRAINT cat_feature_node_epa_default_fkey;
-ALTER TABLE cat_feature_arc DROP CONSTRAINT cat_feature_arc_epa_default_fkey;
-
-ALTER TABLE cat_feature_arc ADD CONSTRAINT cat_feature_arc_inp_check 
-CHECK (epa_default = ANY(ARRAY['PIPE', 'NOT DEFINED', 'UNDEFINED', 'PUMP-IMPORTINP', 'VALVE-IMPORTINP', 'VIRTUALVALVE']));
-ALTER TABLE cat_feature_node ADD CONSTRAINT cat_feature_node_inp_check 
-CHECK (epa_default = ANY(ARRAY['JUNCTION', 'RESERVOIR', 'TANK', 'INLET', 'NOT DEFINED', 'UNDEFINED', 'SHORTPIPE', 'VALVE', 'PUMP']));
-
-ALTER TABLE arc ADD CONSTRAINT arc_epa_type_check 
-CHECK (epa_type = ANY(ARRAY['PIPE', 'NOT DEFINED', 'UNDEFINED', 'PUMP-IMPORTINP', 'VALVE-IMPORTINP', 'VIRTUALVALVE']));
-ALTER TABLE node ADD CONSTRAINT node_epa_type_check 
-CHECK (epa_type = ANY(ARRAY['JUNCTION', 'RESERVOIR', 'TANK', 'INLET', 'NOT DEFINED', 'UNDEFINED', 'SHORTPIPE', 'VALVE', 'PUMP']));
-
-ALTER TABLE node DROP CONSTRAINT node_epa_type_fkey ;
-ALTER TABLE arc DROP CONSTRAINT arc_epa_type_fkey ;
 
 DROP TABLE IF EXISTS inp_node_type;
 DROP TABLE IF EXISTS inp_arc_type;
 
 ALTER TABLE inp_rules_controls_importinp RENAME TO _inp_rules_controls_importinp_ ;
-
-UPDATE cat_feature_node SET epa_default  ='UNDEFINED' WHERE epa_default  ='NOT DEFINED';
-UPDATE cat_feature_arc SET epa_default  ='UNDEFINED' WHERE epa_default  ='NOT DEFINED';
-UPDATE sys_feature_epa_type SET id  ='UNDEFINED' WHERE id  ='NOT DEFINED';
-UPDATE arc SET epa_type ='UNDEFINED' WHERE epa_type  ='NOT DEFINED';
-UPDATE node SET epa_type ='UNDEFINED' WHERE epa_type  ='NOT DEFINED';
-
-ALTER TABLE cat_feature_arc DROP CONSTRAINT cat_feature_arc_inp_check;
-ALTER TABLE cat_feature_arc ADD CONSTRAINT cat_feature_arc_inp_check 
-CHECK (epa_default = ANY(ARRAY['PIPE', 'UNDEFINED', 'PUMP-IMPORTINP', 'VALVE-IMPORTINP', 'VIRTUALVALVE']));
-ALTER TABLE cat_feature_node DROP CONSTRAINT cat_feature_node_inp_check;
-ALTER TABLE cat_feature_node ADD CONSTRAINT cat_feature_node_inp_check 
-CHECK (epa_default = ANY(ARRAY['JUNCTION', 'RESERVOIR', 'TANK', 'INLET', 'UNDEFINED', 'SHORTPIPE', 'VALVE', 'PUMP']));
-
-ALTER TABLE arc DROP CONSTRAINT arc_epa_type_check;
-ALTER TABLE arc ADD CONSTRAINT arc_epa_type_check 
-CHECK (epa_type = ANY(ARRAY['PIPE', 'UNDEFINED', 'PUMP-IMPORTINP', 'VALVE-IMPORTINP', 'VIRTUALVALVE']));
-ALTER TABLE node DROP CONSTRAINT node_epa_type_check;
-ALTER TABLE node ADD CONSTRAINT node_epa_type_check 
-CHECK (epa_type = ANY(ARRAY['JUNCTION', 'RESERVOIR', 'TANK', 'INLET', 'UNDEFINED', 'SHORTPIPE', 'VALVE', 'PUMP']));
 
 
 
