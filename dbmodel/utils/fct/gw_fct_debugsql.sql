98c280bc1f665fb8780ca6b3345b296460bbecb7
Edgar Fuste <efuste@bgeo.es>
2021-03-24 10:53:49 +0100

    Enhancement: rename line for flag

--- dbmodel/utils/fct/gw_fct_debugsql.sql
+++ dbmodel/utils/fct/gw_fct_debugsql.sql
@@ -12,7 +12,7 @@ CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_debugsql(p_data json)
 $BODY$
 /*EXAMPLE:
 SELECT gw_fct_debugsql({"querystring" : "SELECT a.attname FROM pg_index i JOIN pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey) WHERE  i.indrelid = 've_arc_pipe'::regclass AND i.indisprimary",
-					"vars" : {"v_tablename" : "ve_arc_pipe", "null_example" : null}, "funcname" : "gw_fct_getlist", "line" : 222});
+					"vars" : {"v_tablename" : "ve_arc_pipe", "null_example" : null}, "funcname" : "gw_fct_getlist", "flag" : 10});
 */
 
 DECLARE
@@ -22,7 +22,7 @@ v_level integer = 1;
 v_message text;
 v_querystring text;
 v_funcname text;
-v_line int;
+v_flag int;
 v_vars json;
 v_records json;
 v_key text;
@@ -39,7 +39,7 @@ BEGIN
 	
 	v_querystring := (p_data ->> 'querystring');
 	v_funcname := (p_data ->> 'funcname');
-	v_line := (p_data ->> 'line');
+	v_flag := (p_data ->> 'flag');
 	v_vars :=(p_data ->> 'vars')::json;
 	v_msgerr := concat('The next query ''', v_querystring, ''' was executed with NULL values: ');
 	FOR v_key, v_value IN SELECT  var.key, var.value FROM  json_each_text(v_vars) var LOOP
@@ -50,7 +50,7 @@ BEGIN
 	END LOOP;
 
 	IF v_error IS True THEN
-		v_msgerr := concat(v_msgerr, 'in function ''', v_funcname, ''' in line ', v_line);
+		v_msgerr := concat(v_msgerr, 'in function ''', v_funcname, ''' in flag ', v_flag);
 		RETURN ('{"status":"Accepted", "MSGERR":'|| to_json(v_msgerr) ||' }')::json;
 	END IF;
 	-- Return
