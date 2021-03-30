54f5d68bb89a4987ae647675a0daf895afd4df75
Barbara Rzepka <bbrzepka@bgeo.es>
2021-03-30 10:22:36 +0200

    set correct columnindex on config_form_tableview
    (cherry picked from commit 3931b1d07e5371e8c1b1c1ebbee837b66f7e08ea)

--- dbmodel/updates/34/34034/utils/dml.sql
+++ dbmodel/updates/34/34034/utils/dml.sql
@@ -12,3 +12,16 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 INSERT INTO sys_table VALUES ('v_plan_psector_budget_arc', 'View to show budget of every arc related to a psector', 'role_basic', 0) ON CONFLICT (id) DO NOTHING;
 INSERT INTO sys_table VALUES ('v_plan_psector_budget_node', 'View to show budget of every node related to a psector', 'role_basic', 0) ON CONFLICT (id) DO NOTHING;
 INSERT INTO sys_table VALUES ('v_plan_psector_budget_other', 'View to show budget of other prices related to a psector', 'role_basic', 0) ON CONFLICT (id) DO NOTHING;
+
+--2021/03/30
+UPDATE config_form_tableview SET columnindex = 1 WHERE columnname = 'rid' AND tablename ='v_ui_arc_x_relations';
+UPDATE config_form_tableview SET columnindex = 2 WHERE columnname = 'arc_id' AND tablename ='v_ui_arc_x_relations';
+UPDATE config_form_tableview SET columnindex = 3 WHERE columnname = 'featurecat_id' AND tablename ='v_ui_arc_x_relations';
+UPDATE config_form_tableview SET columnindex = 4 WHERE columnname = 'catalog' AND tablename ='v_ui_arc_x_relations';
+UPDATE config_form_tableview SET columnindex = 5 WHERE columnname = 'feature_id' AND tablename ='v_ui_arc_x_relations';
+UPDATE config_form_tableview SET columnindex = 6 WHERE columnname = 'feature_code' AND tablename ='v_ui_arc_x_relations';
+UPDATE config_form_tableview SET columnindex = 7 WHERE columnname = 'sys_type' AND tablename ='v_ui_arc_x_relations';
+UPDATE config_form_tableview SET columnindex = 8 WHERE columnname = 'arc_state' AND tablename ='v_ui_arc_x_relations';
+UPDATE config_form_tableview SET columnindex = 9 WHERE columnname = 'feature_state' AND tablename ='v_ui_arc_x_relations';
+UPDATE config_form_tableview SET columnindex = 10 WHERE columnname = 'x' AND tablename ='v_ui_arc_x_relations';
+UPDATE config_form_tableview SET columnindex = 11 WHERE columnname = 'y' AND tablename ='v_ui_arc_x_relations';
