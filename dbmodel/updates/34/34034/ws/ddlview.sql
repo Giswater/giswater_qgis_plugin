adbff630f491881c5b53e4af0e372ad472120cee
xtorret@bgeo.es <xtorret@bgeo.es>
2021-04-01 23:09:21 +0200

    Enhance vi_valves managing TCV valves as shorpipes
    (cherry picked from commit 0c6beea7201f47b64e82bc0f9847bd2f9234320d)

--- dbmodel/updates/34/34034/ws/ddlview.sql
+++ dbmodel/updates/34/34034/ws/ddlview.sql
@@ -22,3 +22,113 @@ CREATE OR REPLACE VIEW vi_tanks AS
   WHERE rpt_inp_node.result_id::text = selector_inp_result.result_id::text 
   AND rpt_inp_node.epa_type::text = 'TANK'::text AND selector_inp_result.cur_user = "current_user"()::text;
 
+
+
+-- 2021/04/01
+CREATE OR REPLACE VIEW inpvi_valves AS 
+ SELECT DISTINCT ON (a.arc_id) a.arc_id,
+    a.node_1,
+    a.node_2,
+    a.diameter,
+    a.valv_type,
+    a.setting,
+    a.minorloss
+   FROM ( SELECT rpt_inp_arc.arc_id::text AS arc_id,
+            rpt_inp_arc.node_1,
+            rpt_inp_arc.node_2,
+            rpt_inp_arc.diameter,
+            ((rpt_inp_arc.addparam::json ->> 'valv_type'::text))::character varying(18) AS valv_type,
+            rpt_inp_arc.addparam::json ->> 'pressure'::text AS setting,
+            rpt_inp_arc.minorloss
+           FROM inpselector_inp_result,
+            inprpt_inp_arc
+          WHERE ((rpt_inp_arc.addparam::json ->> 'valv_type'::text) = 'PRV'::text OR (rpt_inp_arc.addparam::json ->> 'valv_type'::text) = 'PSV'::text OR (rpt_inp_arc.addparam::json ->> 'valv_type'::text) = 'PBV'::text) AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text
+        UNION
+         SELECT rpt_inp_arc.arc_id,
+            rpt_inp_arc.node_1,
+            rpt_inp_arc.node_2,
+            rpt_inp_arc.diameter,
+            rpt_inp_arc.addparam::json ->> 'valv_type'::text AS valv_type,
+            rpt_inp_arc.addparam::json ->> 'flow'::text AS setting,
+            rpt_inp_arc.minorloss
+           FROM inpselector_inp_result,
+            inprpt_inp_arc
+          WHERE (rpt_inp_arc.addparam::json ->> 'valv_type'::text) = 'FCV'::text AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text
+        UNION
+         SELECT rpt_inp_arc.arc_id,
+            rpt_inp_arc.node_1,
+            rpt_inp_arc.node_2,
+            rpt_inp_arc.diameter,
+            rpt_inp_arc.addparam::json ->> 'valv_type'::text AS valv_type,
+            rpt_inp_arc.addparam::json ->> 'coef_loss'::text AS setting,
+            rpt_inp_arc.minorloss
+           FROM inpselector_inp_result,
+            inprpt_inp_arc
+          WHERE (rpt_inp_arc.addparam::json ->> 'valv_type'::text) = 'TCV'::text AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text
+        UNION
+         SELECT rpt_inp_arc.arc_id,
+            rpt_inp_arc.node_1,
+            rpt_inp_arc.node_2,
+            rpt_inp_arc.diameter,
+            rpt_inp_arc.addparam::json ->> 'valv_type'::text AS valv_type,
+            rpt_inp_arc.addparam::json ->> 'curve_id'::text AS setting,
+            rpt_inp_arc.minorloss
+           FROM inpselector_inp_result,
+            inprpt_inp_arc
+          WHERE (rpt_inp_arc.addparam::json ->> 'valv_type'::text) = 'GPV'::text AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text
+        UNION
+         SELECT rpt_inp_arc.arc_id,
+            rpt_inp_arc.node_1,
+            rpt_inp_arc.node_2,
+            rpt_inp_arc.diameter,
+            'PRV'::character varying(18) AS valv_type,
+            rpt_inp_arc.addparam::json ->> 'pressure'::text AS setting,
+            rpt_inp_arc.minorloss
+           FROM inpselector_inp_result,
+            inprpt_inp_arc
+             JOIN inpinp_pump ON rpt_inp_arc.arc_id::text = concat(inp_pump.node_id, '_n2a_4')
+          WHERE rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text
+        UNION
+         SELECT rpt_inp_arc.arc_id,
+            rpt_inp_arc.node_1,
+            rpt_inp_arc.node_2,
+            rpt_inp_arc.diameter,
+            rpt_inp_arc.addparam::json ->> 'valv_type'::text AS valv_type,
+            rpt_inp_arc.addparam::json ->> 'pressure'::text AS setting,
+            rpt_inp_arc.minorloss
+           FROM inpselector_inp_result,
+            inprpt_inp_arc
+          WHERE ((rpt_inp_arc.addparam::json ->> 'valv_type'::text) = 'PRV'::text OR (rpt_inp_arc.addparam::json ->> 'valv_type'::text) = 'PSV'::text OR (rpt_inp_arc.addparam::json ->> 'valv_type'::text) = 'PBV'::text) AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text
+        UNION
+         SELECT rpt_inp_arc.arc_id,
+            rpt_inp_arc.node_1,
+            rpt_inp_arc.node_2,
+            rpt_inp_arc.diameter,
+            rpt_inp_arc.addparam::json ->> 'valv_type'::text AS valv_type,
+            rpt_inp_arc.addparam::json ->> 'flow'::text AS setting,
+            rpt_inp_arc.minorloss
+           FROM inpselector_inp_result,
+            inprpt_inp_arc
+          WHERE (rpt_inp_arc.addparam::json ->> 'valv_type'::text) = 'FCV'::text AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text
+        UNION
+         SELECT rpt_inp_arc.arc_id,
+            rpt_inp_arc.node_1,
+            rpt_inp_arc.node_2,
+            rpt_inp_arc.diameter,
+            rpt_inp_arc.addparam::json ->> 'valv_type'::text AS valv_type,
+            rpt_inp_arc.addparam::json ->> 'coef_loss'::text AS setting,
+            rpt_inp_arc.minorloss
+           FROM inpselector_inp_result,
+            inprpt_inp_arc
+          WHERE (rpt_inp_arc.addparam::json ->> 'valv_type'::text) = 'TCV'::text AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text
+        UNION
+         SELECT rpt_inp_arc.arc_id,
+            rpt_inp_arc.node_1,
+            rpt_inp_arc.node_2,
+            rpt_inp_arc.diameter,
+            rpt_inp_arc.addparam::json ->> 'valv_type'::text AS valv_type,
+            rpt_inp_arc.addparam::json ->> 'curve_id'::text AS setting,
+            rpt_inp_arc.minorloss
+           FROM inpselector_inp_result,
+            inprpt_inp_arc
+          WHERE (rpt_inp_arc.addparam::json ->> 'valv_type'::text) = 'GPV'::text AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text) a;
