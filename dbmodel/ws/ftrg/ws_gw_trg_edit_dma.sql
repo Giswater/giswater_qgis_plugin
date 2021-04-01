178309bf4798013e6405889f6097e83708218836
xtorret@bgeo.es <xtorret@bgeo.es>
2021-04-01 23:21:33 +0200

    Enhance trigger of dma to add pattern_id on the fly when insert is done
    (cherry picked from commit 99f2e65b81e3e8ba9ad5c45f1ae6928ca64e1ef5)

--- dbmodel/ws/ftrg/ws_gw_trg_edit_dma.sql
+++ dbmodel/ws/ftrg/ws_gw_trg_edit_dma.sql
@@ -10,52 +10,26 @@ CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_edit_dma()  RETURNS trigger AS
 $BODY$
 
 DECLARE 
-	expl_id_int integer;
+expl_id_int integer;
 
 BEGIN
 
-    EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
+	EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
 	
-    IF TG_OP = 'INSERT' THEN
-		/*
-	     -- Sector ID
-        IF (NEW.sector_id IS NULL) THEN
-            IF ((SELECT COUNT(*) FROM sector) = 0) THEN  
-                --PERFORM  gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
-       			 -- "data":{"message":"1008", "function":"1320","debug_msg":null, "variables":null}}$$); 
-            END IF;
-            NEW.sector_id:= (SELECT sector_id FROM sector WHERE ST_DWithin(NEW.the_geom, sector.the_geom,0.001) LIMIT 1);
-            IF (NEW.sector_id IS NULL) THEN
-                --PERFORM  gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
-       			 -- "data":{"message":"1010", "function":"1320","debug_msg":null, "variables":null}}$$);    
-            END IF;            
-        END IF;
-		*/
-		
+	IF TG_OP = 'INSERT' THEN
+	
 		--Exploitation ID
-        IF ((SELECT COUNT(*) FROM exploitation WHERE active IS TRUE) = 0) THEN
-            --PERFORM gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
-       			 -- "data":{"message":"1012", "function":"1112","debug_msg":null, "variables":null}}$$); 
+		IF ((SELECT COUNT(*) FROM exploitation WHERE active IS TRUE) = 0) THEN
 			RETURN NULL;				
-            END IF;
-            expl_id_int := (SELECT expl_id FROM exploitation WHERE active IS TRUE AND ST_DWithin(NEW.the_geom, exploitation.the_geom,0.001) LIMIT 1);
-            IF (expl_id_int IS NULL) THEN
-                --PERFORM gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
-       			 -- "data":{"message":"1014", "function":"1112","debug_msg":null, "variables":null}}$$); 
-				RETURN NULL; 
-            END IF;
-			
-		-- Municipality 
-		/*
-		IF (NEW.muni_id IS NULL) THEN
-			NEW.muni_id := (SELECT "value" FROM config_param_user WHERE "parameter"='edit_municipality_vdefault' AND "cur_user"="current_user"());
-			IF (NEW.muni_id IS NULL) THEN
-				NEW.muni_id := (SELECT muni_id FROM ext_municipality WHERE ST_DWithin(NEW.the_geom, ext_municipality.the_geom,0.001) LIMIT 1);
-					--PERFORM gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
-       			 -- "data":{"message":"2024", "function":"1112","debug_msg":null, "variables":null}}$$); 
-			END IF;
 		END IF;
-		*/
+
+		expl_id_int := (SELECT expl_id FROM exploitation WHERE active IS TRUE AND ST_DWithin(NEW.the_geom, exploitation.the_geom,0.001) LIMIT 1);
+		
+		IF (SELECT value::json->>'ceateNewPatternWhenNewDma' FROM config_param_system WHERE parameter = 'epa_patterns')::boolean = true THEN
+			NEW.pattern_id = NEW.dma_id;
+			INSERT INTO inp_pattern values (NEW.pattern_id)
+			ON CONFLICT (pattern_id) DO NOTHING;
+		END IF;
 			
 		INSERT INTO dma (dma_id, name, descript,  the_geom, undelete,  expl_id, pattern_id, link, minc, maxc, effc, grafconfig, stylesheet)
 		VALUES (NEW.dma_id, NEW.name, NEW.descript, NEW.the_geom, NEW.undelete, expl_id_int, NEW.pattern_id, NEW.link, NEW.minc, 
@@ -63,7 +37,7 @@ BEGIN
 
 		RETURN NEW;
 		
-    ELSIF TG_OP = 'UPDATE' THEN
+	ELSIF TG_OP = 'UPDATE' THEN
    	
 		UPDATE dma 
 		SET dma_id=NEW.dma_id, name=NEW.name, descript=NEW.descript, the_geom=NEW.the_geom, undelete=NEW.undelete, expl_id=NEW.expl_id, 
@@ -72,7 +46,7 @@ BEGIN
 		
 		RETURN NEW;
 		
-    ELSIF TG_OP = 'DELETE' THEN  
+	ELSIF TG_OP = 'DELETE' THEN  
 	 
 		DELETE FROM dma WHERE dma_id = OLD.dma_id;		
 		RETURN NULL;
