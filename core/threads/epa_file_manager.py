3238e8d93c26f45bb4ff1ef19711425369fa66c8
Edgar Fuste <efuste@bgeo.es>
2021-04-19 11:19:06 +0200

    Minor bug fix: clean connexion parameter on execute_procedure call

--- core/threads/epa_file_manager.py
+++ core/threads/epa_file_manager.py
@@ -169,7 +169,7 @@ class GwEpaFileManager(GwTask):
         extras += f', "useNetworkGeom":"{self.net_geom}"'
         extras += f', "dumpSubcatch":"{self.export_subcatch}"'
         body = self._create_body(extras=extras)
-        json_result = tools_gw.execute_procedure('gw_fct_pg2epa_main', body, self.conn)
+        json_result = tools_gw.execute_procedure('gw_fct_pg2epa_main', body)
         self.complet_result = json_result
         if json_result is None or not json_result:
             self.function_failed = True
@@ -416,7 +416,7 @@ class GwEpaFileManager(GwTask):
         if self.json_rpt:
             extras += f', "file": {self.json_rpt}'
         body = self._create_body(extras=extras)
-        json_result = tools_gw.execute_procedure('gw_fct_rpt2pg_main', body, self.conn)
+        json_result = tools_gw.execute_procedure('gw_fct_rpt2pg_main', body)
         self.rpt_result = json_result
         if json_result is None or not json_result:
             self.function_failed = True
