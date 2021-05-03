94b8c93a8b500b690efd835491cf0f2ae5f2db3e
xtorret@bgeo.es <xtorret@bgeo.es>
2021-05-03 14:00:01 +0200

    Enhance export inp file function

--- dbmodel/ws/fct/ws_gw_fct_pg2epa_export_inp.sql
+++ dbmodel/ws/fct/ws_gw_fct_pg2epa_export_inp.sql
@@ -146,7 +146,7 @@ BEGIN
 		union
 			select id, concat(rpad(csv1,22), ' ', csv2)as text from temp_csv where fid  = 141 and cur_user = current_user and source in ('header')
 		union
-			select id, csv1 as text from temp_csv where fid  = 141 and cur_user = current_user and source in ('vi_controls','vi_rules', 'vi_backdrop')
+			select id, csv1 as text from temp_csv where fid  = 141 and cur_user = current_user and source in ('vi_controls','vi_rules', 'vi_backdrop', 'vi_reactions')
 		union
 			-- spacer-19 it's used because a rare bug reading epanet when spacer=20 on target [PATTERNS]????
 			select id, concat(rpad(csv1,19),' ',rpad(coalesce(csv2,''),19),' ', rpad(coalesce(csv3,''),19),' ',rpad(coalesce(csv4,''),19),' ',rpad(coalesce(csv5,''),19),
@@ -159,7 +159,7 @@ BEGIN
 			' ',rpad(coalesce(csv6,''),20),	' ',rpad(coalesce(csv7,''),20),' ',rpad(coalesce(csv8,''),20),' ',rpad(coalesce(csv9,''),20),' ',rpad(coalesce(csv10,''),20),
 			' ',rpad(coalesce(csv11,''),20),' ',rpad(coalesce(csv12,''),20),' ',rpad(csv13,20),' ',rpad(csv14,20),' ',rpad(csv15,20),' ', rpad(csv15,20),' ',
 			rpad(csv16,20),	' ',rpad(csv17,20),' ', rpad(csv20,20), ' ', rpad(csv19,20),' ',rpad(csv20,20)) as text
-			from temp_csv where source not in ('header','vi_controls','vi_rules', 'vi_backdrop','vi_patterns')
+			from temp_csv where source not in ('header','vi_controls','vi_rules', 'vi_backdrop','vi_patterns', 'vi_reactions')
 		order by id)a )row;
 	
 	RETURN v_return;
