a2790c1b50606e689ac5c8e12cd8a1696444dc49
Edgar Fuste <efuste@bgeo.es>
2021-05-07 16:53:04 +0200

    Enhancement: manage mandatory fields on replace feature form (workcat id, new feature type and catalog id) and populate combo catalog_id with feature_type filter if was configured on database

--- core/toolbars/edit/feature_replace_button.py
+++ core/toolbars/edit/feature_replace_button.py
@@ -194,14 +194,18 @@ class GwFeatureReplaceButton(GwMaptool):
             feature_type = feature.attribute(self.feature_type_ws)
         elif self.project_type == 'ud':
             feature_type = feature.attribute(self.feature_type_ud)
-            if self.feature_type in ('node', 'connec'):
-                sql = f"SELECT DISTINCT(id) FROM {self.cat_table} ORDER BY id"
-                rows = tools_db.get_rows(sql)
-                tools_qt.fill_combo_box(self.dlg_replace, "featurecat_id", rows, allow_nulls=False)
-            elif self.feature_type in 'gully':
-                sql = f"SELECT DISTINCT(id) FROM cat_grate ORDER BY id"
-                rows = tools_db.get_rows(sql)
-                tools_qt.fill_combo_box(self.dlg_replace, "featurecat_id", rows, allow_nulls=False)
+            feature_type_new = tools_qt.get_text(self.dlg_replace, "feature_type_new")
+            if feature_type_new:
+                if self.feature_type in ('node', 'connec'):
+                    sql = f"SELECT DISTINCT(id) FROM {self.cat_table} " \
+                          f"WHERE {self.feature_type}_type = '{feature_type_new}' or {self.feature_type}_type IS NULL ORDER BY id"
+                    rows = tools_db.get_rows(sql)
+                    tools_qt.fill_combo_box(self.dlg_replace, "featurecat_id", rows, allow_nulls=False)
+                elif self.feature_type in 'gully':
+                    sql = f"SELECT DISTINCT(id) FROM cat_grate " \
+                          f"WHERE gully_type = '{feature_type_new}' OR gully_type IS NULL ORDER BY id"
+                    rows = tools_db.get_rows(sql)
+                    tools_qt.fill_combo_box(self.dlg_replace, "featurecat_id", rows, allow_nulls=False)
 
         self.dlg_replace.feature_type.setText(feature_type)
         self.dlg_replace.feature_type_new.currentIndexChanged.connect(self._edit_change_elem_type_get_value)
@@ -341,14 +345,14 @@ class GwFeatureReplaceButton(GwMaptool):
         self.workcat_id_end_aux = tools_qt.get_text(dialog, dialog.workcat_id_end)
         self.enddate_aux = dialog.enddate.date().toString('yyyy-MM-dd')
 
-        # Check null values
-        if self.workcat_id_end_aux in (None, 'null'):
-            message = "Mandatory field is missing. Please, set a value"
-            tools_qgis.show_warning(message, parameter='Workcat_id')
-            return
-
         feature_type_new = tools_qt.get_text(dialog, dialog.feature_type_new)
         featurecat_id = tools_qt.get_text(dialog, dialog.featurecat_id)
+
+        # Check null values
+        if self.workcat_id_end_aux in (None, 'null') or feature_type_new in (None, 'null') or featurecat_id in (None, 'null'):
+            message = "Mandatory fields are missing. Please, set values"
+            tools_qgis.show_warning(message, parameter='Workcat id, New feature type and Catalog id')
+            return
 
         # Ask question before executing
         message = "Are you sure you want to replace selected feature with a new one?"
@@ -445,5 +449,17 @@ class GwFeatureReplaceButton(GwMaptool):
                    f"WHERE {self.feature_type_ws} = '{feature_type_new}'")
             rows = tools_db.get_rows(sql)
             tools_qt.fill_combo_box(self.dlg_replace, self.dlg_replace.featurecat_id, rows)
+        elif self.project_type == 'ud':
+            self.dlg_replace.featurecat_id.clear()
+            if self.feature_type in ('node', 'connec'):
+                sql = f"SELECT DISTINCT(id) FROM {self.cat_table} " \
+                      f"WHERE {self.feature_type}_type = '{feature_type_new}' or {self.feature_type}_type IS NULL ORDER BY id"
+                rows = tools_db.get_rows(sql)
+                tools_qt.fill_combo_box(self.dlg_replace, "featurecat_id", rows, allow_nulls=False)
+            elif self.feature_type in 'gully':
+                sql = f"SELECT DISTINCT(id) FROM cat_grate " \
+                      f"WHERE gully_type = '{feature_type_new}' OR gully_type IS NULL ORDER BY id"
+                rows = tools_db.get_rows(sql)
+                tools_qt.fill_combo_box(self.dlg_replace, "featurecat_id", rows, allow_nulls=False)
 
     # endregion
