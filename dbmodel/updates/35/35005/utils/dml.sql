f0c1e0e8396157bf3a4ac0336715e9805de9444e
Barbara Rzepka <bbrzepka@bgeo.es>
2021-05-12 11:48:08 +0200

    update man_type tables if they are badly configured

--- dbmodel/updates/35/35005/utils/dml.sql
+++ dbmodel/updates/35/35005/utils/dml.sql
@@ -13,3 +13,9 @@ INSERT INTO config_param_system(parameter, value, descript, isenabled, project_t
 VALUES ('edit_arc_divide', '{"setArcObsolete":"false","setOldCode":"false"}', 
 'Configuration of arc divide tool. If setArcObsolete true state of old arc would be set to 0, otherwise arc will be deleted. If setOldCode true, new arcs will have same code as old arc.',
 FALSE, 'utils', 'json') ON CONFLICT (parameter) DO NOTHING;
+
+--2021/05/11
+UPDATE man_type_location set featurecat_id=NULL WHERE featurecat_id in ('{NODE}','{CONNEC}','{ARC}','{GULLY}');
+UPDATE man_type_category set featurecat_id=NULL WHERE featurecat_id in ('{NODE}','{CONNEC}','{ARC}','{GULLY}');
+UPDATE man_type_function set featurecat_id=NULL WHERE featurecat_id in ('{NODE}','{CONNEC}','{ARC}','{GULLY}');
+UPDATE man_type_fluid set featurecat_id=NULL WHERE featurecat_id in ('{NODE}','{CONNEC}','{ARC}','{GULLY}');
