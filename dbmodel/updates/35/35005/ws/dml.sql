02d1ab0329a7dcbedf5ca50350d41124e5bdc55c
Barbara Rzepka <bbrzepka@bgeo.es>
2021-05-24 08:29:04 +0200

    cat_mat_roughness define fields on config_form_fields

--- dbmodel/updates/35/35005/ws/dml.sql
+++ dbmodel/updates/35/35005/ws/dml.sql
@@ -10,3 +10,68 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 --2021/05/13
 UPDATE sys_table SET sys_role='role_edit' WHERE id='inp_connec';
+
+
+INSERT INTO config_form_fields(
+formname, formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, iseditable, 
+isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytext_filterc, stylesheet, 
+widgetcontrols, widgetfunction, linkedobject, hidden)
+VALUES ('cat_mat_roughness', 'form_feature', 'main', 'id', null, null, 'integer', 'text', 'id', false, false, true, 
+false, false, null, null,false, null, null,null,
+null,null,null,false);
+
+INSERT INTO config_form_fields(
+formname, formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, iseditable, 
+isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytext_filterc, stylesheet, 
+widgetcontrols, widgetfunction, linkedobject, hidden)
+VALUES ('cat_mat_roughness', 'form_feature', 'main', 'matcat_id', null, null, 'string', 'combo', 'matcat_id', true, false, true, 
+false, false, 'SELECT id, id AS idval FROM cat_mat_arc WHERE id IS NOT NULL', null,false, null, null,null,
+null,null,null,false);
+
+INSERT INTO config_form_fields(
+formname, formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, iseditable, 
+isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytext_filterc, stylesheet, 
+widgetcontrols, widgetfunction, linkedobject, hidden)
+VALUES ('cat_mat_roughness', 'form_feature', 'main', 'period_id', null, null, 'string', 'text', 'period_id', false, false, true, 
+false, false, null, null,false, null, null,null,
+null,null,null,false);
+
+INSERT INTO config_form_fields(
+formname, formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, iseditable, 
+isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytext_filterc, stylesheet, 
+widgetcontrols, widgetfunction, linkedobject, hidden)
+VALUES ('cat_mat_roughness', 'form_feature', 'main', 'init_age', null, null, 'integer', 'text', 'init_age', false, false, true, 
+false, false, null, null,false, null, null,null,
+null,null,null,false);
+
+INSERT INTO config_form_fields(
+formname, formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, iseditable, 
+isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytext_filterc, stylesheet, 
+widgetcontrols, widgetfunction, linkedobject, hidden)
+VALUES ('cat_mat_roughness', 'form_feature', 'main', 'end_age', null, null, 'integer', 'text', 'end_age', false, false, true, 
+false, false, null, null,false, null, null,null,
+null,null,null,false);
+
+INSERT INTO config_form_fields(
+formname, formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, iseditable, 
+isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytext_filterc, stylesheet, 
+widgetcontrols, widgetfunction, linkedobject, hidden)
+VALUES ('cat_mat_roughness', 'form_feature', 'main', 'roughness', null, null, 'numeric', 'text', 'roughness', false, false, true, 
+false, false, null, null,false, null, null,null,
+null,null,null,false);
+
+INSERT INTO config_form_fields(
+formname, formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, iseditable, 
+isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytext_filterc, stylesheet, 
+widgetcontrols, widgetfunction, linkedobject, hidden)
+VALUES ('cat_mat_roughness', 'form_feature', 'main', 'descript', null, null, 'string', 'text', 'descript', false, false, true, 
+false, false, null, null,false, null, null,null,
+null,null,null,false);
+
+INSERT INTO config_form_fields(
+formname, formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, iseditable, 
+isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytext_filterc, stylesheet, 
+widgetcontrols, widgetfunction, linkedobject, hidden)
+VALUES ('cat_mat_roughness', 'form_feature', 'main', 'active', null, null, 'boolean', 'check', 'active', false, false, true, 
+false, false, null, null,false, null, null,null,
+null,null,null,false);
