4b9a6e71347105957af47de1ac2cc926b1c708d4
xtorret@bgeo.es <xtorret@bgeo.es>
2021-05-31 10:28:05 +0200

    New view v_edit_inp_dwf (register on sys_tables later)

---
+++ dbmodel/ud/ftrg/ud_gw_trg_edit_inp_dwf.sql
@@ -0,0 +1,47 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+--FUNCTION CODE: 3036
+
+CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_edit_inp_dwf()  RETURNS trigger AS
+$BODY$
+
+DECLARE 
+	v_expl_id_int integer;
+
+BEGIN
+
+    EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
+	
+    IF TG_OP = 'INSERT' THEN
+
+		INSERT INTO inp_dwf (node_id, value, pat1, pat2, pat3, pat4, dwfscenario_id)
+		VALUES (NEW.node_id, NEW.value, NEW.pat1, NEW.pat2, NEW.pat3, NEW.pat4, NEW.dwfscenario_id);
+
+		RETURN NEW;
+		
+    ELSIF TG_OP = 'UPDATE' THEN
+   	
+		UPDATE inp_dwf 
+		SET node_id=NEW.node_id, value=NEW.value, pat1=NEW.pat1, pat2=NEW.pat2, pat3=NEW.pat3, pat4=NEW.pat4, dwfscenario_id = NEW.dwfscenario_id
+		WHERE id=NEW.id;
+		
+		RETURN NEW;
+		
+    ELSIF TG_OP = 'DELETE' THEN  
+	 
+		DELETE FROM inp_dwf WHERE id = OLD.id;		
+		RETURN NULL;
+     
+	END IF;
+
+END;
+	
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+
--- dbmodel/updates/35/35005/ud/ddlview.sql
+++ dbmodel/updates/35/35005/ud/ddlview.sql
@@ -28,3 +28,181 @@ CREATE OR REPLACE VIEW vp_basic_node AS
 SELECT node_id AS nid,
 node_type AS custom_type
 FROM node;
+
+
+CREATE OR REPLACE VIEW v_edit_inp_dwf AS 
+ SELECT id, node_id, i.value, pat1, pat2, pat3, pat4, dwfscenario_id FROM
+ config_param_user c, inp_dwf i
+ JOIN v_edit_inp_junction USING (node_id)
+  WHERE cur_user=current_user AND parameter = 'inp_options_dwfscenario' AND c.value::integer  = dwfscenario_id;
+
+
+CREATE OR REPLACE VIEW vu_node AS 
+ WITH vu_node AS (
+         SELECT node.node_id,
+            node.code,
+            node.top_elev,
+            node.custom_top_elev,
+                CASE
+                    WHEN node.custom_top_elev IS NOT NULL THEN node.custom_top_elev
+                    ELSE node.top_elev
+                END AS sys_top_elev,
+            node.ymax,
+            node.custom_ymax,
+                CASE
+                    WHEN node.custom_ymax IS NOT NULL THEN node.custom_ymax
+                    ELSE node.ymax
+                END AS sys_ymax,
+            node.elev,
+            node.custom_elev,
+                CASE
+                    WHEN node.elev IS NOT NULL AND node.custom_elev IS NULL THEN node.elev
+                    WHEN node.custom_elev IS NOT NULL THEN node.custom_elev
+                    ELSE NULL::numeric(12,3)
+                END AS sys_elev,
+            node.node_type,
+            cat_feature.system_id AS sys_type,
+            node.nodecat_id,
+                CASE
+                    WHEN node.matcat_id IS NULL THEN cat_node.matcat_id
+                    ELSE node.matcat_id
+                END AS matcat_id,
+            node.epa_type,
+            node.expl_id,
+            exploitation.macroexpl_id,
+            node.sector_id,
+            sector.macrosector_id,
+            node.state,
+            node.state_type,
+            node.annotation,
+            node.observ,
+            node.comment,
+            node.dma_id,
+            dma.macrodma_id,
+            node.soilcat_id,
+            node.function_type,
+            node.category_type,
+            node.fluid_type,
+            node.location_type,
+            node.workcat_id,
+            node.workcat_id_end,
+            node.buildercat_id,
+            node.builtdate,
+            node.enddate,
+            node.ownercat_id,
+            node.muni_id,
+            node.postcode,
+            node.district_id,
+            c.name AS streetname,
+            node.postnumber,
+            node.postcomplement,
+            d.name AS streetname2,
+            node.postnumber2,
+            node.postcomplement2,
+            node.descript,
+            cat_node.svg,
+            node.rotation,
+            concat(cat_feature.link_path, node.link) AS link,
+            node.verified,
+            node.undelete,
+            cat_node.label,
+            node.label_x,
+            node.label_y,
+            node.label_rotation,
+            node.publish,
+            node.inventory,
+            node.uncertain,
+            node.xyz_date,
+            node.unconnected,
+            node.num_value,
+            date_trunc('second'::text, node.tstamp) AS tstamp,
+            node.insert_user,
+            date_trunc('second'::text, node.lastupdate) AS lastupdate,
+            node.lastupdate_user,
+            node.the_geom,
+            node.workcat_id_plan
+           FROM node
+             LEFT JOIN cat_node ON node.nodecat_id::text = cat_node.id::text
+             LEFT JOIN cat_feature ON cat_feature.id::text = node.node_type::text
+             LEFT JOIN dma ON node.dma_id = dma.dma_id
+             LEFT JOIN sector ON node.sector_id = sector.sector_id
+             LEFT JOIN exploitation ON node.expl_id = exploitation.expl_id
+             LEFT JOIN ext_streetaxis c ON c.id::text = node.streetaxis_id::text
+             LEFT JOIN ext_streetaxis d ON d.id::text = node.streetaxis2_id::text
+        )
+ SELECT vu_node.node_id,
+    vu_node.code,
+    vu_node.top_elev,
+    vu_node.custom_top_elev,
+    vu_node.sys_top_elev,
+    vu_node.ymax,
+    vu_node.custom_ymax,
+        CASE
+            WHEN vu_node.sys_ymax IS NOT NULL THEN vu_node.sys_ymax
+            ELSE (vu_node.sys_top_elev - vu_node.sys_elev)::numeric(12,3)
+        END AS sys_ymax,
+    vu_node.elev,
+    vu_node.custom_elev,
+        CASE
+            WHEN vu_node.sys_elev IS NOT NULL THEN vu_node.sys_elev
+            ELSE (vu_node.sys_top_elev - vu_node.sys_ymax)::numeric(12,3)
+        END AS sys_elev,
+    vu_node.node_type,
+    vu_node.sys_type,
+    vu_node.nodecat_id,
+    vu_node.matcat_id,
+    vu_node.epa_type,
+    vu_node.expl_id,
+    vu_node.macroexpl_id,
+    vu_node.sector_id,
+    vu_node.macrosector_id,
+    vu_node.state,
+    vu_node.state_type,
+    vu_node.annotation,
+    vu_node.observ,
+    vu_node.comment,
+    vu_node.dma_id,
+    vu_node.macrodma_id,
+    vu_node.soilcat_id,
+    vu_node.function_type,
+    vu_node.category_type,
+    vu_node.fluid_type,
+    vu_node.location_type,
+    vu_node.workcat_id,
+    vu_node.workcat_id_end,
+    vu_node.buildercat_id,
+    vu_node.builtdate,
+    vu_node.enddate,
+    vu_node.ownercat_id,
+    vu_node.muni_id,
+    vu_node.postcode,
+    vu_node.district_id,
+    vu_node.streetname,
+    vu_node.postnumber,
+    vu_node.postcomplement,
+    vu_node.streetname2,
+    vu_node.postnumber2,
+    vu_node.postcomplement2,
+    vu_node.descript,
+    vu_node.svg,
+    vu_node.rotation,
+    vu_node.link,
+    vu_node.verified,
+    vu_node.the_geom,
+    vu_node.undelete,
+    vu_node.label,
+    vu_node.label_x,
+    vu_node.label_y,
+    vu_node.label_rotation,
+    vu_node.publish,
+    vu_node.inventory,
+    vu_node.uncertain,
+    vu_node.xyz_date,
+    vu_node.unconnected,
+    vu_node.num_value,
+    vu_node.tstamp,
+    vu_node.insert_user,
+    vu_node.lastupdate,
+    vu_node.lastupdate_user,
+    vu_node.workcat_id_plan
+   FROM vu_node;
