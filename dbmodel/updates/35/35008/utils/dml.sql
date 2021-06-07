d9eb3a39791ede250d3a4168b5e9a6e8dd7b30fc
Edgar Fuste <efuste@bgeo.es>
2021-06-07 13:54:25 +0200

    Create variable to manage cusomizable widgets for non api forms

--- dbmodel/updates/35/35008/utils/dml.sql
+++ dbmodel/updates/35/35008/utils/dml.sql
@@ -10,3 +10,11 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 --2021/06/04
 UPDATE config_toolbox SET functionparams = '{"featureType":["arc"]}' WHERE id = 2496;
+
+-- 2021/06/07
+INSERT INTO config_param_system (parameter, value, descript, project_type) VALUES(
+'edit_element_widgets_to_hiden',  '{}', 
+'Variable to customize widgets from element form. Available widggets:
+["element_id", "code", "element_type", "elementcat_id", "num_elements", "state", "state_type", "expl_id", "ownercat_id", "location_type", "buildercat_id", "builtdate", "workcat_id", "workcat_id_end", "comment", "observ", "link", "verified", "rotation", "undelete", "btn_add_geom"]',
+'ws')
+ON CONFLICT (parameter) DO NOTHING;
