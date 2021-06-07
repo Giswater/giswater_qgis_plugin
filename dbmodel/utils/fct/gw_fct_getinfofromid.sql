de57dd3e4329e6ed37d1591d0c752c60c5d94a41
xtorret@bgeo.es <xtorret@bgeo.es>
2021-06-07 17:10:05 +0200

    Enhance profilactic control for v_edit_cad layers

--- dbmodel/utils/fct/gw_fct_getinfofromid.sql
+++ dbmodel/utils/fct/gw_fct_getinfofromid.sql
@@ -308,7 +308,8 @@ BEGIN
 	v_vdefault_array := COALESCE(v_vdefault_array, '[]'); 
 	
 	-- getting source table in order to enhance performance
-	IF v_tablename LIKE 'v_edit_%' THEN v_sourcetable = replace (v_tablename, 'v_edit_', '');
+	IF v_tablename LIKE 'v_edit_cad%' THEN
+	ELSIF v_tablename LIKE 'v_edit_%' THEN v_sourcetable = replace (v_tablename, 'v_edit_', '');
 	ELSIF v_tablename LIKE 've_node_%' THEN v_sourcetable = 'node';
 	ELSIF v_tablename LIKE 've_arc_%' THEN v_sourcetable = 'arc';
 	ELSIF v_tablename LIKE 've_connec_%' THEN v_sourcetable = 'node';
