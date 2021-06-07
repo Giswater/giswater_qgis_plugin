9fa3b4ef0211d0a3494bdc24cc2653593a6a6ab4
xtorret@bgeo.es <xtorret@bgeo.es>
2021-06-07 17:17:45 +0200

    Update from prev commit

--- dbmodel/utils/fct/gw_fct_getinfofromid.sql
+++ dbmodel/utils/fct/gw_fct_getinfofromid.sql
@@ -308,7 +308,7 @@ BEGIN
 	v_vdefault_array := COALESCE(v_vdefault_array, '[]'); 
 	
 	-- getting source table in order to enhance performance
-	IF v_tablename LIKE 'v_edit_cad%' THEN
+	IF v_tablename LIKE 'v_edit_cad%' THEN v_sourcetable = v_tablename;
 	ELSIF v_tablename LIKE 'v_edit_%' THEN v_sourcetable = replace (v_tablename, 'v_edit_', '');
 	ELSIF v_tablename LIKE 've_node_%' THEN v_sourcetable = 'node';
 	ELSIF v_tablename LIKE 've_arc_%' THEN v_sourcetable = 'arc';
