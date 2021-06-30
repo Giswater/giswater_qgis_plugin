cb2fb3fb931ff6772a50b2e5a571b381f118e4aa
Barbara Rzepka <bbrzepka@bgeo.es>
2021-06-30 15:11:52 +0200

    fct_anl_arc_length - set correct fct  number on return

--- dbmodel/utils/fct/gw_fct_anl_arc_length.sql
+++ dbmodel/utils/fct/gw_fct_anl_arc_length.sql
@@ -118,7 +118,7 @@ BEGIN
 		     ',"data":{ "info":'||v_result_info||','||
 			'"line":'||v_result_line||
 		'}}'||
-	    '}')::json, 3040, null, null, null);
+	    '}')::json, 3052, null, null, null);
 
 	EXCEPTION WHEN OTHERS THEN
 	GET STACKED DIAGNOSTICS v_error_context = PG_EXCEPTION_CONTEXT;
