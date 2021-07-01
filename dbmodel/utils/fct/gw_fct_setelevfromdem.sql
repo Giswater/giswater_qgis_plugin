61fe1cb07e341b4e16f73519e342a6e73084ed05
xtorret@bgeo.es <xtorret@bgeo.es>
2021-07-01 12:08:18 +0200

    Enhance message for null values returned on setelevformdem function

--- dbmodel/utils/fct/gw_fct_setelevfromdem.sql
+++ dbmodel/utils/fct/gw_fct_setelevfromdem.sql
@@ -111,7 +111,7 @@ BEGIN
 
 			IF v_check_null_elevation IS NUll THEN
 				INSERT INTO audit_check_data(fid,result_id, error_message)
-				VALUES (168,'elevation from raster','THERE ARE NO FEATURES WITH ELEVATION NULL');
+				VALUES (168,'elevation from raster','There are no features with NULL elevation');
 			END IF;
 
 		END IF;
@@ -129,7 +129,8 @@ BEGIN
 			--if node is out of raster, add warning, if it's inside update value of node layer
 			IF v_elevation = -9999 OR v_elevation IS NULL THEN
 				INSERT INTO audit_check_data(fid,result_id, error_message)
-				VALUES (168,'elevation from raster',concat('WARNING-168: SELECTED FEATURE IS OUT OF RASTER: ', rec.feature_id));
+				VALUES (168,'elevation from raster',concat('WARNING-168: Intersection with feature ',rec.feature_id,
+				' has returned NULL value. Could be out of raster or some problem with algorithm'));
 			ELSE
 				IF v_project_type = 'WS' AND v_feature_type='vnode' THEN 
 					EXECUTE 'UPDATE vnode SET elev = '||v_elevation||'::numeric WHERE vnode_id = '||rec.feature_id||';';
