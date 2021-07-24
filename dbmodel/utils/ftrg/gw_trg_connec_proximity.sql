22ff6ee496a517734fb312e9b950359c37b3ee6c
xtorret@bgeo.es <xtorret@bgeo.es>
2021-07-24 22:36:21 +0200

    Enhance trigger gully proximity managing disable error (fid:105)  if variable is true

--- dbmodel/utils/ftrg/gw_trg_connec_proximity.sql
+++ dbmodel/utils/ftrg/gw_trg_connec_proximity.sql
@@ -10,39 +10,43 @@ CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_connec_proximity()
 RETURNS trigger AS 
 $BODY$
 DECLARE 
-    v_numConnecs numeric;
-    v_connec_proximity double precision;
-    v_connec_proximity_control boolean;
+v_numConnecs numeric;
+v_connec_proximity double precision;
+v_connec_proximity_control boolean;
+v_dsbl_error boolean;
+v_message text;
     
 BEGIN
 
-    EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
+	EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
 
-    -- Get connec tolerance from config table
+	-- Get connec tolerance from config table
+	SELECT ((value::json)->>'value') INTO v_connec_proximity FROM config_param_system WHERE parameter='edit_connec_proximity';
+	SELECT ((value::json)->>'activated') INTO v_connec_proximity_control FROM config_param_system WHERE parameter='edit_connec_proximity';
+	SELECT value::boolean INTO v_dsbl_error FROM config_param_system WHERE parameter='edit_topocontrol_disable_error' ;
 
-    SELECT ((value::json)->>'value') INTO v_connec_proximity FROM config_param_system WHERE parameter='edit_connec_proximity';
-    SELECT ((value::json)->>'activated') INTO v_connec_proximity_control FROM config_param_system WHERE parameter='edit_connec_proximity';
+	IF TG_OP = 'INSERT' THEN
+		-- Existing connecs  
+		v_numConnecs:= (SELECT COUNT(*) FROM connec WHERE ST_DWithin(NEW.the_geom, connec.the_geom, v_connec_proximity) AND connec.connec_id != NEW.connec_id);
 
-    IF TG_OP = 'INSERT' THEN
-        -- Existing connecs  
-        v_numConnecs:= (SELECT COUNT(*) FROM connec WHERE ST_DWithin(NEW.the_geom, connec.the_geom, v_connec_proximity) AND connec.connec_id != NEW.connec_id);
+	ELSIF TG_OP = 'UPDATE' THEN
+		-- Existing connecs  
+		v_numConnecs := (SELECT COUNT(*) FROM connec WHERE ST_DWithin(NEW.the_geom, connec.the_geom, v_connec_proximity) AND connec.connec_id != NEW.connec_id);
+	END IF;
 
-    ELSIF TG_OP = 'UPDATE' THEN
-        -- Existing connecs  
-       v_numConnecs := (SELECT COUNT(*) FROM connec WHERE ST_DWithin(NEW.the_geom, connec.the_geom, v_connec_proximity) AND connec.connec_id != NEW.connec_id);
-    END IF;
+	-- If there is an existing connec closer than 'rec.connec_tolerance' meters --> error
+	IF (v_numConnecs > 0) AND (v_connec_proximity_control IS TRUE) THEN
+		IF v_dsbl_error IS NOT TRUE THEN
+			EXECUTE 'SELECT gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
+			"data":{"message":"1044", "function":"1106","debug_msg":"'||NEW.connec_id||'"}}$$);';
 
-    -- If there is an existing connec closer than 'rec.connec_tolerance' meters --> error
-    IF (v_numConnecs > 0) AND (v_connec_proximity_control IS TRUE) THEN
+		ELSE
+			SELECT concat('ERROR-',id,':',error_message,'.',hint_message) INTO v_message FROM sys_message WHERE id = 1044;
+			INSERT INTO audit_log_data (fid, feature_id, log_message) VALUES (105, NEW.connec_id, v_message);
+		END IF;
+	END IF;
 
-       EXECUTE 'SELECT gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
-        "data":{"message":"1044", "function":"1106","debug_msg":"'||NEW.connec_id||'"}}$$);';
-
-    END IF;
-
-    RETURN NEW;
-
-     
+	RETURN NEW;    
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
