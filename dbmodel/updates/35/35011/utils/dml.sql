8a7c1e4924c38a6f647a392b5de8962055647571
Edgar Fuste <efuste@bgeo.es>
2021-07-30 13:07:37 +0200

    Minor bug fix: on previous commit

--- dbmodel/updates/35/35011/utils/dml.sql
+++ dbmodel/updates/35/35011/utils/dml.sql
@@ -22,10 +22,10 @@ INSERT INTO config_form_list(listname, alias, query_text, device, listtype, list
 VALUES ('arc_length_x_arccat', 'Arc length grouped by catalog','SELECT sum(gis_length), arccat_id FROM v_edit_arc GROUP BY arccat_id', 3 ,'report',
 'list','{"orderBy":"1", "orderType": "DESC"}',
 '[{"columnname":"arccat_id", "label":"Arc catalog:", "widgettype":"combo","datatype":"text","layoutorder":1,
-"dvquerytext":"Select id as id, id as idval FROM cat_arc WHERE id IS NOT NULL","isNullValue":True},{"columnname":"expl_id", "label":"Exploitation:", "widgettype":"combo","datatype":"text","layoutorder":1,
-"dvquerytext":"Select expl_id as id, name as idval FROM exploitation WHERE expl_id IS NOT NULL","isNullValue":True}]');
+"dvquerytext":"Select id as id, id as idval FROM cat_arc WHERE id IS NOT NULL","isNullValue":"true"},{"columnname":"expl_id", "label":"Exploitation:", "widgettype":"combo","datatype":"text","layoutorder":1,
+"dvquerytext":"Select expl_id as id, name as idval FROM exploitation WHERE expl_id IS NOT NULL","isNullValue":"true"}]');
 
 INSERT INTO config_form_list(listname, alias, query_text, device, listtype, listclass, vdefault, filterparam)
 VALUES ('connec_x_expl', 'Connecs by exploitation','SELECT connec_id, code, customer_code FROM v_edit_connec ', 3 ,'report','list','{"orderBy":"1", "orderType": "DESC"}',
 '[{"columnname":"expl_id", "label":"Exploitation:", "widgettype":"combo","datatype":"text","layoutorder":1,
-"dvquerytext":"Select expl_id as id, name as idval FROM exploitation WHERE expl_id IS NOT NULL","isNullValue":True}]');
+"dvquerytext":"Select expl_id as id, name as idval FROM exploitation WHERE expl_id IS NOT NULL","isNullValue":"true"}]');
