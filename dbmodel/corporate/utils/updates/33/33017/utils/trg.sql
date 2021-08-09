2770cf1f08feb6b61c40c71e71cc331455a2bc4c
Albert Bofill <abofill@bgeo.es>
2021-08-09 16:35:33 +0200

    Manage schema_name from utils updates

--- dbmodel/corporate/utils/updates/33/33017/utils/trg.sql
+++ dbmodel/corporate/utils/updates/33/33017/utils/trg.sql
@@ -8,9 +8,9 @@ This version of Giswater is provided by Giswater Association
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
 
-DROP TRIGGER IF EXISTS gw_trg_manage_raster_dem ON SCHEMA_NAME.raster_dem;
+DROP TRIGGER IF EXISTS gw_trg_manage_raster_dem ON raster_dem;
 CREATE TRIGGER gw_trg_manage_raster_dem
-AFTER INSERT OR DELETE ON SCHEMA_NAME.raster_dem
+AFTER INSERT OR DELETE ON raster_dem
 FOR EACH ROW
-EXECUTE PROCEDURE SCHEMA_NAME.gw_trg_manage_raster_dem();
+EXECUTE PROCEDURE gw_trg_manage_raster_dem();
 
--- dbmodel/corporate/utils/updates/33/33043/utils/trg.sql
+++ dbmodel/corporate/utils/updates/33/33043/utils/trg.sql
@@ -5,14 +5,14 @@ This version of Giswater is provided by Giswater Association
 */
 
 
-SET search_path = "SCHEMA_NAME", public, pg_catalog;
+SET search_path = "utils", public, pg_catalog;
 
 -- 2020/04/15
-DROP TRIGGER IF EXISTS gw_trg_manage_raster_dem ON SCHEMA_NAME.raster_dem;
-DROP TRIGGER IF EXISTS gw_trg_manage_raster_dem_insert ON SCHEMA_NAME.raster_dem;
-CREATE TRIGGER gw_trg_manage_raster_dem_insert BEFORE INSERT ON SCHEMA_NAME.raster_dem
+DROP TRIGGER IF EXISTS gw_trg_manage_raster_dem ON raster_dem;
+DROP TRIGGER IF EXISTS gw_trg_manage_raster_dem_insert ON raster_dem;
+CREATE TRIGGER gw_trg_manage_raster_dem_insert BEFORE INSERT ON raster_dem
 FOR EACH ROW EXECUTE PROCEDURE gw_trg_manage_raster_dem();
 
-DROP TRIGGER IF EXISTS gw_trg_manage_raster_dem_delete ON SCHEMA_NAME.raster_dem;
-CREATE TRIGGER gw_trg_manage_raster_dem_delete AFTER DELETE ON SCHEMA_NAME.raster_dem
+DROP TRIGGER IF EXISTS gw_trg_manage_raster_dem_delete ON raster_dem;
+CREATE TRIGGER gw_trg_manage_raster_dem_delete AFTER DELETE ON raster_dem
 FOR EACH ROW EXECUTE PROCEDURE gw_trg_manage_raster_dem();
