01ceda14d90a945dbaef7a26c26da54b715573fe
Sergi Maspons <smaspons@bgeo.es>
2021-08-17 13:57:23 +0200

    Enhancement: added reset of open_search variable in "Reset plugin" action

--- core/load_project_menu.py
+++ core/load_project_menu.py
@@ -302,6 +302,7 @@ class GwMenuLoad(QObject):
 
         self._reset_snapping_managers()
         self._reset_all_rubberbands()
+        tools_gw.set_config_parser('btn_search', 'open_search', 'false')
         self.iface.actionPan().trigger()
         self._reload_layers()
 
