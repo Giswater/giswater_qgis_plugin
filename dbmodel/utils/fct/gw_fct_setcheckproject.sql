27df58a1a1e0357d68201da33ec489f225967a79
xtorret@bgeo.es <xtorret@bgeo.es>
2021-08-31 10:03:26 +0200

    Profilactic delete of legacy values on man_addfields_value on setcheckproject function

--- dbmodel/utils/fct/gw_fct_setcheckproject.sql
+++ dbmodel/utils/fct/gw_fct_setcheckproject.sql
@@ -154,6 +154,22 @@ BEGIN
 	END LOOP;
 	IF query_text IS NOT NULL THEN EXECUTE query_text; END IF;
     
+
+	-- profilactic delete of legacy values on man_addfields_value
+	IF v_project_type='WS' THEN
+		DELETE FROM man_addfields_value WHERE feature_id IN(
+		SELECT m.feature_id FROM man_addfields_value m LEFT JOIN
+		(SELECT arc_id as feature_id from arc UNION SELECT node_id FROM node UNION SELECT connec_id from connec) a USING (feature_id)
+		WHERE a.feature_id is null
+		);
+	ELSE
+		DELETE FROM man_addfields_value WHERE feature_id IN(
+		SELECT m.feature_id FROM man_addfields_value m LEFT JOIN
+		(SELECT arc_id as feature_id from arc UNION SELECT node_id FROM node UNION SELECT connec_id from connec UNION SELECT gully_id from gully) a USING (feature_id)
+		WHERE a.feature_id is null
+		);
+	END IF;	
+		
 	-- delete old values on result table
 	DELETE FROM audit_check_data WHERE fid=101 AND cur_user=current_user;
 	
