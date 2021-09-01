fe916b8b04d0e03a6d3b2eea76bc9505d7f97443
Barbara Rzepka <bbrzepka@bgeo.es>
2021-09-01 15:54:49 +0200

    bug fix on utils sql

--- dbmodel/corporate/utils/ud/ddl.sql
+++ dbmodel/corporate/utils/ud/ddl.sql
@@ -7,12 +7,12 @@ This version of Giswater is provided by Giswater Association
 
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
-ALTER TABLE ext_municipality RENAME ext_municipality_old;
-ALTER TABLE ext_district RENAME ext_district_old;
-ALTER TABLE ext_type_street RENAME ext_type_street_old;
-ALTER TABLE ext_streetaxis RENAME ext_streetaxis_old;
-ALTER TABLE ext_plot RENAME ext_plot_old;
-ALTER TABLE ext_address RENAME ext_address_old;
+ALTER TABLE ext_municipality RENAME TO ext_municipality_old;
+ALTER TABLE ext_district RENAME TO ext_district_old;
+ALTER TABLE ext_type_street RENAME TO ext_type_street_old;
+ALTER TABLE ext_streetaxis RENAME TO ext_streetaxis_old;
+ALTER TABLE ext_plot RENAME TO ext_plot_old;
+ALTER TABLE ext_address RENAME TO ext_address_old;
 
-ALTER TABLE ext_cat_raster RENAME ext_cat_raster_old;
-ALTER TABLE ext_raster_dem RENAME ext_raster_dem_old;
+ALTER TABLE ext_cat_raster RENAME TO ext_cat_raster_old;
+ALTER TABLE ext_raster_dem RENAME TO ext_raster_dem_old;
