6e198844c5111ef169db79097e724fd6e59e0748
xtorret@bgeo.es <xtorret@bgeo.es>
2021-10-01 19:59:40 +0200

    Rename file

--- dbmodel/ws/ftrg/ws_gw_trg_edit_inp_dscenario_demand.sql
+++ dbmodel/ws/ftrg/ws_gw_trg_edit_inp_dscenario_demand.sql
@@ -6,8 +6,7 @@ This version of Giswater is provided by Giswater Association
 
 --FUNCTION CODE: 1308
 
-
-CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_edit_inp_demand() RETURNS trigger LANGUAGE plpgsql AS $$
+CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_edit_inp_dscenario_demand() RETURNS trigger LANGUAGE plpgsql AS $$
 DECLARE 
 
 BEGIN
@@ -19,14 +18,13 @@ BEGIN
 
 		-- overwrite id values (in case of user fill something)
 		PERFORM setval('SCHEMA_NAME.inp_dscenario_demand_id_seq',(SELECT max(id) FROM inp_dscenario_demand), true);
-		NEW.id=nextval('SCHEMA_NAME.inp_dscenario_demand_id_seq'::regclass);
 	
-		INSERT INTO inp_dscenario_demand (id, feature_id, demand, pattern_id, demand_type, dscenario_id, feature_type) 
-		VALUES (NEW.id,NEW.feature_id, NEW.demand, NEW.pattern_id, NEW.demand_type, NEW.dscenario_id, NEW.feature_type);
+		INSERT INTO inp_dscenario_demand (feature_id, demand, pattern_id, demand_type, dscenario_id, feature_type) 
+		VALUES (NEW.feature_id, NEW.demand, NEW.pattern_id, NEW.demand_type, NEW.dscenario_id, NEW.feature_type);
 		RETURN NEW;
 
 	ELSIF TG_OP = 'UPDATE' THEN
-		UPDATE inp_dscenario_demand SET id=NEW.id, feature_id=NEW.feature_id, demand=NEW.demand, pattern_id=NEW.pattern_id, 
+		UPDATE inp_dscenario_demand SET feature_id=NEW.feature_id, demand=NEW.demand, pattern_id=NEW.pattern_id, 
 		demand_type=NEW.demand_type, dscenario_id=NEW.dscenario_id,  feature_type=NEW.feature_type
 		WHERE id=OLD.id;
 		RETURN NEW;
