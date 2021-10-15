c07300e42b539d51286996d990015a7f282c0fc9
Sergi Maspons <smaspons@bgeo.es>
2021-10-15 09:52:23 +0200

    Minor enhancement: order alphabetically the actions on the Giswater menu

--- core/load_project_menu.py
+++ core/load_project_menu.py
@@ -97,27 +97,7 @@ class GwMenuLoad(QObject):
         actions_menu.setIcon(config_icon)
         self.main_menu.addMenu(actions_menu)
 
-        action_set_log_sql = actions_menu.addAction(f"Toggle Log DB")
-        log_sql_shortcut = tools_gw.get_config_parser("system", f"log_sql_shortcut", "user", "init", prefix=False)
-        if not log_sql_shortcut:
-            tools_gw.set_config_parser("log", f"log_sql_shortcut", f"{log_sql_shortcut}", "user", "init",
-                                       prefix=False)
-        action_set_log_sql.setShortcuts(QKeySequence(f"{log_sql_shortcut}"))
-        action_set_log_sql.triggered.connect(self._set_log_sql)
-
-        action_reset_dialogs = actions_menu.addAction(f"Reset dialogs")
-
-        action_reset_dialogs.triggered.connect(self._reset_position_dialog)
-
-        action_open_selections = actions_menu.addAction(f"Show current selectors")
-        action_open_selections_shortcut = tools_gw.get_config_parser("system", f"open_selections_shortcut", "user", "init",
-                                                                    prefix=False)
-        if not action_open_selections_shortcut:
-            tools_gw.set_config_parser("system", f"open_selections_shortcut", f"{action_open_selections_shortcut}", "user",
-                                       "init", prefix=False)
-        action_open_selections.setShortcuts(QKeySequence(f"{action_open_selections_shortcut}"))
-        action_open_selections.triggered.connect(self._open_current_selections)
-
+        # Action 'Get help'
         action_help = actions_menu.addAction(f"Get help")
         action_help_shortcut = tools_gw.get_config_parser("system", f"help_shortcut", "user", "init", prefix=False)
         if not action_help_shortcut:
@@ -126,6 +106,11 @@ class GwMenuLoad(QObject):
         action_help.setShortcuts(QKeySequence(f"{action_help_shortcut}"))
         action_help.triggered.connect(tools_gw.open_dlg_help)
 
+        # Action 'Reset dialogs'
+        action_reset_dialogs = actions_menu.addAction(f"Reset dialogs")
+        action_reset_dialogs.triggered.connect(self._reset_position_dialog)
+
+        # Action 'Reset plugin
         action_reset_plugin = actions_menu.addAction(f"Reset plugin")
         action_reset_plugin_shortcut = tools_gw.get_config_parser("system", f"reset_plugin_shortcut", "user", "init", prefix=False)
         if not action_reset_plugin_shortcut:
@@ -133,6 +118,26 @@ class GwMenuLoad(QObject):
                                        "init", prefix=False)
         action_reset_plugin.setShortcuts(QKeySequence(f"{action_reset_plugin_shortcut}"))
         action_reset_plugin.triggered.connect(self._reset_plugin)
+
+        # Action 'Show current selectors'
+        action_open_selections = actions_menu.addAction(f"Show current selectors")
+        action_open_selections_shortcut = tools_gw.get_config_parser("system", f"open_selections_shortcut", "user", "init",
+                                                                     prefix=False)
+        if not action_open_selections_shortcut:
+            tools_gw.set_config_parser("system", f"open_selections_shortcut", f"{action_open_selections_shortcut}", "user",
+                                       "init", prefix=False)
+        action_open_selections.setShortcuts(QKeySequence(f"{action_open_selections_shortcut}"))
+        action_open_selections.triggered.connect(self._open_current_selections)
+
+        # Action 'Toggle Log DB'
+        action_set_log_sql = actions_menu.addAction(f"Toggle Log DB")
+        log_sql_shortcut = tools_gw.get_config_parser("system", f"log_sql_shortcut", "user", "init", prefix=False)
+        if not log_sql_shortcut:
+            tools_gw.set_config_parser("log", f"log_sql_shortcut", f"{log_sql_shortcut}", "user", "init",
+                                       prefix=False)
+        action_set_log_sql.setShortcuts(QKeySequence(f"{log_sql_shortcut}"))
+        action_set_log_sql.triggered.connect(self._set_log_sql)
+
         # endregion
 
         # region Advanced
