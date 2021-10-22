8c5c248ee19312af3b7aca51368aacafda6e0815
xtorret@bgeo.es <xtorret@bgeo.es>
2021-10-22 16:49:51 +0200

    Enhance views of vi_subcatchment adding column hydrology_id in order to clarify that they are filtered by this column

--- dbmodel/updates/35/35016/ud/ddlview.sql
+++ dbmodel/updates/35/35016/ud/ddlview.sql
@@ -21,3 +21,24 @@ CREATE OR REPLACE VIEW vi_losses AS
    FROM temp_arc
   WHERE kentry > 0::numeric OR kexit > 0::numeric OR kavg > 0::numeric 
   OR flap::text = 'YES'::text OR seepage is not null;
+
+
+DROP VIEW IF EXISTS vi_subcatch2node;
+CREATE OR REPLACE VIEW vi_subcatch2node AS 
+ SELECT s1.subc_id,
+ s2.hydrology_id,
+        CASE
+            WHEN s2.the_geom IS NOT NULL THEN st_makeline(st_centroid(s1.the_geom), st_centroid(s2.the_geom))
+            ELSE st_makeline(st_centroid(s1.the_geom), v_node.the_geom)
+        END AS the_geom
+   FROM v_edit_inp_subcatchment s1
+     LEFT JOIN v_edit_inp_subcatchment s2 ON s2.subc_id::text = s1.outlet_id::text
+     LEFT JOIN v_node ON v_node.node_id::text = s1.outlet_id::text;
+
+
+ DROP VIEW IF EXISTS vi_subcatchcentroid;
+CREATE OR REPLACE VIEW vi_subcatchcentroid AS 
+ SELECT v_edit_inp_subcatchment.subc_id,
+  hydrology_id,
+    st_centroid(v_edit_inp_subcatchment.the_geom) AS the_geom
+   FROM v_edit_inp_subcatchment;
