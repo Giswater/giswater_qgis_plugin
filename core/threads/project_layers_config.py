d9a7abeb84e3b8b73d9fa270bc6d231047841d9e
Edgar Fuste <efuste@bgeo.es>
2021-10-26 08:52:51 +0200

    Minor bug fix: manage sended layers from database when does not exist in project

--- core/threads/project_layers_config.py
+++ core/threads/project_layers_config.py
@@ -198,19 +198,20 @@ class GwProjectLayersConfig(GwTask):
                     if 'activated' in value_relation and value_relation['activated']:
                         try:
                             vr_layer = value_relation['layer']
-                            vr_layer = tools_qgis.get_layer_by_tablename(vr_layer).id()  # Get layer id
-                            vr_key_column = value_relation['keyColumn']  # Get 'Key'
-                            vr_value_column = value_relation['valueColumn']  # Get 'Value'
-                            vr_filter_expression = value_relation['filterExpression']  # Get 'FilterExpression'
-                            if vr_filter_expression is None:
-                                vr_filter_expression = ''
+                            if tools_qgis.get_layer_by_tablename(vr_layer) is not None:
+                                vr_layer = tools_qgis.get_layer_by_tablename(vr_layer).id()  # Get layer id
+                                vr_key_column = value_relation['keyColumn']  # Get 'Key'
+                                vr_value_column = value_relation['valueColumn']  # Get 'Value'
+                                vr_filter_expression = value_relation['filterExpression']  # Get 'FilterExpression'
+                                if vr_filter_expression is None:
+                                    vr_filter_expression = ''
 
-                            # Create and apply ValueRelation config
-                            editor_widget_setup = QgsEditorWidgetSetup('ValueRelation', {'Layer': f'{vr_layer}',
-                                                                                         'Key': f'{vr_key_column}',
-                                                                                         'Value': f'{vr_value_column}',
-                                                                                         'FilterExpression': f'{vr_filter_expression}'})
-                            layer.setEditorWidgetSetup(field_index, editor_widget_setup)
+                                # Create and apply ValueRelation config
+                                editor_widget_setup = QgsEditorWidgetSetup('ValueRelation', {'Layer': f'{vr_layer}',
+                                                                                             'Key': f'{vr_key_column}',
+                                                                                             'Value': f'{vr_value_column}',
+                                                                                             'FilterExpression': f'{vr_filter_expression}'})
+                                layer.setEditorWidgetSetup(field_index, editor_widget_setup)
                         except Exception as e:
                             self.exception = e
                             self.message = f"Error configuring ValueRelation for " \
