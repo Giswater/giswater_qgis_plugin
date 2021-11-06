f15195094377f7d17fab7ea0ef2f5c05eb2bc8f2
xtorret@bgeo.es <xtorret@bgeo.es>
2021-11-06 08:23:59 +0100

    Clean code on pg2epa_demand function

--- dbmodel/ws/fct/ws_gw_fct_pg2epa_demand.sql
+++ dbmodel/ws/fct/ws_gw_fct_pg2epa_demand.sql
@@ -435,14 +435,14 @@ BEGIN
 		END IF;
 	END IF;
 
-	IF v_patternmethod IN (11,12,13,21,22,23, 31,33,41,43) THEN
+	IF v_patternmethod IN (11,12,13,21,22,23,31,33,41,43) THEN
 
 		-- move patterns from inp_pattern_value to rpt_pattern_value
 		INSERT INTO rpt_inp_pattern_value (result_id, pattern_id, factor_1, factor_2, factor_3, factor_4, factor_5, factor_6, factor_7, factor_8, 
 			factor_9, factor_10, factor_11, factor_12, factor_13, factor_14, factor_15, factor_16, factor_17, factor_18)
 		SELECT result_id_var, pattern_id, factor_1, factor_2, factor_3, factor_4, factor_5, factor_6, factor_7, factor_8, factor_9, factor_10, factor_11, 
 			factor_12, factor_13, factor_14, factor_15, factor_16, factor_17, factor_18
-			FROM inp_pattern_value WHERE pattern_id IN (SELECT pattern_id FROM temp_node)
+			FROM inp_pattern_value WHERE pattern_id IN (SELECT DISTINCT pattern_id FROM temp_node)
 			ORDER BY id;
 	END IF;
 
