6074fa8aba02713d7bd3ab8b678fb69fb3f1ff66
xtorret@bgeo.es <xtorret@bgeo.es>
2021-11-06 12:05:04 +0100

    Enhance ud capabilities managing missed data from SWMM when is used (*)

--- dbmodel/ud/fct/ud_gw_fct_import_swmm_inp.sql
+++ dbmodel/ud/fct/ud_gw_fct_import_swmm_inp.sql
@@ -331,15 +331,20 @@ BEGIN
 
 
 			-- improve velocity for conduits using directly tables in spite of vi_conduits view
-			INSERT INTO arc (arc_id, code, node_1,node_2, custom_length, elev1, elev2, sys_elev1, sys_elev2, arc_type, epa_type, arccat_id, matcat_id, sector_id, dma_id, expl_id, state, state_type, slope) 
-			SELECT csv1, csv1, csv2, csv3, csv4::numeric(12,3), csv6::numeric(12,3), csv7::numeric(12,3), csv6::numeric(12,3), csv7::numeric(12,3), 'CONDUIT', 'CONDUIT', csv5, csv5, 1, 1, 1, 1, 2 
+			INSERT INTO arc (arc_id, code, node_1,node_2, custom_length, elev1, elev2, arc_type, epa_type, arccat_id, matcat_id, sector_id, dma_id, expl_id, state, state_type) 
+			SELECT csv1, csv1, csv2, csv3, csv4::numeric(12,3), 
+			CASE WHEN csv6 = '*' THEN NULL ELSE csv6::numeric(12,3) END,
+			CASE WHEN csv7 = '*' THEN NULL ELSE csv7::numeric(12,3) END,
+			'CONDUIT', 'CONDUIT', csv5, csv5, 1, 1, 1, 1, 2 
 			FROM temp_csv where source='[CONDUITS]' AND fid = v_fid  AND (csv1 NOT LIKE '[%' AND csv1 NOT LIKE ';%') AND cur_user=current_user;
 			INSERT INTO man_conduit(arc_id) SELECT csv1
 			FROM temp_csv where source='[CONDUITS]' AND fid = v_fid  AND (csv1 NOT LIKE '[%' AND csv1 NOT LIKE ';%') AND cur_user=current_user;
 			INSERT INTO inp_conduit (arc_id, q0, qmax) SELECT csv1, csv8::numeric(12,3), csv9::numeric(12,3)
 			FROM temp_csv where source='[CONDUITS]' AND fid = v_fid  AND (csv1 NOT LIKE '[%' AND csv1 NOT LIKE ';%') AND cur_user=current_user;
 
-			UPDATE arc SET sys_slope = (sys_elev1-sys_elev2)/st_length(the_geom);
+			-- update arc values
+			UPDATE arc SET sys_elev1 = elev1, sys_elev2 = elev2;
+			UPDATE arc SET sys_slope = (elev1-elev2)/st_length(the_geom) WHERE st_length(the_geom) > 0.001;
 
 			-- insert other catalog tables
 			INSERT INTO cat_work VALUES ('IMPORTINP', 'IMPORTINP') ON CONFLICT (id) DO NOTHING;
