6fd7e7a137656c9b9598aba49c9bd64e56c38d0c
xtorret@bgeo.es <xtorret@bgeo.es>
2021-11-06 10:18:20 +0100

    Improve definitions for value relationsw

--- dbmodel/updates/35/35017/ud/dml.sql
+++ dbmodel/updates/35/35017/ud/dml.sql
@@ -11,5 +11,41 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 INSERT INTO sys_fprocess(fid, fprocess_name, project_type, parameters, source) 
 VALUES (416, 'Gully without pjoint_id or pjoint_type','ud', null, null) ON CONFLICT (fid) DO NOTHING;
 
+--2021/11/06
+UPDATE config_form_fields SET widgetcontrols = (replace(widgetcontrols::text, '"valueRelation":{', '"valueRelation":{"nullValue":false, '))::json 
+WHERE widgetcontrols->>'valueRelation' IS NOT NULL;
 
+UPDATE config_form_fields SET widgetcontrols = (replace(widgetcontrols::text, '"valueRelation": {', '"valueRelation":{"nullValue":false, '))::json 
+WHERE widgetcontrols->>'valueRelation' IS NOT NULL;
 
+UPDATE config_form_fields 
+SET widgetcontrols = gw_fct_json_object_set_key(widgetcontrols, 'valueRelation', '{"nullValue":false, "layer": "cat_dwf_scenario", "activated": true, "keyColumn": "id", "valueColumn": "idval", "filterExpression": ""}'::json)
+WHERE columnname = 'dwfscenario_id' AND formname !='cat_dwf_scenario';
+
+UPDATE config_form_fields 
+SET widgetcontrols = gw_fct_json_object_set_key(widgetcontrols, 'valueRelation', '{"nullValue":true, "layer": "cat_hydrology", "activated": true, "keyColumn": "pattern_id", "valueColumn": "pattern_id", "filterExpression": ""}'::json)
+WHERE columnname = 'pattern_id' AND formname !='inp_pattern';
+
+UPDATE config_form_fields 
+SET widgetcontrols = gw_fct_json_object_set_key(widgetcontrols, 'valueRelation', '{"nullValue":true, "layer": "v_edit_inp_curve", "activated": true, "keyColumn": "id", "valueColumn": "id", "filterExpression": ""}'::json)
+WHERE columnname = 'curve_id' AND formname !='inp_curve';
+
+UPDATE config_form_fields 
+SET widgetcontrols = gw_fct_json_object_set_key(widgetcontrols, 'valueRelation', '{"nullValue":true, "layer": "inp_timeseries", "activated": true, "keyColumn": "id", "valueColumn": "id", "filterExpression": ""}'::json)
+WHERE columnname = 'timser_id' AND formname !='inp_timeseries';
+
+UPDATE config_form_fields 
+SET widgetcontrols = gw_fct_json_object_set_key(widgetcontrols, 'valueRelation', '{"nullValue":true, "layer": "inp_lid_control", "activated": true, "keyColumn": "dscenario_id", "valueColumn": "name", "filterExpression": ""}'::json)
+WHERE columnname = 'dscenario_id' AND formname !='cat_dscenario';
+
+UPDATE config_form_fields 
+SET widgetcontrols = '{"setMultiline": false, "valueRelation":{"nullValue":false,   "layer": "cat_feature", "activated": true, "keyColumn": "id", "valueColumn": "id", "filterExpression":"feature_type=''ARC''"}}'
+WHERE columnname = 'arc_type' AND (formname = 'v_edit_arc' or formname like 've_arc%');
+
+UPDATE config_form_fields 
+SET widgetcontrols = '{"setMultiline": false, "valueRelation":{"nullValue":false,   "layer": "cat_feature", "activated": true, "keyColumn": "id", "valueColumn": "id", "filterExpression":"feature_type=''CONNEC''"}}'
+WHERE columnname = 'connec_type' AND (formname = 'v_edit_connec' or formname like 've_connec%');
+
+UPDATE config_form_fields 
+SET widgetcontrols = '{"setMultiline": false, "valueRelation":{"nullValue":false,   "layer": "cat_feature", "activated": true, "keyColumn": "id", "valueColumn": "id", "filterExpression":"feature_type=''GYLLY''"}}'
+WHERE columnname = 'gully_type' AND (formname = 'v_edit_gully' or formname like 've_gully%' or formname  ='cat_grate');
--- dbmodel/updates/35/35017/ws/dml.sql
+++ dbmodel/updates/35/35017/ws/dml.sql
@@ -16,3 +16,27 @@ VALUES (414, 'Cat connec rows without dint','ws', null, null) ON CONFLICT (fid) 
 --2021/11/03
 update sys_param_user set layoutname='lyt_edit', layoutorder=1 where id='edit_elementcat_vdefault';
 
+--2021/11/06
+UPDATE config_form_fields SET widgetcontrols = (replace(widgetcontrols::text, '"valueRelation":{', '"valueRelation":{"nullValue":false, '))::json 
+WHERE widgetcontrols->>'valueRelation' IS NOT NULL;
+
+UPDATE config_form_fields SET widgetcontrols = (replace(widgetcontrols::text, '"valueRelation": {', '"valueRelation":{"nullValue":false, '))::json 
+WHERE widgetcontrols->>'valueRelation' IS NOT NULL;
+
+UPDATE config_form_fields SET widgetcontrols = (replace(widgetcontrols::text, '"nullValue":false', '"nullValue":true'))::json 
+WHERE widgetcontrols->>'valueRelation' IS NOT NULL AND formname IN ('v_edit_inp_reservoir', 'v_edit_inp_dscenario_reservoir') AND columnname IN('pattern_id');
+
+UPDATE config_form_fields SET widgetcontrols = (replace(widgetcontrols::text, '"nullValue":false', '"nullValue":true'))::json 
+WHERE widgetcontrols->>'valueRelation' IS NOT NULL AND formname IN ('v_edit_inp_tank', 'v_edit_inp_dscenario_tank') AND columnname IN('pattern_id', 'curve_id');
+
+UPDATE config_form_fields SET widgetcontrols = (replace(widgetcontrols::text, '"nullValue":false', '"nullValue":true'))::json 
+WHERE widgetcontrols->>'valueRelation' IS NOT NULL AND formname IN ('v_edit_inp_inlet') AND columnname IN('pattern_id', 'curve_id');
+
+UPDATE config_form_fields SET widgetcontrols = (replace(widgetcontrols::text, '"nullValue":false', '"nullValue":true'))::json 
+WHERE widgetcontrols->>'valueRelation' IS NOT NULL AND formname IN ('v_edit_inp_junction', 'v_edit_inp_dscenario_junction') AND columnname IN('pattern_id');
+
+UPDATE config_form_fields SET widgetcontrols = (replace(widgetcontrols::text, '"nullValue":false', '"nullValue":true'))::json 
+WHERE widgetcontrols->>'valueRelation' IS NOT NULL AND formname IN ('v_edit_inp_pump', 'v_edit_inp_dscenario_pump', 'inp_pump_additional') AND columnname IN('pattern');
+
+UPDATE config_form_fields SET widgetcontrols = (replace(widgetcontrols::text, '"nullValue":false', '"nullValue":true'))::json 
+WHERE widgetcontrols->>'valueRelation' IS NOT NULL AND formname IN ('v_edit_inp_valve', 'v_edit_inp_dscenario_valve', 'v_edit_inp_virtualvalve') AND columnname IN('curve_id');
