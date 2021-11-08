06d874300874794d8a6fe5b30761d5012eea572e
Sergi Maspons <smaspons@bgeo.es>
2021-11-08 12:48:59 +0100

    Minor enhancement: reload feature_cat list on click of arc/point add_btn

--- core/toolbars/edit/arc_add_button.py
+++ core/toolbars/edit/arc_add_button.py
@@ -61,6 +61,9 @@ class GwArcAddButton(GwAction):
             del action
         action_group = self.action.property('action_group')
 
+        # Update featurecatvalues
+        global_vars.feature_cat = tools_gw.manage_feature_cat()
+
         # Get list of different arc types
         features_cat = global_vars.feature_cat
         if features_cat is not None:
