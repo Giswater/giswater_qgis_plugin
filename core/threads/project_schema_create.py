165c0126e7a222859645bcdb3844cbb784b4aec8
David Erill <daviderill79@gmail.com>
2021-11-16 12:03:16 +0100

    Split execution in two functions: main and custom

--- core/threads/project_schema_create.py
+++ core/threads/project_schema_create.py
@@ -9,11 +9,10 @@ from qgis.PyQt.QtCore import pyqtSignal
 
 from .task import GwTask
 from ..utils import tools_gw
-from ...lib import tools_qt
+from ...lib import tools_qt, tools_log
 
 
 class GwCreateSchemaTask(GwTask):
-    """ This shows how to subclass QgsTask """
 
     task_finished = pyqtSignal(list)
 
@@ -25,75 +24,20 @@ class GwCreateSchemaTask(GwTask):
 
 
     def run(self):
-        """ Automatic mincut: Execute function 'gw_fct_mincut' """
 
         super().run()
         self.admin.dlg_readsql_create_project.btn_cancel_task.show()
         self.admin.dlg_readsql_create_project.btn_accept.hide()
         self.is_test = self.params['is_test']
-        project_type = self.params['project_type']
-        exec_last_process = self.params['exec_last_process']
-        project_name_schema = self.params['project_name_schema']
-        project_locale = self.params['project_locale']
-        project_srid = self.params['project_srid']
-        example_data = self.params['example_data']
-
         self.finish_execution = {'import_data': False}
+
         try:
-            # Common execution
-            status = self.admin._load_base(project_type=project_type)
-            if not status and self.admin.dev_commit is False:
+            status = self.main_execution()
+            if not status:
+                tools_log.log_info("Function main_execution returned False")
                 return False
 
-            self.set_progress(10)
-            status = self.admin._update_30to31(new_project=True, project_type=project_type)
-            if not status and self.admin.dev_commit is False:
-                return False
-
-            self.set_progress(20)
-            status = self.admin._load_views(project_type=project_type)
-            if not status and self.admin.dev_commit is False:
-                return False
-
-            self.set_progress(30)
-            status = self.admin._load_trg(project_type=project_type)
-            if not status and self.admin.dev_commit is False:
-                return False
-
-            self.set_progress(40)
-            status = self.admin._update_31to39(new_project=True, project_type=project_type)
-            if not status and self.admin.dev_commit is False:
-                return False
-
-            self.set_progress(60)
-
-            status = True
-            if exec_last_process:
-                status = self.admin._execute_last_process(True, project_name_schema, self.admin.schema_type,
-                                                          project_locale, project_srid)
-
-            if not status and self.admin.dev_commit is False:
-                return False
-
-            # Custom execution
-            if self.admin.rdb_import_data.isChecked():
-                self.finish_execution['import_data'] = True
-                # TODO:
-                self.set_progress(90)
-                return True
-            elif self.admin.rdb_sample.isChecked() and example_data:
-                self.set_progress(80)
-                tools_gw.set_config_parser('btn_admin', 'create_schema_type', 'rdb_sample', prefix=False)
-                self.admin._load_sample_data(project_type=project_type)
-            elif self.admin.rdb_sample_dev.isChecked():
-                self.set_progress(80)
-                tools_gw.set_config_parser('btn_admin', 'create_schema_type', 'rdb_sample_dev', prefix=False)
-                self.admin._load_sample_data(project_type=project_type)
-                self.set_progress(90)
-                self.admin._load_dev_data(project_type=project_type)
-            elif self.admin.rdb_data.isChecked():
-                tools_gw.set_config_parser('btn_admin', 'create_schema_type', 'rdb_data', prefix=False)
-
+            self.custom_execution()
             return True
 
         except KeyError as e:
@@ -136,4 +80,74 @@ class GwCreateSchemaTask(GwTask):
 
         if not self.is_test:
             self.setProgress(value)
+
+
+    def main_execution(self):
+        """ Main common execution """
+
+        project_type = self.params['project_type']
+        exec_last_process = self.params['exec_last_process']
+        project_name_schema = self.params['project_name_schema']
+        project_locale = self.params['project_locale']
+        project_srid = self.params['project_srid']
+
+        status = self.admin._load_base(project_type=project_type)
+        if not status and self.admin.dev_commit is False:
+            return False
+
+        self.set_progress(10)
+        status = self.admin._update_30to31(new_project=True, project_type=project_type)
+        if not status and self.admin.dev_commit is False:
+            return False
+
+        self.set_progress(20)
+        status = self.admin._load_views(project_type=project_type)
+        if not status and self.admin.dev_commit is False:
+            return False
+
+        self.set_progress(30)
+        status = self.admin._load_trg(project_type=project_type)
+        if not status and self.admin.dev_commit is False:
+            return False
+
+        self.set_progress(40)
+        status = self.admin._update_31to39(new_project=True, project_type=project_type)
+        if not status and self.admin.dev_commit is False:
+            return False
+
+        self.set_progress(60)
+
+        status = True
+        if exec_last_process:
+            status = self.admin._execute_last_process(True, project_name_schema, self.admin.schema_type,
+                                                      project_locale, project_srid)
+
+        if not status and self.admin.dev_commit is False:
+            return False
+
+        return True
+
+
+    def custom_execution(self):
+        """ Custom execution """
+
+        example_data = self.params['example_data']
+
+        if self.admin.rdb_import_data.isChecked():
+            self.finish_execution['import_data'] = True
+            # TODO:
+            self.set_progress(90)
+            return True
+        elif self.admin.rdb_sample.isChecked() and example_data:
+            self.set_progress(80)
+            tools_gw.set_config_parser('btn_admin', 'create_schema_type', 'rdb_sample', prefix=False)
+            self.admin._load_sample_data(project_type=project_type)
+        elif self.admin.rdb_sample_dev.isChecked():
+            self.set_progress(80)
+            tools_gw.set_config_parser('btn_admin', 'create_schema_type', 'rdb_sample_dev', prefix=False)
+            self.admin._load_sample_data(project_type=project_type)
+            self.set_progress(90)
+            self.admin._load_dev_data(project_type=project_type)
+        elif self.admin.rdb_data.isChecked():
+            tools_gw.set_config_parser('btn_admin', 'create_schema_type', 'rdb_data', prefix=False)
 
