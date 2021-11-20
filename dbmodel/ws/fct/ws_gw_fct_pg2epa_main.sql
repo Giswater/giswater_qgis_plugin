9b6be2ba8aad40847539f7f3e32195a853556743
xtorret@bgeo.es <xtorret@bgeo.es>
2021-11-20 19:49:33 +0100

    Bug fix recovering call for check data function

--- dbmodel/ws/fct/ws_gw_fct_pg2epa_main.sql
+++ dbmodel/ws/fct/ws_gw_fct_pg2epa_main.sql
@@ -130,10 +130,8 @@ BEGIN
 	PERFORM gw_fct_pg2epa_autorepair_epatype($${"client":{"device":4, "infoType":1, "lang":"ES"}}$$);
 
 	RAISE NOTICE '2 - check system data';
-	IF v_checkdata THEN
-		PERFORM gw_fct_pg2epa_check_data(v_input);
-	END IF;
-
+	PERFORM gw_fct_pg2epa_check_data(v_input);
+	
 	RAISE NOTICE '3 - Fill temp tables';
 	PERFORM gw_fct_pg2epa_fill_data(v_result);
 
