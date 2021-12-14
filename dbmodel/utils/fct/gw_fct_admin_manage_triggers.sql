860223cf3725dc9fc3ea55be08b4913826d06393
Barbara Rzepka <bbrzepka@bgeo.es>
2021-12-14 15:47:51 +0100

    fct manage trg - recreate typevalue trg only for tables

--- dbmodel/utils/fct/gw_fct_admin_manage_triggers.sql
+++ dbmodel/utils/fct/gw_fct_admin_manage_triggers.sql
@@ -75,9 +75,12 @@ BEGIN
 		FOR v_table IN SELECT * FROM sys_foreignkey WHERE active IS TRUE
 		LOOP
 			RAISE NOTICE ' v_table %', v_table;
-			EXECUTE 'DROP TRIGGER IF EXISTS gw_trg_typevalue_fk ON '||v_table.target_table||';';
-			EXECUTE 'CREATE TRIGGER gw_trg_typevalue_fk AFTER INSERT OR UPDATE ON '||v_table.target_table||'
-			FOR EACH ROW EXECUTE PROCEDURE gw_trg_typevalue_fk('''||v_table.target_table||''');';
+			IF (SELECT table_type FROM information_schema.tables WHERE  table_schema = 'ws_sample35' 
+			AND table_name = v_table.target_table) ='BASE TABLE' THEN
+				EXECUTE 'DROP TRIGGER IF EXISTS gw_trg_typevalue_fk ON '||v_table.target_table||';';
+				EXECUTE 'CREATE TRIGGER gw_trg_typevalue_fk AFTER INSERT OR UPDATE ON '||v_table.target_table||'
+				FOR EACH ROW EXECUTE PROCEDURE gw_trg_typevalue_fk('''||v_table.target_table||''');';
+			END IF;
 		END LOOP;
 
 	ELSIF p_action = 'fk' AND p_table IS NOT NULL AND p_table ='CHECK' THEN
