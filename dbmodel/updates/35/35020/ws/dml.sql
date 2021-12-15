e888420bf98660ea2523321987585b29eb777cf0
xtorret@bgeo.es <xtorret@bgeo.es>
2021-12-15 08:40:04 +0100

    Arrange values on sys_table, adding v_edit_inp_curve and v_edit_inp_pattern among others

--- dbmodel/updates/35/35020/ws/dml.sql
+++ dbmodel/updates/35/35020/ws/dml.sql
@@ -12,3 +12,21 @@ ALTER TABLE inp_typevalue DISABLE TRIGGER gw_trg_typevalue_config_fk;
 UPDATE inp_typevalue SET typevalue = '_inp_value_demandtype' WHERE typevalue = 'inp_value_demandtype' and id = '3';
 UPDATE inp_typevalue SET typevalue = '_inp_value_patternmethod' WHERE typevalue = 'inp_value_patternmethod' and id = '24';
 ALTER TABLE inp_typevalue ENABLE TRIGGER gw_trg_typevalue_config_fk;
+
+INSERT INTO sys_table(id, descript, sys_role, sys_criticity)
+VALUES ('v_edit_inp_curve', 'View to edit curves, filteder by sector_id','role_epa', 0)
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO sys_table(id, descript, sys_role, sys_criticity)
+VALUES ('v_edit_inp_curve_value', 'View to edit curve values, filteder by sector_id','role_epa', 0)
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO sys_table(id, descript, sys_role, sys_criticity)
+VALUES ('v_edit_inp_pattern', 'View to edit patterns, filteder by sector_id','role_epa', 0)
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO sys_table(id, descript, sys_role, sys_criticity)
+VALUES ('v_edit_inp_pattern_value', 'View to edit curve values, filteder by sector_id','role_epa', 0)
+ON CONFLICT (id) DO NOTHING;
+
+DELETE FROM sys_table WHERE id IN('v_inp_curve', 'v_inp_pattern');
