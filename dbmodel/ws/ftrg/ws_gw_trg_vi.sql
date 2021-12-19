106e1a2a6335787398207e9f3d2ecd032b5b8b0d
xtorret@bgeo.es <xtorret@bgeo.es>
2021-12-19 19:00:07 +0100

    Bug fix when importing PSV and PBV valves for settings vaules

--- dbmodel/ws/ftrg/ws_gw_trg_vi.sql
+++ dbmodel/ws/ftrg/ws_gw_trg_vi.sql
@@ -66,7 +66,7 @@ BEGIN
 	    INSERT INTO arc (arc_id, node_1, node_2, arccat_id, epa_type, sector_id, dma_id, expl_id, state, state_type) 
 	    VALUES (NEW.arc_id, NEW.node_1, NEW.node_2, concat('ARC',NEW.valv_type),'VALVE-IMPORTINP',1,1,1,1,(SELECT id FROM value_state_type WHERE state=1 LIMIT 1));
 	    INSERT INTO inp_valve_importinp (arc_id, diameter, valv_type, minorloss) VALUES (NEW.arc_id,NEW.diameter, NEW.valv_type, NEW.minorloss);
-	      IF NEW.valv_type='PRV' THEN
+	      IF NEW.valv_type IN ('PRV','PSV','PBV') THEN
 		UPDATE inp_valve_importinp SET pressure=NEW.setting::numeric WHERE arc_id=NEW.arc_id;
 	      ELSIF NEW.valv_type='FCV' THEN 
 		UPDATE inp_valve_importinp SET flow=NEW.setting::numeric WHERE arc_id=NEW.arc_id;
