34be8cf87fcce14f2b056ea51c069bd4dce9084a
xtorret@bgeo.es <xtorret@bgeo.es>
2021-12-22 20:28:49 +0100

    New colum parent_id for sector

--- dbmodel/ud/ftrg/ud_gw_trg_edit_sector.sql
+++ dbmodel/ud/ftrg/ud_gw_trg_edit_sector.sql
@@ -12,47 +12,90 @@ CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_edit_sector()
   RETURNS trigger AS
 $BODY$
 DECLARE 
-
+v_querytext text;
+v_row record;
+v_sector_id integer[];
 
 BEGIN
 
-    EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
-
+	EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
 	
 	IF TG_OP = 'INSERT' THEN
 
-	IF NEW.sector_id IS NULL THEN
-		NEW.sector_id=(SELECT nextval('SCHEMA_NAME.sector_sector_id_seq'::regclass));
-	END IF;
-				
-			
-        -- FEATURE INSERT
-			INSERT INTO sector (sector_id, name, descript, macrosector_id, the_geom, undelete, active)
-			VALUES (NEW.sector_id, NEW.name, NEW.descript, NEW.macrosector_id, NEW.the_geom, NEW.undelete, NEW.active);
-	
-			INSERT INTO selector_sector (sector_id, cur_user) VALUES (NEW.sector_id, current_user);
-		
-		RETURN NEW;
-		
+		IF NEW.sector_id IS NULL THEN
+			NEW.sector_id=(SELECT nextval('SCHEMA_NAME.sector_sector_id_seq'::regclass));
+		END IF;
+					
+		INSERT INTO sector (sector_id, name, descript, macrosector_id, the_geom, undelete, active, parent_id)
+		VALUES (NEW.sector_id, NEW.name, NEW.descript, NEW.macrosector_id, NEW.the_geom, NEW.undelete, NEW.active, NEW.parent_id);
 
-    ELSIF TG_OP = 'UPDATE' THEN
-   -- FEATURE UPDATE		
-			UPDATE sector 
-			SET sector_id=NEW.sector_id, name=NEW.name, descript=NEW.descript, macrosector_id=NEW.macrosector_id, the_geom=NEW.the_geom, 
-			undelete=NEW.undelete, active=NEW.active
-			WHERE sector_id=OLD.sector_id;
-				
-        RETURN NEW;
+	ELSIF TG_OP = 'UPDATE' THEN
 
+		UPDATE sector 
+		SET sector_id=NEW.sector_id, name=NEW.name, descript=NEW.descript, macrosector_id=NEW.macrosector_id, the_geom=NEW.the_geom, 
+		undelete=NEW.undelete, active=NEW.active
+		WHERE sector_id=OLD.sector_id;
 
-     ELSIF TG_OP = 'DELETE' THEN  
-	-- FEATURE DELETE 
-		DELETE FROM sector WHERE sector_id = OLD.sector_id;
-			
-		RETURN NULL;
+	ELSIF TG_OP = 'DELETE' THEN  
      
-     END IF;
+		DELETE FROM sector WHERE sector_id = OLD.sector_id;		
+	END IF;
 
+	-- manage admin_exploitation_x_user
+	IF (SELECT value::boolean FROM config_param_system WHERE parameter = 'admin_exploitation_x_user') THEN
+
+		IF TG_OP = 'INSERT' THEN
+		
+			v_querytext = 'SELECT * FROM cat_manager WHERE id IN (SELECT id FROM (SELECT id, unnest(sector_id) sector_id FROM cat_manager)a WHERE sector_id = '||NEW.parent_id||')';
+			FOR v_row IN EXECUTE v_querytext
+			LOOP
+				v_sector_id = array_append (v_row.sector_id, NEW.sector_id);
+				UPDATE cat_manager SET sector_id = v_sector_id WHERE id = v_row.id;
+			END LOOP;	
+			INSERT INTO selector_sector VALUES (NEW.sector_id, current_user);
+			
+			RETURN NEW;
+
+		ELSIF TG_OP = 'UPDATE' THEN
+
+			IF (NEW.parent_id <> OLD.parent_id) OR (NEW.parent_id IS NULL AND OLD.parent_id IS NOT NULL) OR (OLD.parent_id IS NULL AND NEW.parent_id IS NOT NULL) THEN
+
+				v_querytext = 'SELECT * FROM cat_manager WHERE id IN (SELECT id FROM (SELECT id, unnest(sector_id) sector_id FROM cat_manager)a WHERE sector_id = '||OLD.sector_id||')';
+				FOR v_row IN EXECUTE v_querytext
+				LOOP
+					v_sector_id = array_remove (v_row.sector_id, OLD.sector_id);
+					UPDATE cat_manager SET sector_id = v_sector_id WHERE id = v_row.id;
+				END LOOP;
+			
+				v_querytext = 'SELECT * FROM cat_manager WHERE id IN (SELECT id FROM (SELECT id, unnest(sector_id) sector_id FROM cat_manager)a WHERE sector_id = '||NEW.parent_id||')';
+				FOR v_row IN EXECUTE v_querytext
+				LOOP
+					v_sector_id = array_append (v_row.sector_id, NEW.sector_id);
+					UPDATE cat_manager SET sector_id = v_sector_id WHERE id = v_row.id;
+				END LOOP;	
+			END IF;
+			
+			RETURN NEW;
+		ELSE
+			v_querytext = 'SELECT * FROM cat_manager WHERE id IN (SELECT id FROM (SELECT id, unnest(sector_id) sector_id FROM cat_manager)a WHERE sector_id = '||OLD.sector_id||')';
+			FOR v_row IN EXECUTE v_querytext
+			LOOP
+				v_sector_id = array_remove (v_row.sector_id, OLD.sector_id);
+				UPDATE cat_manager SET sector_id = v_sector_id WHERE id = v_row.id;
+			END LOOP;
+			RETURN NULL;	
+		END IF;
+	ELSE 
+		IF TG_OP ='INSERT'THEN
+			INSERT INTO selector_sector VALUES (NEW.sector_id, current_user);
+			RETURN NEW;
+			
+		ELSIF TG_OP ='UPDATE' THEN
+			RETURN NEW;
+		ELSE
+			RETURN NULL;
+		END IF;	
+	END IF;
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
---
+++ dbmodel/updates/35/35021/ud/ddlview.sql
@@ -0,0 +1,21 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+--2021/12/22
+CREATE OR REPLACE VIEW v_edit_sector AS 
+ SELECT sector.sector_id,
+    sector.name,
+    sector.descript,
+    sector.macrosector_id,
+    sector.the_geom,
+    sector.undelete,
+    sector.active,
+    sector.parent_id
+   FROM selector_sector, sector
+  WHERE sector.sector_id = selector_sector.sector_id AND selector_sector.cur_user = "current_user"()::text;
