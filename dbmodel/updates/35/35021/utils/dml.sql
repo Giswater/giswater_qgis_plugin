fdb3a2b6a5f6b57fc73208dafca03abb51a7e261
xtorret@bgeo.es <xtorret@bgeo.es>
2022-01-01 13:40:04 +0100

    Fill column source

---
+++ dbmodel/updates/35/35021/utils/dml.sql
@@ -0,0 +1,24 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+--2021/12/22
+UPDATE sys_param_user SET source = 'core' WHERE source IS NULL;
+UPDATE sys_param_user SET source = 'core' WHERE source ='giswater';
+
+UPDATE sys_fprocess SET source = 'core' WHERE source IS NULL;
+UPDATE sys_fprocess SET source = 'core' WHERE source ='giswater';
+
+UPDATE sys_function SET source = 'core' WHERE source IS NULL;
+UPDATE sys_function SET source = 'core' WHERE source ='giswater';
+
+UPDATE sys_message SET source = 'core' WHERE source IS NULL;
+UPDATE sys_message SET source = 'core' WHERE source ='giswater';
+
+UPDATE sys_table SET source = 'core' WHERE source IS NULL;
+UPDATE sys_table SET source = 'core' WHERE source ='giswater';
