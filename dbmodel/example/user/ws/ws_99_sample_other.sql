0bff504d639fe715d78d84904b705f11c85bd5de
xtorret@bgeo.es <xtorret@bgeo.es>
2022-01-03 12:00:04 +0100

    Ehnance vdefault for sample

--- dbmodel/example/user/ws/ws_99_sample_other.sql
+++ dbmodel/example/user/ws/ws_99_sample_other.sql
@@ -629,3 +629,5 @@ INSERT INTO inp_connec VALUES ('114464',0.5,'PTN-CONNEC');
 UPDATE inp_dscenario_demand SET feature_type  ='NODE', pattern_id = 'PTN-HYDRANT', demand_type = 'HYDRANT' , source = concat('NODE ', feature_id);
 
 UPDATE inp_junction SET demand = 0.01 ;
+
+INSERT INTO config_param_user VALUES ('edit_pavement_vdefault','Asphalt',current_user);
