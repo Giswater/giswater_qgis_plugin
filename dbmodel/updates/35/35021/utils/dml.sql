21bdbbc79f79441cc5af25a02b5d22c681538794
xtorret@bgeo.es <xtorret@bgeo.es>
2022-01-04 11:02:24 +0100

    Bug fix from prev commit

--- dbmodel/updates/35/35021/utils/dml.sql
+++ dbmodel/updates/35/35021/utils/dml.sql
@@ -31,7 +31,48 @@ UPDATE config_toolbox SET inputparams =
 WHERE id = 3042;
 
 
+
 --2022/01/03
+UPDATE sys_table SET context = NULL, alias = NULL, orderby = null;
+UPDATE sys_table SET context = '{"level_1":"CATALOG"}', alias = 'Feature catalog' , orderby=1 WHERE id ='cat_feature';
+UPDATE sys_table SET context = '{"level_1":"CATALOG"}', alias = 'Node feature catalog' , orderby=2 WHERE id ='cat_feature_node';
+UPDATE sys_table SET context = '{"level_1":"CATALOG"}', alias = 'Node material catalog' , orderby=3 WHERE id ='cat_mat_node';
+UPDATE sys_table SET context = '{"level_1":"CATALOG"}', alias = 'Node catalog' , orderby=4 WHERE id ='cat_node';
+UPDATE sys_table SET context = '{"level_1":"CATALOG"}', alias = 'Arc material catalog' , orderby=5 WHERE id ='cat_mat_arc';
+UPDATE sys_table SET context = '{"level_1":"CATALOG"}', alias = 'Arc catalog' , orderby=6 WHERE id ='cat_arc';
+UPDATE sys_table SET context = '{"level_1":"CATALOG"}', alias = 'Connec catalog' , orderby=7 WHERE id ='cat_connec';
+UPDATE sys_table SET context = '{"level_1":"CATALOG"}', alias = 'Grate catalog' , orderby=8 WHERE id ='cat_grate';
+
+UPDATE sys_table SET context = '{"level_1":"MAPZONE"}', alias = 'Exploitation', orderby=1 WHERE id= 'v_edit_exploitation';
+UPDATE sys_table SET context = '{"level_1":"MAPZONE"}', alias = 'Sector', orderby=2 WHERE id= 'v_edit_sector';
+UPDATE sys_table SET context = '{"level_1":"MAPZONE"}', alias = 'DMA' , orderby=3 WHERE id= 'v_edit_dma';
+UPDATE sys_table SET context = '{"level_1":"MAPZONE"}', alias = 'Presszone', orderby=4 WHERE id= 'v_edit_presszone';
+UPDATE sys_table SET context = '{"level_1":"MAPZONE"}', alias = 'DQA' , orderby=5 WHERE id= 'v_edit_dqa';
+
+UPDATE sys_table SET context = '{"level_1":"NETWORK", "level_2":"ARC"}', alias = initcap(cat_feature.id) FROM cat_feature WHERE sys_table.id = cat_feature.child_layer AND feature_type = 'ARC';
+UPDATE sys_table SET context = '{"level_1":"NETWORK", "level_2":"NODE"}', alias = initcap(cat_feature.id) FROM cat_feature  WHERE sys_table.id = cat_feature.child_layer AND feature_type = 'NODE';
+UPDATE sys_table SET context = '{"level_1":"NETWORK", "level_2":"CONNEC"}', alias = initcap(cat_feature.id) FROM cat_feature WHERE sys_table.id = cat_feature.child_layer AND feature_type = 'CONNEC';
+UPDATE sys_table SET context = '{"level_1":"NETWORK", "level_2":"GULLY"}', alias = initcap(cat_feature.id)  FROM cat_feature WHERE sys_table.id = cat_feature.child_layer AND feature_type = 'GULLY';
+
+UPDATE sys_table SET context = '{"level_1":"O&M"}' WHERE sys_role = 'role_om';
+
+UPDATE sys_table SET context = '{"level_1":"EPA", "level_2":"CATALOG"}' , alias = 'Hydrology catalog' , orderby=1 WHERE id ='v_edit_inp_hydrology';
+UPDATE sys_table SET context = '{"level_1":"EPA", "level_2":"CATALOG"}' , alias = 'DWF catalog' , orderby=2 WHERE id ='v_edit_inp_dwf';
+UPDATE sys_table SET context = '{"level_1":"EPA", "level_2":"CATALOG"}' , alias = 'Dscenario catalog' , orderby=3 WHERE id ='v_edit_cat_dscenario';
+UPDATE sys_table SET context = '{"level_1":"EPA", "level_2":"CATALOG"}' , alias = 'Roughness catalog' , orderby=4 WHERE id ='cat_mat_roughness';
+UPDATE sys_table SET context = '{"level_1":"EPA", "level_2":"CATALOG"}' , alias = 'Curve catalog' , orderby=5 WHERE id ='v_edit_inp_curve';
+UPDATE sys_table SET context = '{"level_1":"EPA", "level_2":"CATALOG"}' , alias = 'Pattern catalog' , orderby=6 WHERE id ='v_edit_inp_pattern';
+UPDATE sys_table SET context = '{"level_1":"EPA", "level_2":"CATALOG"}' , alias = 'Timeseries catalog' , orderby=7 WHERE id ='v_edit_inp_timeseries';
+
+UPDATE sys_table SET context = '{"level_1":"EPA", "level_2":"INPUT"}' , alias = initcap(s.id) FROM sys_feature_epa_type s WHERE sys_table.id = concat('v_edit_',epa_table);
+UPDATE sys_table SET context = '{"level_1":"EPA", "level_2":"RESULT"}', alias = initcap(replace(id,'v_rpt_','')) WHERE id like 'v_rpt%' and id not in (SELECT id FROM sys_table WHERE id like 'v_rpt_comp%');
+
+
+UPDATE sys_table SET context = '{"level_1":"PLAN"}' , alias = 'Plan psector', orderby = 1 WHERE id = 'v_edit_plan_psector';
+UPDATE sys_table SET context = '{"level_1":"PLAN"}' , alias = 'Plan current psector', orderby = 2 WHERE id = 'v_plan_current_psector';
+UPDATE sys_table SET context = '{"level_1":"PLAN"}' , alias = 'Arc pavement', orderby = 3 WHERE id = 'plan_arc_x_pavement';
+
+
 INSERT INTO config_typevalue VALUES ('sys_table_context','CATALOG', null, '{"orderBy":1}');
 INSERT INTO config_typevalue VALUES ('sys_table_context','MAPZONE', null, '{"orderBy":2}');
 INSERT INTO config_typevalue VALUES ('sys_table_context','NETWORK', null, '{"orderBy":3}');
@@ -41,25 +82,6 @@ INSERT INTO config_typevalue VALUES ('sys_table_context','PLAN', null, '{"orderB
 INSERT INTO config_typevalue VALUES ('sys_table_context','ADMIN', null, '{"orderBy":7}');
 
 
-UPDATE sys_table SET context = NULL, alias = NULL, orderby = null;
-UPDATE sys_table SET context = '{"level_1":"CATALOG"}', alias = upper(id) WHERE id like 'cat_%' AND sys_role !=  'role_epa';
-UPDATE sys_table SET context = '{"level_1":"MAPZONE"}', alias = 'EXPLOITATION' WHERE id= 'v_edit_exploitation';
-UPDATE sys_table SET context = '{"level_1":"MAPZONE"}', alias = 'SECTOR' WHERE id= 'v_edit_sector';
-UPDATE sys_table SET context = '{"level_1":"MAPZONE"}', alias = 'DMA' WHERE id= 'v_edit_dma';
-UPDATE sys_table SET context = '{"level_1":"MAPZONE"}', alias = 'PRESSZONE' WHERE id= 'v_edit_presszone';
-UPDATE sys_table SET context = '{"level_1":"MAPZONE"}', alias = 'DQA' WHERE id= 'v_edit_dqa';
-UPDATE sys_table SET context = '{"level_1":"NETWORK", "level_2":"ARC"}', alias = cat_feature.id FROM cat_feature WHERE sys_table.id = cat_feature.child_layer AND feature_type = 'ARC';
-UPDATE sys_table SET context = '{"level_1":"NETWORK", "level_2":"NODE"}', alias = cat_feature.id FROM cat_feature  WHERE sys_table.id = cat_feature.child_layer AND feature_type = 'NODE';
-UPDATE sys_table SET context = '{"level_1":"NETWORK", "level_2":"CONNEC"}', alias = cat_feature.id FROM cat_feature WHERE sys_table.id = cat_feature.child_layer AND feature_type = 'CONNEC';
-UPDATE sys_table SET context = '{"level_1":"NETWORK", "level_2":"GULLY"}', alias = cat_feature.id  FROM cat_feature WHERE sys_table.id = cat_feature.child_layer AND feature_type = 'GULLY';
-UPDATE sys_table SET context = '{"level_1":"O&M"}' WHERE sys_role = 'role_om';
-UPDATE sys_table SET context = '{"level_1":"EPA", "level_2":"CATALOG"}' WHERE id like 'cat_%' AND sys_role = 'role_epa';
-UPDATE sys_table SET context = '{"level_1":"EPA", "level_2":"INPUT"}' , alias = s.id FROM sys_feature_epa_type s WHERE sys_table.id = concat('v_edit_',epa_table);
-UPDATE sys_table SET context = '{"level_1":"EPA", "level_2":"RESULT"}', alias = id WHERE id like 'v_rpt_%' AND sys_role = 'role_epa';
-UPDATE sys_table SET context = '{"level_1":"PLAN"}' , alias = id WHERE sys_role = 'role_plan';
-UPDATE sys_table SET context = '{"level_1":"ADMIN"}', alias = id WHERE sys_role = 'role_admin';
-
-
 INSERT INTO config_form_fields VALUES ('search', 'form_feature', 'main', 'hydro_contains', 'lyt_data_1', NULL, 'string', 'nowidget', 'Use contains', 'Check: allow to search using any of the characters. Unchecked: the search engine only will return results when matching the first characters (useful for short numbers)', NULL, FALSE, FALSE, FALSE, FALSE, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '{"setMultiline":false}',NULL,NULL, FALSE) ON CONFLICT (formname, formtype, columnname, tabname) DO NOTHING;
 
 INSERT INTO config_form_fields(formname, formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label, tooltip, placeholder, ismandatory, 
@@ -67,4 +89,5 @@ isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_is
 widgetcontrols, widgetfunction, linkedobject, hidden)
 VALUES ('v_edit_sector','form_feature', 'main', 'parent_id', null, null, 'string', 'combo', 'parent_id', NULL, NULL,  FALSE,
 FALSE, TRUE, FALSE,FALSE,'SELECT sector_id as id,name as idval FROM sector WHERE sector_id IS NOT NULL AND active IS TRUE ', TRUE, TRUE, NULL, NULL,NULL,
-'{"setMultiline":false}', NULL, NULL, FALSE) ON CONFLICT (formname, formtype, columnname, tabname) DO NOTHING;
+'{"setMultiline": false, "valueRelation":{"nullValue":false, "layer": "v_edit_sector", "activated": true, "keyColumn": "sector_id", "valueColumn": "name", "filterExpression": null}}', 
+NULL, NULL, FALSE) ON CONFLICT (formname, formtype, columnname, tabname) DO NOTHING;
