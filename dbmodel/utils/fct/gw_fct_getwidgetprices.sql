5c4d264f138604795ab42cf18c656cc7a6c36625
Edgar Fuste <efuste@bgeo.es>
2022-01-19 12:48:55 +0100

    Improve last commit

--- dbmodel/utils/fct/gw_fct_getwidgetprices.sql
+++ dbmodel/utils/fct/gw_fct_getwidgetprices.sql
@@ -41,7 +41,7 @@ BEGIN
 	USING v_schemaname, v_tablename;
 
 	-- Get table rows
-	EXECUTE 'SELECT array_agg(row_to_json(a)) FROM (SELECT id, price_id, unit, price_descript, price, measurement, total_budget, descript FROM '||v_tablename||' WHERE psector_id = '||v_psector_id||')a ' 
+	EXECUTE 'SELECT array_agg(row_to_json(a)) FROM (SELECT id, price_id, unit, price_descript, price, measurement, total_budget, observ FROM '||v_tablename||' WHERE psector_id = '||v_psector_id||')a ' 
 	INTO v_fields_array;
 
 	v_columns := array_to_json(v_columns_array);
