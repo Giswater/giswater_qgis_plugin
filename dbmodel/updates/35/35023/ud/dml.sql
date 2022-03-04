221cc78280319853a93ad6f8a0038b626fcc6893
Barbara Rzepka <bbrzepka@bgeo.es>
2022-03-04 13:36:06 +0100

    update config_form_fields for lidco - ud

--- dbmodel/updates/35/35023/ud/dml.sql
+++ dbmodel/updates/35/35023/ud/dml.sql
@@ -10,3 +10,7 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 --2022/01/31
 UPDATE sys_table SET id = 'vi_subcatch2outlet' WHERE id = 'vi_subcatch2node';
 UPDATE sys_table SET sys_role='role_edit' WHERE id='inp_gully';
+
+UPDATE config_form_fields SET widgettype='combo', dv_querytext='SELECT lidco_id AS id, lidco_id as idval FROM inp_lid WHERE lidco_id IS NOT NULL',
+widgetcontrols='{"setMultiline":false,"valueRelation":{"nullValue":true, "layer": "inp_lid", "activated": true, "keyColumn": "lidco_id", "valueColumn": "lidco_id", "filterExpression":""}}'::json
+WHERE columnname='lidco_id' AND formname='inp_lid_value';
