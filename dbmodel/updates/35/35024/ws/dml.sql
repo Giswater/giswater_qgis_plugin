756d2a6cc938665e25c7d1126d42404e59f9a7e3
Barbara Rzepka <bbrzepka@bgeo.es>
2022-04-07 16:59:44 +0200

    inp_dscenario_rules & inp_dscenario_controls - config_form_fields

--- dbmodel/updates/35/35024/ws/dml.sql
+++ dbmodel/updates/35/35024/ws/dml.sql
@@ -8,34 +8,25 @@ This version of Giswater is provided by Giswater Association
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 --2022/04/06
-INSERT INTO sys_fprocess(fid, fprocess_name, project_type, parameters, source, isaudit, fprocess_type)
-VALUES ('440', 'Check outlet_id assigned to subcatchments','ud',null, 'core', true, 'Check epa-config')
-ON CONFLICT (fid) DO NOTHING;
-
-UPDATE config_param_system SET parameter='admin_debug' WHERE parameter = 'om_mincut_debug';
-
-INSERT INTO sys_function(id, function_name, project_type, function_type, input_params, return_type, descript, sys_role, sample_query, source)
-VALUES (3140, 'gw_trg_edit_inp_coverage', 'ud', 'trigger function', NULL, 'json', 'Trigger to make editable v_edit_inp_coverage', 'role_epa', NULL, 'core');
-
 INSERT INTO config_form_fields(formname, formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label, tooltip, 
 placeholder, ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, 
 dv_querytext_filterc, stylesheet, widgetcontrols, widgetfunction, linkedobject, hidden)
-SELECT 'v_edit_inp_dscenario_controls', formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label, tooltip, 
+SELECT 'v_edit_inp_dscenario_rules', formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label, tooltip, 
 placeholder, ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, 
 dv_querytext_filterc, stylesheet, widgetcontrols, widgetfunction, linkedobject, hidden
-FROM config_form_fields WHERE formname='v_edit_inp_controls';
+FROM config_form_fields WHERE formname='v_edit_inp_rules';
 
 INSERT INTO config_form_fields(formname, formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label, tooltip, 
 placeholder, ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, 
 dv_querytext_filterc, stylesheet, widgetcontrols, widgetfunction, linkedobject, hidden)
-SELECT 'v_edit_inp_dscenario_controls', formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label, tooltip, 
+SELECT 'v_edit_inp_dscenario_rules', formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label, tooltip, 
 placeholder, ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, 
 dv_querytext_filterc, stylesheet, widgetcontrols, widgetfunction, linkedobject, hidden
 FROM config_form_fields WHERE columnname='dscenario_id' formname='v_edit_inp_dscenario_junction';
 
 INSERT INTO sys_table(id, descript, sys_role,  context, orderby, alias, source)
-VALUES ('inp_dscenario_controls', '"Table to manage scenario for controls"', 'role_epa', null,null,NULL, 'core');
+VALUES ('inp_dscenario_rules', '"Table to manage scenario for rules"', 'role_epa', null,null,NULL, 'core');
 
 INSERT INTO sys_table(id, descript, sys_role,  context, orderby, alias, source)
-VALUES ('v_edit_inp_dscenario_controls', '"Editable view to manage scenario for controls"', 'role_epa',  '{"level_1":"EPA","level_2":"DSCENARIO"}',15, 'Inp coverage', 
+VALUES ('v_edit_inp_dscenario_rules', '"Editable view to manage scenario for rules"', 'role_epa',  '{"level_1":"EPA","level_2":"DSCENARIO"}',16, 'Inp coverage', 
 'core');
--- dbmodel/updates/35/35024/ws/trg.sql
+++ dbmodel/updates/35/35024/ws/trg.sql
@@ -8,9 +8,6 @@ This version of Giswater is provided by Giswater Association
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 --2022/03/07
-CREATE TRIGGER gw_trg_edit_inp_dscenario_rules
-  INSTEAD OF INSERT OR UPDATE OR DELETE
-  ON v_edit_inp_dscenario_rules
-  FOR EACH ROW
-  EXECUTE PROCEDURE gw_trg_edit_inp_dscenario('RULES');
+CREATE TRIGGER gw_trg_edit_inp_dscenario_rules INSTEAD OF INSERT OR UPDATE OR DELETE
+ON v_edit_inp_dscenario_rules FOR EACH ROW EXECUTE PROCEDURE gw_trg_edit_inp_dscenario('RULES');
 
