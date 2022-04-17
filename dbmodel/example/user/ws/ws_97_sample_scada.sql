3e50efedf1e7730b0adbe4258b579524be0115a4
xtorret@bgeo.es <xtorret@bgeo.es>
2022-04-17 19:24:53 +0200

    Minor bug fix from prev commit

--- dbmodel/example/user/ws/ws_97_sample_scada.sql
+++ dbmodel/example/user/ws/ws_97_sample_scada.sql
@@ -20,34 +20,33 @@ INSERT INTO ext_rtc_scada VALUES ('111111', 'SCD-0005', 'SCD-0005','FLOWMETER');
 INSERT INTO ext_rtc_scada VALUES ('1097', 'SCD-0006', 'SCD-0006','FLOWMETER');
 
 -- dma1 (in)
-INSERT INTO ext_rtc_scada_x_data VALUES ('113873', '5', 10120.5, null, 1,1,1);
-INSERT INTO ext_rtc_scada_x_data VALUES ('113873', '6', 10112.2, null, 1,1,1);
-INSERT INTO ext_rtc_scada_x_data VALUES ('113873', '7', 9734.9, null, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('113873', '5', 10120.5, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('113873', '6', 10112.2, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('113873', '7', 9734.9, 1,1,1);
 
 -- dma2 (in)
-INSERT INTO ext_rtc_scada_x_data VALUES ('1080', '5', 9999.5, null, 1,1,1);
-INSERT INTO ext_rtc_scada_x_data VALUES ('1080', '6', 9945.4, null, 1,1,1);
-INSERT INTO ext_rtc_scada_x_data VALUES ('1080', '7', 9215.3, null, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('1080', '5', 9999.5, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('1080', '6', 9945.4, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('1080', '7', 9215.3, 1,1,1);
 
 -- dma3 (in)
-INSERT INTO ext_rtc_scada_x_data VALUES ('113952', '5', 9315.2, null, 1,1,1);
-INSERT INTO ext_rtc_scada_x_data VALUES ('113952', '6', 9454.4, null, 1,1,1);
-INSERT INTO ext_rtc_scada_x_data VALUES ('113952', '7', 9321.3, null, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('113952', '5', 9315.2, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('113952', '6', 9454.4, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('113952', '7', 9321.3, 1,1,1);
 
 -- dm4 (in)
-INSERT INTO ext_rtc_scada_x_data VALUES ('1101', '5', 5000, null, 1,1,1);
-INSERT INTO ext_rtc_scada_x_data VALUES ('1101', '6', 5000, null, 1,1,1);
-INSERT INTO ext_rtc_scada_x_data VALUES ('1101', '7', 5000, null, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('1101', '5', 5000, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('1101', '6', 5000, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('1101', '7', 5000, 1,1,1);
 
-INSERT INTO ext_rtc_scada_x_data VALUES ('1097', '5', 5120.5, null, 1,1,1);
-INSERT INTO ext_rtc_scada_x_data VALUES ('1097', '6', 5112.2, null, 1,1,1);
-INSERT INTO ext_rtc_scada_x_data VALUES ('1097', '7', 4734.9, null, 1,1,1);
-
+INSERT INTO ext_rtc_scada_x_data VALUES ('1097', '5', 5120.5, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('1097', '6', 5112.2, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('1097', '7', 4734.9, 1,1,1);
 
 -- dma5 (in)
-INSERT INTO ext_rtc_scada_x_data VALUES ('111111', '5', 9315.2, null, 1,1,1);
-INSERT INTO ext_rtc_scada_x_data VALUES ('111111', '6', 9454.4, null, 1,1,1);
-INSERT INTO ext_rtc_scada_x_data VALUES ('111111', '7', 9321.3, null, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('111111', '5', 9315.2, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('111111', '6', 9454.4, 1,1,1);
+INSERT INTO ext_rtc_scada_x_data VALUES ('111111', '7', 9321.3, 1,1,1);
 
 
 
