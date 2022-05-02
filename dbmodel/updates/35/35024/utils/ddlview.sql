80a6d0ed6fc04a116a9db65175fb0a19411cabcc
Albert Bofill <abofill@bgeo.es>
2022-05-02 14:20:45 +0200

    Change v_anl_grafanalytics_mapzones for v_anl_grafanalytics_upstream

--- dbmodel/updates/35/35024/utils/ddlview.sql
+++ dbmodel/updates/35/35024/utils/ddlview.sql
@@ -31,8 +31,8 @@ FROM selector_inp_dscenario, inp_dscenario_controls i
 JOIN cat_dscenario d USING (dscenario_id)
 WHERE i.dscenario_id = selector_inp_dscenario.dscenario_id AND selector_inp_dscenario.cur_user = "current_user"()::text;
 
-
- CREATE OR REPLACE VIEW v_anl_grafanalytics_mapzones AS 
+DROP VIEW IF EXISTS v_anl_grafanalytics_mapzones;
+CREATE OR REPLACE VIEW v_anl_grafanalytics_upstream AS 
  SELECT temp_anlgraf.arc_id,
     temp_anlgraf.node_1,
     temp_anlgraf.node_2,
@@ -48,7 +48,7 @@ WHERE i.dscenario_id = selector_inp_dscenario.dscenario_id AND selector_inp_dsce
             temp_anlgraf_1.flag,
             temp_anlgraf_1.checkf,
             temp_anlgraf_1.value,
-	    temp_anlgraf_1.trace
+            temp_anlgraf_1.trace
            FROM temp_anlgraf temp_anlgraf_1
-          WHERE temp_anlgraf_1.water = 1) a2 ON temp_anlgraf.node_1::text = a2.node_2::text
+          WHERE temp_anlgraf_1.water = 1) a2 ON temp_anlgraf.node_2::text = a2.node_1::text
   WHERE temp_anlgraf.flag < 2 AND temp_anlgraf.water = 0 AND a2.flag = 0;
