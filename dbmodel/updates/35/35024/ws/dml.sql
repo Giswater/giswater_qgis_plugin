3b5f494b35b2c38327e2efc6b88ec10a6b195c8e
xtorret@bgeo.es <xtorret@bgeo.es>
2022-05-03 16:54:45 +0200

    Update enable null values for expl column on epa ws catalogs

--- dbmodel/updates/35/35024/ws/dml.sql
+++ dbmodel/updates/35/35024/ws/dml.sql
@@ -160,3 +160,7 @@ DELETE FROM sys_table WHERE id='v_anl_grafanalytics_mapzones';
 INSERT INTO sys_table (id, descript, sys_role, source)
 VALUES ('v_anl_grafanalytics_upstream', 'Table to work with grafanalytics', 'role_epa', 'giswater') 
 ON CONFLICT (id) DO NOTHING;
+
+UPDATE config_form_fields set widgetcontrols = 
+'{"setMultiline": false, "valueRelation":{"nullValue":true, "layer": "v_edit_exploitation", "activated": true, "keyColumn": "expl_id", "valueColumn": "name", "filterExpression": null}}'
+WHERE columnname = 'expl_id' and formname IN ('v_edit_inp_pattern', 'v_edit_inp_curve', 'v_edit_cat_dscenario');
