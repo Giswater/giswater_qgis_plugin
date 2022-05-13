c3c37faf64fa921d6600cb18cf0b2ee1c18d1869
Sergi Maspons <smaspons@bgeo.es>
2022-05-13 15:23:00 +0200

    Enhancement: getselectors -- get userValues if not passed as parameter

--- dbmodel/utils/fct/gw_fct_getselectors.sql
+++ dbmodel/utils/fct/gw_fct_getselectors.sql
@@ -288,6 +288,9 @@ BEGIN
 		', "message":"Not implemented"'||
 		'}')::json;
 	ELSE 
+		v_uservalues := COALESCE(json_extract_path_text(p_data,'data','userValues'), 
+			(SELECT to_json(array_agg(row_to_json(a))) FROM (SELECT parameter, value FROM config_param_user WHERE parameter IN ('plan_psector_vdefault', 'utils_workspace_vdefault') AND cur_user = current_user ORDER BY parameter)a)::text, 
+			'{}');
 		v_uservalues := COALESCE(json_extract_path_text(p_data,'data','userValues'), '{}');
 		v_action := json_extract_path_text(p_data,'data','action');
 		IF v_action = '' THEN v_action = NULL; END IF;
