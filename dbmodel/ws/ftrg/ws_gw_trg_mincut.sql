a8e6af2b9cb0f65d5a5fe15120d257b3c47a62cd
Barbara Rzepka <bbrzepka@bgeo.es>
2022-05-17 15:45:38 +0200

    modify trg_mincut

--- dbmodel/ws/ftrg/ws_gw_trg_mincut.sql
+++ dbmodel/ws/ftrg/ws_gw_trg_mincut.sql
@@ -16,11 +16,22 @@ BEGIN
 	EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
 	
 
+	IF now()::date != NEW.anl_tstamp::date THEN
 
-	UPDATE om_mincut SET modification_date=now() WHERE id = NEW.id;
+		IF OLD.mincut_type!=NEW.mincut_type or OLD.anl_cause!=NEW.anl_cause OR OLD.anl_tstamp!=NEW.anl_tstamp OR OLD.received_date!=NEW.received_date OR 
+		OLD.work_order!=NEW.work_order OR NEW.mincut_class!=OLD.mincut_class OR NEW.expl_id!=OLD.expl_id OR OLD.macroexpl_id!=NEW.macroexpl_id OR 
+		OLD.muni_id!=NEW.muni_id OR OLD.postcode!=NEW.postcode OR OLD.streetaxis_id!=NEW.streetaxis_id OR NEW.postnumber!=OLD.postnumber OR 
+		NEW.anl_user!=OLD.anl_user OR NEW.anl_descript!=OLD.anl_descript OR OLD.anl_feature_id!=NEW.anl_feature_id OR NEW.anl_feature_type!=OLD.anl_feature_type OR 
+		NEW.anl_the_geom::text!=OLD.anl_the_geom::text OR NEW.forecast_start!=OLD.forecast_start OR NEW.forecast_end!=OLD.forecast_end OR NEW.assigned_to!=OLD.assigned_to THEN
 
+			UPDATE om_mincut SET modification_date=now() WHERE id = NEW.id;
+
+		
+	END IF;
+
+	END IF;
+	
 	RETURN NEW;
-
 
 END;
 	
