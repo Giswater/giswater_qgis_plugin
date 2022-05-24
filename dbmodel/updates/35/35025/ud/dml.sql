9d02fe3059723432ee588e28aadc3098e64e2f90
Barbara Rzepka <bbrzepka@bgeo.es>
2022-05-24 08:28:24 +0200

    config_form_fields for v_edit_cat_hydrology &  v_edit_cat_dwf_scenario

--- dbmodel/updates/35/35025/ud/dml.sql
+++ dbmodel/updates/35/35025/ud/dml.sql
@@ -10,3 +10,20 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 --2022/05/23
 
 UPDATE sys_table SET id='v_edit_cat_dwf_scenario' WHERE id='v_edit_cat_dwf_dscenario';
+
+INSERT INTO config_form_fields(formname, formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label, tooltip, 
+placeholder, ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, 
+dv_querytext_filterc, stylesheet, widgetcontrols, widgetfunction, linkedobject, hidden)
+SELECT 'v_edit_cat_dwf_scenario', formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label, tooltip, 
+placeholder, ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, 
+dv_querytext_filterc, stylesheet, widgetcontrols, widgetfunction, linkedobject, hidden
+FROM config_form_fields WHERE formname='cat_dwf_scenario' ON CONFLICT (formname, formtype, columnname, tabname) DO NOTHING;
+
+INSERT INTO config_form_fields(formname, formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label, tooltip, 
+placeholder, ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, 
+dv_querytext_filterc, stylesheet, widgetcontrols, widgetfunction, linkedobject, hidden)
+SELECT 'v_edit_cat_dwf_scenario', formtype, tabname, columnname, layoutname, layoutorder, datatype, widgettype, label, tooltip, 
+placeholder, ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, 
+dv_querytext_filterc, stylesheet, widgetcontrols, widgetfunction, linkedobject, hidden
+FROM config_form_fields WHERE formname='cat_dscenario' AND columnname IN ('expl_id', 'log') ON CONFLICT (formname, formtype, columnname, tabname) DO NOTHING;
+
