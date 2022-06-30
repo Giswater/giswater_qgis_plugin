0791f1d09fca1276737fda8233cdc7b4ef6321c8
xtorret@bgeo.es <xtorret@bgeo.es>
2022-06-30 19:02:32 +0200

    Minor enhacement on how curves are exported

--- dbmodel/updates/35/35026/ws/ddlview.sql
+++ dbmodel/updates/35/35026/ws/ddlview.sql
@@ -113,3 +113,41 @@ CREATE OR REPLACE VIEW v_edit_inp_dscenario_pump_additional AS
      JOIN inp_dscenario_pump_additional p USING (node_id)
      JOIN cat_dscenario d USING (dscenario_id)
   WHERE p.dscenario_id = selector_inp_dscenario.dscenario_id AND selector_inp_dscenario.cur_user = "current_user"()::text;
+  
+  
+CREATE OR REPLACE VIEW vi_curves AS 
+SELECT
+        CASE
+            WHEN a.x_value IS NULL THEN a.curve_type::character varying(16)
+            ELSE a.curve_id
+        END AS curve_id,
+    a.x_value::numeric(12,4) AS x_value,
+    a.y_value::numeric(12,4) AS y_value,
+    NULL::text AS other
+   FROM ( SELECT row_number() over (order by id)  as rid, * 
+          FROM (SELECT DISTINCT ON (inp_curve_value.curve_id) ( SELECT min(sub.id) AS min
+                   FROM inp_curve_value sub
+                  WHERE sub.curve_id::text = inp_curve_value.curve_id::text) AS id,
+            inp_curve_value.curve_id,
+            concat(';', inp_curve.curve_type, ':', inp_curve.descript) AS curve_type,
+            NULL::numeric AS x_value,
+            NULL::numeric AS y_value
+           FROM inp_curve
+             JOIN inp_curve_value ON inp_curve_value.curve_id::text = inp_curve.id::text
+        UNION
+         SELECT inp_curve_value.id,
+            inp_curve_value.curve_id,
+            inp_curve.curve_type,
+            inp_curve_value.x_value,
+            inp_curve_value.y_value
+           FROM inp_curve_value
+             JOIN inp_curve ON inp_curve_value.curve_id::text = inp_curve.id::text
+  ORDER BY 1, 4 DESC)a) a
+  WHERE (a.curve_id::text IN ( SELECT temp_node.addparam::json ->> 'curve_id'::text
+           FROM temp_node
+        UNION
+         SELECT temp_arc.addparam::json ->> 'curve_id'::text
+           FROM temp_arc))
+  ORDER BY a.rid, NULL::text;
+
+
