842ce507c1b03731c3ede8a147f84fe4a2a6b38c
Edgar Fuste <efuste@bgeo.es>
2022-07-05 15:19:56 +0200

    Minor bug fix: add empty value on widget result_id for epa manager form

--- core/toolbars/epa/go2epa_manager_button.py
+++ core/toolbars/epa/go2epa_manager_button.py
@@ -179,7 +179,7 @@ class GwGo2EpaManagerButton(GwAction):
 
         sql = "SELECT result_id FROM v_ui_rpt_cat_result ORDER BY result_id"
         rows = tools_db.get_rows(sql)
-        tools_qt.fill_combo_values(self.dlg_manager.txt_result_id, rows)
+        tools_qt.fill_combo_values(self.dlg_manager.txt_result_id, rows, add_empty=True)
 
 
     def _filter_by_result_id(self):
