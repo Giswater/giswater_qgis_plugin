03f285290b4f1912e3b6fd6b754ed32acc7a5551
Edgar Fuste <efuste@bgeo.es>
2022-08-11 11:55:32 +0200

    Minor enhancement: Update v_ui_cat_dscenario without active filter

--- dbmodel/updates/35/35026/utils/ddlview.sql
+++ dbmodel/updates/35/35026/utils/ddlview.sql
@@ -36,3 +36,17 @@ CREATE OR REPLACE VIEW vi_controls AS
                   WHERE s.sector_id = d.sector_id AND s.cur_user = "current_user"()::text AND d.active IS NOT FALSE
                   ORDER BY 1)c
   ORDER BY c.id;
+  
+  
+  CREATE OR REPLACE VIEW v_ui_cat_dscenario AS 
+ SELECT DISTINCT ON (c.dscenario_id) c.dscenario_id,
+    c.name,
+    c.descript,
+    c.dscenario_type,
+    c.parent_id,
+    c.expl_id,
+    c.active,
+    c.log
+   FROM cat_dscenario c, selector_expl s
+  WHERE ((s.expl_id = c.expl_id AND s.cur_user = CURRENT_USER::text) OR c.expl_id IS NULL);
+  
