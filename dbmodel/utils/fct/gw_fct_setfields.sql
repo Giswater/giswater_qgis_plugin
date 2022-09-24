9f4fbe6daa6ec113367cda802ad83f0dc4c2a7d1
xtorret@bgeo.es <xtorret@bgeo.es>
2022-09-24 20:43:17 +0200

    Recover triggermapzones

--- dbmodel/utils/fct/gw_fct_setfields.sql
+++ dbmodel/utils/fct/gw_fct_setfields.sql
@@ -197,6 +197,12 @@ BEGIN
 
 	v_message = '{"level": 3, "text": "Feature have been succesfully updated."}';
 
+	-- trigger automatic mapzones
+	IF v_projecttype = 'WS' AND v_closedstatus IS NOT NULL AND v_afterinsert IS NOT TRUE THEN
+
+		EXECUTE 'SELECT gw_fct_setmapzonestrigger($$'||p_data||'$$)' INTO v_message;
+		
+	END IF;
 	
 	-- Control NULL's
 	v_version := COALESCE(v_version, '[]');
