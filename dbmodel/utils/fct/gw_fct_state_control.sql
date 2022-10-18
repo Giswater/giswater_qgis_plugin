0ddb405a90217d345a53a273fa5d8931ce595807
Albert Bofill <abofill@bgeo.es>
2022-10-18 13:48:35 +0200

    Bug fix on fct_state_control

--- dbmodel/utils/fct/gw_fct_state_control.sql
+++ dbmodel/utils/fct/gw_fct_state_control.sql
@@ -174,8 +174,8 @@ BEGIN
 				END IF;
 
 				-- hydrometer control
-				SELECT count(*) INTO v_num_feature FROM ext_rtc_hydrometer h JOIN connec c ON h.connec_id = customer_code WHERE c.connec_id = '3001' AND state IN
-				(SELECT (json_array_elements_text((value::json->>'1')::json))::INTEGER FROM config_param_system where parameter  = 'admin_hydrometer_state');
+				SELECT count(*) INTO v_num_feature FROM ext_rtc_hydrometer h JOIN connec c ON h.connec_id::varchar = customer_code::varchar WHERE c.connec_id = feature_id_aux 
+                AND state IN (SELECT (json_array_elements_text((value::json->>'1')::json))::INTEGER FROM config_param_system where parameter  = 'admin_hydrometer_state');
 				
 				IF v_num_feature > 0 THEN 
 					EXECUTE 'SELECT gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
