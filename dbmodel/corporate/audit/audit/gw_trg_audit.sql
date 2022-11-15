7f5a1b7266f3ba7a64c8d030f52773462df7204b
Albert Bofill <abofill@bgeo.es>
2022-11-15 17:32:08 +0100

    Review files from corporate/audit to make easier its implementation

--- dbmodel/corporate/audit/audit/gw_trg_audit.sql
+++ dbmodel/corporate/audit/audit/gw_trg_audit.sql
@@ -8,19 +8,10 @@ CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_trg_audit()
   RETURNS trigger AS
 $BODY$
 
-/*
-The goal of this trigger is to disable it when mapzones are enabled in order to enhance performance
-*/
-
 
 DECLARE
 v_old_data json;
 v_new_data json;
-v_sector boolean = true;
-v_minsector boolean = true;
-v_presszone boolean = true;
-v_dqa boolean = true;
-v_dma boolean = true;
 
 BEGIN
 
@@ -32,18 +23,12 @@ BEGIN
 		INSERT INTO audit.log (schema,table_name,user_name,action,newdata,query)
 		VALUES (TG_TABLE_SCHEMA::TEXT, TG_TABLE_NAME::TEXT ,session_user::TEXT,substring(TG_OP,1,1),v_new_data, current_query());
 		RETURN NEW;
+        
 	ELSIF (TG_OP = 'UPDATE') THEN
-    
-		IF current_query() like '%gw_fct_grafanalytics_mapzones%' THEN
-		
-		ELSE
-			v_old_data := row_to_json(OLD.*);
-			v_new_data := row_to_json(NEW.*);
-			INSERT INTO audit.log (schema,table_name,user_name,action,olddata,newdata,query) 
-			VALUES (TG_TABLE_SCHEMA::TEXT,TG_TABLE_NAME::TEXT,session_user::TEXT,substring(TG_OP,1,1),v_old_data,v_new_data, current_query());
-		END IF;
-
-	
+        v_old_data := row_to_json(OLD.*);
+        v_new_data := row_to_json(NEW.*);
+        INSERT INTO audit.log (schema,table_name,user_name,action,olddata,newdata,query) 
+        VALUES (TG_TABLE_SCHEMA::TEXT,TG_TABLE_NAME::TEXT,session_user::TEXT,substring(TG_OP,1,1),v_old_data,v_new_data, current_query());
 		RETURN NEW;
 	
 	ELSIF (TG_OP = 'DELETE') THEN
@@ -51,6 +36,7 @@ BEGIN
 		INSERT INTO audit.log (schema,table_name,user_name,action,olddata,query)
 		VALUES (TG_TABLE_SCHEMA::TEXT,TG_TABLE_NAME::TEXT,session_user::TEXT,substring(TG_OP,1,1),v_old_data, current_query());
 		RETURN OLD;
+        
 	END IF;
 END;
 $BODY$
