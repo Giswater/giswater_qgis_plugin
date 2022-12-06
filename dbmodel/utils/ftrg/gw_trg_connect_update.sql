866c496b1db21aedeeeeb6285cfb4256a0988ea7
xtorret@bgeo.es <xtorret@bgeo.es>
2022-12-06 18:59:16 +0100

    Refactor links strategy (WiP) - update connect_update trigger

--- dbmodel/utils/ftrg/gw_trg_connect_update.sql
+++ dbmodel/utils/ftrg/gw_trg_connect_update.sql
@@ -42,10 +42,6 @@ BEGIN
 
 	v_projectype = (SELECT project_type FROM sys_version ORDER BY id DESC LIMIT 1);
 
-	-- control autoupdate_dma and fluid
-	SELECT value::boolean INTO v_autoupdate_dma FROM config_param_system WHERE parameter='edit_connect_autoupdate_dma';
-	SELECT value::boolean INTO v_autoupdate_fluid FROM config_param_system WHERE parameter='edit_connect_autoupdate_fluid';
-	
 	
 	IF v_featuretype='connec' THEN
 	
@@ -68,56 +64,45 @@ BEGIN
 			END LOOP;		
 		END IF;
 		
-		-- update the rest of the feature parameters
-		FOR v_link IN SELECT * FROM link WHERE (exit_type='CONNEC' AND exit_id=OLD.connec_id)
+		-- update the rest of the feature parameters for state = 1 connects
+		FOR v_link IN SELECT * FROM link WHERE (exit_type='CONNEC' AND exit_id=OLD.connec_id) AND state = 1
 		LOOP
-
 			IF v_link.feature_type='CONNEC' THEN
-				IF v_autoupdate_dma IS FALSE THEN
-					-- update connec, mandatory to use v_edit_connec because it's identified and managed when arc_id comes from plan psector tables
-					UPDATE v_edit_connec SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, sector_id=NEW.sector_id, pjoint_id=NEW.pjoint_id, pjoint_type=NEW.pjoint_type
+			
+				IF v_projecttype = 'WS' THEN
+					UPDATE connec SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, sector_id=NEW.sector_id, presszone_id  = NEW.presszone_id, dqa_id = NEW.dqa_id, 
+					minsector_id = NEW.minsector_id, fluid_type = NEW.fluid_type
 					WHERE connec_id=v_link.feature_id;
-				ELSE
-					UPDATE v_edit_connec SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, dma_id= NEW.dma_id, sector_id=NEW.sector_id, pjoint_id=NEW.pjoint_id, pjoint_type=NEW.pjoint_type
+					
+				ELSIF v_projecttype = 'UD' THEN
+					UPDATE connec SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, dma_id= NEW.dma_id, sector_id=NEW.sector_id, fluid_type = NEW.fluid_type
 					WHERE connec_id=v_link.feature_id;
 				END IF;
 							
-			
 			ELSIF v_link.feature_type='GULLY' THEN
-				IF v_autoupdate_dma IS FALSE THEN
-					-- update gully, mandatory to use v_edit_gully because it's identified and managed when arc_id comes from plan psector tables
-					UPDATE v_edit_gully SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, sector_id=NEW.sector_id, pjoint_id=NEW.pjoint_id, pjoint_type=NEW.pjoint_type
-					WHERE gully_id=v_link.feature_id;
-				ELSE
-					UPDATE v_edit_gully SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, dma_id= NEW.dma_id, sector_id=NEW.sector_id, pjoint_id=NEW.pjoint_id, pjoint_type=NEW.pjoint_type
-					WHERE gully_id=v_link.feature_id;
-				END IF;
-				
-			END IF;
 			
-		END LOOP;
-		-- update fields that inherit values from arc
-		IF v_projectype = 'WS' AND NEW.arc_id IS NOT NULL AND (NEW.arc_id != OLD.arc_id) THEN
-			UPDATE connec SET presszone_id=a.presszone_id, dqa_id=a.dqa_id, minsector_id=a.minsector_id
-			FROM (SELECT connec_id, a.presszone_id, a.dqa_id, a.minsector_id FROM v_edit_connec JOIN arc a USING (arc_id) WHERE a.arc_id = NEW.arc_id)a
-			WHERE a.connec_id=connec.connec_id;
-            
-			IF v_autoupdate_fluid IS TRUE THEN
-                UPDATE connec SET fluid_type = a.fluid_type
-                FROM (SELECT connec_id, a.fluid_type FROM v_edit_connec JOIN arc a USING (arc_id) WHERE a.arc_id = NEW.arc_id)a
-                WHERE a.connec_id=connec.connec_id;
+				UPDATE gully SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, dma_id= NEW.dma_id, sector_id=NEW.sector_id
+				WHERE gully_id=v_link.feature_id;			
 			END IF;
-		END IF;
-		
-		IF v_projectype = 'UD' AND v_autoupdate_fluid IS TRUE AND NEW.arc_id IS NOT NULL AND (NEW.arc_id != OLD.arc_id) THEN
-			UPDATE connec SET fluid_type = a.fluid_type
-			FROM (SELECT connec_id, a.fluid_type FROM v_edit_connec JOIN arc a USING (arc_id) WHERE a.arc_id = NEW.arc_id)a
-			WHERE a.connec_id=connec.connec_id;
+		END LOOP;
 
-			UPDATE gully SET fluid_type = a.fluid_type
-			FROM (SELECT gully_id, a.fluid_type FROM v_edit_gully JOIN arc a USING (arc_id) WHERE a.arc_id = NEW.arc_id)a
-			WHERE a.gully_id=gully.gully_id;
-		END IF;
+
+		-- update planned links (and planned connects as well)
+		FOR v_link IN SELECT * FROM link WHERE (exit_type='CONNEC' AND exit_id=OLD.connec_id) AND state = 2
+		LOOP
+			IF v_projectype = 'WS' THEN
+				UPDATE link SET expl_id=NEW.expl_id, sector_id=NEW.sector_id, dma_id = NEW.dma_id, 
+				presszone_id = NEW.presszone_id, dqa_id = NEW.dqa_id, minsector_id = NEW.minsector_id, fluid_type = NEW.fluid_type
+				WHERE link_id=v_link.link_id;
+			ELSE
+				UPDATE link SET expl_id=NEW.expl_id, sector_id=NEW.sector_id, dma_id = NEW.dma_id, 
+				fluid_type = NEW.fluid_type
+				WHERE link_id=v_link.link_id;
+			END IF;
+
+			UPDATE plan_psector_x_connec SET arc_id = NEW.arc_id WHERE link_id = v_link.link_id;
+						
+		END LOOP;
 
 		-- Updating polygon geometry in case of exists it
 		v_pol_id:= (SELECT pol_id FROM polygon WHERE feature_id=OLD.connec_id);
@@ -127,15 +112,15 @@ BEGIN
 			UPDATE polygon SET the_geom=ST_translate(the_geom, xvar, yvar) WHERE pol_id=v_pol_id;
 		END IF;      
 				
-
 	ELSIF v_featuretype='gully' THEN
+	
 		v_pol_id:= (SELECT pol_id FROM polygon WHERE feature_id=OLD.gully_id);
 		-- Updating polygon geometry in case of exists it
 		IF st_equals (NEW.the_geom, OLD.the_geom) IS FALSE AND v_move_polgeom IS TRUE AND (v_pol_id IS NOT NULL) THEN   
 			xvar= (st_x(NEW.the_geom)-st_x(OLD.the_geom));
 			yvar= (st_y(NEW.the_geom)-st_y(OLD.the_geom));		
 			UPDATE polygon SET the_geom=ST_translate(the_geom, xvar, yvar) WHERE pol_id=v_pol_id;
-		END IF;
+		END IF;	
 		
 		-- updating links geom
 		IF st_equals (NEW.the_geom, OLD.the_geom) IS FALSE THEN
@@ -155,46 +140,36 @@ BEGIN
 				USING linkrec.the_geom, NEW.the_geom; 
 			END LOOP;
 		END IF;
-		
-		-- update the rest of feature parameters
-		FOR v_link IN SELECT * FROM link WHERE (exit_type='GULLY' AND exit_id=OLD.gully_id)
+
+
+		-- update the rest of the feature parameters for state = 1 connects
+		FOR v_link IN SELECT * FROM link WHERE (exit_type='GULLY' AND exit_id=OLD.gully_id) AND state = 1
 		LOOP
 			IF v_link.feature_type='CONNEC' THEN
-				IF v_autoupdate_dma IS FALSE THEN			
-					UPDATE v_edit_connec SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, sector_id=NEW.sector_id, pjoint_id=NEW.pjoint_id, pjoint_type=NEW.pjoint_type
-					WHERE connec_id=v_link.feature_id;
-				ELSE
-					UPDATE v_edit_connec SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, dma_id= NEW.dma_id, sector_id=NEW.sector_id, pjoint_id=NEW.pjoint_id, pjoint_type=NEW.pjoint_type
-					WHERE connec_id=v_link.feature_id;
-				END IF;
 			
+				UPDATE connec SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, dma_id= NEW.dma_id, sector_id=NEW.sector_id, fluid_type = NEW.fluid_type
+				WHERE connec_id=v_link.feature_id;
+							
 			ELSIF v_link.feature_type='GULLY' THEN
-				IF v_autoupdate_dma IS FALSE THEN		
-					UPDATE v_edit_gully SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, sector_id=NEW.sector_id, pjoint_id=NEW.pjoint_id, pjoint_type=NEW.pjoint_type
-					WHERE gully_id=v_link.feature_id;
-				ELSE
-					UPDATE v_edit_gully SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, dma_id= NEW.dma_id, sector_id=NEW.sector_id, pjoint_id=NEW.pjoint_id, pjoint_type=NEW.pjoint_type
-					WHERE gully_id=v_link.feature_id;
-				END IF;
-				
+			
+				UPDATE gully SET arc_id=NEW.arc_id, expl_id=NEW.expl_id, dma_id= NEW.dma_id, sector_id=NEW.sector_id
+				WHERE gully_id=v_link.feature_id;			
 			END IF;
-
 		END LOOP;
-        
-        -- update fields that inherit values from arc
-        IF v_autoupdate_fluid IS TRUE AND NEW.arc_id IS NOT NULL AND (NEW.arc_id != OLD.arc_id) THEN
-			UPDATE connec SET fluid_type = a.fluid_type
-			FROM (SELECT connec_id, a.fluid_type FROM v_edit_connec JOIN arc a USING (arc_id) WHERE a.arc_id = NEW.arc_id)a
-			WHERE a.connec_id=connec.connec_id;
 
-			UPDATE gully SET fluid_type = a.fluid_type
-			FROM (SELECT gully_id, a.fluid_type FROM v_edit_gully JOIN arc a USING (arc_id) WHERE a.arc_id = NEW.arc_id)a
-			WHERE a.gully_id=gully.gully_id;
-		END IF;
-		
+
+		-- update planned links (and planned connects as well)
+		FOR v_link IN SELECT * FROM link WHERE (exit_type='GULLY' AND exit_id=OLD.gully_id) AND state = 2
+		LOOP
+			UPDATE link SET expl_id=NEW.expl_id, sector_id=NEW.sector_id, dma_id = NEW.dma_id, fluid_type = NEW.fluid_type
+			WHERE link_id=v_link.feature_id;
+
+			UPDATE plan_psector_x_gully SET arc_id = NEW.arc_id WHERE link_id = v_link.link_id;
+			
+		END LOOP;
 	END IF;
 		
-    RETURN NEW;
+	RETURN NEW;
     
 END; 
 $$;
