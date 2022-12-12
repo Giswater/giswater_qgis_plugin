9f3c58458ba506ddca07c95c246d8b942e4dc2b1
xtorret@bgeo.es <xtorret@bgeo.es>
2022-12-12 10:20:59 +0100

    Refactor links strategy (WiP) -  Control for planned connects

--- dbmodel/utils/ftrg/gw_trg_edit_plan_psector_x_connect.sql
+++ dbmodel/utils/ftrg/gw_trg_edit_plan_psector_x_connect.sql
@@ -41,7 +41,9 @@ BEGIN
 			UPDATE plan_psector_x_connec SET doable = NEW.doable, descript = NEW.descript, arc_id = NEW.arc_id
 			WHERE id = NEW.id;
 
-			IF NEW.state  = 0 AND OLD.state = 1 THEN
+			IF NEW.state  = 0 AND OLD.state = 1 AND (SELECT state FROM connec WHERE connec_id = NEW.connec_id) = 2 THEN
+				RAISE EXCEPTION 'IT DOES NOT MAKE SENSE DOWNGRADE THE STATE OF PLANNED CONNEC.  TO UNLINK IT FROM PSECTOR PLEASE REMOVE ROW OR DELETE CONNEC';
+			ELSIF NEW.state  = 0 AND OLD.state = 1 THEN
 				DELETE FROM plan_psector_x_connec WHERE id = NEW.id;
 			ELSIF NEW.state  = 1 AND OLD.state = 0 THEN
 				INSERT INTO plan_psector_x_connec (psector_id, connec_id, state) VALUES (NEW.psector_id, NEW.connec_id, 1);
@@ -52,8 +54,10 @@ BEGIN
 			UPDATE plan_psector_x_gully SET doable = NEW.doable, descript = NEW.descript, arc_id = NEW.arc_id
 			WHERE id = NEW.id;	
 			
-			IF NEW.state  = 1 AND OLD.state = 0 THEN
-				DELETE FROM plan_psector_x_connec WHERE id = NEW.id;
+			IF NEW.state  = 0 AND OLD.state = 1 AND (SELECT state FROM gully WHERE gully_id = NEW.gully_id) = 2 THEN
+				RAISE EXCEPTION 'IT DOES NOT MAKE SENSE DOWNGRADE THE STATE OF PLANNED GULLY.  TO UNLINK IT FROM PSECTOR PLEASE REMOVE ROW OR DELETE GULLY';
+			ELSIF NEW.state  = 0 AND OLD.state = 1 THEN
+				DELETE FROM plan_psector_x_gully WHERE id = NEW.id;
 			ELSIF NEW.state  = 1 AND OLD.state = 0 THEN
 				INSERT INTO plan_psector_x_gully (psector_id, gully_id, state) VALUES (NEW.psector_id, NEW.gully_id, 1);
 			END IF;
