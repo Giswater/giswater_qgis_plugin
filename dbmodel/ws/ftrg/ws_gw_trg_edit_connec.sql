5dcc6d918536c2f4537cb66f7cb8f39744c64a7f
Albert Bofill <abofill@bgeo.es>
2023-01-03 08:50:23 +0100

    Fix schema_name

--- dbmodel/ws/ftrg/ws_gw_trg_edit_connec.sql
+++ dbmodel/ws/ftrg/ws_gw_trg_edit_connec.sql
@@ -7,7 +7,7 @@ This version of Giswater is provided by Giswater Association
 --FUNCTION CODE: 1304
 
 
-CREATE OR REPLACE FUNCTION "ws_sample".gw_trg_edit_connec()
+CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_trg_edit_connec()
   RETURNS trigger AS
 $BODY$
 
