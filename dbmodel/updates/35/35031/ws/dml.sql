87cc8e2394401631479298ac969af250f616f3bc
Albert Bofill <abofill@bgeo.es>
2023-01-04 13:39:26 +0100

    Set node1/2 values to arc table (WS)

--- dbmodel/updates/35/35031/ws/dml.sql
+++ dbmodel/updates/35/35031/ws/dml.sql
@@ -403,3 +403,12 @@ UPDATE config_form_fields SET label='Sector name' WHERE label='Dma name' AND col
 
 DELETE FROM config_param_system WHERE "parameter"='edit_connect_autoupdate_dma';
 DELETE FROM config_param_system WHERE "parameter"='edit_connect_autoupdate_fluid';
+
+UPDATE arc SET nodetype_1 = node_type FROM vu_node WHERE node_id = node_1;
+UPDATE arc SET nodetype_2 = node_type FROM vu_node WHERE node_id = node_2;
+UPDATE arc SET elevation1 = elevation FROM vu_node WHERE node_id = node_1;
+UPDATE arc SET elevation2 = elevation FROM vu_node WHERE node_id = node_2;
+UPDATE arc SET depth1 = vu_node.depth FROM vu_node WHERE node_id = node_1;
+UPDATE arc SET depth2 = vu_node.depth FROM vu_node WHERE node_id = node_2;
+UPDATE arc SET staticpress1 = vu_node.staticpressure FROM vu_node WHERE node_id = node_1;
+UPDATE arc SET staticpress2 = vu_node.staticpressure FROM vu_node WHERE node_id = node_2;
