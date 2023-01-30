9dd12f64f5b0f1e36e0699f148f13d8e1c714103
Barbara Rzepka <bbrzepk@bgeo.es>
2023-01-30 13:54:45 +0100

    new fields on om_waterbalance, view v_om_waterbalance_report

--- dbmodel/updates/35/35033/ws/ddl.sql
+++ dbmodel/updates/35/35033/ws/ddl.sql
@@ -9,3 +9,20 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 
 SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"minsector", "column":"num_valve", "dataType":"integer"}}$$);
+
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"om_waterbalance", "column":"meters_in", "dataType":"text"}}$$);
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"om_waterbalance", "column":"meters_out", "dataType":"text"}}$$);
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"om_waterbalance", "column":"n_connec", "dataType":"integer"}}$$);
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"om_waterbalance", "column":"n_hydro", "dataType":"integer"}}$$);
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"om_waterbalance", "column":"arc_length", "dataType":"double precision"}}$$);
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"om_waterbalance", "column":"link_length", "dataType":"double precision"}}$$);
+
+
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"om_waterbalance", "column":"dma_rw_eff", "dataType":"double precision"}}$$);
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"om_waterbalance", "column":"dma_nrw_eff", "dataType":"double precision"}}$$);
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"om_waterbalance", "column":"dma_ili", "dataType":"double precision"}}$$);
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"om_waterbalance", "column":"dma_nightvol", "dataType":"double precision"}}$$);
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"om_waterbalance", "column":"dma_m4day", "dataType":"double precision"}}$$);
+
+
+
--- dbmodel/updates/35/35033/ws/ddlview.sql
+++ dbmodel/updates/35/35033/ws/ddlview.sql
@@ -21,3 +21,37 @@ CREATE OR REPLACE VIEW v_minsector
    FROM selector_expl,
     minsector
   WHERE minsector.expl_id = selector_expl.expl_id AND selector_expl.cur_user = "current_user"()::text;
+
+
+CREATE VIEW v_om_waterbalance_report as 
+WITH met_in AS (SELECT string_agg (node_id, ', ') as meters, dma_id FROM om_waterbalance_dma_graph WHERE flow_sign = 1 group by dma_id),
+met_out AS (SELECT string_agg (node_id, ', ') as meters, dma_id FROM om_waterbalance_dma_graph WHERE flow_sign = -1 group by dma_id)
+SELECT 
+w.expl_id,
+e.name as expl_name,
+w.dma_id,
+d.name as dma_name,
+cat_period_id as period,
+concat(startdate,' - ',enddate) as period_dates,
+mi.meters as meters_in,
+mo.meters as meters_out,
+n_connec,
+n_hydro,
+arc_length,
+link_length,
+total_in,
+total_out,
+COALESCE(w.total_sys_input, 0::double precision)::numeric(12,2) AS total,
+NULL AS auth,
+NULL AS nrw,
+dma_rw_eff,
+dma_nrw_eff,
+dma_ili,
+dma_nightvol,
+dma_m4day
+from om_waterbalance w
+join exploitation e using (expl_id)
+join dma d using (dma_id)
+join ext_cat_period p on w.cat_period_id = p.id
+join met_in mi on w.dma_id = mi.dma_id
+join met_out mo on w.dma_id = mo.dma_id;
