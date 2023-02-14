0e49e7ccc534679c0f21cbb4668e3f7ad9b6dc42
Barbara Rzepka <bbrzepk@bgeo.es>
2023-02-14 15:14:46 +0100

    gw_trg_feature_border adjust for sector

--- dbmodel/utils/ftrg/gw_trg_feature_border.sql
+++ dbmodel/utils/ftrg/gw_trg_feature_border.sql
@@ -24,7 +24,21 @@ BEGIN
 
   -- Control insertions ID
   IF TG_OP = 'INSERT' THEN
+  	--sector
+  	INSERT INTO node_border_sector
+		WITH 
+		arcs AS (SELECT arc_id, node_1, node_2, sector_id FROM arc WHERE state=1 AND arc_id = NEW.arc_id)
+		SELECT node_id, a1.sector_id
+		FROM node 
+		JOIN arcs a1 ON node_id=node_1 
+		where a1.sector_id != node.sector_id
+		UNION 
+		SELECT node_id, a2.sector_id
+		FROM node 
+		JOIN arcs a2 ON node_id=node_2 
+		where a2.sector_id != node.sector_id ON CONFLICT (node_id, sector_id) DO NOTHING;	
 
+  	--exploitation
 		INSERT INTO node_border_expl
 		WITH 
 		arcs AS (SELECT arc_id, node_1, node_2, expl_id FROM arc WHERE state=1 AND arc_id = NEW.arc_id)
@@ -54,6 +68,22 @@ BEGIN
 
   ELSIF TG_OP = 'UPDATE' AND v_feature_type = 'ARC' THEN
 	raise notice 'arc';
+
+		--sector
+  	INSERT INTO node_border_sector
+		WITH 
+		arcs AS (SELECT arc_id, node_1, node_2, sector_id FROM arc WHERE state=1 AND arc_id = NEW.arc_id)
+		SELECT node_id, a1.sector_id
+		FROM node 
+		JOIN arcs a1 ON node_id=node_1 
+		where a1.sector_id != node.sector_id
+		UNION 
+		SELECT node_id, a2.sector_id
+		FROM node 
+		JOIN arcs a2 ON node_id=node_2 
+		where a2.sector_id != node.sector_id ON CONFLICT (node_id, sector_id) DO NOTHING;	
+
+		--exploitation
 		INSERT INTO node_border_expl
 		WITH 
 		arcs AS (SELECT arc_id, node_1, node_2, expl_id FROM arc WHERE state=1 AND arc_id = NEW.arc_id)
@@ -81,11 +111,32 @@ BEGIN
 
 
 		v_final_nodes = string_to_array(OLD.node_1,'') ||string_to_array(OLD.node_2,'');
-			raise notice 'arcv_final_nodes,%',v_final_nodes;
-		IF v_final_nodes IS NOT NULL THEN
 
+		IF v_final_nodes IS NOT NULL THEN
+			--sector
 			FOREACH rec IN ARRAY (v_final_nodes) LOOP
-			raise notice 'arc-rec,%',rec;
+	
+				EXECUTE 'WITH 
+					arcs AS (SELECT arc_id, node_1, node_2, sector_id FROM arc WHERE state=1 AND  sector_id='||OLD.sector_id||')
+					select count(*) from
+					(SELECT node_id, a1.sector_id
+					FROM node 
+					JOIN arcs a1 ON node_id=node_1 
+					where a1.sector_id != node.sector_id AND node_id='||quote_literal(rec)||'
+					UNION all
+					SELECT node_id, a2.sector_id
+					FROM node 
+					JOIN arcs a2 ON node_id=node_2 
+					where a2.sector_id != node.sector_id AND node_id='||quote_literal(rec)||' )a'
+					INTO v_count;
+
+		  IF v_count is null OR v_count = 0 THEN
+				EXECUTE 'DELETE FROM node_border_sector WHERE node_id='||quote_literal(rec)||' AND sector_id='||OLD.sector_id||'';
+		  END IF;
+
+			--exploitation
+			FOREACH rec IN ARRAY (v_final_nodes) LOOP
+	
 				EXECUTE 'WITH 
 					arcs AS (SELECT arc_id, node_1, node_2, expl_id FROM arc WHERE state=1 AND  expl_id='||OLD.expl_id||')
 					select count(*) from
@@ -100,7 +151,6 @@ BEGIN
 					where a2.expl_id != node.expl_id AND node_id='||quote_literal(rec)||' )a'
 					INTO v_count;
 
-raise notice 'arcv_v_count,%',v_count;
 		  IF v_count is null OR v_count = 0 THEN
 				EXECUTE 'DELETE FROM node_border_expl WHERE node_id='||quote_literal(rec)||' AND expl_id='||OLD.expl_id||'';
 
@@ -115,7 +165,23 @@ raise notice 'arcv_v_count,%',v_count;
 		RETURN NEW;
 
 	ELSIF TG_OP = 'UPDATE' AND v_feature_type = 'NODE' THEN
+		--sector
+		DELETE FROM node_border_sector WHERE node_id=OLD.node_id;
 
+  	INSERT INTO node_border_sector
+		WITH 
+		arcs AS (SELECT arc_id, node_1, node_2, sector_id FROM arc WHERE state=1 AND arc_id = NEW.arc_id)
+		SELECT node_id, a1.sector_id
+		FROM node 
+		JOIN arcs a1 ON node_id=node_1 
+		where a1.sector_id != node.sector_id
+		UNION 
+		SELECT node_id, a2.sector_id
+		FROM node 
+		JOIN arcs a2 ON node_id=node_2 
+		where a2.sector_id != node.sector_id ON CONFLICT (node_id, sector_id) DO NOTHING;	
+
+		--exploitation
 		DELETE FROM node_border_expl WHERE node_id=OLD.node_id;
 
 		DELETE from node_border_expl where node_id in 
@@ -153,6 +219,28 @@ raise notice 'arcv_v_count,%',v_count;
 		v_final_nodes = string_to_array(OLD.node_1,'') ||string_to_array(OLD.node_2,'');
 		
 			IF v_final_nodes IS NOT NULL THEN
+
+			--sector
+			FOREACH rec IN ARRAY (v_final_nodes) LOOP
+	
+				EXECUTE 'WITH 
+					arcs AS (SELECT arc_id, node_1, node_2, sector_id FROM arc WHERE state=1 AND  sector_id='||OLD.sector_id||')
+					select count(*) from
+					(SELECT node_id, a1.sector_id
+					FROM node 
+					JOIN arcs a1 ON node_id=node_1 
+					where a1.sector_id != node.sector_id AND node_id='||quote_literal(rec)||'
+					UNION all
+					SELECT node_id, a2.sector_id
+					FROM node 
+					JOIN arcs a2 ON node_id=node_2 
+					where a2.sector_id != node.sector_id AND node_id='||quote_literal(rec)||' )a'
+					INTO v_count;
+
+		  IF v_count is null OR v_count = 0 THEN
+				EXECUTE 'DELETE FROM node_border_sector WHERE node_id='||quote_literal(rec)||' AND sector_id='||OLD.sector_id||'';
+		  END IF;
+		  --exploitation
 			FOREACH rec IN ARRAY (v_final_nodes) LOOP
 
 				EXECUTE 'WITH 
