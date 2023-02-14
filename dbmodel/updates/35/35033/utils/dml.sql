278b56444afa6fa92df775f85b98f618cbdd9bbb
Barbara Rzepka <bbrzepk@bgeo.es>
2023-02-14 14:24:06 +0100

    bug fix on config_form_fields defeinition

--- dbmodel/updates/35/35033/utils/dml.sql
+++ dbmodel/updates/35/35033/utils/dml.sql
@@ -34,14 +34,14 @@ UPDATE sys_function set function_name = 'gw_trg_feature_border' WHERE id = 3190;
 
 INSERT INTO config_form_fields (formname, formtype, tabname, columnname, layoutname, layoutorder, 
 datatype, widgettype, label, tooltip,  ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_isnullvalue, hidden)
-SELECT  'v_edit_node','form_feature', 'data', 'parent_id', 'lyt_data_1', max(layoutorder)+1, 'string', 'typeahead', 'parent_id', 'parent_id - Identificador de su nodo pariente. Un nodo padre puede tener varios otros nodos contenidos dentro de él',
+SELECT  'v_edit_arc','form_feature', 'data', 'parent_id', 'lyt_data_1', max(layoutorder)+1, 'string', 'typeahead', 'parent_id', 'parent_id - Identificador de su nodo pariente. Un nodo padre puede tener varios otros nodos contenidos dentro de él',
 false, false,true,false,false, 'SELECT node_id AS id, node_id AS idval FROM node WHERE node_id IS NOT NULL', true, true
 FROM config_form_fields where formname = 'v_edit_node' and layoutname = 'lyt_data_1'
 ON CONFLICT (formname, formtype, columnname, tabname) DO NOTHING;
 
 INSERT INTO config_form_fields (formname, formtype, tabname, columnname, layoutname, layoutorder, 
 datatype, widgettype, label, tooltip,  ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_isnullvalue, hidden)
-select 've_node','form_feature', 'data', 'parent_id', 'lyt_data_1', max(layoutorder)+1, 'string', 'typeahead', 'parent_id', 'parent_id - Identificador de su nodo pariente. Un nodo padre puede tener varios otros nodos contenidos dentro de él',
+select 've_arc','form_feature', 'data', 'parent_id', 'lyt_data_1', max(layoutorder)+1, 'string', 'typeahead', 'parent_id', 'parent_id - Identificador de su nodo pariente. Un nodo padre puede tener varios otros nodos contenidos dentro de él',
 false, false,true,false,false, 'SELECT node_id AS id, node_id AS idval FROM node WHERE node_id IS NOT NULL', true, true
 FROM config_form_fields where formname = 've_node' and layoutname = 'lyt_data_1'
 ON CONFLICT (formname, formtype, columnname, tabname) DO NOTHING;
@@ -51,7 +51,7 @@ datatype, widgettype, label, tooltip,  ismandatory, isparent, iseditable, isauto
 SELECT distinct child_layer,formtype, tabname, columnname, layoutname, max(layoutorder)+1, datatype, widgettype, label, tooltip,  ismandatory, 
 isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytext_filterc, hidden
 FROM config_form_fields, cat_feature
-WHERE feature_type = 'NODE' AND  (formname ilike 'v_edit_node' and columnname = 'parent_id')  group by child_layer,formtype, tabname, columnname, layoutname,
+WHERE feature_type = 'ARC' AND  (formname ilike 'v_edit_node' and columnname = 'parent_id')  group by child_layer,formtype, tabname, columnname, layoutname,
 datatype, widgettype, label, tooltip,  ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytext_filterc,  hidden 
 ON CONFLICT (formname, formtype, columnname, tabname) DO NOTHING;
 
