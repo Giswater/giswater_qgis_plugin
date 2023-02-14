7a0c3708cd994035c4c1a268b3f9ad6646feb461
Albert Bofill <abofill@bgeo.es>
2023-02-14 13:25:09 +0100

    Remove selectedId from config_toolbox in order to use session variable instead

--- dbmodel/updates/35/35033/ud/dml.sql
+++ dbmodel/updates/35/35033/ud/dml.sql
@@ -10,12 +10,12 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 INSERT INTO config_toolbox (id, alias, functionparams, inputparams, observ, active) VALUES(3198, 'Get address values from closest street number', '{"featureType":["node","connec","gully"]}'::json, 
 '[{"widgetname":"catFeature", "label":"Type:","widgettype":"combo","datatype":"text","layoutname":"grl_option_parameters","layoutorder":1,
 "comboIds":["ALL NODES", "ALL CONNECS", "ALL GULLYS", "CHAMBER", "JUNCTION", "MANHOLE", "NETINIT", "OUTFALL", "STORAGE", "VALVE"], 
-"comboNames":["ALL NODES", "ALL CONNECS", "ALL GULLYS", "CHAMBER", "JUNCTION", "MANHOLE", "NETINIT", "OUTFALL", "STORAGE", "VALVE"], "selectedId":"ALL NODES"},
+"comboNames":["ALL NODES", "ALL CONNECS", "ALL GULLYS", "CHAMBER", "JUNCTION", "MANHOLE", "NETINIT", "OUTFALL", "STORAGE", "VALVE"]},
 {"widgetname":"fieldToUpdate", "label":"Field to update:","widgettype":"combo","datatype":"text","layoutname":"grl_option_parameters","layoutorder":2,
-"comboIds":["postnumber", "postcomplement"], "comboNames":["POSTNUMBER", "POSTCOMPLEMENT"], "selectedId":"postnumber"},
+"comboIds":["postnumber", "postcomplement"], "comboNames":["POSTNUMBER", "POSTCOMPLEMENT"]},
 {"widgetname":"searchBuffer", "label":"Search buffer (meters):","widgettype":"text","datatype":"float", "layoutname":"grl_option_parameters","layoutorder":3, "isMandatory":true, "value":"50"},
 {"widgetname":"updateValues", "label":"Elements to update:","widgettype":"combo","datatype":"text","layoutname":"grl_option_parameters","layoutorder":4,
-"comboIds":["allValues", "nullStreet", "nullPostnumber", "nullPostcomplement"], "comboNames":["ALL ELEMENTS", "ELEMENTS WITH NULL STREETAXIS", "ELEMENTS WITH NULL POSTNUMBER", "ELEMENTS WITH NULL POSTCOMPLEMENT"], "selectedId":"nullStreet"}]'::json, NULL, true);
+"comboIds":["allValues", "nullStreet", "nullPostnumber", "nullPostcomplement"], "comboNames":["ALL ELEMENTS", "ELEMENTS WITH NULL STREETAXIS", "ELEMENTS WITH NULL POSTNUMBER", "ELEMENTS WITH NULL POSTCOMPLEMENT"]}]'::json, NULL, true);
 
 INSERT INTO inp_gully SELECT gully_id FROM gully ON CONFLICT DO NOTHING;
 INSERT INTO inp_netgully SELECT node_id FROM man_netgully ON CONFLICT DO NOTHING;
