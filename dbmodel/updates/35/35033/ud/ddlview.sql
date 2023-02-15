881f408f27571689e74863731d3363ca32fe384e
Barbara Rzepka <bbrzepk@bgeo.es>
2023-02-15 09:30:50 +0100

    recreate v_edit_inp_gully to connect correctly links on epa

--- dbmodel/updates/35/35033/ud/ddlview.sql
+++ dbmodel/updates/35/35033/ud/ddlview.sql
@@ -526,3 +526,58 @@ CREATE OR REPLACE VIEW v_edit_inp_storage
      JOIN inp_storage USING (node_id)
      JOIN value_state_type ON value_state_type.id = v_node.state_type
   WHERE value_state_type.is_operative IS TRUE;
+
+
+CREATE OR REPLACE VIEW v_edit_inp_gully
+ AS
+ SELECT g.gully_id,
+    g.code,
+    g.top_elev,
+    g.gully_type,
+    g.gratecat_id,
+    (g.grate_width / 100::numeric)::numeric(12,2) AS grate_width,
+    (g.grate_length / 100::numeric)::numeric(12,2) AS grate_length,
+    g.arc_id,
+    CASE WHEN pjoint_type = 'NODE' THEN pjoint_id ELSE a.node_2 END AS node_id,
+    s.sector_id,
+    g.expl_id,
+    g.state,
+    g.state_type,
+    g.the_geom,
+    g.units,
+    g.units_placement,
+    g.groove,
+    g.groove_height,
+    g.groove_length,
+    cat_grate.a_param,
+    cat_grate.b_param,
+        CASE
+            WHEN g.units_placement::text = 'LENGTH-SIDE'::text THEN (COALESCE(g.units::integer, 1)::numeric * g.grate_width / 100::numeric)::numeric(12,3)
+            WHEN g.units_placement::text = 'WIDTH-SIDE'::text THEN (COALESCE(g.units::integer, 1)::numeric * g.grate_length / 100::numeric)::numeric(12,3)
+            ELSE (cat_grate.width / 100::numeric)::numeric(12,3)
+        END AS total_width,
+        CASE
+            WHEN g.units_placement::text = 'LENGTH-SIDE'::text THEN (COALESCE(g.units::integer, 1)::numeric * g.grate_width / 100::numeric)::numeric(12,3)
+            WHEN g.units_placement::text = 'WIDTH-SIDE'::text THEN (COALESCE(g.units::integer, 1)::numeric * g.grate_length / 100::numeric)::numeric(12,3)
+            ELSE (cat_grate.length / 100::numeric)::numeric(12,3)
+        END AS total_length,
+    g.ymax - COALESCE(g.sandbox, 0::numeric) AS depth,
+    g.annotation,
+    i.outlet_type,
+    i.custom_top_elev,
+    i.custom_width,
+    i.custom_length,
+    i.custom_depth,
+    i.method,
+    i.weir_cd,
+    i.orifice_cd,
+    i.custom_a_param,
+    i.custom_b_param,
+    i.efficiency
+   FROM selector_sector s,
+    v_edit_gully g
+     JOIN inp_gully i USING (gully_id)
+     JOIN cat_grate ON g.gratecat_id::text = cat_grate.id::text
+     JOIN value_state_type vs ON vs.id = g.state_type
+     LEFT JOIN arc a USING (arc_id)
+  WHERE g.sector_id = s.sector_id AND s.cur_user = "current_user"()::text AND vs.is_operative IS TRUE;
