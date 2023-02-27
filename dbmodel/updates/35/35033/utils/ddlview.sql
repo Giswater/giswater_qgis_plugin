ed4ae4077c480a29aeb604fbb330988edb09af3c
Xavier Torret - BGEO <xtorret@bgeo.es>
2023-02-27 13:19:18 +0100

    Bug fix on v_plan_current_psector view

--- dbmodel/updates/35/35033/utils/ddlview.sql
+++ dbmodel/updates/35/35033/utils/ddlview.sql
@@ -55,3 +55,195 @@ UNION
    FROM selector_sector,
     node_border_sector
   WHERE selector_sector.cur_user = "current_user"()::text AND node_border_sector.sector_id = selector_sector.sector_id;
+  
+  
+  
+create or replace view v_plan_current_psector as
+SELECT plan_psector.psector_id,
+    plan_psector.name,
+    plan_psector.psector_type,
+    plan_psector.descript,
+    plan_psector.priority,
+    a.suma::numeric(14,2) AS total_arc,
+    b.suma::numeric(14,2) AS total_node,
+    c.suma::numeric(14,2) AS total_other,
+    plan_psector.text1,
+    plan_psector.text2,
+    plan_psector.observ,
+    plan_psector.rotation,
+    plan_psector.scale,
+    plan_psector.active,
+    (
+        CASE
+            WHEN a.suma IS NULL THEN 0::numeric
+            ELSE a.suma
+        END +
+        CASE
+            WHEN b.suma IS NULL THEN 0::numeric
+            ELSE b.suma
+        END +
+        CASE
+            WHEN c.suma IS NULL THEN 0::numeric
+            ELSE c.suma
+        END)::numeric(14,2) AS pem,
+    plan_psector.gexpenses,
+    ((100::numeric + plan_psector.gexpenses) / 100::numeric)::numeric(14,2) * (
+        CASE
+            WHEN a.suma IS NULL THEN 0::numeric
+            ELSE a.suma
+        END +
+        CASE
+            WHEN b.suma IS NULL THEN 0::numeric
+            ELSE b.suma
+        END +
+        CASE
+            WHEN c.suma IS NULL THEN 0::numeric
+            ELSE c.suma
+        END)::numeric(14,2) AS pec,
+    plan_psector.vat,
+    ((100::numeric + plan_psector.gexpenses) / 100::numeric * ((100::numeric + plan_psector.vat) / 100::numeric))::numeric(14,2) * (
+        CASE
+            WHEN a.suma IS NULL THEN 0::numeric
+            ELSE a.suma
+        END +
+        CASE
+            WHEN b.suma IS NULL THEN 0::numeric
+            ELSE b.suma
+        END +
+        CASE
+            WHEN c.suma IS NULL THEN 0::numeric
+            ELSE c.suma
+        END)::numeric(14,2) AS pec_vat,
+    plan_psector.other,
+    ((100::numeric + plan_psector.gexpenses) / 100::numeric * ((100::numeric + plan_psector.vat) / 100::numeric) * ((100::numeric + plan_psector.other) / 100::numeric))::numeric(14,2) * (
+        CASE
+            WHEN a.suma IS NULL THEN 0::numeric
+            ELSE a.suma
+        END +
+        CASE
+            WHEN b.suma IS NULL THEN 0::numeric
+            ELSE b.suma
+        END +
+        CASE
+            WHEN c.suma IS NULL THEN 0::numeric
+            ELSE c.suma
+        END)::numeric(14,2) AS pca,
+    plan_psector.the_geom
+   FROM config_param_user, plan_psector
+LEFT JOIN ( SELECT sum(v_plan_psector_x_arc.total_budget) AS suma,
+            v_plan_psector_x_arc.psector_id
+           FROM ( SELECT row_number() OVER (ORDER BY v_plan_arc.arc_id) AS rid,
+                    v_plan_arc.arc_id,
+                    v_plan_arc.node_1,
+                    v_plan_arc.node_2,
+                    v_plan_arc.arc_type,
+                    v_plan_arc.arccat_id,
+                    v_plan_arc.epa_type,
+                    v_plan_arc.state,
+                    v_plan_arc.sector_id,
+                    v_plan_arc.expl_id,
+                    v_plan_arc.annotation,
+                    v_plan_arc.soilcat_id,
+                    v_plan_arc.y1,
+                    v_plan_arc.y2,
+                    v_plan_arc.mean_y,
+                    v_plan_arc.z1,
+                    v_plan_arc.z2,
+                    v_plan_arc.thickness,
+                    v_plan_arc.width,
+                    v_plan_arc.b,
+                    v_plan_arc.bulk,
+                    v_plan_arc.geom1,
+                    v_plan_arc.area,
+                    v_plan_arc.y_param,
+                    v_plan_arc.total_y,
+                    v_plan_arc.rec_y,
+                    v_plan_arc.geom1_ext,
+                    v_plan_arc.calculed_y,
+                    v_plan_arc.m3mlexc,
+                    v_plan_arc.m2mltrenchl,
+                    v_plan_arc.m2mlbottom,
+                    v_plan_arc.m2mlpav,
+                    v_plan_arc.m3mlprotec,
+                    v_plan_arc.m3mlfill,
+                    v_plan_arc.m3mlexcess,
+                    v_plan_arc.m3exc_cost,
+                    v_plan_arc.m2trenchl_cost,
+                    v_plan_arc.m2bottom_cost,
+                    v_plan_arc.m2pav_cost,
+                    v_plan_arc.m3protec_cost,
+                    v_plan_arc.m3fill_cost,
+                    v_plan_arc.m3excess_cost,
+                    v_plan_arc.cost_unit,
+                    v_plan_arc.pav_cost,
+                    v_plan_arc.exc_cost,
+                    v_plan_arc.trenchl_cost,
+                    v_plan_arc.base_cost,
+                    v_plan_arc.protec_cost,
+                    v_plan_arc.fill_cost,
+                    v_plan_arc.excess_cost,
+                    v_plan_arc.arc_cost,
+                    v_plan_arc.cost,
+                    v_plan_arc.length,
+                    v_plan_arc.budget,
+                    v_plan_arc.other_budget,
+                    v_plan_arc.total_budget,
+                    v_plan_arc.the_geom,
+                    plan_psector_x_arc.id,
+                    plan_psector_x_arc.arc_id,
+                    plan_psector_x_arc.psector_id,
+                    plan_psector_x_arc.state,
+                    plan_psector_x_arc.doable,
+                    plan_psector_x_arc.descript,
+                    plan_psector_x_arc.addparam
+                   FROM v_plan_arc
+                     JOIN plan_psector_x_arc ON plan_psector_x_arc.arc_id::text = v_plan_arc.arc_id::text
+                     JOIN plan_psector plan_psector_1 ON plan_psector_1.psector_id = plan_psector_x_arc.psector_id
+                  ORDER BY plan_psector_x_arc.psector_id) v_plan_psector_x_arc(rid, arc_id, node_1, node_2, arc_type, arccat_id, epa_type, state, sector_id, expl_id, annotation, soilcat_id, y1, y2, mean_y, z1, z2, thickness, width, b, bulk, geom1, area, y_param, total_y, rec_y, geom1_ext, calculed_y, m3mlexc, m2mltrenchl, m2mlbottom, m2mlpav, m3mlprotec, m3mlfill, m3mlexcess, m3exc_cost, m2trenchl_cost, m2bottom_cost, m2pav_cost, m3protec_cost, m3fill_cost, m3excess_cost, cost_unit, pav_cost, exc_cost, trenchl_cost, base_cost, protec_cost, fill_cost, excess_cost, arc_cost, cost, length, budget, other_budget, total_budget, the_geom, id, arc_id_1, psector_id, state_1, doable, descript, addparam)
+          GROUP BY v_plan_psector_x_arc.psector_id) a ON a.psector_id = plan_psector.psector_id
+     LEFT JOIN ( SELECT sum(v_plan_psector_x_node.budget) AS suma,
+            v_plan_psector_x_node.psector_id
+           FROM ( SELECT row_number() OVER (ORDER BY v_plan_node.node_id) AS rid,
+                    v_plan_node.node_id,
+                    v_plan_node.nodecat_id,
+                    v_plan_node.node_type,
+                    v_plan_node.top_elev,
+                    v_plan_node.elev,
+                    v_plan_node.epa_type,
+                    v_plan_node.state,
+                    v_plan_node.sector_id,
+                    v_plan_node.expl_id,
+                    v_plan_node.annotation,
+                    v_plan_node.cost_unit,
+                    v_plan_node.descript,
+                    v_plan_node.cost,
+                    v_plan_node.measurement,
+                    v_plan_node.budget,
+                    v_plan_node.the_geom,
+                    plan_psector_x_node.id,
+                    plan_psector_x_node.node_id,
+                    plan_psector_x_node.psector_id,
+                    plan_psector_x_node.state,
+                    plan_psector_x_node.doable,
+                    plan_psector_x_node.descript
+                   FROM v_plan_node
+                     JOIN plan_psector_x_node ON plan_psector_x_node.node_id::text = v_plan_node.node_id::text
+                     JOIN plan_psector plan_psector_1 ON plan_psector_1.psector_id = plan_psector_x_node.psector_id
+                  ORDER BY plan_psector_x_node.psector_id) v_plan_psector_x_node(rid, node_id, nodecat_id, node_type, top_elev, elev, epa_type, state, sector_id, expl_id, annotation, cost_unit, descript, cost, measurement, budget, the_geom, id, node_id_1, psector_id, state_1, doable, descript_1)
+          GROUP BY v_plan_psector_x_node.psector_id) b ON b.psector_id = plan_psector.psector_id
+     LEFT JOIN ( SELECT sum(v_plan_psector_x_other.total_budget) AS suma,
+            v_plan_psector_x_other.psector_id
+           FROM ( SELECT plan_psector_x_other.id,
+                    plan_psector_x_other.psector_id,
+                    plan_psector_1.psector_type,
+                    v_price_compost.id AS price_id,
+                    v_price_compost.descript,
+                    v_price_compost.price,
+                    plan_psector_x_other.measurement,
+                    (plan_psector_x_other.measurement * v_price_compost.price)::numeric(14,2) AS total_budget
+                   FROM plan_psector_x_other
+                     JOIN v_price_compost ON v_price_compost.id::text = plan_psector_x_other.price_id::text
+                     JOIN plan_psector plan_psector_1 ON plan_psector_1.psector_id = plan_psector_x_other.psector_id
+                  ORDER BY plan_psector_x_other.psector_id) v_plan_psector_x_other
+          GROUP BY v_plan_psector_x_other.psector_id) c ON c.psector_id = plan_psector.psector_id
+       WHERE config_param_user.cur_user = "current_user"()::text and parameter = 'plan_psector_vdefault' and value::integer = plan_psector.psector_id;
