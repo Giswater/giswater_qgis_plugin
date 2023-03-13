ff8613d7d1df312f0678657e9169498365903e2c
Albert Bofill <abofill@bgeo.es>
2023-03-13 16:47:20 +0100

    Rename v_plan_current_psector_* views to v_plan_psector_*

---
+++ dbmodel/updates/35/35034/utils/dml.sql
@@ -0,0 +1,11 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+INSERT INTO config_param_system ("parameter", value, descript, "label", dv_querytext, dv_filterbyfield, isenabled, layoutorder, project_type, dv_isparent, isautoupdate, "datatype", widgettype, ismandatory, iseditable, dv_orderby_id, dv_isnullvalue, stylesheet, widgetcontrols, placeholder, standardvalue, layoutname)
+VALUES('edit_connec_downgrade_force', 'false', 'If true allow downgrade connecs no matter if they have operative hydrometers related', NULL, NULL, NULL, false, NULL, 'utils', NULL, NULL, 'boolean', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
