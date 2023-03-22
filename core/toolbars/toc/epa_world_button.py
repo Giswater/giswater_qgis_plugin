cbaa2f0f317d795ddd59be1dac8c730cb524577f
Sergi Maspons <smaspons@bgeo.es>
2023-03-22 11:24:30 +0100

    Refactor: epa world button -- make methods static

--- core/toolbars/toc/epa_world_button.py
+++ core/toolbars/toc/epa_world_button.py
@@ -12,6 +12,83 @@ from ....lib import tools_qgis, tools_db, tools_os
 from .... import global_vars
 
 
+def _get_sectors():
+    sectors = "NULL"
+
+    # get selected selectors
+    sql = f'SELECT sector_id FROM selector_sector WHERE cur_user = current_user'
+    rows = tools_db.get_rows(sql)
+    if rows:
+        sectors = ", ".join(str(x[0]) for x in rows)
+
+    return sectors
+
+
+def _get_layers():
+
+    arc_layers = tools_gw.get_layers_from_feature_type('arc')
+    node_layers = tools_gw.get_layers_from_feature_type('node')
+    connec_layers = tools_gw.get_layers_from_feature_type('connec')
+    gully_layers = tools_gw.get_layers_from_feature_type('gully')
+    link_layers = tools_gw.get_layers_from_feature_type('link')
+
+    return arc_layers, node_layers, connec_layers, gully_layers, link_layers
+
+
+def set_epa_world(_set_epa_world=None):
+    # Get layers
+    arc_layers, node_layers, connec_layers, gully_layers, link_layers = _get_layers()
+
+    # Get set_epa_world from config
+    if _set_epa_world is None:
+        _set_epa_world = tools_os.set_boolean(tools_gw.get_config_parser("epa_world", "epa_world_active", 'user', 'session'), False)
+
+    if not _set_epa_world:
+        # disable filters
+        for layer in arc_layers + node_layers + connec_layers + gully_layers + link_layers:
+            layer.setSubsetString(None)
+    else:
+        sectors = _get_sectors()
+        inp_options_networkmode = tools_gw.get_config_value('inp_options_networkmode')
+        sql = f"is_operative = true AND epa_type != 'UNDEFINED' AND sector_id IN ({sectors})"
+        # arc and node
+        for layer in arc_layers + node_layers:
+            layer.setSubsetString(sql)
+
+        if global_vars.project_type == 'ws':
+            # ws connec
+            for layer in connec_layers:
+                if inp_options_networkmode == 4:
+                    layer.setSubsetString(sql)
+                else:
+                    layer.setSubsetString("FALSE")
+
+            # ws link
+            for layer in link_layers:
+                if inp_options_networkmode == 4:
+                    layer.setSubsetString(sql.replace('is_operative = true AND epa_type != \'UNDEFINED\' AND ', ''))
+                else:
+                    layer.setSubsetString("FALSE")
+
+        elif global_vars.project_type == 'ud':
+            # ud connec
+            for layer in connec_layers:
+                layer.setSubsetString("FALSE")
+
+            # ud gully
+            for layer in gully_layers:
+                if inp_options_networkmode == 2:
+                    layer.setSubsetString(sql)
+                else:
+                    layer.setSubsetString("FALSE")
+
+            # ud link
+            for layer in link_layers:
+                layer.setSubsetString("FALSE")
+
+    return _set_epa_world
+
+
 class GwEpaWorldButton(GwAction):
     """ Button 308: Switch EPA world """
 
@@ -20,10 +97,9 @@ class GwEpaWorldButton(GwAction):
         super().__init__(icon_path, action_name, text, toolbar, action_group)
         self.action.setCheckable(True)
         # Apply filter if epa world active
-        epa_world_active = tools_os.set_boolean(tools_gw.get_config_parser("epa_world", "epa_world_active", 'user', 'session'), False)
-
-        set_epa_world = epa_world_active
-        self._set_epa_world(set_epa_world)
+        checked = set_epa_world()
+        # Set action checked
+        self._action_set_checked(checked)
 
 
     def clicked_event(self):
@@ -39,86 +115,24 @@ class GwEpaWorldButton(GwAction):
         epa_world_active = tools_os.set_boolean(tools_gw.get_config_parser("epa_world", "epa_world_active", 'user', 'session'))
 
         # Apply filters
-        set_epa_world = not epa_world_active
-        self._set_epa_world(set_epa_world)
+        _set_epa_world = not epa_world_active
+        set_epa_world(_set_epa_world)
 
         tools_gw.set_config_parser("epa_world", "epa_world_active", str(not epa_world_active), 'user', 'session')
 
+        # Set action checked
+        self._action_set_checked(_set_epa_world)
 
-    def _set_epa_world(self, set_epa_world):
-        # Get layers
-        arc_layers, node_layers, connec_layers, gully_layers, link_layers = self._get_layers()
-
-        if not set_epa_world:
-            # disable filters
-            for layer in arc_layers + node_layers + connec_layers + gully_layers + link_layers:
-                layer.setSubsetString(None)
+        # Show message
+        if _set_epa_world:
+            msg = "Epa world activated"
         else:
-            sectors = self._get_sectors()
-            inp_options_networkmode = tools_gw.get_config_value('inp_options_networkmode')
-            sql = f"is_operative = true AND epa_type != 'UNDEFINED' AND sector_id IN ({sectors})"
-            # arc and node
-            for layer in arc_layers + node_layers:
-                layer.setSubsetString(sql)
+            msg = "Epa world deactivated"
+        tools_qgis.show_info(msg)
 
-            if global_vars.project_type == 'ws':
-                # ws connec
-                for layer in connec_layers:
-                    if inp_options_networkmode == 4:
-                        layer.setSubsetString(sql)
-                    else:
-                        layer.setSubsetString("FALSE")
 
-                # ws link
-                for layer in link_layers:
-                    if inp_options_networkmode == 4:
-                        layer.setSubsetString(sql.replace('is_operative = true AND epa_type != \'UNDEFINED\' AND ', ''))
-                    else:
-                        layer.setSubsetString("FALSE")
-
-            elif global_vars.project_type == 'ud':
-                # ud connec
-                for layer in connec_layers:
-                    layer.setSubsetString("FALSE")
-
-                # ud gully
-                for layer in gully_layers:
-                    if inp_options_networkmode == 2:
-                        layer.setSubsetString(sql)
-                    else:
-                        layer.setSubsetString("FALSE")
-
-                # ud link
-                for layer in link_layers:
-                    layer.setSubsetString("FALSE")
-
+    def _action_set_checked(self, checked):
         # Set checked
-        if set_epa_world:
-            self.action.setChecked(True)
-        else:
-            self.action.setChecked(False)
-
-
-    def _get_layers(self):
-
-        arc_layers = tools_gw.get_layers_from_feature_type('arc')
-        node_layers = tools_gw.get_layers_from_feature_type('node')
-        connec_layers = tools_gw.get_layers_from_feature_type('connec')
-        gully_layers = tools_gw.get_layers_from_feature_type('gully')
-        link_layers = tools_gw.get_layers_from_feature_type('link')
-
-        return arc_layers, node_layers, connec_layers, gully_layers, link_layers
-
-
-    def _get_sectors(self):
-        sectors = ""
-
-        # get selected selectors
-        sql = f'SELECT sector_id FROM selector_sector WHERE cur_user = current_user'
-        rows = tools_db.get_rows(sql)
-        if rows:
-            sectors = ", ".join(str(x[0]) for x in rows)
-
-        return sectors
+        self.action.setChecked(checked)
 
     # endregion
