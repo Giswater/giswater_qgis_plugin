c193525dc22ab9ef0b91de544d33b62b0df42585
Barbara Rzepka <bbrzepk@bgeo.es>
2023-03-24 09:10:19 +0100

    ws trg vi - fix insert inp_dscenario_demand
    (cherry picked from commit d2712155b46e7ff15395872463e019ecb23283be)

--- dbmodel/ws/ftrg/ws_gw_trg_vi.sql
+++ dbmodel/ws/ftrg/ws_gw_trg_vi.sql
@@ -80,7 +80,7 @@ BEGIN
 		INSERT INTO inp_tags(feature_type, feature_id, tag) VALUES (NEW.feature_type, NEW.feature_id, NEW.tag);
 	    
 	  ELSIF v_view='vi_demands' THEN 
-		INSERT INTO inp_dscenario_demand (feature_id, demand, pattern_id, demand_type) VALUES (NEW.feature_id, NEW.demand, NEW.pattern_id, NEW.demand_type);
+		INSERT INTO inp_dscenario_demand (feature_id, demand, pattern_id, demand_type) VALUES (NEW.feature_id, NEW.demand, NEW.pattern_id, NEW.other);
       	    
 	  ELSIF v_view='vi_patterns' THEN 
 		INSERT INTO inp_pattern_value (pattern_id, factor_1,factor_2,factor_3,factor_4,factor_5,factor_6,factor_7,factor_8, factor_9,factor_10,
