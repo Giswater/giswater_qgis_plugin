b03c6c05302139182de7a05d4823687646f0e0ce
Barbara Rzepka <bbrzepk@bgeo.es>
2023-04-03 17:45:39 +0200

    remove schema name from updates
    (cherry picked from commit bafc0aedcf0e735bf821b64045d758ce46722b16)

--- dbmodel/updates/35/35035/ud/ddlview.sql
+++ dbmodel/updates/35/35035/ud/ddlview.sql
@@ -8,7 +8,7 @@ This version of Giswater is provided by Giswater Association
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 
-CREATE OR REPLACE VIEW ud_sample.v_state_connec
+CREATE OR REPLACE VIEW v_state_connec
  AS
  SELECT DISTINCT ON ((a.connec_id::character varying(30))) a.connec_id::character varying(30) AS connec_id,
     a.arc_id
@@ -16,27 +16,27 @@ CREATE OR REPLACE VIEW ud_sample.v_state_connec
                  SELECT connec.connec_id,
                     connec.arc_id,
                     1 AS flag
-                   FROM ud_sample.selector_state,
-                    ud_sample.selector_expl,
-                    ud_sample.connec
+                   FROM selector_state,
+                    selector_expl,
+                    connec
                   WHERE connec.state = selector_state.state_id AND (connec.expl_id = selector_expl.expl_id OR connec.expl_id2 = selector_expl.expl_id) AND selector_state.cur_user = "current_user"()::text AND selector_expl.cur_user = "current_user"()::text
                 EXCEPT
                  SELECT plan_psector_x_connec.connec_id,
                     plan_psector_x_connec.arc_id,
                     1 AS flag
-                   FROM ud_sample.selector_psector,
-                    ud_sample.selector_expl,
-                    ud_sample.plan_psector_x_connec
-                     JOIN ud_sample.plan_psector ON plan_psector.psector_id = plan_psector_x_connec.psector_id
+                   FROM selector_psector,
+                    selector_expl,
+                    plan_psector_x_connec
+                     JOIN plan_psector ON plan_psector.psector_id = plan_psector_x_connec.psector_id
                   WHERE plan_psector_x_connec.psector_id = selector_psector.psector_id AND selector_psector.cur_user = "current_user"()::text AND plan_psector_x_connec.state = 0 AND plan_psector.expl_id = selector_expl.expl_id AND selector_expl.cur_user = "current_user"()::text
         ) UNION
          SELECT plan_psector_x_connec.connec_id,
             plan_psector_x_connec.arc_id,
             2 AS flag
-           FROM ud_sample.selector_psector,
-            ud_sample.selector_expl,
-            ud_sample.plan_psector_x_connec
-             JOIN ud_sample.plan_psector ON plan_psector.psector_id = plan_psector_x_connec.psector_id
+           FROM selector_psector,
+            selector_expl,
+            plan_psector_x_connec
+             JOIN plan_psector ON plan_psector.psector_id = plan_psector_x_connec.psector_id
           WHERE plan_psector_x_connec.psector_id = selector_psector.psector_id AND selector_psector.cur_user = "current_user"()::text AND plan_psector_x_connec.state = 1 AND plan_psector.expl_id = selector_expl.expl_id AND selector_expl.cur_user = "current_user"()::text
   ORDER BY 1, 3 DESC) a;
 
