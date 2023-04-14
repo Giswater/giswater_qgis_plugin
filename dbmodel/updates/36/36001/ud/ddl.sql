89af3abfdb981fac2d899498d0d72bfc2c8ef5f4
Xavier Torret - BGEO <xtorret@bgeo.es>
2023-04-14 09:41:33 +0200

    Create ative column and modify vi views for non-visual obiects timeseries, curves, lids and patterns (ud project)

---
+++ dbmodel/updates/36/36001/ud/ddl.sql
@@ -0,0 +1,23 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+
+--2023/04/03
+
+-- create active for non visual objects
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"inp_timeseries", "column":"active", "dataType":"boolean", "isUtils":"False"}}$$);
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"inp_curve", "column":"active", "dataType":"boolean", "isUtils":"False"}}$$);
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"inp_lid", "column":"active", "dataType":"boolean", "isUtils":"False"}}$$);
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"inp_pattern", "column":"active", "dataType":"boolean", "isUtils":"False"}}$$);
+
+-- create vdefault
+ALTER TABLE inp_timeseries ALTER column active SET default true;
+ALTER TABLE inp_curve ALTER column active SET default true;
+ALTER TABLE inp_lid ALTER column active SET default true;
+ALTER TABLE inp_pattern ALTER column active SET default true;
--- dbmodel/updates/36/36001/ud/ddlview.sql
+++ dbmodel/updates/36/36001/ud/ddlview.sql
@@ -875,3 +875,169 @@ JOIN v_state_link USING (link_id);
 
 CREATE OR REPLACE VIEW v_edit_link AS SELECT *
 FROM v_link l;
+
+
+CREATE OR REPLACE VIEW vi_timeseries
+AS SELECT DISTINCT t.timser_id,
+    t.other1,
+    t.other2,
+    t.other3
+   FROM selector_expl s,
+    ( SELECT a.timser_id,
+            a.other1,
+            a.other2,
+            a.other3,
+            a.expl_id
+           FROM ( SELECT inp_timeseries_value.id,
+                    inp_timeseries_value.timser_id,
+                    inp_timeseries_value.date AS other1,
+                    inp_timeseries_value.hour AS other2,
+                    inp_timeseries_value.value AS other3,
+                    inp_timeseries.expl_id
+                   FROM inp_timeseries_value
+                     JOIN inp_timeseries ON inp_timeseries_value.timser_id::text = inp_timeseries.id::text
+                  WHERE inp_timeseries.times_type::text = 'ABSOLUTE'::text and active
+                UNION
+                 SELECT inp_timeseries_value.id,
+                    inp_timeseries_value.timser_id,
+                    concat('FILE', ' ', inp_timeseries.fname) AS other1,
+                    NULL::character varying AS other2,
+                    NULL::numeric AS other3,
+                    inp_timeseries.expl_id
+                   FROM inp_timeseries_value
+                     JOIN inp_timeseries ON inp_timeseries_value.timser_id::text = inp_timeseries.id::text
+                  WHERE inp_timeseries.times_type::text = 'FILE'::text and active
+                UNION
+                 SELECT inp_timeseries_value.id,
+                    inp_timeseries_value.timser_id,
+                    NULL::text AS other1,
+                    inp_timeseries_value."time" AS other2,
+                    inp_timeseries_value.value::numeric AS other3,
+                    inp_timeseries.expl_id
+                   FROM inp_timeseries_value
+                     JOIN inp_timeseries ON inp_timeseries_value.timser_id::text = inp_timeseries.id::text
+                  WHERE inp_timeseries.times_type::text = 'RELATIVE'::text and active) a
+          ORDER BY a.id) t
+  WHERE (t.expl_id = s.expl_id AND s.cur_user = "current_user"()::text OR t.expl_id IS NULL) AND (t.timser_id::text IN ( SELECT raingage.timser_id
+           FROM raingage))
+  ORDER BY t.timser_id, t.other1, t.other2;
+  
+  
+  
+  
+CREATE OR REPLACE VIEW vi_curves
+SELECT a.curve_id,
+    a.curve_type,
+    a.x_value,
+    a.y_value
+   FROM ( WITH qt AS (
+                 SELECT inp_curve_value.id,
+                    inp_curve_value.curve_id,
+                        CASE
+                            WHEN inp_curve_value.id = (( SELECT min(sub.id) AS min
+                               FROM inp_curve_value sub
+                              WHERE sub.curve_id::text = inp_curve_value.curve_id::text)) THEN inp_typevalue.idval
+                            ELSE NULL::character varying
+                        END AS curve_type,
+                    inp_curve_value.x_value,
+                    inp_curve_value.y_value,
+                    c.expl_id
+                   FROM inp_curve c
+                     JOIN inp_curve_value ON c.id::text = inp_curve_value.curve_id::text
+                     LEFT JOIN inp_typevalue ON inp_typevalue.id::text = c.curve_type::text
+                  WHERE inp_typevalue.typevalue::text = 'inp_value_curve'::text and active
+                )
+         SELECT qt.id,
+            qt.curve_id,
+            qt.curve_type,
+            qt.x_value,
+            qt.y_value,
+            qt.expl_id
+           FROM qt
+             JOIN selector_expl s USING (expl_id)
+          WHERE s.cur_user = "current_user"()::text
+        UNION
+         SELECT qt.id,
+            qt.curve_id,
+            qt.curve_type,
+            qt.x_value,
+            qt.y_value,
+            qt.expl_id
+           FROM qt
+          WHERE qt.expl_id IS NULL) a
+  ORDER BY a.id;
+  
+
+
+CREATE OR REPLACE VIEW vi_patterns
+AS SELECT p.pattern_id,
+    p.pattern_type,
+    inp_pattern_value.factor_1,
+    inp_pattern_value.factor_2,
+    inp_pattern_value.factor_3,
+    inp_pattern_value.factor_4,
+    inp_pattern_value.factor_5,
+    inp_pattern_value.factor_6,
+    inp_pattern_value.factor_7,
+    inp_pattern_value.factor_8,
+    inp_pattern_value.factor_9,
+    inp_pattern_value.factor_10,
+    inp_pattern_value.factor_11,
+    inp_pattern_value.factor_12,
+    inp_pattern_value.factor_13,
+    inp_pattern_value.factor_14,
+    inp_pattern_value.factor_15,
+    inp_pattern_value.factor_16,
+    inp_pattern_value.factor_17,
+    inp_pattern_value.factor_18,
+    inp_pattern_value.factor_19,
+    inp_pattern_value.factor_20,
+    inp_pattern_value.factor_21,
+    inp_pattern_value.factor_22,
+    inp_pattern_value.factor_23,
+    inp_pattern_value.factor_24
+   FROM selector_expl s,
+    inp_pattern p
+     JOIN inp_pattern_value USING (pattern_id)
+  WHERE active and p.expl_id = s.expl_id AND s.cur_user = "current_user"()::text OR p.expl_id IS NULL
+  ORDER BY p.pattern_id;
+
+
+
+cREATE OR REPLACE VIEW vi_lid_controls
+AS SELECT a.lidco_id,
+    a.lidco_type,
+    a.other1,
+    a.other2,
+    a.other3,
+    a.other4,
+    a.other5,
+    a.other6,
+    a.other7
+   FROM ( SELECT 0 AS id,
+            inp_lid.lidco_id,
+            inp_lid.lidco_type,
+            NULL::numeric AS other1,
+            NULL::numeric AS other2,
+            NULL::numeric AS other3,
+            NULL::numeric AS other4,
+            NULL::text AS other5,
+            NULL::text AS other6,
+            NULL::text AS other7
+           FROM inp_lid
+		   WHERE active
+        UNION
+         SELECT inp_lid_value.id,
+            inp_lid_value.lidco_id,
+            inp_typevalue.idval AS lidco_type,
+            inp_lid_value.value_2 AS other1,
+            inp_lid_value.value_3 AS other2,
+            inp_lid_value.value_4 AS other3,
+            inp_lid_value.value_5 AS other4,
+            inp_lid_value.value_6 AS other5,
+            inp_lid_value.value_7 AS other6,
+            inp_lid_value.value_8 AS other7
+           FROM inp_lid_value join inp_lid using (lidco_id)
+             LEFT JOIN inp_typevalue ON inp_typevalue.id::text = inp_lid_value.lidlayer::text
+          WHERE active and inp_typevalue.typevalue::text = 'inp_value_lidlayer'::text) a
+  ORDER BY a.lidco_id, a.id;
