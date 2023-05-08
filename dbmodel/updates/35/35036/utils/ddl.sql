ab82d54bcc8df034708a8bba1e4ab744e25a36b5
xtorret@bgeo.es <xtorret@bgeo.es>
2023-05-08 14:47:00 +0200

    Create new column addparam for anl_node, anl_arc, anl_connec & anl_gully tables
    (cherry picked from commit 04bb09cbf621293cffb31688b00081f739a9076d)

---
+++ dbmodel/updates/35/35036/utils/ddl.sql
@@ -0,0 +1,13 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+
+SELECT ws.gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"anl_node", "column":"addparam", "dataType":"text"}}$$);
+SELECT ws.gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"anl_arc", "column":"addparam", "dataType":"text"}}$$);
+SELECT ws.gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"anl_connec", "column":"addparam", "dataType":"text"}}$$);
