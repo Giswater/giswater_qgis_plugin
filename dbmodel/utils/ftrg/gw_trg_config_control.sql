b16c7b7dcaafff33f173531054be4e7871044c0d
Xavier Torret <xtorret@bgeo.es>
2023-05-10 16:08:53 +0200

    Enhance trigger config_control allowing typeahead with null values on dv_parent_id and when dv_querytext_filterc exists
    (cherry picked from commit 85945111a9e28f6a6fe8f7483f346e72c12f73e4)

--- dbmodel/utils/ftrg/gw_trg_config_control.sql
+++ dbmodel/utils/ftrg/gw_trg_config_control.sql
@@ -56,7 +56,6 @@ BEGIN
 						PERFORM gw_fct_getmessage(v_message);
 					END IF;
 				END LOOP;
-
 			END IF;
 
 		ELSIF v_configtable = 'config_form_fields' THEN 
@@ -75,29 +74,33 @@ BEGIN
 					END IF;
 
 					--check that when dv_querytextfilterc exists dv_parent_id also
-					IF NEW.dv_querytext_filterc IS NOT NULL THEN
+					IF NEW.dv_querytext_filterc IS NOT null THEN
 
-						--check if dv_parent_id is not null
-						IF NEW.dv_parent_id IS NULL THEN
+						--check if dv_parent_id is not null (only for combo because for typeahead null value means feature_id)
+						IF NEW.dv_parent_id IS null AND NEW.widgettype != 'typeahead' THEN
 							v_message = concat('{"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},"data":{"message":"3102", "function":"2816",
 							"debug":null, "variables":"',v_variables,'"}}');
 							PERFORM gw_fct_getmessage(v_message);
-							
-						ELSE
-							--check if dv_parent_id is correct (another existing columnname)
-							EXECUTE 'SELECT columnname FROM config_form_fields WHERE columnname = '||quote_literal(NEW.dv_parent_id)||' AND formname = '||
-							quote_literal(NEW.formname)||'' INTO v_widgettype;
+					
+						elsif NEW.dv_parent_id IS null AND NEW.widgettype = 'typeahead' THEN
+							NEW.dv_parent_id= '';
+						end if;
+					
+						--check if dv_parent_id is correct (another existing columnname)
+						EXECUTE 'SELECT columnname FROM config_form_fields WHERE columnname = '||quote_literal(NEW.dv_parent_id)||' AND formname = '||
+						quote_literal(NEW.formname)||'' INTO v_widgettype;
 
-							IF v_widgettype IS NULL THEN
-								v_message = concat('{"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},"data":{"message":"3104",
-								"function":"2816", "debug":null, "variables":"',v_variables,'"}}');
-								PERFORM gw_fct_getmessage(v_message);
-							END IF;	
-						END IF;
+						IF v_widgettype IS NULL and NEW.widgettype != 'typeahead'THEN
+							v_message = concat('{"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},"data":{"message":"3104",
+							"function":"2816", "debug":null, "variables":"',v_variables,'"}}');
+							PERFORM gw_fct_getmessage(v_message);
+						END IF;	
 					END IF;
+				END IF;
 
-					--check isparent/dv_parent_id
-					IF NEW.isparent IS TRUE THEN
+				--check isparent/dv_parent_id
+				IF NEW.isparent IS TRUE THEN
+				
 						--count if dv_parent_id exists before set isparent=TRUE
 						SELECT count(*) FROM config_form_fields WHERE formname=NEW.formname AND dv_parent_id=NEW.columnname INTO v_count;
 
@@ -106,26 +109,25 @@ BEGIN
 							"function":"2816", "debug":null, "variables":"',v_variables,'"}}');
 							PERFORM gw_fct_getmessage(v_message);
 						END IF;
-					END IF;
 				END IF;
 			
 				--check for typeahead some additional restrictions
 				IF NEW.widgettype = 'typeahead' THEN
-
+	
 					-- isautoupdate is FALSE
 					IF NEW.isautoupdate = TRUE THEN
 						PERFORM gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
 						"data":{"message":"3096", "function":"2816","debug":null}}$$);	
 					END IF;
-		
+			
 					--query text HAS SAME id THAN idval (with the exception of streetname and streename2)
 					IF NEW.dv_querytext IS NOT NULL THEN
-
+	
 						IF NEW.columnname = 'streetname' OR NEW.columnname = 'streetname2' THEN
 							-- do nothing (with the exception of streetname and streename2
 						ELSE
 							EXECUTE 'SELECT count(*) FROM( ' ||NEW.dv_querytext|| ')a WHERE id::text != idval::text' INTO v_count;
-
+	
 							IF v_count > 0 THEN
 								v_message = concat('{"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},"data":{"message":"3100",
 								"function":"2816", "debug":null, "variables":"',v_variables,'"}}');
@@ -136,18 +138,21 @@ BEGIN
 				END IF;
 
 			ELSIF TG_OP = 'UPDATE' THEN
-
+	
 				IF NEW.dv_parent_id IS NULL THEN
+			
 					-- check if related isparent is TRUE when setting dv_parent_id to NULL
-					IF (SELECT isparent FROM config_form_fields WHERE formname=NEW.formname AND columnname=OLD.dv_parent_id) IS TRUE THEN
+					IF (SELECT isparent FROM config_form_fields WHERE formname=NEW.formname AND columnname=OLD.dv_parent_id
+					and NEW.widgettype != 'typeahead') is true THEN 
+						
 						--only if there're no more fields related to the parent
 						SELECT count(*) FROM config_form_fields WHERE formname=NEW.formname AND dv_parent_id=OLD.dv_parent_id INTO v_count;
-							IF v_count = 1 THEN
-								
+					
+						IF v_count = 1 THEN		
 								v_message = concat('{"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},"data":{"message":"3170",
 								"function":"2816", "debug":null, "variables":"',v_variables,'"}}');
 								PERFORM gw_fct_getmessage(v_message);
-							END IF;
+						END IF;
 					END IF;
 				END IF;
 			END IF;
