670490e06fc125413d04455f7902cdad0e11b5dc
Maria Guzmán <mguzman@bgeo.es>
2023-05-12 12:17:55 +0200

    Fix indentation and syntax error (END IF;) on trg_link_data

--- dbmodel/utils/ftrg/gw_trg_link_data.sql
+++ dbmodel/utils/ftrg/gw_trg_link_data.sql
@@ -25,41 +25,42 @@ BEGIN
 
 	    IF v_feature_type = 'CONNEC' THEN
 
-		IF v_projecttype = 'WS' THEN  
-		    UPDATE link SET epa_type = NEW.epa_type, is_operative = v.is_operative, expl_id2 = NEW.expl_id2 
-		    FROM value_state_type v WHERE id = NEW.state_type AND feature_id = NEW.connec_id;
-		ELSE
-		    UPDATE link SET is_operative = v.is_operative, expl_id2 = NEW.expl_id2 
-		    FROM value_state_type v WHERE id = NEW.state_type AND feature_id = NEW.connec_id;
-		END IF;
-
-	    ELSIF v_feature_type = 'GULLY' THEN
-		UPDATE link SET epa_type = NEW.epa_type, is_operative = v.is_operative, expl_id2 = NEW.expl_id2
-		FROM value_state_type v WHERE id = NEW.state_type AND feature_id = NEW.gully_id;
+			IF v_projecttype = 'WS' THEN  
+				UPDATE link SET epa_type = NEW.epa_type, is_operative = v.is_operative, expl_id2 = NEW.expl_id2 
+				FROM value_state_type v WHERE id = NEW.state_type AND feature_id = NEW.connec_id;
+			ELSE
+				UPDATE link SET is_operative = v.is_operative, expl_id2 = NEW.expl_id2 
+				FROM value_state_type v WHERE id = NEW.state_type AND feature_id = NEW.connec_id;
+			END IF;
+			
+		ELSIF v_feature_type = 'GULLY' THEN
+			UPDATE link SET epa_type = NEW.epa_type, is_operative = v.is_operative, expl_id2 = NEW.expl_id2
+			FROM value_state_type v WHERE id = NEW.state_type AND feature_id = NEW.gully_id;
 
 	    ELSIF v_feature_type = 'LINK' THEN
-		IF v_projecttype = 'WS' THEN
-		    UPDATE link l SET epa_type = c.epa_type, is_operative = v.is_operative, expl_id2 = c.expl_id2 
-		    FROM connec c
-		    JOIN value_state_type v ON v.id = c.state_type WHERE l.feature_id = c.connec_id AND c.connec_id = NEW.feature_id;
-		ELSE
-		    UPDATE link l SET epa_type = c.epa_type, is_operative = v.is_operative, expl_id2 = c.expl_id2 
-		    FROM gully c
-		    JOIN value_state_type v ON v.id = c.state_type WHERE l.feature_id = c.gully_id AND c.gully_id = NEW.feature_id;
+			IF v_projecttype = 'WS' THEN
+				UPDATE link l SET epa_type = c.epa_type, is_operative = v.is_operative, expl_id2 = c.expl_id2 
+				FROM connec c
+				JOIN value_state_type v ON v.id = c.state_type WHERE l.feature_id = c.connec_id AND c.connec_id = NEW.feature_id;
+			ELSE
+				UPDATE link l SET epa_type = c.epa_type, is_operative = v.is_operative, expl_id2 = c.expl_id2 
+				FROM gully c
+				JOIN value_state_type v ON v.id = c.state_type WHERE l.feature_id = c.gully_id AND c.gully_id = NEW.feature_id;
 
-		    UPDATE link l SET is_operative = v.is_operative, expl_id2 = c.expl_id2 
-		    FROM connec c
-		    JOIN value_state_type v ON v.id = c.state_type WHERE l.feature_id = c.connec_id AND c.connec_id = NEW.feature_id;
+				UPDATE link l SET is_operative = v.is_operative, expl_id2 = c.expl_id2 
+				FROM connec c
+				JOIN value_state_type v ON v.id = c.state_type WHERE l.feature_id = c.connec_id AND c.connec_id = NEW.feature_id;
+			END IF;
 		END IF;
-	    END IF;
-
+		
 	ELSIF TG_OP = 'UPDATE' THEN
-
+		IF v_feature_type = 'LINK' THEN
 		-- only apply for traceability when the_geom changes
-		UPDATE link SET lastupdate = now(), lastupdate_user = current_user WHERE link_id = NEW.link_id;
+			UPDATE link SET lastupdate = now(), lastupdate_user = current_user WHERE link_id = NEW.link_id;
+		END IF;
 	END IF;
 	  
-	RETURN NEW;    
+RETURN NEW;    
     
 END;
 $BODY$
