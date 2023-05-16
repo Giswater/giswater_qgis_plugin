428df6583f2d7955ff99aa7fbeb45cf1b001d561
Xavier Torret <xtorret@bgeo.es>
2023-05-16 12:16:30 +0200

    Bug fix on check result function

--- dbmodel/ws/fct/ws_gw_fct_pg2epa_check_result.sql
+++ dbmodel/ws/fct/ws_gw_fct_pg2epa_check_result.sql
@@ -401,9 +401,9 @@ BEGIN
 				IF object_rec.colname IN ('arc', 'node') THEN
 				
 					EXECUTE 'INSERT INTO anl_'||object_rec.colname||' ('||object_rec.colname||'_id, fid, descript, the_geom) 
-					SELECT '||object_rec.colname||'_id, 396, concat(''Present on '',count(*),'' enabled dscenarios''), the_geom FROM v_edit_inp_dscenario_'||object_rec.tabname||' JOIN '||
-					object_rec.colname||' USING ('||object_rec.colname||'_id) GROUP BY '||object_rec.colname||'_id, v_edit_inp_dscenario_'||object_rec.tabname||'.the_geom having count(arc_id) > 1';
-
+					SELECT '||object_rec.colname||'_id, 396, concat(''Present on '',count(*),'' enabled dscenarios''), v_edit_inp_dscenario_'||object_rec.tabname||'.the_geom 
+					FROM v_edit_inp_dscenario_'||object_rec.tabname||' JOIN '||	object_rec.colname||' USING ('||object_rec.colname||'_id) GROUP 
+					BY '||object_rec.colname||'_id, v_edit_inp_dscenario_'||object_rec.tabname||'.the_geom having count(arc_id) > 1';
 
 
 					INSERT INTO audit_check_data (fid, result_id, criticity, error_message)
