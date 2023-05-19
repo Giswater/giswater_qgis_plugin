22d8fb1b39fdd6007d4d0268880a48696cfb33e3
xtorret@bgeo.es <xtorret@bgeo.es>
2023-05-19 11:12:18 +0200

    Add header on functions getvisit and getvisit_manager

--- dbmodel/utils/fct/gw_fct_getvisit.sql
+++ dbmodel/utils/fct/gw_fct_getvisit.sql
@@ -1,3 +1,11 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+--FUNCTION CODE: 2604
+
 CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_getvisit(p_data json)
  RETURNS json
  LANGUAGE plpgsql
