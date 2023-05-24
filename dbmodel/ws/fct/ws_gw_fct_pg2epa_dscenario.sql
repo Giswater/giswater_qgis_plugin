4652b82d543ac3b70df4ffa04e43ed25fe2bed0a
Xavier Torret <xtorret@bgeo.es>
2023-05-24 16:48:17 +0200

    Bug fix on dscenario pattern method for dma

--- dbmodel/ws/fct/ws_gw_fct_pg2epa_dscenario.sql
+++ dbmodel/ws/fct/ws_gw_fct_pg2epa_dscenario.sql
@@ -140,9 +140,9 @@ BEGIN
 			 AND temp_demand.pattern_id IS NULL;
 		
 		ELSIF v_patternmethod = 13 THEN -- DMA PATTERN (NODE)
-			UPDATE temp_demand SET pattern_id=sector.pattern_id FROM node JOIN dma ON dma.sector_id=node.dma_id WHERE temp_demand.feature_id=node.node_id
+			UPDATE temp_demand SET pattern_id=sector.pattern_id FROM node JOIN dma ON dma.dma_id=node.dma_id WHERE temp_demand.feature_id=node.node_id
 			 AND temp_demand.pattern_id IS NULL;
-			UPDATE temp_demand SET pattern_id=sector.pattern_id FROM connec JOIN dma ON dma.sector_id=connec.dma_id WHERE temp_demand.feature_id=connec.connec_id
+			UPDATE temp_demand SET pattern_id=sector.pattern_id FROM connec JOIN dma ON dma.dma_id=connec.dma_id WHERE temp_demand.feature_id=connec.connec_id
 			 AND temp_demand.pattern_id IS NULL;
 			
 		ELSIF v_patternmethod = 14 THEN -- FEATURE PATTERN (NODE)
