c1e03b54888400eb3d79d4666464a5a85d2c31da
Edgar Fuste <efuste@bgeo.es>
2023-05-26 10:31:19 +0200

    Enhancement: hide tab plan on info feature when device is 5

--- dbmodel/example/user/ws/ws_99_sample_other.sql
+++ dbmodel/example/user/ws/ws_99_sample_other.sql
@@ -996,3 +996,4 @@ GRANT SELECT, TRIGGER, UPDATE, DELETE, REFERENCES, INSERT, TRUNCATE ON TABLE ve_
 
 UPDATE config_form_tabs SET device = '{4,5}' where formname = 've_node_water_connection';
 UPDATE config_form_tabs SET device = '{5}' where formname like 'visit_%';
+UPDATE config_form_tabs SET device = '{4}' where formname like 'v_edit_%' and tabname = 'tab_plan';
