19fb2a78f3141757392123ee63c61b577150a1f8
Edgar Fuste <efuste@bgeo.es>
2023-05-26 10:45:40 +0200

    Enhancement: add column visit_type on v_ui_om_visit

--- dbmodel/updates/36/36001/utils/trg.sql
+++ dbmodel/updates/36/36001/utils/trg.sql
@@ -11,3 +11,21 @@ DROP TRIGGER gw_trg_link_data ON link;
 
 CREATE TRIGGER gw_trg_link_data  AFTER INSERT OR UPDATE of the_geom ON link
 FOR EACH ROW EXECUTE PROCEDURE gw_trg_link_data('link');
+
+CREATE OR REPLACE VIEW v_ui_om_visit
+AS SELECT om_visit.id,
+    om_visit_cat.name AS visit_catalog,
+    om_visit.ext_code,
+    om_visit.startdate,
+    om_visit.enddate,
+    om_visit.user_name,
+    om_visit.webclient_id,
+    exploitation.name AS exploitation,
+    om_visit.the_geom,
+    om_visit.descript,
+    om_visit.is_done,
+    om_visit.visit_type
+   FROM om_visit
+     JOIN om_visit_cat ON om_visit.visitcat_id = om_visit_cat.id
+     LEFT JOIN exploitation ON exploitation.expl_id = om_visit.expl_id;
+     
