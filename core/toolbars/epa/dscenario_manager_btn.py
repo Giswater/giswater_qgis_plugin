431411e2aa342517e8c2a645b45755e2d943fb65
Edgar Fuste <efuste@bgeo.es>
2023-05-31 10:06:09 +0200

    Minor bug fix: manage feature_type on tab change

--- core/toolbars/epa/dscenario_manager_btn.py
+++ core/toolbars/epa/dscenario_manager_btn.py
@@ -393,7 +393,7 @@ class GwDscenarioManagerButton(GwAction):
 
         for x in self.feature_types:
             col_idx = tools_qt.get_col_index_by_col_name(tableview, x)
-            if col_idx is not False:
+            if col_idx is not None:
                 feature_type = x
                 break
 
