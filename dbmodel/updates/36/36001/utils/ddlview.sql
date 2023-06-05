dbe8b85397e123c20c95a70dabe4a1333fdf7157
Albert Bofill <abofill@bgeo.es>
2023-06-05 12:38:18 +0200

    Add lastupdate and lastupdate_user to v_edit_element

---
+++ dbmodel/updates/36/36001/utils/ddlview.sql
@@ -0,0 +1,50 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+CREATE OR REPLACE VIEW v_edit_element
+AS SELECT element.element_id,
+    element.code,
+    element.elementcat_id,
+    cat_element.elementtype_id,
+    element.serial_number,
+    element.state,
+    element.state_type,
+    element.num_elements,
+    element.observ,
+    element.comment,
+    element.function_type,
+    element.category_type,
+    element.location_type,
+    element.fluid_type,
+    element.workcat_id,
+    element.workcat_id_end,
+    element.buildercat_id,
+    element.builtdate,
+    element.enddate,
+    element.ownercat_id,
+    element.rotation,
+    concat(element_type.link_path, element.link) AS link,
+    element.verified,
+    element.the_geom,
+    element.label_x,
+    element.label_y,
+    element.label_rotation,
+    element.publish,
+    element.inventory,
+    element.undelete,
+    element.expl_id,
+    element.pol_id,
+    element.lastupdate,
+    element.lastupdate_user
+   FROM selector_expl,
+    element
+     JOIN v_state_element ON element.element_id::text = v_state_element.element_id::text
+     JOIN cat_element ON element.elementcat_id::text = cat_element.id::text
+     JOIN element_type ON element_type.id::text = cat_element.elementtype_id::text
+  WHERE element.expl_id = selector_expl.expl_id AND selector_expl.cur_user = "current_user"()::text;
