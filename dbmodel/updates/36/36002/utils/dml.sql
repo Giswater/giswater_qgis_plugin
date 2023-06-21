bebcb2c8adde34d3c24116498b930c4adc1dcd08
Sergi Maspons <smaspons@bgeo.es>
2023-06-21 08:35:52 +0200

    Fix typo in dml

--- dbmodel/updates/36/36002/utils/dml.sql
+++ dbmodel/updates/36/36002/utils/dml.sql
@@ -17,4 +17,4 @@ isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytex
 FROM config_form_fields where columnname in ('expl_id2','top_elev','elevation') 
 and formname ilike 've_node';
 
-UPDATE config_form_tabs set device = '{4,5}' where forname = 'search';
+UPDATE config_form_tabs set device = '{4,5}' where formname = 'search';
