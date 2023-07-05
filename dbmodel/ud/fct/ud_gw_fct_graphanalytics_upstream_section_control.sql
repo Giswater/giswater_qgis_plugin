156a72d6ee66d09e9a4cfd1040385d87c5d4b9d4
Barbara Rzepka <bbrzepka@gmail.com>
2023-07-05 09:56:01 +0200

    section control - allow executing for entire outfall layer

--- dbmodel/ud/fct/ud_gw_fct_graphanalytics_upstream_section_control.sql
+++ dbmodel/ud/fct/ud_gw_fct_graphanalytics_upstream_section_control.sql
@@ -54,6 +54,11 @@ v_point public.geometry;
 
 v_fid integer =477;
 v_count integer;
+v_node_list text[];
+rec_node_id text;
+v_layer text;
+v_node_id text;
+v_exists boolean;
 BEGIN
 
 	-- Search path
@@ -65,7 +70,9 @@ BEGIN
 	v_ycoord := ((p_data ->> 'data')::json->> 'coordinates')::json->>'ycoord';
 	v_epsg := (SELECT epsg FROM sys_version ORDER BY id DESC LIMIT 1);
 	v_client_epsg := (p_data ->> 'client')::json->> 'epsg';
-	
+	v_node_id =json_extract_path_text(p_data,'data','parameters','node');
+	v_layer = json_extract_path_text(p_data,'feature','tableName');
+
 	IF v_client_epsg IS NULL THEN v_client_epsg = v_epsg; END IF;
 	
 	v_prev_cur_user = current_user;
@@ -81,30 +88,37 @@ BEGIN
 	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, concat('SECTION CONTROL ANALYSIS'));
 	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, '-------------------------------------------------------------');
 
-
 	-- select version
 	SELECT giswater INTO v_version FROM sys_version ORDER BY id DESC LIMIT 1;
 
-
 	SELECT gw_fct_json_object_set_key (p_data,'fid'::text, v_fid) INTO p_data;
 
-	-- Compute the tributary area using recursive function
-	EXECUTE 'SELECT gw_fct_graphanalytics_upstream_recursive($$'||p_data||'$$);'
-	INTO v_result_json;
+	IF v_node_id IS NULL AND v_layer in ('v_edit_storage', 'v_edit_inp_outfall') THEN 
+			EXECUTE 'SELECT array_agg(node_id) FROM '||v_layer||''
+			INTO v_node_list;
+	
+		FOREACH rec_node_id in ARRAY(v_node_list) LOOP	
+			p_data = jsonb_set(p_data::jsonb,'{data,parameters,node}'::text[], rec_node_id::jsonb);
+			EXECUTE 'SELECT gw_fct_graphanalytics_upstream_recursive($$'||p_data||'$$);'
+			INTO v_result_json;
+		END LOOP;
 
-	IF (v_result_json->>'status')::TEXT = 'Accepted' THEN
-
-		IF v_audit_result is null THEN
-			v_status = 'Accepted';
-			v_level = 3;
-			v_message = 'Analysis done successfully';
-		ELSE
-
-			SELECT ((((v_audit_result::json ->> 'body')::json ->> 'data')::json ->> 'info')::json ->> 'status')::text INTO v_status; 
-			SELECT ((((v_audit_result::json ->> 'body')::json ->> 'data')::json ->> 'info')::json ->> 'level')::integer INTO v_level;
-			SELECT ((((v_audit_result::json ->> 'body')::json ->> 'data')::json ->> 'info')::json ->> 'message')::text INTO v_message;
-
+	ELSIF v_node_id IS NOT NULL THEN
+		EXECUTE 'select exists(select 1 from '||v_layer||' where node_id='||quote_literal(v_node_id)||')' into v_exists; 
+		IF v_exists THEN
+			EXECUTE 'SELECT gw_fct_graphanalytics_upstream_recursive($$'||p_data||'$$);'
+			INTO v_result_json;
 		END IF;
+
+	END IF;
+
+	IF v_node_id IS NULL AND v_layer !='v_edit_inp_outfall' THEN
+		INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, 'In order to execute the process you need to specify starting node_id.');
+		INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, 'You can use outfalls without doing it and execute the process for the entire layer.');
+	ELSIF v_exists IS FALSE THEN
+		INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, concat('Node ',v_node_id ,' does''t exist in the selected layer.'));
+		INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, 'Choose another node to continue.');
+	ELSE
 
 		SELECT count(*) INTO v_count FROM anl_arc WHERE cur_user="current_user"() AND fid=v_fid;
 
@@ -119,7 +133,22 @@ BEGIN
 			SELECT v_fid,  concat ('Arc_id: ',string_agg(arc_id, ', '), '.' ), v_count 
 			FROM anl_arc WHERE cur_user="current_user"() AND fid=v_fid;
 		END IF;
-	
+	END IF;
+		
+
+		IF v_audit_result is null THEN
+			v_status = 'Accepted';
+			v_level = 3;
+			v_message = 'Analysis done successfully';
+		ELSE
+
+			SELECT ((((v_audit_result::json ->> 'body')::json ->> 'data')::json ->> 'info')::json ->> 'status')::text INTO v_status; 
+			SELECT ((((v_audit_result::json ->> 'body')::json ->> 'data')::json ->> 'info')::json ->> 'level')::integer INTO v_level;
+			SELECT ((((v_audit_result::json ->> 'body')::json ->> 'data')::json ->> 'info')::json ->> 'message')::text INTO v_message;
+
+		END IF;
+
+		
 		-- info
 		SELECT array_to_json(array_agg(row_to_json(row))) INTO v_result 
 		FROM (SELECT id, error_message as message FROM audit_check_data WHERE cur_user="current_user"() AND fid=v_fid order by  id asc) row;
@@ -158,9 +187,7 @@ BEGIN
 					  '"polygon":'||v_result_polygon||'}'||
 					 '}'
 			  '}')::json, 3176, null, null, null);
-	ELSE 
-		RETURN v_result_json;
-	END IF;
+
 
 	EXCEPTION WHEN OTHERS THEN
 	GET STACKED DIAGNOSTICS v_error_context = PG_EXCEPTION_CONTEXT;
@@ -170,4 +197,3 @@ END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-
