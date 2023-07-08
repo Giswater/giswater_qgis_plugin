78b8fff5d3edb904b1e07115746a60ba5f0c89b3
xtorret@bgeo.es <xtorret@bgeo.es>
2023-07-08 13:37:02 +0200

    Trigger for rpt cat result view

---
+++ dbmodel/updates/35/35039/ws/trg.sql
@@ -0,0 +1,11 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME ,public;
+
+CREATE TRIGGER gw_trg_ui_rpt_cat_result INSTEAD OF INSERT OR UPDATE OR DELETE
+ON v_ui_rpt_cat_result FOR EACH ROW EXECUTE PROCEDURE gw_trg_ui_rpt_cat_result();
