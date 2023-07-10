bc9bc084b69c93835cc16cf0fc64e4c08bc18fae
xtorret@bgeo.es <xtorret@bgeo.es>
2023-07-10 17:13:02 +0200

    Update query files to translate dbobjects

---
+++ resources/i18n/ca_ES_help4dbdiag.sql
@@ -0,0 +1,13 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+-- get priority in terms of translate
+select count(*), source, tt_es_es from i18n.dbdialog group by source, tt_es_es order by 1 desc
+
+select * from i18n.dbdialog
+
+-- translate for catalan language
---
+++ resources/i18n/ca_ES_help4pydiag.sql
@@ -0,0 +1,16 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+-- get priority in terms of translate
+select count(*), source from i18n.dbdialog group by source order by 1 desc
+
+
+select count(*), source, lb_enen from i18n.pydialog group by source,lb_enen order by 1 desc
+
+select * from i18n.pydialog
+
+-- translate for catalan language
