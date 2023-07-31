44d10bd092f0b6b53b30d803d1859879fe5a83fb
Albert Bofill <abofill@bgeo.es>
2023-07-31 14:23:23 +0200

    Enhance minsector function description
    (cherry picked from commit d28d275e29b7cfeb6cfc3dfeaecd3e63ba32e32f)

--- dbmodel/updates/35/35039/ws/dml.sql
+++ dbmodel/updates/35/35039/ws/dml.sql
@@ -8,3 +8,8 @@ This version of Giswater is provided by Giswater Association
 SET search_path = SCHEMA_NAME ,public;
 
 UPDATE config_function set style = '{"style": {"polygon": {"style": "categorized","field": "minsector_id",  "transparency": 0.5}}}' WHERE id = 2706;
+
+update sys_function set descript='Dynamic analisys to sectorize network using the flow traceability function and establish Minimum Sectors. 
+Before start you need to configure:
+- Field graph_delimiter on [cat_feature_node] table to establish which elements will be used to sectorize. 
+- Enable status for minsector on utils_graphanalytics_status variable from [config_param_system] table.' where function_name='gw_fct_graphanalytics_minsector';
