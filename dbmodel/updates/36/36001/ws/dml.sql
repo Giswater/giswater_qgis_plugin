820bb72fa4d8eade9650bbf3a481b9a800871c29
Barbara Rzepka <bbrzepka@gmail.com>
2023-08-01 13:39:22 +0200

    remove creating epa_tab from updates

--- dbmodel/updates/36/36001/ws/dml.sql
+++ dbmodel/updates/36/36001/ws/dml.sql
@@ -1677,15 +1677,6 @@ VALUES('ve_epa_connec', 'tab_epa', 'EPA', NULL, 'role_basic', NULL, '[
 {"actionName":"actionGetArcId","actionTooltip":"Set arc_id","disabled":false},
 {"actionName":"actionDemand","actionTooltip":"Demands","disabled":false}]'::json, 2, '{4}');
 
-INSERT INTO config_form_tabs(formname, tabname, label, tooltip, sys_role, tabfunction, tabactions, orderby, device) 
-VALUES ('v_edit_connec','tab_epa','EPA results',NULL,'role_basic',NULL,'[{"actionName":"actionEdit", "actionTooltip":"Edit",  "disabled":false},
-{"actionName":"actionZoom", "actionTooltip":"Zoom In",  "disabled":false}, {"actionName":"actionCentered", "actionTooltip":"Center",  "disabled":false},
-{"actionName":"actionZoomOut", "actionTooltip":"Zoom Out",  "disabled":false}, {"actionName":"actionCatalog", "actionTooltip":"Change Catalog",  "disabled":false},
-{"actionName":"actionWorkcat", "actionTooltip":"Add Workcat",  "disabled":false}, {"actionName":"actionCopyPaste", "actionTooltip":"Copy Paste",  "disabled":false},
-{"actionName":"actionLink", "actionTooltip":"Open Link",  "disabled":false}, {"actionName":"actionHelp", "actionTooltip":"Help",  "disabled":false},
-{"actionName":"actionMapZone", "actionTooltip":"Add Mapzone",  "disabled":false}, {"actionName":"actionSetToArc", "actionTooltip":"Set to_arc",  "disabled":false},
-{"actionName":"actionGetParentId", "actionTooltip":"Set parent_id",  "disabled":false}, {"actionName":"actionGetArcId", "actionTooltip":"Set arc_id",  "disabled":false},
-{"actionName": "actionRotation", "actionTooltip": "Rotation","disabled": false}]',2, '{4}');
 
 -- set layoutorder for dscenario widgets
 UPDATE config_form_fields 
