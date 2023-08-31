251035d96d87573d05c19ed3cbcdcbb4c7cd5fe4
Barbara Rzepka <bbrzepka@gmail.com>
2023-08-31 09:02:01 +0200

    copy man_type_fluid configuration for connec if it uses values from arc

--- dbmodel/utils/ftrg/gw_trg_connect_update.sql
+++ dbmodel/utils/ftrg/gw_trg_connect_update.sql
@@ -56,7 +56,12 @@ BEGIN
    	IF v_dma_value IS NULL THEN
    		v_dma_value = NEW.dma_id;
    	END IF;
-    IF v_fluidtype_autoupdate is true or v_fluidtype_autoupdate is null THEN v_fluidtype_value = v_arc.fluid_type; ELSE v_fluidtype_value = NEW.fluid_type; END IF;
+    IF v_fluidtype_autoupdate is true or v_fluidtype_autoupdate is null THEN v_fluidtype_value = v_arc.fluid_type; 
+    	IF v_fluidtype_value not in (SELECT fluid_type FROM man_type_fluid WHERE feature_type='CONNEC') THEN
+			INSERT INTO man_type_fluid (fluid_type, feature_type) VALUES (v_fluidtype_value, 'CONNEC') ON CONFLICT (fluid_type, feature_type) DO NOTHING;
+		END IF;
+    ELSE v_fluidtype_value = NEW.fluid_type; 
+    END IF;
 	
 	IF v_featuretype='connec' THEN
 	
