ca5921c8873e5c44cb29618192c2ac30a39ac4c2
Sergi Maspons <smaspons@bgeo.es>
2023-09-05 16:56:54 +0200

    Hide archive button for ud

--- core/toolbars/epa/go2epa_manager_button.py
+++ core/toolbars/epa/go2epa_manager_button.py
@@ -47,6 +47,7 @@ class GwGo2EpaManagerButton(GwAction):
         self.dlg_manager.btn_set_corporate.setEnabled(False)
         if self.project_type != 'ws':
             self.dlg_manager.btn_set_corporate.setVisible(False)
+            self.dlg_manager.btn_archive.setVisible(False)
 
         # Fill combo box and table view
         # self._fill_combo_result_id()
