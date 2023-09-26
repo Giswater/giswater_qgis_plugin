f7169835ff996c7968dc6a6033efc01c928fec05
xtorret@bgeo.es <xtorret@bgeo.es>
2023-09-26 21:59:07 +0200

    Minor update from prev commit

--- dbmodel/utils/fct/gw_fct_setplan.sql
+++ dbmodel/utils/fct/gw_fct_setplan.sql
@@ -47,12 +47,12 @@ BEGIN
 
 		--control psector topology: find arc, that on psector is defined as operative, on psector there are no nodes
 		v_query = '
-		select a.arc_id, node_id, ''Arc operative without node_1 in this psector'',  a.the_geom FROM arc a
+		select a.arc_id, node_id, concat(''Arc on service '',a.arc_id, '' has not node_1 ('', node_id, '') in this psector'') as descript,  a.the_geom FROM arc a
 		join (select node_id from plan_psector_x_node where psector_id = '|| v_psector ||' and state = 0)n on node_1= node_id
 		left join (select * from plan_psector_x_arc where psector_id = '|| v_psector ||') p ON p.arc_id = a.arc_id
 		where p.arc_id is null and a.state=1
 		union
-		select a.arc_id, node_id, ''Arc operative without node_2 in this psector'', a.the_geom FROM arc a
+		select a.arc_id, node_id, concat(''Arc on service '',a.arc_id, '' has not node_2 ('', node_id, '') in this psector'') as descript, a.the_geom FROM arc a
 		join (select node_id from plan_psector_x_node where psector_id = '|| v_psector ||' and state = 0 )n on node_2 = node_id
 		left join (select * from plan_psector_x_arc where psector_id = '|| v_psector ||') p ON p.arc_id = a.arc_id
 		where p.arc_id is null and a.state=1';
@@ -63,19 +63,28 @@ BEGIN
 
 		IF v_count > 0 THEN
 
-			EXECUTE 'SELECT gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
-			"data":{"message":"3164", "function":"3002","debug_msg":null, "is_process":true}}$$)' INTO v_audit_result;
+			-- return el json. There are some topological inconsistences on this psector. Would you like to see the log (YES)  (LATER)
+			/*
+			INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (354, 4, concat('TOPOLOGICAL INCONSISTENCES'));
+			INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (354, 4, '-------------------------------------------------------------');
 
+			EXECUTE concat ('INSERT INTO anl_arc (fid, arc_id, arccat_id, descript, the_geom,state)
+			SELECT 355, c.arc_id, c.arccat_id, concat(''Operative arc '', arc_id ,'' without final nodes in this psector '',c.psector_id), c.the_geom, 2 FROM (', v_query,')c ');
+			
+			INSERT INTO temp_audit_check_data (fid, result_id,  criticity, enabled,  error_message, fcount)
+			VALUES (v_fid, '354', 3, FALSE, concat('ERROR-354 (anl_arc): There are ',v_count,' operative arcs without final nodes in this psector.'),v_count);
+			*/
+		
 		END IF;		
 
 		--control psector topology: find arc, that on inventory is defined as planified, but on psector there are no nodes
 		v_query = '
-		select a.arc_id, node_id, ''Arc planned without node_1 in this psector'' as descript,  a.the_geom FROM arc a
+		select a.arc_id, node_id, concat(''Arc planned '',a.arc_id, '' has not node_1 ('', node_id, '') in this psector'') as descript,  a.the_geom FROM arc a
 		join (select node_id from plan_psector_x_node where psector_id = '|| v_psector ||' and state = 0)n on node_1= node_id
 		left join (select * from plan_psector_x_arc where psector_id = '|| v_psector ||') p ON p.arc_id = a.arc_id
 		where p.arc_id is not null and a.state=2
 		union
-		select a.arc_id, node_id, ''Arc planned without node_2 in this psector'', a.the_geom FROM arc a
+		select a.arc_id, node_id, concat(''Arc planned '',a.arc_id, '' has not node_2 ('', node_id, '') in this psector'') as descript, a.the_geom FROM arc a
 		join (select node_id from plan_psector_x_node where psector_id = '|| v_psector ||' and state = 0 )n on node_2 = node_id
 		left join (select * from plan_psector_x_arc where psector_id = '|| v_psector ||') p ON p.arc_id = a.arc_id
 		where p.arc_id is not null and a.state=2';
@@ -85,8 +94,18 @@ BEGIN
 
 		IF v_count > 0 THEN
 
-			EXECUTE 'SELECT gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
-			"data":{"message":"3164", "function":"3002","debug_msg":null, "is_process":true}}$$)' INTO v_audit_result;
+			-- return el json. There are some topological inconsistences on this psector. Would you like to see the log (YES)  (LATER)
+			/*
+			INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (355, 4, concat('TOPOLOGICAL INCONSISTENCES'));
+			INSERT INTO audit_check_data (fid, criticity, error_message) VALUES (355, 4, '-------------------------------------------------------------');
+
+			EXECUTE concat ('INSERT INTO anl_arc (fid, arc_id, arccat_id, descript, the_geom,state)
+			SELECT 355, c.arc_id, c.arccat_id, concat(''Planned arc  '', arc_id ,'' without final nodes in this psector''), c.the_geom, 2 FROM (', v_query,')c ');
+			
+			INSERT INTO temp_audit_check_data (fid, result_id,  criticity, enabled,  error_message, fcount)
+			VALUES (v_fid, '355', 3, FALSE, concat('ERROR-354 (anl_arc): There are ',v_count,' planned arcs without final nodes in this psector.'),v_count);
+			*/								 
+
 		END IF;		
 
 	END IF;
@@ -107,6 +126,7 @@ BEGIN
 	PERFORM gw_fct_workspacemanager($${"client":{"device":4, "infoType":1, "lang":"ES"}, "form":{}, "feature":{},"data":{"filterFields":{}, "pageInfo":{}, "action":"CHECK"}}$$);
 	v_uservalues = (SELECT to_json(array_agg(row_to_json(a))) FROM (SELECT parameter, value FROM config_param_user WHERE parameter IN ('plan_psector_vdefault', 'utils_workspace_vdefault')
 	AND cur_user = current_user ORDER BY parameter)a);
+
 	--control nulls
 	v_uservalues := COALESCE(v_uservalues, '{}');
 		--  Return
@@ -116,9 +136,9 @@ BEGIN
 			'}}'||
 	    '}')::json, 3002, null, null, null);
 
-	   EXCEPTION WHEN OTHERS THEN
-    GET STACKED DIAGNOSTICS v_error_context = PG_EXCEPTION_CONTEXT;
-    RETURN ('{"status":"Failed","NOSQLERR":' || to_json(SQLERRM) || ',"SQLSTATE":' || to_json(SQLSTATE) ||',"SQLCONTEXT":' || to_json(v_error_context) || '}')::json;
+	EXCEPTION WHEN OTHERS THEN
+	GET STACKED DIAGNOSTICS v_error_context = PG_EXCEPTION_CONTEXT;
+	RETURN ('{"status":"Failed","NOSQLERR":' || to_json(SQLERRM) || ',"SQLSTATE":' || to_json(SQLSTATE) ||',"SQLCONTEXT":' || to_json(v_error_context) || '}')::json;
 
  
 END;
