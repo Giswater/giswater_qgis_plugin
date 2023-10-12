7ed39fa7123bf08e85f6c3fafdc70d019854ed1b
xtorret@bgeo.es <xtorret@bgeo.es>
2023-10-12 14:13:26 +0200

    Minor bug from prev commit
    (cherry picked from commit 3dde0295061755551671fab7793e39a0f2f4a073)

--- dbmodel/utils/ftrg/gw_trg_edit_psector.sql
+++ dbmodel/utils/ftrg/gw_trg_edit_psector.sql
@@ -100,12 +100,6 @@ BEGIN
 			v_psector_geom = (SELECT the_geom FROM plan_psector WHERE psector_id=NEW.psector_id);
 		
 			-- copy values into traceability tables
-	
-			INSERT INTO audit_psector_node_traceability
-			SELECT nextval('SCHEMA_NAME.audit_psector_node_traceability_id_seq'), psector_id, pn.state, doable, addparam, now(), current_user, 'Execute psector', node.*
-			FROM plan_psector_x_node pn JOIN node USING (node_id) 
-			WHERE psector_id=NEW.psector_id;
-		
 			INSERT INTO audit_psector_connec_traceability
 			SELECT nextval('SCHEMA_NAME.audit_psector_connec_traceability_id_seq'), psector_id, pc.state, doable, pc.arc_id, l.link_id, l.the_geom, now(), current_user, 'Execute psector', connec.*
 			FROM plan_psector_x_connec pc JOIN connec USING (connec_id)
@@ -114,15 +108,20 @@ BEGIN
 
 			IF v_projectype = 'WS' THEN
 
-				-- arc insert is different from ud because UD has legacy of _sys_length and the impossibility to remove it from old production environments
+				-- arc & node insert is different from ud because UD has legacy of _sys_length & _sys_elev and the impossibility to remove it from old production environments
 				INSERT INTO audit_psector_arc_traceability
 				SELECT nextval('SCHEMA_NAME.audit_psector_arc_traceability_id_seq'), psector_id, pa.state, doable, addparam, now(), current_user, 'Execute psector', arc.*
 				FROM plan_psector_x_arc pa JOIN arc USING (arc_id) 
 				WHERE psector_id=NEW.psector_id;
-			
+
+				INSERT INTO audit_psector_node_traceability
+				SELECT nextval('SCHEMA_NAME.audit_psector_node_traceability_id_seq'), psector_id, pn.state, doable, addparam, now(), current_user, 'Execute psector', node.*
+				FROM plan_psector_x_node pn JOIN node USING (node_id) 
+				WHERE psector_id=NEW.psector_id;
+		
 			ELSIF v_projectype = 'UD' THEN
 
-				-- arc insert is different from ws because legacy of _sys_length and the impossibility to remove it from old production environments
+				-- arc & node insert is different from ws because legacy of _sys_length & _sys_elev and the impossibility to remove it from old production environments
 				INSERT INTO audit_psector_arc_traceability
 				SELECT nextval('SCHEMA_NAME.audit_psector_arc_traceability_id_seq'), psector_id, pa.state, doable, addparam, now(), current_user, 'Execute psector',
 				a.arc_id,code,node_1,node_2,y1,y2,elev1,elev2,custom_y1,custom_y2,custom_elev1,custom_elev2,sys_elev1,sys_elev2,arc_type,arccat_id,
@@ -133,6 +132,16 @@ BEGIN
 				district_id,workcat_id_plan,asset_id,pavcat_id,drainzone_id,nodetype_1,node_sys_top_elev_1,node_sys_elev_1,nodetype_2,node_sys_top_elev_2,
 				node_sys_elev_2,parent_id,expl_id2, adate, adescript	
 				FROM plan_psector_x_arc pa JOIN arc a USING (arc_id) 
+				WHERE psector_id=NEW.psector_id;
+
+				INSERT INTO audit_psector_node_traceability
+				SELECT nextval('SCHEMA_NAME.audit_psector_node_traceability_id_seq'), psector_id, pn.state, doable, addparam, now(), current_user, 'Execute psector', 
+				n.node_id,code,top_elev,ymax,elev,custom_top_elev,custom_ymax,custom_elev,node_type,nodecat_id,epa_type,sector_id,n.state,state_type,annotation,observ,
+				comment,dma_id,soilcat_id,function_type,category_type,fluid_type,location_type,workcat_id,workcat_id_end,buildercat_id,builtdate,enddate,ownercat_id,
+				muni_id,postcode,streetaxis_id,postnumber,postcomplement,streetaxis2_id,postnumber2,postcomplement2,n.descript,rotation,link,verified,the_geom,undelete,
+				label_x,label_y,label_rotation,publish,inventory,xyz_date,uncertain,unconnected,expl_id,num_value,feature_type,tstamp,arc_id,lastupdate,lastupdate_user,
+				n.insert_user,matcat_id,district_id,workcat_id_plan,asset_id,drainzone_id,parent_id,expl_id2
+				FROM plan_psector_x_node pn JOIN node n USING (node_id) 
 				WHERE psector_id=NEW.psector_id;
 			
 				INSERT INTO audit_psector_gully_traceability
