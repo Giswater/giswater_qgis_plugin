5fa0c78f78fc4cb667982623703dd869e1e84878
xtorret@bgeo.es <xtorret@bgeo.es>
2023-10-12 20:50:44 +0200

    Reset for new projects: search according feature_id & do not force preferences for graphanalytics funtion

--- dbmodel/utils/fct/gw_fct_admin_schema_lastprocess.sql
+++ dbmodel/utils/fct/gw_fct_admin_schema_lastprocess.sql
@@ -63,7 +63,6 @@ BEGIN
 	SET search_path = "SCHEMA_NAME", public;
 	v_schemaname = 'SCHEMA_NAME';
 	
-
 	-- get input parameters
 	v_gwversion := (p_data ->> 'data')::json->> 'gwVersion';
 	v_language := (p_data ->> 'client')::json->> 'lang';
@@ -83,17 +82,16 @@ BEGIN
 		(SELECT table_name, column_name, sequence_name FROM information_schema.columns,   information_schema.sequences
 		WHERE table_schema ='SCHEMA_NAME' and sequence_schema ='SCHEMA_NAME' and  column_default ILIKE '%' || sequence_name || '%'
 		AND sequence_name!='urn_id_seq' AND sequence_name!='doc_seq' AND sequence_name!='raingage_rg_id_seq')
-        LOOP 
-            v_querytext:= 'SELECT max('||v_rectable.column_name||') FROM '||v_rectable.table_name||';' ;
-            EXECUTE v_querytext INTO v_max_seq_id;	
-            
-            IF v_max_seq_id IS NOT NULL AND v_max_seq_id > 0 THEN 
-                EXECUTE 'SELECT setval(''SCHEMA_NAME.'||v_rectable.sequence_name||' '','||v_max_seq_id||', true)';			
-            END IF;
-        END LOOP;
-    v_message='Sequeneces set correctly';
+		LOOP 
+		    v_querytext:= 'SELECT max('||v_rectable.column_name||') FROM '||v_rectable.table_name||';' ;
+		    EXECUTE v_querytext INTO v_max_seq_id;	
+		    
+		    IF v_max_seq_id IS NOT NULL AND v_max_seq_id > 0 THEN 
+			EXECUTE 'SELECT setval(''SCHEMA_NAME.'||v_rectable.sequence_name||' '','||v_max_seq_id||', true)';			
+		    END IF;
+		END LOOP;
+		v_message='Sequeneces set correctly';
 	ELSE
-
 		-- enable triggers on typevalue tables
 		ALTER TABLE om_typevalue ENABLE TRIGGER gw_trg_typevalue_config_fk;
 		ALTER TABLE edit_typevalue ENABLE TRIGGER gw_trg_typevalue_config_fk;
@@ -333,20 +331,58 @@ BEGIN
 				columnname IN ('om_state', 'conserv_state', 'priority', 'valve_location', 'valve_type', 'shutoff_valve', 'access_type', 'placement_type', 'crmzone_id')) 
 				AND formname ilike 've_connec%';
                 
-                UPDATE config_form_fields SET hidden=true WHERE formname = 'v_edit_arc' AND columnname='parent_id';
-                UPDATE config_form_fields SET hidden=true WHERE formname like 've_arc%' AND columnname='parent_id';
-                UPDATE config_form_fields SET hidden=true WHERE formname = 'v_edit_arc' AND columnname='observ';
-                UPDATE config_form_fields SET hidden=true WHERE formname like 've_arc%' AND columnname='observ';
+				UPDATE config_form_fields SET hidden=true WHERE formname = 'v_edit_arc' AND columnname='parent_id';
+				UPDATE config_form_fields SET hidden=true WHERE formname like 've_arc%' AND columnname='parent_id';
+				UPDATE config_form_fields SET hidden=true WHERE formname = 'v_edit_arc' AND columnname='observ';
+				UPDATE config_form_fields SET hidden=true WHERE formname like 've_arc%' AND columnname='observ';
 
-					
+				-- setting mapzone graph analytics without value in terms of mapzone constructor and graphclass
+
+				UPDATE config_toolbox SET inputparams = '[
+					{"widgetname":"graphClass", "label":"Graph class:", "widgettype":"combo","datatype":"text","tooltip": "Graphanalytics method used", "layoutname":"grl_option_parameters","layoutorder":1,"comboIds":["PRESSZONE","DQA","DMA","SECTOR"],
+					"comboNames":["Pressure Zonification (PRESSZONE)", "District Quality Areas (DQA) ", "District Metering Areas (DMA)", "Inlet Sectorization (SECTOR-HIGH / SECTOR-LOW)"], "selectedId":""}, 
+					{"widgetname":"exploitation", "label":"Exploitation:","widgettype":"combo","datatype":"text","tooltip": "Choose exploitation to work with", "layoutname":"grl_option_parameters","layoutorder":2, 
+					"dvQueryText":"select expl_id as id, name as idval from exploitation where active is not false order by name", "selectedId":"$userExploitation"},
+					{"widgetname":"floodOnlyMapzone", "label":"Flood only one mapzone: (*)","widgettype":"linetext","datatype":"text", "isMandatory":false, "tooltip":"Flood only identified mapzones. The purpose of this is update only network elements affected by this flooding keeping rest of network as is. Recommended to gain performance when mapzones ecosystem is under work", "placeholder":"1001", "layoutname":"grl_option_parameters","layoutorder":3, "value":""},
+					{"widgetname":"forceOpen", "label":"Force open nodes: (*)","widgettype":"linetext","datatype":"text", "isMandatory":false, "tooltip":"Optative node id(s) to temporary open closed node(s) in order to force algorithm to continue there", "placeholder":"1015,2231,3123", "layoutname":"grl_option_parameters","layoutorder":5, "value":""},
+					{"widgetname":"forceClosed", "label":"Force closed nodes: (*)","widgettype":"text","datatype":"text", "isMandatory":false, "tooltip":"Optative node id(s) to temporary close open node(s) to force algorithm to stop there","placeholder":"1015,2231,3123", "layoutname":"grl_option_parameters","layoutorder":6,"value":""},
+					{"widgetname":"usePlanPsector", "label":"Use selected psectors:", "widgettype":"check","datatype":"boolean","tooltip":"If true, use selected psectors. If false ignore selected psectors and only works with on-service network" , "layoutname":"grl_option_parameters","layoutorder":7,"value":""},
+					{"widgetname":"commitChanges", "label":"Commit changes:", "widgettype":"check","datatype":"boolean","tooltip":"If true, changes will be applied to DB. If false, algorithm results will be saved in anl tables" , "layoutname":"grl_option_parameters","layoutorder":8,"value":""},
+					{"widgetname":"valueForDisconnected", "label":"Value for disconn. and conflict: (*)","widgettype":"linetext","datatype":"text", "isMandatory":false, "tooltip":"Value to use for disconnected features. Usefull for work in progress with dynamic mpzonesnode" , "placeholder":"", "layoutname":"grl_option_parameters","layoutorder":9, "value":""},
+					{"widgetname":"updateMapZone", "label":"Mapzone constructor method:","widgettype":"combo","datatype":"integer","layoutname":"grl_option_parameters","layoutorder":10,
+					"comboIds":[0,1,2,3,4], "comboNames":["NONE", "CONCAVE POLYGON", "PIPE BUFFER", "PLOT & PIPE BUFFER", "LINK & PIPE BUFFER"], "selectedId":""}, 
+					{"widgetname":"geomParamUpdate", "label":"Pipe buffer","widgettype":"text","datatype":"float","tooltip":"Buffer from arcs to create mapzone geometry using [PIPE BUFFER] options. Normal values maybe between 3-20 mts.", "layoutname":"grl_option_parameters","layoutorder":11, "isMandatory":false, "placeholder":"5-30", "value":""}
+					]'
+				WHERE id = 2768;
+								
 			ELSIF v_projecttype = 'UD' THEN
 				UPDATE config_form_fields SET hidden = false where 
 				columnname IN ('step_pp', 'step_fe', 'step_replace', 'cover') AND formname ilike 've_node%';
                 
-                UPDATE config_form_fields SET layoutorder=30 WHERE formname = 'v_edit_arc' AND columnname='pavcat_id';
-                UPDATE config_form_fields SET layoutorder=30 WHERE formname like 've_arc%' AND columnname='pavcat_id';
-                
+				UPDATE config_form_fields SET layoutorder=30 WHERE formname = 'v_edit_arc' AND columnname='pavcat_id';
+				UPDATE config_form_fields SET layoutorder=30 WHERE formname like 've_arc%' AND columnname='pavcat_id';
 			END IF;
+
+			-- setting search where search_field is arc_id, node_id, connec_id, gully_id
+			UPDATE config_param_system SET value = 
+			'{"sys_table_id":"v_edit_arc","sys_id_field":"arc_id","sys_search_field":"arc_id","alias":"Arcs","cat_field":"arccat_id","orderby":"1","search_type":"arc"}'
+			WHERE parameter = 'basic_search_network_arc';
+
+			UPDATE config_param_system SET value =
+			'{"sys_table_id":"v_edit_connec","sys_id_field":"connec_id","sys_search_field":"connec_id","alias":"Connecs","cat_field":"connecat_id","orderby":"3","search_type":"connec"}'
+			WHERE parameter = 'basic_search_network_connec';
+
+			UPDATE config_param_system SET value =
+			'{"sys_table_id":"v_edit_element","sys_id_field":"element_id","sys_search_field":"element_id","alias":"Elements","cat_field":"elementcat_id","orderby":"5","search_type":"element"}'
+			WHERE parameter = 'basic_search_network_element';
+
+			UPDATE config_param_system SET value =
+			'{"sys_table_id":"v_edit_node","sys_id_field":"node_id","sys_search_field":"node_id","alias":"Nodes","cat_field":"nodecat_id","orderby":"2","search_type":"node"}'
+			WHERE  parameter = 'basic_search_network_node';
+
+			UPDATE config_param_system SET value =
+			'{"sys_table_id":"v_edit_gully","sys_id_field":"gully_id","sys_search_field":"gully_id","alias":"Gullies","cat_field":"gratecat_id","orderby":"3","search_type":"gully"}'
+			WHERE  parameter = 'basic_search_network_gully';
 
 		ELSIF v_isnew IS FALSE THEN
 		
