090587f82854bb44a4cbb6e831802deb778efd5e
Barbara Rzepka <bbrzepka@gmail.com>
2023-10-16 11:28:23 +0200

    fct_linknetwork -control of fluid_type values

--- dbmodel/utils/fct/gw_fct_linktonetwork.sql
+++ dbmodel/utils/fct/gw_fct_linktonetwork.sql
@@ -416,7 +416,7 @@ BEGIN
 			IF v_fluidtype_autoupdate is true or v_fluidtype_autoupdate is null THEN 
 				v_fluidtype_value = v_arc.fluid_type;
 				IF v_fluidtype_value not in (SELECT fluid_type FROM man_type_fluid WHERE feature_type='CONNEC') AND v_fluidtype_value IS NOT NULL THEN
-					INSERT INTO man_type_fluid (fluid_type, feature_type) VALUES (NEW.fluid_type, 'CONNEC') ON CONFLICT (v_fluidtype_value, feature_type) DO NOTHING;
+					INSERT INTO man_type_fluid (fluid_type, feature_type) VALUES (v_fluidtype_value, 'CONNEC') ON CONFLICT (fluid_type, feature_type) DO NOTHING;
 				END IF;
 			ELSE v_fluidtype_value = v_connect.fluid_type; 
 			END IF;
