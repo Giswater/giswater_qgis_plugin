35b5995f274e9812b05d202b9e90a6c12621e9ce
xtorret@bgeo.es <xtorret@bgeo.es>
2023-10-21 22:44:23 +0200

    Add domain value tables (man_type_*) on load giswater layer button

--- dbmodel/updates/36/36006/utils/dml.sql
+++ dbmodel/updates/36/36006/utils/dml.sql
@@ -10,3 +10,12 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 INSERT INTO sys_fprocess(fid, fprocess_name, project_type, parameters, source, isaudit, fprocess_type, addparam)
 VALUES (518, 'Set end feature', 'utils', null, 'core', true, 'Function process', null) 
 ON CONFLICT (fid) DO NOTHING;
+
+-- 21/10/2023
+INSERT INTO config_typevalue (typevalue, id, addparam) VALUES ('sys_table_context','{"level_1":"INVENTORY","level_2":"VALUE DOMAIN"}', '{"orderBy":99}');
+ON CONFLICT (typevalue, id) DO NOTHING;
+
+UPDATE sys_table SET context = '{"level_1":"INVENTORY","level_2":"VALUE DOMAIN"}' , alias = 'Category type' WHERE id = 'man_type_category';
+UPDATE sys_table SET context = '{"level_1":"INVENTORY","level_2":"VALUE DOMAIN"}' , alias = 'Fluid type' WHERE id = 'man_type_fluid';
+UPDATE sys_table SET context = '{"level_1":"INVENTORY","level_2":"VALUE DOMAIN"}' , alias = 'Location type' WHERE id = 'man_type_location';
+UPDATE sys_table SET context = '{"level_1":"INVENTORY","level_2":"VALUE DOMAIN"}' , alias = 'Function type' WHERE id = 'man_type_function';
