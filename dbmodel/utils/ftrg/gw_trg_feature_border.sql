3c3e4b27ac3bd4d1a64bb2eb9063cea3ce68be31
Maria Guzmán <mguzman@bgeo.es>
2023-10-26 14:21:29 +0200

    Bug fix on node_border_expl

--- dbmodel/utils/ftrg/gw_trg_feature_border.sql
+++ dbmodel/utils/ftrg/gw_trg_feature_border.sql
@@ -22,10 +22,9 @@ BEGIN
 
 	v_feature_type:= upper(TG_ARGV[0]);
 
-	IF TG_OP = 'UPDATE' AND NEW.parent_id IS NULL AND v_feature_type = 'ARC' THEN
-		DELETE FROM arc_border_expl WHERE arc_id = NEW.arc_id;
-	ELSIF TG_OP = 'UPDATE' AND NEW.parent_id IS NULL AND v_feature_type = 'NODE' THEN
-		DELETE FROM node_border_expl WHERE node_id = NEW.node_id;
+		
+	IF TG_OP = 'UPDATE' AND NEW.parent_id IS NULL AND v_feature_type = 'NODE' THEN
+		
 		DELETE FROM node_border_sector WHERE node_id = NEW.node_id;
 	END IF;
 
@@ -45,32 +44,6 @@ BEGIN
 		JOIN arcs a2 ON node_id=node_2 
 		where a2.sector_id != node.sector_id ON CONFLICT (node_id, sector_id) DO NOTHING;	
 
-  	--exploitation
-		INSERT INTO node_border_expl
-		WITH 
-		arcs AS (SELECT arc_id, node_1, node_2, expl_id FROM arc WHERE state=1 AND arc_id = NEW.arc_id)
-		SELECT node_id, a1.expl_id
-		FROM node 
-		JOIN arcs a1 ON node_id=node_1 
-		where a1.expl_id != node.expl_id
-		UNION 
-		SELECT node_id, a2.expl_id
-		FROM node 
-		JOIN arcs a2 ON node_id=node_2 
-		where a2.expl_id != node.expl_id ON CONFLICT (node_id, expl_id) DO NOTHING;
-
-		INSERT INTO node_border_expl
-		SELECT a.node_id,  n.expl_id
-		FROM v_edit_node a
-		JOIN v_edit_node  n ON n.node_id=a.parent_id::text
-		WHERE a.expl_id != n.expl_id ON CONFLICT (node_id, expl_id) DO NOTHING;
-
-		INSERT INTO arc_border_expl
-		SELECT a.arc_id,  n.expl_id
-		FROM v_edit_arc a
-		JOIN v_edit_node  n ON n.node_id=a.parent_id::text
-		WHERE a.expl_id != n.expl_id ON CONFLICT (arc_id, expl_id) DO NOTHING;
-
     RETURN NEW;
 
   ELSIF TG_OP = 'UPDATE' AND v_feature_type = 'ARC' THEN
@@ -88,32 +61,6 @@ BEGIN
 		FROM node 
 		JOIN arcs a2 ON node_id=node_2 
 		where a2.sector_id != node.sector_id ON CONFLICT (node_id, sector_id) DO NOTHING;	
-
-		--exploitation
-		INSERT INTO node_border_expl
-		WITH 
-		arcs AS (SELECT arc_id, node_1, node_2, expl_id FROM arc WHERE state=1 AND arc_id = NEW.arc_id)
-		SELECT node_id, a1.expl_id
-		FROM node 
-		JOIN arcs a1 ON node_id=node_1 
-		where a1.expl_id != node.expl_id
-		UNION 
-		SELECT node_id, a2.expl_id
-		FROM node 
-		JOIN arcs a2 ON node_id=node_2 
-		where a2.expl_id != node.expl_id ON CONFLICT (node_id, expl_id) DO NOTHING;
-		
-		INSERT INTO node_border_expl
-		SELECT a.node_id,  n.expl_id
-		FROM v_edit_node a
-		JOIN v_edit_node  n ON n.node_id=a.parent_id::text
-		WHERE a.expl_id != n.expl_id ON CONFLICT (node_id, expl_id) DO NOTHING;
-
-		INSERT INTO arc_border_expl
-		SELECT a.arc_id,  n.expl_id
-		FROM v_edit_arc a
-		JOIN v_edit_node  n ON n.node_id=a.parent_id::text
-		WHERE a.expl_id != n.expl_id ON CONFLICT (arc_id, expl_id) DO NOTHING;
 
 		v_final_nodes = string_to_array(OLD.node_1,'') ||string_to_array(OLD.node_2,'');
 
@@ -141,33 +88,7 @@ BEGIN
 		  END IF;
 		 END LOOP;
 
-			--exploitation
-			FOREACH rec IN ARRAY (v_final_nodes) LOOP
-	
-				EXECUTE 'WITH 
-					arcs AS (SELECT arc_id, node_1, node_2, expl_id FROM arc WHERE state=1 AND  expl_id='||OLD.expl_id||')
-					select count(*) from
-					(SELECT node_id, a1.expl_id
-					FROM node 
-					JOIN arcs a1 ON node_id=node_1 
-					where a1.expl_id != node.expl_id AND node_id='||quote_literal(rec)||'
-					UNION all
-					SELECT node_id, a2.expl_id
-					FROM node 
-					JOIN arcs a2 ON node_id=node_2 
-					where a2.expl_id != node.expl_id AND node_id='||quote_literal(rec)||' )a'
-					INTO v_count;
-
-		  IF v_count is null OR v_count = 0 THEN
-				EXECUTE 'DELETE FROM node_border_expl WHERE node_id='||quote_literal(rec)||' AND expl_id='||OLD.expl_id||'';
-
-				EXECUTE 'DELETE from node_border_expl where node_id in (SELECT node_id FROM node WHERE parent_id::text='||quote_literal(rec)||') AND expl_id='||OLD.expl_id||'';
-
-				EXECUTE 'DELETE from arc_border_expl where arc_id in (SELECT arc_id FROM arc WHERE parent_id::text='||quote_literal(rec)||') AND expl_id='||OLD.expl_id||'';
-
-		  END IF;
-
-    	END LOOP;  
+ 
 	  END IF;
 		RETURN NEW;
 
@@ -188,37 +109,7 @@ BEGIN
 		JOIN arcs a2 ON node_id=node_2 
 		where a2.sector_id != node.sector_id ON CONFLICT (node_id, sector_id) DO NOTHING;	
 
-		--exploitation
-		DELETE FROM node_border_expl WHERE node_id=OLD.node_id;
-
-		DELETE from node_border_expl where node_id in 
-		(SELECT node_id FROM node WHERE parent_id::text=OLD.node_id);
-
-
-		INSERT INTO node_border_expl
-		WITH 
-		arcs AS (SELECT arc_id, node_1, node_2, expl_id FROM arc WHERE state=1)
-		SELECT node_id, a1.expl_id
-		FROM node 
-		JOIN arcs a1 ON node_id=node_1 
-		where a1.expl_id != node.expl_id and node_id=NEW.node_id
-		UNION 
-		SELECT node_id, a2.expl_id
-		FROM node 
-		JOIN arcs a2 ON node_id=node_2 
-		where a2.expl_id != node.expl_id and node_id=NEW.node_id ON CONFLICT (node_id, expl_id) DO NOTHING;	
-
-		INSERT INTO node_border_expl
-		SELECT a.node_id, n.expl_id
-		FROM v_edit_node  a
-		JOIN v_edit_node  n ON n.node_id=a.parent_id::text
-		WHERE  n.expl_id != a.expl_id ON CONFLICT (node_id, expl_id) DO NOTHING;
-
-		INSERT INTO arc_border_expl
-		SELECT a.arc_id,  n.expl_id
-		FROM v_edit_arc a
-		JOIN v_edit_node  n ON n.node_id=a.parent_id::text
-		WHERE a.expl_id != n.expl_id ON CONFLICT (arc_id, expl_id) DO NOTHING;
+		
 		
 		RETURN NEW;
   ELSIF TG_OP = 'DELETE' THEN
@@ -248,30 +139,7 @@ BEGIN
 				EXECUTE 'DELETE FROM node_border_sector WHERE node_id='||quote_literal(rec)||' AND sector_id='||OLD.sector_id||'';
 		  END IF;
 		 END LOOP;
-		  --exploitation
-			FOREACH rec IN ARRAY (v_final_nodes) LOOP
-
-				EXECUTE 'WITH 
-					arcs AS (SELECT arc_id, node_1, node_2, expl_id FROM arc WHERE state=1 AND expl_id='||OLD.expl_id||')
-					select count(*) from
-					(SELECT node_id, a1.expl_id
-					FROM node 
-					JOIN arcs a1 ON node_id=node_1 
-					where a1.expl_id != node.expl_id AND node_id='||quote_literal(rec)||'
-					UNION all
-					SELECT node_id, a2.expl_id
-					FROM node 
-					JOIN arcs a2 ON node_id=node_2 
-					where a2.expl_id != node.expl_id AND node_id='||quote_literal(rec)||')a'
-					INTO v_count;
-
-				IF v_count is null OR v_count = 0 THEN
-					EXECUTE 'DELETE FROM node_border_expl WHERE node_id='||quote_literal(rec)||' AND expl_id='||OLD.expl_id||'';
-					EXECUTE 'DELETE from node_border_expl where node_id in (SELECT node_id FROM node WHERE parent_id::text='||quote_literal(rec)||') AND expl_id='||OLD.expl_id||'';
-					EXECUTE 'DELETE from arc_border_expl where arc_id in (SELECT arc_id FROM arc WHERE parent_id::text='||quote_literal(rec)||') AND expl_id='||OLD.expl_id||'';
-
-				END IF;
-			END LOOP;
+		  
 		END IF;
 	  RETURN NULL;
 	END IF;
