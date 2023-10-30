a149d7c0ed0133fa946f0a4068e0052127580ce1
edgarfuste <34737125+edgarfuste@users.noreply.github.com>
2023-10-30 12:47:18 +0100

    Update config_form_tabs on "v_edit_arc" formname

---
+++ dbmodel/updates/36/36006/ud/dml.sql
@@ -0,0 +1,20 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+UPDATE config_form_tabs SET tabactions='[{"actionName":"actionEdit", "disabled":false},
+{"actionName":"actionZoom", "disabled":false},
+{"actionName":"actionCentered", "disabled":false},
+{"actionName":"actionZoomOut", "disabled":false},
+{"actionName":"actionCatalog", "disabled":false},
+{"actionName":"actionWorkcat", "disabled":false},
+{"actionName":"actionCopyPaste","disabled":false},
+{"actionName":"actionSection", "disabled":false},
+{"actionName":"actionGetParentId", "disabled":false},
+{"actionName":"actionLink",  "disabled":false},
+{"actionName": "actionHelp", "disabled": false}]'::json WHERE formname='v_edit_arc' AND tabname='tab_data';
