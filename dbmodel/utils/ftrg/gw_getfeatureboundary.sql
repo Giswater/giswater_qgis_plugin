9b39fb7721879a06a01bac6380ea8bd5e9a719de
jblanch <102949291+jblanchg@users.noreply.github.com>
2023-11-07 12:43:28 +0100

    Return geojson and add support for type 'time'

--- dbmodel/utils/ftrg/gw_getfeatureboundary.sql
+++ dbmodel/utils/ftrg/gw_getfeatureboundary.sql
@@ -11,96 +11,84 @@ CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_getfeatureboundary(p_data json)
 $BODY$
 
 /*
-SELECT "SCHEMA_NAME".gw_fct_getfeatureboundary($${"client":{"device":4, "infoType":1, "lang":"ES"},"form":{},"feature":{"arc":[2001,2002], "node":[], "connec":[3001, 3002]},"data":{"type":"feature"}}$$)
-SELECT "SCHEMA_NAME".gw_fct_getfeatureboundary($${"client":{"device":4, "infoType":1, "lang":"ES"},"form":{},"feature":{},"data":{"type":"time", "lastSeed":"2023-05-05"}}$$)
-SELECT "SCHEMA_NAME".gw_fct_getfeatureboundary($${"client":{"device":4, "infoType":1, "lang":"ES"},"form":{},"feature":{},"data":{"type":"area", "polygon":"11223344}}$$) -- not solved
+SELECT SCHEMA_NAME.gw_fct_getfeatureboundary($${"client":{"device":4, "infoType":1, "lang":"ES"},"form":{},"feature":{"arc":[2001,2002], "node":[], "connec":[3001, 3002]},"data":{"type":"feature"}}$$)
+SELECT SCHEMA_NAME.gw_fct_getfeatureboundary($${"client":{"device":4, "infoType":1, "lang":"ES"},"form":{},"feature":{"update_tables":["node", "arc", "connec", "link"]},"data":{"type":"time", "lastSeed":"2023-05-05"}}$$)
+SELECT SCHEMA_NAME.gw_fct_getfeatureboundary($${"client":{"device":4, "infoType":1, "lang":"ES"},"form":{},"feature":{},"data":{"type":"area", "polygon":"11223344}}$$) -- not solved
 */  
 
 DECLARE    
-
-v_type text;
-v_lastseed text;
-v_data json;
-v_version json;
-v_querynode text ='';
-v_queryarc text ='';
-v_queryconnec text ='';
-v_querygully text ='';
-v_array text;
-v_querytext text;
-v_boundary text;
-v_union text = '';
-    
+  v_type text;
+  v_data json;
+  v_querynode text = '';
+  v_queryarc text = '';
+  v_queryconnec text = '';
+  v_querygully text = '';
+  v_array text;
+  v_querytext text;
+  v_boundary geometry;
+  v_geojson json;
+  v_lastseed text;
+  v_table text;
+  v_updatetables text;
 BEGIN
+  -- Set search path to local schema
+  SET search_path = "SCHEMA_NAME", public;
 
-	-- Set search path to local schema
-	SET search_path = "SCHEMA_NAME", public;
+  v_type = ((p_data ->>'data')::json->>'type');
 
-	-- Get api version
-	EXECUTE 'SELECT row_to_json(row) FROM (SELECT value FROM config_param_system WHERE parameter=''admin_version'') row'
-        INTO v_version;
+  IF v_type = 'time' then
+  
+   v_lastseed = ((p_data ->>'data')::json->>'lastSeed');
+   v_updatetables = ((p_data ->>'feature')::json->>'update_tables');
+   v_querytext='';
+   FOR v_table IN SELECT json_array_elements_text(v_updatetables::json) loop
+      raise notice 'table: %', v_table;
+      v_querytext = concat(v_querytext, 
+        ' SELECT the_geom FROM v_edit_', v_table, 
+        ' WHERE lastupdate > ''', v_lastseed, '''::timestamp UNION'
+      );
+   END LOOP;
+   v_querytext = left(v_querytext, length(v_querytext) - 6);
+   v_querytext = CONCAT('SELECT ST_AsGeoJSON(COALESCE(ST_Union(ST_Buffer(the_geom, 2)), ST_GeomFromText(''POINT EMPTY'')))
+   FROM (', v_querytext, ') AS combined_geometries');
+  
+  ELSIF v_type = 'area' THEN
+    -- Add code to handle 'area' type (if needed)
+  
+  ELSIF v_type = 'feature' THEN
+    v_array =  COALESCE(replace(replace(((p_data ->>'feature')::json->>'node'),'[','('),']',')'),'()') ;
+    IF v_array != '()' THEN
+      v_querynode = concat('SELECT the_geom FROM v_edit_node WHERE node_id::integer IN ', v_array);
+    END IF;
 
-	v_type = ((p_data ->>'data')::json->>'type');
+    v_array =  COALESCE(replace(replace(((p_data ->>'feature')::json->>'arc'),'[','('),']',')'),'()') ;
+    IF v_array != '()' THEN
+      v_queryarc = concat('SELECT the_geom FROM v_edit_arc WHERE arc_id::integer IN ', v_array);
+    END IF;
 
-	IF v_type = 'time' THEN
+    v_array =  COALESCE(replace(replace(((p_data ->>'feature')::json->>'connec'),'[','('),']',')'),'()') ;
+    IF v_array != '()' THEN
+      v_queryconnec = concat('SELECT the_geom FROM v_edit_connec WHERE connec_id::integer IN ', v_array);
+    END IF;
 
-		v_lastseed = ((p_data ->>'data')::json->>'lastSeed');
-		-- copy current code from service
-		--v_querytext = concat ('SELECT st_union(st_buffer(the_geom, 1)) from ......................;
+    v_array =  COALESCE(replace(replace(((p_data ->>'feature')::json->>'gully'),'[','('),']',')'),'()') ;
+    IF v_array != '()' THEN
+      v_querygully = concat('SELECT the_geom FROM v_edit_gully WHERE gully_id::integer IN ', v_array);
+    END IF;
 
+    -- Building the query text
+    v_querytext = concat('SELECT ST_AsGeoJSON(ST_Union(ST_Buffer(the_geom, 2))) from (', v_querynode, v_queryarc, v_queryconnec, v_querygully, ') a');
+  END IF;
 
-	ELSIF v_type = 'area' THEN
-		-- todo
+  -- Execute query text and set boundary geometry
+  EXECUTE v_querytext INTO v_geojson;
 
-	ELSIF v_type = 'feature' THEN
+  RETURN v_geojson;
 
-		v_array =  COALESCE (replace(replace(((p_data ->>'feature')::json->>'node'),'[','('),']',')'),'()') ;
-		IF v_array !='()' THEN
-			v_querynode = concat ('SELECT the_geom FROM v_edit_node WHERE node_id::integer IN ', v_array);
-			v_union  = ' UNION ';
-		END IF;
-
-		v_array =  COALESCE (replace(replace(((p_data ->>'feature')::json->>'arc'),'[','('),']',')'),'()') ;
-		IF v_array !='()' THEN
-			v_queryarc = concat (v_union, 'SELECT the_geom FROM v_edit_arc WHERE arc_id::integer IN ', v_array);
-			v_union  = ' UNION ';
-		END IF;
-		
-		v_array =  COALESCE (replace(replace(((p_data ->>'feature')::json->>'connec'),'[','('),']',')'),'()') ;
-		IF v_array !='()' THEN
-			v_queryconnec = concat (v_union, 'SELECT the_geom FROM v_edit_connec WHERE connec_id::integer IN ', v_array);
-			v_union  = ' UNION ';
-		END IF;
-
-		v_array =  COALESCE (replace(replace(((p_data ->>'feature')::json->>'gully'),'[','('),']',')'),'()') ;
-		IF v_array !='()' THEN
-			v_querygully = concat (v_union, 'SELECT the_geom FROM v_edit_gully WHERE gully_id::integer IN ', v_array);
-		END IF;
-
-		--building querytext
-		v_querytext = concat ('SELECT st_union(st_buffer(the_geom, 2)) from (',v_querynode, v_queryarc , v_queryconnec , v_querygully, ')a');
-		
-	END IF;
-
-	-- execute query text and buildup boundary geometry
-	EXECUTE v_querytext INTO v_boundary;
-	v_data = concat ('{"boundary":"',v_boundary,'"}');
-
-	-- check null
-	v_version := COALESCE(v_version, '[]');
-	v_data := COALESCE(v_data, '{}'); 
-
-	-- Return
-	RETURN ('{"status":"Accepted"' ||
-        ', "version":'|| v_version ||
-        ', "data":'|| v_data || 
-        '}')::json;
-
-	-- Exception handling
-	EXCEPTION WHEN OTHERS THEN 
-	RETURN ('{"status":"Failed","SQLERR":' || to_json(SQLERRM) || ', "version":'|| v_version || ',"SQLSTATE":' || to_json(SQLSTATE) || '}')::json;
-
+  -- Exception handling
+  EXCEPTION WHEN OTHERS THEN 
+    RETURN NULL;
 END;
 $BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
+LANGUAGE plpgsql VOLATILE
+COST 100;
