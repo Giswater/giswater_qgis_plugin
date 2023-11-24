91c650e4917c6ce524c2b5684d9db56f8a917b93
smaspons <smaspons@bgeo.es>
2023-11-24 15:10:00 +0100

    Bug fix: Disable double-click on arc/node/connec tableviews in netscenario

--- core/toolbars/plan/netscenario_manager_btn.py
+++ core/toolbars/plan/netscenario_manager_btn.py
@@ -365,7 +365,8 @@ class GwNetscenarioManagerButton(GwAction):
                 qtableview.clicked.connect(partial(self._manage_highlight, qtableview, view))
                 tab_idx = self.dlg_netscenario.main_tab.addTab(qtableview, f"{view.split('_')[-1].capitalize()}")
                 self.dlg_netscenario.main_tab.widget(tab_idx).setObjectName(view)
-                qtableview.doubleClicked.connect(partial(self._manage_update))
+                if view not in ('plan_netscenario_arc', 'plan_netscenario_node', 'plan_netscenario_connec'):
+                    qtableview.doubleClicked.connect(partial(self._manage_update))
                 if view.split('_')[-1].upper() == self.selected_netscenario_type:
                     default_tab_idx = tab_idx
 
