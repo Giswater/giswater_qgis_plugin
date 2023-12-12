84628e59adaf6096152dcb6d9ceae93e6f7330e6
edgarfuste <34737125+edgarfuste@users.noreply.github.com>
2023-12-12 16:44:54 +0100

    Bug fix: manage dwf_scenarios and hydrology_scenarios

--- dbmodel/ud/fct/ud_gw_fct_create_dwf_scenario_empty.sql
+++ dbmodel/ud/fct/ud_gw_fct_create_dwf_scenario_empty.sql
@@ -6,40 +6,20 @@ This version of Giswater is provided by Giswater Association
 
 --FUNCTION CODE: 3134
 
-CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gw_fct_create_dscenario_empty(p_data json) 
-RETURNS json AS 
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_create_dwf_scenario_empty(p_data json)
+RETURNS json AS
 $BODY$
 
-/*EXAMPLE
-SELECT SCHEMA_NAME.gw_fct_create_dscenario_empty($${"client":{"device":4, "infoType":1, "lang":"ES"}, "data":{"parameters":{"target":"1", "copyFrom":"2", "action":"DELETE-COPY"}}}$$)
-
--- fid: 438
-
-*/
-
-
-DECLARE
-
-object_rec record;
-
-v_version text;
-v_result json;CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_create_dwf_scenario_empty(p_data json)
- RETURNS json
- LANGUAGE plpgsql
-AS $function$
 
 /*EXAMPLE
 SELECT SCHEMA_NAME.gw_fct_create_dwf_scenario_empty($${"client":{"device":4, "infoType":1, "lang":"ES"}, "data":{"parameters":{"target":"1", "copyFrom":"2", "action":"DELETE-COPY"}}}$$)
-
--- fid: 524
-
+-- fid: 438
 */
 
 
 DECLARE
 
 object_rec record;
-
 v_version text;
 v_result json;
 v_result_info json;
@@ -55,7 +35,6 @@ v_target_name text;
 v_action text;
 v_querytext text;
 v_result_id text = null;
-
 v_idval text;
 v_startdate date;
 v_enddate date;
@@ -64,7 +43,6 @@ v_expl_id integer;
 v_active boolean;
 v_scenarioid text;
 v_aux_params json;
-
 v_inp_dwf text;
 
 
@@ -74,23 +52,23 @@ BEGIN
 
 	-- select version
 	SELECT giswater, project_type INTO v_version, v_projecttype FROM sys_version ORDER BY id DESC LIMIT 1;
-	
-	-- getting variables	
+
+	-- getting variables
 	v_inp_dwf = (SELECT value FROM config_param_user WHERE parameter = 'inp_options_dwfscenario' AND cur_user = current_user limit 1);
 
-	-- getting input data 	
+	-- getting input data
 	v_idval :=  ((p_data ->>'data')::json->>'parameters')::json->>'idval';
 	v_startdate :=  ((p_data ->>'data')::json->>'parameters')::json->>'startdate';
 	v_enddate :=  ((p_data ->>'data')::json->>'parameters')::json->>'enddate';
 	v_observ :=  ((p_data ->>'data')::json->>'parameters')::json->>'observ';
 	v_expl_id :=  ((p_data ->>'data')::json->>'parameters')::json->>'expl';
 	v_active :=  ((p_data ->>'data')::json->>'parameters')::json->>'active';
-	v_aux_params := ((p_data ->>'data')::json->>'aux_params')::json;	
-		
+	v_aux_params := ((p_data ->>'data')::json->>'aux_params')::json;
+
 	-- Reset values
 	DELETE FROM anl_node WHERE cur_user="current_user"() AND fid=v_fid;
-	DELETE FROM audit_check_data WHERE cur_user="current_user"() AND fid=v_fid;	
-	
+	DELETE FROM audit_check_data WHERE cur_user="current_user"() AND fid=v_fid;
+
 	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, concat('CREATE EMPTY DSCENARIO'));
 	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, '--------------------------------------------------');
 	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, concat('Id_val: ',v_idval));
@@ -104,7 +82,7 @@ BEGIN
 
 	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 3, 'ERRORS');
 	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 3, '--------');
-	
+
 	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 2, 'WARNINGS');
 	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 2, '---------');
 
@@ -113,18 +91,18 @@ BEGIN
 
 	-- process
 	-- inserting on catalog table
-	PERFORM setval('SCHEMA_NAME.cat_dwf_scenario_id_seq'::regclass,(SELECT max(dscenario_id) FROM cat_dscenario) ,true);
+	PERFORM setval('SCHEMA_NAME.cat_dwf_scenario_id_seq'::regclass,(SELECT max(id) FROM cat_dwf_scenario) ,true);
 
-	INSERT INTO cat_dwf_scenario (idval, startdate, enddate, observ, active, expl_id,log) 
+	INSERT INTO cat_dwf_scenario (idval, startdate, enddate, observ, active, expl_id,log)
 	VALUES (v_idval, v_startdate, v_enddate, v_observ, v_active, v_expl_id, concat('Created by ',current_user,' on ',substring(now()::text,0,20)))
 	ON CONFLICT DO NOTHING
 	RETURNING id INTO v_scenarioid;
 
 	IF v_scenarioid IS NULL THEN
 		SELECT id INTO v_scenarioid FROM cat_dwf_scenario where idval = v_idval;
-		INSERT INTO audit_check_data (fid, result_id, criticity, error_message)	
+		INSERT INTO audit_check_data (fid, result_id, criticity, error_message)
 		VALUES (v_fid, null, 3, concat('ERROR: The dwf scenario ( ',v_scenarioid,' ) already exists with proposed name ',v_scenarioid ,'. Please try another one.'));
-	ELSE 
+	ELSE
 		INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 1, 'The new dscenario have been created sucessfully');
 	END IF;
 
@@ -134,135 +112,25 @@ BEGIN
 
 	-- get results
 	-- info
-	SELECT array_to_json(array_agg(row_to_json(row))) INTO v_result 
+	SELECT array_to_json(array_agg(row_to_json(row))) INTO v_result
 	FROM (SELECT id, error_message as message FROM audit_check_data WHERE cur_user="current_user"() AND fid=v_fid order by criticity desc, id asc) row;
-	v_result := COALESCE(v_result, '{}'); 
+	v_result := COALESCE(v_result, '{}');
 	v_result_info = concat ('{"geometryType":"", "values":',v_result, '}');
-	
+
 	-- Execute aux_func
 	if v_aux_params->>'aux_fct'::text = '3102' then
 		execute 'SELECT gw_fct_manage_dwf_values($${"client":{"device":4, "lang":"", "infoType":1, "epsg":25831}, "form":{}, "feature":{}, "data":{"filterFields":{}, "pageInfo":{}, "parameters":{"target":"'||v_scenarioid||'", "sector":"-999", "action":"KEEP-COPY", "copyFrom":"'||v_inp_dwf||'"}}}$$);';
 	end if;
 
 	-- Control nulls
-	v_result_info := COALESCE(v_result_info, '{}'); 
+	v_result_info := COALESCE(v_result_info, '{}');
 
 	-- Return
 	RETURN gw_fct_json_create_return(('{"status":"Accepted", "message":{"level":1, "text":"Analysis done successfully"}, "version":"'||v_version||'"'||
              ',"body":{"form":{}'||
 		     ',"data":{ "info":'||v_result_info||
 			'}}'||
-	    '}')::json, 3042, null, null, null); 
-
-	-- manage exceptions
-	EXCEPTION WHEN OTHERS THEN
-	GET STACKED DIAGNOSTICS v_error_context = PG_EXCEPTION_CONTEXT;
-	RETURN ('{"status":"Failed","NOSQLERR":' || to_json(SQLERRM) || ',"SQLSTATE":' || to_json(SQLSTATE) ||',"SQLCONTEXT":' || to_json(v_error_context) || '}')::json;
-
-END;
-$function$
-;
-
-
-v_result_info json;
-v_copyfrom integer;
-v_target integer;
-v_error_context text;
-v_count integer;
-v_count2 integer;
-v_projecttype text;
-v_fid integer = 438;
-v_source_name text;
-v_target_name text;
-v_action text;
-v_querytext text;
-v_result_id text = null;
-
-v_name text;
-v_descript text;
-v_parent_id integer;
-v_dscenario_type text;
-v_active boolean;
-v_expl_id integer;
-v_scenarioid integer;
-
-
-BEGIN
-
-	SET search_path = "SCHEMA_NAME", public;
-
-	-- select version
-	SELECT giswater, project_type INTO v_version, v_projecttype FROM sys_version ORDER BY id DESC LIMIT 1;
-	
-	-- getting input data 	
-	v_name :=  ((p_data ->>'data')::json->>'parameters')::json->>'name';
-	v_descript :=  ((p_data ->>'data')::json->>'parameters')::json->>'descript';
-	v_parent_id :=  ((p_data ->>'data')::json->>'parameters')::json->>'parent';
-	v_dscenario_type :=  ((p_data ->>'data')::json->>'parameters')::json->>'type';
-	v_active :=  ((p_data ->>'data')::json->>'parameters')::json->>'active';
-	v_expl_id :=  ((p_data ->>'data')::json->>'parameters')::json->>'expl';
-	
-		
-	-- Reset values
-	DELETE FROM anl_node WHERE cur_user="current_user"() AND fid=v_fid;
-	DELETE FROM audit_check_data WHERE cur_user="current_user"() AND fid=v_fid;	
-	
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, concat('CREATE EMPTY DSCENARIO'));
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, '--------------------------------------------------');
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, concat('Name: ',v_name));
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, concat('Descript: ',v_descript));
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, concat('Parent: ',v_parent_id));
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, concat('Type: ',v_dscenario_type));
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, concat('active: ',v_active));
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, concat(''));
-
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 3, 'ERRORS');
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 3, '--------');
-	
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 2, 'WARNINGS');
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 2, '---------');
-
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 1, 'INFO');
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 1, '---------');
-
-	-- process
-	-- inserting on catalog table
-	PERFORM setval('SCHEMA_NAME.cat_dscenario_dscenario_id_seq'::regclass,(SELECT max(dscenario_id) FROM cat_dscenario) ,true);
-
-	INSERT INTO cat_dscenario (name, descript, parent_id, dscenario_type, active, expl_id,log) 
-	VALUES (v_name, v_descript, v_parent_id, v_dscenario_type, v_active, v_expl_id, concat('Created by ',current_user,' on ',substring(now()::text,0,20)))
-	ON CONFLICT (name) DO NOTHING
-	RETURNING dscenario_id INTO v_scenarioid;
-
-	IF v_scenarioid IS NULL THEN
-		SELECT dscenario_id INTO v_scenarioid FROM cat_dscenario where name = v_name;
-		INSERT INTO audit_check_data (fid, result_id, criticity, error_message)	
-		VALUES (v_fid, null, 3, concat('ERROR: The dscenario ( ',v_scenarioid,' ) already exists with proposed name ',v_name ,'. Please try another one.'));
-	ELSE 
-	
-		INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 1, 'The new dscenario have been created sucessfully');
-	END IF;
-
-	-- insert spacers
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 3, concat(''));
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 2, concat(''));
-
-	-- get results
-	-- info
-	SELECT array_to_json(array_agg(row_to_json(row))) INTO v_result 
-	FROM (SELECT id, error_message as message FROM audit_check_data WHERE cur_user="current_user"() AND fid=v_fid order by criticity desc, id asc) row;
-	v_result := COALESCE(v_result, '{}'); 
-	v_result_info = concat ('{"geometryType":"", "values":',v_result, '}');
-
-	-- Control nulls
-	v_result_info := COALESCE(v_result_info, '{}'); 
-
-	-- Return
-	RETURN gw_fct_json_create_return(('{"status":"Accepted", "message":{"level":1, "text":"Analysis done successfully"}, "version":"'||v_version||'"'||
-             ',"body":{"form":{}'||
-		     ',"data":{ "info":'||v_result_info||
-			'}}'||
-	    '}')::json, 3042, null, null, null); 
+	    '}')::json, 3042, null, null, null);
 
 	-- manage exceptions
 	EXCEPTION WHEN OTHERS THEN
