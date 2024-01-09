7f7eac5759aea6700cbe64a64e661903e17f4a5e
xtorret@bgeo.es <xtorret@bgeo.es>
2024-01-09 17:30:16 +0100

    Reserve sys_fprocess codes for ddss plugin

---
+++ dbmodel/updates/36/36007/ws/dml.sql
@@ -0,0 +1,23 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+
+-- 9/1/2024;
+
+/*
+reserve for ddss plugin
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type, parameters, "source", isaudit, fprocess_type, addparam) 
+VALUES(527, 'Consumptions analysis', 'ws', NULL, 'ddss plugin', NULL, 'Function process', NULL);
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type, parameters, "source", isaudit, fprocess_type, addparam) 
+VALUES(528, 'Hydro without census', 'ws', NULL, 'ddss plugin', NULL, 'Function process', NULL);
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type, parameters, "source", isaudit, fprocess_type, addparam) 
+VALUES(529, 'Census whitout hydro', 'ws', NULL, 'ddss plugin' , NULL, 'Function process', NULL);
+*/
