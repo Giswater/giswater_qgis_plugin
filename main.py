e2545daeae9dce00729b70a46f30e53c5d3c58e4
smaspons <smaspons@bgeo.es>
2024-01-10 09:36:33 +0100

    Minor fix from last commit

--- main.py
+++ main.py
@@ -279,7 +279,7 @@ class Giswater(QObject):
 
             if not global_vars.gw_dev_mode:
                 # Copy user_params file to user config folder
-                src = f"{lib_vars.plugin_dir}{os.sep}config{os.sep}user_params.config"
+                src = f"{global_vars.plugin_dir}{os.sep}config{os.sep}user_params.config"
                 dst = f"{config_folder}{os.sep}user_params.config"
                 shutil.copyfile(src, dst)
 
