56e4f4d7d276446e556b183264b6d71606725d9d
Daniel Marin <dmarin@bgeo.es>
2024-01-25 11:37:01 +0100

    WIP rename files to read in i18n folder, only read xx_schema_model.sql

--- core/admin/admin_btn.py
+++ core/admin/admin_btn.py
@@ -1765,8 +1765,7 @@ class GwAdminButton:
             manage_i18n = True
 
         if manage_i18n:
-            files_to_execute = [f"{self.project_type_selected}.sql", "utils.sql", "ddl.sql", "ddlview.sql", "dml.sql",
-                                "tablect.sql", "trg.sql"]
+            files_to_execute = [f"{self.project_type_selected}_schema_model.sql"]
             for file in filelist:
                 status = True
                 if file in files_to_execute:
