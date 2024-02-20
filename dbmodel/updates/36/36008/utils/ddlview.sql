259cdc524e4ee2981359d3ad56a7e0391cfb4377
smaspons <smaspons@bgeo.es>
2024-02-20 16:21:07 +0100

    Enhancement: add geometry to psector other prices

---
+++ dbmodel/updates/36/36008/utils/ddlview.sql
@@ -0,0 +1,25 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+CREATE OR REPLACE VIEW v_edit_plan_psector_x_other
+AS SELECT plan_psector_x_other.id,
+    plan_psector_x_other.psector_id,
+    v_price_compost.id AS price_id,
+    v_price_compost.unit,
+    rpad(v_price_compost.descript::text, 125) AS price_descript,
+    v_price_compost.price,
+    plan_psector_x_other.measurement,
+    (plan_psector_x_other.measurement * v_price_compost.price)::numeric(14,2) AS total_budget,
+    plan_psector_x_other.observ,
+    plan_psector.atlas_id,
+    plan_psector_x_other.the_geom
+   FROM plan_psector_x_other
+     JOIN v_price_compost ON v_price_compost.id::text = plan_psector_x_other.price_id::text
+     JOIN plan_psector ON plan_psector.psector_id = plan_psector_x_other.psector_id
+  ORDER BY plan_psector_x_other.psector_id;
