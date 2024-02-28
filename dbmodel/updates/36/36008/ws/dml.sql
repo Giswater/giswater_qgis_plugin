6cec9a327d7e96737a8f5cd4862e51edfc54b249
smaspons <smaspons@bgeo.es>
2024-02-28 09:31:33 +0100

    Config inp_dscenario forms

--- dbmodel/updates/36/36008/ws/dml.sql
+++ dbmodel/updates/36/36008/ws/dml.sql
@@ -22,3 +22,14 @@ DELETE FROM config_form_fields WHERE formname = 've_epa_virtualpump' and columnn
 UPDATE config_form_fields
 	SET widgetcontrols='{"saveValue": false, "tableUpsert": "v_edit_inp_dscenario_virtualpump"}'::json, linkedobject='tbl_inp_dscenario_virtualpump', columnname='tbl_inp_virtualpump'
 	WHERE formname='ve_epa_virtualpump' AND columnname='tbl_inp_pump';
+
+-- 28/02/2024
+
+DELETE FROM config_form_fields
+	WHERE formname='inp_dscenario_connec' AND formtype='form_feature' AND columnname='pjoint_type' AND tabname='tab_none';
+DELETE FROM config_form_fields
+	WHERE formname='inp_dscenario_connec' AND formtype='form_feature' AND columnname='pjoint_id' AND tabname='tab_none';
+
+UPDATE sys_table
+	SET addparam='{"pkey": "dscenario_id, feature_id"}'::json
+	WHERE id='inp_dscenario_demand';
