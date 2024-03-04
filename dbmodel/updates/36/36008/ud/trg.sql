bb5d05d7ebd2c71661c93b386deffc9d3f29ca13
smaspons <smaspons@bgeo.es>
2024-03-04 09:48:02 +0100

    Bug fix: move trg_node_rotation_update to updates
    (cherry picked from commit 65c33a7a20cff061b6bcfda4ee20709db9fe85a1)

---
+++ dbmodel/updates/36/36008/ud/trg.sql
@@ -0,0 +1,12 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+
+--2024/03/04
+CREATE TRIGGER gw_trg_node_rotation_update AFTER INSERT OR UPDATE OF hemisphere, the_geom ON node FOR EACH ROW EXECUTE FUNCTION gw_trg_node_rotation_update();
