42802725bd8a5751c24fbcaba648fe70238b369b
Maria Guzmán <mguzman@bgeo.es>
2024-03-07 12:16:14 +0100

    Set graphconfig and stylesheet as text on v_edit_drainzone (ud)

---
+++ dbmodel/updates/36/36008/ud/ddlview.sql
@@ -0,0 +1,25 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+drop view IF EXISTS v_edit_drainzone;
+
+CREATE OR REPLACE VIEW v_edit_drainzone
+AS SELECT drainzone.drainzone_id,
+    drainzone.name,
+    drainzone.expl_id,
+    drainzone.descript,
+    drainzone.undelete,
+    drainzone.link,
+    drainzone.graphconfig::text,
+    drainzone.stylesheet::text,
+    drainzone.active,
+    drainzone.the_geom
+   FROM selector_expl,
+    drainzone
+  WHERE drainzone.expl_id = selector_expl.expl_id AND selector_expl.cur_user = "current_user"()::text;
