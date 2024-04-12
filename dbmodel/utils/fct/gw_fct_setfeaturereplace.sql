dae26295084ce259335e378df82a23c06457ccfa
Albert Bofill <abofill@bgeo.es>
2024-04-12 13:38:20 +0200

    Enhance fct_setfeaturereplace: always get epa_default, fix epa table update and delete OUTFALL condition

--- dbmodel/utils/fct/gw_fct_setfeaturereplace.sql
+++ dbmodel/utils/fct/gw_fct_setfeaturereplace.sql
@@ -36,7 +36,7 @@ v_column varchar;
 v_value text;
 v_state integer;
 v_state_type integer;
-v_epa_type text;
+v_old_epa_type text;
 v_epa_type_new text;
 rec_arc record;	
 v_old_featuretype varchar;
@@ -167,6 +167,9 @@ BEGIN
 	EXECUTE 'SELECT  '|| v_cat_column||' FROM '|| v_feature_layer  ||'  WHERE '||v_id_column||'='''||v_old_feature_id||''';'
 	INTO v_old_featurecat;
 
+	EXECUTE 'SELECT epa_type FROM '|| v_feature_layer  ||'  WHERE '||v_id_column||'='''||v_old_feature_id||''';'
+	INTO v_old_epa_type;
+
 	EXECUTE 'SELECT sector_id FROM '||v_feature_layer||' WHERE '||v_id_column||'='''||v_old_feature_id||''';'
 	INTO v_sector_id;
 	EXECUTE 'SELECT state_type FROM '||v_feature_layer||' WHERE '||v_id_column||'='''||v_old_feature_id||''';'
@@ -216,11 +219,12 @@ BEGIN
 	IF v_feature_type ='connec' AND v_project_type = 'UD' THEN
 		-- is not epa
 	ELSE
-		EXECUTE 'SELECT epa_type FROM '||v_feature_layer||' WHERE '||v_id_column||'='''||v_old_feature_id||''';'
+		EXECUTE 'select epa_default from cat_feature_'||v_feature_type||' where id='''||v_feature_type_new||''';'	
 		INTO v_epa_type_new;
 
-		EXECUTE 'SELECT epa_table FROM sys_feature_epa_type WHERE feature_type = '||quote_literal(v_feature_type)||' AND id = '||quote_literal(v_epa_type_new)
-		INTO v_epa_table_new;
+		IF v_epa_type_new <> 'UNDEFINED' THEN
+			v_epa_table_new = concat('inp_', lower(v_epa_type_new));
+		END IF;
 	END IF;
 	
 	-- Control of state(1)
@@ -260,11 +264,6 @@ BEGIN
 				VALUES (v_id, v_code, v_old_featuretype, v_old_featurecat, v_epa_type_new, v_sector_id, v_dma_id, v_expl_id, 
 				0, v_state_type, v_workcat_id_end, v_the_geom, v_category, v_function, v_fluid, v_location, v_drainzone_id);
 
-				IF v_epa_type_new = 'OUTFALL' THEN
-					DELETE FROM drainzone WHERE name = v_old_feature_id;
-					INSERT INTO drainzone (name,expl_id, graphconfig)
-					VALUES (v_id,v_expl_id, concat('{"use":[{"nodeParent":"',v_id,'"}], "ignore":[], "forceClosed":[]}')::json);
-				END IF;
 			END IF;
 
 			INSERT INTO audit_check_data (fid, result_id, error_message)
@@ -321,12 +320,12 @@ BEGIN
 			EXECUTE 'SELECT man_table FROM cat_feature c JOIN sys_feature_cat s ON c.system_id = s.id WHERE c.id='''||v_feature_type_new||''';'
 				INTO v_man_table;
 		
-			v_query_string_insert='INSERT INTO '||v_man_table||' VALUES ('||v_id||');';
+			v_query_string_insert='INSERT INTO '||v_man_table||' VALUES ('||v_id||') ON CONFLICT ('||v_feature_type||'_id) DO NOTHING;';
 			EXECUTE v_query_string_insert;
 		END IF;
 
 		IF v_epa_table_new IS NOT NULL THEN
-			v_query_string_insert='INSERT INTO '||v_epa_table_new||' VALUES ('||v_id||');';
+			v_query_string_insert='INSERT INTO '||v_epa_table_new||' VALUES ('||v_id||') ON CONFLICT ('||v_feature_type||'_id) DO NOTHING;';
 			EXECUTE v_query_string_insert;
 		END IF;
 				
@@ -374,19 +373,19 @@ BEGIN
 		END IF;
 			
 		-- updating values on table epa_table from values of old feature
-		IF (v_feature_type='node' or v_feature_type='arc' or (v_feature_type='connec' AND v_project_type='WS')) and v_epa_table is not null AND 
-		v_epa_type_new = v_epa_type THEN
+		IF (v_feature_type='node' or v_feature_type='arc' or (v_feature_type='connec' AND v_project_type='WS')) and v_epa_table_new is not null AND 
+		v_epa_type_new = v_old_epa_type THEN
 			v_sql:='select column_name  FROM information_schema.columns 
 								where (table_schema=''SCHEMA_NAME'' and udt_name <> ''inet'' and 
-								table_name='''||v_epa_table||''') and column_name!='''||v_id_column||''';';
+								table_name='''||v_epa_table_new||''') and column_name!='''||v_id_column||''';';
 			
 			FOR v_column IN EXECUTE v_sql LOOP
-				v_query_string_select= 'SELECT '||v_column||' FROM '||v_epa_table||' where '||v_feature_type||'_id='||quote_literal(v_old_feature_id)||';';
+				v_query_string_select= 'SELECT '||v_column||' FROM '||v_epa_table_new||' where '||v_feature_type||'_id='||quote_literal(v_old_feature_id)||';';
 				IF v_query_string_select IS NOT NULL THEN
 					EXECUTE v_query_string_select INTO v_value;	
 				END IF;
 				
-				v_query_string_update= 'UPDATE '||v_epa_table||' set '||v_column||'='||quote_literal(v_value)||' where '||v_id_column||'='||quote_literal(v_id)||';';
+				v_query_string_update= 'UPDATE '||v_epa_table_new||' set '||v_column||'='||quote_literal(v_value)||' where '||v_id_column||'='||quote_literal(v_id)||';';
 				IF v_query_string_update IS NOT NULL THEN
 					EXECUTE v_query_string_update; 
 
