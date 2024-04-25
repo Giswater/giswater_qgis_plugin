e28629fa7c198250558c45ee02f5a9d2de626e4a
Albert Bofill <abofill@bgeo.es>
2024-04-25 10:30:35 +0200

    Set cascade FK on inp_flwreg_pump related to arc

---
+++ dbmodel/updates/36/36010/ud/tablect.sql
@@ -0,0 +1,15 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+
+ALTER TABLE inp_flwreg_pump DROP CONSTRAINT inp_flwreg_pump_to_arc_fkey;
+
+ALTER TABLE inp_flwreg_pump ADD CONSTRAINT inp_flwreg_pump_to_arc_fkey FOREIGN KEY (to_arc) 
+REFERENCES arc(arc_id) ON UPDATE CASCADE ON DELETE CASCADE;
+
