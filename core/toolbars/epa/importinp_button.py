7ae118e81cc660d94d99730d2e2a28d35e33fb1b
Pablo Marques <pahbloo@gmail.com>
2024-05-03 12:22:57 -0300

    Check for EPA and SWMM packages before importing INPs

--- core/toolbars/epa/importinp_button.py
+++ core/toolbars/epa/importinp_button.py
@@ -1,7 +1,16 @@
+from enum import Enum
+
 from qgis.PyQt.QtWidgets import QActionGroup
 
+from .... import global_vars
+from ....libs import tools_qgis
 from ...models.plugin_toolbar import GwPluginToolbar
 from ..dialog import GwAction
+
+
+class ProjectType(Enum):
+    WS = "ws"
+    UD = "ud"
 
 
 class GwImportInp(GwAction):
@@ -16,3 +25,24 @@ class GwImportInp(GwAction):
         action_group: QActionGroup,
     ) -> None:
         super().__init__(icon_path, action_name, text, toolbar, action_group)
+
+    def clicked_event(self) -> None:
+        """Start the Import INP workflow"""
+
+        # WS
+        if global_vars.project_type == ProjectType.WS.value:
+            try:
+                import wntr
+                tools_qgis.show_message("OK")
+            except ImportError:
+                message: str = "wntr package not installed. Open OSGeo4W Shell and execute 'python -m pip install wntr'."
+                tools_qgis.show_message(message)
+
+        # UD
+        if global_vars.project_type == ProjectType.UD.value:
+            try:
+                import swmm_api
+                tools_qgis.show_message("OK")
+            except ImportError:
+                message: str = "swmm-api package not installed. Open OSGeo4W Shell and execute 'python -m pip install swmm-api'."
+                tools_qgis.show_message(message)
