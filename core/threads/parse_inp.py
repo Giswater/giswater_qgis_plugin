080c6023ddef5ec9a58f09415a3c9b0813053c8d
Pablo Marques <pahbloo@gmail.com>
2024-05-08 10:24:26 -0300

    Check units of INP file

--- core/threads/parse_inp.py
+++ core/threads/parse_inp.py
@@ -18,6 +18,15 @@ class GwParseInpTask(GwTask):
         super().run()
         try:
             wn = wntr.network.WaterNetworkModel(self.inp_file_path)
+            flow_units: str = wn.options.hydraulic.inpfile_units
+
+            if flow_units not in ("LPS", "LPM", "MLD", "CMH", "CMD"):
+                message: str = (
+                    "Flow units should be in liters or cubic meters for importing. "
+                    f"INP file units: {flow_units}"
+                )
+                raise ValueError(message)
+
             return True
         except Exception as e:
             self.exception: str = traceback.format_exc()
