00adc302de2c6b32dba429bfa194a046a3b6d0b1
Pablo Marques <pahbloo@gmail.com>
2024-05-21 09:26:23 -0300

    Save curves

--- core/threads/import_inp.py
+++ core/threads/import_inp.py
@@ -4,8 +4,9 @@ from itertools import count, islice
 from typing import Any
 
 import psycopg2
+from wntr.epanet.util import FlowUnits, HydParam, from_si
+from wntr.network.model import WaterNetworkModel
 
-# from wntr.network.model import WaterNetworkModel
 from ...libs import tools_db
 from .task import GwTask
 
@@ -32,14 +33,13 @@ class GwImportInpTask(GwTask):
     ) -> None:
         super().__init__(description)
         self.filepath = filepath
-        # self.network: WaterNetworkModel = network
-        self.network = network
+        self.network: WaterNetworkModel = network
         self.workcat: str = workcat
         self.exploitation: str = exploitation
         self.sector: str = sector
         self.catalogs: dict[str, Any] = catalogs
         self.log: list[str] = []
-        self.mappings: dict[str, dict[str, str]] = {"patterns": {}}
+        self.mappings: dict[str, dict[str, str]] = {"curves": {}, "patterns": {}}
 
     def run(self) -> bool:
         super().run()
@@ -61,6 +61,15 @@ class GwImportInpTask(GwTask):
                 with conn.cursor() as cur:
                     if tools_db.dao.set_search_path:
                         cur.execute(tools_db.dao.set_search_path)
+
+                    # Get units of project
+                    cur.execute(
+                        "SELECT value FROM vi_options WHERE parameter = 'UNITS'"
+                    )
+                    if cur.rowcount > 0:
+                        self.db_units: str = cur.fetchone()[0]
+                    else:
+                        raise ValueError("Units not specified in the Giswater project.")
 
                     # Create workcat
                     sql = """
@@ -104,6 +113,44 @@ class GwImportInpTask(GwTask):
                                 b: tuple[Any] = b + (None,)
                             values = (pattern_name,) + b
                             cur.execute(sql, values)
+
+                    # Save curves
+                    cur.execute("SELECT id FROM inp_curve")
+                    curves_db: set[str] = set()
+                    if cur.rowcount > 0:
+                        curves_db = {x[0] for x in cur.fetchall()}
+
+                    for curve_name, curve in self.network.curves.items():
+                        if curve.curve_type is None:
+                            message = f'The "{curve_name}" curve does not have a specified curve type and was not imported.'
+                            continue
+
+                        if curve_name in curves_db:
+                            for i in count(2):
+                                new_name = f"{curve_name}_{i}"
+                                if new_name in curves_db:
+                                    continue
+                                message = f'The curve "{curve_name}" has been renamed to "{new_name}" to avoid a collision with an existing curve.'
+                                self.mappings["curves"][curve_name] = new_name
+                                curve_name = new_name
+                                break
+
+                        curve_type: str = curve.curve_type
+                        if curve.curve_type == "HEAD":
+                            curve_type = "PUMP"
+
+                        cur.execute(
+                            "INSERT INTO inp_curve (id, curve_type) VALUES (%s, %s)",
+                            (curve_name, curve_type),
+                        )
+
+                        for x, y in curve.points:
+                            if curve_type != "VOLUME":
+                                x = from_si(FlowUnits[self.db_units], x, HydParam.Flow)
+                            cur.execute(
+                                "INSERT INTO inp_curve_value (curve_id, x_value, y_value) VALUES (%s, %s, %s)",
+                                (curve_name, x, y),
+                            )
 
             return True
         except Exception as e:
