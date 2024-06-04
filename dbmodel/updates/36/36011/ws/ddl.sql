b402c2d6d879640fd9fa7e5c2499df6f805df204
smaspons <smaspons@bgeo.es>
2024-06-04 09:16:53 +0200

    Bug fix: missing `;` on ddl

--- dbmodel/updates/36/36011/ws/ddl.sql
+++ dbmodel/updates/36/36011/ws/ddl.sql
@@ -7,7 +7,7 @@ This version of Giswater is provided by Giswater Association
 
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
-SELECT gw_fct_admin_manage_fields($${"data":{"action":"RENAME","table":"inp_shortpipe", "column":"status", "newName":"_status_"}}$$)
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"RENAME","table":"inp_shortpipe", "column":"status", "newName":"_status_"}}$$);
 
 ALTER TABLE config_graph_inlet DROP CONSTRAINT config_graph_inlet_pkey;
 ALTER TABLE config_graph_inlet ADD CONSTRAINT config_graph_inlet_pkey PRIMARY KEY (node_id);
