5a00c167800dc5100ed5233411746b6fb05b0168
xtorret@bgeo.es <xtorret@bgeo.es>
2024-06-25 09:21:48 +0200

    Minor changes from prev commits

--- dbmodel/updates/36/36011/utils/dml.sql
+++ dbmodel/updates/36/36011/utils/dml.sql
@@ -10,7 +10,7 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 INSERT INTO inp_typevalue VALUES ('inp_typevalue_dscenario','NETWORK','NETWORK');
 
-INSERT INTO sys_function (id, function_name, project_type, function_type, input_params, return_type, descript, sys_role, sample_query, "source")
+INSERT INTO sys_function (id, function_name, project_type, function_type, input_params, return_type, descript, sys_role, sample_query, 'source')
 VALUES(3308, 'gw_fct_admin_create_message', 'utils', 'function', 'json', 'json', 'Function to create sys_message efficiently', 'role_admin', NULL, 'core')
 ON CONFLICT (id) DO NOTHING;
 
@@ -21,7 +21,7 @@ INSERT INTO config_form_tabs VALUES ('selector_basic','tab_municipality','Muni',
 
 INSERT INTO config_param_system VALUES (
 'basic_selector_tab_municipality',
-'{"table":"ext_municipality","selector":"selector_municipality","table_id":"muni_id","selector_id":"muni_id","label":"muni_id, ''- '', name","orderBy":"muni_id","manageAll":true,"selectionMode":"keepPreviousUsingShift","query_filter":"AND muni_id > 0","typeaheadFilter":" AND lower(concat(muni_id, '' - '', name))","typeaheadForced":true,"explFromMuni":true}',
+'{'table':'ext_municipality','selector':'selector_municipality','table_id':'muni_id','selector_id':'muni_id','label':'muni_id, ''- '', name','orderBy':'muni_id','manageAll':true,'selectionMode':'keepPreviousUsingShift','query_filter':'AND muni_id > 0','typeaheadFilter':' AND lower(concat(muni_id, '' - '', name))','typeaheadForced':true,'explFromMuni':true}',
 'Variable to configura all options related to search for the specificic tab','Selector variables',NULL,NULL,FALSE,NULL,'utils',NULL,NULL,'json','text');
 
 UPDATE config_form_fields SET widgettype='combo',
@@ -31,3 +31,10 @@ dv_orderby_id=true WHERE formname='v_edit_dimensions' AND formtype='form_feature
 -- 21/06/2024
 DELETE FROM sys_foreignkey WHERE typevalue_name = 'psector_type' AND target_table='plan_psector';
 DELETE FROM plan_typevalue WHERE typevalue='psector_type' AND id='1';
+
+INSERT INTO edit_typevalue VALUES ('presszone_type', 'BUSTER', 'BUSTER');
+INSERT INTO edit_typevalue VALUES ('presszone_type', 'TANK', 'TANK');
+INSERT INTO edit_typevalue VALUES ('presszone_type', 'PRV', 'PRV');
+INSERT INTO edit_typevalue VALUES ('presszone_type', 'PSV', 'PSV');
+INSERT INTO edit_typevalue VALUES ('presszone_type', 'PUMP', 'PUMP');
+INSERT INTO edit_typevalue VALUES ('presszone_type', 'UNDEFINED', 'UNDEFINED');
