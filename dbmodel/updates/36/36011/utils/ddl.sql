033ba4ac9a6a1471ef446f6b019ddad10b8677b8
xtorret@bgeo.es <xtorret@bgeo.es>
2024-07-05 20:16:46 +0200

    Add utils table ext_province_x_region with double fk to ext_municipality

--- dbmodel/updates/36/36011/utils/ddl.sql
+++ dbmodel/updates/36/36011/utils/ddl.sql
@@ -36,8 +36,63 @@ ALTER TABLE plan_psector ALTER COLUMN psector_type SET DEFAULT 1;
 
 ALTER TABLE cat_feature RENAME COLUMN config TO addparam;
 
+
 SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD", "table":"ext_municipality", "column":"ext_code", "dataType":"varchar(50)", "isUtils":"True"}}$$);
-
 SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD", "table":"ext_region", "column":"ext_code", "dataType":"varchar(50)", "isUtils":"True"}}$$);
-
 SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD", "table":"ext_province", "column":"ext_code", "dataType":"varchar(50)", "isUtils":"True"}}$$);
+
+do $$ 
+declare
+    v_utils boolean; 
+begin
+     SELECT value::boolean INTO v_utils FROM config_param_system WHERE parameter='admin_utils_schema';
+	 
+	 if v_utils is true then
+	 
+		-- create table utils.region_x_province
+	 	CREATE TABLE IF NOT EXISTS utils.region_x_province (
+			region_id int4 NOT NULL,
+			province_id int4 NOT NULL,
+			CONSTRAINT region_x_province_pkey PRIMARY KEY (region_id, province_id));
+		
+		-- insert values on table
+		INSERT INTO region_x_province SELECT region_id, province_id 
+		FROM municipality where region_id is not null and province_id is not null 
+		on conflict (region_id, province_id) do nothing;
+	
+		-- drop province_id from ext_region
+		DROP VIEW SCHEMA_NAME.ext_region AS SELECT * FROM utils.region;
+		CREATE TABLE utils._region_ AS SELECT * FROM utils.region;
+		ALTER TABLE utils.region DROP COLUMN province_id;
+
+		-- refresh views
+		CREATE VIEW SCHEMA_NAME.ext_region AS SELECT * FROM utils.region;
+		CREATE OR REPLACE VIEW SCHEMA_NAME.ext_province AS SELECT * FROM utils.province;
+				
+		-- create fk
+		ALTER TABLE utils.municipality ADD CONSTRAINT municipality_province_region_fk FOREIGN KEY (province_id, region_id)
+        REFERENCES utils.region_x_province (province_id, region_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE RESTRICT;
+	
+     else
+	 
+	 	-- create table region_x_province
+     	CREATE TABLE IF NOT EXISTS SCHEMA_NAME.ext_region_x_province (
+			region_id int4 NOT NULL,
+			province_id int4 NOT NULL,
+			CONSTRAINT ext_region_x_province_pkey PRIMARY KEY (region_id, province_id));
+	
+		-- insert values on table
+		INSERT INTO ext_region_x_province SELECT region_id, province_id 
+		FROM ext_municipality where region_id is not null and province_id is not null 
+		on conflict (region_id, province_id) do nothing;
+	
+		-- drop province_id from ext_region
+		CREATE TABLE SCHEMA_NAME._ext_region_ AS SELECT * FROM SCHEMA_NAME.ext_region;
+		ALTER TABLE SCHEMA_NAME.ext_region DROP COLUMN province_id;
+		
+		-- create fk
+		ALTER TABLE SCHEMA_NAME.ext_municipality ADD CONSTRAINT ext_municipality_province_region_fk FOREIGN KEY (province_id, region_id)
+        REFERENCES SCHEMA_NAME.ext_region_x_province (province_id, region_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE RESTRICT;
+		
+	 end if;
+end; $$;
