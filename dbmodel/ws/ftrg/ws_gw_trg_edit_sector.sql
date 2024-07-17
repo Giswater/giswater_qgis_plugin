75fe34ea9485414233b7b235a69536b48b4d2282
mguzman14 <mguzman@bgeo.es>
2024-07-17 12:42:12 +0200

    fix(trg_edit_sector): remove expl_id2 from trigger

--- dbmodel/ws/ftrg/ws_gw_trg_edit_sector.sql
+++ dbmodel/ws/ftrg/ws_gw_trg_edit_sector.sql
@@ -24,16 +24,16 @@ BEGIN
 
 		NEW.active = TRUE;
 
-		INSERT INTO sector (sector_id, name, descript, macrosector_id, the_geom, undelete, graphconfig, stylesheet, active, parent_id, pattern_id, avg_press, expl_id2)
+		INSERT INTO sector (sector_id, name, descript, macrosector_id, the_geom, undelete, graphconfig, stylesheet, active, parent_id, pattern_id, avg_press)
 		VALUES (NEW.sector_id, NEW.name, NEW.descript, NEW.macrosector_id, NEW.the_geom, NEW.undelete, 
-		NEW.graphconfig::json, NEW.stylesheet::json, NEW.active, NEW.parent_id, NEW.pattern_id, NEW.avg_press, NEW.expl_id2);
+		NEW.graphconfig::json, NEW.stylesheet::json, NEW.active, NEW.parent_id, NEW.pattern_id, NEW.avg_press);
 				
 	ELSIF TG_OP = 'UPDATE' THEN
 
 		UPDATE sector 
 		SET sector_id=NEW.sector_id, name=NEW.name, descript=NEW.descript, macrosector_id=NEW.macrosector_id, the_geom=NEW.the_geom, 
 		undelete=NEW.undelete, graphconfig=NEW.graphconfig::json, stylesheet = NEW.stylesheet::json, active = NEW.active, parent_id = NEW.parent_id, pattern_id = NEW.pattern_id, 
-		lastupdate=now(), lastupdate_user = current_user, avg_press = NEW.avg_press, expl_id2 = NEW.expl_id2
+		lastupdate=now(), lastupdate_user = current_user, avg_press = NEW.avg_press
 		WHERE sector_id=OLD.sector_id;
 				
 	ELSIF TG_OP = 'DELETE' THEN  
