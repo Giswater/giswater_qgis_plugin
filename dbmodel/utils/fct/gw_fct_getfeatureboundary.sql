f33cb0e7fd5a99c32994a6de6ee6d373d36bc8c1
xtorret@bgeo.es <xtorret@bgeo.es>
2024-07-19 09:56:07 +0200

    fix(getfeatureboundary): improve identation and clean code
    (cherry picked from commit 02307fa1e2790c0e4a4b5be592ff8948b8550781)

--- dbmodel/utils/fct/gw_fct_getfeatureboundary.sql
+++ dbmodel/utils/fct/gw_fct_getfeatureboundary.sql
@@ -13,7 +13,6 @@ $BODY$
 /*
 SELECT SCHEMA_NAME.gw_fct_getfeatureboundary($${"client":{"device":4, "infoType":1, "lang":"ES"},"form":{},"feature":{"arc":[2001,2002], "node":[], "connec":[3001, 3002]},"data":{"type":"feature"}}$$)
 SELECT SCHEMA_NAME.gw_fct_getfeatureboundary($${"client":{"device":4, "infoType":1, "lang":"ES"},"form":{},"feature":{"update_tables":["node", "arc", "connec", "link"]},"data":{"type":"time", "lastSeed":"2023-05-05"}}$$)
-SELECT SCHEMA_NAME.gw_fct_getfeatureboundary($${"client":{"device":4, "infoType":1, "lang":"ES"},"form":{},"feature":{},"data":{"type":"area", "polygon":"11223344}}$$) -- not solved
 */  
 
 DECLARE    
@@ -31,63 +30,63 @@ DECLARE
   v_table text;
   v_updatetables text;
 BEGIN
-  -- Set search path to local schema
-  SET search_path = "SCHEMA_NAME", public;
 
-  v_type = ((p_data ->>'data')::json->>'type');
+	-- Set search path to local schema
+	SET search_path = "SCHEMA_NAME", public;
 
-  IF v_type = 'time' then
-  
-   v_lastseed = ((p_data ->>'data')::json->>'lastSeed');
-   v_updatetables = ((p_data ->>'feature')::json->>'update_tables');
-   v_querytext='';
-   FOR v_table IN SELECT json_array_elements_text(v_updatetables::json) loop
-      raise notice 'table: %', v_table;
-      v_querytext = concat(v_querytext, 
-        ' SELECT the_geom FROM v_edit_', v_table, 
-        ' WHERE lastupdate > ''', v_lastseed, '''::timestamp UNION'
-      );
-   END LOOP;
-   v_querytext = left(v_querytext, length(v_querytext) - 6);
-   v_querytext = CONCAT('SELECT ST_AsGeoJSON(COALESCE(ST_Collect(ST_Buffer(the_geom, 2)), ST_GeomFromText(''POINT EMPTY'')))
-   FROM (', v_querytext, ') AS combined_geometries');
-  
-  ELSIF v_type = 'area' THEN
-    -- Add code to handle 'area' type (if needed)
-  
-  ELSIF v_type = 'feature' THEN
-    v_array =  COALESCE(replace(replace(((p_data ->>'feature')::json->>'node'),'[','('),']',')'),'()') ;
-    IF v_array != '()' THEN
-      v_querynode = concat('SELECT the_geom FROM v_edit_node WHERE node_id::integer IN ', v_array);
-    END IF;
+	v_type = ((p_data ->>'data')::json->>'type');
 
-    v_array =  COALESCE(replace(replace(((p_data ->>'feature')::json->>'arc'),'[','('),']',')'),'()') ;
-    IF v_array != '()' THEN
-      v_queryarc = concat('SELECT the_geom FROM v_edit_arc WHERE arc_id::integer IN ', v_array);
-    END IF;
+	IF v_type = 'time' then
 
-    v_array =  COALESCE(replace(replace(((p_data ->>'feature')::json->>'connec'),'[','('),']',')'),'()') ;
-    IF v_array != '()' THEN
-      v_queryconnec = concat('SELECT the_geom FROM v_edit_connec WHERE connec_id::integer IN ', v_array);
-    END IF;
+		v_lastseed = ((p_data ->>'data')::json->>'lastSeed');
+		v_updatetables = ((p_data ->>'feature')::json->>'update_tables');
+		v_querytext='';
+		
+		FOR v_table IN SELECT json_array_elements_text(v_updatetables::json) loop
+			raise notice 'table: %', v_table;
+			v_querytext = concat(v_querytext, 
+			' SELECT the_geom FROM v_edit_', v_table, 
+			' WHERE lastupdate > ''', v_lastseed, '''::timestamp UNION'
+			);
+		END LOOP;
+		
+		v_querytext = left(v_querytext, length(v_querytext) - 6);
+		v_querytext = CONCAT('SELECT ST_AsGeoJSON(COALESCE(ST_Collect(ST_Buffer(the_geom, 2)), ST_GeomFromText(''POINT EMPTY'')))
+		FROM (', v_querytext, ') AS combined_geometries');
 
-    v_array =  COALESCE(replace(replace(((p_data ->>'feature')::json->>'gully'),'[','('),']',')'),'()') ;
-    IF v_array != '()' THEN
-      v_querygully = concat('SELECT the_geom FROM v_edit_gully WHERE gully_id::integer IN ', v_array);
-    END IF;
+	ELSIF v_type = 'feature' THEN
+	v_array =  COALESCE(replace(replace(((p_data ->>'feature')::json->>'node'),'[','('),']',')'),'()') ;
+	IF v_array != '()' THEN
+		v_querynode = concat('SELECT the_geom FROM v_edit_node WHERE node_id::integer IN ', v_array);
+	END IF;
 
-    -- Building the query text
-    v_querytext = concat('SELECT ST_AsGeoJSON(ST_Collect(ST_Buffer(the_geom, 2))) from (', v_querynode, v_queryarc, v_queryconnec, v_querygully, ') a');
-  END IF;
+	v_array =  COALESCE(replace(replace(((p_data ->>'feature')::json->>'arc'),'[','('),']',')'),'()') ;
+	IF v_array != '()' THEN
+		v_queryarc = concat('SELECT the_geom FROM v_edit_arc WHERE arc_id::integer IN ', v_array);
+	END IF;
 
-  -- Execute query text and set boundary geometry
-  EXECUTE v_querytext INTO v_geojson;
+	v_array =  COALESCE(replace(replace(((p_data ->>'feature')::json->>'connec'),'[','('),']',')'),'()') ;
+	IF v_array != '()' THEN
+		v_queryconnec = concat('SELECT the_geom FROM v_edit_connec WHERE connec_id::integer IN ', v_array);
+	END IF;
 
-  RETURN v_geojson;
+	v_array =  COALESCE(replace(replace(((p_data ->>'feature')::json->>'gully'),'[','('),']',')'),'()') ;
+	IF v_array != '()' THEN
+		v_querygully = concat('SELECT the_geom FROM v_edit_gully WHERE gully_id::integer IN ', v_array);
+	END IF;
 
-  -- Exception handling
-  EXCEPTION WHEN OTHERS THEN 
-    RETURN NULL;
+	-- Building the query text
+	v_querytext = concat('SELECT ST_AsGeoJSON(ST_Collect(ST_Buffer(the_geom, 2))) from (', v_querynode, v_queryarc, v_queryconnec, v_querygully, ') a');
+	END IF;
+
+	-- Execute query text and set boundary geometry
+	EXECUTE v_querytext INTO v_geojson;
+
+	RETURN v_geojson;
+
+	-- Exception handling
+	EXCEPTION WHEN OTHERS THEN RETURN NULL;
+	
 END;
 $BODY$
 LANGUAGE plpgsql VOLATILE
