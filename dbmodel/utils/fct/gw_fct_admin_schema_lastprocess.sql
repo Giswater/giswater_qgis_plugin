ecb9db688ab732584abb7bd5d56384297de80ff1
mguzman14 <mguzman@bgeo.es>
2024-07-25 12:02:57 +0200

    fix(last_process): comment exception when others in order to ease update version process

--- dbmodel/utils/fct/gw_fct_admin_schema_lastprocess.sql
+++ dbmodel/utils/fct/gw_fct_admin_schema_lastprocess.sql
@@ -502,9 +502,9 @@ BEGIN
 			'}}}');
 
     -- Exception handling
-    EXCEPTION WHEN OTHERS THEN
-    GET STACKED DIAGNOSTICS v_error_context = PG_EXCEPTION_CONTEXT;
-    RETURN ('{"status":"Failed","NOSQLERR":' || to_json(SQLERRM) || ',"SQLSTATE":' || to_json(SQLSTATE) ||',"SQLCONTEXT":' || to_json(v_error_context) || '}')::json;
+    --EXCEPTION WHEN OTHERS THEN
+    --GET STACKED DIAGNOSTICS v_error_context = PG_EXCEPTION_CONTEXT;
+    --RETURN ('{"status":"Failed","NOSQLERR":' || to_json(SQLERRM) || ',"SQLSTATE":' || to_json(SQLSTATE) ||',"SQLCONTEXT":' || to_json(v_error_context) || '}')::json;
 
 END;
 $BODY$
