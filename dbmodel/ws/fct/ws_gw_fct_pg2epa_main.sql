bfa984f9015f84737667c53648b67693e3a7b769
smaspons <smaspons@bgeo.es>
2024-07-26 15:31:52 +0200

    style(pg2epa_main): remove trailing whitespaces

--- dbmodel/ws/fct/ws_gw_fct_pg2epa_main.sql
+++ dbmodel/ws/fct/ws_gw_fct_pg2epa_main.sql
@@ -9,8 +9,8 @@ This version of Giswater is provided by Giswater Association
 DROP FUNCTION IF EXISTS SCHEMA_NAME.gw_fct_pg2epa(character varying, boolean, boolean);
 DROP FUNCTION IF EXISTS SCHEMA_NAME.gw_fct_pg2epa(character varying, boolean);
 DROP FUNCTION IF EXISTS SCHEMA_NAME.gw_fct_pg2epa(json);
-CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_pg2epa_main(p_data json)  
-RETURNS json AS 
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_pg2epa_main(p_data json)
+RETURNS json AS
 $BODY$
 
 /*EXAMPLE
@@ -19,7 +19,7 @@ SELECT SCHEMA_NAME.gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "la
 SELECT SCHEMA_NAME.gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"3"}}$$); -- CHECK DATA
 SELECT SCHEMA_NAME.gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"4"}}$$); -- STRUCTURE DATA
 SELECT SCHEMA_NAME.gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"5"}}$$); -- CHECK GRAPH
-SELECT SCHEMA_NAME.gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"6"}}$$); -- BUILD INP 
+SELECT SCHEMA_NAME.gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"6"}}$$); -- BUILD INP
 SELECT SCHEMA_NAME.gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"7"}}$$); -- POST-PROCESS
 
 select * from temp_audit_check_data order by 1 asc
@@ -29,7 +29,7 @@ select * from temp_audit_check_data order by 1 asc
 
 */
 
-DECLARE	
+DECLARE
 
 v_networkmode integer = 1;
 v_return json;
@@ -57,20 +57,20 @@ v_step integer=0;
 v_autorepair boolean;
 v_expl integer = null;
 v_sector_0 boolean = false;
-	
+
 BEGIN
 
 	-- set search path
 	SET search_path = "SCHEMA_NAME", public;
-	
+
 	-- get input data
 	v_result = (p_data->>'data')::json->>'resultId';
 	v_step = (p_data->>'data')::json->>'step';  -- use network previously defined
 	v_input = concat('{"data":{"parameters":{"resultId":"',v_result,'", "fid":227}}}')::json;
-		
+
 	-- step 1: preprocess
 	IF v_step = 1 THEN
-	
+
 		-- force sector =0 disabled
 		DELETE FROM selector_sector  WHERE sector_id = 0 and cur_user = current_user;
 
@@ -78,13 +78,13 @@ BEGIN
 		SELECT count(*) INTO v_count FROM selector_sector WHERE cur_user = current_user;
 		IF v_count = 0 THEN
 			RETURN ('{"status":"Failed","message":{"level":1, "text":"There is no sector selected. Please select at least one"}}')::json;
-		END IF;		
-		
+		END IF;
+
 		-- force only state 1 selector
 		DELETE FROM selector_state WHERE cur_user=current_user;
 		INSERT INTO selector_state (state_id, cur_user) VALUES (1, current_user);
-		
-		
+
+
 		-- create temp tables
 		CREATE TEMP TABLE temp_vnode(
 		  id serial NOT NULL,
@@ -93,7 +93,7 @@ BEGIN
 		  l2 integer,
 		  v2 integer,
 		  CONSTRAINT temp_vnode_pkey PRIMARY KEY (id));
-	  
+
 		CREATE TEMP TABLE temp_link(
 		  link_id integer NOT NULL,
 		  vnode_id integer,
@@ -112,7 +112,7 @@ BEGIN
 		  the_geom_endpoint geometry(Point,SRID_VALUE),
 		  flag boolean,
 		  CONSTRAINT temp_link_pkey PRIMARY KEY (link_id));
-		
+
 		CREATE TEMP TABLE temp_link_x_arc(
 		  link_id integer NOT NULL,
 		  vnode_id integer,
@@ -146,10 +146,10 @@ BEGIN
 		CREATE TEMP TABLE temp_t_go2epa (LIKE SCHEMA_NAME.temp_go2epa INCLUDING ALL);
 
 		-- setting selectors
-		v_inpoptions = (SELECT (replace (replace (replace (array_to_json(array_agg(json_build_object((t.parameter),(t.value))))::text,'},{', ' , '),'[',''),']',''))::json 
-			FROM (SELECT parameter, value FROM config_param_user 
+		v_inpoptions = (SELECT (replace (replace (replace (array_to_json(array_agg(json_build_object((t.parameter),(t.value))))::text,'},{', ' , '),'[',''),']',''))::json
+			FROM (SELECT parameter, value FROM config_param_user
 			JOIN sys_param_user a ON a.id=parameter	WHERE cur_user=current_user AND formname='epaoptions')t);
-	
+
 		DELETE FROM rpt_cat_result WHERE result_id=v_result;
 		INSERT INTO rpt_cat_result (result_id, inp_options, status, expl_id) VALUES (v_result, v_inpoptions, 1, ARRAY[v_expl]);
 		DELETE FROM rpt_inp_pattern_value WHERE result_id=v_result;
@@ -169,26 +169,26 @@ BEGIN
 		RETURN v_return;
 
 	-- step 2: autorepair epa-type
-	ELSIF v_step = 2 THEN 
+	ELSIF v_step = 2 THEN
 
 		PERFORM gw_fct_pg2epa_autorepair_epatype($${"client":{"device":4, "infoType":1, "lang":"ES"}}$$);
 		v_return = '{"status": "Accepted", "message":{"level":1, "text":"Export INP file 2/7 - Autorepair epa_type...... done succesfully"}}'::json;
 		RETURN v_return;
 
 	-- step 3: check data
-	ELSIF v_step = 3 THEN 
+	ELSIF v_step = 3 THEN
 
 		PERFORM gw_fct_pg2epa_check_data(v_input);
 		v_return = '{"status": "Accepted", "message":{"level":1, "text":"Export INP file 3/7 - Check according EPA rules...... done succesfully"}}'::json;
 		RETURN v_return;
-	
+
 	-- step 4: structure data
-	ELSIF v_step = 4 THEN 
-	
+	ELSIF v_step = 4 THEN
+
 		-- get user parameteres
 		v_networkmode = (SELECT value FROM config_param_user WHERE parameter='inp_options_networkmode' AND cur_user=current_user);
 		IF v_networkmode = 1 THEN v_onlymandatory_nodarc = TRUE; END IF;
-		
+
 		v_buildupmode = (SELECT value FROM config_param_user WHERE parameter='inp_options_buildup_mode' AND cur_user=current_user);
 		v_advancedsettings = (SELECT value::json->>'status' FROM config_param_user WHERE parameter='inp_options_advancedsettings' AND cur_user=current_user)::boolean;
 		v_vdefault = (SELECT value::json->>'status' FROM config_param_user WHERE parameter='inp_options_vdefault' AND cur_user=current_user);
@@ -199,7 +199,7 @@ BEGIN
 		-- get debug parameters
 		v_setdemand = (SELECT value::json->>'setDemand' FROM config_param_user WHERE parameter='inp_options_debug' AND cur_user=current_user)::boolean;
 		v_breakpipes = (SELECT (value::json->>'breakPipes')::json->>'status' FROM config_param_user WHERE parameter='inp_options_debug' AND cur_user=current_user)::boolean;
-		
+
 		RAISE NOTICE '4.1 - Fill temp tables';
 		PERFORM gw_fct_pg2epa_fill_data(v_result);
 
@@ -208,16 +208,16 @@ BEGIN
 
 		RAISE NOTICE '4.3 - Call gw_fct_pg2epa_doublenod2arc';
 		PERFORM gw_fct_pg2epa_nod2arc_double(v_result);
-				
+
 		RAISE NOTICE '4.4 - Call gw_fct_pg2epa_pump_additional function';
 		PERFORM gw_fct_pg2epa_pump_additional(v_result);
 
 		RAISE NOTICE '4.5 - manage varcs';
-		PERFORM gw_fct_pg2epa_manage_varc(v_result);	
+		PERFORM gw_fct_pg2epa_manage_varc(v_result);
 
 		RAISE NOTICE '4.6 - Trim arcs';
 		IF v_networkmode IN (3,4) THEN
-		
+
 			IF v_breakpipes THEN
 				PERFORM gw_fct_pg2epa_breakpipes(v_result);
 			END IF;
@@ -228,7 +228,7 @@ BEGIN
 		RAISE NOTICE '4.7 - Execute buildup model';
 		IF v_buildupmode = 1 THEN
 			PERFORM gw_fct_pg2epa_buildup_supply(v_result);
-			
+
 		ELSIF v_buildupmode = 2 THEN
 			PERFORM gw_fct_pg2epa_buildup_transport(v_result);
 		END IF;
@@ -241,13 +241,13 @@ BEGIN
 		RAISE NOTICE '4.9 - Set ceros';
 		UPDATE temp_t_node SET elevation = 0 WHERE elevation IS NULL;
 		UPDATE temp_t_node SET addparam = replace (addparam, '""','null');
-		
+
 		RAISE NOTICE '4.10 - Set length > 0.05 when length is 0';
 		UPDATE temp_t_arc SET length=0.05 WHERE length=0;
 
 		RAISE NOTICE '4.11 - Set demands & patterns';
 		IF v_setdemand THEN
-			PERFORM gw_fct_pg2epa_demand(v_result);		
+			PERFORM gw_fct_pg2epa_demand(v_result);
 		END IF;
 
 		RAISE NOTICE '4.12 - Setting valve status';
@@ -279,32 +279,32 @@ BEGIN
 
 		-- for those elements like filter o flowmeter which they do not have the attribute on the inventory table
 		UPDATE temp_t_arc SET status = 'OPEN' WHERE status IS NULL OR status = '';
-		
+
 		UPDATE temp_t_node SET dqa_id = 0 WHERE dqa_id IS NULL;
 		UPDATE temp_t_arc SET dqa_id = 0 WHERE dqa_id IS NULL;
-		
+
 		UPDATE temp_t_node SET dma_id = 0 WHERE dma_id IS NULL;
 		UPDATE temp_t_arc SET dma_id = 0 WHERE dma_id IS NULL;
-		
+
 		UPDATE temp_t_node SET presszone_id = 0 WHERE presszone_id IS NULL;
 		UPDATE temp_t_arc SET presszone_id = 0 WHERE presszone_id IS NULL;
 
-		-- remove pattern when breakPipes is enabled	
+		-- remove pattern when breakPipes is enabled
 		IF v_breakpipes THEN
-			UPDATE temp_t_node n SET pattern_id  = null , demand = 0 FROM temp_table t WHERE n.node_id = concat('VN',t.id);				
-		END IF;		
+			UPDATE temp_t_node n SET pattern_id  = null , demand = 0 FROM temp_table t WHERE n.node_id = concat('VN',t.id);
+		END IF;
 
 		RAISE NOTICE '4.14 - Update values for temp table';
 		UPDATE temp_t_arc SET result_id  = v_result;
 		UPDATE temp_t_node SET result_id  = v_result;
-		
+
 		-- move patterns used
-		INSERT INTO temp_rpt_inp_pattern_value (result_id, pattern_id, factor_1, factor_2, factor_3, factor_4, factor_5, factor_6, factor_7, factor_8, 
+		INSERT INTO temp_rpt_inp_pattern_value (result_id, pattern_id, factor_1, factor_2, factor_3, factor_4, factor_5, factor_6, factor_7, factor_8,
 			factor_9, factor_10, factor_11, factor_12, factor_13, factor_14, factor_15, factor_16, factor_17, factor_18)
-		SELECT  v_result, pattern_id, factor_1, factor_2, factor_3, factor_4, factor_5, factor_6, factor_7, factor_8, 
-			factor_9, factor_10, factor_11, factor_12, factor_13, factor_14, factor_15, factor_16, factor_17, factor_18 
-			from inp_pattern_value p 
-			WHERE 
+		SELECT  v_result, pattern_id, factor_1, factor_2, factor_3, factor_4, factor_5, factor_6, factor_7, factor_8,
+			factor_9, factor_10, factor_11, factor_12, factor_13, factor_14, factor_15, factor_16, factor_17, factor_18
+			from inp_pattern_value p
+			WHERE
 			pattern_id IN (SELECT distinct (pattern_id) FROM inp_dscenario_demand d, selector_inp_dscenario s WHERE cur_user = current_user AND d.dscenario_id = s.dscenario_id
 						   AND pattern_id IS NOT NULL UNION SELECT distinct (pattern_id) FROM temp_t_node WHERE pattern_id IS NOT NULL)
 			order by pattern_id, id;
@@ -313,7 +313,7 @@ BEGIN
 
 		v_return = '{"status": "Accepted", "message":{"level":1, "text":"Export INP file 4/7 - Structure data...... done succesfully"}}'::json;
 		RETURN v_return;
-		
+
 	-- step 5: analyze graph
 	ELSIF v_step=5 THEN
 
@@ -330,47 +330,47 @@ BEGIN
 		UPDATE temp_t_arc t SET epa_type = 'TODELETE' FROM (SELECT a.id FROM temp_t_arc a LEFT JOIN temp_t_node ON node_1=node_id WHERE temp_t_node.node_id is null) a WHERE t.id = a.id;
 		UPDATE temp_t_arc t SET epa_type = 'TODELETE' FROM (SELECT a.id FROM temp_t_arc a LEFT JOIN temp_t_node ON node_2=node_id WHERE temp_t_node.node_id is null) a WHERE t.id = a.id;
 
-		INSERT INTO temp_audit_log_data (fid, feature_id, feature_type, log_message) 
+		INSERT INTO temp_audit_log_data (fid, feature_id, feature_type, log_message)
 		SELECT v_fid, arc_id, 'ARC', '23 - Profilactic last delete' FROM temp_t_arc WHERE epa_type  ='TODELETE';
-		
+
 		DELETE FROM temp_t_arc WHERE epa_type = 'TODELETE';
 
 		UPDATE temp_t_arc SET result_id = v_result WHERE result_id IS NULL;
 		UPDATE temp_t_node SET result_id = v_result WHERE result_id IS NULL;
 
 		-- deleting nodes without arcs
-		UPDATE temp_t_node t SET epa_type = 'TODELETE' FROM 
-		(SELECT id FROM temp_t_node LEFT JOIN (SELECT node_1 as node_id FROM temp_t_arc UNION SELECT node_2 FROM temp_t_arc) a USING (node_id) WHERE a.node_id IS NULL) a 
+		UPDATE temp_t_node t SET epa_type = 'TODELETE' FROM
+		(SELECT id FROM temp_t_node LEFT JOIN (SELECT node_1 as node_id FROM temp_t_arc UNION SELECT node_2 FROM temp_t_arc) a USING (node_id) WHERE a.node_id IS NULL) a
 		WHERE t.id = a.id;
 
-		INSERT INTO temp_audit_log_data (fid, feature_id, feature_type, log_message) 
+		INSERT INTO temp_audit_log_data (fid, feature_id, feature_type, log_message)
 		SELECT v_fid, node_id, 'NODE', '23 - Profilactic last delete' FROM temp_t_node WHERE epa_type  ='TODELETE';
-			
+
 		DELETE FROM temp_t_node WHERE epa_type = 'TODELETE';
-		
+
 		SELECT gw_fct_pg2epa_export_inp(p_data) INTO v_file;
 		v_body = gw_fct_json_object_set_key((v_return->>'body')::json, 'file', v_file);
 		v_return = gw_fct_json_object_set_key(v_return, 'body', v_body);
-		v_return = replace(v_return::text, '"message":{"level":1, "text":"Data quality analysis done succesfully"}', 
+		v_return = replace(v_return::text, '"message":{"level":1, "text":"Data quality analysis done succesfully"}',
 		'"message":{"level":1, "text":"Export INP file 6/7 - Writing the INP file...... done succesfully"}')::json;
 
-		RETURN v_return;	
+		RETURN v_return;
 
 	-- step 7: post-proces
 	ELSIF v_step=7 THEN
-	
+
 		-- move nodes data
-		INSERT INTO rpt_inp_node (result_id, node_id, elevation, elev, node_type, nodecat_id, epa_type, sector_id, state, state_type, annotation, demand, 
+		INSERT INTO rpt_inp_node (result_id, node_id, elevation, elev, node_type, nodecat_id, epa_type, sector_id, state, state_type, annotation, demand,
 		the_geom, expl_id, pattern_id, addparam, nodeparent, arcposition,dma_id, presszone_id, dqa_id, minsector_id)
-		SELECT result_id, node_id, elevation, case when elev is null then elevation else elev end, node_type, nodecat_id, epa_type, sector_id, state, 
+		SELECT result_id, node_id, elevation, case when elev is null then elevation else elev end, node_type, nodecat_id, epa_type, sector_id, state,
 		state_type, annotation, demand, the_geom, expl_id, pattern_id, addparam, nodeparent, arcposition,dma_id, presszone_id, dqa_id, minsector_id
 		FROM temp_t_node;
-		
+
 		-- move arcs data
-		INSERT INTO rpt_inp_arc 
-		(result_id, arc_id, node_1, node_2, arc_type, arccat_id, epa_type, sector_id, state, state_type, annotation, diameter, roughness, length, status, 
+		INSERT INTO rpt_inp_arc
+		(result_id, arc_id, node_1, node_2, arc_type, arccat_id, epa_type, sector_id, state, state_type, annotation, diameter, roughness, length, status,
 		the_geom, expl_id, flw_code, minorloss, addparam, arcparent,dma_id, presszone_id, dqa_id, minsector_id)
-		SELECT result_id, arc_id, node_1, node_2, arc_type, arccat_id, epa_type, sector_id, state, state_type, annotation, diameter, roughness, length, 
+		SELECT result_id, arc_id, node_1, node_2, arc_type, arccat_id, epa_type, sector_id, state, state_type, annotation, diameter, roughness, length,
 		status, the_geom, expl_id, flw_code, minorloss, addparam, arcparent,dma_id, presszone_id, dqa_id, minsector_id
 		FROM temp_t_arc;
 
@@ -382,14 +382,14 @@ BEGIN
 		DELETE FROM anl_node WHERE cur_user = current_user AND fid IN (107,153,164,165,166,167,169,170,171,188,198,227,229,230,292,294,295,371,379,433,411,412,430,432,480,	-- CHECK DATA
 									       172,375,377,413,414,415,400,297,373,396,402,407,								-- CHECK RESULT
 									       228,454,290,404,231,139,232,233,431);									-- CHECK NETWORK
-		
+
 		DELETE FROM anl_connec WHERE cur_user = current_user AND fid IN (107,153,164,165,166,167,169,170,171,188,198,227,229,230,292,294,295,371,379,433,411,412,430,432,480,	-- CHECK DATA
 									       172,375,377,413,414,415,400,297,373,396,402,407,								-- CHECK RESULT
 									       228,454,290,404,231,139,232,233,431);									-- CHECK NETWORK
 
 
 		-- move patterns data
-		INSERT INTO rpt_inp_pattern_value SELECT * FROM temp_rpt_inp_pattern_value;	
+		INSERT INTO rpt_inp_pattern_value SELECT * FROM temp_rpt_inp_pattern_value;
 
 		-- restore hydrometer selector
 		DELETE FROM selector_hydrometer WHERE cur_user = current_user;
@@ -401,7 +401,7 @@ BEGIN
 		DROP TABLE IF EXISTS temp_vnode;
 		DROP TABLE IF EXISTS temp_link;
 		DROP TABLE IF EXISTS temp_link_x_arc;
-		
+
 		DROP TABLE IF EXISTS temp_t_csv;
 		DROP TABLE IF EXISTS temp_audit_check_data;
 		DROP TABLE IF EXISTS temp_audit_log_data;
@@ -410,11 +410,11 @@ BEGIN
 		DROP TABLE IF EXISTS temp_t_arc;
 		DROP TABLE IF EXISTS temp_t_demand;
 		DROP TABLE IF EXISTS temp_t_anlgraph;
-		
+
 		DROP TABLE IF EXISTS temp_anl_arc;
 		DROP TABLE IF EXISTS temp_anl_node;
 		DROP TABLE IF EXISTS temp_anl_connec;
-		
+
 		DROP TABLE IF EXISTS temp_rpt_inp_pattern_value;
 		DROP TABLE IF EXISTS temp_t_go2epa;
 
