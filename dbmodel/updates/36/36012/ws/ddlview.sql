8865763b50027cb39b8514c2e1951c5ed1752046
edgarfuste <34737125+edgarfuste@users.noreply.github.com>
2024-08-07 13:24:19 +0200

    refactor(ddlview): move from utils to ws

--- dbmodel/updates/36/36012/ws/ddlview.sql
+++ dbmodel/updates/36/36012/ws/ddlview.sql
@@ -28,3 +28,90 @@ AS SELECT DISTINCT ON (rpt_cat_result.result_id) rpt_cat_result.result_id,
      LEFT JOIN inp_typevalue t1 ON rpt_cat_result.status::text = t1.id::text
      LEFT JOIN inp_typevalue t2 ON rpt_cat_result.network_type::text = t2.id::text
   WHERE t1.typevalue::text = 'inp_result_status'::text AND t2.typevalue::text = 'inp_options_networkmode'::text AND ((s.expl_id = ANY (rpt_cat_result.expl_id)) AND s.cur_user = CURRENT_USER OR rpt_cat_result.expl_id = ARRAY[NULL]::INTEGER[]);
+
+
+-- 07/08/2024
+CREATE OR REPLACE VIEW vu_sector
+AS SELECT s.sector_id,
+    s.name,
+    ms.name AS macrosector,
+    s.descript,
+    s.undelete,
+    s.sector_type,
+    s.active,
+    s.parent_id,
+    s.pattern_id,
+    s.tstamp,
+    s.insert_user,
+    s.lastupdate,
+    s.lastupdate_user,
+    s.graphconfig,
+    s.stylesheet
+   FROM sector s
+     LEFT JOIN macrosector ms ON ms.macrosector_id = s.macrosector_id
+  ORDER BY s.sector_id;
+
+CREATE OR REPLACE VIEW vu_dma
+AS SELECT d.dma_id,
+    d.name,
+    d.descript,
+    d.expl_id,
+    md.name AS macrodma,
+    d.active,
+    d.undelete,
+    d.minc,
+    d.maxc,
+    d.effc,
+    d.avg_press,
+    d.pattern_id,
+    d.link,
+    d.graphconfig,
+    d.stylesheet,
+    d.tstamp,
+    d.insert_user,
+    d.lastupdate,
+    d.lastupdate_user
+   FROM dma d
+     LEFT JOIN macrodma md ON md.macrodma_id = d.macrodma_id
+  ORDER BY d.dma_id;
+
+CREATE OR REPLACE VIEW vu_presszone
+AS SELECT p.presszone_id,
+    p.name,
+    p.descript,
+    p.expl_id,
+    p.link,
+    p.head,
+    p.active,
+    p.graphconfig,
+    p.stylesheet,
+    p.tstamp,
+    p.insert_user,
+    p.lastupdate,
+    p.lastupdate_user
+   FROM presszone p
+  ORDER BY p.presszone_id;
+
+
+CREATE OR REPLACE VIEW vu_dqa
+AS SELECT d.dqa_id,
+    d.name,
+    d.descript,
+    d.expl_id,
+    md.name AS macrodma,
+    d.active,
+    d.undelete,
+    d.the_geom,
+    d.pattern_id,
+    d.dqa_type,
+    d.link,
+    d.graphconfig,
+    d.stylesheet,
+    d.tstamp,
+    d.insert_user,
+    d.lastupdate,
+    d.lastupdate_user
+   FROM dqa d
+     LEFT JOIN macrodqa md ON md.macrodqa_id = d.macrodqa_id
+  ORDER BY d.dqa_id;
+
