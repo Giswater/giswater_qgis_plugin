cdb2a434af6c126f69ff9088a1232da2e76cf226
Daniel Marin <dmarin@bgeo.es>
2024-08-13 14:41:05 +0200

    refactor(worflows): improve pg_prove call

--- dbmodel/test/plsql/ws/test_gw_fct_pg2epa_main.sql
+++ dbmodel/test/plsql/ws/test_gw_fct_pg2epa_main.sql
@@ -3,20 +3,20 @@ This file is part of Giswater 3
 The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This version of Giswater is provided by Giswater Association
 */
-BEGIN;
+-- BEGIN;
 
-SET search_path = "SCHEMA_NAME", public, pg_catalog;
+-- SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
--- gw_fct_pg2epa_main
--- SELECT gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"1"}}$$);
--- SELECT gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"2"}}$$);
--- SELECT gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"3"}}$$);
--- SELECT gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"4"}}$$);
--- SELECT gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"5"}}$$);
--- SELECT gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"6"}}$$);
--- SELECT gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"7"}}$$);
+-- -- gw_fct_pg2epa_main
+-- -- SELECT gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"1"}}$$);
+-- -- SELECT gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"2"}}$$);
+-- -- SELECT gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"3"}}$$);
+-- -- SELECT gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"4"}}$$);
+-- -- SELECT gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"5"}}$$);
+-- -- SELECT gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"6"}}$$);
+-- -- SELECT gw_fct_pg2epa_main($${"client":{"device":4, "infoType":1, "lang":"ES", "epsg":25831}, "data":{"resultId":"test1", "step":"7"}}$$);
 
--- Finish the test
-SELECT finish();
+-- -- Finish the test
+-- SELECT finish();
 
-ROLLBACK;
+-- ROLLBACK;
--- dbmodel/test/plsql/ws/test_gw_fct_setlinktonetwork.sql
+++ dbmodel/test/plsql/ws/test_gw_fct_setlinktonetwork.sql
@@ -3,21 +3,21 @@ This file is part of Giswater 3
 The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This version of Giswater is provided by Giswater Association
 */
-BEGIN;
+-- BEGIN;
 
-SET search_path = "SCHEMA_NAME", public, pg_catalog;
+-- SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
--- gw_fct_setlinktonetwork
--- SELECT gw_fct_setlinktonetwork($${"client":{"device":4, "infoType":1, "lang":"ES"},
--- "feature":{"id":["3201","3200"]},"data":{"feature_type":"CONNEC", "forcedArcs":["2001","2002"]}}$$);
+-- -- gw_fct_setlinktonetwork
+-- -- SELECT gw_fct_setlinktonetwork($${"client":{"device":4, "infoType":1, "lang":"ES"},
+-- -- "feature":{"id":["3201","3200"]},"data":{"feature_type":"CONNEC", "forcedArcs":["2001","2002"]}}$$);
 
--- SELECT gw_fct_setlinktonetwork($${"client":{"device":4, "infoType":1, "lang":"ES"},
--- "feature":{"id":["100013"]},"data":{"feature_type":"CONNEC"}}$$);
+-- -- SELECT gw_fct_setlinktonetwork($${"client":{"device":4, "infoType":1, "lang":"ES"},
+-- -- "feature":{"id":["100013"]},"data":{"feature_type":"CONNEC"}}$$);
 
--- SELECT gw_fct_setlinktonetwork($${"client":{"device":4, "infoType":1, "lang":"ES"},
--- "feature":{"id":["100014"]},"data":{"feature_type":"GULLY"}}$$);
+-- -- SELECT gw_fct_setlinktonetwork($${"client":{"device":4, "infoType":1, "lang":"ES"},
+-- -- "feature":{"id":["100014"]},"data":{"feature_type":"GULLY"}}$$);
 
--- Finish the test
-SELECT finish();
+-- -- Finish the test
+-- SELECT finish();
 
-ROLLBACK;
+-- ROLLBACK;
