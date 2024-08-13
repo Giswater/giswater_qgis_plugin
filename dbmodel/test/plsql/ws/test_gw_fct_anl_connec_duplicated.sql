461d5f8b4b79b56ed81766023706c9896009da57
Daniel Marin <dmarin@bgeo.es>
2024-08-13 14:36:36 +0200

    tests: fix gw_fct_anl_connec_duplicated call

--- dbmodel/test/plsql/ws/test_gw_fct_anl_connec_duplicated.sql
+++ dbmodel/test/plsql/ws/test_gw_fct_anl_connec_duplicated.sql
@@ -13,14 +13,14 @@ SELECT plan(2);
 -- Extract and test the "status" field from the function's JSON response
 SELECT is (
     (gw_fct_anl_connec_duplicated($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{"tableName":"v_edit_connec"},
-    "data":{"selectionMode":"wholeSelection", "parameters":{"connecTolerance":10}, "parameters":{}}}$$)::JSON)->>'status',
+    "data":{"selectionMode":"wholeSelection", "parameters":{"connecTolerance":10}}}$$)::JSON)->>'status',
     'Accepted',
     'Check if gw_fct_anl_connec_duplicated -> wholeSelection returns status "Accepted"'
 );
 
 SELECT is (
     (gw_fct_anl_connec_duplicated($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{"tableName":"v_edit_connec",
-    "id":[3101, 3102]}, "data":{"selectionMode":"previousSelection", "parameters":{"connecTolerance":10}, "parameters":{}}}$$)::JSON)->>'status',
+    "id":[3101, 3102]}, "data":{"selectionMode":"previousSelection", "parameters":{"connecTolerance":10}}}$$)::JSON)->>'status',
     'Accepted',
     'Check if gw_fct_anl_connec_duplicated -> previousSelection returns status "Accepted"'
 );
