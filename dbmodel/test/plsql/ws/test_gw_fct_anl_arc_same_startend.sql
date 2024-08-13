e6a5cddfa201b458d1a30a3914ef3e9d538ef119
Daniel Marin <dmarin@bgeo.es>
2024-08-13 13:21:19 +0200

    tests(ws): remove toolbar files, and create each function test file

---
+++ dbmodel/test/plsql/ws/test_gw_fct_anl_arc_same_startend.sql
@@ -0,0 +1,32 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+BEGIN;
+
+SET search_path = "SCHEMA_NAME", public, pg_catalog;
+
+-- Plan for 2 test
+SELECT plan(2);
+
+-- Extract and test the "status" field from the function's JSON response
+SELECT is (
+    (gw_fct_anl_arc_same_startend($${"client":{"device":4, "infoType":1, "lang":"ES"},
+    "feature":{"tableName":"v_edit_arc", "id":[2001,2002]}, "data":{"selectionMode":"previousSelection","parameters":{}}}$$)::JSON)->>'status',
+    'Accepted',
+    'Check if gw_fct_anl_arc_same_startend -> previousSelection returns status "Accepted"'
+);
+
+SELECT is (
+    (gw_fct_anl_arc_same_startend($${"client":{"device":4, "infoType":1, "lang":"ES"},
+    "feature":{"tableName":"v_edit_arc"}, "data":{"selectionMode":"wholeSelection","parameters":{}}}$$)::JSON)->>'status',
+    'Accepted',
+    'Check if gw_fct_anl_arc_same_startend -> wholeSelection returns status "Accepted"'
+);
+
+
+-- Finish the test
+SELECT finish();
+
+ROLLBACK;
---
+++ dbmodel/test/plsql/ws/test_gw_fct_setlinktonetwork.sql
@@ -0,0 +1,23 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+BEGIN;
+
+SET search_path = "SCHEMA_NAME", public, pg_catalog;
+
+-- gw_fct_setlinktonetwork
+-- SELECT gw_fct_setlinktonetwork($${"client":{"device":4, "infoType":1, "lang":"ES"},
+-- "feature":{"id":["3201","3200"]},"data":{"feature_type":"CONNEC", "forcedArcs":["2001","2002"]}}$$);
+
+-- SELECT gw_fct_setlinktonetwork($${"client":{"device":4, "infoType":1, "lang":"ES"},
+-- "feature":{"id":["100013"]},"data":{"feature_type":"CONNEC"}}$$);
+
+-- SELECT gw_fct_setlinktonetwork($${"client":{"device":4, "infoType":1, "lang":"ES"},
+-- "feature":{"id":["100014"]},"data":{"feature_type":"GULLY"}}$$);
+
+-- Finish the test
+SELECT finish();
+
+ROLLBACK;
