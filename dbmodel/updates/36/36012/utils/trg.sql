b87deb2abc1150959f75058be3861e03d5820c5c
Albert Bofill <abofill@bgeo.es>
2024-08-20 12:42:35 +0200

    enhancement: prophylactic drop trigger before create anew

--- dbmodel/updates/36/36012/utils/trg.sql
+++ dbmodel/updates/36/36012/utils/trg.sql
@@ -8,14 +8,18 @@ This version of Giswater is provided by Giswater Association
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 --05/08/2024
+DROP TRIGGER IF EXISTS gw_trg_ui_doc_x_visit ON v_ui_doc_x_visit;
 CREATE trigger gw_trg_ui_doc_x_visit instead OF
 INSERT OR DELETE OR UPDATE ON v_ui_doc_x_visit FOR each row EXECUTE function gw_trg_ui_doc('doc_x_visit');
 
+DROP TRIGGER IF EXISTS gw_trg_ui_doc_x_arc ON v_ui_doc_x_arc;
 CREATE trigger gw_trg_ui_doc_x_arc instead OF
 INSERT OR DELETE OR UPDATE ON v_ui_doc_x_arc FOR each row EXECUTE function gw_trg_ui_doc('doc_x_arc');
 
+DROP TRIGGER IF EXISTS gw_trg_ui_doc_x_connec ON v_ui_doc_x_connec;
 CREATE trigger gw_trg_ui_doc_x_connec instead OF
 INSERT OR DELETE OR UPDATE ON v_ui_doc_x_connec FOR each row EXECUTE function gw_trg_ui_doc('doc_x_connec');
 
+DROP TRIGGER IF EXISTS gw_trg_ui_doc_x_node ON v_ui_doc_x_node;
 CREATE trigger gw_trg_ui_doc_x_node instead OF
 INSERT OR DELETE OR UPDATE ON v_ui_doc_x_node FOR each row EXECUTE function gw_trg_ui_doc('doc_x_node');
