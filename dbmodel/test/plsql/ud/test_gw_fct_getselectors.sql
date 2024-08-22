63a2f0b3225d571565b88dd2f19f723b43200faf
Nico Pérez <nperez@bgeo.es>
2024-08-22 10:34:43 +0200

    tests(plsql): add new test functions in ud

--- dbmodel/test/plsql/ud/test_gw_fct_getselectors.sql
+++ dbmodel/test/plsql/ud/test_gw_fct_getselectors.sql
@@ -10,8 +10,8 @@ SET client_min_messages TO WARNING;
 
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
--- Plan for 3 test
-SELECT plan(3);
+-- Plan for 4 test
+SELECT plan(4);
 
 -- Extract and test the "status" field from the function's JSON response
 SELECT is (
@@ -35,6 +35,13 @@ SELECT is (
     'Check if gw_fct_getselectors --> "currentTab":"None" returns status "Accepted"'
 );
 
+SELECT is (
+    (gw_fct_getselectors($${"client":{"device":4, "lang":"es_ES", "infoType":1, "epsg":25831}, "form":{}, "feature":{},
+    "data":{"filterFields":{}, "pageInfo":{}, "selectorType":"selector_basic", "tabName":"tab_psector", "id":1, "isAlone":"False",
+    "disableParent":"False", "value":"True"}}$$)::JSON)->>'status',
+    'Accepted',
+    'Check if gw_fct_getselectors --> "tabName":"tab_psector" returns status "Accepted"'
+);
 
 -- Finish the test
 SELECT finish();
