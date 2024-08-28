5715e63498917d9594651dccb2687a0040d100cf
nullptr <eliesbertran@gmail.com>
2024-08-28 10:21:48 +0200

    feat(mincut web): added apply button to mincut

--- dbmodel/updates/36/36012/ws/dml.sql
+++ dbmodel/updates/36/36012/ws/dml.sql
@@ -353,3 +353,9 @@ INSERT INTO sys_table (id, descript, sys_role, "source") VALUES('v_minsector_gra
 INSERT INTO sys_table (id, descript, sys_role, "source") VALUES('v_ui_sector', NULL, 'role_basic', 'core') ON CONFLICT (id) DO NOTHING ;
 INSERT INTO sys_table (id, descript, sys_role, "source") VALUES('v_ui_dma', NULL, 'role_basic', 'core') ON CONFLICT (id) DO NOTHING ;
 INSERT INTO sys_table (id, descript, sys_role, "source") VALUES('v_ui_presszone', NULL, 'role_basic', 'core') ON CONFLICT (id) DO NOTHING ;
+
+
+-- 28/08/2024
+INSERT INTO config_form_fields (formname, formtype, tabname, columnname, layoutname, widgettype, ismandatory, isparent, iseditable, isautoupdate, isfilter, widgetcontrols, widgetfunction, hidden, web_layoutorder)
+	VALUES ('mincut', 'form_mincut', 'tab_mincut', 'btn_apply', 'lyt_bot_1', 'button', false, false, true, false, false, '{"text":"Apply"}'::json, '{"functionName":"apply","params": {}}'::json, false, 1);
+UPDATE config_form_fields SET web_layoutorder = 2 WHERE formname='mincut' AND formtype='form_mincut' AND tabname='tab_mincut' AND columnname = 'btn_cancel';
