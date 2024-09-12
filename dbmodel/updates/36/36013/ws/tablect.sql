4dabddcf2fd4be38b936d89015959fdb98ef32fe
mguzman14 <mguzman@bgeo.es>
2024-09-12 16:36:46 +0200

    fix(om_mincut): move commit to ws

--- dbmodel/updates/36/36013/ws/tablect.sql
+++ dbmodel/updates/36/36013/ws/tablect.sql
@@ -4,4 +4,6 @@ The program is free software: you can redistribute it and/or modify it under the
 This version of Giswater is provided by Giswater Association
 */
 
-alter table doc_x_workcat add constraint unique_doc_id_workcat_id unique (doc_id, workcat_id);
+ALTER TABLE om_mincut ADD CONSTRAINT om_mincut_expl_id_fkey FOREIGN KEY (expl_id) REFERENCES exploitation(expl_id) ON UPDATE CASCADE ON DELETE RESTRICT;
+ALTER TABLE om_mincut ADD CONSTRAINT om_mincut_muni_id_fkey FOREIGN KEY (muni_id) REFERENCES ext_municipality(muni_id) ON UPDATE CASCADE ON DELETE RESTRICT;
+
