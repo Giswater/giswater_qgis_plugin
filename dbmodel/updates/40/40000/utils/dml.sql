60b4870030873844e918324829b961853db3645d
Daniel Marin <dmarin@bgeo.es>
2024-09-20 11:45:42 +0200

    feat(macrominsector): add new function to calculate macrominsectors

--- dbmodel/updates/40/40000/utils/dml.sql
+++ dbmodel/updates/40/40000/utils/dml.sql
@@ -20,6 +20,10 @@ INSERT INTO sys_function (id, function_name, project_type, function_type, input_
 VALUES (3332, 'gw_fct_graphanalytics_settempgeom', 'utils', 'function', NULL, 'json', 'Function to update the geometry of the mapzones in the temp_minsector table for graphanalytics', 'role_basic', NULL, 'core')
 ON CONFLICT (id) DO NOTHING;
 
+INSERT INTO sys_function (id, function_name, project_type, function_type, input_params, return_type, descript, sys_role, sample_query, "source")
+VALUES (3334, 'gw_fct_graphanalytics_macrominsector', 'utils', 'function', NULL, 'json', 'Function to create macrominsectors', 'role_basic', NULL, 'core')
+ON CONFLICT (id) DO NOTHING;
+
 -- update graphanalytic_minsector description to include explotation id description
 UPDATE sys_function SET descript='Dynamic analisys to sectorize network using the flow traceability function and establish Minimum Sectors. 
 Before start you need to configure:
@@ -28,3 +32,15 @@ Before start you need to configure:
 
 In explotation id you can use ''-9'' to select all explotations, or a list of explotations separated by comma.'
 WHERE id=2706;
+
+ALTER TABLE node DROP CONSTRAINT arc_macrominsector_id_fkey; -- arc_ prefix because wrong name in before version
+ALTER TABLE arc DROP CONSTRAINT arc_macrominsector_id_fkey;
+ALTER TABLE connec DROP CONSTRAINT connec_macrominsector_id_fkey;
+ALTER TABLE link DROP CONSTRAINT link_macrominsector_id_fkey;
+
+DROP TABLE IF EXISTS macrominsector;
+
+INSERT INTO config_toolbox (id, alias, functionparams, inputparams, observ, active, device)
+VALUES(3334, 'Macrominsector analysis', '{"featureType":[]}'::json,
+'[{"widgetname":"commitChanges", "label":"Commit changes:","widgettype":"check","datatype":"boolean","layoutname":"grl_option_parameters","layoutorder":1, "value":"", "tooltip": "Commit changes"}]'::json,
+NULL, true, '{4}');
---
+++ dbmodel/utils/fct/gw_fct_graphanalytics_macrominsector.sql
@@ -0,0 +1,76 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+
+The code of this inundation function have been provided by Claudia Dragoste (Aigues de Manresa, S.A.)
+*/
+
+-- FUNCTION CODE: 3334
+
+DROP FUNCTION IF EXISTS SCHEMA_NAME.gw_fct_grafanalytics_macrominsector(json);
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_graphanalytics_macrominsector(p_data json)
+RETURNS json AS
+$BODY$
+DECLARE
+
+/*
+SELECT SCHEMA_NAME.gw_fct_grafanalytics_macrominsector('{"data":{"parameters":{"commitChanges":true}}}');
+*/
+
+v_version text;
+v_project_type text;
+
+BEGIN
+
+    SET search_path = "SCHEMA_NAME", public;
+
+    SELECT giswater, project_type INTO v_version, v_project_type FROM sys_version ORDER BY id DESC LIMIT 1;
+
+    -- TODO: use this variable to commit changes or not
+    v_commitchanges = (SELECT ((p_data::json->>'data')::json->>'parameters')::json->>'commitChanges');
+
+    UPDATE node SET macrominsector_id = 0;
+    UPDATE arc SET macrominsector_id = 0;
+    UPDATE connec SET macrominsector_id = 0;
+    UPDATE link SET macrominsector_id = 0;
+
+    UPDATE node n SET macrominsector_id = c.component
+    FROM (
+        SELECT seq, component, node
+        FROM pgr_connectedcomponents('
+            SELECT arc_id::int AS id, node_1::int AS source, node_2::int AS target, 1 AS cost 
+            FROM arc WHERE node_1 IS NOT NULL and node_2 IS NOT NULL
+        ')
+    ) c
+    WHERE n.node_id::int = c.node;
+
+    UPDATE arc a SET macrominsector_id = n.macrominsector_id
+    FROM (SELECT node_id, macrominsector_id FROM node) n
+    WHERE a.node_2 = n.node_id AND n.macrominsector_id <> 0;
+
+    UPDATE connec c SET macrominsector_id = a.macrominsector_id
+    FROM (SELECT arc_id, macrominsector_id FROM arc) a
+    WHERE c.arc_id = a.arc_id AND a.macrominsector_id <> 0;
+
+    UPDATE link l SET macrominsector_id = c.macrominsector_id
+    FROM (SELECT connec_id, macrominsector_id FROM connec) c
+    WHERE l.feature_id = c.connec_id AND c.macrominsector_id <> 0;
+
+    IF v_project_type = 'UD' THEN
+        UPDATE gully g SET macrominsector_id = a.macrominsector_id
+        FROM (SELECT arc_id, macrominsector_id FROM arc) a
+        WHERE g.arc_id = a.arc_id AND a.macrominsector_id <> 0;
+    END IF;
+
+    RETURN gw_fct_json_create_return(
+        ('{"status":"Accepted", "message":{"level":1, "text":"Macrominsector analysis done successfully"}, "version":"' || v_version || '"' ||
+        ',"body":{"form":{}' ||
+        '}' ||
+        '}')::json, 3334, NULL, NULL, NULL
+    );
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
