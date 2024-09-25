1ae07ed6b53cdbf58434c068fc2a1d2f235e689e
mguzman14 <mguzman@bgeo.es>
2024-09-25 14:21:07 +0200

    fix(ci): minor fix on plan_netscenario tests
    (cherry picked from commit 69c0f44ef56a706d69b09fa36716e82d3474cf3a)

--- dbmodel/test/upsert/ws/test_v_edit_plan_netscenario_dma.sql
+++ dbmodel/test/upsert/ws/test_v_edit_plan_netscenario_dma.sql
@@ -14,7 +14,7 @@ SELECT plan(6);
 
 insert into plan_netscenario (netscenario_id) values (-901);
 INSERT INTO v_edit_plan_netscenario_dma (netscenario_id, netscenario_name, dma_id, "name", pattern_id, graphconfig, the_geom, active, stylesheet, expl_id2) 
-VALUES(-901, '', 0, '', '', null, null, false, null, 0);
+VALUES(-901, '', -901, '', '', null, null, false, null, 0);
 SELECT is((SELECT count(*)::integer FROM v_edit_plan_netscenario_dma WHERE dma_id = -901), 1, 'INSERT: v_edit_plan_netscenario_dma -901 was inserted');
 SELECT is((SELECT count(*)::integer FROM plan_netscenario_dma WHERE dma_id = -901), 1, 'INSERT: plan_netscenario_dma -901 was inserted');
 
