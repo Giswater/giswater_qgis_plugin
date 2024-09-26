f8416491b93be780a4957a67c20c00c3c51802dd
edgarfuste <34737125+edgarfuste@users.noreply.github.com>
2024-09-26 08:22:17 +0200

    fix(testing): improve previous commit
    (cherry picked from commit 73225fc09699bd42dc2ce3dd5667d64f07bf0036)

--- dbmodel/test/upsert/ws/test_v_edit_plan_psector_x_other.sql
+++ dbmodel/test/upsert/ws/test_v_edit_plan_psector_x_other.sql
@@ -13,14 +13,14 @@ SET search_path = "SCHEMA_NAME", public, pg_catalog;
 SELECT plan(6);
 
 INSERT INTO v_edit_plan_psector_x_other (id, psector_id, price_id, unit, price_descript, price, measurement, total_budget, observ, atlas_id, the_geom) 
-VALUES(-901, 1, 'S_EXC', 'm3', '', 0, 0, 0, '', '', null);
+VALUES(-901, 1, 'S_EXC', '', '', 0, 0, 0, '', '', null);
 SELECT is((SELECT count(*)::integer FROM v_edit_plan_psector_x_other WHERE id = -901), 1, 'INSERT: v_edit_plan_psector_x_other -901 was inserted');
 SELECT is((SELECT count(*)::integer FROM plan_psector_x_other WHERE id = -901), 1, 'INSERT: plan_psector_x_connec -901 was inserted');
 
 
-UPDATE v_edit_plan_psector_x_other SET unit = 'updated unit' WHERE id = -901;
-SELECT is((SELECT unit FROM v_edit_plan_psector_x_other WHERE id = -901), 'updated unit', 'UPDATE: v_edit_plan_psector_x_other -901 was updated');
-SELECT is((SELECT unit FROM plan_psector_x_connec WHERE id = -901), 'updated unit', 'UPDATE: plan_psector_x_connec -901 was updated');
+UPDATE v_edit_plan_psector_x_other SET unit = 'm3' WHERE id = -901;
+SELECT is((SELECT unit FROM v_edit_plan_psector_x_other WHERE id = -901), 'm3', 'UPDATE: v_edit_plan_psector_x_other -901 was updated');
+SELECT is((SELECT unit FROM plan_psector_x_connec WHERE id = -901), 'm3', 'UPDATE: plan_psector_x_connec -901 was updated');
 
 
 DELETE FROM v_edit_plan_psector_x_other WHERE id = -901;
