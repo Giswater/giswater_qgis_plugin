d1a3ce60b48bd64b95f4a4282950100e062b8c55
Ferran <fmartinez@bgeo.es>
2024-10-25 12:35:16 +0200

    feat(getpsectorfeatures): accept more than one psector_id

--- dbmodel/utils/fct/gw_fct_getpsectorfeatures.sql
+++ dbmodel/utils/fct/gw_fct_getpsectorfeatures.sql
@@ -13,7 +13,11 @@ $BODY$
 
 /*
 --EXAMPLE
-SELECT SCHEMA_NAME.gw_fct_getpsectorfeatures($$"{"data":{"psectorId":"1"}}"$$);
+SELECT SCHEMA_NAME.gw_fct_getpsectorfeatures($${"client":{"device":4, "lang":"en_US", "infoType":1, "epsg":25831},
+"form":{}, "feature":{}, "data":{"filterFields":{}, "pageInfo":{}, "psector_id":"[1, 2]"}}$$);
+
+SELECT SCHEMA_NAME.gw_fct_getpsectorfeatures($${"client":{"device":4, "lang":"en_US", "infoType":1, "epsg":25831},
+"form":{}, "feature":{}, "data":{"filterFields":{}, "pageInfo":{}, "psector_id":"[1]"}}$$);
 
 */
 
@@ -21,103 +25,109 @@ DECLARE
 
 v_project_type text;
 v_version text;
-
-v_psector_id int;
-
+v_psector_id text;
+v_psector_id_aux int[];
 v_result json;
 v_result_point json;
 v_result_line json;
 
-v_querytext text;
 
 
 BEGIN
 	-- Search path
 	SET search_path = "SCHEMA_NAME", public;
 
-	--  Get input data
-	v_psector_id = ((p_data->>'data')::json->>'psector_id')::int;
-	
-	-- get project type
-	SELECT project_type, giswater INTO v_project_type, v_version FROM sys_version ORDER BY id DESC LIMIT 1;
-	
-	-- get results
-	--points
-	v_result = null;
-	IF v_project_type  = 'WS' THEN
-		SELECT jsonb_agg(features.feature) INTO v_result
-		FROM (
-		SELECT jsonb_build_object(
-			'type',       'Feature',
-			'geometry',   ST_AsGeoJSON(the_geom)::jsonb,
-			'properties', to_jsonb(row) - 'the_geom_p'
-			) AS feature
-			FROM (SELECT pn.node_id AS feature_id, pn.state, n.the_geom, 'NODE' AS feature_type
-					FROM plan_psector_x_node pn
-					JOIN node n ON n.node_id = pn.node_id 
-					WHERE pn.psector_id = v_psector_id
-					UNION 
-					SELECT pc.link_id::text AS feature_id, pc.state, c.the_geom , 'CONNEC' AS feature_type
-					FROM plan_psector_x_connec pc
-					JOIN connec c ON c.connec_id = pc.connec_id 
-					WHERE pc.psector_id = v_psector_id
-			) row) features;
-	ELSIF v_project_type  = 'UD' THEN
-		SELECT jsonb_agg(features.feature) INTO v_result
-		FROM (
-		SELECT jsonb_build_object(
-			'type',       'Feature',
-			'geometry',   ST_AsGeoJSON(the_geom)::jsonb,
-			'properties', to_jsonb(row) - 'the_geom_p'
-			) AS feature
-			FROM (SELECT pn.node_id AS feature_id, pn.state, n.the_geom, 'NODE' AS feature_type
-					FROM plan_psector_x_node pn
-					JOIN node n ON n.node_id = pn.node_id 
-					WHERE pn.psector_id = v_psector_id
-					UNION 
-					SELECT pc.link_id::text AS feature_id, pc.state, c.the_geom , 'CONNEC' AS feature_type
-					FROM plan_psector_x_connec pc
-					JOIN connec c ON c.connec_id = pc.connec_id 
-					WHERE pc.psector_id = v_psector_id
-					UNION
-					SELECT pg.gully_id AS feature_id, pg.state, g.the_geom, 'GULLY' AS feature_type
-					FROM plan_psector_x_gully pg
-					JOIN gully g ON g.gully_id = pg.gully_id
-					WHERE pg.psector_id = v_psector_id
-			) row) features;
-	END IF;
-	v_result := COALESCE(v_result, '{}'); 
-	v_result_point = concat ('{"geometryType":"Point", "features":',v_result, '}');
+    -- Get input data
+    v_psector_id := (SELECT (p_data->>'data')::json->>'psector_id');
+    v_psector_id = replace(v_psector_id , '[', '');
+    v_psector_id = replace(v_psector_id , ']', '');
+    v_psector_id = replace(v_psector_id , '''', '');
+    SELECT string_to_array(v_psector_id , ',') into v_psector_id_aux;
 
+    -- Get project type
+    SELECT project_type, giswater INTO v_project_type, v_version FROM sys_version ORDER BY id DESC LIMIT 1;
 
-	
-	--lines
-	v_result = null;
-	SELECT jsonb_agg(features.feature) INTO v_result
-	FROM (
-  	SELECT jsonb_build_object(
-		'type',       'Feature',
-		'geometry',   ST_AsGeoJSON(the_geom)::jsonb,
-		'properties', to_jsonb(row) - 'the_geom'
-		) AS feature
-		FROM (SELECT pa.arc_id AS feature_id, pa.state, a.the_geom,  'ARC' AS feature_type
-				FROM plan_psector_x_arc  pa
-				JOIN arc a
-				ON a.arc_id = pa.arc_id 
-				WHERE pa.psector_id = v_psector_id
-				UNION 
-				SELECT ppxc.link_id::text AS feature_id, ppxc.state, l.the_geom , 'LINK' AS	feature_type
-				FROM plan_psector_x_connec ppxc
-				JOIN link l ON l.link_id = ppxc.link_id
-				WHERE ppxc.psector_id = v_psector_id
-			 ) row) features;
+    -- Get results
+    -- Points
+    v_result = null;
+    IF v_project_type = 'WS' THEN
+        SELECT jsonb_agg(features.feature) INTO v_result
+        FROM (
+            SELECT jsonb_build_object(
+                'type', 'Feature',
+                'geometry', ST_AsGeoJSON(the_geom)::jsonb,
+                'properties', to_jsonb(row) - 'the_geom_p'
+            ) AS feature
+            FROM (
+                SELECT pn.node_id AS feature_id, pn.state, 'NODE' AS feature_type, n.the_geom
+                FROM plan_psector_x_node pn
+                JOIN node n ON n.node_id = pn.node_id
+                WHERE pn.psector_id = ANY(v_psector_id_aux)
+                UNION
+                SELECT pc.link_id::text AS feature_id, pc.state, 'CONNEC' AS feature_type, c.the_geom
+                FROM plan_psector_x_connec pc
+                JOIN connec c ON c.connec_id = pc.connec_id
+                WHERE pc.psector_id = ANY(v_psector_id_aux)
+            ) row
+        ) features;
+    ELSIF v_project_type = 'UD' THEN
+        SELECT jsonb_agg(features.feature) INTO v_result
+        FROM (
+            SELECT jsonb_build_object(
+                'type', 'Feature',
+                'geometry', ST_AsGeoJSON(the_geom)::jsonb,
+                'properties', to_jsonb(row) - 'the_geom_p'
+            ) AS feature
+            FROM (
+                SELECT pn.node_id AS feature_id, pn.state, 'NODE' AS feature_type, n.the_geom
+                FROM plan_psector_x_node pn
+                JOIN node n ON n.node_id = pn.node_id
+                WHERE pn.psector_id = ANY(v_psector_id_aux)
+                UNION
+                SELECT pc.link_id::text AS feature_id, pc.state, 'CONNEC' AS feature_type, c.the_geom
+                FROM plan_psector_x_connec pc
+                JOIN connec c ON connec_id = pc.connec_id
+                WHERE pc.psector_id = ANY(v_psector_id_aux)
+                UNION
+                SELECT pg.gully_id AS feature_id, pg.state, 'GULLY' AS feature_type, g.the_geom
+                FROM plan_psector_x_gully pg
+                JOIN gully g ON g.gully_id = pg.gully_id
+                WHERE pg.psector_id = ANY(v_psector_id_aux)
+            ) row
+        ) features;
+    END IF;
 
-	v_result := COALESCE(v_result, '{}'); 
-	v_result_line = concat ('{"geometryType":"LineString", "features":',v_result, '}'); 
-	
-	-- Control nulls
-	v_result_point := COALESCE(v_result_point, '{}'); 
-	v_result_line := COALESCE(v_result_line, '{}'); 
+    v_result := COALESCE(v_result, '{}');
+    v_result_point := concat('{"geometryType":"Point", "features":', v_result, '}');
+
+    -- Lines
+    v_result = null;
+    SELECT jsonb_agg(features.feature) INTO v_result
+    FROM (
+        SELECT jsonb_build_object(
+            'type', 'Feature',
+            'geometry', ST_AsGeoJSON(the_geom)::jsonb,
+            'properties', to_jsonb(row) - 'the_geom'
+        ) AS feature
+        FROM (
+            SELECT pa.arc_id AS feature_id, pa.state, 'ARC' AS feature_type, a.the_geom
+            FROM plan_psector_x_arc pa
+            JOIN arc a ON a.arc_id = pa.arc_id
+            WHERE pa.psector_id = ANY(v_psector_id_aux)
+            UNION
+            SELECT ppxc.link_id::text AS feature_id, ppxc.state, 'LINK' AS feature_type, l.the_geom
+            FROM plan_psector_x_connec ppxc
+            JOIN link l ON l.link_id = ppxc.link_id
+            WHERE ppxc.psector_id = ANY(v_psector_id_aux)
+        ) row
+    ) features;
+
+    v_result := COALESCE(v_result, '{}');
+    v_result_line := concat('{"geometryType":"LineString", "features":', v_result, '}');
+
+    -- Control nulls
+    v_result_point := COALESCE(v_result_point, '{}');
+    v_result_line := COALESCE(v_result_line, '{}');
 
 
 	--  Return
