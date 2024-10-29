5d89131efa4e9f31097a37a875d4cf92d05ab358
Ferran <fmartinez@bgeo.es>
2024-10-29 12:17:12 +0100

    feat(fct_set_current): new function to deal with values from config_param_user and standardizing behavior to set current text in current_label

---
+++ dbmodel/utils/fct/gw_fct_set_current.sql
@@ -0,0 +1,132 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+--FUNCTION CODE:
+
+DROP FUNCTION IF EXISTS SCHEMA_NAME.gw_fct_set_current(json);
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_set_current(p_data JSON)
+RETURNS json AS
+$BODY$
+
+/*
+--EXAMPLE
+SELECT SCHEMA_NAME.gw_fct_set_current($${"client":{"device":4, "lang":"en_US", "infoType":1, "epsg":25831},
+"form":{}, "feature":{},
+"data":{"filterFields":{}, "pageInfo":{}, "type":"netscenario", "id": "5"}}$$);
+
+SELECT SCHEMA_NAME.gw_fct_set_current($${"client":{"device":4, "lang":"en_US", "infoType":1, "epsg":25831},
+"form":{}, "feature":{},
+"data":{"filterFields":{}, "pageInfo":{}, "type": "psector", "id": "1"}}$$);
+
+*/
+
+DECLARE
+    v_project_type TEXT;
+    v_version TEXT;
+    v_type TEXT;
+    v_id TEXT;
+
+    v_type_name TEXT;
+    v_type_id INT;
+    result JSON;
+    query TEXT;
+    parameter_name TEXT;
+
+BEGIN
+
+    -- Set the search path
+    SET search_path = "SCHEMA_NAME", public;
+
+    v_type :=  ((p_data->>'data')::json)->>'type'::text;
+   	v_id := ((p_data->>'data')::json)->>'id'::text;
+
+    IF v_type IS NULL THEN
+        --TODO ('Harmonize all functions messages with this logic to have the same msg from sys_message ')
+        RAISE EXCEPTION 'Input parameter "type" is required';
+    END IF;
+
+    -- Get project type and version info
+    SELECT project_type, giswater INTO v_project_type, v_version
+    FROM sys_version
+    ORDER BY id DESC
+    LIMIT 1;
+
+    -- Determine the parameter name based on the provided type
+    CASE p_data->'data'->>'type'
+        WHEN 'netscenario' THEN
+            parameter_name := 'plan_netscenario_vdefault';
+
+        WHEN 'psector' THEN
+            parameter_name := 'plan_psector_vdefault';
+
+        WHEN 'hydrology' THEN
+            parameter_name := 'inp_options_hydrology_scenario';
+
+        WHEN 'dwf' THEN
+            parameter_name := 'inp_options_dwfscenario';
+
+        ELSE
+            RAISE EXCEPTION 'Unknown type';
+    END CASE;
+
+    -- Upsert logic for config_param_user: insert if not exists, otherwise update
+    IF v_id IS NOT NULL THEN
+    	IF EXISTS (SELECT 1 FROM config_param_user WHERE cur_user = current_user AND parameter = parameter_name) THEN
+	        -- Update if the parameter exists
+	        UPDATE config_param_user
+	        SET value = v_id
+	        WHERE cur_user = current_user AND parameter = parameter_name;
+	    ELSE
+	        -- Insert a new record if it does not exist
+	        INSERT INTO config_param_user (parameter, value, cur_user)
+	        VALUES (parameter_name, v_id, current_user);
+	    END IF;
+	END IF;
+
+    -- Determine the query based on the provided type
+    CASE p_data->'data'->>'type'
+        WHEN 'netscenario' THEN
+		    query := 'SELECT t1.netscenario_id, t1.name FROM plan_netscenario AS t1 '
+		          || 'INNER JOIN config_param_user AS t2 ON t1.netscenario_id::text = t2.value '
+		          || 'WHERE t2.parameter = ''plan_netscenario_vdefault'' AND t2.cur_user = current_user';
+		    EXECUTE query INTO v_type_id, v_type_name;
+		    result := json_build_object('netscenario_id', v_type_id, 'name', v_type_name);
+
+        WHEN 'psector' THEN
+            query := 'SELECT t1.psector_id, t1.name FROM plan_psector AS t1 '
+                  || 'INNER JOIN config_param_user AS t2 ON t1.psector_id::text = t2.value '
+                  || 'WHERE t2.parameter = ''plan_psector_vdefault'' AND t2.cur_user = current_user';
+            EXECUTE query INTO v_type_id, v_type_name;
+            result := json_build_object('psector_id', v_type_id, 'name', v_type_name);
+
+        WHEN 'hydrology' THEN
+            query := 'SELECT t1.hydrology_id, t1.name FROM cat_hydrology AS t1 '
+                  || 'INNER JOIN config_param_user AS t2 ON t1.hydrology_id::text = t2.value '
+                  || 'WHERE t2.parameter = ''inp_options_hydrology_scenario'' AND t2.cur_user = current_user';
+            EXECUTE query INTO v_type_id, v_type_name;
+            result := json_build_object('hydrology_id', v_type_id, 'name', v_type_name);
+
+        WHEN 'dwf' THEN
+            query := 'SELECT t1.id, t1.idval FROM cat_dwf_scenario AS t1 '
+                  || 'INNER JOIN config_param_user AS t2 ON t1.id::text = t2.value '
+                  || 'WHERE t2.parameter = ''inp_options_dwfscenario'' AND t2.cur_user = current_user';
+            EXECUTE query INTO v_type_id, v_type_name;
+            result := json_build_object('dwf_id', v_type_id, 'name', v_type_name);
+
+    END CASE;
+
+    -- Return JSON response
+    RETURN (
+        '{"status":"Accepted", "message":{"level":1, "text":"Change done successfully"}, "version":"' || v_version || '"' ||
+        ',"body":{"form":{}' ||
+        ',"feature":{}' ||
+        ',"data":' || result::text || '}}'
+    )::json;
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
