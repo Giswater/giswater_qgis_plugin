23833a2e956c189e11d76ae2fccb84d1729b42e6
Ferran <fmartinez@bgeo.es>
2024-10-30 09:23:01 +0100

    test(plsql): add test for get_psectorfeature function

---
+++ dbmodel/test/plsql/ws/test_gw_fct_getpsectorfeatures.sql
@@ -0,0 +1,33 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+BEGIN;
+
+-- Suppress NOTICE messages
+SET client_min_messages TO WARNING;
+
+SET search_path = "SCHEMA_NAME", public, pg_catalog;
+
+SELECT plan(2);
+
+-- Extract and test the "status" field from the function's JSON response
+SELECT is (
+    (gw_fct_getpsectorfeatures($${"client":{"device":4, "lang":"en_US", "infoType":1, "epsg":25831},
+    "form":{}, "feature":{}, "data":{"filterFields":{}, "pageInfo":{}, "psector_id":"[1]"}}$$)::JSON)->>'status',
+    'Accepted',
+    'Check if gw_fct_getpsectorfeatures with 1 psector returns status "Accepted"'
+);
+
+SELECT is (
+    (gw_fct_getpsectorfeatures($${"client":{"device":4, "lang":"en_US", "infoType":1, "epsg":25831},
+    "form":{}, "feature":{}, "data":{"filterFields":{}, "pageInfo":{}, "psector_id":"[1, 2]"}}$$)::JSON)->>'status',
+    'Accepted',
+    'Check if gw_fct_getpsectorfeatures with more than 1 psector returns status "Accepted"'
+);
+
+-- Finish the test
+SELECT finish();
+
+ROLLBACK;
