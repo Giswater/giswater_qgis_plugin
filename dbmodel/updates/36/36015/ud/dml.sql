615066661fe1c74b71b79423689346f6c793ef7f
Albert Bofill <abofill@bgeo.es>
2024-10-30 17:51:19 +0100

    fix: minor typo on function descript
    (cherry picked from commit bc6e50c7a9cc71834365cf9c20077b168e70a116)

--- dbmodel/updates/36/36015/ud/dml.sql
+++ dbmodel/updates/36/36015/ud/dml.sql
@@ -58,3 +58,5 @@ WHERE columnname  = 'muni_id' AND formname = 'v_ext_address';
 UPDATE sys_param_user set dv_querytext = 'SELECT id, id as idval FROM v_edit_inp_timeseries WHERE timser_type= ''Rainfall'' and active' where id = 'inp_options_setallraingages';
 
 INSERT INTO inp_typevalue VALUES ('inp_value_options_in','MODIFIED_GREEN_AMPT','MODIFIED_GREEN_AMPT');
+
+UPDATE sys_function SET descript='Function to manage values of defined target hydrology catalog (delete or copy from another one). It works with all objects linked with hydrology catalog (Subcatchment, Lids, Loadings, Coverages and Groundwater).' WHERE id=3100 AND function_name='gw_fct_manage_hydrology_values';
