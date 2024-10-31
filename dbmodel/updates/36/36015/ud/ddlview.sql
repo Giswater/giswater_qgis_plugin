682ba905160e7f8bcbf6e325f671db53356121c1
xtorret@bgeo.es <xtorret@bgeo.es>
2024-10-31 12:50:02 +0100

    fix(links): Add SELECT DISTINCT on v_state for links in order to prevent conflicts on plan mode
    (cherry picked from commit 8663e8d436a534e5d72b94feb45bb330c6f232e3)

--- dbmodel/updates/36/36015/ud/ddlview.sql
+++ dbmodel/updates/36/36015/ud/ddlview.sql
@@ -142,6 +142,47 @@ CREATE OR REPLACE VIEW v_rpt_arcflow_sum AS
 
 
 
+CREATE OR REPLACE VIEW v_state_link_connec AS 
+WITH 
+p AS (SELECT connec_id, psector_id, state, link_id FROM plan_psector_x_connec WHERE active), 
+sp AS (SELECT * FROM selector_psector WHERE cur_user = current_user), 
+se AS (SELECT * FROM selector_expl WHERE cur_user = current_user), 
+l AS (SELECT link_id, state, expl_id, expl_id2 FROM link)
+SELECT l.link_id  FROM selector_state, se, l WHERE l.state = selector_state.state_id AND (l.expl_id = se.expl_id OR l.expl_id2 = se.expl_id) AND selector_state.cur_user = "current_user"()::text AND se.cur_user = "current_user"()::text
+	EXCEPT ALL
+SELECT p.link_id FROM sp, se, p JOIN l USING (link_id) WHERE p.psector_id = sp.psector_id AND sp.cur_user = "current_user"()::text AND p.state = 0 AND l.expl_id = se.expl_id AND se.cur_user = CURRENT_USER::text
+	UNION ALL
+SELECT DISTINCT p.link_id FROM sp, se, p JOIN l USING (link_id) WHERE p.psector_id = sp.psector_id AND sp.cur_user = "current_user"()::text AND p.state = 1 AND l.expl_id = se.expl_id AND se.cur_user = CURRENT_USER::text;
 
+
+CREATE OR REPLACE VIEW v_state_link_gully AS 
+WITH 
+p AS (SELECT gully_id, psector_id, state, link_id FROM plan_psector_x_gully WHERE active), 
+sp AS (SELECT * FROM selector_psector WHERE cur_user = current_user), 
+se AS (SELECT * FROM selector_expl WHERE cur_user = current_user), 
+l AS (SELECT link_id, state, expl_id, expl_id2 FROM link)
+SELECT l.link_id  FROM selector_state, se, l WHERE l.state = selector_state.state_id AND (l.expl_id = se.expl_id OR l.expl_id2 = se.expl_id) AND selector_state.cur_user = "current_user"()::text AND se.cur_user = "current_user"()::text
+	EXCEPT ALL
+SELECT p.link_id FROM sp, se, p JOIN l USING (link_id) WHERE p.psector_id = sp.psector_id AND sp.cur_user = "current_user"()::text AND p.state = 0 AND l.expl_id = se.expl_id AND se.cur_user = CURRENT_USER::text
+	UNION ALL
+SELECT DISTINCT p.link_id FROM sp, se, p JOIN l USING (link_id) WHERE p.psector_id = sp.psector_id AND sp.cur_user = "current_user"()::text AND p.state = 1 AND l.expl_id = se.expl_id AND se.cur_user = CURRENT_USER::text;
+
+
+CREATE OR REPLACE VIEW v_state_link AS
+WITH 
+c AS (SELECT connec_id, psector_id, state, link_id FROM plan_psector_x_connec WHERE active), 
+g AS (SELECT gully_id, psector_id, state, link_id FROM plan_psector_x_gully WHERE active), 
+sp AS (SELECT * FROM selector_psector WHERE cur_user = current_user), 
+se AS (SELECT * FROM selector_expl WHERE cur_user = current_user), 
+l AS (SELECT link_id, state, expl_id, expl_id2 FROM link)
+SELECT l.link_id  FROM selector_state, se, l WHERE l.state = selector_state.state_id AND (l.expl_id = se.expl_id OR l.expl_id2 = se.expl_id) AND selector_state.cur_user = "current_user"()::text AND se.cur_user = "current_user"()::text
+	EXCEPT ALL
+SELECT c.link_id FROM sp, se, c JOIN l USING (link_id) WHERE c.psector_id = sp.psector_id AND sp.cur_user = "current_user"()::text AND c.state = 0 AND l.expl_id = se.expl_id AND se.cur_user = CURRENT_USER::text
+	EXCEPT ALL
+SELECT g.link_id FROM sp, se, g JOIN l USING (link_id) WHERE g.psector_id = sp.psector_id AND sp.cur_user = "current_user"()::text AND g.state = 0 AND l.expl_id = se.expl_id AND se.cur_user = CURRENT_USER::text
+	UNION ALL
+SELECT DISTINCT c.link_id FROM sp, se, c JOIN l USING (link_id) WHERE c.psector_id = sp.psector_id AND sp.cur_user = "current_user"()::text AND c.state = 1 AND l.expl_id = se.expl_id AND se.cur_user = CURRENT_USER::text
+	UNION ALL
+SELECT DISTINCT g.link_id FROM sp, se, g JOIN l USING (link_id) WHERE g.psector_id = sp.psector_id AND sp.cur_user = "current_user"()::text AND g.state = 1 AND l.expl_id = se.expl_id AND se.cur_user = CURRENT_USER::text;
    
 
---
+++ dbmodel/updates/36/36015/ws/ddlview.sql
@@ -0,0 +1,21 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+CREATE OR REPLACE VIEW v_state_link AS 
+WITH 
+p AS (SELECT connec_id, psector_id, state, link_id FROM plan_psector_x_connec WHERE active), 
+sp AS (SELECT * FROM selector_psector WHERE cur_user = current_user), 
+se AS (SELECT * FROM selector_expl WHERE cur_user = current_user), 
+l AS (SELECT link_id, state, expl_id, expl_id2 FROM link)
+SELECT l.link_id  FROM selector_state, se, l WHERE l.state = selector_state.state_id AND (l.expl_id = se.expl_id OR l.expl_id2 = se.expl_id) AND selector_state.cur_user = "current_user"()::text AND se.cur_user = "current_user"()::text
+	EXCEPT ALL
+SELECT p.link_id FROM sp, se, p JOIN l USING (link_id) WHERE p.psector_id = sp.psector_id AND sp.cur_user = "current_user"()::text AND p.state = 0 AND l.expl_id = se.expl_id AND se.cur_user = CURRENT_USER::text
+	UNION ALL
+SELECT DISTINCT p.link_id FROM sp, se, p JOIN l USING (link_id) WHERE p.psector_id = sp.psector_id AND sp.cur_user = "current_user"()::text AND p.state = 1 AND l.expl_id = se.expl_id AND se.cur_user = CURRENT_USER::text;
+
