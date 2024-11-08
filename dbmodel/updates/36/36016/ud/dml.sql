5686c8fef9fe60f0a76f24137c40bf4d2820f5de
xtorret@bgeo.es <xtorret@bgeo.es>
2024-11-08 01:13:57 +0100

    enhan(raingage) Update combo of raingage showing only actives an Rainfall type
    (cherry picked from commit de5d77e204f7fa8014815ef1bc55056852f2c31f)

---
+++ dbmodel/updates/36/36016/ud/dml.sql
@@ -0,0 +1,11 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+UPDATE config_form_fields SET dv_querytext = 'SELECT id, id as idval FROM inp_timeseries WHERE timser_type = ''Rainfall'' AND active' 
+WHERE formname = 'v_edit_raingage' and columnname = 'rg_id'
