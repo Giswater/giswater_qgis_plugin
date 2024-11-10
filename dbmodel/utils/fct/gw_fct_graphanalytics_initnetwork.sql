a26b3522f9197dc80606a1f1676172741fc04328
claudia dragoste <claudiadragoste@yahoo.com>
2024-11-10 22:02:42 +0100

    fix(gw_fct_graphanalytics_initnetwork): clean SELECTs

--- dbmodel/utils/fct/gw_fct_graphanalytics_initnetwork.sql
+++ dbmodel/utils/fct/gw_fct_graphanalytics_initnetwork.sql
@@ -63,10 +63,7 @@ BEGIN
     INSERT INTO temp_pgr_node (pgr_node_id, node_id)
     (
         SELECT node_id::INT, node_id
-        FROM
-        (
-            SELECT node_id, state, state_type, macrominsector_id FROM node
-        ) n
+        FROM node n
         JOIN value_state_type s ON s.id = n.state_type
         WHERE n.state = 1 AND s.is_operative = TRUE
         AND n.macrominsector_id::TEXT IN (v_macrominsector_id_node)
@@ -85,11 +82,8 @@ BEGIN
     INSERT INTO temp_pgr_connec (connec_id, arc_id)
     (
         SELECT c.connec_id, c.arc_id
-        FROM
-        (
-            SELECT connec_id, arc_id, state, state_type, macrominsector_id FROM connec
-        ) c
-        JOIN temp_pgr_arc a ON c.arc_id::INT = a.pgr_arc_id
+        FROM connec c
+        JOIN temp_pgr_arc a ON c.arc_id = a.arc_id
         JOIN value_state_type s ON s.id = c.state_type
         WHERE c.state = 1 AND s.is_operative = TRUE
         AND c.macrominsector_id::TEXT IN (v_macrominsector_id_connec)
@@ -98,10 +92,7 @@ BEGIN
     INSERT INTO temp_pgr_link (link_id, feature_id, feature_type)
     (
         SELECT link_id, feature_id, feature_type
-        FROM
-        (
-            SELECT link_id, feature_id, feature_type, state, macrominsector_id FROM link
-        ) l
+        FROM link l
         JOIN temp_pgr_connec c ON l.feature_id=c.connec_id
         WHERE l.state = 1 AND l.feature_type = 'CONNEC'
         AND l.macrominsector_id::TEXT IN (v_macrominsector_id_link)
@@ -111,10 +102,8 @@ BEGIN
         INSERT INTO temp_pgr_gully (gully_id, arc_id)
         (
             SELECT g.gully_id, g.arc_id
-            FROM (
-                SELECT gully_id, arc_id, state, state_type, macrominsector_id FROM gully
-            ) g
-            JOIN temp_pgr_arc a ON g.arc_id::INT = a.pgr_arc_id
+            FROM gully g
+            JOIN temp_pgr_arc a ON g.arc_id = a.arc_id
             JOIN value_state_type s ON s.id = g.state_type
             WHERE g.state = 1 AND s.is_operative = TRUE
             AND g.macrominsector_id::TEXT IN (v_macrominsector_id_gully)
@@ -123,9 +112,7 @@ BEGIN
         INSERT INTO temp_pgr_link (link_id, feature_id, feature_type)
         (
             SELECT link_id, feature_id, feature_type
-            FROM (
-                SELECT link_id, feature_id, feature_type, state, macrominsector_id FROM link
-            ) l
+            FROM link l
             JOIN temp_pgr_gully g ON l.feature_id = g.gully_id
             WHERE l.state = 1 AND l.feature_type = 'GULLY'
             AND l.macrominsector_id::TEXT IN (v_macrominsector_id_link)
