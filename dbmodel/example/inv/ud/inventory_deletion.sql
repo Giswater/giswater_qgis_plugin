4f9172cf67307f1b35a95814e3d8ed6c1a2cba93
xtorret@bgeo.es <xtorret@bgeo.es>
2024-11-11 17:48:42 +0100

    fix(inventory_deletion): Recover data from inp_pump and inp_weir
    (cherry picked from commit f8e78778a6e993b5b518bf94536cdf79af5932ec)

--- dbmodel/example/inv/ud/inventory_deletion.sql
+++ dbmodel/example/inv/ud/inventory_deletion.sql
@@ -32,8 +32,8 @@ DELETE FROM inp_flwreg_pump;
 DELETE FROM inp_flwreg_orifice;
 DELETE FROM inp_flwreg_outlet;
 
-DELETE FROM inp_weir;
-DELETE FROM inp_pump;
+UPDATE inp_weir SET weir_type=null, offsetval=null, cd=null, geom1=null, geom2=null, geom3=null, geom4=null;
+UPDATE inp_pump SET curve_id=null, status=null, startup=null , shutoff=null;
 
 UPDATE cat_mat_arc SET n = null;
 
