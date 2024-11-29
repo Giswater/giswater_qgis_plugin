c489f415b83d1774e5a7f5fb90b14fe00469272c
Albert Bofill <abofill@bgeo.es>
2024-11-29 14:06:47 +0100

    enhancement: change inputparam labels for elevation from dem function. Use VALUES instead of NODES (can be nodes or connecs)
    (cherry picked from commit 3f9e6682a29b40ac735abbc9019862b90ae9b540)

--- dbmodel/updates/36/36016/utils/dml.sql
+++ dbmodel/updates/36/36016/utils/dml.sql
@@ -10,3 +10,6 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 INSERT INTO sys_function
 (id, function_name, project_type, function_type, input_params, return_type, descript, sys_role, sample_query, "source")
 VALUES(3328, 'gw_trg_edit_municipality', 'utils', 'function', 'json', 'json', 'Trigger to insert or update elements in v_ext_municipality table.', 'role_edit', NULL, 'core');
+
+UPDATE config_toolbox SET inputparams='[{"widgetname":"updateValues", "label":"Values to update:","widgettype":"combo","datatype":"text","layoutname":"grl_option_parameters","layoutorder":1,
+"comboIds":["allValues", "nullValues"], "comboNames":["ALL VALUES", "NULL ELEVATION VALUES"], "selectedId":"nullValues"}]'::json WHERE id=2760;
