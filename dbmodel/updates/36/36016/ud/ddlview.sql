412e8cea35348e5d12d9f11d0edb9f42e10b3d51
xtorret@bgeo.es <xtorret@bgeo.es>
2024-12-03 16:57:20 +0100

    fix(from prev commit)

--- dbmodel/updates/36/36016/ud/ddlview.sql
+++ dbmodel/updates/36/36016/ud/ddlview.sql
@@ -455,7 +455,39 @@ AS SELECT raingage.rg_id,
  
 
 CREATE OR REPLACE VIEW v_edit_inp_subcatchment AS 
- select inp_subcatchment.*
+ select inp_subcatchment.hydrology_id,
+    inp_subcatchment.subc_id,
+    inp_subcatchment.outlet_id,
+    inp_subcatchment.rg_id,
+    inp_subcatchment.area,
+    inp_subcatchment.imperv,
+    inp_subcatchment.width,
+    inp_subcatchment.slope,
+    inp_subcatchment.clength,
+    inp_subcatchment.snow_id,
+    inp_subcatchment.nimp,
+    inp_subcatchment.nperv,
+    inp_subcatchment.simp,
+    inp_subcatchment.sperv,
+    inp_subcatchment.zero,
+    inp_subcatchment.routeto,
+    inp_subcatchment.rted,
+    inp_subcatchment.maxrate,
+    inp_subcatchment.minrate,
+    inp_subcatchment.decay,
+    inp_subcatchment.drytime,
+    inp_subcatchment.maxinfil,
+    inp_subcatchment.suction,
+    inp_subcatchment.conduct,
+    inp_subcatchment.initdef,
+    inp_subcatchment.curveno,
+    inp_subcatchment.conduct_2,
+    inp_subcatchment.drytime_2,
+    inp_subcatchment.sector_id,
+    inp_subcatchment.the_geom,
+    inp_subcatchment.descript,
+    inp_subcatchment.minelev,
+    inp_subcatchment.muni_id
    FROM inp_subcatchment, config_param_user, selector_sector, selector_municipality
    WHERE inp_subcatchment.sector_id = selector_sector.sector_id AND selector_sector.cur_user = "current_user"()::text 
    AND inp_subcatchment.muni_id = selector_municipality.muni_id AND selector_municipality.cur_user = "current_user"()::text 
