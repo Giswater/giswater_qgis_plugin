d949fb21786a9653cb397169bc7408d4f51947ef
xtorret@bgeo.es <xtorret@bgeo.es>
2024-12-03 14:12:06 +0100

    fix(from prev commit)

--- dbmodel/test/structure/ws/test_tables.sql
+++ dbmodel/test/structure/ws/test_tables.sql
@@ -10,7 +10,7 @@ SET client_min_messages TO WARNING;
 
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
-SELECT plan(318);
+SELECT plan(316);
 
 SELECT has_table('anl_arc') AS "Table 'anl_arc' exists";
 SELECT has_table('anl_arc_x_node') AS "Table 'anl_arc_x_node' exists";
