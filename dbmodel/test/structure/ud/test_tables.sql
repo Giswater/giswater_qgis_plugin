15b3f11e39eb7a3fd2ae598c7906681d1f47ce3c
Daniel Marin <dmarin@bgeo.es>
2024-12-09 12:16:26 +0100

    fix(test): correct bad plan on test_tables

--- dbmodel/test/structure/ud/test_tables.sql
+++ dbmodel/test/structure/ud/test_tables.sql
@@ -10,7 +10,7 @@ SET client_min_messages TO WARNING;
 
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
-SELECT plan(355);
+SELECT plan(354);
 
 SELECT has_table('anl_arc') AS "Table 'anl_arc' exists";
 SELECT has_table('anl_arc_x_node') AS "Table 'anl_arc_x_node' exists";
