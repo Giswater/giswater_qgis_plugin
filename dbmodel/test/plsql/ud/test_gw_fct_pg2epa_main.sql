1849399ca9fd15ba41faef58a110ba0925ca7fab
Daniel Marin <dmarin@bgeo.es>
2025-01-22 12:54:46 +0100

    refactor(tests): update gw_fct_pg2epa_main tests to use consistent 'resultId' and expand test coverage from 1 to 7 steps on ws

--- dbmodel/test/plsql/ud/test_gw_fct_pg2epa_main.sql
+++ dbmodel/test/plsql/ud/test_gw_fct_pg2epa_main.sql
@@ -16,49 +16,49 @@ SELECT plan(7);
 -- Extract and test the "status" field from the function's JSON response
 SELECT is(
     (gw_fct_pg2epa_main($${"client":{"device":4, "lang":"", "infoType":1, "epsg":25831}, "form":{}, "feature":{},
-    "data":{"filterFields":{}, "pageInfo":{}, "resultId":"expl_1", "dumpSubcatch":"False", "step": 1}}$$)::JSON)->>'status',
+    "data":{"filterFields":{}, "pageInfo":{}, "resultId":"testing", "dumpSubcatch":"False", "step": 1}}$$)::JSON)->>'status',
     'Accepted',
     'Check if gw_fct_pg2epa_main: dumpSubcatch=False | step=1 returns status "Accepted"'
 );
 
 SELECT is(
     (gw_fct_pg2epa_main($${"client":{"device":4, "lang":"", "infoType":1, "epsg":25831}, "form":{}, "feature":{},
-    "data":{"filterFields":{}, "pageInfo":{}, "resultId":"expl_1", "dumpSubcatch":"False", "step": 2}}$$)::JSON)->>'status',
+    "data":{"filterFields":{}, "pageInfo":{}, "resultId":"testing", "dumpSubcatch":"False", "step": 2}}$$)::JSON)->>'status',
     'Accepted',
     'Check if gw_fct_pg2epa_main: dumpSubcatch=False | step=2 returns status "Accepted"'
 );
 
 SELECT is(
     (gw_fct_pg2epa_main($${"client":{"device":4, "lang":"", "infoType":1, "epsg":25831}, "form":{}, "feature":{},
-    "data":{"filterFields":{}, "pageInfo":{}, "resultId":"expl_1", "dumpSubcatch":"False", "step": 3}}$$)::JSON)->>'status',
+    "data":{"filterFields":{}, "pageInfo":{}, "resultId":"testing", "dumpSubcatch":"False", "step": 3}}$$)::JSON)->>'status',
     'Accepted',
     'Check if gw_fct_pg2epa_main: dumpSubcatch=False | step=3 returns status "Accepted"'
 );
 
 SELECT is(
     (gw_fct_pg2epa_main($${"client":{"device":4, "lang":"", "infoType":1, "epsg":25831}, "form":{}, "feature":{},
-    "data":{"filterFields":{}, "pageInfo":{}, "resultId":"expl_1", "dumpSubcatch":"False", "step": 4}}$$)::JSON)->>'status',
+    "data":{"filterFields":{}, "pageInfo":{}, "resultId":"testing", "dumpSubcatch":"False", "step": 4}}$$)::JSON)->>'status',
     'Accepted',
     'Check if gw_fct_pg2epa_main: dumpSubcatch=False | step=4 returns status "Accepted"'
 );
 
 SELECT is(
     (gw_fct_pg2epa_main($${"client":{"device":4, "lang":"", "infoType":1, "epsg":25831}, "form":{}, "feature":{},
-    "data":{"filterFields":{}, "pageInfo":{}, "resultId":"expl_1", "dumpSubcatch":"False", "step": 5}}$$)::JSON)->>'status',
+    "data":{"filterFields":{}, "pageInfo":{}, "resultId":"testing", "dumpSubcatch":"False", "step": 5}}$$)::JSON)->>'status',
     'Accepted',
     'Check if gw_fct_pg2epa_main: dumpSubcatch=False | step=5 returns status "Accepted"'
 );
 
 SELECT is(
     (gw_fct_pg2epa_main($${"client":{"device":4, "lang":"", "infoType":1, "epsg":25831}, "form":{}, "feature":{},
-    "data":{"filterFields":{}, "pageInfo":{}, "resultId":"expl_1", "dumpSubcatch":"False", "step": 6}}$$)::JSON)->>'status',
+    "data":{"filterFields":{}, "pageInfo":{}, "resultId":"testing", "dumpSubcatch":"False", "step": 6}}$$)::JSON)->>'status',
     'Accepted',
     'Check if gw_fct_pg2epa_main: dumpSubcatch=False | step=6 returns status "Accepted"'
 );
 
 SELECT is(
     (gw_fct_pg2epa_main($${"client":{"device":4, "lang":"", "infoType":1, "epsg":25831}, "form":{}, "feature":{},
-    "data":{"filterFields":{}, "pageInfo":{}, "resultId":"expl_1", "dumpSubcatch":"False", "step": 7}}$$)::JSON)->>'status',
+    "data":{"filterFields":{}, "pageInfo":{}, "resultId":"testing", "dumpSubcatch":"False", "step": 7}}$$)::JSON)->>'status',
     'Accepted',
     'Check if gw_fct_pg2epa_main: dumpSubcatch=False | step=7 returns status "Accepted"'
 );
