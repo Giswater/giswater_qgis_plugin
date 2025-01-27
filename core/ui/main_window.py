1aa856561c045c3a73a26a452d78bb770a617262
arnauurgeles <aurgeles@bgeo.es>
2025-01-27 10:29:02 +0100

    fix(main_window behavior): Default parent is null

--- core/ui/main_window.py
+++ core/ui/main_window.py
@@ -27,7 +27,7 @@ class GwMainWindow(QMainWindow):
 
     def __init__(self, class_obj, subtag=None, parent=None):
 
-        super().__init__(parent or iface.mainWindow())
+        super().__init__(parent)
         self.setupUi(self)
 
         # Check if CONTEXT and UINAME are defined and set properties accordingly
