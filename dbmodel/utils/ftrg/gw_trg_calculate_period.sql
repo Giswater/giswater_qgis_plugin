b77083818a4829204804fecae877f6d0ab205793
mguzman14 <mguzman@bgeo.es>
2025-01-29 18:04:56 +0100

    fix(period): bug fix on calculating period seconds

--- dbmodel/utils/ftrg/gw_trg_calculate_period.sql
+++ dbmodel/utils/ftrg/gw_trg_calculate_period.sql
@@ -19,13 +19,15 @@ BEGIN
 
 EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||',public';
 
- IF TG_OP = 'INSERT' THEN
+ IF TG_OP = 'INSERT' THEN -- TO-DO manage TG_OP = 'UPDATE' (recursivity issue)
  
 	SELECT * INTO v_record FROM ext_cat_period where id = NEW.id;
  
-	IF v_record.start_date IS NOT NULL AND v_record.end_date IS NOT NULL AND v_record.end_date > v_record.start_date THEN
+	IF v_record.start_date IS NOT NULL AND v_record.end_date IS NOT NULL AND v_record.end_date >= v_record.start_date THEN
 	
-		UPDATE ext_cat_period SET period_seconds = (EXTRACT(EPOCH FROM v_record.end_date) - EXTRACT(EPOCH FROM v_record.start_date))::integer WHERE ext_cat_period.id = NEW.id; 
+	
+		UPDATE ext_cat_period SET period_seconds = (EXTRACT(EPOCH FROM v_record.end_date::date + 1) - EXTRACT(EPOCH FROM v_record.start_date::date))::integer 
+		WHERE ext_cat_period.id = NEW.id; 
 			
 	ELSE
 		NEW.period_seconds := (SELECT value FROM config_param_system WHERE  parameter = 'admin_crm_periodseconds_vdefault');
