e76bb1bbf464070d812ddff59a93f6d08d9d02f0
smaspons <smaspons@bgeo.es>
2025-02-05 11:33:41 +0100

    fix(test): use setting column in test_v_edit_inp_dscenario_valve

--- dbmodel/test/upsert/ws/test_v_edit_inp_dscenario_valve.sql
+++ dbmodel/test/upsert/ws/test_v_edit_inp_dscenario_valve.sql
@@ -12,18 +12,18 @@ SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
 SELECT plan(6);
 
-INSERT INTO v_edit_inp_dscenario_valve (dscenario_id, node_id, nodarc_id, valv_type, pressure, flow, coef_loss, curve_id, minorloss, status, add_settings, init_quality, the_geom)
-VALUES(1, '1083', '1083_n2a', 'PRV', -901, 0.0000, 0.0000, NULL, 0.0000, 'ACTIVE', 0.0, 0.0, 'SRID=25831;POINT (419524.43246714014 4576488.44256104)'::public.geometry);
-SELECT is((SELECT count(*)::integer FROM v_edit_inp_dscenario_valve WHERE pressure = -901), 1, 'INSERT: v_edit_inp_dscenario_valve -901 was inserted');
-SELECT is((SELECT count(*)::integer FROM inp_dscenario_valve WHERE pressure = -901), 1, 'INSERT: inp_dscenario_valve -901 was inserted');
+INSERT INTO v_edit_inp_dscenario_valve (dscenario_id, node_id, nodarc_id, valv_type, setting, curve_id, minorloss, status, add_settings, init_quality, the_geom)
+VALUES(1, '1083', '1083_n2a', 'PRV', -901, NULL, 0.0000, 'ACTIVE', 0.0, 0.0, 'SRID=25831;POINT (419524.43246714014 4576488.44256104)'::public.geometry);
+SELECT is((SELECT count(*)::integer FROM v_edit_inp_dscenario_valve WHERE setting = -901), 1, 'INSERT: v_edit_inp_dscenario_valve -901 was inserted');
+SELECT is((SELECT count(*)::integer FROM inp_dscenario_valve WHERE setting = -901), 1, 'INSERT: inp_dscenario_valve -901 was inserted');
 
-UPDATE v_edit_inp_dscenario_valve SET status = 'ACTIVE' WHERE pressure = -901;
-SELECT is((SELECT status FROM v_edit_inp_dscenario_valve WHERE pressure = -901), 'ACTIVE', 'UPDATE: v_edit_inp_dscenario_valve -901 was updated');
-SELECT is((SELECT status FROM inp_dscenario_valve WHERE pressure = -901), 'ACTIVE', 'UPDATE: inp_dscenario_valve -901 was updated');
+UPDATE v_edit_inp_dscenario_valve SET status = 'ACTIVE' WHERE setting = -901;
+SELECT is((SELECT status FROM v_edit_inp_dscenario_valve WHERE setting = -901), 'ACTIVE', 'UPDATE: v_edit_inp_dscenario_valve -901 was updated');
+SELECT is((SELECT status FROM inp_dscenario_valve WHERE setting = -901), 'ACTIVE', 'UPDATE: inp_dscenario_valve -901 was updated');
 
-DELETE FROM v_edit_inp_dscenario_valve WHERE pressure = -901;
-SELECT is((SELECT count(*)::integer FROM v_edit_inp_dscenario_valve WHERE pressure = -901), 0, 'DELETE: v_edit_inp_dscenario_valve -901 was deleted');
-SELECT is((SELECT count(*)::integer FROM inp_dscenario_valve WHERE pressure = -901), 0, 'DELETE: inp_dscenario_valve -901 was deleted');
+DELETE FROM v_edit_inp_dscenario_valve WHERE setting = -901;
+SELECT is((SELECT count(*)::integer FROM v_edit_inp_dscenario_valve WHERE setting = -901), 0, 'DELETE: v_edit_inp_dscenario_valve -901 was deleted');
+SELECT is((SELECT count(*)::integer FROM inp_dscenario_valve WHERE setting = -901), 0, 'DELETE: inp_dscenario_valve -901 was deleted');
 
 
 SELECT * FROM finish();
