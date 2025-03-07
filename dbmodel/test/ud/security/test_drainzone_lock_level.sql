ad10ec62afe3ec43e5455f5749054d8dfeb6132c
Daniel Marin <dmarin@bgeo.es>
2025-03-07 13:08:38 +0100

    fix(test): check lock_level consistency

--- dbmodel/test/ud/security/test_drainzone_lock_level.sql
+++ dbmodel/test/ud/security/test_drainzone_lock_level.sql
@@ -12,6 +12,9 @@ SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
 SELECT * FROM no_plan();
 
+-- ===========================
+-- PREPARE DATA
+-- ===========================
 INSERT INTO drainzone (drainzone_id, name) VALUES (-999, 'Drainzone999');
 INSERT INTO drainzone (drainzone_id, name, lock_level) VALUES (-998, 'Drainzone998', 0);
 INSERT INTO drainzone (drainzone_id, name, lock_level) VALUES (-997, 'Drainzone997', 1);
@@ -20,15 +23,17 @@ INSERT INTO drainzone (drainzone_id, name, lock_level) VALUES (-995, 'Drainzone9
 
 REVOKE UPDATE ON TABLE drainzone FROM role_edit;
 GRANT UPDATE (drainzone_id, name, expl_id, descript, the_geom, link, graphconfig, stylesheet, active, tstamp, insert_user, lastupdate, lastupdate_user, drainzone_type) ON drainzone TO role_edit;
+GRANT UPDATE (lock_level) ON drainzone TO role_admin;
 
 CREATE USER admin_user;
-GRANT role_system to admin_user;
+GRANT role_admin to admin_user;
 
 CREATE USER normal_user;
 GRANT role_edit to normal_user;
 
-
+-- ===========================
 -- normal_user
+-- ===========================
 SET role normal_user;
 
 -- lock_level is NULL
@@ -53,7 +58,6 @@ SELECT lives_ok(
     'Normal user can delete when lock_level = 0'
 );
 
-
 -- lock_level = 1
 SELECT throws_ok(
     'UPDATE drainzone set name=''zone'' WHERE drainzone_id=-997;',
@@ -67,7 +71,6 @@ SELECT lives_ok(
     'Normal user can delete when lock_level is 1'
 );
 
-
 -- lock_level = 2
 SELECT lives_ok(
     'UPDATE drainzone set name=''zone'' WHERE drainzone_id=-996;',
@@ -80,7 +83,6 @@ SELECT throws_ok(
     'Function: [gw_trg_edit_controls] - CANNOT DO THIS OPERATION BECAUSE THE LOCK LEVEL IS SET TO 2. HINT: PLEASE REVIEW THE LOCK LEVEL',
     'Normal user can not delete when lock_level is 2'
 );
-
 
 -- lock_level = 3
 SELECT throws_ok(
@@ -105,16 +107,14 @@ SELECT throws_ok(
     'Normal user can not update lock_level'
 );
 
-
-
-
-
-
-
-
-
+-- ===========================
 -- admin_user
+-- ===========================
 SET ROLE admin_user;
+
+-- recreate the drainzone with lock_level = 1 deleted by normal_user
+INSERT INTO drainzone (drainzone_id, name, lock_level) VALUES (-997, 'Drainzone997', 1);
+
 -- lock_level is NULL
 SELECT lives_ok(
     'UPDATE drainzone set name=''zone'' WHERE drainzone_id=-999;',
@@ -137,7 +137,6 @@ SELECT lives_ok(
     'Admin user can delete when lock_level = 0'
 );
 
-
 -- lock_level = 1
 SELECT throws_ok(
     'UPDATE drainzone set name=''zone'' WHERE drainzone_id=-997;',
@@ -151,7 +150,6 @@ SELECT lives_ok(
     'Admin user can delete when lock_level is 1'
 );
 
-
 -- lock_level = 2
 SELECT lives_ok(
     'UPDATE drainzone set name=''zone'' WHERE drainzone_id=-996;',
@@ -164,7 +162,6 @@ SELECT throws_ok(
     'Function: [gw_trg_edit_controls] - CANNOT DO THIS OPERATION BECAUSE THE LOCK LEVEL IS SET TO 2. HINT: PLEASE REVIEW THE LOCK LEVEL',
     'Admin user can not delete when lock_level is 2'
 );
-
 
 -- lock_level = 3
 SELECT throws_ok(
@@ -188,9 +185,6 @@ SELECT lives_ok(
 );
 
 
-
-
 SELECT * FROM finish();
-
 
 ROLLBACK;
