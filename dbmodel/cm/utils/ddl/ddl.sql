768aa1b1874262fcac57779b2d5298b6869ee0dd
xtorret@bgeo.es <xtorret@bgeo.es>
2025-03-10 23:45:33 +0100

    cm(wip): Datamodel adjustment

--- dbmodel/cm/utils/ddl/ddl.sql
+++ dbmodel/cm/utils/ddl/ddl.sql
@@ -9,16 +9,58 @@ CREATE SCHEMA "SCHEMA_NAME";
 
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
-CREATE TABLE cat_team
+
+CREATE TABLE cat_organitzation
 (
   id serial NOT NULL,
   idval text,
   descript text,
   active boolean DEFAULT true,
-  CONSTRAINT cat_team_pkey PRIMARY KEY (id)
+  CONSTRAINT cat_organitzation_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE om_visit_lot
+
+CREATE TABLE cat_team
+(
+  id serial NOT NULL,
+  idval text,
+  organitzation_id text,
+  descript text,
+  active boolean DEFAULT true,
+  CONSTRAINT cat_team_pkey PRIMARY KEY (id, organitzation_id)
+);
+
+
+
+CREATE TABLE om_team_x_user
+(
+  id serial NOT NULL,
+  user_id character varying(50),
+  team_id integer,
+  CONSTRAINT om_team_x_user_pkey PRIMARY KEY (id)
+);
+
+
+CREATE TABLE om_campaignclass
+(
+  id serial NOT NULL,
+  idval text,
+  descript text,
+  active boolean DEFAULT true,
+  CONSTRAINT om_campaignclass_pkey PRIMARY KEY (id)
+);
+
+
+CREATE TABLE campaignclass_x_layer
+(
+  campaignclass_id integer NOT NULL,
+  layer text NOT NULL,
+  active boolean DEFAULT true,
+  CONSTRAINT cat_organitzation_pkey PRIMARY KEY (visitclass_id, layer)
+);
+
+
+CREATE TABLE om_campaign
 (
   id serial NOT NULL,
   startdate date DEFAULT now(),
@@ -26,6 +68,80 @@ CREATE TABLE om_visit_lot
   real_startdate date,
   real_enddate date,
   visitclass_id integer,
+  descript text,
+  active boolean DEFAULT true,
+  organitzation_id integer,
+  duration text,
+  status integer,
+  the_geom geometry(MultiPolygon,SRID_VALUE),
+  rotation numeric(8,4),
+  exercise integer,
+  serie character varying(10),
+  address text,
+  CONSTRAINT om_visit_campaign_pkey PRIMARY KEY (id)
+);
+
+
+CREATE TABLE selector_campaign
+(
+  id serial NOT NULL,
+  campaing_id integer,
+  cur_user text DEFAULT "current_user"(),
+  CONSTRAINT selector_campaign_pkey PRIMARY KEY (id)
+);
+
+
+CREATE TABLE om_campaign_x_arc
+(
+  campaing_id integer NOT NULL,
+  arc_id character varying(16) NOT NULL,
+  code character varying(30),
+  status integer,
+  observ text,
+  CONSTRAINT om_campaign_x_arc_pkey PRIMARY KEY (campaing_id, arc_id)
+);
+
+CREATE TABLE om_campaign_x_connec
+(
+  campaign_id integer NOT NULL,
+  connec_id character varying(16) NOT NULL,
+  code character varying(30),
+  status integer,
+  observ text,
+  CONSTRAINT om_campaign_x_connec_pkey PRIMARY KEY (campaign_id, connec_id)
+);
+
+
+CREATE TABLE om_campaign_x_link
+(
+  campaign_id integer NOT NULL,
+  link_id character varying(16) NOT NULL,
+  code character varying(30),
+  status integer,
+  observ text,
+  CONSTRAINT om_lot_x_link_pkey PRIMARY KEY (campaign_id, link_id)
+);
+
+
+CREATE TABLE om_campaign_x_node
+(
+  campaign_id integer NOT NULL,
+  node_id character varying(16) NOT NULL,
+  code character varying(30),
+  status integer,
+  observ text,
+  CONSTRAINT om_campaign_x_node_pkey PRIMARY KEY (campaign_id, node_id)
+);
+
+
+CREATE TABLE om_campaign_lot
+(
+  id serial NOT NULL,
+  startdate date DEFAULT now(),
+  enddate date,
+  real_startdate date,
+  real_enddate date,
+  campaign_id integer,
   descript text,
   active boolean DEFAULT true,
   team_id integer,
@@ -39,7 +155,7 @@ CREATE TABLE om_visit_lot
   serie character varying(10),
   "number" integer,
   address text,
-  CONSTRAINT om_visit_lot_pkey PRIMARY KEY (id)
+  CONSTRAINT om_campaign_lot_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE selector_lot
@@ -50,42 +166,54 @@ CREATE TABLE selector_lot
   CONSTRAINT selector_lot_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE om_visit_lot_x_arc
+CREATE TABLE om_campaign_lot_x_arc
 (
   lot_id integer NOT NULL,
   arc_id character varying(16) NOT NULL,
   code character varying(30),
   status integer,
   observ text,
-  CONSTRAINT om_visit_lot_x_arc_pkey PRIMARY KEY (lot_id, arc_id)
+  CONSTRAINT om_campaign_lot_x_arc_pkey PRIMARY KEY (lot_id, arc_id)
 );
 
-CREATE TABLE om_visit_lot_x_connec
+CREATE TABLE om_campaign_lot_x_connec
 (
   lot_id integer NOT NULL,
   connec_id character varying(16) NOT NULL,
   code character varying(30),
   status integer,
   observ text,
-  CONSTRAINT om_visit_lot_x_connec_pkey PRIMARY KEY (lot_id, connec_id)
+  CONSTRAINT om_campaign_lot_x_connec_pkey PRIMARY KEY (lot_id, connec_id)
 );
 
-CREATE TABLE om_visit_lot_x_node
+
+CREATE TABLE om_campaign_lot_x_link
+(
+  lot_id integer NOT NULL,
+  link_id character varying(16) NOT NULL,
+  code character varying(30),
+  status integer,
+  observ text,
+  CONSTRAINT om_campaign_lot_x_link_pkey PRIMARY KEY (lot_id, link_id)
+);
+
+
+CREATE TABLE om_campaign_lot_x_node
 (
   lot_id integer NOT NULL,
   node_id character varying(16) NOT NULL,
   code character varying(30),
   status integer,
   observ text,
-  CONSTRAINT om_visit_lot_x_node_pkey PRIMARY KEY (lot_id, node_id)
+  CONSTRAINT om_campaign_lot_x_node_pkey PRIMARY KEY (lot_id, node_id)
 );
 
 CREATE TABLE config_visit_class_x_workorder
 (
   visitclass_id integer NOT NULL,
-  wotype_id character varying(50) NOT NULL,
+  workorder_type character varying(50) NOT NULL,
   active boolean DEFAULT true,
-  CONSTRAINT config_visit_class_x_workorder_pkey PRIMARY KEY (visitclass_id, wotype_id)
+  CONSTRAINT config_visit_class_x_workorder_pkey PRIMARY KEY (visitclass_id, workorder_type)
 );
 
 CREATE TABLE ext_workorder_type
@@ -105,79 +233,17 @@ CREATE TABLE ext_workorder_class
 
 CREATE TABLE ext_workorder
 (
-  class_id integer NOT NULL,
-  class_name character varying(50),
+  workorder_id integer NOT NULL,
+  workorder_name character varying(50),
+  workorder_type character varying(50),
+  workorder_class character varying(200),
   exercise integer,
   serie character varying(10),
-  "number" integer,
   startdate date,
   address character varying(50),
-  wotype_id character varying(50),
   visitclass_id integer,
-  wotype_name character varying(120),
   observations text,
   cost numeric,
   ct text,
-  CONSTRAINT ext_workorder_pkey PRIMARY KEY (class_id)
-);
-
-CREATE TABLE om_visit_lot_x_user
-(
-  id serial NOT NULL,
-  user_id character varying(16) NOT NULL DEFAULT "current_user"(),
-  team_id integer NOT NULL,
-  lot_id integer NOT NULL,
-  starttime timestamp without time zone DEFAULT ("left"((date_trunc('second'::text, now()))::text, 19))::timestamp without time zone,
-  endtime timestamp without time zone,
-  the_geom geometry(Point,SRID_VALUE),
-  vehicle_id integer,
-  CONSTRAINT om_visit_lot_x_user_pkey PRIMARY KEY (id)
-);
-
-
-CREATE TABLE ext_cat_vehicle
-(
-  id character varying(50) NOT NULL,
-  idval character varying(50),
-  descript character varying(50),
-  model character varying(50),
-  number_plate character varying(50),
-  CONSTRAINT ext_cat_vehicle_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE om_team_x_vehicle
-(
-  id serial NOT NULL,
-  team_id integer,
-  vehicle_id character varying(50),
-  CONSTRAINT om_team_x_vehicle_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE om_vehicle_x_parameters
-(
-  id serial NOT NULL,
-  vehicle_id character varying(50),
-  lot_id integer,
-  team_id integer,
-  image text,
-  load character varying(50),
-  cur_user character varying(50) DEFAULT "current_user"(),
-  tstamp timestamp without time zone,
-  CONSTRAINT om_vehicle_x_parameters_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE om_team_x_visitclass
-(
-  id serial NOT NULL,
-  team_id integer,
-  visitclass_id integer,
-  CONSTRAINT om_team_x_visitclass_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE om_team_x_user
-(
-  id serial NOT NULL,
-  user_id character varying(50),
-  team_id integer,
-  CONSTRAINT om_team_x_user_pkey PRIMARY KEY (id)
+  CONSTRAINT ext_workorder_pkey PRIMARY KEY (workcat_id)
 );
