97533d5cc5bce835ed12ed9ed2869bb737707250
edgarfuste <34737125+edgarfuste@users.noreply.github.com>
2025-03-12 10:21:02 +0100

    Fix (tab_visit on feature form): Manage btn_open_gallery and open visit with double click on tableview

--- core/shared/visit.py
+++ core/shared/visit.py
@@ -1144,8 +1144,7 @@ class GwVisit(QObject):
                        f" FROM om_visit"
                        f" WHERE id = '{visit_id}' ")
                 id_visitcat = tools_db.get_row(sql)
-                if id_visitcat is not None:
-
+                if id_visitcat and str(id_visitcat[0]) not in (None, 'None'):
                     sql = (f"SELECT id, name"
                            f" FROM om_visit_cat"
                            f" WHERE active is true AND id = '{id_visitcat[0]}' "
@@ -1884,7 +1883,8 @@ class GwVisit(QObject):
         row = tools_db.get_row(sql)
         if row:
             parameter_id = row["parameter_id"]
-            feature_type = row["feature_type"].lower()
+            if row["feature_type"]:
+                feature_type = row["feature_type"].lower()
 
         return parameter_id, feature_type
 
