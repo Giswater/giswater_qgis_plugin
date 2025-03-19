6d90d20de6619931365c2d038ca295b630d98bfd
Arnau Torret <atorret@bgeo.es>
2025-03-19 10:20:15 +0100

    feat(i18n): Update translation files of en_us, es_es, es_cr.

---
+++ dbmodel/i18n/en_US/ud_dml.sql
@@ -0,0 +1,39 @@
+/*
+This file is part of Giswater
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association,
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+UPDATE config_form_fields SET formname_en_us = 've_node_chamber' WHERE formname_en_us LIKE 've_node_chamber';
+UPDATE config_form_fields SET formname_en_us = 've_arc_pump_pipe' WHERE formname_en_us LIKE 've_arc_pump_pipe';
+UPDATE config_form_fields SET formname_en_us = 've_arc_varc' WHERE formname_en_us LIKE 've_arc_varc';
+UPDATE config_form_fields SET formname_en_us = 've_node_wwtp' WHERE formname_en_us LIKE 've_node_wwtp';
+UPDATE config_form_fields SET formname_en_us = 've_connec_vconnec' WHERE formname_en_us LIKE 've_connec_vconnec';
+UPDATE config_form_fields SET formname_en_us = 've_node_pump_station' WHERE formname_en_us LIKE 've_node_pump_station';
+UPDATE config_form_fields SET formname_en_us = 've_gully_vgully' WHERE formname_en_us LIKE 've_gully_vgully';
+UPDATE config_form_fields SET formname_en_us = 've_node_outfall' WHERE formname_en_us LIKE 've_node_outfall';
+UPDATE config_form_fields SET formname_en_us = 've_arc_siphon' WHERE formname_en_us LIKE 've_arc_siphon';
+UPDATE config_form_fields SET formname_en_us = 've_node_overflow_storage' WHERE formname_en_us LIKE 've_node_overflow_storage';
+UPDATE config_form_fields SET formname_en_us = 've_gully_pgully' WHERE formname_en_us LIKE 've_gully_pgully';
+UPDATE config_form_fields SET formname_en_us = 've_node_valve' WHERE formname_en_us LIKE 've_node_valve';
+UPDATE config_form_fields SET formname_en_us = 've_node_netelement' WHERE formname_en_us LIKE 've_node_netelement';
+UPDATE config_form_fields SET formname_en_us = 've_node_virtual_node' WHERE formname_en_us LIKE 've_node_virtual_node';
+UPDATE config_form_fields SET formname_en_us = 've_node_weir' WHERE formname_en_us LIKE 've_node_weir';
+UPDATE config_form_fields SET formname_en_us = 've_node_register' WHERE formname_en_us LIKE 've_node_register';
+UPDATE config_form_fields SET formname_en_us = 've_node_change' WHERE formname_en_us LIKE 've_node_change';
+UPDATE config_form_fields SET formname_en_us = 've_arc_waccel' WHERE formname_en_us LIKE 've_arc_waccel';
+UPDATE config_form_fields SET formname_en_us = 've_node_circ_manhole' WHERE formname_en_us LIKE 've_node_circ_manhole';
+UPDATE config_form_fields SET formname_en_us = 've_node_netinit' WHERE formname_en_us LIKE 've_node_netinit';
+UPDATE config_form_fields SET formname_en_us = 've_node_rect_manhole' WHERE formname_en_us LIKE 've_node_rect_manhole';
+UPDATE config_form_fields SET formname_en_us = 've_arc_conduit' WHERE formname_en_us LIKE 've_arc_conduit';
+UPDATE config_form_fields SET formname_en_us = 've_connec_connec' WHERE formname_en_us LIKE 've_connec_connec';
+UPDATE config_form_fields SET formname_en_us = 've_node_sandbox' WHERE formname_en_us LIKE 've_node_sandbox';
+UPDATE config_form_fields SET formname_en_us = 've_gully_gully' WHERE formname_en_us LIKE 've_gully_gully';
+UPDATE config_form_fields SET formname_en_us = 've_node_highpoint' WHERE formname_en_us LIKE 've_node_highpoint';
+UPDATE config_form_fields SET formname_en_us = 've_node_sewer_storage' WHERE formname_en_us LIKE 've_node_sewer_storage';
+UPDATE config_form_fields SET formname_en_us = 've_node_jump' WHERE formname_en_us LIKE 've_node_jump';
+UPDATE config_form_fields SET formname_en_us = 've_node_junction' WHERE formname_en_us LIKE 've_node_junction';
+UPDATE config_form_fields SET formname_en_us = 've_node_netgully' WHERE formname_en_us LIKE 've_node_netgully';
