e4c2a62d8269436e7e50b64ed55098a50a037b51
Nico Pérez <nperez@bgeo.es>
2025-03-21 13:29:11 +0100

    feat(snapshot): create table to store snapshots and insert values when user do it one

---
+++ dbmodel/corporate/audit/activate/audit_snapshot_ddl.sql
@@ -0,0 +1,11 @@
+/*
+This file is part of Giswater
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+CREATE TABLE SCHEMA_NAME.snapshot (
+    date DATE PRIMARY KEY DEFAULT CURRENT_DATE,
+    descripcion TEXT NULL
+);
--- dbmodel/corporate/audit/activate/gw_fct_set_snapshot.sql
+++ dbmodel/corporate/audit/activate/gw_fct_set_snapshot.sql
@@ -6,7 +6,7 @@ This version of Giswater is provided by Giswater Association
 
 -- DROP FUNCTION SCHEMA_NAME.gw_fct_set_snapshot();
 
-CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_set_snapshot()
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_set_snapshot(p_description TEXT DEFAULT NULL)
  RETURNS integer
  LANGUAGE plpgsql
 AS $function$
@@ -17,11 +17,15 @@ v_schemaname text;
 v_child text;
 v_pk_name text;
 v_table_name text;
+v_date DATE := CURRENT_DATE;
 
 BEGIN
 	-- search path
 	SET search_path = "SCHEMA_NAME", public;
 	v_schemaname = 'SCHEMA_NAME';
+
+	-- Insert the first snapshot
+	INSERT INTO snapshot (date, descripcion) VALUES (v_date, p_description);
 
 	-- Get childs from cat_feature
 	FOR v_child IN (SELECT child_layer FROM PARENT_SCHEMA.cat_feature) LOOP
@@ -39,16 +43,19 @@ BEGIN
 			SELECT column_name INTO v_pk_name FROM information_schema.COLUMNS
 			WHERE table_schema = 'PARENT_SCHEMA' AND table_name = v_child LIMIT 1;
 
-			-- Add timestamp column at the end of the table
-            EXECUTE format('ALTER TABLE %I ADD COLUMN timestamp TIMESTAMP DEFAULT now()', v_table_name);
+			-- Add date column at the end of the table
+            EXECUTE format('ALTER TABLE %I ADD COLUMN date DATE ', v_table_name);
+
+			-- Add foreign key constraint to reference the primary key of snapshot
+			EXECUTE format('ALTER TABLE %I ADD CONSTRAINT fk_snapshot_date FOREIGN KEY (date) REFERENCES snapshot(date)', v_table_name);
 
 			-- Add primary keys
-            EXECUTE format('ALTER TABLE %I ADD PRIMARY KEY (timestamp, %I)', v_table_name, v_pk_name);
+            EXECUTE format('ALTER TABLE %I ADD PRIMARY KEY (date, %I)', v_table_name, v_pk_name);
 
 		END IF;
 
 		-- Insert current data and current date from v_child into v_table_name
-		EXECUTE format('INSERT INTO %I SELECT *, now() FROM PARENT_SCHEMA.%I', v_table_name, v_child);
+		EXECUTE format('INSERT INTO %I SELECT *, %L FROM PARENT_SCHEMA.%I', v_table_name, v_date, v_child);
 
     END LOOP;
 
@@ -57,6 +64,3 @@ END;
 
 $function$
 ;
-
--- Execute the function to do the first snapshot
-SELECT SCHEMA_NAME.gw_fct_set_snapshot();
