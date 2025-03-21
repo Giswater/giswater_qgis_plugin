4187418f8b7b6142c817b18c082ae5292e41d00c
Nico Pérez <nperez@bgeo.es>
2025-03-21 09:07:20 +0100

    feat(function): add new function 'gw_fct_set_snapshot' to do an snapshot on the current date and current water network data

---
+++ dbmodel/corporate/audit/activate/gw_fct_set_snapshot.sql
@@ -0,0 +1,60 @@
+/*
+Copyright © 2023 by BGEO. All rights reserved.
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+
+-- DROP FUNCTION SCHEMA_NAME.gw_fct_set_snapshot();
+
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_set_snapshot()
+ RETURNS integer
+ LANGUAGE plpgsql
+AS $function$
+
+DECLARE
+
+v_schemaname text;
+v_child text;
+v_pk_name text;
+v_table_name text;
+
+BEGIN
+	-- search path
+	SET search_path = "SCHEMA_NAME", public;
+	v_schemaname = 'SCHEMA_NAME';
+
+	-- Get childs from cat_feature
+	FOR v_child IN (SELECT child_layer FROM PARENT_SCHEMA.cat_feature) LOOP
+
+		-- Create table name
+		v_table_name := format('PARENT_SCHEMA_%I', v_child);
+
+		-- Check if table exists
+		IF NOT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = v_schemaname AND table_name = v_table_name) THEN
+
+			 -- Create empty table with the same columns structure
+            EXECUTE FORMAT('CREATE TABLE %I (LIKE PARENT_SCHEMA.%I INCLUDING ALL)', v_table_name, v_child);
+
+			-- Get primary key column name
+			SELECT column_name INTO v_pk_name FROM information_schema.COLUMNS
+			WHERE table_schema = 'PARENT_SCHEMA' AND table_name = v_child LIMIT 1;
+
+			-- Add timestamp column at the end of the table
+            EXECUTE format('ALTER TABLE %I ADD COLUMN timestamp TIMESTAMP DEFAULT now()', v_table_name);
+
+			-- Add primary keys
+            EXECUTE format('ALTER TABLE %I ADD PRIMARY KEY (timestamp, %I)', v_table_name, v_pk_name);
+
+		END IF;
+
+		-- Insert current data and current date from v_child into v_table_name
+		EXECUTE format('INSERT INTO %I SELECT *, now() FROM PARENT_SCHEMA.%I', v_table_name, v_child);
+
+    END LOOP;
+
+return 0;
+END;
+
+$function$
+;
