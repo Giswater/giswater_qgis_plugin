bf77fb7bf2f6ccf664eb72e8eb8d5f1b7737aa20
Nico Pérez <nperez@bgeo.es>
2025-03-28 12:21:27 +0100

    refractor: improve function to get all layers from snapshot

--- dbmodel/corporate/audit/activate/gw_fct_getsnapshot.sql
+++ dbmodel/corporate/audit/activate/gw_fct_getsnapshot.sql
@@ -18,17 +18,13 @@ v_version text;
 v_error_context text;
 v_date date;
 v_polygon text;
-v_features text[];
+v_selected_features text[];
 v_last_snapshot_date date;
 v_tables text[];
 v_table text;
 v_log record;
 v_result json;
 v_query text;
-v_test json;
-v_feature jsonb;
-v_point_list jsonb[];
-v_line_list jsonb[];
 temp_table_name text;
 snapshot_table_name text;
 v_counter integer;
@@ -36,6 +32,9 @@ v_columns text;
 v_values text;
 v_sql text;
 v_set_clause text;
+v_layer JSONB := '[]'::JSONB;
+v_features JSONB := '[]'::JSONB;
+v_geometry_type text;
 
 BEGIN
 	-- search path
@@ -47,21 +46,21 @@ BEGIN
 
 	v_date = ((p_data ->>'form')::json->>'date');
 	v_polygon = ((p_data ->>'form')::json->>'polygon');
-	v_features := ARRAY(SELECT * FROM jsonb_array_elements_text((p_data -> 'form' ->> 'features')::jsonb));
+	v_selected_features := ARRAY(SELECT * FROM jsonb_array_elements_text((p_data -> 'form' ->> 'features')::jsonb));
 
 	-- Get last snapshot date and tables
 	SELECT "date", tables INTO v_last_snapshot_date, v_tables
 	FROM "snapshot" WHERE "date" <= v_date ORDER BY "date" DESC LIMIT 1;
 
 	-- Loop throught tables of the last snapshot date
-	FOREACH v_table IN ARRAY v_tables LOOP
+	FOREACH v_table IN ARRAY COALESCE(v_tables, '{}') LOOP
 
 		-- Set table names
 		snapshot_table_name = FORMAT('PARENT_SCHEMA_%I', v_table);
 		temp_table_name := FORMAT('temp_%I', snapshot_table_name);
 
 		-- Create temporal table with values of the last snapshot
-		EXECUTE FORMAT('CREATE TABLE IF NOT EXISTS %I AS SELECT * FROM %I WHERE date = %L',
+		EXECUTE FORMAT('CREATE TABLE %I AS SELECT * FROM %I WHERE date = %L',
 					temp_table_name, snapshot_table_name, v_last_snapshot_date);
 
 		EXECUTE FORMAT ('ALTER TABLE %I DROP COLUMN IF EXISTS date', temp_table_name);
@@ -111,65 +110,41 @@ BEGIN
 			v_sql := regexp_replace(v_sql, '''(null|true|false|[0-9]+(\\.[0-9]+)?)''', '\1', 'g');
 			v_sql := REPLACE(v_sql, '"', '');
 
-			RAISE NOTICE '%', v_sql;
-
+			-- RAISE NOTICE '%', v_sql;
 			-- EXECUTE (v_sql);
 
 		END LOOP;
 
 		-- Build layers for each temp table updated
-		IF v_table ILIKE '%node%' THEN
-			EXECUTE FORMAT('SELECT jsonb_build_object(
-									''type'', ''Feature'',''geometry'', 
-									ST_AsGeoJSON(the_geom)::jsonb,
-		        					''properties'', to_jsonb(row) - ''the_geom'') 
-								    FROM (
-								        SELECT descript, the_geom
-								        FROM %I
-								    ) row;', temp_table_name) INTO v_feature;
-			IF v_feature IS NOT NULL THEN
-		       v_point_list := array_append(v_point_list, v_feature);
-			END IF;
-		ELSEIF v_table ILIKE '%arc%' THEN
-			EXECUTE FORMAT('SELECT jsonb_build_object(
-									''type'', ''Feature'',''geometry'', 
-									ST_AsGeoJSON(the_geom)::jsonb,
-		        					''properties'', to_jsonb(row) - ''the_geom'') 
-								    FROM (
-								        SELECT descript, the_geom
-								        FROM %I
-								    ) row;', temp_table_name) INTO v_feature;
-			IF v_feature IS NOT NULL THEN
-		       v_line_list := array_append(v_line_list, v_feature);
-			END IF;
-		END IF;
+		EXECUTE FORMAT('SELECT jsonb_agg(jsonb_build_object(''type'', ''Feature'',
+			           ''geometry'', ST_AsGeoJSON(the_geom)::jsonb,
+			        ''properties'', to_jsonb(row) - ''the_geom''
+			    )) FROM (SELECT the_geom FROM %I) row;', temp_table_name) INTO v_features;
+
+
+		v_geometry_type := CASE
+		WHEN v_table ILIKE '%node%' OR v_table ILIKE '%connec%' THEN 'Point'
+		ELSE 'LineString' END;
+
+		v_layer := COALESCE(v_layer, '[]'::jsonb) || jsonb_build_array(
+			    jsonb_build_object(
+		            'layerName', v_table,
+		            'features', v_features,
+		            'geometryType', v_geometry_type
+			    )
+		);
 
 		-- Delete temporal table
 		EXECUTE FORMAT('DROP TABLE %I', temp_table_name);
+
 	END LOOP;
 
-
-	SELECT jsonb_build_object(
-			'point', jsonb_build_object(
-		    	'features', v_point_list,
-				'geometryType', 'Point'
-			),'line', jsonb_build_object(
-		    	'features', v_line_list,
-				'geometryType', 'LineString'
-			)
-		) INTO v_result;
-
-
 	-- Return JSON
-	RETURN PARENT_SCHEMA.gw_fct_json_create_return(
-	    jsonb_build_object(
+	RETURN jsonb_build_object(
 	        'status', 'Accepted',
 	        'version', to_jsonb(v_version),
-	        'body', jsonb_build_object('data',v_result)
-	    )::json,
-	    3348, null, null, null
-	)::json;
-
+	        'body', jsonb_build_object('data', v_layer)
+	    );
 
 	EXCEPTION
 		WHEN OTHERS THEN
