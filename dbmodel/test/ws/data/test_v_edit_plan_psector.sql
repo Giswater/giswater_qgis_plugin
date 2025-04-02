823c2434953600e8c61b017aa8e28d08c3d9222a
Daniel Marin <dmarin@bgeo.es>
2025-04-02 11:55:26 +0200

    fix(test, e9c83d4): include new archived column

--- dbmodel/test/ws/data/test_v_edit_plan_psector.sql
+++ dbmodel/test/ws/data/test_v_edit_plan_psector.sql
@@ -12,8 +12,8 @@ SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
 SELECT plan(6);
 
-INSERT INTO v_edit_plan_psector ( "name", descript, priority, text1, text2, observ, rotation, "scale", atlas_id, gexpenses, vat, other, the_geom, expl_id, psector_type, active, ext_code, status, text3, text4, text5, text6, num_value, workcat_id, parent_id)
-VALUES('-901', 'Expanding the capacity of the pipes located on Francesc Layret street.', '2', NULL, NULL, 'Action caused by the headloss of the pipe.', 0.0000, 750.00, '01', 19.00, 21.00, 0.00, 'SRID=25831;MULTIPOLYGON (((419209.60503196524 4576553.424854076, 419209.60503196524 4576503.357122214, 419089.7020131364 4576503.357122214, 419089.7020131364 4576553.424854076, 419209.60503196524 4576553.424854076)))'::public.geometry, 1, 1, true, NULL, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO v_edit_plan_psector ( "name", descript, priority, text1, text2, observ, rotation, "scale", atlas_id, gexpenses, vat, other, the_geom, expl_id, psector_type, active, archived, ext_code, status, text3, text4, text5, text6, num_value, workcat_id, parent_id)
+VALUES('-901', 'Expanding the capacity of the pipes located on Francesc Layret street.', '2', NULL, NULL, 'Action caused by the headloss of the pipe.', 0.0000, 750.00, '01', 19.00, 21.00, 0.00, 'SRID=25831;MULTIPOLYGON (((419209.60503196524 4576553.424854076, 419209.60503196524 4576503.357122214, 419089.7020131364 4576503.357122214, 419089.7020131364 4576553.424854076, 419209.60503196524 4576553.424854076)))'::public.geometry, 1, 1, true, false, NULL, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 SELECT is((SELECT count(*)::integer FROM v_edit_plan_psector WHERE name = '-901'), 1, 'INSERT: v_edit_plan_psector -901 was inserted');
 SELECT is((SELECT count(*)::integer FROM plan_psector WHERE name = '-901'), 1, 'INSERT: plan_psector -901 was inserted');
 
