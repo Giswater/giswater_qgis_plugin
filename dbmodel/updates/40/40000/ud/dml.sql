03e0f21884d6355c1ef259c31c22edcfc54a7405
Daniel Marin <dmarin@bgeo.es>
2025-04-08 10:07:26 +0200

    fix(b93dff6): disable config_control on config_form_fields

--- dbmodel/updates/40/40000/ud/dml.sql
+++ dbmodel/updates/40/40000/ud/dml.sql
@@ -31,7 +31,6 @@ UPDATE sys_param_user SET dv_querytext='SELECT id AS id, id AS idval FROM cat_gu
 DELETE FROM sys_foreignkey WHERE typevalue_table='inp_typevalue' AND typevalue_name='inp_typevalue_snow' AND target_table='inp_snowpack' AND target_field='snow_type ';
 
 
-ALTER TABLE config_form_fields ENABLE TRIGGER gw_trg_config_control;
 --04/11/2024
 ALTER TABLE inp_typevalue DISABLE TRIGGER gw_trg_typevalue_config_fk;
 DELETE FROM inp_typevalue WHERE typevalue='inp_typevalue_snow' AND id='PLOWABLE';
