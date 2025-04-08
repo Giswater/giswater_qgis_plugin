787cc93eb340d518a71e79e8322e237459a3a94f
xtorret@bgeo.es <xtorret@bgeo.es>
2025-04-08 20:16:33 +0200

    fix(from prev commit): Minor bug fix from prev commit
    (cherry picked from commit c08514754f7a754d50da3fdd3988b151386c0435)

--- dbmodel/updates/36/36019/utils/tablect.sql
+++ dbmodel/updates/36/36019/utils/tablect.sql
@@ -20,3 +20,19 @@ ALTER TABLE arc alter column muni_id set default 0;
 ALTER TABLE node alter column muni_id set default 0;
 ALTER TABLE connec alter column muni_id set default 0;
 ALTER TABLE link alter column muni_id set default 0;
+
+DROP RULE IF EXISTS dma_undefined ON dma;
+DROP RULE IF EXISTS dma_conflict ON dma;
+update dma set macrodma_id = 0 where macrodma_id is null;
+ALTER TABLE dma alter column macrodma_id set NOT NULL;
+ALTER TABLE dma alter column macrodma_id set default 0;
+CREATE RULE dma_conflict AS ON UPDATE TO dma WHERE ((new.dma_id = -1) OR (old.dma_id = -1)) DO INSTEAD NOTHING;
+CREATE RULE dma_undefined AS ON UPDATE TO dma WHERE ((new.dma_id = 0) OR (old.dma_id = 0)) DO INSTEAD NOTHING;;
+
+update sector set macrosector_id = 0 where macrosector_id is null;
+ALTER TABLE sector alter column macrosector_id set NOT NULL;
+ALTER TABLE sector alter column macrosector_id set default 0;
+
+update exploitation set macroexpl_id = 0 where macroexpl_id is null;
+ALTER TABLE exploitation alter column macroexpl_id set NOT NULL;
+ALTER TABLE exploitation alter column macroexpl_id set default 0;
