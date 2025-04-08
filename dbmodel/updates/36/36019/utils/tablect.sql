a357faa5d2a3f907823bd6a7fdaf7afc6e8615fc
Xavier Torret <xtorret@bgeo.es>
2025-04-08 12:12:46 +0200

    enah(muni_id): Set not null and default value on parent layers
    (cherry picked from commit beb8287241be3d66fa6deef083f1457eb19fd4be)

---
+++ dbmodel/updates/36/36019/utils/tablect.sql
@@ -0,0 +1,16 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+
+ALTER TABLE arc alter column muni_id set NOT NULL;
+ALTER TABLE node alter column muni_id set NOT NULL;
+ALTER TABLE connec alter column muni_id set NOT NULL;
+
+ALTER TABLE arc alter column muni_id set default 0;
+ALTER TABLE node alter column muni_id set default 0;
+ALTER TABLE connec alter column muni_id set default 0;
