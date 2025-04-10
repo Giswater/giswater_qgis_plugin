cfb6520e10205919d5f5541bd485e724c512fd01
Xavier Torret <xtorret@bgeo.es>
2025-04-10 12:29:20 +0200

    enhan(demand scenario): Export demand scenario when connec is over arc
    (cherry picked from commit b5010ffe02d099d354e65f483377e997583016a8)

--- dbmodel/ws/fct/ws_gw_fct_pg2epa_dscenario.sql
+++ dbmodel/ws/fct/ws_gw_fct_pg2epa_dscenario.sql
@@ -89,12 +89,13 @@ BEGIN
 		IF v_networkmode IN(1,2) THEN
 
 			-- demands for connec related to arcs
-			INSERT INTO temp_t_demand (dscenario_id, feature_id, demand, pattern_id, demand_type, source)
+		INSERT INTO temp_t_demand (dscenario_id, feature_id, demand, pattern_id, demand_type, source)
 			SELECT dscenario_id, node_1 AS node_id, d.demand/2 as demand, d.pattern_id, demand_type, source FROM temp_t_arc JOIN v_edit_inp_connec USING (arc_id)
-			JOIN inp_dscenario_demand d ON feature_id = connec_id WHERE dscenario_id IN (SELECT unnest(v_userscenario)) AND pjoint_type = 'ARC'
+			JOIN inp_dscenario_demand d ON feature_id = connec_id WHERE dscenario_id IN (SELECT unnest(v_userscenario)) AND pjoint_type in ('ARC', 'CONNEC')
 			UNION ALL
 			SELECT dscenario_id, node_2 AS node_id, d.demand/2 as demand, d.pattern_id, demand_type, source  FROM temp_t_arc JOIN v_edit_inp_connec USING (arc_id)
-			JOIN inp_dscenario_demand d ON feature_id = connec_id WHERE dscenario_id IN (SELECT unnest(v_userscenario)) AND pjoint_type = 'ARC';
+			JOIN inp_dscenario_demand d ON feature_id = connec_id WHERE dscenario_id IN (SELECT unnest(v_userscenario)) AND pjoint_type in ('ARC', 'CONNEC');
+
 
 			-- demands for connec related to nodes
 			INSERT INTO temp_t_demand (dscenario_id, feature_id, demand, pattern_id, demand_type, source)
