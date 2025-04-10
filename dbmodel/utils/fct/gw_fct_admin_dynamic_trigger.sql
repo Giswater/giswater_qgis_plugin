c08fb2954dbd73f88f6a6ea2183f6a95ae3019ae
mguzman14 <mguzman@bgeo.es>
2025-04-10 21:00:22 +0200

    fix(dynamic_trigger): fix query to get the origin table for each view's column

--- dbmodel/utils/fct/gw_fct_admin_dynamic_trigger.sql
+++ dbmodel/utils/fct/gw_fct_admin_dynamic_trigger.sql
@@ -38,6 +38,8 @@ v_value_pkey TEXT;
 v_column_pkey TEXT;
 v_json_new_data JSON;
 v_json_old_data JSON;
+v_parent_layer TEXT;
+v_feature_type TEXT;
 
 BEGIN
 	
@@ -46,14 +48,17 @@ BEGIN
 	v_value_pkey :=  ((p_data ->> 'data')::json->>'parameters')::json->> 'pkeyValue';
 
 	v_json_new_data :=  ((p_data ->> 'data')::json->>'parameters')::json->> 'jsonNewData';
-	v_json_new_data :=  ((p_data ->> 'data')::json->>'parameters')::json->> 'jsonNewData';
+	v_json_old_data :=  ((p_data ->> 'data')::json->>'parameters')::json->> 'jsonOldData';
 
 	v_action :=  ((p_data ->> 'data')::json->>'parameters')::json->> 'action';
 
+	SELECT parent_layer, lower(feature_type) INTO v_parent_layer, v_feature_type FROM cat_feature 
+	WHERE child_layer = v_origin_table;
 
 	DROP TABLE IF EXISTS temp_new_vals;
 
     CREATE TEMP TABLE temp_new_vals AS 
+	WITH aaa AS (
    		WITH aux AS (
    			SELECT 1 AS id, v_json_new_data AS js
    		), json_vals AS (
@@ -68,20 +73,28 @@ BEGIN
 			    AND c.view_name = v.viewname
 			WHERE v.viewname = v_origin_table
 			AND table_schema = CURRENT_SCHEMA 
-			)
-	SELECT v.*, c.table_name, NULL AS exec_order 
-	FROM json_vals v LEFT JOIN mapping_cols c USING (col);
+		)
+		SELECT v.*, c.table_name, NULL AS exec_order 
+		FROM json_vals v LEFT JOIN mapping_cols c USING (col)
+	), bbb AS (
+	select c.column_name AS col, c.table_name FROM information_schema.view_column_usage c
+	JOIN pg_views v ON c.view_schema = v.schemaname AND c.view_name = v.viewname
+	WHERE v.viewname = v_parent_layer AND table_schema = CURRENT_SCHEMA
+	)
+	SELECT CASE WHEN aaa.table_name = v_parent_layer THEN bbb.table_name ELSE aaa.table_name END AS table_name, 
+	aaa.col, aaa.val, aaa.exec_order FROM aaa LEFT JOIN bbb USING (col);
+
 
 
 	-- clean table
-	DELETE FROM temp_new_vals WHERE table_name ILIKE 'selector_%' OR table_name ILIKE 'v_%' OR table_name ILIKE '%ve_%';
-	DELETE FROM temp_new_vals WHERE table_name ILIKE 'cat_%';
+	DELETE FROM temp_new_vals WHERE table_name ILIKE 'selector_%' OR table_name ILIKE 'cat_%';
 
 
 	-- prepare table
 	UPDATE temp_new_vals SET val = st_astext(st_geomfromgeojson(val)) WHERE col = 'the_geom' AND val <> 'null';
+	UPDATE temp_new_vals SET val = quote_literal(val) WHERE col = 'the_geom';
 
-	UPDATE temp_new_vals SET exec_order = 1 WHERE table_name = 'element';
+	UPDATE temp_new_vals SET exec_order = 1 WHERE table_name = v_feature_type;
 	UPDATE temp_new_vals SET exec_order = 2 WHERE exec_order is null;
 
 	IF v_action = 'INSERT' THEN
