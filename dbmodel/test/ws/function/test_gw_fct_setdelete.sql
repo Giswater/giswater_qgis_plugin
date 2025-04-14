24558742ea1f829880ad94561099679abde11a0e
Daniel Marin <dmarin@bgeo.es>
2025-04-14 20:25:58 +0200

    fix(1b8ef9c, test): correct name on plan_psector
    - todo: rename audit columns

--- dbmodel/test/ws/function/test_gw_fct_setdelete.sql
+++ dbmodel/test/ws/function/test_gw_fct_setdelete.sql
@@ -15,7 +15,7 @@ SELECT plan(1);
 
 -- initial data is prepared before proceeding with the test
 INSERT INTO plan_psector (psector_id, "name", psector_type, descript, expl_id, priority, text1, text2, observ, rotation, "scale", atlas_id, gexpenses, vat, other, active,
-the_geom, enable_all, status, ext_code, text3, text4, text5, text6, num_value, workcat_id, parent_id, created_at, created_by, updated_at, updated_by)
+the_geom, enable_all, status, ext_code, text3, text4, text5, text6, num_value, workcat_id, parent_id, tstamp, insert_user, lastupdate, lastupdate_user)
 VALUES(9, 'Masterplan 09', 1, 'Psector 9, prepared in test', 1, '3', NULL, NULL, 'Action caused by the headloss of the pipe.', 90.0000, 750.00, '02', 19.00, 21.00, 0.00, true,
 'SRID=25831;MULTIPOLYGON (((419077.6619419637 4576838.663100324, 419077.6619419637 4576761.052890681, 419029.32346890226 4576761.052890681, 419029.32346890226 4576838.663100324, 419077.6619419637 4576838.663100324)))'::public.geometry,
 false, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2024-02-21 12:08:16.880', 'postgres', NULL, NULL);
