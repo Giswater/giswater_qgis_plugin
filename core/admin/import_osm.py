6cf07e84a5145ec96c71027651cf76a7379ad5da
Nico Pérez <nperez@bgeo.es>
2025-04-23 10:13:29 +0200

    style: replace headers from files

--- core/admin/import_osm.py
+++ core/admin/import_osm.py
@@ -1,5 +1,5 @@
 """
-Copyright © 2025 by BGEO. All rights reserved.
+This file is part of Giswater
 The program is free software: you can redistribute it and/or modify it under the terms of the GNU
 General Public License as published by the Free Software Foundation, either version 3 of the License,
 or (at your option) any later version.
