1485b9442e680113e3819287d53b8376f70b7d7f
mguzman14 <mguzman@bgeo.es>
2025-04-28 10:19:36 +0200

    fix(dynamic_trigger): enhance code and change data_type strategy to avoid performance loss

--- dbmodel/utils/fct/gw_fct_admin_dynamic_trigger.sql
+++ dbmodel/utils/fct/gw_fct_admin_dynamic_trigger.sql
@@ -47,7 +47,7 @@ BEGIN
 
 	SET search_path = "SCHEMA_NAME", public;
 	
-	v_origin_table :=  ((p_data ->> 'data')::json->>'parameters')::json->> 'originTable';
+	v_origin_table :=  ((p_data ->> 'data')::json->>'parameters')::json->> 'tgTableName';
 	v_sql_feature :=  ((p_data ->> 'data')::json->>'parameters')::json->> 'sqlFeature';
 	v_column_pkey := ((p_data ->> 'data')::json->>'parameters')::json->> 'pkeyColumn';
 	v_value_pkey :=  ((p_data ->> 'data')::json->>'parameters')::json->> 'pkeyValue';
@@ -62,7 +62,6 @@ BEGIN
 
 
 	DROP TABLE IF EXISTS temp_new_vals;
-
 
 	v_sql = '
     CREATE TABLE temp_new_vals AS 
@@ -83,45 +82,51 @@ BEGIN
 		SELECT v.*, c.table_name, NULL AS exec_order 
 		FROM json_vals v LEFT JOIN mapping_cols c USING (col)
 	), bbb AS (
-		select c.column_name AS col, c.table_name, lower(col_info.data_type) AS data_type FROM information_schema.view_column_usage c
+		select c.column_name AS col, c.table_name FROM information_schema.view_column_usage c
     	JOIN pg_views v ON c.view_schema = v.schemaname AND c.view_name = v.viewname
-    	JOIN information_schema.columns col_info ON c.table_name = col_info.table_name AND c.column_name = col_info.column_name
 	WHERE v.viewname = '||quote_literal(v_parent_layer)||' AND c.table_schema = CURRENT_SCHEMA
 	)
 	SELECT DISTINCT CASE WHEN aaa.table_name = '||quote_literal(v_parent_layer)||' THEN bbb.table_name ELSE aaa.table_name END AS table_name, 
-	aaa.col, aaa.val, bbb.data_type, aaa.exec_order FROM aaa LEFT JOIN bbb USING (col)
+	aaa.col, aaa.val, null data_type, aaa.exec_order FROM aaa LEFT JOIN bbb USING (col)
 	';
-	
+
 	EXECUTE v_sql;
 
-	-- clean table
+
+	-- discard non-updatable tables from INSERT/UPDATE statement
 	DELETE FROM temp_new_vals WHERE table_name ILIKE 'selector_%' OR table_name ILIKE 'cat_%';
 	DELETE FROM temp_new_vals WHERE table_name IS NULL;
 
 
-	-- prepare table
+	-- adjust geometry column
 	UPDATE temp_new_vals SET val = replace(val, '''', '"') WHERE col = 'the_geom' AND val <> 'null';
 	UPDATE temp_new_vals SET val = quote_literal(st_astext(st_geomfromgeojson(val))) WHERE col = 'the_geom' AND val <> 'null';
+	UPDATE temp_new_vals SET data_type = '::geometry' WHERE col = 'the_geom';
 
-	UPDATE temp_new_vals SET val = concat('''', val, ''''), data_type = 'text' WHERE data_type ILIKE '%ARRAY%';
-	UPDATE temp_new_vals SET data_type = 'geometry' WHERE col = 'the_geom';
+	-- adjust array columns	
+	UPDATE temp_new_vals SET val = REPLACE(REPLACE(val, '[', '{'), ']', '}') WHERE val ILIKE '[%' AND val ILIKE '%]';
+	UPDATE temp_new_vals SET val = concat('''', val, '''') WHERE val ILIKE '{%' AND val ILIKE '%}';
+	
+	-- set '' to data_type 
+	UPDATE temp_new_vals SET data_type = '' WHERE data_type IS NULL;
 
+
+	-- set execution order of INSERT statements
 	UPDATE temp_new_vals SET exec_order = 1 WHERE table_name = v_feature_type;
 	UPDATE temp_new_vals SET exec_order = 2 WHERE exec_order is null;
+
 
 	IF v_action = 'INSERT' THEN
 
 	    v_sql = 'SELECT concat(
 	    ''INSERT INTO '', table_name, '' ('', string_agg(col, '', ''), '') 
-    	VALUES ('', string_agg(concat(val, ''::'', data_type), '', ''), '')''
+    	VALUES ('', string_agg(concat(val, data_type), '', ''), '')''
 		) AS insert_sentence FROM temp_new_vals GROUP BY table_name, exec_order ORDER BY exec_order';
 	
 		v_sql = REPLACE(REPLACE(replace(v_sql, E'\n', ''), E'\t', ''), E'\r', '');
 	
-		
+		-- built INSERT statements of the necessary tables ordered by exec_order
 	    EXECUTE v_sql INTO v_rec_insert;
-			
-	   	SELECT json_agg(row_to_json(v_rec_insert)) INTO v_json_insert;
 	   
 	    FOR v_rec_sentence IN EXECUTE v_sql
 	    LOOP
@@ -130,14 +135,19 @@ BEGIN
 	      	       
 	    END LOOP;
 	   
+	   	-- keep results
+		SELECT json_agg(row_to_json(v_rec_insert)) INTO v_json_insert;
+	   
 	ELSIF v_action = 'UPDATE' THEN
 
 		v_sql = 'SELECT CONCAT(
-		''UPDATE '', table_name, '' SET '', string_agg(concat(col, '' = '', concat(val, ''::'', data_type)), '', ''), '' WHERE '', '||quote_literal(v_column_pkey)||', '' = '', '||quote_literal(quote_literal(v_value_pkey))||') AS update_sentence 
+		''UPDATE '', table_name, '' SET '', string_agg(concat(col, '' = '', val, data_type), '', ''), '' WHERE '', '||quote_literal(v_column_pkey)||', '' = '', '||quote_literal(quote_literal(v_value_pkey))||') AS update_sentence 
 		FROM temp_new_vals GROUP BY table_name, exec_order ORDER BY exec_order';
 		
 		v_sql = REPLACE(REPLACE(replace(v_sql, E'\n', ''), E'\t', ''), E'\r', '');
 	
+	
+		-- built UPDATE statements of the necessary tables ordered by exec_order
 		EXECUTE v_sql INTO v_rec_update;
 	
 		FOR v_rec_sentence IN EXECUTE v_sql
