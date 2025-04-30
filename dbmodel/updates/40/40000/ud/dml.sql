6bbac7602ada25bd7885fc0f2143ac135a40b8ec
Nico Pérez <nperez@bgeo.es>
2025-04-30 11:31:02 +0200

    fix: remove unnecessary btn filter expression

--- dbmodel/updates/40/40000/ud/dml.sql
+++ dbmodel/updates/40/40000/ud/dml.sql
@@ -2914,12 +2914,6 @@ INSERT INTO config_form_fields (formname, formtype, tabname, columnname, layoutn
   "functionName": "close",
   "module": "connect_link_btn"
 }'::json, NULL, false, 0);
-INSERT INTO config_form_fields (formname, formtype, tabname, columnname, layoutname, layoutorder, "datatype", widgettype, "label", tooltip, placeholder, ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytext_filterc, stylesheet, widgetcontrols, widgetfunction, linkedobject, hidden, web_layoutorder) VALUES('generic', 'link_to_gully', 'tab_none', 'btn_filter_expr', 'lyt_connect_link_2', 4, NULL, 'button', NULL, 'Filter expression', NULL, false, false, true, false, false, NULL, NULL, NULL, NULL, NULL, '{
-  "icon": "178"
-}'::json, NULL, '{
-  "functionName": "filter_expression",
-  "module": "connect_link_btn"
-}'::json, NULL, false, 0);
 INSERT INTO config_form_fields (formname, formtype, tabname, columnname, layoutname, layoutorder, "datatype", widgettype, "label", tooltip, placeholder, ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytext_filterc, stylesheet, widgetcontrols, widgetfunction, linkedobject, hidden, web_layoutorder) VALUES('generic', 'link_to_gully', 'tab_none', 'btn_remove', 'lyt_connect_link_2', 2, NULL, 'button', NULL, 'Remove', NULL, false, false, true, false, false, NULL, NULL, NULL, NULL, NULL, '{
   "icon": "112"
 }'::json, NULL, '{
