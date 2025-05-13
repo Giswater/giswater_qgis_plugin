8e0453c3231513804eca825b51fb20574e6f0f2a
smaspons <smaspons@bgeo.es>
2025-05-13 15:17:37 +0200

    fix(info): manage fields_reload variable

--- core/shared/info.py
+++ core/shared/info.py
@@ -1726,7 +1726,6 @@ class GwInfo(QObject):
         id_name = complet_result['body']['feature']['idName']
         newfeature_id = complet_result['body']['feature']['id']
         parent_fields = complet_result['body']['data']['parentFields']
-        fields_reload = ""
         list_mandatory = []
         # Manage autoupdate and mandatory widgets
         fields_reload = self._manage_autoupdate_and_mandatory_widgets(dialog, complet_result, p_widget, list_mandatory)
@@ -1889,6 +1888,7 @@ class GwInfo(QObject):
         return True
 
     def _manage_autoupdate_and_mandatory_widgets(self, dialog, complet_result, p_widget, list_mandatory):
+        fields_reload = ""
         for field in complet_result['body']['data']['fields']:
             if field.get('hidden', False):
                 continue
