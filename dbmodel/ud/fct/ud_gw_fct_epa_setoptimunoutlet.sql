90d153d0efdcfcb7428d8c8ec36c8959541b90e4
xtorret@bgeo.es <xtorret@bgeo.es>
2025-05-14 08:41:15 +0200

    enhan(setoptimunoulet): Make visible and enhance by using buffer and selection mode

--- dbmodel/ud/fct/ud_gw_fct_epa_setoptimunoutlet.sql
+++ dbmodel/ud/fct/ud_gw_fct_epa_setoptimunoutlet.sql
@@ -7,11 +7,13 @@ or (at your option) any later version.
 
 --FUNCTION CODE: 3242
 
-CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_epa_setoptimumoutlet(p_data json) RETURNS json AS
-$BODY$
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_epa_setoptimumoutlet(p_data json)
+ RETURNS json
+ LANGUAGE plpgsql
+AS $function$
 
 /*
-SELECT SCHEMA_NAME.gw_fct_epa_setoptimumoutlet($${"client":{"device":4, "infoType":1, "lang":"ES"},"data":{"filterFields":{}, "pageInfo":{}, "parameters":{"sector":21}}}$$)::text
+SELECT gw_fct_epa_setoptimumoutlet($${"client":{},"data":{"parameters":{"buffer":1000}}}$$)::text
 
 update inp_subcatchment SET outlet_id = null
 
@@ -27,6 +29,8 @@ fid: 495
 
 DECLARE
 
+v_selectionmode text;
+v_worklayer text;
 rec_node record;
 rec_subc record;
 v_id json;
@@ -39,11 +43,12 @@ v_error_context text;
 v_count integer;
 v_fid integer = 495;
 i integer = 0;
-v_sector integer;
+v_sector text;
 v_hydrology integer;
 v_name text;
 v_count1 integer;
 v_count2 integer;
+v_buffer numeric;
 
 BEGIN
 
@@ -52,16 +57,17 @@ BEGIN
 
 	-- select version
 	SELECT giswater INTO v_version FROM sys_version ORDER BY id DESC LIMIT 1;
+	
+	-- getting input data 	
+	v_buffer := ((p_data ->>'data')::json->>'parameters')::json->>'buffer';
+	v_id :=  ((p_data ->>'feature')::json->>'id')::json;
+	v_worklayer := ((p_data ->>'feature')::json->>'tableName')::text;
+	v_selectionmode :=  ((p_data ->>'data')::json->>'selectionMode')::text;
 
-	-- getting input data
-	v_sector := ((p_data ->>'data')::json->>'parameters')::json->>'sector';
-	IF v_sector = -999 THEN
-		v_sector = (SELECT replace (replace ((array_agg(sector_id))::text, '{', ''), '}', '') FROM v_edit_sector);
-	END IF;
-
+	-- controls for input data
+	v_buffer = coalesce (v_buffer, 1000);
 	select string_agg(quote_literal(a),',') into v_array from json_array_elements_text(v_id) a;
 
-	raise notice '0 - Reset';
 	TRUNCATE temp_node;
 	DELETE FROM anl_node WHERE cur_user="current_user"() AND fid=v_fid;
 	DELETE FROM anl_arc WHERE cur_user="current_user"() AND fid=v_fid;
@@ -70,38 +76,55 @@ BEGIN
 	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, concat('SET OPTIMUM OUTLET'));
 	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, '-------------------------------------------------------------');
 
-	INSERT INTO temp_node (node_id, the_geom, elev) SELECT node_id, the_geom, elev FROM v_edit_node
-	WHERE epa_type = 'JUNCTION' AND state > 0 AND sector_id = v_sector;
+	-- starting process
+	v_sector = (SELECT replace (replace ((array_agg(sector_id))::text, '{', ''), '}', '') FROM v_edit_sector);
 
-	SELECT value::integer INTO v_hydrology FROM config_param_user where parameter = 'inp_options_hydrology_current' and cur_user = current_user;
-	select name into v_name from cat_hydrology where hydrology_id = 11;
+	IF v_selectionmode = 'previousSelection' then 
+		if v_array is not null then	
+			EXECUTE 'INSERT INTO temp_node (node_id, the_geom, elev) SELECT node_id, the_geom, elev FROM v_edit_node 
+			WHERE epa_type = ''JUNCTION'' AND state > 0 AND sector_id in (select sector_id from v_edit_sector) and node_id in ('||v_array||')';
+		end if;
+	else
+		INSERT INTO temp_node (node_id, the_geom, elev) SELECT node_id, the_geom, elev FROM v_edit_node 
+		WHERE epa_type = 'JUNCTION' AND state > 0 AND sector_id in (select sector_id from v_edit_sector);
+	end if; 
+
+	SELECT value::integer INTO v_hydrology FROM config_param_user where parameter = 'inp_options_hydrology_scenario' and cur_user = current_user;
+	select name into v_name from cat_hydrology where hydrology_id = v_hydrology;
 
 	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 3, concat('SECTOR ID: ', v_sector));
 	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 4, concat('HYDROLOGY SCENARIO: ', v_name));
 
 	select count(*) into v_count1 from v_edit_inp_subcatchment where outlet_id is null;
 
-	FOR rec_subc IN SELECT * FROM inp_subcatchment WHERE hydrology_id = v_hydrology AND sector_id in (v_sector)
-	loop
+	if (select count(*) from temp_node) > 0 then 
 
-		raise notice 'loop rec_subc % ', rec_subc.subc_id;
+		FOR rec_subc IN SELECT * FROM inp_subcatchment WHERE hydrology_id = v_hydrology AND sector_id in (select sector_id from v_edit_sector)
+		loop
+			
+			raise notice 'loop rec_subc % ', rec_subc.subc_id;
+			
+			IF rec_subc.minelev IS NULL THEN
+				SELECT n.* INTO rec_node FROM temp_node n WHERE st_dwithin(rec_subc.the_geom, n.the_geom, v_buffer) 
+				ORDER BY st_distance(rec_subc.the_geom, n.the_geom) ASC LIMIT 1;
+			ELSE
+				SELECT n.* INTO rec_node FROM temp_node n WHERE st_dwithin(rec_subc.the_geom, n.the_geom, v_buffer) 
+				AND n.elev > rec_subc.minelev
+				ORDER BY st_distance(rec_subc.the_geom, n.the_geom) ASC LIMIT 1;
+			END IF;
+		
+			UPDATE inp_subcatchment SET outlet_id = rec_node.node_id WHERE hydrology_id = v_hydrology AND subc_id = rec_subc.subc_id;
+			
+		END LOOP;
+		
+		select count(*) into v_count2 from v_edit_inp_subcatchment where outlet_id is null;
+		INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 1, concat (v_count2-v_count1,' subcatchments have been updated with outlet values'));
 
-		IF rec_subc.minelev IS NULL THEN
-			SELECT n.* INTO rec_node FROM temp_node n WHERE st_dwithin(rec_subc.the_geom, n.the_geom, 5000)
-			ORDER BY st_distance(rec_subc.the_geom, n.the_geom) ASC LIMIT 1;
-		ELSE
-			SELECT n.* INTO rec_node FROM temp_node n WHERE st_dwithin(rec_subc.the_geom, n.the_geom, 5000)
-			AND n.elev > rec_subc.minelev
-			ORDER BY st_distance(rec_subc.the_geom, n.the_geom) ASC LIMIT 1;
-		END IF;
-
-		UPDATE inp_subcatchment SET outlet_id = rec_node.node_id WHERE hydrology_id = v_hydrology AND subc_id = rec_subc.subc_id;
-
-	END LOOP;
-
-	select count(*) into v_count2 from v_edit_inp_subcatchment where outlet_id is null;
-	INSERT INTO audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, null, 1, concat (v_count1-v_count2,' subcatchments have been updated with outlet values'));
-
+	else
+		INSERT INTO audit_check_data (fid, result_id, criticity, error_message) 
+		VALUES (v_fid, null, 1, concat ('0 subcatchments have been updated with outlet values'));
+	end if;
+	
 	-- get results
 	-- info
 	SELECT array_to_json(array_agg(row_to_json(row))) INTO v_result
@@ -120,6 +143,5 @@ BEGIN
 	    '}')::json, 4230, null, null, null);
 
 END;
-$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
+$function$
+;
---
+++ dbmodel/updates/36/36021/ud/dml.sql
@@ -0,0 +1,15 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+UPDATE config_csv SET descript='Function to assist the import of timeseries for inp models. The csv file must containts next columns on same position: timseries, timser_type, times_type, idval, expl_id, date, hour, time, value (fill date/hour for ABSOLUTE or time for RELATIVE)' WHERE fid=385;
+
+UPDATE sys_function set sys_role = 'role_epa', source = 'core' where id = 3242;
+
+UPDATE config_toolbox set functionparams = '{"featureType":["node"]}', 
+inputparams = '[{"widgetname":"buffer", "label":"Buffer (meters):","widgettype":"text","datatype":"float", "layoutname":"grl_option_parameters","layoutorder":3, "isMandatory":true, "value":null}]' 
+where id = 3242;
