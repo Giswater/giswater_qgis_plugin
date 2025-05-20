6446ab7868804799d5c217e4d1fda5ffcb5caec1
Daniel Marin <dmarin@bgeo.es>
2025-05-20 10:42:51 +0200

    fix(gw_trg): update feature_type handling in triggers for element management

--- dbmodel/utils/ftrg/gw_trg_ui_element.sql
+++ dbmodel/utils/ftrg/gw_trg_ui_element.sql
@@ -10,38 +10,61 @@ or (at your option) any later version.
 CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_trg_ui_element() RETURNS trigger AS $BODY$
 DECLARE
     feature_type text;
-	feature_column text;
+    feature_column text;
     element_table text;
-    v_sql text;
-    variable text;
+    v_sql_insert text;
+    v_sql_delete text;
+    v_new_value text;
     v_old_value text;
 BEGIN
 
     EXECUTE 'SET search_path TO '||quote_literal(TG_TABLE_SCHEMA)||', public';
-    feature_type:= TG_ARGV[0];
-    element_table:= 'element_x_'||feature_type;
-	feature_column:= feature_type||'_id';
+    feature_type := TG_ARGV[0];
+    element_table := 'element_x_'||feature_type;
+    feature_column := feature_type||'_id';
+
+    v_sql_insert := 'INSERT INTO '||element_table||' (element_id, '||feature_column||') VALUES ($1, $2)';
+    v_sql_delete := 'DELETE FROM '||element_table||' WHERE element_id = $1 AND '||feature_column||' = $2';
+
+    -- i want to print all new. values
+    RAISE NOTICE 'NEW: %', NEW;
 
     IF TG_OP = 'INSERT' THEN
 
-        --PERFORM gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
-        -- "data":{"message":"1", "function":"1140","parameters":null, "variables":null}}$$);
+        EXECUTE 'SELECT ($1).' || quote_ident(feature_column)
+            INTO v_new_value
+            USING NEW;
+
+        EXECUTE v_sql_insert USING NEW.element_id, v_new_value;
 
         RETURN NEW;
 
     ELSIF TG_OP = 'UPDATE' THEN
 
-        --PERFORM gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
-        -- "data":{"message":"2", "function":"1140","parameters":null, "variables":null}}$$);
+        EXECUTE 'SELECT ($1).' || quote_ident(feature_column)
+            INTO v_new_value
+            USING NEW;
+
+        EXECUTE 'SELECT ($1).' || quote_ident(feature_column)
+            INTO v_old_value
+            USING OLD;
+
+        IF v_new_value <> v_old_value THEN
+            EXECUTE v_sql_delete USING NEW.element_id, v_old_value;
+            EXECUTE v_sql_insert USING NEW.element_id, v_new_value;
+        ELSE
+            -- nothing to do
+        END IF;
+
         RETURN NEW;
 
     ELSIF TG_OP = 'DELETE' THEN
-		EXECUTE format('SELECT ($1).%I', feature_column)
-      	INTO v_old_value
-      	USING OLD;
 
-        v_sql:= 'DELETE FROM '||element_table||' WHERE element_id = '||quote_literal(OLD.element_id)||' AND	 '||feature_column||' = '||quote_literal(v_old_value)||';';
-        EXECUTE v_sql;
+        EXECUTE 'SELECT ($1).' || quote_ident(feature_column)
+            INTO v_old_value
+            USING OLD;
+
+        EXECUTE v_sql_delete USING OLD.element_id, v_old_value;
         RETURN NULL;
 
     END IF;
@@ -50,7 +73,3 @@ END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-
-
-
-
