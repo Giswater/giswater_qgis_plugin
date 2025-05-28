4c3cd3e18c6f944c8e023384357467f946866ff6
Ferran <fmartinez@bgeo.es>
2025-05-28 13:27:23 +0200

    feat(cm): create views and tables of cat_feature parent_schema

---
+++ dbmodel/cm/parent_schema/utils/ddl/ddl.sql
@@ -0,0 +1,64 @@
+/*
+This file is part of Giswater
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+-- Create tables and views related with cat_feature
+DO $$
+DECLARE
+  parent_s   CONSTANT text := 'PARENT_SCHEMA';
+  new_s      CONSTANT text := 'SCHEMA_NAME';
+  rec        RECORD;
+  tbl_name   text;
+  view_name  text;
+BEGIN
+
+  -- Create one empty table per feature, cloning structure of the view/table
+  FOR rec IN
+    SELECT id, child_layer
+      FROM   PARENT_SCHEMA.cat_feature
+  LOOP
+    tbl_name := format('%I.%I_%s',
+                       new_s,
+                       parent_s,
+                       lower(rec.id));
+
+    EXECUTE format(
+      'CREATE TABLE IF NOT EXISTS %s (
+           lot_id integer,
+           LIKE %I.%I INCLUDING ALL);',
+      tbl_name,
+      parent_s, rec.child_layer
+    );
+  END LOOP;
+
+  -- Create corresponding empty views named ve_<PARENT>_lot_<feature_id>
+  FOR rec IN
+    SELECT id
+      FROM   PARENT_SCHEMA.cat_feature
+  LOOP
+    view_name := format('%I.ve_%s_lot_%s',
+                        new_s,
+                        parent_s,
+                        lower(rec.id));
+
+    tbl_name := format('%I.%I_%s',
+                       new_s,
+                       parent_s,
+                       lower(rec.id));
+
+    EXECUTE format(
+      'CREATE OR REPLACE VIEW %s AS
+         SELECT * FROM %s
+        WHERE false;',
+      view_name,
+      tbl_name
+    );
+  END LOOP;
+
+END
+$$;
