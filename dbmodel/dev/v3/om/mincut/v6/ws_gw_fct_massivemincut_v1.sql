a4a72ba93a9f2d42dfec109f006bbdafe7122118
Daniel Marin <dmarin@bgeo.es>
2025-06-05 10:15:58 +0200

    wip: add new function gw_fct_massivemincut_v1 with new dialog

---
+++ dbmodel/dev/v3/om/mincut/v6/ws_gw_fct_massivemincut_v1.sql
@@ -0,0 +1,132 @@
+/*
+This file is part of Giswater
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+
+--FUNCTION CODE: 3424
+
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_massivemincut_v1(p_data json)
+  RETURNS json AS
+$BODY$
+
+/*
+
+*/
+
+DECLARE
+
+	-- system variables
+	v_version TEXT;
+	v_srid INTEGER;
+	v_project_type TEXT;
+
+	v_fid integer;
+
+	-- dialog variables
+	v_expl_id text;
+	v_expl_id_array text;
+	v_usepsector boolean;
+	v_recalculate_minsectors boolean;
+	v_commitchanges boolean;
+
+	v_level integer;
+	v_status text;
+	v_message text;
+
+	v_querytext text;
+	v_data json;
+
+	v_result text;
+	v_result_info json;
+	v_result_point json;
+	v_result_line json;
+	v_result_polygon json;
+
+	v_response JSON;
+
+
+BEGIN
+
+	-- Search path
+	SET search_path = "SCHEMA_NAME", public;
+
+    -- Select configuration values
+	SELECT giswater, epsg, UPPER(project_type) INTO v_version, v_srid, v_project_type FROM sys_version ORDER BY id DESC LIMIT 1;
+
+	v_expl_id := (SELECT ((p_data::json->>'data')::json->>'parameters')::json->>'exploitation');
+
+	v_usepsector := (SELECT ((p_data::json->>'data')::json->>'parameters')::json->>'usePlanPsector');
+	v_recalculate_minsectors := (SELECT ((p_data::json->>'data')::json->>'parameters')::json->>'recalculateMinsectors');
+	v_commitchanges := (SELECT ((p_data::json->>'data')::json->>'parameters')::json->>'commitChanges');
+
+
+	-- it's not allowed to commit changes when psectors are used
+ 	IF v_usepsector = 'true' THEN
+		v_commitchanges := 'false';
+	END IF;
+
+	-- if user select use psectors always recalculate minsectors, if not let user to choose
+	IF v_usepsector = 'true' THEN
+		v_recalculate_minsectors := 'true';
+	END IF;
+
+	-- MANAGE EXPL ARR
+    -- For user selected exploitations
+    IF v_expl_id = '-901' THEN
+        SELECT string_to_array(string_agg(DISTINCT expl_id::text, ','), ',') INTO v_expl_id_array
+		FROM selector_expl;
+    -- For all exploitations
+    ELSIF v_expl_id = '-902' THEN
+        SELECT string_to_array(string_agg(DISTINCT expl_id::text, ','), ',') INTO v_expl_id_array
+        FROM exploitation
+		WHERE active;
+    -- For a specific exploitation/s
+    ELSE
+		v_expl_id_array := string_to_array(v_expl_id, ',');
+    END IF;
+
+	-- Delete temporary tables
+	-- =======================
+	v_data := '{"data":{"action":"DROP", "fct_name":"MINCUT"}}';
+	SELECT gw_fct_graphanalytics_manage_temporary(v_data) INTO v_response;
+
+	-- Create temporary tables
+	-- =======================
+	v_data := '{"data":{"action":"CREATE", "fct_name":"MINCUT", "use_psector":"'|| v_usepsector ||'"}}';
+	SELECT gw_fct_graphanalytics_manage_temporary(v_data) INTO v_response;
+
+    IF v_response->>'status' <> 'Accepted' THEN
+        RETURN v_response;
+    END IF;
+
+	-- Start Building Log Message
+	-- =======================
+	INSERT INTO temp_audit_check_data (fid, error_message) VALUES (v_fid, concat('MASSIVE MINCUT - ', upper(v_class)));
+	INSERT INTO temp_audit_check_data (fid, error_message) VALUES (v_fid, concat('------------------------------------------------------------------'));
+	INSERT INTO temp_audit_check_data (fid, error_message) VALUES (v_fid, concat('Use psectors: ', upper(v_usepsector::text)));
+	INSERT INTO temp_audit_check_data (fid, error_message) VALUES (v_fid, concat('Recalculate minsectors: ', upper(v_recalculate_minsectors::text)));
+
+	INSERT INTO temp_audit_check_data (fid, error_message) VALUES (v_fid, concat(''));
+	INSERT INTO temp_audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, NULL, 3, 'ERRORS');
+	INSERT INTO temp_audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, NULL, 3, '-----------');
+	INSERT INTO temp_audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, NULL, 2, '');
+	INSERT INTO temp_audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, NULL, 2, 'WARNINGS');
+	INSERT INTO temp_audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, NULL, 2, '--------------');
+	INSERT INTO temp_audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, NULL, 1, 'INFO');
+	INSERT INTO temp_audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, NULL, 1, '-------');
+	INSERT INTO temp_audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, NULL, 0, 'DETAILS');
+	INSERT INTO temp_audit_check_data (fid, result_id, criticity, error_message) VALUES (v_fid, NULL, 0, '----------');
+
+	-- ALGORITHM
+
+
+
+
+
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
