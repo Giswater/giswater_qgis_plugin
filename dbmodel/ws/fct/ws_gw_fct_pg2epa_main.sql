71ea12535e518bc68ed79fbde2e064103d5ca66a
xtorret@bgeo.es <xtorret@bgeo.es>
2025-06-09 22:43:53 +0200

    feat(roughness for valves) : Update roughness for shortpipes and valves using neighbourg

--- dbmodel/ws/fct/ws_gw_fct_pg2epa_main.sql
+++ dbmodel/ws/fct/ws_gw_fct_pg2epa_main.sql
@@ -231,6 +231,14 @@ BEGIN
 		UPDATE temp_t_arc SET diameter = dint FROM (
 		SELECT node_2 as n2, diameter dint FROM temp_t_arc UNION SELECT node_2, diameter FROM temp_t_arc
 		)t WHERE t.dint IS NOT NULL AND t.n2 = node_2 AND diameter IS NULL;
+		
+		-- update roughness for shortpipes and valves using neighbourg
+		update temp_arc t set roughness = rough from (
+		select a.arc_id, (avg(a1.roughness))::numeric(12,3) as rough, 
+		a.epa_type from temp_arc a
+		left join temp_arc a1 on (a1.node_1 = a.node_1 or a1.node_2= a.node_1 or a1.node_1 = a.node_2 or a1.node_2= a.node_2)
+		where a.epa_type in ('SHORTPIPE', 'VALVE') and a.arc_id != a1.arc_id group by 1,3
+		) a where t.arc_id = a.arc_id;
 
 		-- other null values
 		UPDATE temp_t_arc SET minorloss = 0 WHERE minorloss IS NULL;
