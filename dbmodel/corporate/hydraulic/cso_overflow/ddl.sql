2348f21bc16cadd3fd8357aa2ed08a14307e632a
mguzman14 <mguzman@bgeo.es>
2025-06-09 18:34:43 +0200

    feat(cso): add tables for voronoi polygons

--- dbmodel/corporate/hydraulic/cso_overflow/ddl.sql
+++ dbmodel/corporate/hydraulic/cso_overflow/ddl.sql
@@ -25,9 +25,8 @@ CREATE TABLE cso_inp_system_subc (
 	CONSTRAINT unique_node_id_drainzone_id UNIQUE (node_id, drainzone_id)
 );
 
-
 CREATE TABLE cso_out_vol (
-	rowid serial4 NOT NULL,
+	rowid serial4 NOT NULL PRIMARY KEY,
 	node_id text NOT NULL,
 	drainzone_id text NULL,
 	rf_name text NULL,
@@ -51,12 +50,34 @@ CREATE TABLE cso_out_vol (
 	rf_intensity numeric(10,3) NULL
 );
 
+CREATE TABLE cso_subc_wwf (
+	node_id text NULL,
+	node_type text NULL,
+	drainzone_id int4 NULL,
+	lito_reclass text NULL,
+	usos_reclass text NULL,
+	slope_reclass text NULL,
+	cn_code text NULL,
+	cn_value int4 NULL,
+	po numeric(10,3) NULL,
+	c_value numeric(10,3) NULL,
+	ci_value numeric(10,3) NULL,
+	the_geom public.geometry(MULTIPOLYGON, SRID_VALUE) NULL
+	CONSTRAINT cso_subc_wwf_pkey PRIMARY KEY (node_id)
+);
+
+CREATE TABLE cso_subc_dwf (
+	node_id text NOT NULL,
+	node_type text NULL,
+	drainzone_id int4 NULL,
+	consumption numeric(10,3) NULL,
+	the_geom public.geometry(MULTIPOLYGON, SRID_VALUE) NULL
+	CONSTRAINT cso_subc_dwf_pkey PRIMARY KEY (node_id)
+);
+
+
+
 
 CREATE INDEX cso_out_vol_node_id ON cso_out_vol (node_id);
 CREATE INDEX cso_out_vol_rf_name ON cso_out_vol (rf_name);
 CREATE INDEX cso_out_vol_rf_tstep ON cso_out_vol (rf_tstep);
-
-
-
-
-
