52fca8f6f39d8408cf75d733ba31ac83a1bec462
Daniel Marin <dmarin@bgeo.es>
2025-06-10 10:37:12 +0200

    fix(config_form_fields): delete fluid_type from element forms

--- dbmodel/updates/40/40000/utils/dml.sql
+++ dbmodel/updates/40/40000/utils/dml.sql
@@ -2399,3 +2399,7 @@ DELETE FROM config_form_fields
 WHERE formname='generic' AND formtype='form_featuretype_change' AND columnname='fluid_type' AND tabname='tab_none';
 
 UPDATE config_form_fields SET iseditable = FALSE WHERE columnname = 'fluid_type';
+
+-- 10/06/2025
+-- remove fluid_type from element forms
+DELETE FROM config_form_fields WHERE formname ILIKE '%elem%' AND formtype = 'form_feature' AND columnname = 'fluid_type';
