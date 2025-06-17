d5d31ed62b148d1636ba5153f5abe3629174f0f7
Ferran <fmartinez@bgeo.es>
2025-06-17 15:33:19 +0200

    feat(cm): add new config_param_system var to select what create campaign options have

--- dbmodel/cm/base/dml.sql
+++ dbmodel/cm/base/dml.sql
@@ -21,6 +21,9 @@ INSERT INTO config_param_system VALUES ('basic_selector_tab_campaign',
 '{"table":"temp_om_campaign","selector":"selector_campaign","table_id":"campaign_id","selector_id":"campaign_id","label":"campaign_id, '' - '', name","orderBy":"campaign_id","manageAll":true,"query_filter":"","typeaheadFilter":" AND lower(concat(id'' - '', name))","selectionMode":"keepPreviousUsingShift", "orderbyCheck":false}',
 'Variable to configura all options related to search for the specificic tab','Selector variables',null, null, true, null, 'utils', null, null, 'json','text');
 
+INSERT INTO config_param_system ("parameter", value, descript, "label", dv_querytext, dv_filterbyfield, isenabled, layoutorder, project_type, dv_isparent, isautoupdate, "datatype", widgettype, ismandatory, iseditable, dv_orderby_id, dv_isnullvalue, stylesheet, widgetcontrols, placeholder, standardvalue, layoutname) 
+VALUES('create_campaign_type', '{"campaignReview":"true","campaignVisit":"false"}', 'Variable to specify wich type of campaign we whant to see when create', NULL, NULL, NULL, true, NULL, 'utils', NULL, NULL, 'json', 'text', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
 INSERT INTO config_form_tabs VALUES ('selector_campaign','tab_campaign','Campaign','Campaign','role_basic',null, null, 1, '{4}');
 INSERT INTO config_form_tabs VALUES ('selector_campaign','tab_lot','Lot','Lot','role_basic', null, null, 2,'{4}');
 
