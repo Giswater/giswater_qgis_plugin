cb6eb97bf11ff22498206ec2d8cae9b258f874b9
arnauurgeles <aurgeles@bgeo.es>
2025-06-20 11:58:17 +0200

    fix(dml): remove duplicated insert on sys_foreignkey

--- dbmodel/updates/40/40001/ud/dml.sql
+++ dbmodel/updates/40/40001/ud/dml.sql
@@ -498,7 +498,6 @@ INSERT INTO edit_typevalue (typevalue, id, idval, descript, addparam) VALUES('dw
 INSERT INTO edit_typevalue (typevalue, id, idval, descript, addparam) VALUES('dwfzone_type', '3', 'MIXED', NULL, NULL) ON CONFLICT DO NOTHING;
 
 INSERT INTO sys_foreignkey (typevalue_table, typevalue_name, target_table, target_field, parameter_id, active) VALUES('edit_typevalue', 'dwfzone_type', 'dwfzone', 'dwfzone_type', NULL, true) ON CONFLICT DO NOTHING;
-INSERT INTO sys_foreignkey (typevalue_table, typevalue_name, target_table, target_field, parameter_id, active) VALUES('edit_typevalue', 'dwfzone_type', 'drainzone', 'drainzone_type', NULL, true) ON CONFLICT DO NOTHING;
 
 -- 19/05/2025
 INSERT INTO sys_table (id, descript, sys_role, project_template, context, orderby, alias, notify_action, isaudit, keepauditdays, "source", addparam) VALUES('v_edit_dwfzone', 'Shows editable information about dwfzone.', 'role_edit', '{"template": [1]}', '{"level_1":"INVENTORY","level_2":"MAP ZONES"}', 1, 'DWFZONE', NULL, NULL, NULL, 'core', NULL);
