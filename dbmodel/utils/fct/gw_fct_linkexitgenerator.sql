0837ee3c5a9c83c7a71c528baee020486b469300
Daniel Marin <dmarin@bgeo.es>
2025-07-01 10:50:31 +0200

    fix(gw_fct_linkexitgenerator): refactor variable names for consistency and improve SQL queries for clarity

--- dbmodel/utils/fct/gw_fct_linkexitgenerator.sql
+++ dbmodel/utils/fct/gw_fct_linkexitgenerator.sql
@@ -25,76 +25,81 @@ p_input = 2 profiletool
 
 DECLARE
 
-v_link record;
-v_id integer;
-v_version text;
-v_projecttype text;
-v_tolerance double precision = 0.02;
-v_links record;
-v_links_2 record;
-v_geom public.geometry;
-v_loop record;
-v_node record;
-v_end_point public.geometry;
+	v_link record;
+	v_id integer;
+	v_version text;
+	v_project_type text;
+	v_tolerance double precision = 0.02;
+	v_links record;
+	v_links_2 record;
+	v_geom public.geometry;
+	v_loop record;
+	v_node record;
+	v_end_point public.geometry;
 
-BEGIN 
+BEGIN
 
 	SET search_path= 'SCHEMA_NAME','public';
 
 	-- select config values
-	SELECT project_type, giswater  INTO v_projecttype, v_version FROM sys_version ORDER BY id DESC LIMIT 1;
-	
+	SELECT project_type, giswater INTO v_project_type, v_version FROM sys_version ORDER BY id DESC LIMIT 1;
+
 	-- insert features temp_link
-	INSERT INTO temp_link SELECT link_id, link_id, exit_type, feature_id, feature_type, exit_id, exit_type, state, expl_id, 
-	sector_id, dma_id, exit_topelev, exit_elev, the_geom, st_endpoint(the_geom), false FROM v_edit_link where exit_type = 'ARC';
+	INSERT INTO temp_link SELECT link_id, link_id, exit_type, feature_id, feature_type, exit_id, exit_type, state, expl_id,
+	sector_id, top_elev2, elevation2, the_geom, st_endpoint(the_geom), false FROM v_edit_link where exit_type = 'ARC';
 
-	INSERT INTO temp_link SELECT link_id, exit_id::integer, exit_type, feature_id, feature_type, exit_id, exit_type, state, expl_id, 
-	sector_id, dma_id, exit_topelev, exit_elev, the_geom, st_endpoint(the_geom), false FROM v_edit_link where exit_type IN ('NODE', 'CONNEC');
+	IF v_project_type = 'WS' THEN
+		UPDATE temp_link SET dma_id = l.dma_id FROM v_edit_link l WHERE temp_link.link_id = l.link_id AND l.exit_type = 'ARC';
+	END IF;
 
+	INSERT INTO temp_link SELECT link_id, exit_id::integer, exit_type, feature_id, feature_type, exit_id, exit_type, state, expl_id,
+	sector_id, top_elev2, elevation2, the_geom, st_endpoint(the_geom), false FROM v_edit_link where exit_type IN ('NODE', 'CONNEC');
+
+	IF v_project_type = 'WS' THEN
+		UPDATE temp_link SET dma_id = l.dma_id FROM v_edit_link l WHERE temp_link.link_id = l.link_id AND l.exit_type IN ('NODE', 'CONNEC');
+	END IF;
 
 	-- insert duplicated features on temp_vnode
-	if p_input = 1 then -- the whole v_edit_link
+	IF p_input = 1 THEN -- the whole v_edit_link
 		INSERT INTO temp_vnode (l1,v1,l2,v2)
-		SELECT n1.link_id as l1, n1.vnode_id as v1, n2.link_id as l2, n2.vnode_id as v2 FROM temp_link n1, temp_link n2 
+		SELECT n1.link_id AS l1, n1.vnode_id AS v1, n2.link_id AS l2, n2.vnode_id AS v2 FROM temp_link n1, temp_link n2
 		WHERE st_dwithin(n1.the_geom_endpoint, n2.the_geom_endpoint, 0.02)
-		AND n1.link_id != n2.link_id and n1.exit_id = n2.exit_id ORDER BY 1; 
-
-	
-	elsif p_input = 2 then -- only those links wich are on arcs present on temp_anl_arc and nodes present temp_anl_node
+		AND n1.link_id != n2.link_id AND n1.exit_id = n2.exit_id ORDER BY 1;
+	ELSIF p_input = 2 THEN -- only those links wich are on arcs present on temp_anl_arc and nodes present temp_anl_node
 		INSERT INTO temp_vnode (l1,v1,l2,v2)
-		SELECT n1.link_id as l1, n1.vnode_id as v1, n2.link_id as l2, n2.vnode_id as v2 FROM temp_link n1, temp_link n2 
+		SELECT n1.link_id AS l1, n1.vnode_id AS v1, n2.link_id AS l2, n2.vnode_id AS v2 FROM temp_link n1, temp_link n2
 		WHERE st_dwithin(n1.the_geom_endpoint, n2.the_geom_endpoint, 0.02)
-		AND n1.link_id != n2.link_id 
-		and n1.feature_id in (select arc_id from temp_anl_arc union select node_id from temp_anl_node)
-		and n1.exit_id = n2.exit_id ORDER BY 1; 
-	end if;
+		AND n1.link_id != n2.link_id
+		AND n1.feature_id IN (SELECT arc_id FROM temp_anl_arc UNION SELECT node_id FROM temp_anl_node)
+		AND n1.exit_id = n2.exit_id ORDER BY 1;
+	END IF;
 
 	-- harmonize those links with same endpoint
 	FOR v_links IN SELECT * FROM temp_vnode order by 1
-	LOOP	
-		UPDATE temp_link a SET vnode_id = t.vnode_id, flag=true FROM temp_link t WHERE a.link_id = v_links.l1 AND t.link_id = v_links.l2 AND a.flag is false;					
+	LOOP
+		UPDATE temp_link a SET vnode_id = t.vnode_id, flag=true FROM temp_link t WHERE a.link_id = v_links.l1 AND t.link_id = v_links.l2 AND a.flag IS false;
 	END LOOP;
-	
+
 	truncate temp_vnode;
 	INSERT INTO temp_vnode (l1,v1,l2,v2)
-	SELECT n1.link_id , n1.vnode_id , n2.node_id::integer, n2.node_id::integer FROM temp_link n1, temp_node n2 join arc on node_id = node_1 
-	WHERE st_dwithin(n1.the_geom_endpoint, n2.the_geom, 0.01) and arc_id = exit_id AND exit_type = 'ARC' ORDER BY 3; 
+	SELECT n1.link_id, n1.vnode_id, n2.node_id::integer, n2.node_id::integer FROM temp_link n1, temp_node n2 JOIN arc ON node_id::integer = node_1::integer
+	WHERE st_dwithin(n1.the_geom_endpoint, n2.the_geom, 0.01) AND arc_id::integer = exit_id::integer AND exit_type = 'ARC' ORDER BY 3;
 
 	INSERT INTO temp_vnode (l1,v1,l2,v2)
-	SELECT n1.link_id , n1.vnode_id , n2.node_id::integer, n2.node_id::integer FROM temp_link n1, temp_node n2 join arc on node_id = node_2 
-	WHERE st_dwithin(n1.the_geom_endpoint, n2.the_geom, 0.01) and arc_id = exit_id AND exit_type = 'ARC' ORDER BY 3; 
+	SELECT n1.link_id, n1.vnode_id, n2.node_id::integer, n2.node_id::integer FROM temp_link n1, temp_node n2 JOIN arc ON node_id::integer = node_2::integer
+	WHERE st_dwithin(n1.the_geom_endpoint, n2.the_geom, 0.01) AND arc_id::integer = exit_id::integer AND exit_type = 'ARC' ORDER BY 3;
 
 
 	-- harmonize those links with same endpoint whith node
-	FOR v_links IN SELECT l1,v1, v2 FROM temp_vnode order by 1
-	LOOP	
-		UPDATE temp_link a SET vnode_id = v_links.v2, vnode_type = 'NODE' WHERE link_id = v_links.l1;
+	FOR v_links IN SELECT l1,v1, v2 FROM temp_vnode ORDER BY 1
+	LOOP
+		UPDATE temp_link a SET vnode_id = v_links.v2, vnode_type = 'NODE' WHERE link_id::integer = v_links.l1::integer;
 	END LOOP;
 
 	--  Return
 	RETURN 0;
-    
-END;  
+
+END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
