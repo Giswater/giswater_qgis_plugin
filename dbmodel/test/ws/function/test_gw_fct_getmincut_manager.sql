613d2958de13739d76fe225816019db4ce9fc6ae
Daniel Marin <dmarin@bgeo.es>
2025-07-02 10:02:11 +0200

    test(ws): add new test gw_fct_getmincut_manager

---
+++ dbmodel/test/ws/function/test_gw_fct_getmincut_manager.sql
@@ -0,0 +1,27 @@
+/*
+This file is part of Giswater
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+BEGIN;
+
+-- Suppress NOTICE messages
+SET client_min_messages TO WARNING;
+
+SET search_path = "SCHEMA_NAME", public, pg_catalog;
+
+SELECT plan(1);
+
+SELECT is(
+    (gw_fct_getmincut_manager($${"client":{"device":4, "lang":"es_ES", "version":"4.0.002", "infoType":1, "epsg":25831},
+    "form":{}, "feature":{}, "data":{"filterFields":{}, "pageInfo":{}}}$$)::JSON)->>'status',
+    'Accepted',
+    'Check if gw_fct_getmincut_manager returns status "Accepted"'
+);
+
+
+-- Finish the test
+SELECT finish();
+
+ROLLBACK;
