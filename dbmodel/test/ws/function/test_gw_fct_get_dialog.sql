14ea35620e33e6fd54625c53fc8849063033cd32
Daniel Marin <dmarin@bgeo.es>
2025-07-02 10:04:48 +0200

    test(ws): add a new test case for 'check_project'

--- dbmodel/test/ws/function/test_gw_fct_get_dialog.sql
+++ dbmodel/test/ws/function/test_gw_fct_get_dialog.sql
@@ -11,13 +11,20 @@ SET client_min_messages TO WARNING;
 
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
-SELECT plan(1);
+SELECT plan(2);
 
 SELECT is(
     (gw_fct_get_dialog($${"client": {"cur_user": "postgres", "device": 4, "lang": "es_ES", "infoType": 1, "epsg": "25831"},
     "form": {"formName": "generic", "formType": "link_to_connec"}, "feature": {}, "data": {"filterFields": {}, "pageInfo": {}}}$$)::JSON)->>'status',
     'Accepted',
     'Check if gw_fct_get_dialog (link_to_connec) returns status "Accepted"'
+);
+
+SELECT is(
+    (gw_fct_get_dialog($${"client":{"device":4, "lang":"es_ES", "version":"4.0.002", "infoType":1, "epsg":25831},
+    "form":{"formName":"generic","formType":"check_project"}, "feature":{}, "data":{"filterFields":{}, "pageInfo":{}}}$$)::JSON)->>'status',
+    'Accepted',
+    'Check if gw_fct_get_dialog (check_project) returns status "Accepted"'
 );
 
 
