4e01a57db2285bd6df9e4aa5518073bc4643f570
smaspons <smaspons@bgeo.es>
2025-07-08 13:38:53 +0200

    fix(featurechanges): change queries and use `gw_fct_getmessage`

--- dbmodel/utils/fct/gw_fct_featurechanges.sql
+++ dbmodel/utils/fct/gw_fct_featurechanges.sql
@@ -13,10 +13,10 @@ CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_featurechanges(p_data json)
 AS $function$
 
 /*EXAMPLE
-SELECT SCHEMA_NAME.gw_fct_featurechanges('{"client":{"device":6, "epsg":SRID_VALUE}, "feature":{"feature_type": "FEATURE"}, "data": {"action":"INSERT", "lastFeeding":"2024-11-11"}}');
-SELECT SCHEMA_NAME.gw_fct_featurechanges('{"client":{"device":6, "epsg":SRID_VALUE}, "feature":{"feature_type": "ELEMENT"}, "data": {"action":"INSERT", "lastFeeding":"2024-11-11"}}');
-SELECT SCHEMA_NAME.gw_fct_featurechanges('{"client":{"device":6, "epsg":SRID_VALUE}, "feature":{"feature_type": "FEATURE"}, "data": {"action":"UPDATE", "lastFeeding":"2024-11-11"}}');
-SELECT SCHEMA_NAME.gw_fct_featurechanges('{"client":{"device":6, "epsg":SRID_VALUE}, "feature":{"feature_type": "ELEMENT"}, "data": {"action":"UPDATE", "lastFeeding":"2024-11-11"}}');
+SELECT SCHEMA_NAME.gw_fct_featurechanges('{"client":{"device":5, "epsg":SRID_VALUE}, "feature":{"feature_type": "FEATURE"}, "data": {"action":"INSERT", "lastFeeding":"2024-11-11"}}');
+SELECT SCHEMA_NAME.gw_fct_featurechanges('{"client":{"device":5, "epsg":SRID_VALUE}, "feature":{"feature_type": "ELEMENT"}, "data": {"action":"INSERT", "lastFeeding":"2024-11-11"}}');
+SELECT SCHEMA_NAME.gw_fct_featurechanges('{"client":{"device":5, "epsg":SRID_VALUE}, "feature":{"feature_type": "FEATURE"}, "data": {"action":"UPDATE", "lastFeeding":"2024-11-11"}}');
+SELECT SCHEMA_NAME.gw_fct_featurechanges('{"client":{"device":5, "epsg":SRID_VALUE}, "feature":{"feature_type": "ELEMENT"}, "data": {"action":"UPDATE", "lastFeeding":"2024-11-11"}}');
 **/
 
 DECLARE
@@ -56,13 +56,13 @@ BEGIN
 					-- NODE
 					v_feature = 'node';
 			        v_select_node = 'SELECT '||v_feature||'_id as "'||lower(v_feature)||'Id", '||
-			                          lower(v_feature)||'cat_id as "featureClass", macrosector_code as "macrosectorCode", comment as "aresepId", asset_id as "assetId", state FROM v_edit_'||v_feature|| ' WHERE updated_at >= '''||v_lastfeeding||'''';
+			                          lower(v_feature)||'cat_id as "featureClass", macrosector_id as "macroSector", comment as "aresepId", asset_id as "assetId", state FROM v_edit_'||v_feature|| ' WHERE updated_at >= '''||v_lastfeeding||'''';
 
 					-- CONNEC
 					v_feature = 'connec';
 					v_select_connec = 'SELECT '||v_feature||'_id as "'||lower(v_feature)||'Id", '||
 					-- Combine the query parts and execute it
-			                          lower(v_feature)||'at_id as "featureClass", macrosector_code as "macrosectorCode", comment as "aresepId", asset_id as "assetId", state FROM v_edit_'||v_feature|| ' WHERE updated_at >= '''||v_lastfeeding||'''';
+			                          lower(v_feature)||'cat_id as "featureClass", macrosector_id as "macroSector", comment as "aresepId", asset_id as "assetId", state FROM v_edit_'||v_feature|| ' WHERE updated_at >= '''||v_lastfeeding||'''';
 					EXECUTE format('SELECT array_agg(row_to_json(a)) FROM (%s UNION ALL %s) a',
 					               v_select_node, v_select_connec)
 					INTO v_fields_array;
@@ -71,13 +71,13 @@ BEGIN
 					-- NODE
 					v_feature = 'node';
 			        v_select_element_node = 'SELECT element_id as "elementId", '||
-			                          'elementcat_id as featureClass, node_id as "nodeId", serial_number as "serialNumber", '||
+			                          'elementcat_id as "featureClass", node_id as "nodeId", serial_number as "serialNumber", '||
 			                          'brand_id as brand, model_id as model, descript as descript, comment as "aresepId" FROM vu_element_x_'||v_feature|| ' WHERE updated_at >= '''||v_lastfeeding||'''';
 
 					-- CONNEC
 					v_feature = 'connec';
 			        v_select_element_connec = 'SELECT element_id as "elementId", '||
-			                          'elementcat_id as featureClass, connec_id as "connecId", serial_number as "serialNumber", '||
+			                          'elementcat_id as "featureClass", connec_id as "connecId", serial_number as "serialNumber", '||
 					-- Combine the query parts and execute it
 			                          'brand_id as brand, model_id as model, descript as descript, comment as "aresepId" FROM vu_element_x_'||v_feature|| ' WHERE updated_at >= '''||v_lastfeeding||'''';
 					EXECUTE format('SELECT array_agg(row_to_json(a)) FROM (%s UNION ALL %s) a',
@@ -85,7 +85,8 @@ BEGIN
 					INTO v_fields_array;
 
 			    ELSE
-			        RAISE EXCEPTION 'The specified feature type is not supported: %', v_featuretype;
+					EXECUTE 'SELECT gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
+					"data":{"message":"4140", "function":"3374","parameters":{"feature_type":"'||v_featuretype::text||'"}}}$$);';
 			END CASE;
 		WHEN 'UPDATE' THEN
 			CASE v_featuretype
@@ -93,12 +94,12 @@ BEGIN
 					-- NODE
 					v_feature = 'node';
 					v_select_node = 'SELECT '||v_feature||'_id as "'||lower(v_feature)||'Id", '||
-				    				lower(v_feature)||'cat_id as "featureClass", macrosector_code as "macrosectorCode", comment as "aresepId", asset_id as "assetId", state FROM v_edit_'||v_feature|| ' WHERE updated_at >= '''||v_lastfeeding||''' and '||v_feature||'_id IN (SELECT newdata->>''node_id'' as id FROM audit.log WHERE exists (SELECT 1 FROM unnest(array[''macrosector_id'', ''state'', ''nodecat_id'', ''asset_id'']) AS key WHERE olddata->key IS NOT NULL AND newdata->key IS NOT NULL and table_name ilike ''%_element_x_%'' AND schema = ''SCHEMA_NAME''))';
+				    				lower(v_feature)||'cat_id as "featureClass", macrosector_id as "macroSector", comment as "aresepId", asset_id as "assetId", state FROM v_edit_'||v_feature|| ' WHERE updated_at >= '''||v_lastfeeding||''' and '||v_feature||'_id IN (SELECT newdata->>''node_id'' as id FROM audit.log WHERE exists (SELECT 1 FROM unnest(array[''macrosector_id'', ''state'', ''nodecat_id'', ''asset_id'']) AS key WHERE olddata->key IS NOT NULL AND newdata->key IS NOT NULL and table_name ilike ''%_element_x_%'' AND schema = ''SCHEMA_NAME''))';
 					-- CONNEC
 					v_feature = 'connec';
 					v_select_connec = 'SELECT '||v_feature||'_id as "'||lower(v_feature)||'Id", '||
 					-- Combine the query parts and execute it
-		                   			lower(v_feature)||'at_id as "featureClass", macrosector_code as "macrosectorCode", comment as "aresepId", asset_id as "assetId", state FROM v_edit_'||v_feature|| ' WHERE updated_at >= '''||v_lastfeeding||''' and '||v_feature||'_id IN (SELECT newdata->>''connec_id'' as id FROM audit.log WHERE exists (SELECT 1 FROM unnest(array[''macrosector_id'', ''state'', ''nodecat_id'', ''asset_id'']) AS key WHERE olddata->key IS NOT NULL AND newdata->key IS NOT NULL and table_name ilike ''%_element_x_%'' AND schema = ''SCHEMA_NAME''))';
+		                   			lower(v_feature)||'cat_id as "featureClass", macrosector_id as "macroSector", comment as "aresepId", asset_id as "assetId", state FROM v_edit_'||v_feature|| ' WHERE updated_at >= '''||v_lastfeeding||''' and '||v_feature||'_id IN (SELECT newdata->>''connec_id'' as id FROM audit.log WHERE exists (SELECT 1 FROM unnest(array[''macrosector_id'', ''state'', ''nodecat_id'', ''asset_id'']) AS key WHERE olddata->key IS NOT NULL AND newdata->key IS NOT NULL and table_name ilike ''%_element_x_%'' AND schema = ''SCHEMA_NAME''))';
 
 					EXECUTE format('SELECT array_agg(row_to_json(a)) FROM (%s UNION ALL %s) a',
 					               v_select_node, v_select_connec)
@@ -107,13 +108,13 @@ BEGIN
 					-- NODE
 					v_feature = 'node';
 			        v_select_element_node = 'SELECT element_id as "elementId", '||
-			                          'elementcat_id as featureClass, node_id as "nodeId", serial_number as "serialNumber", '||
+			                          'elementcat_id as "featureClass", node_id as "nodeId", serial_number as "serialNumber", '||
 			                          'brand_id as brand, model_id as model, descript as descript, comment as "aresepId" FROM vu_element_x_'||v_feature|| ' WHERE updated_at >= '''||v_lastfeeding||'''and element_id IN (SELECT newdata->>''node_id'' as id FROM audit.log WHERE exists (SELECT 1 FROM unnest(array[''macrosector_id'', ''state'', ''nodecat_id'', ''asset_id'']) AS key WHERE olddata->key IS NOT NULL AND newdata->key IS NOT NULL and table_name ilike ''%_'||v_feature||'%'' AND schema = ''SCHEMA_NAME''))';
 
 					-- CONNEC
 					v_feature = 'connec';
 			        v_select_element_connec = 'SELECT element_id as "elementId", '||
-			                          'elementcat_id as featureClass, connec_id as "connecId", serial_number as "serialNumber", '||
+			                          'elementcat_id as "featureClass", connec_id as "connecId", serial_number as "serialNumber", '||
 					-- Combine the query parts and execute it
 			                          'brand_id as brand, model_id as model, descript as descript, comment as "aresepId" FROM vu_element_x_'||v_feature|| ' WHERE updated_at >= '''||v_lastfeeding||'''and element_id IN (SELECT newdata->>''connec_id'' as id FROM audit.log WHERE exists (SELECT 1 FROM unnest(array[''macrosector_id'', ''state'', ''nodecat_id'', ''asset_id'']) AS key WHERE olddata->key IS NOT NULL AND newdata->key IS NOT NULL and table_name ilike ''%_'||v_feature||'%'' AND schema = ''SCHEMA_NAME''))';
 
@@ -122,20 +123,21 @@ BEGIN
 					INTO v_fields_array;
 
 			    ELSE
-			        RAISE EXCEPTION 'The specified feature type is not supported: %', v_featuretype;
+					EXECUTE 'SELECT gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
+					"data":{"message":"4140", "function":"3374","parameters":{"feature_type":"'||v_featuretype::text||'"}}}$$);';
 			END CASE;
 	END CASE;
 
 	v_fields := array_to_json(v_fields_array);
 
 	-- Control nulls
-	v_fields := COALESCE(v_fields, '{}');
+	v_fields := COALESCE(v_fields, '[]');
 
 	-- Return
 	RETURN ('{"status":"Accepted", "message":{"level":1, "text":"Process done sucessfully"}, "version":"'||v_version||'"'||
 			 ',"body":{"form":{}'||
-			 ',"feature":'||v_fields||''||
-			 ',"data":{}}'||'}')::json;
+			 ',"feature":{}'||
+			 ',"data":{"features":'||v_fields||'}}'||'}')::json;
 
 END;
 $function$
