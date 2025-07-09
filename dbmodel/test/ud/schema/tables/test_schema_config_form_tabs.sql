e33634ef430818f3d20c103f32505b991fdcf104
Marleen <marleenepskamp@live.nl>
2025-07-09 14:46:09 +0200

    test(config_form_tabs):create test for config_form_config

--- dbmodel/test/ud/schema/tables/test_schema_config_form_tabs.sql
+++ dbmodel/test/ud/schema/tables/test_schema_config_form_tabs.sql
@@ -14,29 +14,29 @@ SET search_path = "SCHEMA_NAME", public, pg_catalog;
 SELECT * FROM no_plan();
 
 --check if table exists
-SELECT has_table('config_form_tab'::name, 'Table config_form_tab should exist');
+SELECT has_table('config_form_tabs'::name, 'Table config_form_tabs should exist');
 
 -- check columns names 
 
 
 SELECT columns_are(
-    'config_form_tab',
+    'config_form_tabs',
     ARRAY[
       'formname', 'tabname', '"label"', 'tooltip', 'sys_role', 'tabfunction', 'tabactions', 'orderby', 'device'
     ],
-    'Table config_form_tab should have the correct columns'
+    'Table config_form_tabs should have the correct columns'
 
 );
 -- check columns names
-SELECT col_type_is('config_form_tab', 'formname', 'varchar(50)', 'Column formname should be varchar(50)');
-SELECT col_type_is('config_form_tab', 'tabname', 'text', 'Column tabname should be text');
-SELECT col_type_is('config_form_tab', 'label', 'text', 'Column label should be text');
-SELECT col_type_is('config_form_tab', 'tooltip', 'text', 'Column tooltip should be text');
-SELECT col_type_is('config_form_tab', 'sys_role', 'text', 'Column sys_role should be text');
-SELECT col_type_is('config_form_tab', 'tabfunction', 'json', 'Column tabfunction should be json');
-SELECT col_type_is('config_form_tab', 'tabactions', 'json', 'Column tabactions should be json');
-SELECT col_type_is('config_form_tab', 'orderby', 'int4', 'Column orderby should be int4');
-SELECT col_type_is('config_form_tab', 'device', '_int4', 'Column device should be _int4');
+SELECT col_type_is('config_form_tabs', 'formname', 'varchar(50)', 'Column formname should be varchar(50)');
+SELECT col_type_is('config_form_tabs', 'tabname', 'text', 'Column tabname should be text');
+SELECT col_type_is('config_form_tabs', 'label', 'text', 'Column label should be text');
+SELECT col_type_is('config_form_tabs', 'tooltip', 'text', 'Column tooltip should be text');
+SELECT col_type_is('config_form_tabs', 'sys_role', 'text', 'Column sys_role should be text');
+SELECT col_type_is('config_form_tabs', 'tabfunction', 'json', 'Column tabfunction should be json');
+SELECT col_type_is('config_form_tabs', 'tabactions', 'json', 'Column tabactions should be json');
+SELECT col_type_is('config_form_tabs', 'orderby', 'int4', 'Column orderby should be int4');
+SELECT col_type_is('config_form_tabs', 'device', '_int4', 'Column device should be _int4');
 
 
 --check default values
@@ -46,14 +46,14 @@ SELECT col_type_is('config_form_tab', 'device', '_int4', 'Column device should b
 
 
 -- check indexes
-SELECT has_index('config_form_tab', 'formname', 'Table config_form_tab should have index on formname');
-SELECT has_index('config_form_tab', 'tabname', 'Table config_form_tab should have index on tabname');
+SELECT has_index('config_form_tabs', 'formname', 'Table config_form_tabs should have index on formname');
+SELECT has_index('config_form_tabs', 'tabname', 'Table config_form_tabs should have index on tabname');
 
 
 
 --check trigger 
-SELECT has_trigger('config_form_tab', 'gw_trg_typevalue_fk_insert', 'Table config_form_tab should have trigger gw_trg_typevalue_fk_insert');
-SELECT has_trigger('config_form_tab', 'gw_trg_typevalue_fk_update', 'Table config_form_tab should have trigger gw_trg_typevalue_fk_update');
+SELECT has_trigger('config_form_tabs', 'gw_trg_typevalue_fk_insert', 'Table config_form_tabs should have trigger gw_trg_typevalue_fk_insert');
+SELECT has_trigger('config_form_tabs', 'gw_trg_typevalue_fk_update', 'Table config_form_tabs should have trigger gw_trg_typevalue_fk_update');
 --check rule 
 
 SELECT * FROM finish();
