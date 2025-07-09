44c3bd206b5a91265f4a83e1a340cf3dae78572d
Marleen <marleenepskamp@live.nl>
2025-07-09 15:11:20 +0200

    test(config_param_user):create test for config_param_user

---
+++ dbmodel/test/ud/schema/tables/test_schema_config_param_user.sql
@@ -0,0 +1,59 @@
+/*
+This file is part of Giswater
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+BEGIN;
+
+-- Suppress NOTICE messages
+SET client_min_messages TO WARNING;
+
+SET search_path = "SCHEMA_NAME", public, pg_catalog;
+
+SELECT * FROM no_plan();
+
+--check if table exists
+SELECT has_table('config_param_user'::name, 'Table config_param_user should exist');
+
+-- check columns names 
+
+
+SELECT columns_are(
+    'config_param_user',
+    ARRAY[
+      '"parameter"', 'value', 'cur_user'
+    ],
+    'Table config_param_user should have the correct columns'
+
+);
+-- check columns names
+
+SELECT col_type_is('config_param_user', 'parameter', 'varchar(50)', 'Column parameter should be varchar(50)');
+SELECT col_type_is('config_param_user', 'value', 'text', 'Column value should be text');
+SELECT col_type_is('config_param_user', 'cur_user', 'varchar(50)', 'Column cur_user should be varchar(50)');
+
+
+
+--check default values
+
+
+-- check foreign keys
+
+
+-- check indexes
+SELECT has_index('config_param_user', 'cur_user', 'Table config_param_user should have index on cur_user');
+SELECT has_index('config_param_user', 'parameter', 'Table config_param_user should have index on parameter');
+SELECT has_index('config_param_user', 'value', 'Table config_param_user should have index on value');
+
+
+
+
+
+--check trigger 
+
+--check rule 
+
+SELECT * FROM finish();
+
+ROLLBACK;
