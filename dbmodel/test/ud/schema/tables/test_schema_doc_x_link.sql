20225b1af6e5f1e7b6a33a897a3dba8869441948
Marleen <marleenepskamp@live.nl>
2025-07-10 09:55:56 +0200

    test(doc_x_node):create test for doc_x_node

--- dbmodel/test/ud/schema/tables/test_schema_doc_x_link.sql
+++ dbmodel/test/ud/schema/tables/test_schema_doc_x_link.sql
@@ -29,7 +29,7 @@ SELECT columns_are(
 );
 -- check columns names
 SELECT col_type_is('doc_x_link', 'doc_id', 'varchar(30)', 'Column doc_id should be varchar(30)');
-SELECT col_type_is('doc_x_link', 'link_id', 'int4', 'Column link_id should be varchar(30)');
+SELECT col_type_is('doc_x_link', 'link_id', 'int4', 'Column link_id should be int4');
 
 
 
---
+++ dbmodel/test/ud/schema/tables/test_schema_doc_x_node.sql
@@ -0,0 +1,56 @@
+/*
+This file is part of Giswater
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+BEGIN;
+
+-- Suppress NOTICE messages
+SET client_min_messages TO WARNING;
+
+SET search_path = "SCHEMA_NAME", public, pg_catalog;
+
+SELECT * FROM no_plan();
+
+--check if table exists
+SELECT has_table('doc_x_node'::name, 'Table doc_x_node should exist');
+
+-- check columns names 
+
+
+SELECT columns_are(
+    'doc_x_node',
+    ARRAY[
+      'doc_id', 'node_id'
+    ],
+    'Table doc_x_node should have the correct columns'
+
+);
+-- check columns names
+SELECT col_type_is('doc_x_node', 'doc_id', 'varchar(30)', 'Column doc_id should be varchar(30)');
+SELECT col_type_is('doc_x_node', 'node_id', 'int4', 'Column node_id should be int4');
+
+
+
+
+
+--check default values
+
+
+-- check foreign keys
+SELECT fk_ok('doc_x_node','doc_id','doc','id','Table should have foreign key from doc_id to doc.id');
+SELECT fk_ok('doc_x_node','node_id', 'node','node_id','Table should have foreign key from node_id to node.node_id');
+
+-- check indexes
+SELECT has_index('doc_x_node', 'doc_id', 'Table doc_x_node should have index on doc_id');
+SELECT has_index('doc_x_node', 'node_id', 'Table doc_x_node should have index on node_id');
+
+
+--check trigger 
+
+--check rule 
+
+SELECT * FROM finish();
+
+ROLLBACK;
