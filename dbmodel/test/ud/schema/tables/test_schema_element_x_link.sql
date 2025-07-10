62763547989ca7419ae5dde1ef0ac112c41af23c
tvdeelen <tvdeelen@bgeo.es>
2025-07-10 11:57:44 +0200

    test(element_x_link): create test for element_x_link

---
+++ dbmodel/test/ud/schema/tables/test_schema_element_x_link.sql
@@ -0,0 +1,49 @@
+/*
+This file is part of Giswater
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+BEGIN;
+
+-- Suppress NOTICE messages
+SET client_min_messages TO WARNING;
+
+SET search_path = "SCHEMA_NAME", public, pg_catalog;
+
+SELECT * FROM no_plan();
+
+-- Check table
+SELECT has_table('element_x_link'::name, 'Table element_x_link should exist');
+
+-- Check columns
+SELECT columns_are(
+    'element_x_link',
+    ARRAY[
+        'element_id', 'link_id'
+    ],
+    'Table element_x_link should have the correct columns'
+);
+
+-- Check primary key
+SELECT col_is_pk('element_x_link', 'element_id', 'Column element_id should be primary key'); 
+SELECT col_is_pk('element_x_link', 'link_id', 'Column link_id should be primary key'); 
+
+-- Check column types
+SELECT col_type_is('element_x_link', 'element_id', 'int4', 'Column element_id should be int4');
+SELECT col_type_is('element_x_link', 'link_id', 'int4', 'Column link_id should be int4');
+
+-- Check foreign keys
+SELECT has_fk('element_x_link', 'Table element_x_link should have foreign keys');
+
+SELECT fk_ok('element_x_link', 'element_id', 'element', 'element_id', 'Table should have foreign key from element_id to element.element_id');
+SELECT fk_ok('element_x_link', 'link_id', 'link', 'link_id', 'Table should have foreign key from link_id to link.link_id');
+
+-- Check indexes
+SELECT has_index('element_x_link', 'element_id', 'Table should have index on element_id');
+SELECT has_index('element_x_link', 'link_id', 'Table should have index on link_id');
+
+-- Finish
+SELECT * FROM finish();
+
+ROLLBACK;
