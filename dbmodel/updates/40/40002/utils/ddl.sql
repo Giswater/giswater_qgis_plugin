ce7901cb03faffa86e86c4b97a75397b5a0a4b80
Albert Bofill <abofill@bgeo.es>
2025-07-21 12:03:14 +0200

    feat(crm): add new column 'crm_number' on 'ext_rtc_hydrometer_x_data' to store values from the device where the data has been taken

--- dbmodel/updates/40/40002/utils/ddl.sql
+++ dbmodel/updates/40/40002/utils/ddl.sql
@@ -7,3 +7,5 @@ or (at your option) any later version.
 
 
 SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"ext_rtc_hydrometer_x_data", "column":"crm_number", "dataType":"text"}}$$);
