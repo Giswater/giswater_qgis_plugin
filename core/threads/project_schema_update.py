ac389861d0721a2181fceffb3041d5e17d8ca251
Daniel Marin <dmarin@bgeo.es>
2025-07-22 13:29:21 +0200

    fix(project_schema_update): remove unused import of os

--- core/threads/project_schema_update.py
+++ core/threads/project_schema_update.py
@@ -5,7 +5,6 @@ General Public License as published by the Free Software Foundation, either vers
 or (at your option) any later version.
 """
 # -*- coding: utf-8 -*-
-import os
 from qgis.PyQt.QtCore import Qt, pyqtSignal
 
 from .task import GwTask
