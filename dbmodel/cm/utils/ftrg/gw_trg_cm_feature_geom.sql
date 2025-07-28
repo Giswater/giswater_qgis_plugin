9e1fcb12200cc30d71d64e3f85c93448d023495d
Albert Bofill <abofill@bgeo.es>
2025-07-28 15:34:33 +0200

    feat(cm): enhance trg_cm_feature_geom also performing fct_cm_polygon_geom to update the polygon geometry

--- dbmodel/cm/utils/ftrg/gw_trg_cm_feature_geom.sql
+++ dbmodel/cm/utils/ftrg/gw_trg_cm_feature_geom.sql
@@ -18,19 +18,28 @@ DECLARE
 
 BEGIN
 	SELECT campaign_id INTO v_campaign FROM om_campaign_lot WHERE lot_id = NEW.lot_id;
+	
+	-- perform only when insert or the geometry is different
+	IF TG_OP = 'INSERT' OR (TG_OP = 'UPDATE' AND NOT ST_Equals(NEW.the_geom, OLD.the_geom)) THEN
 
-    IF v_feature = 'node' THEN
-        id_value := NEW.node_id;
-    ELSIF v_feature = 'arc' THEN
-        id_value := NEW.arc_id;
-    ELSIF v_feature = 'connec' THEN
-        id_value := NEW.connec_id;
-    END IF;
+	    IF v_feature = 'node' THEN
+	        id_value := NEW.node_id;
+	    ELSIF v_feature = 'arc' THEN
+	        id_value := NEW.arc_id;
+	    ELSIF v_feature = 'connec' THEN
+	        id_value := NEW.connec_id;
+	    END IF;
+		
+		-- update feature geometry on campaign_x_* table
+	    EXECUTE format('UPDATE om_campaign_x_%I SET the_geom = $1 
+	                    WHERE campaign_id = $2 AND %I = $3', 
+	                    v_feature, v_feature || '_id')
+	    USING NEW.the_geom, v_campaign, id_value;
+		
+		-- perform lot polygon geometry update
+		EXECUTE format('SELECT gw_fct_cm_polygon_geom(''{"id":%s, "name":"lot"}'')', NEW.lot_id);
 
-    EXECUTE format('UPDATE om_campaign_x_%I SET the_geom = $1 
-                    WHERE campaign_id = $2 AND %I = $3', 
-                    v_feature, v_feature || '_id')
-    USING NEW.the_geom, v_campaign, id_value;
+	END IF;
 
     RETURN NEW;
 END;
