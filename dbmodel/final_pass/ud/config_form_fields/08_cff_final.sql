4a0b50d62b6b254223897d269271d61d87572a5b
mguzman14 <mguzman@bgeo.es>
2025-08-05 17:12:41 +0200

    fix(example_data): fix dv_querytext for column "state"

--- dbmodel/final_pass/ud/config_form_fields/08_cff_final.sql
+++ dbmodel/final_pass/ud/config_form_fields/08_cff_final.sql
@@ -8,4 +8,18 @@ or (at your option) any later version.
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
 
+UPDATE config_form_fields SET dv_querytext = 'WITH check_value AS (
+  SELECT value::integer AS psector_value 
+  FROM config_param_user 
+  WHERE parameter = ''plan_psector_current''
+  AND cur_user = current_user
+)
+SELECT id, name as idval 
+FROM value_state 
+WHERE id IS NOT NULL 
+AND CASE 
+  WHEN (SELECT psector_value FROM check_value) IS NULL THEN id != 2 
+  ELSE id=2 
+END' WHERE columnname = 'state';
+
 ALTER TABLE config_form_fields ENABLE TRIGGER ALL;
