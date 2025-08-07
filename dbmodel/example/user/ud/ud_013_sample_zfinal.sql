31b4492dec4e95c07142739c37e749bf7d0bf706
arnauurgeles <aurgeles@bgeo.es>
2025-08-07 09:43:07 +0200

    fix(sample): add geometry on flowregulator elements

--- dbmodel/example/user/ud/ud_013_sample_zfinal.sql
+++ dbmodel/example/user/ud/ud_013_sample_zfinal.sql
@@ -153,10 +153,10 @@ update cat_feature set descript = concat(left(id,1), substring(lower(id), 2,99))
 UPDATE config_param_system SET isenabled = false where parameter = ' basic_selector_tab_municipality';
 
 
-INSERT INTO element  (element_id, code, elementcat_id, epa_type, state, state_type, num_elements, rotation, verified, publish, inventory, expl_id, feature_type, top_elev, muni_id, sector_id) VALUES
-('100020', 'E100020', 'WEIR-01','FRWEIR', 1,2,1,79.731, 1,true,true,2,'ELEMENT',30.190,2,2),
-('100021', 'E100021', 'ORIFICE-01','FRORIFICE', 1,2,1,122.505,1,true,true,1,'ELEMENT',19.230,1,1),
-('100022', 'E100022', 'PUMP-01','FRPUMP', 1,2,1,79.731, 1,true,true,2,'ELEMENT',30.190,2,2);
+INSERT INTO element  (element_id, code, elementcat_id, epa_type, state, state_type, num_elements, rotation, verified, publish, inventory, expl_id, feature_type, top_elev, muni_id, sector_id, the_geom) VALUES
+('100020', 'E100020', 'WEIR-01','FRWEIR', 1,2,1,79.731, 1,true,true,2,'ELEMENT',30.190,2,2,'POINT (418716.0233455198 4577601.812087212)'),
+('100021', 'E100021', 'ORIFICE-01','FRORIFICE', 1,2,1,122.505,1,true,true,1,'ELEMENT',19.230,1,1,'POINT (419597.7191116698 4576460.6400896525)'),
+('100022', 'E100022', 'PUMP-01','FRPUMP', 1,2,1,79.731, 1,true,true,2,'ELEMENT',30.190,2,2,'POINT (418716.0233455198 4577601.812087212)');
 
 INSERT INTO man_frelem (element_id, node_id, order_id, to_arc, flwreg_length) VALUES
 ('100020','18828',1,'18969',0.5),
