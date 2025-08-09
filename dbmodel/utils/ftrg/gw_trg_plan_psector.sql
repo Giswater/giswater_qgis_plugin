b8103fa961a23efa70c922e1ecf304c9da418808
Xavier Torret <xtorret@bgeo.es>
2025-08-09 18:43:34 +0200

    feat(trg_plan_sector): Improve controls for status and active of psectors

--- dbmodel/utils/ftrg/gw_trg_plan_psector.sql
+++ dbmodel/utils/ftrg/gw_trg_plan_psector.sql
@@ -24,53 +24,35 @@ BEGIN
 	
    	v_projectype := (SELECT project_type FROM sys_version ORDER BY id DESC LIMIT 1);
 	
-   
-   
-   	IF NEW.active IS TRUE THEN
-   	
-   		
-   		-- check if trg has been executed via dialog or by editing the table plan_psector
-   		IF v_projectype = 'UD' THEN
-   		
-   			v_partialquery = ' union
-			SELECT gully_id, gullycat_id, state, descript, expl_id, fid, the_geom FROM anl_gully WHERE cur_user="current_user"() AND fid IN (354,355)';
+	IF TG_OP = 'INSERT' THEN
+
+		-- set current psectoR
+		UPDATE config_param_user SET value = NEW.psector_id WHERE cur_user = current_user AND parameter = 'plan_psector_current';
+
+		-- set selected psector
+		INSERT INTO selector_psector VALUES (NEW.psector_id, current_user) ON CONFLICT (psector_id, cur_user) DO NOTHING; 
+
+		RETURN NEW;
+
+	ELSIF TG_OP = 'UPDATE' THEN
 	
-		ELSE
-		
-			v_partialquery = '';
-		
-		END IF;
-   		
-   		v_query = '
-		SELECT node_id, nodecat_id, state, descript, expl_id, fid, the_geom FROM anl_node WHERE cur_user="current_user"() AND fid IN (354,355) UNION
-		SELECT arc_id, arccat_id, state, descript, expl_id, fid, the_geom FROM anl_arc WHERE cur_user="current_user"() AND fid IN (354,355) union
-		SELECT connec_id, conneccat_id, state, descript, expl_id, fid, the_geom FROM anl_connec WHERE cur_user="current_user"() AND fid IN (354,355) '||v_partialquery||'';
-   	
-   		EXECUTE 'SELECT count(*) from ('||v_query||')a' INTO v_count;
-   	
-   		IF v_count = 0 THEN 
-   	
-   	
-	   		EXECUTE 'SELECT gw_fct_checktopologypsector($${"client":{"device":4, "infoType":1, "lang":"ES"}, "form":{}, 
-			"feature":{}, "data":{"filterFields":{}, "pageInfo":{},"psectorId":"'||NEW.psector_id||'"}}$$)' INTO v_result;
-		
+	   	--manage checktopologypsector
+		IF (SELECT value::boolean FROM config_param_user WHERE parameter = 'plan_psector_disable_checktopology_trigger' AND cur_user = current_user) = false -- triggered from dialog  
+		AND (NEW.active IS TRUE) AND (NEW.active IS DISTINCT FROM OLD.active) THEN
+	   	   		EXECUTE 'SELECT gw_fct_checktopologypsector($${"client":{"device":4, "infoType":1, "lang":"ES"}, "form":{}, 
+				"feature":{}, "data":{"filterFields":{}, "pageInfo":{},"psectorId":"'||NEW.psector_id||'"}}$$)' INTO v_result;	
 			
+				IF ((v_result->>'message')::json->>'level')::integer = 1 AND (SELECT count(*) FROM plan_psector_x_node WHERE psector_id = NEW.psector_id) > 0 THEN
+					 EXECUTE 'SELECT gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
+		             "data":{"message":"4336", "function":"2446","parameters":null}}$$)';
+		            
+		            RETURN OLD;
+				END IF;  	
+	   	END IF; 
 		
-			IF ((v_result->>'message')::json->>'level')::integer = 1 AND (SELECT count(*) FROM plan_psector_x_node WHERE psector_id = NEW.psector_id) > 0 THEN
-			
-				 EXECUTE 'SELECT gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
-	             "data":{"message":"4336", "function":"2446","parameters":null}}$$)';
-	            
-	            RETURN OLD;
-			
-			END IF;  	
-		
-		END IF;
-   	
-   	END IF;
-   
-	
-RETURN NEW;
+		RETURN NEW;
+
+	END IF;
 			
 END;
 $BODY$
