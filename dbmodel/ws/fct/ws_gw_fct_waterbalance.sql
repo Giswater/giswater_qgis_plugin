29c7b3ea8d4f049bcff58aaa7d2d7c2945578812
mguzman14 <mguzman@bgeo.es>
2025-08-11 18:10:44 +0200

    fix(waterbalance): update mapzone "updated_at"  in order to make coherent the threshold days and execution of dma's

--- dbmodel/ws/fct/ws_gw_fct_waterbalance.sql
+++ dbmodel/ws/fct/ws_gw_fct_waterbalance.sql
@@ -224,7 +224,7 @@ v_queryhydro =
 			v_paramupdate = (SELECT (value::json->>'DMA')::json->>'geomParamUpdate' FROM config_param_system WHERE parameter  = 'utils_graphanalytics_vdefault');
 
 			v_data =  concat ('{"data":{"parameters":{"graphClass":"DMA", "exploitation": "',v_expl,'", "updateMapZone":',v_updatemapzone,
-			', "geomParamUpdate":',v_paramupdate,', "updateFeature":true}}}');
+			', "geomParamUpdate":',v_paramupdate,', "updateFeature":true, "commitChanges":true}}}');
 
 			PERFORM gw_fct_graphanalytics_mapzones(v_data);
 
