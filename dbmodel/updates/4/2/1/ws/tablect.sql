ca4fc462169f5ee1dd56a3c550d9bb2dc0db1958
arnauurgeles <aurgeles@bgeo.es>
2025-08-13 13:53:20 +0200

    feat(rpt_cat_result): add constraint so network dma can not be corporate

---
+++ dbmodel/updates/4/2/1/ws/tablect.sql
@@ -0,0 +1,12 @@
+/*
+This file is part of Giswater
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+DROP CONSTRAINT IF EXISTS rpt_cat_result_network_dma_corporate ON rpt_cat_result;
+ALTER TABLE rpt_cat_result ADD CONSTRAINT rpt_cat_result_network_dma_corporate CHECK (NOT (iscorporate = TRUE AND network_type = '5'));
