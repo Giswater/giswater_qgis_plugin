558090859b70f201dc853b975153d1bf0b017c96
arnauurgeles <aurgeles@bgeo.es>
2025-08-13 14:53:45 +0200

    feat(rpt_cat_result): add dma_id to v_ui_rpt_cat_result

--- dbmodel/updates/4/2/1/ws/dml.sql
+++ dbmodel/updates/4/2/1/ws/dml.sql
@@ -11,3 +11,6 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 UPDATE sys_feature_class SET epa_default = 'UNDEFINED' WHERE type IN ('ELEMENT','CONNEC');
 
 UPDATE sys_function SET function_name = 'gw_fct_pg2epa_flwreg2arc', descript = 'This functions transform flwreg elements to arcs.' WHERE id = 2318;
+
+INSERT INTO config_form_tableview (location_type, project_type, objectname, columnname, columnindex, visible, width, alias, "style", addparam)
+VALUES('epa_toolbar', 'utils', 'v_ui_rpt_cat_result', 'dma_id', (SELECT MAX(columnindex) FROM config_form_tableview WHERE location_type = 'epa_toolbar' AND project_type = 'utils' AND objectname = 'v_ui_rpt_cat_result'), true, NULL, 'dma_id', NULL, NULL);
