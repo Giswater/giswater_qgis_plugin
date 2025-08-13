bf2624bfdd535003eb3219cc021cd0b7d1a074e3
Arnau Torret <atorret@bgeo.es>
2025-08-13 18:18:52 +0200

    enahn(elements): Generate config_form_fields for elements depending on the original elements

--- dbmodel/updates/4/2/1/ud/dml.sql
+++ dbmodel/updates/4/2/1/ud/dml.sql
@@ -8,14 +8,7 @@ or (at your option) any later version.
 
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
-UPDATE sys_feature_class SET epa_default = 'UNDEFINED' WHERE type IN ('ELEMENT','CONNEC');
-
-UPDATE sys_function SET function_name = 'gw_fct_pg2epa_flwreg2arc', descript = 'This functions transform flwreg elements to arcs.' WHERE id = 2318;
-
-INSERT INTO config_form_tableview (location_type, project_type, objectname, columnname, columnindex, visible, width, alias, "style", addparam)
-VALUES('epa_toolbar', 'utils', 'v_ui_rpt_cat_result', 'dma_id', (SELECT MAX(columnindex) FROM config_form_tableview WHERE location_type = 'epa_toolbar' AND project_type = 'utils' AND objectname = 'v_ui_rpt_cat_result'), true, NULL, 'dma_id', NULL, NULL);
-
-
+-- 13/08/2025
 DELETE FROM config_form_fields WHERE formname ILIKE 've_element_%';
 DO $$
 DECLARE
@@ -185,12 +178,13 @@ BEGIN
 END $$;
 
 -- FRELEM
+-- EPUMP
 DO $$
 DECLARE
   rec record;
   v_view text;
 BEGIN
-  FOR rec IN SELECT val AS v_view,  elem as id FROM (VALUES ('ve_element_epump', 'EPUMP'), ('ve_element_evalve', 'EVALVE'), ('ve_element_emeter', 'EMETER')) AS t(val, elem)
+  FOR rec IN SELECT val AS v_view,  elem as id FROM (VALUES ('ve_element_epump', 'EPUMP'), ('ve_element_eorifice', 'EORIFICE'), ('ve_element_eweir', 'EWEIR'), ('ve_element_eoutlet', 'EOUTLET') ) AS t(val, elem)
   LOOP
     INSERT INTO config_form_fields (formname,formtype,tabname,columnname,layoutname,layoutorder,"datatype",widgettype,"label",tooltip,ismandatory,isparent,iseditable,isautoupdate,dv_querytext,dv_orderby_id,dv_isnullvalue,stylesheet,widgetcontrols,hidden)
       VALUES (rec.v_view,'form_feature','tab_data','sector_id','lyt_bot_1',1,'integer','combo','Sector ID:','Sector ID',false,false,true,false,'SELECT sector_id as id, name as idval FROM sector WHERE sector_id IS NOT NULL',true,false,'{"label":"color:blue; font-weight:bold;"}'::json,'{"setMultiline": false, "labelPosition": "top"}'::json,false);
