5757d354a5ba283854406a8724810e3bd7665fbd
xtorret@bgeo.es <xtorret@bgeo.es>
2025-08-19 18:31:43 +0200

    update(cff): Apply last changes on cff

--- dbmodel/final_pass/ws/config_form_fields/06_cff_updates.sql
+++ dbmodel/final_pass/ws/config_form_fields/06_cff_updates.sql
@@ -453,3 +453,25 @@ BEGIN
   END LOOP;
 END $$;
 DELETE FROM config_form_fields WHERE formname ILIKE 've_element%' AND formtype='form_feature' AND columnname='tbl_element_x_gully' AND tabname='tab_features';
+
+
+UPDATE config_form_fields SET dv_querytext =
+'WITH psector_value AS (
+  		SELECT value::integer AS psector_value 
+  		FROM config_param_user 
+  		WHERE parameter = ''plan_psector_current'' AND cur_user = current_user),
+	 tg_op_value AS (
+  		SELECT value::text AS tg_op_value 
+  		FROM config_param_user 
+  		WHERE parameter = ''utils_transaction_mode'' AND cur_user = current_user)  
+SELECT id::integer as id, name as idval
+FROM value_state 
+WHERE id IS NOT NULL 
+AND CASE 
+  WHEN (SELECT tg_op_value FROM tg_op_value)!=''INSERT'' THEN id IN (0,1,2)
+  WHEN (SELECT tg_op_value FROM tg_op_value) =''INSERT'' AND (SELECT psector_value FROM psector_value) IS NOT NULL THEN id = 2 
+  ELSE id < 2 
+END' 
+WHERE columnname = 'state';
+
+update config_form_fields set dv_orderby_id = true where formtype ='psector' and columnname ='status';
