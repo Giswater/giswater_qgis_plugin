0f8da2b835fc4f0070f887404e7235cd520c70aa
Daniel Marin <dmarin@bgeo.es>
2025-08-27 13:50:56 +0200

    fix(i18n): Update Spanish translations and add new contexts for work management and visit features

--- i18n/giswater_es_CR.ts
+++ i18n/giswater_es_CR.ts
@@ -5702,15 +5702,11 @@
 			<translation>Tarea abortada: {0}.</translation>
 		</message>
 		<message>
-			<source>The procedure will delete features on database unless it is a node that doesn't divide arc.\n
-
- Please ensure that features has no undelete value on true.\n
-
+			<source>The procedure will delete features on database unless it is a node that doesn't divide arc.\n
+ Please ensure that features has no undelete value on true.\n
  On the other hand you must know that traceability table will storage precedent information.</source>
-			<translation>El procedimiento eliminará elementos de red en la base de datos a menos que sea un nodo que no divide el arco.\ N
-
-Asegúrese de que los elementos de red no tengan un valor de recuperación en verdadero.\ N
-
+			<translation>El procedimiento eliminará elementos de red en la base de datos a menos que sea un nodo que no divide el arco.\ N
+Asegúrese de que los elementos de red no tengan un valor de recuperación en verdadero.\ N
 Por otro lado, debe saber que la tabla de trazabilidad almacenará información precedente.</translation>
 		</message>
 		<message>
