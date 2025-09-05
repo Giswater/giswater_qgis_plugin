07e9f3cbcae24ec95e30f8bfe2d31111173302db
mguzman14 <mguzman@bgeo.es>
2025-09-05 11:42:34 +0200

    fix(dml): fix mapzones analysis dialog of toolbox

--- dbmodel/updates/4/2/2/ws/dml.sql
+++ dbmodel/updates/4/2/2/ws/dml.sql
@@ -758,3 +758,125 @@ UPDATE config_form_fields SET columnname='dint' WHERE formname='ve_epa_valve' AN
 
 -- 05/09/2025
 UPDATE sys_table SET project_template='{"template": [1],"visibility": false,"levels_to_read": 2}'::jsonb WHERE id='ve_dqa';
+
+UPDATE config_toolbox SET inputparams='[
+  {
+    "widgetname": "graphClass",
+    "label": "Graph class:",
+    "widgettype": "combo",
+    "datatype": "text",
+    "tooltip": "Graphanalytics method used",
+    "layoutname": "grl_option_parameters",
+    "layoutorder": 1,
+    "comboIds": [
+      "PRESSZONE",
+      "SECTOR",
+      "DMA"
+    ],
+    "comboNames": [
+      "PRESSZONE",
+      "SECTOR",
+      "DMA"
+    ],
+    "selectedId": null
+  },
+  {
+    "widgetname": "exploitation",
+    "label": "Exploitation:",
+    "widgettype": "combo",
+    "datatype": "text",
+    "tooltip": "Choose exploitation to work with",
+    "layoutname": "grl_option_parameters",
+    "layoutorder": 2,
+    "dvQueryText": "SELECT id, idval FROM ( SELECT -901 AS id, ''User selected expl'' AS idval, ''a'' AS sort_order UNION SELECT -902 AS id, ''All exploitations'' AS idval, ''b'' AS sort_order UNION SELECT expl_id AS id, name AS idval, ''c'' AS sort_order FROM exploitation WHERE active IS NOT FALSE ) a ORDER BY sort_order ASC, idval ASC",
+    "selectedId": null
+  },
+  {
+    "widgetname": "forceOpen",
+    "label": "Force open nodes: (*)",
+    "widgettype": "linetext",
+    "datatype": "text",
+    "isMandatory": false,
+    "tooltip": "Optative node id(s) to temporary open closed node(s) in order to force algorithm to continue there",
+    "placeholder": "1015,2231,3123",
+    "layoutname": "grl_option_parameters",
+    "layoutorder": 5,
+    "value": null
+  },
+  {
+    "widgetname": "forceClosed",
+    "label": "Force closed nodes: (*)",
+    "widgettype": "text",
+    "datatype": "text",
+    "isMandatory": false,
+    "tooltip": "Optative node id(s) to temporary close open node(s) to force algorithm to stop there",
+    "placeholder": "1015,2231,3123",
+    "layoutname": "grl_option_parameters",
+    "layoutorder": 6,
+    "value": null
+  },
+  {
+    "widgetname": "usePlanPsector",
+    "label": "Use selected psectors:",
+    "widgettype": "check",
+    "datatype": "boolean",
+    "tooltip": "If true, use selected psectors. If false ignore selected psectors and only works with on-service network",
+    "layoutname": "grl_option_parameters",
+    "layoutorder": 7,
+    "value": null
+  },
+  {
+    "widgetname": "commitChanges",
+    "label": "Commit changes:",
+    "widgettype": "check",
+    "datatype": "boolean",
+    "tooltip": "If true, changes will be applied to DB. If false, algorithm results will be saved in anl tables",
+    "layoutname": "grl_option_parameters",
+    "layoutorder": 8,
+    "value": null
+  },
+  {
+    "widgetname": "updateMapZone",
+    "label": "Mapzone constructor method:",
+    "widgettype": "combo",
+    "datatype": "integer",
+    "layoutname": "grl_option_parameters",
+    "layoutorder": 10,
+    "comboIds": [
+      0,
+      1,
+      2,
+      6
+    ],
+    "comboNames": [
+      "NONE",
+      "CONCAVE POLYGON",
+      "PIPE BUFFER",
+      "PLOT & PIPE BUFFER",
+      "LINK & PIPE BUFFER"
+    ],
+    "selectedId": null
+  },
+  {
+    "widgetname": "geomParamUpdate",
+    "label": "Pipe buffer",
+    "widgettype": "text",
+    "datatype": "float",
+    "tooltip": "Buffer from arcs to create mapzone geometry using [PIPE BUFFER] options. Normal values maybe between 3-20 mts.",
+    "layoutname": "grl_option_parameters",
+    "layoutorder": 11,
+    "isMandatory": false,
+    "placeholder": "5-30",
+    "value": null
+  },
+  {
+    "widgetname": "fromZero",
+    "label": "Mapzones from zero:",
+    "widgettype": "check",
+    "datatype": "boolean",
+    "tooltip": "If true, mapzones are calculated automatically from zero",
+    "layoutname": "grl_option_parameters",
+    "layoutorder": 12,
+    "value": null
+  }
+]'::json WHERE id=2768;
