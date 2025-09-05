093245cb72f8bf0ffada25083c31baffc10cd2f8
claudia dragoste <claudiadragoste@yahoo.com>
2025-09-05 17:07:57 +0200

    enhan(ddlview): fix prev commit

--- dbmodel/updates/4/2/2/ws/trg.sql
+++ dbmodel/updates/4/2/2/ws/trg.sql
@@ -7,8 +7,6 @@ or (at your option) any later version.
 
 
 SET search_path = SCHEMA_NAME, public, pg_catalog;
-<<<<<<< Updated upstream
-=======
 
 DROP TRIGGER IF EXISTS gw_trg_edit_arc ON ve_arc;
 CREATE TRIGGER gw_trg_edit_arc INSTEAD OF INSERT OR DELETE OR UPDATE ON ve_arc
@@ -25,4 +23,3 @@ FOR EACH ROW EXECUTE FUNCTION gw_trg_edit_connec('parent');
 DROP TRIGGER IF EXISTS gw_trg_edit_link ON ve_link;
 CREATE TRIGGER gw_trg_edit_link INSTEAD OF INSERT OR DELETE OR UPDATE ON ve_link 
 FOR EACH ROW EXECUTE FUNCTION gw_trg_edit_link('LINK');
->>>>>>> Stashed changes
