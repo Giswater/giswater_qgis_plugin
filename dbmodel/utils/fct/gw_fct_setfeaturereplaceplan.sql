05c1b0fc7f14cb484fffa30e9eec0cb5bfa8d2f4
opueyo <opueyo@bgeo.es>
2025-09-10 14:26:34 +0200

    fix(gw_fct_setfeaturereplaceplan): remove non-existent field

--- dbmodel/utils/fct/gw_fct_setfeaturereplaceplan.sql
+++ dbmodel/utils/fct/gw_fct_setfeaturereplaceplan.sql
@@ -179,9 +179,9 @@ BEGIN
 					IF v_project_type = 'WS' THEN
 
 						INSERT INTO link (feature_id, feature_type, exit_id, exit_type, state, expl_id, the_geom, sector_id, presszone_id,
-						linkcat_id, state_type, dma_id, dqa_id, minsector_id, fluid_type, link_type)
+						linkcat_id, state_type, dma_id, dqa_id, minsector_id, fluid_type)
 						SELECT feature_id, feature_type, v_arc, 'ARC', 2, expl_id, the_geom, sector_id,	presszone_id,
-						linkcat_id, state_type, dma_id, dqa_id, minsector_id, fluid_type, link_type FROM link WHERE link_id = v_link.link_id RETURNING link_id INTO v_link_id;
+						linkcat_id, state_type, dma_id, dqa_id, minsector_id, fluid_type FROM link WHERE link_id = v_link.link_id RETURNING link_id INTO v_link_id;
 					ELSE
 						INSERT INTO link (feature_id, feature_type, exit_id, exit_type, state, expl_id, the_geom, sector_id, dma_id, linkcat_id, fluid_type, state_type, link_type)
 						SELECT feature_id, feature_type, v_arc, 'ARC', 2, expl_id, the_geom, sector_id, dma_id, linkcat_id, fluid_type, state_type, link_type
