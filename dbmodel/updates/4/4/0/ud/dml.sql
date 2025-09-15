db27c435b5e1726fa6f4da96831136ecd7c67f9f
Daniel Marín <dmarin@bgeo.es>
2025-09-15 11:25:51 +0200

    feat(dml): add new conflict dwfzone entry for graphanalytics algorithm

--- dbmodel/updates/4/4/0/ud/dml.sql
+++ dbmodel/updates/4/4/0/ud/dml.sql
@@ -9,3 +9,5 @@ or (at your option) any later version.
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 UPDATE sys_table SET alias = 'Inp Raingage' WHERE id = 've_raingage';
+
+INSERT INTO dwfzone (dwfzone_id, code, "name", dwfzone_type, expl_id, sector_id, muni_id, descript, link, graphconfig, stylesheet, lock_level, active, the_geom, created_at, created_by, updated_at, updated_by, drainzone_id, addparam) VALUES(-1, '-1', 'Conflict', NULL, '{NULL}', NULL, NULL, 'Dwfzone used on graphanalytics algorithm when two ore more zones has conflict in terms of some interconnection.', NULL, '{"use":[{"nodeParent":""}], "ignore":[], "forceClosed":[]}'::json, NULL, NULL, true, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
