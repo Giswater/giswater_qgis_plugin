ac40805f9ae8af3631fc68e674897f5b88ccd087
Daniel Marín <dmarin@bgeo.es>
2025-10-08 09:17:44 +0200

    fix(ddlview): add new definition of view ve_inp_dwf

---
+++ dbmodel/updates/4/4/1/ud/ddlview.sql
@@ -0,0 +1,28 @@
+/*
+This file is part of Giswater
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+CREATE OR REPLACE VIEW ve_inp_dwf AS
+WITH inp_options_dwfscenario_current AS (
+  SELECT value::integer AS dwfscenario_id
+  FROM config_param_user
+  WHERE cur_user = CURRENT_USER
+    AND parameter = 'inp_options_dwfscenario_current'
+  LIMIT 1
+)
+SELECT i.dwfscenario_id,
+    i.node_id,
+    i.value,
+    i.pat1,
+    i.pat2,
+    i.pat3,
+    i.pat4
+FROM inp_dwf i
+JOIN ve_inp_junction USING (node_id)
+JOIN inp_options_dwfscenario_current iodc ON iodc.dwfscenario_id = i.dwfscenario_id;
