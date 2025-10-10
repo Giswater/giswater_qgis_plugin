a3822f079ef327bae8b3596077ccb922c1830967
arnauurgeles <aurgeles@bgeo.es>
2025-10-10 13:29:18 +0200

    fix(config_form_fields): set ismandatory to false for connection_type in various valve forms

--- dbmodel/updates/4/5/0/ws/dml.sql
+++ dbmodel/updates/4/5/0/ws/dml.sql
@@ -27,3 +27,26 @@ UPDATE config_form_fields SET dv_isnullvalue=true
 WHERE formname='inp_dscenario_virtualvalve' AND formtype='form_feature' AND columnname='valve_type' AND tabname='tab_none';
 UPDATE config_form_fields SET dv_isnullvalue=true
 WHERE formname='ve_inp_dscenario_virtualvalve' AND formtype='form_feature' AND columnname='valve_type' AND tabname='tab_none';
+
+UPDATE config_form_fields SET ismandatory=false
+WHERE formname='ve_node_air_valve' AND formtype='form_feature' AND columnname='connection_type' AND tabname='tab_data';
+UPDATE config_form_fields SET ismandatory=false
+WHERE formname='ve_node_check_valve' AND formtype='form_feature' AND columnname='connection_type' AND tabname='tab_data';
+UPDATE config_form_fields SET ismandatory=false
+WHERE formname='ve_node_fl_contr_valve' AND formtype='form_feature' AND columnname='connection_type' AND tabname='tab_data';
+UPDATE config_form_fields SET ismandatory=false
+WHERE formname='ve_node_gen_purp_valve' AND formtype='form_feature' AND columnname='connection_type' AND tabname='tab_data';
+UPDATE config_form_fields SET ismandatory=false
+WHERE formname='ve_node_green_valve' AND formtype='form_feature' AND columnname='connection_type' AND tabname='tab_data';
+UPDATE config_form_fields SET ismandatory=false
+WHERE formname='ve_node_outfall_valve' AND formtype='form_feature' AND columnname='connection_type' AND tabname='tab_data';
+UPDATE config_form_fields SET ismandatory=false
+WHERE formname='ve_node_pr_break_valve' AND formtype='form_feature' AND columnname='connection_type' AND tabname='tab_data';
+UPDATE config_form_fields SET ismandatory=false
+WHERE formname='ve_node_pr_reduc_valve' AND formtype='form_feature' AND columnname='connection_type' AND tabname='tab_data';
+UPDATE config_form_fields SET ismandatory=false
+WHERE formname='ve_node_pr_susta_valve' AND formtype='form_feature' AND columnname='connection_type' AND tabname='tab_data';
+UPDATE config_form_fields SET ismandatory=false
+WHERE formname='ve_node_shutoff_valve' AND formtype='form_feature' AND columnname='connection_type' AND tabname='tab_data';
+UPDATE config_form_fields SET ismandatory=false
+WHERE formname='ve_node_throttle_valve' AND formtype='form_feature' AND columnname='connection_type' AND tabname='tab_data';
