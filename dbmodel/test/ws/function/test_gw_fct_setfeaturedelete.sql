33fbabe697ee4661d335c22185fe6246d895299a
Daniel Marín <dmarin@bgeo.es>
2025-10-16 09:02:27 +0200

    refactor(test): update config parameter for plan_psector_current

--- dbmodel/test/ws/function/test_gw_fct_setfeaturedelete.sql
+++ dbmodel/test/ws/function/test_gw_fct_setfeaturedelete.sql
@@ -14,21 +14,8 @@ SET search_path = "SCHEMA_NAME", public, pg_catalog;
 -- Plan for 1 test
 SELECT plan(1);
 
--- Create roles for testing
-CREATE USER plan_user;
-GRANT role_plan to plan_user;
-
-CREATE USER epa_user;
-GRANT role_epa to epa_user;
-
-CREATE USER edit_user;
-GRANT role_edit to edit_user;
-
-CREATE USER om_user;
-GRANT role_om to om_user;
-
-CREATE USER basic_user;
-GRANT role_basic to basic_user;
+UPDATE config_param_user SET value = NULL
+WHERE "parameter" = 'plan_psector_current';
 
 -- Extract and test the "status" field from the function's JSON response
 SELECT is (
