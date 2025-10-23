48dcdd4541c77ed05c1681a5b64935b472883d37
smaspons <smaspons@bgeo.es>
2025-10-23 10:02:12 +0200

    fix(import_swmm): create 'VARC' catalog if doesn't exist
    (cherry picked from commit 3aef7e92e9b866d9985968e6fb0295b81b2b15a8)

--- core/threads/import_inp/import_swmm_task.py
+++ core/threads/import_inp/import_swmm_task.py
@@ -525,6 +525,18 @@ class GwImportInpTask(GwTask):
                 continue
 
             if self.manage_flwreg.get(varc_type):
+                # Create the 'VARC' if it doesn't exist
+                execute_sql("""
+                    INSERT INTO cat_feature (
+                        id, feature_class, feature_type, shortcut_key, parent_layer,
+                        child_layer, descript, link_path, code_autofill, active,
+                        addparam, inventory_vdefault
+                    ) VALUES (
+                        'VARC', 'VARC', 'ARC', NULL, 've_arc',
+                        've_arc_varc', 'Virtual Arc', NULL, true, true,
+                        NULL, NULL
+                    ) ON CONFLICT DO NOTHING;
+                """, commit=self.force_commit)
                 # Just create the 'VARC' catalog to temporarly insert them as varcs
                 execute_sql("""
                     INSERT INTO cat_arc (id, arc_type, shape, geom1)
