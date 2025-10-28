d041bad11f5b446a3f8b6553d75a53f6049b8c60
smaspons <smaspons@bgeo.es>
2025-10-28 13:23:24 +0100

    fix(import_epanet): manage null options

--- core/threads/import_inp/import_epanet_task.py
+++ core/threads/import_inp/import_epanet_task.py
@@ -328,12 +328,10 @@ class GwImportInpTask(GwTask):
                         value = "YES" if value else "NO"
                     elif category == "time" and key not in ("pattern_start", "statistic"):
                         value = f"{int(value) // 3600}:{(int(value) % 3600) // 60:02d}" if value is not None else None
-                    if value is None:
-                        value = ""
                     prefix = prefix_map.get(category, "inp_options_")
                     param_name = params_map[category].get(key.lower(), key.lower())
                     param_name = f"{prefix}{param_name}"
-                    update_params.append((str(value), param_name))
+                    update_params.append((str(value) if value is not None else value, param_name))
 
         # Iterate over each category and its options
         for category, options in options_dict.items():
