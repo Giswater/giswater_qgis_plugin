560c6832342b39129c7a18bb420b427b8327c915
arnauurgeles <aurgeles@bgeo.es>
2025-10-29 10:16:02 +0100

    fix(config_form_fields): update dv_querytext for epa_selector form to not use hardocded conditions

--- dbmodel/updates/4/5/4/utils/dml.sql
+++ dbmodel/updates/4/5/4/utils/dml.sql
@@ -9,3 +9,11 @@ or (at your option) any later version.
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 UPDATE sys_message SET log_level = 2 WHERE id = 4432;
+
+UPDATE config_form_fields
+SET dv_querytext='SELECT r.result_id AS id, r.result_id AS idval FROM rpt_cat_result r JOIN v_ui_rpt_cat_result vr ON vr.result_id = r.result_id WHERE r.status = 2'
+WHERE formname='generic' AND formtype='epa_selector' AND columnname='result_name_compare' AND tabname='tab_result';
+
+UPDATE config_form_fields
+SET dv_querytext='SELECT r.result_id AS id, r.result_id AS idval FROM rpt_cat_result r JOIN v_ui_rpt_cat_result vr ON vr.result_id = r.result_id WHERE r.status = 2'
+WHERE formname='generic' AND formtype='epa_selector' AND columnname='result_name_show' AND tabname='tab_result';
