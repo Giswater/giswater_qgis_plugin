062c64213f1080173a3d64d1e5ee1ba36fe2303b
Daniel Marín <dmarin@bgeo.es>
2025-10-29 13:14:00 +0100

    refactor(gw_fct_getfeatureproperties): simplify SQL function and improve variable handling

--- dbmodel/utils/fct/gw_fct_getfeatureproperties.sql
+++ dbmodel/utils/fct/gw_fct_getfeatureproperties.sql
@@ -14,15 +14,7 @@ $BODY$
 
 /* Example usage:
 
-SELECT gw_fct_getfeatureproperties($${
-	"data":{
-		"parameters":{
-			"featureId":1,
-			"featureType":"ARC",
-			"featureClass":"PIPE",
-		}
-	}
-}$$);
+SELECT gw_fct_getfeatureproperties(json_build_object('data', json_build_object('parameters', json_build_object('featureId', 1, 'featureType', 'ARC'))));
 
 */
 
@@ -37,9 +29,9 @@ DECLARE
 	-- dialog variables
 	v_feature_id integer;
 	v_feature_type text; -- ARC, NODE, CONNEC, LINK, GULLY
-	v_feature_class text; -- PIPE, JUNCTION, ...
 
 	-- query variables
+	v_count integer;
 	v_query_text text;
 	v_query_text_aux text;
 	v_data json;
@@ -47,7 +39,12 @@ DECLARE
 	-- extra variables
 	v_feature_table text;
 	v_feature_id_field text;
+	v_feature_type_field text;
+	v_feature_cat_feature_table text;
 	v_feature_cat_table text;
+	v_feature_cat_field text;
+	v_feature_class text; -- PIPE, JUNCTION, ...
+	v_psector_table text;
 
 	-- result variables
 	v_result text;
@@ -59,7 +56,7 @@ DECLARE
 
 	v_is_arc_divide_candidate boolean;
 	v_is_arc_fusion_candidate boolean;
-	v_feature_state text;
+	v_feature_state integer;
 	v_feature_expl_id integer;
 	v_feature_psector_id integer;
 
@@ -94,15 +91,40 @@ BEGIN
 	v_feature_type := lower(v_feature_type);
 	v_feature_table := v_feature_type;
 	v_feature_id_field := v_feature_type || '_id';
-	v_feature_cat_table := 'cat_feature_' || v_feature_type;
+	v_feature_type_field := v_feature_type || '_type';
+	v_feature_cat_feature_table := 'cat_feature_' || v_feature_type;
+	v_feature_cat_table := 'cat_' || v_feature_type;
+	v_feature_cat_field := v_feature_type || 'cat_id';
+	v_psector_table := 'plan_psector_x_' || v_feature_type;
 
-	IF v_feature_type = 'NODE' THEN
-		EXECUTE format('SELECT isarcdivide FROM %I WHERE id = $1', v_feature_cat_table) INTO v_is_arc_divide_candidate USING v_feature_class;
+	EXECUTE format('
+			SELECT c.%I AS feature_type
+			FROM %I t
+			JOIN %I c ON c.id = t.%I	
+			WHERE t.%I = $1
+		', v_feature_type_field , v_feature_table, v_feature_cat_table, v_feature_cat_field , v_feature_id_field
+	) INTO v_feature_class USING v_feature_id;
+
+	IF v_feature_type = 'node' THEN
+		v_query_text := format('SELECT isarcdivide FROM %I WHERE id = $1', v_feature_cat_feature_table);
+		EXECUTE v_query_text INTO v_is_arc_divide_candidate USING v_feature_class;
+
+		v_query_text := format('SELECT COUNT(*) FROM ve_arc WHERE node_1 = $1 OR node_2 = $1 AND state > 0');
+		EXECUTE v_query_text INTO v_count USING v_feature_id;
+		IF v_count = 2 THEN
+			v_is_arc_fusion_candidate := TRUE;
+		END IF;
 	END IF;
 
-	EXECUTE format('SELECT expl_id FROM %I WHERE %I = $1', v_feature_table, v_feature_id_field) INTO v_feature_expl_id USING v_feature_id;
+	v_query_text := format('SELECT expl_id FROM %I WHERE %I = $1', v_feature_table, v_feature_id_field);
+	RAISE NOTICE 'v_query_text: %', v_query_text;
+	EXECUTE v_query_text INTO v_feature_expl_id USING v_feature_id;
 
-	EXECUTE format('SELECT psector_id FROM %I WHERE %I = $1', v_feature_table, v_feature_id_field) INTO v_feature_psector_id USING v_feature_id;
+	v_query_text := format('SELECT state FROM %I WHERE %I = $1', v_feature_table, v_feature_id_field);
+	RAISE NOTICE 'v_query_text: %', v_query_text;
+	EXECUTE v_query_text INTO v_feature_state USING v_feature_id;
+
+	v_status := 'Accepted';
 
 	-- Control NULL values
 	v_status := COALESCE(v_status, 'Failed');
@@ -111,7 +133,7 @@ BEGIN
 	v_version := COALESCE(v_version, '');
 	v_is_arc_divide_candidate := COALESCE(v_is_arc_divide_candidate, false);
 	v_is_arc_fusion_candidate := COALESCE(v_is_arc_fusion_candidate, false);
-	v_feature_state := COALESCE(v_feature_state, '');
+	v_feature_state := COALESCE(v_feature_state, -1);
 	v_feature_expl_id := COALESCE(v_feature_expl_id, -1);
 	v_feature_psector_id := COALESCE(v_feature_psector_id, -1);
 
