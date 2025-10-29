3312402cc8b8e2e32264485ebe07dba02190fa81
Ferran <fmartinez@bgeo.es>
2025-10-29 15:43:37 +0100

    feat(getselectors): send active in all cases but not for mincut to avoid error in selector_mincut_result

--- dbmodel/utils/fct/gw_fct_getselectors.sql
+++ dbmodel/utils/fct/gw_fct_getselectors.sql
@@ -318,13 +318,24 @@ BEGIN
 		END IF;
 		
 		-- final query
-		v_finalquery = concat('SELECT array_to_json(array_agg(row_to_json(b))) FROM (
-				select *, row_number() OVER (',v_orderby_query,') as orderby from (
-				SELECT ',quote_ident(v_table_id),', concat(' , v_label , ') AS label, ',v_name,' as name, ', v_table_id , '::text as widgetname, ' ,
-				v_orderby, ' as orderby , ''', v_selector_id , ''' as columnname, ''check'' as type, ''boolean'' as "dataType",
-				EXISTS (SELECT ', v_selector_id, ' FROM ', v_selector, ' e WHERE e.', v_selector_id, ' = s.', v_table_id, ' and cur_user=current_user) as "value",
-				case when EXISTS(SELECT ', v_table_id, ' FROM ', v_table, ' e WHERE e.', v_table_id, ' = s.', v_table_id, ') is not true then ''color: lightgray; font-style: italic;'' end as stylesheet
-				FROM ', v_table ,' s WHERE TRUE ', v_fullfilter, ' ) a)b');
+		-- Add conditional active filter (not applicable for mincut selector)
+		IF v_selector = 'selector_mincut_result' THEN
+			v_finalquery = concat('SELECT array_to_json(array_agg(row_to_json(b))) FROM (
+					select *, row_number() OVER (',v_orderby_query,') as orderby from (
+					SELECT ',quote_ident(v_table_id),', concat(' , v_label , ') AS label, ',v_name,' as name, ', v_table_id , '::text as widgetname, ' ,
+					v_orderby, ' as orderby , ''', v_selector_id , ''' as columnname, ''check'' as type, ''boolean'' as "dataType",
+					EXISTS (SELECT ', v_selector_id, ' FROM ', v_selector, ' e WHERE e.', v_selector_id, ' = s.', v_table_id, ' and cur_user=current_user) as "value", null as active,
+					null as stylesheet
+					FROM ', v_table ,' s WHERE TRUE ', v_fullfilter, ' ) a)b');
+		ELSE
+			v_finalquery = concat('SELECT array_to_json(array_agg(row_to_json(b))) FROM (
+					select *, row_number() OVER (',v_orderby_query,') as orderby from (
+					SELECT ',quote_ident(v_table_id),', concat(' , v_label , ') AS label, ',v_name,' as name, ', v_table_id , '::text as widgetname, ' ,
+					v_orderby, ' as orderby , ''', v_selector_id , ''' as columnname, ''check'' as type, ''boolean'' as "dataType",
+					EXISTS (SELECT ', v_selector_id, ' FROM ', v_selector, ' e WHERE e.', v_selector_id, ' = s.', v_table_id, ' and cur_user=current_user) as "value", active,
+					case when EXISTS(SELECT ', v_table_id, ' FROM ', v_table, ' e WHERE active and e.', v_table_id, ' = s.', v_table_id, ') is not true then ''color: lightgray; font-style: italic;'' end as stylesheet
+					FROM ', v_table ,' s WHERE TRUE ', v_fullfilter, ' ) a)b');
+		END IF;
 
 		v_debug_vars := json_build_object('v_table_id', v_table_id, 'v_label', v_label, 'v_orderby', v_orderby, 'v_name', v_name, 'v_selector_id', v_selector_id,
 						  'v_table', v_table, 'v_selector', v_selector, 'current_user', current_user, 'v_fullfilter', v_fullfilter);
