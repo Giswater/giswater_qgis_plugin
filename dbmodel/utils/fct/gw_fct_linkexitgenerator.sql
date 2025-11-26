cfffc7a2fdeec3b04af188309dc3d7d7c8e8261d
claudia dragoste <claudiadragoste@yahoo.com>
2025-11-26 20:59:28 +0100

    fix(linkexitgenerator): fix and improve the code

--- dbmodel/utils/fct/gw_fct_linkexitgenerator.sql
+++ dbmodel/utils/fct/gw_fct_linkexitgenerator.sql
@@ -36,6 +36,9 @@ DECLARE
 	v_loop record;
 	v_node record;
 	v_end_point public.geometry;
+	v_query_text text;
+	v_query_text_aux text;
+	v_temp_node_table regclass;
 
 BEGIN
 
@@ -44,38 +47,64 @@ BEGIN
 	-- select config values
 	SELECT project_type, giswater INTO v_project_type, v_version FROM sys_version ORDER BY id DESC LIMIT 1;
 
-	-- insert features temp_link
-	INSERT INTO temp_link (link_id, vnode_id, vnode_type, feature_id, feature_type, exit_id, exit_type, state, expl_id,
-	sector_id, exit_topelev, exit_elev, the_geom, the_geom_endpoint, flag) 
-	SELECT link_id, link_id, exit_type, feature_id, feature_type, exit_id, exit_type, state, expl_id,
-	sector_id, top_elev2, elevation2, the_geom, st_endpoint(the_geom), false FROM ve_link where exit_type = 'ARC' AND state > 0;
-
-	IF v_project_type = 'WS' THEN
-		UPDATE temp_link SET omzone_id=l.omzone_id, dma_id = l.dma_id, supplyzone_id = l.supplyzone_id FROM ve_link l WHERE temp_link.link_id = l.link_id AND l.exit_type = 'ARC';
+	IF p_input = 1 THEN -- the whole ve_link
+		v_query_text_aux := '';
+	ELSIF p_input = 2 THEN -- only those links wich are on arcs present on temp_anl_arc and nodes present temp_anl_node
+		v_query_text_aux := ' AND exit_id::varchar(16) IN (SELECT arc_id FROM temp_anl_arc UNION SELECT node_id FROM temp_anl_node)';
 	END IF;
 
-	INSERT INTO temp_link (link_id, vnode_id, vnode_type, feature_id, feature_type, exit_id, exit_type, state, expl_id,
-	sector_id, exit_topelev, exit_elev, the_geom, the_geom_endpoint, flag) 
-	SELECT link_id, exit_id, exit_type, feature_id, feature_type, exit_id, exit_type, state, expl_id,
-	sector_id, top_elev2, elevation2, the_geom, st_endpoint(the_geom), false FROM ve_link where exit_type IN ('NODE', 'CONNEC');
+	-- insert features temp_link
+	v_query_text := format($sql$
+		INSERT INTO temp_link (
+			link_id, vnode_id, vnode_type, feature_id, feature_type,
+			exit_id, exit_type, state, expl_id,
+			sector_id, exit_topelev, exit_elev,
+			the_geom, the_geom_endpoint, flag
+		)
+		SELECT
+			link_id,
+			CASE WHEN exit_type = 'ARC' THEN link_id ELSE exit_id END AS vnode_id,
+			exit_type AS vnode_type,
+			feature_id,
+			feature_type,
+			exit_id,
+			exit_type,
+			state,
+			expl_id,
+			sector_id,
+			top_elev2,
+			elevation2,
+			the_geom,
+			ST_EndPoint(the_geom),
+			FALSE
+		FROM ve_link n1
+		WHERE state > 0 %s;
+	$sql$, v_query_text_aux);
+
+	EXECUTE v_query_text;
 
 	IF v_project_type = 'WS' THEN
-		UPDATE temp_link SET omzone_id=l.omzone_id, dma_id = l.dma_id, supplyzone_id = l.supplyzone_id FROM ve_link l WHERE temp_link.link_id = l.link_id AND l.exit_type IN ('NODE', 'CONNEC');
+		UPDATE temp_link SET omzone_id=l.omzone_id, dma_id = l.dma_id, supplyzone_id = l.supplyzone_id 
+		FROM ve_link l 
+		WHERE temp_link.link_id = l.link_id;
 	END IF;
 
 	-- insert duplicated features on temp_vnode
-	IF p_input = 1 THEN -- the whole ve_link
-		INSERT INTO temp_vnode (l1,v1,l2,v2)
-		SELECT n1.link_id AS l1, n1.vnode_id AS v1, n2.link_id AS l2, n2.vnode_id AS v2 FROM temp_link n1, temp_link n2
-		WHERE st_dwithin(n1.the_geom_endpoint, n2.the_geom_endpoint, 0.02)
-		AND n1.link_id < n2.link_id AND n1.exit_id = n2.exit_id ORDER BY 1;
-	ELSIF p_input = 2 THEN -- only those links wich are on arcs present on temp_anl_arc and nodes present temp_anl_node
-		INSERT INTO temp_vnode (l1,v1,l2,v2)
-		SELECT n1.link_id AS l1, n1.vnode_id AS v1, n2.link_id AS l2, n2.vnode_id AS v2 FROM temp_link n1, temp_link n2
-		WHERE st_dwithin(n1.the_geom_endpoint, n2.the_geom_endpoint, 0.02)
-		AND n1.link_id < n2.link_id
-		AND n1.feature_id IN (SELECT arc_id FROM temp_anl_arc UNION SELECT node_id FROM temp_anl_node)
-		AND n1.exit_id = n2.exit_id ORDER BY 1;
+	INSERT INTO temp_vnode (l1,v1,l2,v2)
+	SELECT 
+		n1.link_id AS l1, 
+		n1.vnode_id AS v1, 
+		n2.link_id AS l2, 
+		n2.vnode_id AS v2 
+	FROM temp_link n1
+	JOIN temp_link n2 ON st_dwithin(n1.the_geom_endpoint, n2.the_geom_endpoint, 0.02)
+	WHERE n1.link_id < n2.link_id AND n1.exit_id = n2.exit_id
+	ORDER BY 1;
+
+	IF p_input = 1 THEN 
+		v_temp_node_table := 'temp_node';
+	ELSIF p_input = 2 THEN 
+		v_temp_node_table := 'temp_anl_node';
 	END IF;
 
 	-- harmonize those links with same endpoint
@@ -86,14 +115,36 @@ BEGIN
 	WHERE a.link_id = v.l1; 
 
 	truncate temp_vnode;
-	INSERT INTO temp_vnode (l1,v1,l2,v2)
-	SELECT n1.link_id, n1.vnode_id, n2.node_id::integer, n2.node_id::integer FROM temp_link n1, temp_node n2 JOIN arc ON node_id::integer = node_1::integer
-	WHERE st_dwithin(n1.the_geom_endpoint, n2.the_geom, 0.01) AND arc_id::integer = exit_id::integer AND exit_type = 'ARC' ORDER BY 3;
+	
+	v_query_text := format($sql$
+		INSERT INTO temp_vnode (l1, v1, l2, v2)
+		SELECT 
+			n1.link_id,
+			n1.vnode_id,
+			n2.node_id::integer,
+			n2.node_id::integer
+		FROM temp_link n1
+		JOIN %I n2 ON ST_DWithin(n1.the_geom_endpoint, n2.the_geom, 0.01)
+		JOIN arc a ON n2.node_id::integer = a.node_1::integer
+		WHERE a.arc_id = n1.exit_id::integer
+		AND n1.exit_type = 'ARC'
+		ORDER BY 3;
+	$sql$, v_temp_node_table); 
 
-	INSERT INTO temp_vnode (l1,v1,l2,v2)
-	SELECT n1.link_id, n1.vnode_id, n2.node_id::integer, n2.node_id::integer FROM temp_link n1, temp_node n2 JOIN arc ON node_id::integer = node_2::integer
-	WHERE st_dwithin(n1.the_geom_endpoint, n2.the_geom, 0.01) AND arc_id::integer = exit_id::integer AND exit_type = 'ARC' ORDER BY 3;
-
+	v_query_text := format($sql$
+		INSERT INTO temp_vnode (l1, v1, l2, v2)
+		SELECT 
+			n1.link_id,
+			n1.vnode_id,
+			n2.node_id::integer,
+			n2.node_id::integer
+		FROM temp_link n1
+		JOIN %I n2 ON ST_DWithin(n1.the_geom_endpoint, n2.the_geom, 0.01)
+		JOIN arc a ON n2.node_id::integer = a.node_2::integer
+		WHERE a.arc_id = n1.exit_id::integer
+		AND n1.exit_type = 'ARC'
+		ORDER BY 3;
+	$sql$, v_temp_node_table); 
 
 	-- harmonize those links with same endpoint whith node
 	UPDATE temp_link a 
