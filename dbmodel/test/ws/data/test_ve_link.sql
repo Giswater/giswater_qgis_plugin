2de4b49f1920f49a71c2ec3dcb619055d9fa9397
arnauurgeles <aurgeles@bgeo.es>
2025-12-03 17:37:36 +0100

    fix(test): update test_ve_link insert statement to use VIRTUAL linkcat_id

--- dbmodel/test/ws/data/test_ve_link.sql
+++ dbmodel/test/ws/data/test_ve_link.sql
@@ -30,7 +30,7 @@ SELECT is((SELECT count(*)::integer FROM link WHERE code = '-901'), 0, 'DELETE: 
 
 
 INSERT INTO ve_link (link_id, code, link_type, feature_type, feature_id, exit_type, exit_id, state, expl_id, sector_id, sector_type, macrosector_id, presszone_id, presszone_type, presszone_head, dma_id, dma_type, macrodma_id, dqa_id, dqa_type, macrodqa_id, top_elev2, elevation1, fluid_type, gis_length, the_geom, muni_id, is_operative, staticpressure1, linkcat_id, workcat_id, workcat_id_end, builtdate, enddate, updated_at, updated_by, uncertain, minsector_id, verified, state_type, brand_id, model_id)
-VALUES(-901, '-901', 'VLINK', 'CONNEC', '3008', 'ARC', '2067', 1, 1, 3, 'DISTRIBUTION', 1, '3', NULL, 71.75, 2, NULL, NULL, 1, NULL, NULL, NULL, NULL, 'St. Fluid', 16.646, 'SRID=25831;LINESTRING (419084.18264611065 4576806.076099069, 419093.3076407612 4576819.998540623)'::public.geometry, 1, true, 22.741, 'PVC25-PN16', NULL, NULL, '2002-04-21', NULL, NULL, NULL, false, 113854, 0, 2, NULL, NULL);
+VALUES(-901, '-901', 'VLINK', 'CONNEC', '3008', 'ARC', '2067', 1, 1, 3, 'DISTRIBUTION', 1, '3', NULL, 71.75, 2, NULL, NULL, 1, NULL, NULL, NULL, NULL, 'St. Fluid', 16.646, 'SRID=25831;LINESTRING (419084.18264611065 4576806.076099069, 419093.3076407612 4576819.998540623)'::public.geometry, 1, true, 22.741, 'VIRTUAL', NULL, NULL, '2002-04-21', NULL, NULL, NULL, false, 113854, 0, 2, NULL, NULL);
 SELECT is((SELECT count(*)::integer FROM ve_link WHERE code = '-901'), 1, 'INSERT: ve_link -901 was inserted');
 SELECT is((SELECT count(*)::integer FROM link WHERE code = '-901'), 1, 'INSERT: link -901 was inserted');
 
