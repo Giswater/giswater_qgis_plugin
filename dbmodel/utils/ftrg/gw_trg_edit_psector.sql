42beedf1fff4d183f1f44a2c7c489517d836ff23
Arnau Torret <atorret@bgeo.es>
2025-12-05 12:00:15 +0100

    fix(psector): Be able to change creation date value

--- dbmodel/utils/ftrg/gw_trg_edit_psector.sql
+++ dbmodel/utils/ftrg/gw_trg_edit_psector.sql
@@ -65,14 +65,18 @@ BEGIN
 			NEW.workcat_id = NULL;
 		END IF;
 
+		IF NEW.creation_date IS NULL THEN
+			NEW.creation_date = NOW();
+		END IF;
+
 		NEW.psector_id:= (SELECT nextval('plan_psector_id_seq'));
 
 		-- archived is false by default
 		INSERT INTO plan_psector (psector_id, name, psector_type, descript, priority, text1, text2, observ, rotation, scale,
-		 atlas_id, gexpenses, vat, other, the_geom, expl_id, active, ext_code, status, text3, text4, text5, text6, num_value, workcat_id, workcat_id_plan, parent_id)
+		 atlas_id, gexpenses, vat, other, the_geom, expl_id, active, ext_code, status, text3, text4, text5, text6, num_value, workcat_id, workcat_id_plan, parent_id, creation_date)
 		VALUES  (NEW.psector_id, NEW.name, NEW.psector_type, NEW.descript, NEW.priority, NEW.text1, NEW.text2, NEW.observ, NEW.rotation,
 		NEW.scale, NEW.atlas_id, NEW.gexpenses, NEW.vat, NEW.other, NEW.the_geom, NEW.expl_id, NEW.active,
-		NEW.ext_code, NEW.status, NEW.text3, NEW.text4, NEW.text5, NEW.text6, NEW.num_value, NEW.workcat_id, NEW.workcat_id_plan, NEW.parent_id);
+		NEW.ext_code, NEW.status, NEW.text3, NEW.text4, NEW.text5, NEW.text6, NEW.num_value, NEW.workcat_id, NEW.workcat_id_plan, NEW.parent_id, NEW.creation_date);
 
 	    RETURN NEW;
 
@@ -100,7 +104,8 @@ BEGIN
 		SET psector_id=NEW.psector_id, name=NEW.name, psector_type=NEW.psector_type, descript=NEW.descript, priority=NEW.priority, text1=NEW.text1,
 		text2=NEW.text2, observ=NEW.observ, rotation=NEW.rotation, scale=NEW.scale, atlas_id=NEW.atlas_id,
 		gexpenses=NEW.gexpenses, vat=NEW.vat, other=NEW.other, expl_id=NEW.expl_id, active=NEW.active, ext_code=NEW.ext_code, status=NEW.status,
-		text3=NEW.text3, text4=NEW.text4, text5=NEW.text5, text6=NEW.text6, num_value=NEW.num_value, workcat_id=new.workcat_id, workcat_id_plan=new.workcat_id_plan, parent_id=new.parent_id, updated_at=now(), updated_by=current_user
+		text3=NEW.text3, text4=NEW.text4, text5=NEW.text5, text6=NEW.text6, num_value=NEW.num_value, workcat_id=new.workcat_id, workcat_id_plan=new.workcat_id_plan, 
+		parent_id=new.parent_id, updated_at=now(), updated_by=current_user, creation_date=NEW.creation_date
 		WHERE psector_id=OLD.psector_id;
 
 		-- update psector status to MADE OPERATIONAL (Archived)
