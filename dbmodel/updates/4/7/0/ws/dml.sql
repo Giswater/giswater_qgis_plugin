159cef1028e95865f222d03d3fe0d2152464373f
arnauurgeles <aurgeles@bgeo.es>
2026-01-07 15:56:50 +0100

    fix(sys_function): update input_params type from 'text, date' to 'text, integer' on gw_fct_get_epa_result_families function

--- dbmodel/updates/4/7/0/ws/dml.sql
+++ dbmodel/updates/4/7/0/ws/dml.sql
@@ -9,4 +9,4 @@ or (at your option) any later version.
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 -- 05/01/2026
-INSERT INTO sys_function (id, function_name, project_type, function_type, input_params, return_type, descript, sys_role, sample_query, "source", function_alias) VALUES(3528, 'gw_fct_get_epa_result_families', 'ws', 'function', 'text, date', 'json', 'Function to get json with EPA result families.', NULL, NULL, 'core', NULL);
+INSERT INTO sys_function (id, function_name, project_type, function_type, input_params, return_type, descript, sys_role, sample_query, "source", function_alias) VALUES(3528, 'gw_fct_get_epa_result_families', 'ws', 'function', 'text, integer', 'json', 'Function to get json with EPA result families.', NULL, NULL, 'core', NULL);
