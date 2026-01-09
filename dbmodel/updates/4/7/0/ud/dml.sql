aad9606a4428172395c6d136b1c0119f4f0a4127
arnauurgeles <aurgeles@bgeo.es>
2026-01-09 12:30:07 +0100

    fix(sys_param_user): update descript for edit_node_topelev_options

--- dbmodel/updates/4/7/0/ud/dml.sql
+++ dbmodel/updates/4/7/0/ud/dml.sql
@@ -14,3 +14,6 @@ INSERT INTO omunit (omunit_id) VALUES(0) ON CONFLICT (omunit_id) DO NOTHING;
 INSERT INTO sys_param_user (id,formname,descript,sys_role,"label",isenabled,layoutorder,project_type,isparent,isautoupdate,"datatype",widgettype,ismandatory,layoutname,iseditable,"source")
 VALUES ('edit_insert_show_elevation_from_dem','config','If true, the elevation will be showed from the DEM raster when inserting a new feature','role_edit','Show elevation from DEM:',true,28,'utils',false,false,'boolean','check',false,'lyt_other',true,'core');
 INSERT INTO config_param_user ("parameter", value, cur_user) VALUES('edit_insert_show_elevation_from_dem', 'true', 'postgres');
+
+UPDATE sys_param_user SET descript = 'If elev, try to recalculate elev if ymax is not null. If ymax, try to recalculate ymax if elev is not null.'
+WHERE id = 'edit_node_topelev_options';
