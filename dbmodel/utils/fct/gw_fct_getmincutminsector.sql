b45f419bc34ddb73e6d7911c93cd0b3b00f80be6
mguzman14 <mguzman@bgeo.es>
2026-01-14 13:14:13 +0100

    fix: finish code of getmincutminsector

--- dbmodel/utils/fct/gw_fct_getmincutminsector.sql
+++ dbmodel/utils/fct/gw_fct_getmincutminsector.sql
@@ -5,51 +5,69 @@ General Public License as published by the Free Software Foundation, either vers
 or (at your option) any later version.
 */
 
--- DROP FUNCTION SCHEMA_NAME.gw_fct_getfeaturesfrompolygon(json);
 
-CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_getfeaturesfrompolygon(p_data json)
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_getmincutminsector(p_data json)
  RETURNS json
  LANGUAGE plpgsql
 AS $function$
 
 -- Function id: 3536
 
-
 /*
 EXAMPLE:
 
-SELECT gw_fct_getfeaturesfrompolygon($${"client":{"device":4, "lang":"es_ES", "version":"4.0.001", "infoType":1, "epsg":25831},
-"form":{}, "feature":{}, "data":{"filterFields":{}, "pageInfo":{}, "parameters": {"mincutMinsectorId":"12345"}}}$$);
+SELECT gw_fct_getmincutminsector($${"client":{"device":4, "lang":"es_ES", "version":"4.0.001", "infoType":1, "epsg":25831},
+"form":{}, "feature":{}, "data":{"filterFields":{}, "pageInfo":{}, "parameters": {"mincutId":"10"}}}$$);
 
 */
 
 DECLARE
-v_mincut_minsector_id INTEGER;
-v_minsector_affected JSON;
+v_mincut_id INTEGER;
+v_minsector_affected INTEGER;
 v_minsector_dependent_arcs JSON;
 v_proposed_valves JSON;
+v_arc_id TEXT;
+v_version TEXT;
 
+v_status TEXT;
+v_message JSON;
+v_data JSON;
+v_error_context TEXT;
 
 BEGIN
 
 	SET search_path = "SCHEMA_NAME", public;
 
 	-- Get input parameters
+    v_mincut_id := (p_data->'data'->'parameters'->>'mincutId')::INTEGER;
+
 	SELECT giswater INTO v_version FROM sys_version ORDER BY id DESC LIMIT 1;
+	SELECT anl_feature_id INTO v_arc_id FROM om_mincut WHERE id = v_mincut_id;
 
-    v_mincut_minsector_id := (p_data->'data'->'parameters'->>'mincutMinsectorId')::INTEGER;
+    -- Minsector affected
+	SELECT minsector_id INTO v_minsector_affected FROM arc WHERE arc_id::text = v_arc_id;
+
+    -- Dependent arcs from the minsector
+	SELECT json_agg(arc_id) INTO v_minsector_dependent_arcs FROM arc WHERE minsector_id IN (
+		SELECT mincut_minsector_id FROM minsector_mincut WHERE minsector_id = v_minsector_affected
+	);
+
+    -- Proposed valves
+	SELECT json_agg(js) FROM (
+		SELECT json_build_object('toOpen', proposed, 'node_id', json_agg(node_id)) AS js 
+		FROM om_mincut_valve WHERE result_id = v_mincut_id GROUP BY proposed
+	) INTO v_proposed_valves;
 
 
-    -- minsector affected
+	-- Control NULLs
+	v_minsector_affected := COALESCE(v_minsector_affected, 0);
+	v_minsector_dependent_arcs := COALESCE(v_minsector_dependent_arcs, '{}');
+	v_proposed_valves := COALESCE(v_proposed_valves, '{}');
 
-
-    -- dependent arcs from the minsector
-
-
-    -- proposed valves
-
-
-
+   	v_status := COALESCE(v_status, '');
+   	v_message := COALESCE(v_message, '{}');
+	v_version := COALESCE(v_version, '');
+    v_data := COALESCE(v_data, '{}');
 
     SELECT JSONB_BUILD_OBJECT(
         'minsectorAffected', v_minsector_affected,
@@ -57,15 +75,7 @@ BEGIN
         'proposedValves', v_proposed_valves
     ) INTO v_data;
 
-   
-	-- Control NULLs
-   	v_status := COALESCE(v_status, '');
-   	v_message := COALESCE(v_message, '{}');
-	v_version := COALESCE(v_version, '');
-    v_data := COALESCE(v_data, '{}');
-
-
-	--  Return
+   	--  Return
 	RETURN ('{"status":"'||v_status||'", "message":'||v_message||', "version":"'||v_version||'","body":{"form":{},"data":'||v_data||'}}')::json;
 
 	EXCEPTION WHEN OTHERS THEN
