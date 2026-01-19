2b4c86dd2434b122bacacbe6b84898df47dac61d
claudia dragoste <claudiadragoste@yahoo.com>
2026-01-19 18:36:46 +0100

    fix(graphinundation): bug prev commit

--- dbmodel/utils/fct/gw_fct_getgraphinundation.sql
+++ dbmodel/utils/fct/gw_fct_getgraphinundation.sql
@@ -22,12 +22,18 @@ $BODY$
 DECLARE
     geojson_result json;
 	v_version text;
-    v_querytext text;
 
     v_mapzone text;
     v_mapzone_field text;
 
+    -- response variables
+
+    v_error_context text;
+
 BEGIN
+
+    -- Search path
+	SET search_path = "SCHEMA_NAME", public;
 
 	SELECT giswater INTO v_version FROM sys_version ORDER BY id DESC LIMIT 1;
 
@@ -47,23 +53,31 @@ BEGIN
 			'properties', to_jsonb(r) - 'the_geom'
 			) AS feature
 			FROM (
-			SELECT
-				SELECT a.arc_id, d.start_vid, a.node_1, a.node_2, a.arccat_id, a.state, a.expl_id,
-				array_to_string(m.mapzone_ids, ',') AS %I,
-				(a.%I)::text AS %I,
-				a.the_geom,
-				m.name AS descript
-			FROM temp_pgr_arc ta
-            JOIN temp_pgr_drivingdistance d ON ta.pgr_arc_id = d.node
-			JOIN arc a ON a.arc_id = ta.pgr_arc_id
-			JOIN temp_pgr_mapzone m ON m.component = ta.component
-			WHERE ta.mapzone_id IN (0, -1)
-			%s
+                SELECT 
+                    a.arc_id, 
+                    d.start_vid, 
+                    a.node_1, 
+                    a.node_2, 
+                    ca.arc_type, 
+                    a.arccat_id, 
+                    a.state, 
+                    a.state_type,
+                    v.is_operative,
+                    array_to_string(m.mapzone_ids, ',') AS %I,
+                    (a.%I)::text AS %I,
+                    m.name AS descript,
+                    a.the_geom,
+                    (date_trunc('day', now()) + d.agg_cost * interval '1 second')::timestamp AS timestep
+                FROM temp_pgr_arc ta
+                JOIN temp_pgr_drivingdistance d ON ta.pgr_arc_id = d.node
+                JOIN arc a ON a.arc_id = ta.pgr_arc_id
+                JOIN cat_arc ca ON ca.id = a.arccat_id
+                JOIN value_state_type v ON v.id = a.state_type 
+                JOIN temp_pgr_mapzone m ON m.component = ta.component
 			) r
 		) f
 		$sql$,
-		v_mapzone_field, v_mapzone_field, 'old_' || v_mapzone_field,
-		v_query_text_aux
+		v_mapzone_field, v_mapzone_field, 'old_' || v_mapzone_field
 		) INTO geojson_result;
 
     RETURN gw_fct_json_create_return((
@@ -94,7 +108,7 @@ BEGIN
 		'SQLSTATE', SQLSTATE,
 		'SQLCONTEXT', v_error_context
 	);
-    
+
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
