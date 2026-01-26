2dddf30e270b64834b6721f909be5d44f0588846
smaspons <smaspons@bgeo.es>
2026-01-26 12:57:49 +0100

    fix(ci, docker): use psql with socket mode [skip ci]

--- dbmodel/docker/gw-db/restore.sh
+++ dbmodel/docker/gw-db/restore.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Create extensions if they don't exist.
-psql -h localhost -U postgres -d gw_db -c 'CREATE EXTENSION IF NOT EXISTS postgis;'
-psql -h localhost -U postgres -d gw_db -c 'CREATE EXTENSION IF NOT EXISTS pgrouting;'
-psql -h localhost -U postgres -d gw_db -c 'CREATE EXTENSION IF NOT EXISTS postgis_raster;'
-psql -h localhost -U postgres -d gw_db -c 'CREATE EXTENSION IF NOT EXISTS postgis_topology;'
-psql -h localhost -U postgres -d gw_db -c 'CREATE EXTENSION IF NOT EXISTS pgtap;'
+# Create extensions.
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE EXTENSION IF NOT EXISTS pgrouting;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE EXTENSION IF NOT EXISTS postgis_raster;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE EXTENSION IF NOT EXISTS postgis_topology;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE EXTENSION IF NOT EXISTS pgtap;"
 
-# Create roles if they don't exist.
-psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_admin;'
-psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_basic;'
-psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_crm;'
-psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_edit;'
-psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_epa;'
-psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_om;'
-psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_plan;'
-psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_system;'
+# Create roles
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE ROLE role_admin;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE ROLE role_basic;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE ROLE role_crm;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE ROLE role_edit;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE ROLE role_epa;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE ROLE role_om;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE ROLE role_plan;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE ROLE role_system;"
 
-# Grant roles to roles if they don't exist.
-psql -h localhost -U postgres -d gw_db -c 'GRANT role_plan TO role_admin;'
-psql -h localhost -U postgres -d gw_db -c 'GRANT role_om TO role_edit;'
-psql -h localhost -U postgres -d gw_db -c 'GRANT role_edit TO role_epa;'
-psql -h localhost -U postgres -d gw_db -c 'GRANT role_basic TO role_om;'
-psql -h localhost -U postgres -d gw_db -c 'GRANT role_epa TO role_plan;'
-psql -h localhost -U postgres -d gw_db -c 'GRANT role_admin TO role_system;'
+# Grant roles.
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "GRANT role_plan TO role_admin;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "GRANT role_om TO role_edit;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "GRANT role_edit TO role_epa;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "GRANT role_basic TO role_om;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "GRANT role_epa TO role_plan;"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "GRANT role_admin TO role_system;"
 
 # Skip topology schema managed by PostGIS to avoid conflict with existing extension.
 pg_restore \
