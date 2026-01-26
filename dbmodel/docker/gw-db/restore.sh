edf7d8354028007e0438fc318f694fabea14dffc
smaspons <smaspons@bgeo.es>
2026-01-26 12:44:54 +0100

    fix(ci, docker): create roles on restore [skip ci]

--- dbmodel/docker/gw-db/restore.sh
+++ dbmodel/docker/gw-db/restore.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Restore roles/privileges if globals exist, then restore DB dump.
-if [ -f /docker-entrypoint-initdb.d/00-gw_globals.sql ]; then
-  psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d postgres /docker-entrypoint-initdb.d/00-gw_globals.sql
-fi
+# Create extensions if they don't exist.
+psql -h localhost -U postgres -d gw_db -c 'CREATE EXTENSION IF NOT EXISTS postgis;'
+psql -h localhost -U postgres -d gw_db -c 'CREATE EXTENSION IF NOT EXISTS pgrouting;'
+psql -h localhost -U postgres -d gw_db -c 'CREATE EXTENSION IF NOT EXISTS postgis_raster;'
+psql -h localhost -U postgres -d gw_db -c 'CREATE EXTENSION IF NOT EXISTS postgis_topology;'
+psql -h localhost -U postgres -d gw_db -c 'CREATE EXTENSION IF NOT EXISTS pgtap;'
+
+# Create roles if they don't exist.
+psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_admin;'
+psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_basic;'
+psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_crm;'
+psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_edit;'
+psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_epa;'
+psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_om;'
+psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_plan;'
+psql -h localhost -U postgres -d gw_db -c 'CREATE ROLE IF NOT EXISTS role_system;'
+
+# Grant roles to roles if they don't exist.
+psql -h localhost -U postgres -d gw_db -c 'GRANT role_plan TO role_admin;'
+psql -h localhost -U postgres -d gw_db -c 'GRANT role_om TO role_edit;'
+psql -h localhost -U postgres -d gw_db -c 'GRANT role_edit TO role_epa;'
+psql -h localhost -U postgres -d gw_db -c 'GRANT role_basic TO role_om;'
+psql -h localhost -U postgres -d gw_db -c 'GRANT role_epa TO role_plan;'
+psql -h localhost -U postgres -d gw_db -c 'GRANT role_admin TO role_system;'
 
 # Skip topology schema managed by PostGIS to avoid conflict with existing extension.
 pg_restore \
