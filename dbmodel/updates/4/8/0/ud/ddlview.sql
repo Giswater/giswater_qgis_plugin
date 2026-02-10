393c9b68474de48bf76641399d651648eb210342
arnauurgeles <aurgeles@bgeo.es>
2026-02-10 10:07:31 +0100

    fix(v_ui_node_x_connection): update references to v_edit parent views

--- dbmodel/updates/4/8/0/ud/ddlview.sql
+++ dbmodel/updates/4/8/0/ud/ddlview.sql
@@ -148,3 +148,151 @@ CREATE OR REPLACE VIEW v_ui_om_visit_x_doc
 AS SELECT doc_id,
     visit_id
    FROM doc_x_visit;
+
+-- 10/02/2026
+CREATE OR REPLACE VIEW v_ui_node_x_connection_downstream
+AS SELECT row_number() OVER (ORDER BY ve_arc.node_1) + 1000000 AS rid,
+    ve_arc.node_1 AS node_id,
+    ve_arc.arc_id AS feature_id,
+    ve_arc.code AS feature_code,
+    ve_arc.arc_type AS featurecat_id,
+    ve_arc.arccat_id,
+    ve_arc.y2 AS depth,
+    st_length2d(ve_arc.the_geom)::numeric(12,2) AS length,
+    node.node_id AS downstream_id,
+    node.code AS downstream_code,
+    node.node_type::text AS downstream_type,
+    ve_arc.y1 AS downstream_depth,
+    ve_arc.sys_type,
+    st_x(st_lineinterpolatepoint(ve_arc.the_geom, 0.5::double precision)) AS x,
+    st_y(st_lineinterpolatepoint(ve_arc.the_geom, 0.5::double precision)) AS y,
+    cat_arc.descript,
+    value_state.name AS state,
+    've_arc'::text AS sys_table_id
+   FROM ve_arc
+     JOIN node ON ve_arc.node_2 = node.node_id
+     LEFT JOIN cat_arc ON ve_arc.arccat_id::text = cat_arc.id::text
+     JOIN value_state ON ve_arc.state = value_state.id;
+
+
+CREATE OR REPLACE VIEW v_ui_node_x_connection_upstream
+AS SELECT row_number() OVER (ORDER BY ve_arc.node_2) + 1000000 AS rid,
+    ve_arc.node_2 AS node_id,
+    ve_arc.arc_id AS feature_id,
+    ve_arc.code AS feature_code,
+    ve_arc.arc_type AS featurecat_id,
+    ve_arc.arccat_id,
+    ve_arc.y1 AS depth,
+    st_length2d(ve_arc.the_geom)::numeric(12,2) AS length,
+    node.node_id AS upstream_id,
+    node.code AS upstream_code,
+    node.node_type AS upstream_type,
+    ve_arc.y2 AS upstream_depth,
+    ve_arc.sys_type,
+    st_x(st_lineinterpolatepoint(ve_arc.the_geom, 0.5::double precision)) AS x,
+    st_y(st_lineinterpolatepoint(ve_arc.the_geom, 0.5::double precision)) AS y,
+    cat_arc.descript,
+    value_state.name AS state,
+    've_arc'::text AS sys_table_id
+   FROM ve_arc
+     JOIN node ON ve_arc.node_1 = node.node_id
+     LEFT JOIN cat_arc ON ve_arc.arccat_id::text = cat_arc.id::text
+     JOIN value_state ON ve_arc.state = value_state.id
+UNION
+ SELECT DISTINCT ON (ve_connec.connec_id) row_number() OVER (ORDER BY node.node_id) + 2000000 AS rid,
+    node.node_id,
+    ve_connec.connec_id AS feature_id,
+    ve_connec.code AS feature_code,
+    ve_connec.connec_type AS featurecat_id,
+    ve_connec.conneccat_id AS arccat_id,
+    ve_connec.y1 AS depth,
+    st_length2d(link.the_geom)::numeric(12,2) AS length,
+    ve_connec.connec_id AS upstream_id,
+    ve_connec.code AS upstream_code,
+    ve_connec.connec_type AS upstream_type,
+    ve_connec.y2 AS upstream_depth,
+    ve_connec.sys_type,
+    st_x(ve_connec.the_geom) AS x,
+    st_y(ve_connec.the_geom) AS y,
+    cat_connec.descript,
+    value_state.name AS state,
+    've_connec'::text AS sys_table_id
+   FROM ve_connec
+     JOIN link ON link.feature_id = ve_connec.connec_id AND link.feature_type::text = 'CONNEC'::text
+     JOIN node ON ve_connec.pjoint_id = node.node_id AND ve_connec.pjoint_type::text = 'NODE'::text
+     LEFT JOIN cat_connec ON ve_connec.conneccat_id::text = cat_connec.id::text
+     JOIN value_state ON ve_connec.state = value_state.id
+UNION
+ SELECT DISTINCT ON (ve_connec.connec_id) row_number() OVER (ORDER BY node.node_id) + 3000000 AS rid,
+    node.node_id,
+    ve_connec.connec_id AS feature_id,
+    ve_connec.code AS feature_code,
+    ve_connec.connec_type AS featurecat_id,
+    ve_connec.conneccat_id AS arccat_id,
+    ve_connec.y1 AS depth,
+    st_length2d(link.the_geom)::numeric(12,2) AS length,
+    ve_connec.connec_id AS upstream_id,
+    ve_connec.code AS upstream_code,
+    ve_connec.connec_type AS upstream_type,
+    ve_connec.y2 AS upstream_depth,
+    ve_connec.sys_type,
+    st_x(ve_connec.the_geom) AS x,
+    st_y(ve_connec.the_geom) AS y,
+    cat_connec.descript,
+    value_state.name AS state,
+    've_connec'::text AS sys_table_id
+   FROM ve_connec
+     JOIN link ON link.feature_id = ve_connec.connec_id AND link.feature_type::text = 'CONNEC'::text AND link.exit_type::text = 'CONNEC'::text
+     JOIN connec ON connec.connec_id = link.exit_id AND connec.pjoint_type::text = 'NODE'::text
+     JOIN node ON connec.pjoint_id = node.node_id
+     LEFT JOIN cat_connec ON ve_connec.conneccat_id::text = cat_connec.id::text
+     JOIN value_state ON ve_connec.state = value_state.id
+UNION
+ SELECT DISTINCT ON (ve_gully.gully_id) row_number() OVER (ORDER BY node.node_id) + 4000000 AS rid,
+    node.node_id,
+    ve_gully.gully_id AS feature_id,
+    ve_gully.code AS feature_code,
+    ve_gully.gully_type AS featurecat_id,
+    ve_gully.connec_arccat_id AS arccat_id,
+    ve_gully.ymax - ve_gully.sandbox AS depth,
+    ve_gully.connec_length AS length,
+    ve_gully.gully_id AS upstream_id,
+    ve_gully.code AS upstream_code,
+    ve_gully.gully_type AS upstream_type,
+    ve_gully.connec_depth AS upstream_depth,
+    ve_gully.sys_type,
+    st_x(ve_gully.the_geom) AS x,
+    st_y(ve_gully.the_geom) AS y,
+    cat_connec.descript,
+    value_state.name AS state,
+    've_gully'::text AS sys_table_id
+   FROM ve_gully
+     JOIN link ON link.feature_id = ve_gully.gully_id AND link.feature_type::text = 'GULLY'::text
+     JOIN node ON ve_gully.pjoint_id = node.node_id AND ve_gully.pjoint_type::text = 'NODE'::text
+     LEFT JOIN cat_connec ON ve_gully.connec_arccat_id::text = cat_connec.id::text
+     JOIN value_state ON ve_gully.state = value_state.id
+UNION
+ SELECT DISTINCT ON (ve_gully.gully_id) row_number() OVER (ORDER BY node.node_id) + 5000000 AS rid,
+    node.node_id,
+    ve_gully.gully_id AS feature_id,
+    ve_gully.code AS feature_code,
+    ve_gully.gully_type AS featurecat_id,
+    ve_gully.connec_arccat_id AS arccat_id,
+    ve_gully.ymax - ve_gully.sandbox AS depth,
+    ve_gully.connec_length AS length,
+    ve_gully.gully_id AS upstream_id,
+    ve_gully.code AS upstream_code,
+    ve_gully.gully_type AS upstream_type,
+    ve_gully.connec_depth AS upstream_depth,
+    ve_gully.sys_type,
+    st_x(ve_gully.the_geom) AS x,
+    st_y(ve_gully.the_geom) AS y,
+    cat_connec.descript,
+    value_state.name AS state,
+    've_gully'::text AS sys_table_id
+   FROM ve_gully
+     JOIN link ON link.feature_id = ve_gully.gully_id AND link.feature_type::text = 'GULLY'::text AND link.exit_type::text = 'GULLY'::text
+     JOIN gully ON gully.gully_id = link.exit_id AND gully.pjoint_type::text = 'NODE'::text
+     JOIN node ON gully.pjoint_id = node.node_id
+     LEFT JOIN cat_connec ON ve_gully.connec_arccat_id::text = cat_connec.id::text
+     JOIN value_state ON ve_gully.state = value_state.id;
