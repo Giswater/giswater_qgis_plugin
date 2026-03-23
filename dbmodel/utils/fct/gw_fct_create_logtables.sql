f1ed5b1b5b4d863ed3ee42a6d59be3e6be086b9d
arnauurgeles <aurgeles@bgeo.es>
2026-03-23 16:15:17 +0100

    fix(logtables): move blank line above section headers

--- dbmodel/utils/fct/gw_fct_create_logtables.sql
+++ dbmodel/utils/fct/gw_fct_create_logtables.sql
@@ -140,17 +140,17 @@ BEGIN
 		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-13, current_user, v_fid, 4, '-------------------------------------------------------------------------------------');
 		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-14, current_user, v_fid, 4, '');
 
-		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-9, current_user, v_fid, 3, 'CRITICAL ERRORS');
-		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-10, current_user, v_fid, 3, '----------------------');
-		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-11, current_user, v_fid, 3, '');
+		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-9, current_user, v_fid, 3, '');
+		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-10, current_user, v_fid, 3, 'CRITICAL ERRORS');
+		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-11, current_user, v_fid, 3, '----------------------');
 
-		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-6, current_user, v_fid, 2, 'WARNINGS');
-		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-7, current_user, v_fid, 2, '--------------');
-		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-8, current_user, v_fid, 2, '');
+		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-6, current_user, v_fid, 2, '');
+		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-7, current_user, v_fid, 2, 'WARNINGS');
+		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-8, current_user, v_fid, 2, '--------------');
 
-		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-3, current_user, v_fid, 1, 'INFO');
-		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-4, current_user, v_fid, 1, '-------');
-		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-5, current_user, v_fid, 1, '');
+		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-3, current_user, v_fid, 1, '');
+		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-4, current_user, v_fid, 1, 'INFO');
+		INSERT INTO t_audit_check_data (id, cur_user, fid, criticity, error_message) VALUES (-5, current_user, v_fid, 1, '-------');
 
 		INSERT INTO t_audit_check_data (fid, cur_user, criticity, error_message) VALUES (v_fid, current_user, 4, concat('Result id: ', v_result_id));
 		INSERT INTO t_audit_check_data (fid, cur_user, criticity, error_message) VALUES (v_fid, current_user, 4, concat('Created by: ', current_user, ', on ', to_char(now(),'YYYY/MM/DD - HH:MM:SS')));
