4c9b7661504ce7b17f414496d4335156f5fa8f5e
mguzman14 <mguzman@bgeo.es>
2026-03-27 14:37:11 +0100

    fix: create index for plot_code.connec

---
+++ dbmodel/updates/4/9/0/utils/tablect.sql
@@ -0,0 +1,9 @@
+/*
+This file is part of Giswater
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+
+
+CREATE INDEX connec_plot_code ON connec USING btree (plot_code);
