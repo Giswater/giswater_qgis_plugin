c53a702f88c62fcb98184f6319175fbbd4654060
Xavier Torret - BGEO <xtorret@bgeo.es>
2026-04-04 14:10:46 +0200

    feat(family for calibration): Add more families and more thresholds for calibration

---
+++ dbmodel/updates/4/8/1/ws/dml.sql
@@ -0,0 +1,23 @@
+/*
+This file is part of Giswater
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+-- 04/04/2026
+INSERT INTO inp_family (family_id,descript,age) VALUES
+	 ('PRV','Pressure Reducing Valve',10),
+	 ('PBV','Pressure Breaker Valve',10),
+	 ('GPV','General Purpose Valve',10),
+	 ('FCV','Flow Control Valve',10),
+	 ('TCV','Throttle Control Valve',10),
+	 ('PSV','Pressure Sustaining Valve',10),
+	 ('HEADPUMP','Head pump (buster)',10),
+	 ('POWERPUMP','Power pump (flow)',10),
+	 ('RESERVOIR','Reservoir',10),
+	 ('TANK','Tank',10),
+	 ('JUNCTION','Junction',10);
