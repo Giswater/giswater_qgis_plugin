6aee494af432d4a3af7af23de5a74eb5999a41a9
Daniel Marín <dmarin@bgeo.es>
2026-04-07 15:16:29 +0200

    refactor(test_gw_fct_setfields): update test cases to include additional psector scenarios and adjust plan count

--- dbmodel/test/ud/function/test_gw_fct_setfields.sql
+++ dbmodel/test/ud/function/test_gw_fct_setfields.sql
@@ -11,24 +11,8 @@ SET client_min_messages TO WARNING;
 
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
--- Plan for 2 test
-SELECT plan(2);
-
--- Create roles for testing
-CREATE USER plan_user;
-GRANT role_plan to plan_user;
-
-CREATE USER epa_user;
-GRANT role_epa to epa_user;
-
-CREATE USER edit_user;
-GRANT role_edit to edit_user;
-
-CREATE USER om_user;
-GRANT role_om to om_user;
-
-CREATE USER basic_user;
-GRANT role_basic to basic_user;
+-- Plan for 4 test
+SELECT plan(4);
 
 -- Extract and test the "status" field from the function's JSON response
 SELECT is(
@@ -48,6 +32,51 @@ SELECT is (
     'Accepted',
     'Check if gw_fct_setfields --> "tableName":"ve_inp_timeseries" returns status "Accepted"'
 );
+
+-- PSECTOR TESTS
+UPDATE config_param_user SET value = NULL WHERE "parameter" = 'plan_psector_current' AND cur_user = current_user;
+
+-- setfields with psector active (replace node)
+INSERT INTO ve_plan_psector ( "name", descript, priority, text1, text2, observ, rotation, "scale", atlas_id, gexpenses, vat, other, the_geom, expl_id, psector_type, active, ext_code, status, text3, text4, text5, text6, num_value, workcat_id, parent_id)
+VALUES('-901', 'Test psector for setfields', '2', NULL, NULL, 'Test psector for setfields', 0.0000, 750.00, '01', 19.00, 21.00, 0.00, 'SRID=25831;MULTIPOLYGON (((419209.60503196524 4576553.424854076, 419209.60503196524 4576503.357122214, 419089.7020131364 4576503.357122214, 419089.7020131364 4576553.424854076, 419209.60503196524 4576553.424854076)))'::public.geometry, 1, 1, true, NULL, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+DELETE FROM selector_psector WHERE cur_user = current_user;
+UPDATE config_param_user SET value = (SELECT id FROM ve_plan_psector WHERE name = '-901')
+WHERE "parameter" = 'plan_psector_current' AND cur_user = current_user;
+INSERT INTO selector_psector (psector_id, cur_user) VALUES ((SELECT id FROM ve_plan_psector WHERE name = '-901'), current_user);
+
+SELECT is (
+    (gw_fct_setfields($${"client":{"device":4, "lang":"en_US", "version":"4.8.1", "infoType":1, "epsg":25831}, "form":{}, 
+    "feature":{"id":"100055", "tableName":"ve_node_junction", "featureType":"node" }, "data":{"filterFields":{}, "pageInfo":{}, 
+    "fields":{"sector_id": "1", "dwfzone_id": "1", "state": "2", "state_type": "3", "code": "100055", "workcat_id": "work1", 
+    "builtdate": "2026/04/07", "unconnected": "False", "soilcat_id": "soil1", "expl_id": "1", "brand_id": "brand3", "model_id": "model4", 
+    "muni_id": "1", "node_type": "JUNCTION", "epa_type": "JUNCTION", "nodecat_id": "JUNCTION-01",
+    "the_geom": "Point (419100.72600000002421439 4576521.49000000022351742)"}, "reload":""}}$$)::JSON)->>'status',
+    'Accepted',
+    'Check if gw_fct_setfields --> "tableName":"ve_node_junction" (Active psector) returns status "Accepted"'
+);
+
+-- setfields with psector active (add new node and setarcdivide)
+INSERT INTO ve_plan_psector ( "name", descript, priority, text1, text2, observ, rotation, "scale", atlas_id, gexpenses, vat, other, the_geom, expl_id, psector_type, active, ext_code, status, text3, text4, text5, text6, num_value, workcat_id, parent_id)
+VALUES('-902', 'Test psector for setfields with arcdivide', '2', NULL, NULL, 'Test psector for setfields', 0.0000, 750.00, '01', 19.00, 21.00, 0.00, 'SRID=25831;MULTIPOLYGON (((419209.60503196524 4576553.424854076, 419209.60503196524 4576503.357122214, 419089.7020131364 4576503.357122214, 419089.7020131364 4576553.424854076, 419209.60503196524 4576553.424854076)))'::public.geometry, 1, 1, true, NULL, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+UPDATE config_param_user SET value = NULL WHERE "parameter" = 'plan_psector_current' AND cur_user = current_user;
+DELETE FROM selector_psector WHERE cur_user = current_user;
+UPDATE config_param_user SET value = (SELECT id FROM ve_plan_psector WHERE name = '-902')
+WHERE "parameter" = 'plan_psector_current' AND cur_user = current_user;
+INSERT INTO selector_psector (psector_id, cur_user) VALUES ((SELECT id FROM ve_plan_psector WHERE name = '-902'), current_user);
+
+SELECT is (
+    (gw_fct_setfields($${"client":{"device":4, "lang":"en_US", "version":"4.8.1", "infoType":1, "epsg":25831}, "form":{}, 
+    "feature":{"id":"100058", "tableName":"ve_node_junction", "featureType":"node" }, "data":{"filterFields":{}, "pageInfo":{}, 
+    "fields":{"sector_id": "1", "dwfzone_id": "1", "state": "2", "state_type": "3", "code": "100058", "workcat_id": "work1", 
+    "builtdate": "2026/04/07", "unconnected": "False", "soilcat_id": "soil1", "expl_id": "1", "brand_id": "brand3", "model_id": "model4", 
+    "muni_id": "1", "node_type": "JUNCTION", "epa_type": "JUNCTION", "nodecat_id": "JUNCTION-01", 
+    "the_geom": "Point (419105.49698512122267857 4576544.63723930902779102)"}, "reload":""}}$$)::JSON)->>'status',
+    'Accepted',
+    'Check if gw_fct_setfields --> "tableName":"ve_node_junction" (Active psector) returns status "Accepted"'
+);
+
 
 -- Finish the test
 SELECT finish();
