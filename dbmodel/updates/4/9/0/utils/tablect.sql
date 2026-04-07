a874774763fbc37fd0ea1246ade658060a8f8b26
mguzman14 <mguzman@bgeo.es>
2026-04-07 17:28:48 +0200

    feat: add fkey to expl_visibility

--- dbmodel/updates/4/9/0/utils/tablect.sql
+++ dbmodel/updates/4/9/0/utils/tablect.sql
@@ -13,3 +13,18 @@ CREATE INDEX connec_plot_code ON connec USING btree (code);
 ALTER TABLE connec ADD CONSTRAINT connec_plot_id_fkey FOREIGN KEY (plot_id) REFERENCES ext_plot(id) ON UPDATE CASCADE ON DELETE RESTRICT;
 
 ALTER TABLE cat_feature_node ALTER COLUMN graph_delimiter SET DEFAULT '{NONE}';
+
+CREATE TRIGGER gw_trg_fk_array_array_table_expl AFTER INSERT OR UPDATE ON node
+FOR EACH ROW EXECUTE FUNCTION gw_trg_array_fk_array_table('expl_visibility', 'exploitation', 'expl_id');
+
+CREATE TRIGGER gw_trg_fk_array_array_table_expl AFTER INSERT OR UPDATE ON arc
+FOR EACH ROW EXECUTE FUNCTION gw_trg_array_fk_array_table('expl_visibility', 'exploitation', 'expl_id');
+
+CREATE TRIGGER gw_trg_fk_array_array_table_expl AFTER INSERT OR UPDATE ON connec
+FOR EACH ROW EXECUTE FUNCTION gw_trg_array_fk_array_table('expl_visibility', 'exploitation', 'expl_id');
+
+CREATE TRIGGER gw_trg_fk_array_array_table_expl AFTER INSERT OR UPDATE ON link
+FOR EACH ROW EXECUTE FUNCTION gw_trg_array_fk_array_table('expl_visibility', 'exploitation', 'expl_id');
+
+CREATE TRIGGER gw_trg_fk_array_array_table_expl AFTER INSERT OR UPDATE ON element
+FOR EACH ROW EXECUTE FUNCTION gw_trg_array_fk_array_table('expl_visibility', 'exploitation', 'expl_id');
