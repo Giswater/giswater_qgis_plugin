36646a2e87be0ae2924951e889c8915e7019cf1d
arnauurgeles <aurgeles@bgeo.es>
2026-05-04 15:10:23 +0200

    fix(dml.sql): remove config_param_system update for epa_automatic_man2graph_values

--- dbmodel/updates/4/9/0/ws/dml.sql
+++ dbmodel/updates/4/9/0/ws/dml.sql
@@ -422,13 +422,6 @@ VALUES('mapzone_manager', 'form_mapzone', 'tab_none', 'tab_main', 'lyt_mapzone_m
   ]
 }'::json, NULL, NULL, false, 10);
 
-UPDATE config_param_system
-	SET value='{"status":true, "values":[
-{"sourceTable":"ve_node_pr_reduc_valve", "query":"UPDATE presszone t SET head = CASE WHEN top_elev + pressure_exit IS NOT NULL THEN top_elev + pressure_exit ELSE t.head END FROM ve_node_pr_reduc_valve s "},
-{"sourceTable":"ve_node_pump", "query":"UPDATE presszone t SET head = CASE WHEN top_elev + pressure_exit IS NOT NULL THEN top_elev + pressure_exit ELSE t.head END FROM ve_node_pump s "},
-{"sourceTable":"ve_node_tank", "query":"UPDATE presszone t SET head= CASE WHEN invert_level + hmax IS NOT NULL THEN invert_level + hmax ELSE t.head END FROM ve_node_tank s "}]}'
-	WHERE "parameter"='epa_automatic_man2graph_values';
-
 UPDATE config_toolbox SET id = 2212 WHERE id = 2302;
 DELETE FROM sys_function WHERE id = 2302 AND function_name = 'gw_fct_anl_node_topological_consistency';
 
