7bd385fa7256e36571f758d7320b2112b6006d7e
Daniel Marín <dmarin@bgeo.es>
2026-05-20 13:11:37 +0200

    fix(dml): update plan_psector status and clean up plan_typevalue entries

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Removed
 
 - `sector_id` and `muni_id` from `exploitation` table.
+- `psector_status` typevalue `4` (EXECUTED).
 
 ### Fixed
 
--- dbmodel/updates/4/11/0/utils/dml.sql
+++ dbmodel/updates/4/11/0/utils/dml.sql
@@ -11,3 +11,10 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 -- 19/05/2026
 DELETE FROM config_form_fields WHERE formname='ve_exploitation' AND formtype='form_feature' AND columnname='sector_id' AND tabname='tab_none';
 DELETE FROM config_form_fields WHERE formname='ve_exploitation' AND formtype='form_feature' AND columnname='muni_id' AND tabname='tab_none';
+
+-- 20/05/2026
+UPDATE plan_psector SET status = 3 WHERE status = 4;
+
+ALTER TABLE plan_typevalue DISABLE TRIGGER ALL;
+DELETE FROM plan_typevalue WHERE typevalue='psector_status' AND id='4';
+ALTER TABLE plan_typevalue ENABLE TRIGGER ALL;
