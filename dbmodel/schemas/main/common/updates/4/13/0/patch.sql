999de923e1bb60b2e2fe186e3980aad47bfb2a3d
mguzman14 <mguzman@bgeo.es>
2026-06-03 14:44:50 +0200

    fix: add cols to views

--- dbmodel/schemas/main/common/updates/4/13/0/patch.sql
+++ dbmodel/schemas/main/common/updates/4/13/0/patch.sql
@@ -20,3 +20,29 @@ WHERE id=3586;
 UPDATE sys_message
 SET error_message='There are %v_count_state2% arcs with state 2 without start/final nodes.'
 WHERE id=3588;
+
+DO $$ 
+
+DECLARE
+rec record;
+
+BEGIN
+
+    FOR rec IN SELECT lower(id) as id FROM sys_feature_type --arc/node/connec/link/etc
+    LOOP
+
+    EXECUTE format('ALTER TABLE %s ADD COLUMN IF NOT EXISTS dataquality INTEGER', rec.id);
+    EXECUTE format('ALTER TABLE %s ADD COLUMN IF NOT EXISTS dataquality_obs _int4 DEFAULT ARRAY[0]', rec.id);
+
+    END LOOP;
+
+END $$;
+
+
+INSERT INTO config_form_fields (formname, formtype, tabname, columnname, layoutname, layoutorder, "datatype", widgettype, "label", tooltip, placeholder, ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytext_filterc, stylesheet, widgetcontrols, widgetfunction, linkedobject, hidden, web_layoutorder) 
+SELECT child_layer, 'form_feature', 'tab_data', 'dataquality', 'lyt_data_1', 56, 'integer', 'text', 'Dataquality', 'To indicate the number of closing-opening turns when operating the valve.', NULL, false, false, true, false, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, false, NULL 
+FROM cat_feature ON CONFLICT DO NOTHING;
+
+INSERT INTO config_form_fields (formname, formtype, tabname, columnname, layoutname, layoutorder, "datatype", widgettype, "label", tooltip, placeholder, ismandatory, isparent, iseditable, isautoupdate, isfilter, dv_querytext, dv_orderby_id, dv_isnullvalue, dv_parent_id, dv_querytext_filterc, stylesheet, widgetcontrols, widgetfunction, linkedobject, hidden, web_layoutorder) 
+SELECT child_layer, 'form_feature', 'tab_data', 'dataquality_obs', 'lyt_data_1', 58, 'text', 'text', 'Dataquality_obs', 'Observations supporting the assigned utility survey quality level.', NULL, false, false, true, false, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, false, NULL 
+FROM cat_feature ON CONFLICT DO NOTHING;
