fdea7d2e7ea604bed484b20122b847c549db020b
Daniel Marín <dmarin@bgeo.es>
2026-06-04 08:59:37 +0200

    fix(changelog): remove default value for dataquality_obs columns and add corresponding SQL patches

---
+++ dbmodel/schemas/main/common/updates/4/13/1/patch.sql
@@ -0,0 +1,15 @@
+/*
+This file is part of Giswater
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+ALTER TABLE arc ALTER COLUMN dataquality_obs DROP DEFAULT;
+ALTER TABLE node ALTER COLUMN dataquality_obs DROP DEFAULT;
+ALTER TABLE connec ALTER COLUMN dataquality_obs DROP DEFAULT;
+ALTER TABLE link ALTER COLUMN dataquality_obs DROP DEFAULT;
+ALTER TABLE element ALTER COLUMN dataquality_obs DROP DEFAULT;
