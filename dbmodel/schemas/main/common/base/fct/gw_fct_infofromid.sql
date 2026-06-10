5db8bc895735eca45106d0a557e721f7d03bd79f
Daniel Marín <dmarin@bgeo.es>
2026-06-10 10:09:43 +0200

    refactor(gw_fct_infofromid): remove redundant ID generation logic for specific table names to streamline code

--- dbmodel/schemas/main/common/base/fct/gw_fct_infofromid.sql
+++ dbmodel/schemas/main/common/base/fct/gw_fct_infofromid.sql
@@ -769,14 +769,6 @@ BEGIN
 			-- TODO: revise this code
 			IF v_id IS NULL AND v_isepa IS true THEN
 			    v_id = '';
-			ELSIF v_id IS NULL AND v_tablename in  ('ve_dma', 've_dqa', 've_sector', 've_drainzone', 've_supplyzone', 've_macrodma', 've_macrodqa', 've_macrosector', 've_dwfzone', 've_omzone', 've_macroomzone', 've_presszone') THEN
-				v_zone = replace(v_tablename,'ve_','');
-				v_querystring = format('SELECT max(%I_id::integer)+1 FROM %I WHERE %I_id::text ~ ''^[0-9]+$''', v_zone, v_zone, v_zone);
-				EXECUTE v_querystring INTO v_id;
-			ELSIF v_id IS NULL AND v_tablename IN ('plan_netscenario_dma', 'plan_netscenario_presszone') THEN
-				v_zone = replace(v_tablename, 'plan_netscenario_', '');
-				v_querystring = format('SELECT coalesce(max(%I_id::integer)+1, 1) FROM %I WHERE %I_id::text ~ ''^[0-9]+$''', v_zone, v_tablename, v_zone);
-				EXECUTE v_querystring INTO v_id;
             ELSIF v_id IS NULL AND v_featuretype = 'flwreg' THEN
                 -- WARNING: this code is also in gw_fct_getfeatureupsert. If it needs to be changed here, it will most likely have to be changed there too.
                 -- Get node id from initial clicked point
