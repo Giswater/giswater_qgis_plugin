6451e454c7c55c2d53884161ecd139b11d0e558a
Daniel Marín <dmarin@bgeo.es>
2026-06-19 12:19:12 +0200

    feat(publi): add initial schema manifest and related SQL functions for version management

--- dbmodel/schemas/addon/publi/base/base.sql
+++ dbmodel/schemas/addon/publi/base/base.sql
@@ -7,6 +7,19 @@ or (at your option) any later version.
 
 SET search_path = publi, public;
 
+CREATE TABLE publi.sys_version (
+	id serial4 NOT NULL,
+	giswater varchar(16) NOT NULL,
+	project_type varchar(16) NOT NULL,
+	postgres varchar(512) NOT NULL,
+	postgis varchar(512) NOT NULL,
+	"date" timestamp(6) DEFAULT now() NOT NULL,
+	"language" varchar(50) NOT NULL,
+	epsg int4 NOT NULL,
+    addparam jsonb,
+	CONSTRAINT sys_version_pkey PRIMARY KEY (id)
+);
+
 CREATE TABLE publi.vm_refresh_list (
 	id serial4 NOT NULL,
 	mv_name text NOT NULL,
