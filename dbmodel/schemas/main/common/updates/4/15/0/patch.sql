ff7d45c445c5cc762b8fdaa2a84070238146a1a3
Daniel Marín <dmarin@bgeo.es>
2026-06-22 13:20:06 +0200

    refactor(patch.sql): encapsulate SQL logic in a DO block for managing view dependencies and updating wmeter_number type

--- dbmodel/schemas/main/common/updates/4/15/0/patch.sql
+++ dbmodel/schemas/main/common/updates/4/15/0/patch.sql
@@ -8,8 +8,11 @@ or (at your option) any later version.
 
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
-IF (SELECT value::boolean FROM config_param_system WHERE parameter = 'admin_cibs_schema') IS NOT TRUE THEN
-    SELECT gw_fct_admin_manage_view_dependencies($${"data":{"action":"SAVE-DROP","rootViews":["v_hydrometer"],"batchId":1}}$$)::JSON;
-    ALTER TABLE ext_hydrometer ALTER COLUMN wmeter_number TYPE text USING wmeter_number::text;
-    SELECT gw_fct_admin_manage_view_dependencies($${"data":{"action":"RESTORE","rootViews":["v_hydrometer"],"batchId":1}}$$)::JSON;
-END IF;
+DO $$
+BEGIN
+    IF (SELECT value::boolean FROM config_param_system WHERE parameter = 'admin_cibs_schema') IS NOT TRUE THEN
+        PERFORM gw_fct_admin_manage_view_dependencies($${"data":{"action":"SAVE-DROP","rootViews":["v_hydrometer"],"batchId":1}}$$)::JSON;
+        ALTER TABLE ext_hydrometer ALTER COLUMN wmeter_number TYPE text USING wmeter_number::text;
+        PERFORM gw_fct_admin_manage_view_dependencies($${"data":{"action":"RESTORE","rootViews":["v_hydrometer"],"batchId":1}}$$)::JSON;
+    END IF;
+END $$;
