de7efad4faf8f03cca599393cf8f1ae5ba53e442
Daniel Marín <dmarin@bgeo.es>
2026-06-22 13:23:03 +0200

    refactor(patch.sql): enhance view dependency management by using json_build_object for action parameters

--- dbmodel/schemas/main/common/updates/4/15/0/patch.sql
+++ dbmodel/schemas/main/common/updates/4/15/0/patch.sql
@@ -8,11 +8,27 @@ or (at your option) any later version.
 
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
-DO $$
+DO $patch$
 BEGIN
     IF (SELECT value::boolean FROM config_param_system WHERE parameter = 'admin_cibs_schema') IS NOT TRUE THEN
-        PERFORM gw_fct_admin_manage_view_dependencies($${"data":{"action":"SAVE-DROP","rootViews":["v_hydrometer"],"batchId":1}}$$)::JSON;
+        PERFORM gw_fct_admin_manage_view_dependencies(
+            json_build_object(
+                'data', json_build_object(
+                    'action', 'SAVE-DROP',
+                    'rootViews', json_build_array('v_hydrometer'),
+                    'batchId', 1
+                )
+            )
+        );
         ALTER TABLE ext_hydrometer ALTER COLUMN wmeter_number TYPE text USING wmeter_number::text;
-        PERFORM gw_fct_admin_manage_view_dependencies($${"data":{"action":"RESTORE","rootViews":["v_hydrometer"],"batchId":1}}$$)::JSON;
+        PERFORM gw_fct_admin_manage_view_dependencies(
+            json_build_object(
+                'data', json_build_object(
+                    'action', 'RESTORE',
+                    'rootViews', json_build_array('v_hydrometer'),
+                    'batchId', 1
+                )
+            )
+        );
     END IF;
-END $$;
+END $patch$;
