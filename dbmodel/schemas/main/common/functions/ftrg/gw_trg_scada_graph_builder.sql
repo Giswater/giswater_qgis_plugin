567416c5ba2f97cf3915f5127ac00eb464377cb5
mguzman14 <mguzman@bgeo.es>
2026-07-03 14:48:49 +0200

    fix: rename trigger with standard name

--- dbmodel/schemas/main/common/functions/ftrg/gw_trg_scada_graph_builder.sql
+++ dbmodel/schemas/main/common/functions/ftrg/gw_trg_scada_graph_builder.sql
@@ -5,9 +5,9 @@ General Public License as published by the Free Software Foundation, either vers
 or (at your option) any later version.
 */
 
-DROP FUNCTION IF EXISTS SCHEMA_NAME.gw_fct_scada_graph_builder();
+DROP FUNCTION IF EXISTS SCHEMA_NAME.gw_trg_scada_graph_builder();
 
-CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_scada_graph_builder()
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_trg_scada_graph_builder()
  RETURNS trigger
  LANGUAGE plpgsql
 AS $function$
