58e3f94fc79248effc96ce30d6106fd26367246f
Daniel Marín <dmarin@bgeo.es>
2026-07-17 13:38:44 +0200

    fix(config): temporarily disable trigger while updating `dv_querytext` in `config_form_fields` for correct view and customer code names

--- dbmodel/schemas/main/common/updates/4/15/3/patch.sql
+++ dbmodel/schemas/main/common/updates/4/15/3/patch.sql
@@ -8,5 +8,7 @@ or (at your option) any later version.
 
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
+ALTER TABLE config_form_fields DISABLE TRIGGER gw_trg_config_control;
 UPDATE config_form_fields SET dv_querytext = REPLACE(dv_querytext, 'v_rtc_hydrometer', 'vf_hydrometer') WHERE dv_querytext ILIKE '%v_rtc_hydrometer%';
 UPDATE config_form_fields SET dv_querytext = REPLACE(dv_querytext, 'hydrometer_customer_code', 'hydro_customer_code') WHERE dv_querytext ILIKE '%hydrometer_customer_code%';
+ALTER TABLE config_form_fields ENABLE TRIGGER gw_trg_config_control;
